898fe3be97490ce90f0257bd03ea4aa9354f4fff
yennanliue <f339339@gmail.com>
2018-06-09 15:44:17 +0100

    add consecutive-numbers sql

---
+++ leetcode_SQL/consecutive-numbers.sql
@@ -0,0 +1,72 @@
+
+
+/*
+
+
+Write a SQL query to find all numbers that appear at least three times consecutively.
+
++----+-----+
+| Id | Num |
++----+-----+
+| 1  |  1  |
+| 2  |  1  |
+| 3  |  1  |
+| 4  |  2  |
+| 5  |  1  |
+| 6  |  2  |
+| 7  |  2  |
++----+-----+
+For example, given the above Logs table, 1 is the only number that appears consecutively for at least three times.
+
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+
+
+
+# https://leetcode.com/problems/consecutive-numbers/description/
+# https://github.com/kamyu104/LeetCode/blob/master/MySQL/consecutive-numbers.sql
+
+
+
+
+/ * V1 */
+
+# join 3 same table to grab 2nd element 
+# (if there are at least consecutively 3 elements)
+
+
+SELECT DISTINCT a.Num AS ConsecutiveNums 
+FROM Logs a,
+     Logs b,
+     Logs c
+WHERE a.Num = b.Num
+  AND b.Id +1 = a.Id
+  AND c.Num = a.Num
+  AND c.Id = a.Id +1
+
+
+
+
+/* V2 */
+
+
+
+SELECT DISTINCT(Num) AS ConsecutiveNums
+FROM (
+    SELECT
+    Num,
+    @counter := IF(@prev = Num, @counter + 1, 1) AS how_many_cnt_in_a_row,
+    @prev := Num
+    FROM Logs y, (SELECT @counter:=1, @prev:=NULL) vars
+) sq
+WHERE how_many_cnt_in_a_row >= 3
+
+
+
+
+
+
+
