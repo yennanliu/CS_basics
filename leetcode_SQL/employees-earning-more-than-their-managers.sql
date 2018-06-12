65892a1c5554450286f2a0dabe65dc49c132150e
yennanliue <f339339@gmail.com>
2018-06-12 06:44:56 +0100

    add employees-earning-more-than-their-managers sql

---
+++ leetcode_SQL/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,63 @@
+
+
+/*
+
+https://leetcode.com/problems/employees-earning-more-than-their-managers/description/
+
+
+
+The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++----+-------+--------+-----------+
+| Id | Name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+
+
+
+*/ 
+
+
+/* V1  */
+
+
+SELECT a.Name as Employee
+FROM Employee a
+LEFT JOIN Employee b ON a.ManagerId = b.Id
+WHERE a.Salary > b.Salary
+
+
+
+
+
+/* V2 */
+
+
+SELECT Name AS Employee 
+    FROM Employee e 
+    WHERE e.ManagerId IS NOT NULL AND e.Salary > (SELECT Salary 
+                          FROM Employee 
+                          WHERE e.ManagerId = Id)
+
+
+
+
+
+                          
+
+
+
+
+
+
