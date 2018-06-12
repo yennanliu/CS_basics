ce96930a9629c6f7a4005c1839c73388dc774dd3
yennanliue <f339339@gmail.com>
2018-06-12 06:51:50 +0100

    add duplicate-emails.sql

---
+++ leetcode_SQL/duplicate-emails.sql
@@ -0,0 +1,52 @@
+
+
+/* 
+
+
+https://leetcode.com/problems/duplicate-emails/description/
+
+
+
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+
+
+
+
+*/ 
+
+/* V1 */
+
+SELECT Email
+FROM
+  (SELECT Email,
+          count(*)
+   FROM Person a
+   GROUP BY Email
+   HAVING count(*) > 1) sub
+
+
+
+/* V2 */ 
+
+
+
+SELECT Email FROM Person GROUP BY Email HAVING COUNT(*) > 1
+
+
+
+
