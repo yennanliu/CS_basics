c9d98714deaa8a6654b1c34eb3b4b9a5cadf6801
yennanliue <f339339@gmail.com>
2018-06-12 21:04:32 +0100

    backup dev code

---
+++ leetcode_python/reverse-bits.py
@@ -0,0 +1,27 @@
+
+
+
+"""
+
+https://leetcode.com/problems/reverse-bits/description/
+
+
+# Time : O(logn) = O(32)
+# Space: O(1)
+#
+# Reverse bits of a given 32 bits unsigned integer.
+#
+# For example, given input 43261596 (represented in binary as
+# 00000010100101000001111010011100), return 964176192 (represented in binary
+# as 00111001011110000010100101000000).
+#
+# Follow up:
+# If this function is called many times, how would you optimize it?
+#
+
+
+"""
+
+
+# dev 
+
