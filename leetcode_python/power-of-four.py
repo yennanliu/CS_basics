87901711fc91eae50af471ac632d3db400ccb7ff
yennanliue <f339339@gmail.com>
2018-06-14 00:17:35 +0100

    backup code

---
+++ leetcode_python/power-of-four.py
@@ -0,0 +1,15 @@
+
+# https://leetcode.com/problems/power-of-four/description/
+
+# Time:  O(1)
+# Space: O(1)
+
+# Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+#
+# Example:
+# Given num = 16, return true. Given num = 5, return false.
+#
+# Follow up: Could you solve it without loops/recursion?
+
+
+# dev 
---
+++ leetcode_python/sum-of-two-integers.py
@@ -0,0 +1,14 @@
+
+
+# https://leetcode.com/problems/sum-of-two-integers/description/
+# Time:  O(1)
+# Space: O(1)
+
+# Calculate the sum of two integers a and b,
+# but you are not allowed to use the operator + and -.
+#
+# Example:
+# Given a = 1 and b = 2, return 3.
+
+
+# dev 
