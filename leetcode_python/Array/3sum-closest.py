8553bd627715d868111d3a85b2b25c7112e6fa34
yennanliue <f339339@gmail.com>
2018-06-14 09:14:42 +0100

    backup array python code

---
+++ leetcode_python/Array/3sum-closest.py
@@ -0,0 +1,49 @@
+# https://leetcode.com/problems/3sum-closest/description/
+# Time:  O(n^2)
+# Space: O(1)
+
+# Given an array S of n integers,
+# find three integers in S such that the sum is closest to a given number,
+# target.
+# Return the sum of the three integers.
+# You may assume that each input would have exactly one solution.
+#
+# For example, given array S = {-1 2 1 -4}, and target = 1.
+#
+# The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+
+
+# V1 : dev 
+
+
+
+
+# V2 
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums, result, min_diff, i = sorted(nums), float("inf"), float("inf"), 0
+        while i < len(nums) - 2:
+            if i == 0 or nums[i] != nums[i - 1]:
+                j, k = i + 1, len(nums) - 1
+                while j < k:
+                    diff = nums[i] + nums[j] + nums[k] - target
+                    if abs(diff) < min_diff:
+                        min_diff = abs(diff)
+                        result = nums[i] + nums[j] + nums[k]
+                    if diff < 0:
+                        j += 1
+                    elif diff > 0:
+                        k -= 1
+                    else:
+                        return target
+            i += 1
+        return result
+
+
+
