616c45c31a5859caa30f645b786a609cfa4e064e
yennanliue <f339339@gmail.com>
2018-06-16 17:27:33 +0100

    backup dev code

---
+++ leetcode_python/Array/spiral-matrix-ii.py
@@ -0,0 +1,16 @@
+
+# Time:  O(n^2)
+# Space: O(1)
+#
+# Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+#
+# For example,
+# Given n = 3,
+#
+# You should return the following matrix:
+# [
+#  [ 1, 2, 3 ],
+#  [ 8, 9, 4 ],
+#  [ 7, 6, 5 ]
+# ]
+#
---
+++ leetcode_python/Array/spiral-matrix.py
@@ -0,0 +1,20 @@
+
+
+# Time:  O(m * n)
+# Space: O(1)
+#
+# Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+#
+# For example,
+# Given the following matrix:
+#
+# [
+#  [ 1, 2, 3 ],
+#  [ 4, 5, 6 ],
+#  [ 7, 8, 9 ]
+# ]
+# You should return [1,2,3,6,9,8,7,4,5].
+#
+
+
+# V1 : dev 
