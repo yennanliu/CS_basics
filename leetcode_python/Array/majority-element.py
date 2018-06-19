d7f071f95d3c3746abe12bf02f5f79a411785df9
yennanliue <f339339@gmail.com>
2018-06-19 05:24:21 +0100

    add majority-element and backup dev code

---
+++ leetcode_python/Array/majority-element.py
@@ -0,0 +1,69 @@
+"""
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+"""
+
+
+# V1 
+class Solution:
+	def majorityElement(self, nums):
+		len_nums = len(nums)
+		for num in list(set(nums)):
+			if nums.count(num) > len_nums/2:
+				return num
+			else:
+				pass 
+
+
+# V2 
+
+import collections
+
+
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        idx, cnt = 0, 1
+
+        for i in xrange(1, len(nums)):
+            if nums[idx] == nums[i]:
+                cnt += 1
+            else:
+                cnt -= 1
+                if cnt == 0:
+                    idx = i
+                    cnt = 1
+
+        return nums[idx]
+
+    def majorityElement2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sorted(collections.Counter(nums).items(), key=lambda a: a[1], reverse=True)[0][0]
+
+
+        
+
+
+
+
+
+
+
+
+
---
+++ leetcode_python/Array/read-n-characters-given-read4.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# The API: int read4(char *buf) reads 4 characters at a time from a file.
+#
+# The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
+#
+# By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+#
+# Note:
+# The read function will only be called once for each test case.
+#
+
+# The read4 API is already defined for you.
+# @param buf, a list of characters
+# @return an integer
+
+
+# V1 : dev 
+# https://github.com/yennanliu/LeetCode/blob/master/Python/read-n-characters-given-read4.py
