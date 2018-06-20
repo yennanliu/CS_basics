f10c746a7b45563194f81854590911bc447c9ec8
yennanliu <f339339@gmail.com>
2018-06-20 17:23:59 +0800

    add find-all-numbers-disappeared-in-an-array and dev python code

---
+++ leetcode_python/Array/find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,64 @@
+
+"""
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+"""
+
+
+# V1 
+
+class Solution(object):
+	def findDisappearedNumbers(self, nums):
+		try:
+			return list( set(range(1, len(nums)+1)) - set(nums))
+		except:
+			return None 
+
+
+
+# V2 
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in xrange(len(nums)):
+            if nums[abs(nums[i]) - 1] > 0:
+                nums[abs(nums[i]) - 1] *= -1
+
+        result = []
+        for i in xrange(len(nums)):
+            if nums[i] > 0:
+                result.append(i+1)
+            else:
+                nums[i] *= -1
+        return result
+
+    def findDisappearedNumbers2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return list(set(range(1, len(nums) + 1)) - set(nums))
+
+    def findDisappearedNumbers3(self, nums):
+        for i in range(len(nums)):
+            index = abs(nums[i]) - 1
+            nums[index] = - abs(nums[index])
+
+        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
+
+
+
