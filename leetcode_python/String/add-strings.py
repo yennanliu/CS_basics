046dad5ffcfa8e7c6dc09a08b27c6d20955f6031
yennanliue <f339339@gmail.com>
2018-06-30 09:02:34 +0100

    add add-strings python code

---
+++ leetcode_python/String/add-strings.py
@@ -0,0 +1,90 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Given two non-negative numbers num1 and num2 represented as string,
+# return the sum of num1 and num2.
+#
+# Note:
+#
+# The length of both num1 and num2 is < 5100.
+# Both num1 and num2 contains only digits 0-9.
+# Both num1 and num2 does not contain any leading zero.
+# You must not use any built-in BigInteger library or
+# convert the inputs to integer directly.
+
+
+
+# V1 
+class Solution:
+    def addStrings(self, num1, num2):
+        output = []
+        carry_fix = 0 
+        lenth_ = max(len(num1), len(num2))
+        num1 = '0'*(lenth_ - len(num1)) + num1 
+        num2 = '0'*(lenth_ - len(num2)) + num2
+        print (num1)
+        print (num2)
+        for i in reversed(range(len(num1))):
+            sum_ = int(num1[i])+ int(num2[i]) + int(carry_fix)
+            if (sum_ >= 10 and i != 0 ):
+                sum_ = sum_ - 10 
+                output.append( str(sum_))
+                carry_fix = 1 
+            elif (sum_ >= 10 and i ==0):
+                output.append( str(sum_))        
+            else:
+                output.append( str(sum_))    
+                carry_fix = 0 
+        return ''.join(list(reversed(output)))
+            
+             
+        
+           
+
+# V2 
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        result = []
+        i, j, carry = len(num1) - 1, len(num2) - 1, 0
+
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += ord(num1[i]) - ord('0')
+                i -= 1
+            if j >= 0:
+                carry += ord(num2[j]) - ord('0')
+                j -= 1
+            result.append(str(carry % 10))
+            carry /= 10
+        result.reverse()
+
+        return "".join(result)
+
+    def addStrings2(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        length = max(len(num1), len(num2))
+        num1 = num1.zfill(length)[::-1]
+        num2 = num2.zfill(length)[::-1]
+        res, plus = '', 0
+        for index, num in enumerate(num1):
+            tmp = str(int(num) + int(num2[index]) + plus)
+            res += tmp[-1]
+            if int(tmp) > 9:
+                plus = 1
+            else:
+                plus = 0
+        if plus:
+            res += '1'
+        return res[::-1]
+
+
+
