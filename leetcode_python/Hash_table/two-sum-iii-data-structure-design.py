5a7c899f11527a53e929e49ef0d0337267bf2cbe
yennanliue <f339339@gmail.com>
2018-07-05 06:07:32 +0100

    backup dev code

---
+++ leetcode_python/Hash_table/two-sum-iii-data-structure-design.py
@@ -0,0 +1,91 @@
+"""
+
+
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+For example,
+
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+
+"""
+
+# Time:  O(n)
+# Space: O(n)
+
+# Design and implement a TwoSum class. It should support the following operations: add and find.
+#
+# add - Add the number to an internal data structure.
+# find - Find if there exists any pair of numbers which sum is equal to the value.
+#
+# For example,
+# add(1); add(3); add(5);
+# find(4) -> true
+# find(7) -> false
+
+
+# V1  : dev 
+
+
+
+# V2 
+from collections import defaultdict
+
+class TwoSum(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here
+        """
+        self.lookup = defaultdict(int)
+
+
+
+    def add(self, number):
+        """
+        Add the number to an internal data structure.
+        :rtype: nothing
+        """
+        self.lookup[number] += 1
+
+
+    def find(self, value):
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        :type value: int
+        :rtype: bool
+        """
+        for key in self.lookup:
+            num = value - key
+            if num in self.lookup and (num != key or self.lookup[key] > 1):
+                return True
+        return False
+
+
+# V3 
+
+class TwoSum(object):
+
+    def __init__(self):
+        self.dic = {}
+
+    def add(self, number):
+        if number not in self.dic:
+            self.dic[number] = 1
+        else:
+            self.dic[number] += 1
+
+    def find(self, value):
+        dic = self.dic
+        for num in dic:
+            if value - num in dic and (value - num != num or dic[num] > 1):
+                return True
+        return False
+
+
+
+
---
+++ leetcode_python/Hash_table/valid-sudoku.py
@@ -0,0 +1,57 @@
+# Time:  O(9^2)
+# Space: O(9)
+
+# Determine if a Sudoku is valid,
+# according to: Sudoku Puzzles - The Rules.
+#
+# The Sudoku board could be partially filled,
+# where empty cells are filled with the character '.'.
+#
+# A partially filled sudoku which is valid.
+#
+# Note:
+# A valid Sudoku board (partially filled) is not necessarily solvable.
+# Only the filled cells need to be validated.
+
+
+# V1 : dev 
+
+
+
+
+# V2 
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        for i in xrange(9):
+            if not self.isValidList([board[i][j] for j in xrange(9)]) or \
+               not self.isValidList([board[j][i] for j in xrange(9)]):
+                return False
+        for i in xrange(3):
+            for j in xrange(3):
+                if not self.isValidList([board[m][n] for n in xrange(3 * j, 3 * j + 3) \
+                                                     for m in xrange(3 * i, 3 * i + 3)]):
+                    return False
+        return True
+
+    def isValidList(self, xs):
+        xs = filter(lambda x: x != '.', xs)
+        return len(set(xs)) == len(xs)
+
+
+if __name__ == "__main__":
+    board = [[1, '.', '.', '.', '.', '.', '.', '.', '.'],
+             ['.', 2, '.', '.', '.', '.', '.', '.', '.'],
+             ['.', '.', 3, '.', '.', '.', '.', '.', '.'],
+             ['.', '.', '.', 4, '.', '.', '.', '.', '.'],
+             ['.', '.', '.', '.', 5, '.', '.', '.', '.'],
+             ['.', '.', '.', '.', '.', 6, '.', '.', '.'],
+             ['.', '.', '.', '.', '.', '.', 7, '.', '.'],
+             ['.', '.', '.', '.', '.', '.', '.', 8, '.'],
+             ['.', '.', '.', '.', '.', '.', '.', '.', 9]]
+    print Solution().isValidSudoku(board)
+
+    
