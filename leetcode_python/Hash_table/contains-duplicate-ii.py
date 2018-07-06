69547a8cbbc94671c54befe38c4701ffa23fa38d
yennanliue <f339339@gmail.com>
2018-07-06 07:11:18 +0100

    add strobogrammatic-number and backup dev code

---
+++ leetcode_python/Hash_table/contains-duplicate-ii.py
@@ -0,0 +1,54 @@
+
+"""
+
+Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+
+"""
+
+# Time:  O(n)
+# Space: O(n)
+#
+# Given an array of integers and an integer k, return true if
+# and only if there are two distinct indices i and j in the array
+# such that nums[i] = nums[j] and the difference between i and j is at most k.
+#
+
+
+
+# V1 : dev 
+
+
+# V2 
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {boolean}
+    def containsNearbyDuplicate(self, nums, k):
+        lookup = {}
+        for i, num in enumerate(nums):
+            if num not in lookup:
+                lookup[num] = i
+            else:
+                # It the value occurs before, check the difference.
+                if i - lookup[num] <= k:
+                    return True
+                # Update the index of the value.
+                lookup[num] = i
+        return False
+
+
+
---
+++ leetcode_python/Hash_table/strobogrammatic-number.py
@@ -0,0 +1,43 @@
+
+"""
+
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Write a function to determine if a number is strobogrammatic. The number is represented as a string.
+
+For example, the numbers "69", "88", and "818" are all strobogrammatic.
+
+"""
+
+# Time:  O(n)
+# Space: O(1)
+
+
+
+# V1  :  need to double check 
+class Solution:
+    lookup = {'0':'0', '1':'1', '6':'9', '8':'8', '9':'6'}
+    def isStrobogrammatic(self, nums):
+        for i, num in enumerate(list(nums)):
+            #print (num)
+            if str(num) in lookup:
+                pass
+            else:
+                return False
+        return True 
+
+
+
+# V2 
+class Solution:
+    lookup = {'0':'0', '1':'1', '6':'9', '8':'8', '9':'6'}
+
+    # @param {string} num
+    # @return {boolean}
+    def isStrobogrammatic(self, num):
+        n = len(num)
+        for i in xrange((n+1) / 2):
+            if num[n-1-i] not in self.lookup or \
+               num[i] != self.lookup[num[n-1-i]]:
+                return False
+        return True
