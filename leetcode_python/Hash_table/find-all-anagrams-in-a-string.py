6ed84f1bb81dbaf71a19bc3cfc64de3215daecf8
yennanliue <f339339@gmail.com>
2018-07-07 10:47:03 +0100

    backup dev code

---
+++ leetcode_python/Hash_table/find-all-anagrams-in-a-string.py
@@ -0,0 +1,161 @@
+"""
+Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+
+Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+
+The order of output does not matter.
+
+Example 1:
+
+Input:
+s: "cbaebabacd" p: "abc"
+
+Output:
+[0, 6]
+
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+Example 2:
+
+Input:
+s: "abab" p: "ab"
+
+Output:
+[0, 1, 2]
+
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+
+"""
+# Time:  O(n)
+# Space: O(1)
+
+# Given a string s and a non-empty string p, find all the start indices
+# of p's anagrams in s.
+#
+# Strings consists of lowercase English letters only and the length of
+# both strings s and p will not be larger than 20,100.
+#
+# The order of output does not matter.
+#
+# Example 1:
+#
+# Input:
+# s: "cbaebabacd" p: "abc"
+#
+# Output:
+# [0, 6]
+#
+# Explanation:
+# The substring with start index = 0 is "cba", which is an anagram of "abc".
+# The substring with start index = 6 is "bac", which is an anagram of "abc".
+# Example 2:
+#
+# Input:
+# s: "abab" p: "ab"
+#
+# Output:
+# [0, 1, 2]
+#
+# Explanation:
+# The substring with start index = 0 is "ab", which is an anagram of "ab".
+# The substring with start index = 1 is "ba", which is an anagram of "ab".
+# The substring with start index = 2 is "ab", which is an anagram of "ab".
+
+
+# V1  : dev 
+
+"""
+class Solution(object):
+    def permutation(lst):
+ 
+    # If lst is empty then there are no permutations
+    if len(lst) == 0:
+        return []
+ 
+    # If there is only one element in lst then, only
+    # one permuatation is possible
+    if len(lst) == 1:
+        return [lst]
+ 
+    # Find the permutations for lst if there are
+    # more than 1 characters
+ 
+    l = [] # empty list that will store current permutation
+ 
+    # Iterate the input(lst) and calculate the permutation
+    for i in range(len(lst)):
+       m = lst[i]
+ 
+       # Extract lst[i] or m from the list.  remLst is
+       # remaining list
+       remLst = lst[:i] + lst[i+1:]
+ 
+       # Generating all permutations where m is first
+       # element
+       for p in permutation(remLst):
+           l.append([m] + p)
+    return l
+ 
+    def findAnagrams(self, s, p):
+        output = []
+        #p_list = [ p[i:] + p [: -(len(p) - i)] for i in range(len(p)) ] 
+        for p_ in permutation(p):
+            p_list.append(''.join(p))
+        for p_ in p_list:
+            if p_ in s:
+                output.append(s.find(p_))
+            else:
+                pass
+        return output 
+
+"""
+"""
+class Solution(object):
+    def findAnagrams(self, s, p):
+        output = []
+        p_list = [ p[i:] + p [: -(len(p) - i)] for i in range(len(p)) ] 
+        print (p_list)
+        for p_ in p_list:
+            if p_ in s:
+                output.append(s.find(p_))
+            else:
+                pass
+        return output 
+
+
+"""
+
+
+
+
+# V2 
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        result = []
+
+        cnts = [0] * 26
+        for c in p:
+            cnts[ord(c) - ord('a')] += 1
+
+        left, right = 0, 0
+        while right < len(s):
+            cnts[ord(s[right]) - ord('a')] -= 1
+            while left <= right and cnts[ord(s[right]) - ord('a')] < 0:
+                cnts[ord(s[left]) - ord('a')] += 1
+                left += 1
+            if right - left + 1 == len(p):
+                result.append(left)
+            right += 1
+
+        return result
+
+
