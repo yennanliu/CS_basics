945bf271ca7df820b11d882e239b1f3284c3fde3
yennanliue <f339339@gmail.com>
2018-07-07 08:00:54 +0100

    backup dev code

---
+++ leetcode_python/Hash_table/unique-word-abbreviation.py
@@ -0,0 +1,76 @@
+"""
+
+An abbreviation of a word follows the form <first letter><number><last letter>. Below are some examples of word abbreviations:
+
+a) it                      --> it    (no abbreviation)
+
+     1
+b) d|o|g                   --> d1g
+
+              1    1  1
+     1---5----0----5--8
+c) i|nternationalizatio|n  --> i18n
+
+              1
+     1---5----0
+d) l|ocalizatio|n          --> l10n
+Assume you have a dictionary and given a word, find whether its abbreviation is unique in the dictionary. A word's abbreviation is unique if no other word from the dictionary has the same abbreviation.
+
+Example: 
+
+Given dictionary = [ "deer", "door", "cake", "card" ]
+
+isUnique("dear") -> false
+isUnique("cart") -> true
+isUnique("cane") -> false
+isUnique("make") -> true
+
+http://leetcode.com/problems/unique-word-abbreviation/
+
+"""
+
+# Time:  ctor:   O(n), n is number of words in the dictionary.
+#        lookup: O(1)
+# Space: O(k), k is number of unique words.
+
+
+# V1  : dev 
+
+
+
+# V2 
+import collections
+
+
+class ValidWordAbbr(object):
+    def __init__(self, dictionary):
+        """
+        initialize your data structure here.
+        :type dictionary: List[str]
+        """
+        self.lookup_ = collections.defaultdict(set)
+        for word in dictionary:
+            abbr = self.abbreviation(word)
+            self.lookup_[abbr].add(word)
+
+
+    def isUnique(self, word):
+        """
+        check if a word is unique.
+        :type word: str
+        :rtype: bool
+        """
+        abbr = self.abbreviation(word)
+        return self.lookup_[abbr] <= {word}
+
+
+    def abbreviation(self, word):
+        if len(word) <= 2:
+            return word
+        return word[0] + str(len(word)-2) + word[-1]
+
+
+# Your ValidWordAbbr object will be instantiated and called as such:
+# vwa = ValidWordAbbr(dictionary)
+# vwa.isUnique("word")
+# vwa.isUnique("anotherWord")
---
+++ leetcode_python/Hash_table/word-pattern.py
@@ -0,0 +1,110 @@
+# Time:  O(n)
+# Space: O(c), c is unique count of pattern
+
+# Given a pattern and a string str, find if str follows the same pattern.
+#
+# Examples:
+#   1. pattern = "abba", str = "dog cat cat dog" should return true.
+#   2. pattern = "abba", str = "dog cat cat fish" should return false.
+#   3. pattern = "aaaa", str = "dog cat cat dog" should return false.
+#   4. pattern = "abba", str = "dog dog dog dog" should return false.
+#
+# Notes:
+#   1. Both pattern and str contains only lowercase alphabetical letters.
+#   2. Both pattern and str do not have leading or trailing spaces.
+#   3. Each word in str is separated by a single space.
+#   4. Each letter in pattern must map to a word with length that is at least 1.
+
+
+# V1 : dev 
+
+"""
+class Solution2(object):
+    def wordPattern(self, pattern, str):
+        words = str.split()  # Space: O(n)
+        if len(pattern) != len(words):
+            return False
+
+        w2p, p2w = {}, {}
+        for p, w in izip(pattern, words):
+            if w not in w2p and p not in p2w:
+                # Build mapping. Space: O(c)
+                w2p[w] = p
+                p2w[p] = w
+            elif w not in w2p or w2p[w] != p:
+                # Contradict mapping.
+                return False
+        return True
+"""
+
+
+# V2 
+from itertools import izip  # Generator version of zip.
+
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        if len(pattern) != self.wordCount(str):
+            return False
+
+        w2p, p2w = {}, {}
+        for p, w in izip(pattern, self.wordGenerator(str)):
+            if w not in w2p and p not in p2w:
+                # Build mapping. Space: O(c)
+                w2p[w] = p
+                p2w[p] = w
+            elif w not in w2p or w2p[w] != p:
+                # Contradict mapping.
+                return False
+        return True
+
+    def wordCount(self, str):
+        cnt = 1 if str else 0
+        for c in str:
+            if c == ' ':
+                cnt += 1
+        return cnt
+
+    # Generate a word at a time without saving all the words.
+    def wordGenerator(self, str):
+        w = ""
+        for c in str:
+            if c == ' ':
+                yield w
+                w = ""
+            else:
+                w += c
+        yield w
+
+
+# V3 
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        words = str.split()  # Space: O(n)
+        if len(pattern) != len(words):
+            return False
+
+        w2p, p2w = {}, {}
+        for p, w in izip(pattern, words):
+            if w not in w2p and p not in p2w:
+                # Build mapping. Space: O(c)
+                w2p[w] = p
+                p2w[p] = w
+            elif w not in w2p or w2p[w] != p:
+                # Contradict mapping.
+                return False
+        return True
+
+
+
