108b3728111244e8f6d1320e015d6fe9a4d5a55d
yennanliue <f339339@gmail.com>
2018-07-11 01:59:32 +0100

    add ref V2 code and its ref

--- leetcode_python/Hash_table/longest-harmonious-subsequence.py
+++ leetcode_python/Hash_table/longest-harmonious-subsequence.py
@@ -17,6 +17,25 @@
 # V1  : dev 
 
 # V2 
+# https://www.jianshu.com/p/a155ea910fb5
+class Solution(object):
+    def findLHS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_count = {}
+        for num in nums:
+            nums_count[num] = nums_count.get(num, 0) + 1
+
+        result = 0
+        for num in nums_count:
+            count = nums_count[num]
+            if nums_count.get(num+1):
+                result = max(result, count + nums_count[num+1])
+        return result
+
+# V3 
 import collections
 
 
