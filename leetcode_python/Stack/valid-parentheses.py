dd5104c93379d84d2d88aab1d8b56829e57960b2
yennanliue <f339339@gmail.com>
2018-07-12 07:16:35 +0100

    back up dev code

---
+++ leetcode_python/Stack/valid-parentheses.py
@@ -0,0 +1,85 @@
+"""
+
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+
+"""
+
+# Time:  O(n)
+# Space: O(n)
+#
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+# determine if the input string is valid.
+#
+# The brackets must close in the correct order, "()" and "()[]{}"
+# are all valid but "(]" and "([)]" are not.
+#
+
+
+
+# V1 : dev 
+"""
+
+class Solution(object):
+    def isValid(self, s):
+        lookup =  {"(": ")", "{": "}", "[": "]"}
+        # case 1 
+        for i  in range(0,len(s),2):
+            if s[i:i+2] in "()[]{}":
+                pass
+            else:
+                pass
+        pass
+    
+
+"""
+
+
+
+# V2 
+class Solution:
+    # @return a boolean
+    def isValid(self, s):
+        stack, lookup = [], {"(": ")", "{": "}", "[": "]"}
+        for parenthese in s:
+            if parenthese in lookup:
+                stack.append(parenthese)
+            elif len(stack) == 0 or lookup[stack.pop()] != parenthese:
+                return False
+        return len(stack) == 0
+
+if __name__ == "__main__":
+    print Solution().isValid("()[]{}")
+    print Solution().isValid("()[{]}")
+
+
+
+
+
