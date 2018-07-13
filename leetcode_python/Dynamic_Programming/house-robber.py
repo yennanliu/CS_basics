10fe91d3db31904f57b8f51bcc89cccbfe3dbe12
yennanliue <f339339@gmail.com>
2018-07-13 22:46:16 +0100

    backup dev code

---
+++ leetcode_python/Dynamic_Programming/house-robber.py
@@ -0,0 +1,66 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# You are a professional robber planning to rob houses along a street.
+# Each house has a certain amount of money stashed, the only constraint stopping you
+# from robbing each of them is that adjacent houses have security system connected
+# and it will automatically contact the police if two adjacent houses were broken into on the same night.
+#
+# Given a list of non-negative integers representing the amount of money of each house,
+# determine the maximum amount of money you can rob tonight without alerting the police.
+#
+
+
+# V1 : dev ( double check time out error )
+
+class Solution(object):
+    def rob(self, nums):
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        rob_sum = [ sum(nums[1::n]) for n in range(2,len(nums))   ] + [ sum(nums[0::n])  for n in range(2,len(nums))   ]
+        return max(rob_sum)
+    
+
+# V2 
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def rob(self, num):
+        if len(num) == 0:
+            return 0
+
+        if len(num) == 1:
+            return num[0]
+
+        num_i, num_i_1 = max(num[1], num[0]), num[0]
+        for i in xrange(2, len(num)):
+            num_i_1, num_i_2 = num_i, num_i_1
+            num_i = max(num[i] + num_i_2, num_i_1);
+
+        return num_i
+
+    def rob2(self, nums):
+        """
+        rob[i] = nums[i] + pass[i-1]
+        pass[i] = max(rob[i-1], pass[i-1])
+        """
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        last, now = 0, 0
+        for i in nums:
+            last, now = now, max(last + i, now)
+        return now
+
+
+
+"""
+if __name__ == '__main__':
+        print Solution().rob([8,4,8,5,9,6,5,4,4,10])
+"""
+
+
