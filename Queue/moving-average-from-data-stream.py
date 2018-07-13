691aad057a46129d4d68b8925969a5a8a57f7211
yennanliue <f339339@gmail.com>
2018-07-13 09:00:13 +0100

    moving-average-from-data-stream python code

---
+++ Queue/moving-average-from-data-stream.py
@@ -0,0 +1,71 @@
+
+"""
+
+
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+For example,
+
+MovingAverage m = new MovingAverage(3);
+m.next(1) = 1
+m.next(10) = (1 + 10) / 2
+m.next(3) = (1 + 10 + 3) / 3
+m.next(5) = (10 + 3 + 5) / 3
+
+
+"""
+
+
+
+# V1 : dev 
+
+class MovingAverage(object):
+
+    def __init__(self, size):
+        self.__size = size
+        self.__q = deque([])
+
+    def next(self, val):
+        if len(self.__q) == self.__size:
+            self.__q.popleft()            
+        self.__q.append(val)
+        return 1.0 * sum(self.__q) / len(self.__q)
+
+
+
+# V2 
+# Time:  O(1)
+# Space: O(w)
+
+from collections import deque
+
+class MovingAverage(object):
+
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        :type size: int
+        """
+        self.__size = size
+        self.__sum = 0
+        self.__q = deque([])
+
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+        if len(self.__q) == self.__size:
+            self.__sum -= self.__q.popleft()
+        self.__sum += val
+        self.__q.append(val)
+        return 1.0 * self.__sum / len(self.__q)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
+
+
+
+
