ff3afed40d4e4ef9554edebeb26b0005de9f134a
yennanliue <f339339@gmail.com>
2018-07-14 09:20:22 +0100

    backup dev code

---
+++ leetcode_python/Dynamic_Programming/rotated-digits.py
@@ -0,0 +1,113 @@
+# Time:  O(logn)
+# Space: O(logn)
+
+# X is a good number if after rotating each digit individually by 180 degrees,
+# we get a valid number that is different from X.
+# A number is valid if each digit remains a digit after rotation.
+# 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other;
+# 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number.
+#
+# Now given a positive number N, how many numbers X from 1 to N are good?
+#
+# Example:
+# Input: 10
+# Output: 4
+# Explanation:
+# There are four good numbers in the range [1, 10] : 2, 5, 6, 9.
+# Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
+#
+# Note:
+# - N  will be in range [1, 10000].
+
+
+# V1  : dev 
+"""
+class Solution(object):
+    def rotatedDigits(self, N):
+        N_ = range(N[0], N[1])
+        digits_ = []
+        #lookup = {0,1,2,5,6,8,9}
+        lookup = {2,5,6,9}
+        for i in N_:
+            if i in lookup:
+                digits_.append(i)
+            else:
+                pass
+        return len(digits_)
+
+"""
+
+
+
+# V2 
+# memoization (top-down dp)
+class Solution(object):
+    def rotatedDigits(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        A = map(int, str(N))
+        invalid, diff = set([3, 4, 7]), set([2, 5, 6, 9])
+        def dp(A, i, is_prefix_equal, is_good, lookup):
+            if i == len(A): return int(is_good)
+            if (i, is_prefix_equal, is_good) not in lookup:
+                result = 0
+                for d in xrange(A[i]+1 if is_prefix_equal else 10):
+                    if d in invalid: continue
+                    result += dp(A, i+1,
+                                 is_prefix_equal and d == A[i],
+                                 is_good or d in diff,
+                                 lookup)
+                lookup[i, is_prefix_equal, is_good] = result
+            return lookup[i, is_prefix_equal, is_good]
+
+        lookup = {}
+        return dp(A, 0, True, False, lookup)
+
+
+# V3 
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def rotatedDigits(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        INVALID, SAME, DIFF = 0, 1, 2
+        same, diff = [0, 1, 8], [2, 5, 6, 9]
+        dp = [0] * (N+1)
+        dp[0] = SAME
+        for i in xrange(N//10+1):
+            if dp[i] != INVALID:
+                for j in same:
+                    if i*10+j <= N:
+                        dp[i*10+j] = max(SAME, dp[i])
+                for j in diff:
+                    if i*10+j <= N:
+                        dp[i*10+j] = DIFF
+        return dp.count(DIFF)
+
+
+# V4
+# Time:  O(nlogn) = O(n), because O(logn) = O(32) by this input
+# Space: O(logn) = O(1)
+class Solution3(object):
+    def rotatedDigits(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        invalid, diff = set(['3', '4', '7']), set(['2', '5', '6', '9'])
+        result = 0
+        for i in xrange(N+1):
+            lookup = set(list(str(i)))
+            if invalid & lookup:
+                continue
+            if diff & lookup:
+                result += 1
+        return result
+
+
+
