b1da5488378e5c861c6a3b768124002523e05b4c
yennanliue <f339339@gmail.com>
2018-07-15 08:41:42 +0100

    backup dev code

---
+++ leetcode_python/Two_Pointers/move-zeroes.py
@@ -0,0 +1,74 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Given an array nums, write a function to move all 0's
+# to the end of it while maintaining the relative order
+# of the non-zero elements.
+#
+# For example, given nums = [0, 1, 0, 3, 12], after
+# calling your function, nums should be [1, 3, 12, 0, 0].
+#
+# Note:
+# You must do this in-place without making a copy of the array.
+# Minimize the total number of operations.
+
+
+# V1  : dev , check  "Do not return anything, modify nums in-place instead." error 
+"""        
+class Solution(object):
+    def moveZeroes(self, nums):
+        zero_count = nums.count(0)
+        nums_ = sorted(nums)
+        output = [x for x in nums_  if x > 0] + [0]*zero_count
+        return output
+
+"""        
+
+
+
+# V2 
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        for i in xrange(len(nums)):
+            if nums[i]:
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
+
+    def moveZeroes2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(cmp=lambda a, b: 0 if b else -1)
+
+
+# V3 
+class Solution2(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        for i in xrange(len(nums)):
+            if nums[i]:
+                nums[pos] = nums[i]
+                pos += 1
+
+        for i in xrange(pos, len(nums)):
+            nums[i] = 0
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.moveZeroes([0, 1, 0, 3, 12])
+    print r
+
+
+
+
