a8e8223ca38685391e83be6b4cb1a8230909209f
yennanliue <f339339@gmail.com>
2018-07-19 05:19:09 +0100

    backup V1' code

--- leetcode_python/Two_Pointers/move-zeroes.py
+++ leetcode_python/Two_Pointers/move-zeroes.py
@@ -24,6 +24,16 @@ class Solution(object):
 
 """        
 
+# V1'
+# http://bookshadow.com/weblog/2015/09/19/leetcode-move-zeroes/
+class Solution(object):
+    def moveZeroes(self, nums):
+        y = 0
+        for x in range(len(nums)):
+            if nums[x]:
+                nums[x], nums[y] = nums[y], nums[x]
+                y += 1
+
 
 
 # V2 
