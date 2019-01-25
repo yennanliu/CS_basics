2002d7ce1fffbcc71778edb7acaafa90d2ab5a18
yennanliue <f339339@gmail.com>
2019-01-25 09:21:26 +0000

    add solution #2

--- leetcode_python/Recursion/balanced-binary-tree.py
+++ leetcode_python/Recursion/balanced-binary-tree.py
@@ -1,11 +1,41 @@
 
-# V1 
-# dev 
+
+# V1 : dev 
 
 
-# V2 
+# V2  
+
+
+class Solution(object):
+    def maxDepth(self, root):        
+        if root == None:
+            return 0        
+        else:
+            return max( self.maxDepth(root.left), self.maxDepth(root.right)) + 1                               
+        
+    
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root == None:
+            return True
+        
+        else:
+            if abs( self.maxDepth(root.left) - self.maxDepth(root.right) ) > 1:
+                return False
+            else:
+                # EVERY NODE SHOULD FOLLOW THE DEFINITON OF binary tree (depth of NODE < 1 )
+                # a binary tree in which the depth of the two subtrees of EVERY NODE never differ by more than 1.
+                return self.isBalanced(root.left) and self.isBalanced(root.right) 
+                
+
+
+# V3 
 # Time:  O(n)
 # Space: O(h), h is height of binary tree
+
 
 class TreeNode(object):
     def __init__(self, x):
