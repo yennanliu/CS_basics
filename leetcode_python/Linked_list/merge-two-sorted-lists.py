f360ea221c0e7eada7bc2d5ae67916683dd21272
yennanliue <f339339@gmail.com>
2019-02-01 07:32:15 +0000

    add merge-two-sorted-lists leetcode code

---
+++ leetcode_python/Linked_list/merge-two-sorted-lists.py
@@ -0,0 +1,63 @@
+
+# V1 :  
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, self.next)
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        output  = []
+        curr = dummy = ListNode(0)
+        while l1 or l2:
+            try:
+                output.append(l1.val)
+                l1 = l1.next
+            except:
+                output.append(l2.val)
+                l2 = l2.next
+        return sorted(output)
+
+
+
+
+# V2 
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, self.next)
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        curr = dummy = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        return dummy.next
