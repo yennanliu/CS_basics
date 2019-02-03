63e504c383b3fd44f101a38a10a323e18b4038b7
yennanliue <f339339@gmail.com>
2019-02-03 01:57:22 +0000

    add add leetcode python remove-duplicates-from-sorted-list-ii

---
+++ leetcode_python/Linked_list/remove-duplicates-from-sorted-list-ii.py
@@ -0,0 +1,70 @@
+
+# V1 : DEV 
+
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#     def __repr__(self):
+#         if self is None:
+#             return "Nil"
+#         else:
+#             return "{} -> {}".format(self.val, repr(self.next))
+
+# class Solution(object):
+#     def deleteDuplicates(self, head):
+#         """
+#         :type head: ListNode
+#         :rtype: ListNode
+#         """
+# 		if not head.next:
+# 			return head 
+# 		dummy = ListNode(0)
+# 		pre, cur = dummy, head
+# 		while  cur.next and ( cur.val = cur.next.val):
+# 			cur = cur.next
+# 			pre.next = cur 
+# 		if head.next:
+# 			cur = cur.next
+# 			pre = cur 
+# 			cur = cur.next # double check 
+# 		return dummy.next
+
+
+# V2 
+# Time:  O(n)
+# Space: O(1)
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self is None:
+            return "Nil"
+        else:
+            return "{} -> {}".format(self.val, repr(self.next))
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        pre, cur = dummy, head
+        while cur:
+            if cur.next and cur.next.val == cur.val:
+                val = cur.val
+                while cur and cur.val == val:
+                    cur = cur.next
+                pre.next = cur
+            else:
+                pre.next = cur
+                pre = cur
+                cur = cur.next
+        return dummy.next
+
+
