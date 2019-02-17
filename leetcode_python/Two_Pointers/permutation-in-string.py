2237d75ec8651a7b633774efb4d6730e39701341
yennanliue <f339339@gmail.com>
2019-02-17 09:31:13 +0000

    add leetcode permutation-in-strin (dev)

---
+++ leetcode_python/Two_Pointers/permutation-in-string.py
@@ -0,0 +1,87 @@
+
+# V1  : dev 
+
+
+
+# V2 
+# http://bookshadow.com/weblog/2017/04/30/leetcode-permutation-in-string/
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        l1, l2 = len(s1), len(s2)
+        c1 = collections.Counter(s1)
+        c2 = collections.Counter()
+        p = q = 0
+        while q < l2:
+            c2[s2[q]] += 1
+            if c1 == c2:
+                return True
+            q += 1
+            if q - p + 1 > l1:
+                c2[s2[p]] -= 1
+                if c2[s2[p]] == 0:
+                    del c2[s2[p]]
+                p += 1
+        return False
+
+# V3 
+# http://bookshadow.com/weblog/2017/04/30/leetcode-permutation-in-string/
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        l1, l2 = len(s1), len(s2)
+        c1 = collections.Counter(s1)
+        c2 = collections.Counter()
+        cnt = 0
+        p = q = 0
+        while q < l2:
+            c2[s2[q]] += 1
+            if c1[s2[q]] == c2[s2[q]]:
+                cnt += 1
+            if cnt == len(c1):
+                return True
+            q += 1
+            if q - p + 1 > l1:
+                if c1[s2[p]] == c2[s2[p]]:
+                    cnt -= 1
+                c2[s2[p]] -= 1
+                if c2[s2[p]] == 0:
+                    del c2[s2[p]]
+                p += 1
+        return False
+
+
+
+# V4 
+# Time:  O(n)
+# Space: O(1)
+
+import collections
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        counts = collections.Counter(s1)
+        l = len(s1)
+        for i in xrange(len(s2)):
+            if counts[s2[i]] > 0:
+                l -= 1
+            counts[s2[i]] -= 1
+            if l == 0:
+                return True
+            start = i + 1 - len(s1)
+            if start >= 0:
+                counts[s2[start]] += 1
+                if counts[s2[start]] > 0:
+                    l += 1
