089f027327eb4a87eb353d3e400a892a0b179376
yennanliue <f339339@gmail.com>
2019-02-19 11:16:09 +0000

    add leetcode Two_Pointers/middle-of-the-linked-list (dev)

---
+++ leetcode_python/Two_Pointers/middle-of-the-linked-list.py
@@ -0,0 +1,38 @@
+
+
+
+#  V1 
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# class Solution(object):
+#     import math 
+#     def middleNode(self, head):
+#         my_list = []
+#         for i in head:
+#             my_list.append(i)
+#             #cur = cur.next
+#         return cur[math.ceil(len(my_list)/2)]
+
+
+# V2 
+# Time:  O(n)
+# Space: O(1)
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        return slow
