23993926cd922a705d877d463ab7a9f78386454f
yennanliue <f339339@gmail.com>
2019-02-22 07:32:18 +0000

    add solution V2 and comment

--- leetcode_python/Sort/sort-colors.py
+++ leetcode_python/Sort/sort-colors.py
@@ -1,14 +1,33 @@
 
 
-# V1 
+# V1 : dev 
 
-class Solution(object):
-    def sortColors(self, nums):
-        return [0] * nums.count(0) + [1] * nums.count(1) + [2] * nums.count(2)
+# class Solution(object):
+#     def sortColors(self, nums):
+#         return [0] * nums.count(0) + [1] * nums.count(1) + [2] * nums.count(2)
     
 
 
 # V2 
+# https://blog.csdn.net/maymay_/article/details/80093460
+class Solution(object):
+    def sortColors(self, nums):
+        if nums == []:
+            return 
+        i,l,r = 0,0,len(nums)-1    # define two pointers : l, r. and index 
+        while i <= r :
+            if nums[i] == 2:       # if the value of current index = 2 
+                nums[i],nums[r] = nums[r],nums[i]   # then switch the index with the one next to it (right) 
+                r -=1                # then move right point to left 
+            elif nums[i] == 0:  # if current value of index = 0 
+                nums[i],nums[l] = nums[l],nums[i]  # then switch the index with the one next to it (left)  
+                l +=1
+                i +=1         # then move left point  and index to right 
+            else :
+                i+=1       # if current value of index = 1, then move index to right, keep left point unchanged  
+
+
+# V3 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79392195
 from collections import Counter
 class Solution(object):
@@ -26,9 +45,8 @@ class Solution(object):
             else:
                 nums[i] = 2
 
-
     
-# V3 
+# V4 
 # Time:  O(n)
 # Space: O(1)
 class Solution(object):
