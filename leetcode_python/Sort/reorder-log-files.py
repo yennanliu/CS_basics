66653f62c035b9e546dff4767c965d9d3f854704
yennanliue <f339339@gmail.com>
2019-03-01 07:34:10 +0000

    add leetcode reorder-log-files (dev)

---
+++ leetcode_python/Sort/reorder-log-files.py
@@ -0,0 +1,36 @@
+
+
+# V1  : dev 
+
+
+# V2 
+# https://blog.csdn.net/GQxxxxxl/article/details/83961863
+class Solution:
+    def reorderLogFiles(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: List[str]
+        """
+        def f(log):
+            id_, rest = log.split(" ", 1)
+            return (0, rest, id_) if rest[0].isalpha() else (1,)
+
+        return sorted(logs, key = f)
+
+
+# V3 
+# Time:  O(nlogn * l), n is the length of files, l is the average length of strings
+# Space: O(l)
+
+class Solution(object):
+    def reorderLogFiles(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: List[str]
+        """
+        def f(log):
+            i, content = log.split(" ", 1)
+            return (0, content, i) if content[0].isalpha() else (1,)
+
+        logs.sort(key=f)
+        return logs
