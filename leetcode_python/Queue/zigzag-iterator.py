cf1e27362867ce917b78740147533a136f16acbe
yennanliue <f339339@gmail.com>
2019-03-03 16:47:58 +0000

    add leetcode zigzag-iterator

---
+++ leetcode_python/Queue/zigzag-iterator.py
@@ -0,0 +1,81 @@
+
+
+# Given two 1d vectors, implement an iterator to return their elements alternately.
+
+# For example, given two 1d vectors:
+
+# v1 = [1, 2]
+# v2 = [3, 4, 5, 6]
+# By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1, 3, 2, 4, 5, 6].
+
+# Follow up: What if you are given k 1d vectors? How well can your code be extended to such cases?
+
+# Clarification for the follow up question - Update (2015-09-18):
+# The "Zigzag" order is not clearly defined and is ambiguous for k > 2 cases. If "Zigzag" does not look right to you, replace "Zigzag" with "Cyclic". For example, given the following input:
+
+# [1,2,3]
+# [4,5,6,7]
+# [8,9]
+# It should return [1,4,8,2,5,9,3,6,7].
+
+
+
+class ZigzagIterator(object):
+
+    def Zigzag(self, v1, v2):
+        output = []
+        if len(v2) > len(v1):
+            v1, v2 = v2, v1
+        #print (v1, v2)
+        for i in range(len(v1)):
+            #print (v1[i])
+            output.append(v1[i])
+            if i < len(v2):
+                output.append(v2[i])
+            else:
+                pass
+        return output
+
+
+# V1 
+
+
+# V2 
+# Time:  O(n)
+# Space: O(k)
+
+import collections
+
+
+class ZigzagIterator(object):
+
+    def __init__(self, v1, v2):
+        """
+        Initialize your q structure here.
+        :type v1: List[int]
+        :type v2: List[int]
+        """
+        self.q = collections.deque([(len(v), iter(v)) for v in (v1, v2) if v])
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        len, iter = self.q.popleft()
+        if len > 1:
+            self.q.append((len-1, iter))
+        return next(iter)
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return bool(self.q)
+
+
+
+
+
+
+
+
