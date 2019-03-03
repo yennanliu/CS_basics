3ec1eb945614801dc13a1252ebd3d6f0c11680e4
yennanliue <f339339@gmail.com>
2019-03-03 10:15:17 +0000

    add leetcode largest-perimeter-triangle

---
+++ leetcode_python/Sort/largest-perimeter-triangle.py
@@ -0,0 +1,31 @@
+
+
+
+# V1 
+class Solution(object):
+	def largestPerimeter(self, A):
+		A.sort()
+		for i in list(reversed(range(len(A)-2))):
+			if ( A[i] + A[i+1] > A[i+2] and 
+			 A[i+2] + A[i+1] > A[i] and
+			 A[i] + A[i+2] > A[i+1] ):
+				return A[i] + A[i+1] + A[i+2]
+		return 0 
+
+
+
+# V2 
+# Time:  O(nlogn)
+# Space: O(1)
+
+class Solution(object):
+    def largestPerimeter(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        A.sort()
+        for i in reversed(xrange(len(A) - 2)):
+            if A[i] + A[i+1] > A[i+2]:
+                return A[i] + A[i+1] + A[i+2]
+        return 0
