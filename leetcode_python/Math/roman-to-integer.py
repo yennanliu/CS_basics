4a615b6a1395558645cc0fbbdd82b04f0cafd481
yennanliue <f339339@gmail.com>
2019-03-13 08:19:59 +0000

    add leetcode roman-to-integer (dev)

---
+++ leetcode_python/Math/roman-to-integer.py
@@ -0,0 +1,38 @@
+
+
+# V1  : dev 
+
+
+# V2 
+# https://blog.csdn.net/fuxuemingzhu/article/details/48208995
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        res = roman[s[-1]]
+        N = len(s)
+        for i in range(N - 2, -1, -1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+        return res
+
+
+# V3
+# Time:  O(n)
+# Space: O(1)
+class Solution(object):
+    # @return an integer
+    def romanToInt(self, s):
+        numeral_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C":100, "D": 500, "M": 1000}
+        decimal = 0
+        for i in range(len(s)):
+            if i > 0 and numeral_map[s[i]] > numeral_map[s[i - 1]]:
+                decimal += numeral_map[s[i]] - 2 * numeral_map[s[i - 1]]
+            else:
+                decimal += numeral_map[s[i]]
+        return decimal
