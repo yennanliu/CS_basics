5bd46a9e2ebec6a02d02cbf222a2986eac917d50
yennanliue <f339339@gmail.com>
2019-03-17 17:39:46 +0000

    add leetcode excel-sheet-column-number (dev)

---
+++ leetcode_python/Math/excel-sheet-column-number.py
@@ -0,0 +1,33 @@
+
+# V1  : DEV 
+
+
+# V2 
+# https://blog.csdn.net/coder_orz/article/details/51406455
+class Solution(object):
+	# transform   26 Carry ->  10 Carry (Decimal)  
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum = 0
+        for c in s:
+            sum = sum*26 + ord(c) - 64 # 64 = ord('A') - 1
+        return sum
+
+# V3 
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0
+        for i in range(len(s)):
+            result *= 26
+            result += ord(s[i]) - ord('A') + 1
+        return result
