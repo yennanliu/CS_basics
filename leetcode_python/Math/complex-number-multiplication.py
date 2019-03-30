c395d8d48ed2bc9d94b627490664444d43d8a929
yennanliue <f339339@gmail.com>
2019-03-30 17:21:23 +0000

    add leetcode complex-number-multiplication.py

---
+++ leetcode_python/Math/complex-number-multiplication.py
@@ -0,0 +1,23 @@
+# V1 
+class Solution(object):
+	# a : a1+a2*i
+	# b : b2+b2*i
+	def complexNumberMultiply(self, a, b):
+		ra, ia = float(a.split('+')[0]), float(a.split('+')[1].split('i')[0])
+		rb, ib = float(b.split('+')[0]), float(b.split('+')[1].split('i')[0])
+		return '%d+%di' % ((ra*rb - ia*ib), ia*rb + ib*ra)
+
+# V2 
+# Time:  O(1)
+# Space: O(1)
+
+class Solution(object):
+    def complexNumberMultiply(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        ra, ia = map(int, a[:-1].split('+'))
+        rb, ib = map(int, b[:-1].split('+'))
+        return '%d+%di' % (ra * rb - ia * ib, ra * ib + ia * rb)
