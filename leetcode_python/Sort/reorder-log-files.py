50e4fa2e3be61476e22ec8623e7af7575cfe973c
yennanliue <f339339@gmail.com>
2019-04-01 09:51:57 +0100

    update leetcode redo progress (sort)

--- leetcode_python/Sort/reorder-log-files.py
+++ leetcode_python/Sort/reorder-log-files.py
@@ -2,9 +2,13 @@
 
 # V1  : dev 
 
+# V2
+# https://www.programiz.com/python-programming/methods/string/isalpha
+# isalpha()
+# The isalpha() method returns True if all characters in the string are alphabets. If not, it returns False.
 
-# V2 
 # https://blog.csdn.net/GQxxxxxl/article/details/83961863
+
 class Solution:
     def reorderLogFiles(self, logs):
         """
@@ -17,6 +21,18 @@ class Solution:
 
         return sorted(logs, key = f)
 
+# V2'
+class Solution:
+    def reorderLogFiles(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: List[str]
+        """
+        def f(log):
+            id_, rest = log.split(" ", 1)[0], log.split(" ", 1)[1:][0]
+            return (0, rest, id_) if rest[0].isalpha() else (1,)
+
+        return sorted(logs, key = f)
 
 # V3 
 # Time:  O(nlogn * l), n is the length of files, l is the average length of strings
