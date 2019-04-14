08cc05465d571043b1501b11b60ac67754d22d91
yennanliue <f339339@gmail.com>
2019-04-14 15:43:15 +0100

    add leetcode x-of-a-kind-in-a-deck-of-cards.py (dev)

---
+++ leetcode_python/Math/x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,43 @@
+# V1 : dev 
+
+# V2 
+# https://blog.csdn.net/danspace1/article/details/88956805
+class Solution(object):
+    def hasGroupsSizeX(self, deck):
+        """
+        :type deck: List[int]
+        :rtype: bool
+        """
+        def check(i):
+            for n in d:
+                if d[n] % i != 0:
+                    return False
+            return True
+        
+        if len(deck) < 2:
+            return False
+        d = collections.Counter(deck)
+        m = min(d.values())
+        for i in range(2, m+1):
+            # check if the great common divisor is i
+            if check(i):
+                return True
+        return False
+
+# V3 
+# Time:  O(n * (logn)^2)
+# Space: O(n)
+import collections
+class Solution(object):
+    def hasGroupsSizeX(self, deck):
+        """
+        :type deck: List[int]
+        :rtype: bool
+        """
+        def gcd(a, b):  # Time: O((logn)^2)
+            while b:
+                a, b = b, a % b
+            return a
+
+        vals = collections.Counter(deck).values()
+        return reduce(gcd, vals) >= 2
