988d2a57c63913818536c4f66ab9519c299f8f3a
yennanliue <f339339@gmail.com>
2019-04-26 14:03:45 +0100

    add leetcode longest-substring-with-at-least-k-repeating-characters.py (dev)

---
+++ leetcode_python/Recursion/longest-substring-with-at-least-k-repeating-characters.py
@@ -0,0 +1,49 @@
+# V0 : DEV 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/82889933
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if len(s) < k:
+            return 0
+        for c in set(s):
+            if s.count(c) < k:
+                return max(self.longestSubstring(t, k) for t in s.split(c))
+        return len(s)
+        
+# V2 
+# Time:  O(26 * n) = O(n)
+# Space: O(26) = O(1)
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        def longestSubstringHelper(s, k, start, end):
+            count = [0] * 26
+            for i in xrange(start, end):
+                count[ord(s[i]) - ord('a')] += 1
+            max_len = 0
+            i = start
+            while i < end:
+                while i < end and count[ord(s[i]) - ord('a')] < k:
+                    i += 1
+                j = i
+                while j < end and count[ord(s[j]) - ord('a')] >= k:
+                    j += 1
+
+                if i == start and j == end:
+                    return end - start
+
+                max_len = max(max_len, longestSubstringHelper(s, k, i, j))
+                i = j
+            return max_len
+
+        return longestSubstringHelper(s, k, 0, len(s))
