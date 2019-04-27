8ff8a48a990e93b113280a5230986d84c06b43ab
yennanliue <f339339@gmail.com>
2019-04-27 11:13:06 +0100

    add leetcode /trim-a-binary-search-tree.py (dev)

---
+++ leetcode_python/Recursion/trim-a-binary-search-tree.py
@@ -0,0 +1,68 @@
+# V0 : DEV 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/83869684
+class Solution:
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        elif root.val < L:
+            return self.trimBST(root.right, L, R)
+        else:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+            return root
+            
+# V3
+# https://www.polarxiong.com/archives/LeetCode-669-trim-a-binary-search-tree.html
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return root
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        root.left = self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        return root
+
+# V3 
+# Time:  O(n)
+# Space: O(h)
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        root.left, root.right = self.trimBST(root.left, L, R), self.trimBST(root.right, L, R)
+        return root
