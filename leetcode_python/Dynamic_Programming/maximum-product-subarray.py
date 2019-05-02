ccd17ad3cafacc5527d272ecf822df16afd22c13
yennanliue <f339339@gmail.com>
2019-05-02 17:41:00 +0100

    add a more easy to understand solution

--- leetcode_python/Dynamic_Programming/maximum-product-subarray.py
+++ leetcode_python/Dynamic_Programming/maximum-product-subarray.py
@@ -1,6 +1,42 @@
 # V0 : DEV 
 
 # V1 
+# https://blog.csdn.net/XX_123_1_RJ/article/details/81321978
+# idea
+# dpmax[i] = max(nums[i], dpmax[i - 1] * nums[i], dpmin[i - 1] * nums[i])  # record the maximum
+# dpmin[i] = min(nums[i], dpmax[i - 1] * nums[i], dpmin[i - 1] * nums[i])  # record the minimum，and consider the negative numbeer case 
+# so, ->
+# dpmax  = max(nums[i], dpmax * nums[i], dpmin * nums[i])
+# dpmin  = min(nums[i], dpmax * nums[i], dpmin * nums[i])
+# maxout = max(maxout, dpmax)
+
+
+class Solution:
+    def maxstrtest(self, nums):
+        n = len(nums)
+        if n == 1: return nums[0]
+        if n < 1: None
+        dpmax, dpmin = [0]*n, [0]*n  # 初始化dp
+        dpmax[0] = dpmin[0] = nums[0]
+        for i in range(1, n):
+            dpmax[i] = max(nums[i], dpmax[i - 1] * nums[i], dpmin[i - 1] * nums[i])  # 记录最大值
+            dpmin[i] = min(nums[i], dpmax[i - 1] * nums[i], dpmin[i - 1] * nums[i])  # 记录最小值，考虑负数的情况
+        return max(dpmax)
+
+    def maxstrtest1(self, nums):
+        if len(nums) == 1: return nums[0]
+        dpmax, dpmin, maxout= 1, 1, float('-inf')
+        for xi in nums:
+            dpmax, dpmin = max(xi, dpmax * xi, dpmin * xi), min(xi, dpmax * xi, dpmin * xi)
+            maxout = max(maxout, dpmax)
+        return maxout
+
+# if __name__ == '__main__':
+#     nums = [2, 3, -2, 4]
+#     solu = Solution()
+#     print(solu.maxstrtest1(nums))
+
+# V2 
 # https://blog.csdn.net/fuxuemingzhu/article/details/83211451
 class Solution(object):
     def maxProduct(self, nums):
@@ -19,7 +55,7 @@ class Solution(object):
             res = max(res, f[i])
         return res
 
-# V2 
+# V3 
 # Time:  O(n)
 # Space: O(1)
 class Solution(object):
