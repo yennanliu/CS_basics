bc37ac5f56f24f7d5fee319f3d8b75c66821f96e
yennanliu <f339339@gmail.com>
2019-06-19 22:24:06 +0800

    add DP paint-fence.py and update progress (dev)

--- leetcode_python/Dynamic_Programming/paint-fence.py
+++ leetcode_python/Dynamic_Programming/paint-fence.py
@@ -16,23 +16,44 @@ Therefore, f(3) = (k-1)*k + k*(k-1)*k = (k-1)(k+k*k)
 
 """
 
-# V1  : dev 
-# https://www.programcreek.com/2014/05/leetcode-pain-fence-java/
-"""
+# # V1  : dev 
+# # https://www.programcreek.com/2014/05/leetcode-pain-fence-java/
+
+# class Solution(object):
+#     def numWays(self, n, k):
+#         for i in range(2,n):
+#             dp[3] = (k-1)*(dp(1) + dp(2))
+#             dp[1] = dp[2]
+#             dp[2] = dp[3]
+
+#         return dp[3]
+
+
+# V2 
+# http://www.voidcn.com/article/p-cqzythcg-qp.html
 class Solution(object):
     def numWays(self, n, k):
-        for i in range(2,n):
-            dp[3] = (k-1)*(dp(1) + dp(2))
-            dp[1] = dp[2]
-            dp[2] = dp[3]
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        if n == 0: return 0
+        if n == 1: return k
 
-        return dp[3]
+        # for the first 2 posts
+        sameColor = k
+        diffColor = k * (k - 1)
 
-"""
+        for i in range(2, n):
+            temp = diffColor
+            diffColor = (sameColor + diffColor) * (k - 1)
+            sameColor = temp
+
+        return sameColor + diffColor
 
 
-
-
+# V3 
 # Time:  O(n)
 # Space: O(1)
 # V2 
@@ -55,7 +76,7 @@ class Solution(object):
             ways[i % 3] = (k - 1) * (ways[(i - 1) % 3] + ways[(i - 2) % 3])
         return ways[(n - 1) % 3]
 
-# V3 
+# V4 
 # Time:  O(n)
 # Space: O(n)
 # DP solution.
@@ -76,5 +97,3 @@ class Solution2(object):
         for i in xrange(2, n):
             ways[i] = (k - 1) * (ways[i - 1] + ways[i - 2])
         return ways[n - 1]
-
-
