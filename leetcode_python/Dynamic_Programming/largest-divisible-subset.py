0b23ea77acb2ced126993c84ded0a63af69adbeb
yennanliu <f339339@gmail.com>
2019-06-23 14:23:24 +0800

    add DP largest-divisible-subset.py (dev)

---
+++ leetcode_python/Dynamic_Programming/largest-divisible-subset.py
@@ -0,0 +1,62 @@
+# V0  : dev 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/83027364
+class Solution:
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums: return []
+        N = len(nums)
+        nums.sort()
+        dp = [0] * N #LDS
+        parent = [0] * N
+        mx = 0
+        mx_index = -1
+        for i in range(N):
+            for j in range(i - 1, -1 , -1):
+                if nums[i] % nums[j] == 0 and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+                    parent[i] = j
+                    if dp[i] > mx:
+                        mx = dp[i]
+                        mx_index = i
+        res = list()
+        for k in range(mx + 1):
+            res.append(nums[mx_index])
+            mx_index = parent[mx_index]
+        return res[::-1]
+
+# V2 
+# Time:  O(n^2)
+# Space: O(n)
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+
+        nums.sort()
+        dp = [1] * len(nums)
+        prev = [-1] * len(nums)
+        largest_idx = 0
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0:
+                    if dp[i] < dp[j] + 1:
+                        dp[i] = dp[j] + 1
+                        prev[i] = j
+            if dp[largest_idx] < dp[i]:
+                largest_idx = i
+
+        result = []
+        i = largest_idx
+        while i != -1:
+            result.append(nums[i])
+            i = prev[i]
+        return result[::-1]
