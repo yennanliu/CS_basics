38d02721262b4fcf500276651e2fb346add88a7f
yennanliue <f339339@gmail.com>
2019-06-27 00:09:20 +0800

    add Stack min-stack.py (dev), and update gitignore

--- leetcode_python/Stack/min-stack.py
+++ leetcode_python/Stack/min-stack.py
@@ -1,23 +1,19 @@
-"""
 
-Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
-push(x) -- Push element x onto stack.
-pop() -- Removes the element on top of the stack.
-top() -- Get the top element.
-getMin() -- Retrieve the minimum element in the stack.
-Example:
-MinStack minStack = new MinStack();
-minStack.push(-2);
-minStack.push(0);
-minStack.push(-3);
-minStack.getMin();   --> Returns -3.
-minStack.pop();
-minStack.top();      --> Returns 0.
-minStack.getMin();   --> Returns -2.
-
-
-"""
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# getMin() -- Retrieve the minimum element in the stack.
+# Example:
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin();   --> Returns -3.
+# minStack.pop();
+# minStack.top();      --> Returns 0.
+# minStack.getMin();   --> Returns -2.
 
 
 # Time:  O(n)
@@ -35,34 +31,91 @@ minStack.getMin();   --> Returns -2.
 ### Stack : 「Last-In-First-Out」
 ### http://alrightchiu.github.io/SecondRound/stack-introjian-jie.html
 
- 
-# V1  : dev 
-"""
+# V0 : DEV 
 
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79253237
 class MinStack(object):
 
     def __init__(self):
-        self.min = None
+        """
+        initialize your data structure here.
+        """
         self.stack = []
         
 
     def push(self, x):
-        self.stack.append(x)
+        """
+        :type x: int
+        :rtype: void
+        """
+        if not self.stack:
+            self.stack.append((x, x))
+        else:
+            self.stack.append((x, min(x, self.stack[-1][1])))
         
 
     def pop(self):
-        return self.stack.pop()
-
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+        
 
     def top(self):
-        return self.stack[0]
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
         
 
     def getMin(self):
-        return min(self.stack)
-        
-"""
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
 
+# V1'
+# https://blog.csdn.net/fuxuemingzhu/article/details/79253237
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+        if not self.min:
+            self.min.append(x)
+        else:
+            self.min.append(min(self.min[-1], x))
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min[-1]
 
 # V2 
 # https://www.cnblogs.com/zuoyuan/p/4091870.html
@@ -94,9 +147,6 @@ class MinStack:
     # @return an integer
     def getMin(self):
         return self.stack2[-1]
-
-
-
 
 # V3 
 class MinStack:
@@ -166,7 +216,102 @@ class MinStack2:
     def getMin(self):
         return self.minStack[-1][0]
 
-if __name__ == "__main__":
-    stack = MinStack()
-    stack.push(-1)
-    print([stack.top(), stack.getMin()])
+# if __name__ == "__main__":
+#     stack = MinStack()
+#     stack.push(-1)
+#     print([stack.top(), stack.getMin()])
+
+# V4
+# Time:  O(n)
+# Space: O(1)
+class MinStack(object):
+    def __init__(self):
+        self.min = None
+        self.stack = []
+
+    # @param x, an integer
+    # @return an integer
+    def push(self, x):
+        if not self.stack:
+            self.stack.append(0)
+            self.min = x
+        else:
+            self.stack.append(x - self.min)
+            if x < self.min:
+                self.min = x
+
+    # @return nothing
+    def pop(self):
+        x = self.stack.pop()
+        if x < 0:
+            self.min = self.min - x
+
+    # @return an integer
+    def top(self):
+        x = self.stack[-1]
+        if x > 0:
+            return x + self.min
+        else:
+            return self.min
+
+    # @return an integer
+    def getMin(self):
+        return self.min
+
+# V4' 
+# Time:  O(n)
+# Space: O(n)
+class MinStack2(object):
+    def __init__(self):
+        self.stack, self.minStack = [], []
+    # @param x, an integer
+    # @return an integer
+    def push(self, x):
+        self.stack.append(x)
+        if len(self.minStack):
+            if x < self.minStack[-1][0]:
+                self.minStack.append([x, 1])
+            elif x == self.minStack[-1][0]:
+                self.minStack[-1][1] += 1
+        else:
+            self.minStack.append([x, 1])
+
+    # @return nothing
+    def pop(self):
+        x = self.stack.pop()
+        if x == self.minStack[-1][0]:
+            self.minStack[-1][1] -= 1
+            if self.minStack[-1][1] == 0:
+                self.minStack.pop()
+
+    # @return an integer
+    def top(self):
+        return self.stack[-1]
+
+    # @return an integer
+    def getMin(self):
+        return self.minStack[-1][0]
+
+# V4''
+# time: O(1)
+# space: O(n)
+class MinStack3(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        if self.stack:
+            current_min = min(x, self.stack[-1][0])
+            self.stack.append((current_min, x))
+        else:
+            self.stack.append((x, x))
+
+    def pop(self):
+        return self.stack.pop()[1]
+
+    def top(self):
+        return self.stack[-1][1]
+
+    def getMin(self):
+        return self.stack[-1][0]
