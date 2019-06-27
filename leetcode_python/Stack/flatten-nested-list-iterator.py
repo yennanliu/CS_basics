5b436c62f9563fc6dfda5ec14b7596afe5b8f11b
yennanliu <f339339@gmail.com>
2019-06-27 21:52:51 +0800

    add stack flatten-nested-list-iterator.py (dev)

---
+++ leetcode_python/Stack/flatten-nested-list-iterator.py
@@ -0,0 +1,68 @@
+# V0
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79529982
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.queue = collections.deque()
+        def getAll(nests):
+            for nest in nests:
+                if nest.isInteger():
+                    self.queue.append(nest.getInteger())
+                else:
+                    getAll(nest.getList())
+        getAll(nestedList)
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.queue.popleft()
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return len(self.queue)
+
+# V2 
+# Time:  O(n), n is the number of the integers.
+# Space: O(h), h is the depth of the nested lists.
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.__depth = [[nestedList, 0]]
+
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        nestedList, i = self.__depth[-1]
+        self.__depth[-1][1] += 1
+        return nestedList[i].getInteger()
+
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        while self.__depth:
+            nestedList, i = self.__depth[-1]
+            if i == len(nestedList):
+                self.__depth.pop()
+            elif nestedList[i].isInteger():
+                    return True
+            else:
+                self.__depth[-1][1] += 1
+                self.__depth.append([nestedList[i].getList(), 0])
+        return False
