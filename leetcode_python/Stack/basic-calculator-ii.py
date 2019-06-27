9cbc3ba4783e1fa94ef9d44c04c6ca69345304d1
yennanliu <f339339@gmail.com>
2019-06-27 19:47:56 +0800

    add stack basic-calculator-ii.py (dev)

---
+++ leetcode_python/Stack/basic-calculator-ii.py
@@ -0,0 +1,76 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/80826333
+class Solution:
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = []
+        pre_op = '+'
+        num = 0
+        for i, each in enumerate(s):
+            if each.isdigit():
+                num = 10 * num + int(each)
+            if i == len(s) - 1 or each in '+-*/':
+                if pre_op == '+':
+                    stack.append(num)
+                elif pre_op == '-':
+                    stack.append(-num)
+                elif pre_op == '*':
+                    stack.append(stack.pop() * num)
+                elif pre_op == '/':
+                    top = stack.pop()
+                    if top < 0:
+                        stack.append(int(top / num))
+                    else:
+                        stack.append(top // num)
+                pre_op = each
+                num = 0
+        return sum(stack)
+
+# V2 
+# Time:  O(n)
+# Space: O(n)
+class Solution(object):
+    # @param {string} s
+    # @return {integer}
+    def calculate(self, s):
+        operands, operators = [], []
+        operand = ""
+        for i in reversed(xrange(len(s))):
+            if s[i].isdigit():
+                operand += s[i]
+                if i == 0 or not s[i-1].isdigit():
+                    operands.append(int(operand[::-1]))
+                    operand = ""
+            elif s[i] == ')' or s[i] == '*' or s[i] == '/':
+                operators.append(s[i])
+            elif s[i] == '+' or s[i] == '-':
+                while operators and \
+                      (operators[-1] == '*' or operators[-1] == '/'):
+                    self.compute(operands, operators)
+                operators.append(s[i])
+            elif s[i] == '(':
+                while operators[-1] != ')':
+                    self.compute(operands, operators)
+                operators.pop()
+
+        while operators:
+            self.compute(operands, operators)
+
+        return operands[-1]
+
+    def compute(self, operands, operators):
+        left, right = operands.pop(), operands.pop()
+        op = operators.pop()
+        if op == '+':
+            operands.append(left + right)
+        elif op == '-':
+            operands.append(left - right)
+        elif op == '*':
+            operands.append(left * right)
+        elif op == '/':
+            operands.append(left / right)
