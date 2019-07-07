7f860dcecf09e21108164fa4259d1527c6ac5512
yennanliu <f339339@gmail.com>
2019-07-07 17:08:29 +0800

    add tree smallest-subtree-with-all-the-deepest-nodes.py (dev)

---
+++ leetcode_python/Tree/smallest-subtree-with-all-the-deepest-nodes.py
@@ -0,0 +1,51 @@
+# V0
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/82432130
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def subtreeWithAllDeepest(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        return self.depth(root)[1]
+        
+    def depth(self, root):
+        if not root: return 0, None
+        l, r = self.depth(root.left), self.depth(root.right)
+        if l[0] > r[0]:
+            return l[0] + 1, l[1]
+        elif l[0] < r[0]:
+            return r[0] + 1, r[1]
+        else:
+            return l[0] + 1, root
+
+# V2 
+# Time:  O(n)
+# Space: O(h)
+import collections
+class Solution(object):
+    def subtreeWithAllDeepest(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        Result = collections.namedtuple("Result", ("node", "depth"))
+
+        def dfs(node):
+            if not node:
+                return Result(None, 0)
+            left, right = dfs(node.left), dfs(node.right)
+            if left.depth > right.depth:
+                return Result(left.node, left.depth+1)
+            if left.depth < right.depth:
+                return Result(right.node, right.depth+1)
+            return Result(node, left.depth+1)
+
+        return dfs(root).node
