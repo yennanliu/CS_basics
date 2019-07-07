20d4f1c4e27b034d31ef028fe3fb79dbd204a99e
yennanliue <f339339@gmail.com>
2019-07-07 23:37:25 +0800

    add tree complete-binary-tree-inserter.py (dev

---
+++ leetcode_python/Tree/complete-binary-tree-inserter.py
@@ -0,0 +1,99 @@
+# V0
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/82958284
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class CBTInserter(object):
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.tree = list()
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            self.tree.append(node)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+    def insert(self, v):
+        """
+        :type v: int
+        :rtype: int
+        """
+        _len = len(self.tree)
+        father = self.tree[(_len - 1) / 2]
+        node = TreeNode(v)
+        if not father.left:
+            father.left = node
+        else:
+            father.right = node
+        self.tree.append(node)
+        return father.val
+        
+
+    def get_root(self):
+        """
+        :rtype: TreeNode
+        """
+        return self.tree[0]
+
+
+# Your CBTInserter object will be instantiated and called as such:
+# obj = CBTInserter(root)
+# param_1 = obj.insert(v)
+# param_2 = obj.get_root()
+
+# V2 
+# Time:  ctor:     O(n)
+#        insert:   O(1)
+#        get_root: O(1)
+# Space: O(n)
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class CBTInserter(object):
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.__tree = [root]
+        for i in self.__tree:
+            if i.left:
+                self.__tree.append(i.left)
+            if i.right:
+                self.__tree.append(i.right)        
+
+    def insert(self, v):
+        """
+        :type v: int
+        :rtype: int
+        """
+        n = len(self.__tree)
+        self.__tree.append(TreeNode(v))
+        if n % 2:
+            self.__tree[(n-1)//2].left = self.__tree[-1]
+        else:
+            self.__tree[(n-1)//2].right = self.__tree[-1]
+        return self.__tree[(n-1)//2].val
+
+    def get_root(self):
+        """
+        :rtype: TreeNode
+        """
+        return self.__tree[0]
