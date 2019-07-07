6444f0207bbf3234ec43a94b96f08606b7d7d39b
yennanliu <f339339@gmail.com>
2019-07-07 17:34:39 +0800

    add tree /construct-binary-tree-from-preorder-and-postorder-traversal.py (dev)

---
+++ leetcode_python/Tree/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,85 @@
+# V0
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/82391321
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def constructFromPrePost(self, pre, post):
+        """
+        :type pre: List[int]
+        :type post: List[int]
+        :rtype: TreeNode
+        """
+        if not pre or not post: return None
+        root = TreeNode(pre[0])
+        if len(pre) == 1:
+            return root
+        idx = pre.index(post[-2])
+        root.left = self.constructFromPrePost(pre[1:idx], post[:idx-1])
+        root.right = self.constructFromPrePost(pre[idx:], post[idx-1:-1])
+        return root
+
+# V2 
+# Time:  O(n)
+# Space: O(h)
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def constructFromPrePost(self, pre, post):
+        """
+        :type pre: List[int]
+        :type post: List[int]
+        :rtype: TreeNode
+        """
+        stack = [TreeNode(pre[0])]
+        j = 0
+        for i in range(1, len(pre)):
+            node = TreeNode(pre[i])
+            while stack[-1].val == post[j]:
+                stack.pop()
+                j += 1
+            if not stack[-1].left:
+                stack[-1].left = node
+            else:
+                stack[-1].right = node
+            stack.append(node)
+        return stack[0]
+
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def constructFromPrePost(self, pre, post):
+        """
+        :type pre: List[int]
+        :type post: List[int]
+        :rtype: TreeNode
+        """
+        def constructFromPrePostHelper(pre, pre_s, pre_e, post, post_s, post_e, post_entry_idx_map):
+            if pre_s >= pre_e or post_s >= post_e:
+                return None
+            node = TreeNode(pre[pre_s])
+            if pre_e-pre_s > 1:
+                left_tree_size = post_entry_idx_map[pre[pre_s+1]]-post_s+1
+                node.left = constructFromPrePostHelper(pre, pre_s+1, pre_s+1+left_tree_size, 
+                                                       post, post_s, post_s+left_tree_size,
+                                                       post_entry_idx_map)
+                node.right = constructFromPrePostHelper(pre, pre_s+1+left_tree_size, pre_e,
+                                                        post, post_s+left_tree_size, post_e-1,
+                                                        post_entry_idx_map)
+            return node
+
+        post_entry_idx_map = {}
+        for i, val in enumerate(post):
+            post_entry_idx_map[val] = i
+        return constructFromPrePostHelper(pre, 0, len(pre), post, 0, len(post), post_entry_idx_map)
