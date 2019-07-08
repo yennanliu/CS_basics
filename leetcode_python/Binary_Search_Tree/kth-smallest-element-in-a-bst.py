24c1d9a6939e7f9691cd7346f7b52d6a2aa1ec42
yennanliue <f339339@gmail.com>
2019-07-08 23:56:03 +0800

    add bst /kth-smallest-element-in-a-bst.py (dev)

---
+++ leetcode_python/Binary_Search_Tree/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,81 @@
+# V0 
+
+# V1 
+# http://bookshadow.com/weblog/2015/07/02/leetcode-kth-smallest-element-bst/
+# IDEA : using inorder survey through whole BST
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @param {integer} k
+    # @return {integer}
+    def kthSmallest(self, root, k):
+        stack = []
+        node = root
+        while node:
+            stack.append(node)
+            node = node.left
+        x = 1
+        while stack and x <= k:
+            node = stack.pop()
+            x += 1
+            right = node.right
+            while right:
+                stack.append(right)
+                right = right.left
+        return node.val
+
+# V2 
+# Time:  O(max(h, k))
+# Space: O(h)
+
+class Solution(object):
+    # @param {TreeNode} root
+    # @param {integer} k
+    # @return {integer}
+    def kthSmallest(self, root, k):
+        s, cur, rank = [], root, 0
+
+        while s or cur:
+            if cur:
+                s.append(cur)
+                cur = cur.left
+            else:
+                cur = s.pop()
+                rank += 1
+                if rank == k:
+                    return cur.val
+                cur = cur.right
+
+        return float("-inf")
+
+
+# time: O(max(h, k))
+# space: O(h)
+
+from itertools import islice
+
+
+class Solution2(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        def gen_inorder(root):
+            if root:
+                for n in gen_inorder(root.left):
+                    yield n
+
+                yield root.val
+
+                for n in gen_inorder(root.right):
+                    yield n
+
+        return next(islice(gen_inorder(root), k-1, k))
