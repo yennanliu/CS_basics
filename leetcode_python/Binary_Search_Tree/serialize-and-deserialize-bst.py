58df9bf5a55b4d1ffc4f0deef4c011d0d7d0301e
yennanliu <f339339@gmail.com>
2019-07-09 18:12:41 +0800

    add bst serialize-and-deserialize-bst.py ()

---
+++ leetcode_python/Binary_Search_Tree/serialize-and-deserialize-bst.py
@@ -0,0 +1,204 @@
+# V0 
+
+# V1 
+# http://bookshadow.com/weblog/2015/10/26/leetcode-serialize-and-deserialize-binary-tree/
+class Codec:
+
+    def serialize(self, root):
+        def doit(node):
+            if node:
+                vals.append(str(node.val))
+                doit(node.left)
+                doit(node.right)
+            else:
+                vals.append('#')
+        vals = []
+        doit(root)
+        return ' '.join(vals)
+
+    def deserialize(self, data):
+        def doit():
+            val = next(vals)
+            if val == '#':
+                return None
+            node = TreeNode(int(val))
+            node.left = doit()
+            node.right = doit()
+            return node
+        vals = iter(data.split())
+        return doit()
+
+# V1'
+# http://bookshadow.com/weblog/2015/10/26/leetcode-serialize-and-deserialize-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return '[]'
+        res = [root.val]
+        q = collections.deque([root])
+        while q:
+            front = q.popleft()
+            if front.left:
+                q.append(front.left)
+            if front.right:
+                q.append(front.right)
+            res.append(front.left.val if front.left else 'null')
+            res.append(front.right.val if front.right else 'null')
+        while res and res[-1] == 'null':
+            res.pop()
+        return '[' + ','.join(map(str, res)) + ']'
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == '[]':
+            return None
+        nodes = collections.deque([[TreeNode(o), None][o == 'null']
+                                    for o in data[1:-1].split(',')])
+        q = collections.deque([nodes.popleft()]) if nodes else None
+        root = q[0] if q else None
+        while q:
+            parent = q.popleft()
+            left = nodes.popleft() if nodes else None
+            right = nodes.popleft() if nodes else None
+            parent.left, parent.right = left, right
+            if left:
+                q.append(left)
+            if right:
+                q.append(right)
+        return root
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
+# V1''
+# http://bookshadow.com/weblog/2015/10/26/leetcode-serialize-and-deserialize-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+import json
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return 'null'
+        nodes = collections.deque([root])
+        maps = collections.deque([{'v' : root.val}])
+        tree = maps[0]
+        while nodes:
+            frontNode = nodes.popleft()
+            frontMap = maps.popleft()
+            if frontNode.left:
+                frontMap['l'] = {'v' : frontNode.left.val}
+                nodes.append(frontNode.left)
+                maps.append(frontMap['l'])
+            if frontNode.right:
+                frontMap['r'] = {'v' : frontNode.right.val}
+                nodes.append(frontNode.right)
+                maps.append(frontMap['r'])
+        return json.dumps(tree)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        tree = json.loads(data)
+        if not tree:
+            return None
+        root = TreeNode(tree['v'])
+        maps = collections.deque([tree])
+        nodes = collections.deque([root])
+        while nodes:
+            frontNode = nodes.popleft()
+            frontMap = maps.popleft()
+            left, right = frontMap.get('l'), frontMap.get('r')
+            if left:
+                frontNode.left = TreeNode(left['v'])
+                maps.append(left)
+                nodes.append(frontNode.left)
+            if right:
+                frontNode.right = TreeNode(right['v'])
+                maps.append(right)
+                nodes.append(frontNode.right)
+        return root
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
+# V2 
+# Time:  O(n)
+# Space: O(h)
+import collections
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec(object):
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        :type root: TreeNode
+        :rtype: str
+        """
+        def serializeHelper(node, vals):
+            if node:
+                vals.append(node.val)
+                serializeHelper(node.left, vals)
+                serializeHelper(node.right, vals)
+
+        vals = []
+        serializeHelper(root, vals)
+
+        return ' '.join(map(str, vals))
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        :type data: str
+        :rtype: TreeNode
+        """
+        def deserializeHelper(minVal, maxVal, vals):
+            if not vals:
+                return None
+
+            if minVal < vals[0] < maxVal:
+                val = vals.popleft()
+                node = TreeNode(val)
+                node.left = deserializeHelper(minVal, val, vals)
+                node.right = deserializeHelper(val, maxVal, vals)
+                return node
+            else:
+                return None
+
+        vals = collections.deque([int(val) for val in data.split()])
+
+        return deserializeHelper(float('-inf'), float('inf'), vals)
+
