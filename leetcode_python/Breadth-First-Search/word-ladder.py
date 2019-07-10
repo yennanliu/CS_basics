2a54957e0d9e84e0c46934c440c9e9c8a4da9262
yennanliu <f339339@gmail.com>
2019-07-10 17:58:06 +0800

    add bfs word-ladder.py (dev)

---
+++ leetcode_python/Breadth-First-Search/word-ladder.py
@@ -0,0 +1,92 @@
+# V0
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/82903681
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        wordset = set(wordList)
+        if endWord not in wordset:
+            return 0
+        visited = set([beginWord])
+        chrs = [chr(ord('a') + i) for i in range(26)]
+        bfs = collections.deque([beginWord])
+        res = 1
+        while bfs:
+            len_bfs = len(bfs)
+            for _ in range(len_bfs):
+                origin = bfs.popleft()
+                for i in range(len(origin)):
+                    originlist = list(origin)
+                    for c in chrs:
+                        originlist[i] = c
+                        transword = "".join(originlist)
+                        if transword not in visited:
+                            if transword == endWord:
+                                return res + 1
+                            elif transword in wordset:
+                                bfs.append(transword)
+                                visited.add(transword)
+            res += 1
+        return 0
+
+# V1' 
+# https://blog.csdn.net/fuxuemingzhu/article/details/82903681
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        wordset = set(wordList)
+        bfs = collections.deque()
+        bfs.append((beginWord, 1))
+        while bfs:
+            word, length = bfs.popleft()
+            if word == endWord:
+                return length
+            for i in range(len(word)):
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    newWord = word[:i] + c + word[i + 1:]
+                    if newWord in wordset and newWord != word:
+                        wordset.remove(newWord)
+                        bfs.append((newWord, length + 1))
+        return 0
+        
+# V2 
+# Time:  O(n * d), n is length of string, d is size of dictionary
+# Space: O(d)
+from string import ascii_lowercase
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        distance, cur, visited, lookup = 0, [beginWord], set([beginWord]), set(wordList)
+
+        while cur:
+            next_queue = []
+
+            for word in cur:
+                if word == endWord:
+                    return distance + 1
+                for i in range(len(word)):
+                    for j in ascii_lowercase:
+                        candidate = word[:i] + j + word[i+1:]
+                        if candidate not in visited and candidate in lookup:
+                            next_queue.append(candidate)
+                            visited.add(candidate)
+            distance += 1
+            cur = next_queue
+
+        return 0
