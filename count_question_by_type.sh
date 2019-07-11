b5d785c107e849ae65c33ea1708fc955d348459c
yennanliu <f339339@gmail.com>
2019-07-11 18:39:22 +0800

    backup code update (count_question_by_type.sh)

--- count_question_by_type.sh
+++ count_question_by_type.sh
@@ -3,7 +3,13 @@
 #################################################################
 # SCRIPT COUNT NUMBER OF LEETCODE QUESTIONS BY TYPE  
 #################################################################
-
-cat README.md | grep '##'
+# https://unix.stackexchange.com/questions/80399/cat-all-lines-from-file-which-are-after-keyword
 # dev 
 
+question_type=$(cat README.md | grep '##')
+
+for type in  "${question_type[@]}"
+  do
+     #cat README.md | sed '1,/^## Shell Script easy and medium$/d'
+     echo $type && cat README.md | sed '1,/^$type$/d'
+  done 
