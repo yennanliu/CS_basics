db4cafedea435a0550e512898bf8371b0ed3469a
yennanliu <f339339@gmail.com>
2019-07-11 14:35:34 +0800

    add bst sequence-reconstruction.py (dev)

---
+++ leetcode_python/Breadth-First-Search/sequence-reconstruction.py
@@ -0,0 +1,145 @@
+# V0 
+
+# V1 
+# http://bookshadow.com/weblog/2016/10/30/leetcode-sequence-reconstruction/
+class Solution(object):
+    def sequenceReconstruction(self, org, seqs):
+        """
+        :type org: List[int]
+        :type seqs: List[List[int]]
+        :rtype: bool
+        """
+        size = len(org)
+        indeg = [0] * size
+        sucset = [set() for x in range(size)]
+        if not seqs: return False
+        for seq in seqs:
+            if any(s > size or s < 1 for s in seq):
+                return False
+            for i in range(1, len(seq)):
+                if seq[i] not in sucset[seq[i - 1] - 1]:
+                    indeg[seq[i] - 1] += 1
+                    sucset[seq[i - 1] - 1].add(seq[i])
+
+        q = [i for i in org if not indeg[i - 1]]
+        for x in range(size):
+            if len(q) != 1 or q[0] != org[x]:
+                return False
+            nq = []
+            for suc in sucset[q[0] - 1]:
+                indeg[suc - 1] -= 1
+                if not indeg[suc - 1]:
+                    nq.append(suc)
+            q = nq
+        return True
+
+# V1' 
+# http://bookshadow.com/weblog/2016/10/30/leetcode-sequence-reconstruction/
+class Solution(object):
+    def sequenceReconstruction(self, org, seqs):
+        """
+        :type org: List[int]
+        :type seqs: List[List[int]]
+        :rtype: bool
+        """
+        indexes = {e : i for i, e in enumerate(org)}
+        edges = set()
+
+        if not seqs: return False
+        for seq in seqs:
+            for s in seq:
+                if s not in indexes:
+                    return False
+            for i in range(1, len(seq)):
+                pre, cur = seq[i - 1], seq[i]
+                if indexes[pre] > indexes[cur]:
+                    return False
+                edges.add((pre, cur))
+
+        for x in range(1, len(org)):
+            if (org[x - 1], org[x]) not in edges:
+                return False
+        return True
+        
+# V2 
+# Time:  O(n * s), n is the size of org, s is the size of seqs
+# Space: O(n)
+import collections
+class Solution(object):
+    def sequenceReconstruction(self, org, seqs):
+        """
+        :type org: List[int]
+        :type seqs: List[List[int]]
+        :rtype: bool
+        """
+        if not seqs:
+            return False
+        pos = [0] * (len(org) + 1)
+        for i in range(len(org)):
+            pos[org[i]] = i
+
+        is_matched = [False] * (len(org) + 1)
+        cnt_to_match = len(org) - 1
+        for seq in seqs:
+            for i in range(len(seq)):
+                if not 0 < seq[i] <= len(org):
+                    return False
+                if i == 0:
+                    continue
+                if pos[seq[i-1]] >= pos[seq[i]]:
+                    return False
+                if is_matched[seq[i-1]] == False and pos[seq[i-1]] + 1 == pos[seq[i]]:
+                    is_matched[seq[i-1]] = True
+                    cnt_to_match -= 1
+
+        return cnt_to_match == 0
+
+
+# Time:  O(|V| + |E|)
+# Space: O(|E|)
+class Solution2(object):
+    def sequenceReconstruction(self, org, seqs):
+        """
+        :type org: List[int]
+        :type seqs: List[List[int]]
+        :rtype: bool
+        """
+        graph = collections.defaultdict(set)
+        indegree = collections.defaultdict(int)
+        integer_set = set()
+        for seq in seqs:
+            for i in seq:
+                integer_set.add(i)
+            if len(seq) == 1:
+                if seq[0] not in indegree:
+                    indegree[seq[0]] = 0
+                continue
+            for i in range(len(seq)-1):
+                if seq[i] not in indegree:
+                    indegree[seq[i]] = 0
+                if seq[i+1] not in graph[seq[i]]:
+                    graph[seq[i]].add(seq[i+1])
+                    indegree[seq[i+1]] += 1
+
+        cnt_of_zero_indegree = 0
+        res = []
+        q = []
+        for i in indegree:
+            if indegree[i] == 0:
+                cnt_of_zero_indegree += 1
+                if cnt_of_zero_indegree > 1:
+                    return False
+                q.append(i)
+
+        while q:
+            i = q.pop()
+            res.append(i)
+            cnt_of_zero_indegree = 0
+            for j in graph[i]:
+                indegree[j] -= 1
+                if indegree[j] == 0:
+                    cnt_of_zero_indegree += 1
+                    if cnt_of_zero_indegree > 1:
+                        return False
+                    q.append(j)
+        return res == org and len(org) == len(integer_set)
