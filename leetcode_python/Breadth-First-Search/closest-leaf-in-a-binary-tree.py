e20fc8d33e6b552921e5cfe5a6a09b76d2f5544e
yennanliu <f339339@gmail.com>
2019-07-11 16:27:09 +0800

    add bfs closest-leaf-in-a-binary-tree.py (dev)

---
+++ leetcode_python/Breadth-First-Search/closest-leaf-in-a-binary-tree.py
@@ -0,0 +1,88 @@
+# V0 
+
+# V1 
+# http://bookshadow.com/weblog/2017/12/10/leetcode-closest-leaf-in-a-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def findClosestLeaf(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        parents = {}
+        leaves = []
+        self.knode = None
+        def traverse(root):
+            if root.val == k: self.knode = root
+            if not root.left and not root.right:
+                leaves.append(root)
+                return
+            for child in (root.left, root.right):
+                if not child: continue
+                traverse(child)
+                parents[child.val] = root
+        def findParents(node):
+            ans = [node.val]
+            while node.val in parents:
+                node = parents[node.val]
+                ans.append(node.val)
+            return ans
+        traverse(root)
+        kParents = findParents(self.knode)
+        ans, dist = None, 0x7FFFFFFF
+        for leaf in leaves:
+            leafParents = findParents(leaf)
+            cross = [n for n in leafParents if n in kParents][0]
+            ndist = leafParents.index(cross) + kParents.index(cross)
+            if ndist < dist:
+                dist = ndist
+                ans = leaf
+        return ans.val
+
+# V2 
+# Time:  O(n)
+# Space: O(n)
+import collections
+class Solution(object):
+    def findClosestLeaf(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        def traverse(node, neighbors, leaves):
+            if not node:
+                return
+            if not node.left and not node.right:
+                leaves.add(node.val)
+                return
+            if node.left:
+                neighbors[node.val].append(node.left.val)
+                neighbors[node.left.val].append(node.val)
+                traverse(node.left, neighbors, leaves)
+            if node.right:
+                neighbors[node.val].append(node.right.val)
+                neighbors[node.right.val].append(node.val)
+                traverse(node.right, neighbors, leaves)
+
+        neighbors, leaves = collections.defaultdict(list), set()
+        traverse(root, neighbors, leaves)
+        q, lookup = [k], set([k])
+        while q:
+            next_q = []
+            for u in q:
+                if u in leaves:
+                    return u
+                for v in neighbors[u]:
+                    if v in lookup:
+                        continue
+                    lookup.add(v)
+                    next_q.append(v)
+            q = next_q
+        return 0
