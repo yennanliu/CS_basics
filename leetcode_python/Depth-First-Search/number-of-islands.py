5656aeacfd5a8b502e0e3f2f8e14d914426745e6
yennanliu <f339339@gmail.com>
2019-07-12 18:59:11 +0800

    add dfs number-of-islands.py (dev)

---
+++ leetcode_python/Depth-First-Search/number-of-islands.py
@@ -0,0 +1,90 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/81126995
+# IDEA : DFS 
+class Solution:
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        res = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    self.dfs(grid, r, c)
+                    res += 1
+        return res
+        
+    def dfs(self, grid, i, j):
+        dirs = [[-1, 0], [0, 1], [0, -1], [1, 0]]
+        grid[i][j] = "0"
+        for dir in dirs:
+            nr, nc = i + dir[0], j + dir[1]
+            if nr >= 0 and nc >= 0 and nr < len(grid) and nc < len(grid[0]):
+                if grid[nr][nc] == "1":
+                    self.dfs(grid, nr, nc)
+
+# V1' 
+# https://blog.csdn.net/fuxuemingzhu/article/details/81126995
+# IDEA : BFS
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid or not grid[0]: return 0
+        M, N = len(grid), len(grid[0])
+        que = collections.deque()
+        res = 0
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == '1':
+                    res += 1
+                    grid[i][j] = '0'
+                    que.append((i, j))
+                    while que:
+                        x, y = que.pop()
+                        for d in directions:
+                            nx, ny = x + d[0], y + d[1]
+                            if 0 <= nx < M and 0 <= ny < N and grid[nx][ny] == '1':
+                                grid[nx][ny] = '0'
+                                que.append((nx, ny))
+        return res
+        
+# V2 
+# Time:  O(m * n)
+# Space: O(m * n)
+class Solution(object):
+    # @param {boolean[][]} grid a boolean 2D matrix
+    # @return {int} an integer
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+
+        row = len(grid)
+        col = len(grid[0])
+        count = 0
+        for i in xrange(row):
+            for j in xrange(col):
+                if grid[i][j] == '1':
+                    self.dfs(grid, row, col, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, row, col, x, y):
+        if grid[x][y] == '0':
+            return
+        grid[x][y] = '0'
+
+        if x != 0:
+            self.dfs(grid, row, col, x - 1, y)
+        if x != row - 1:
+            self.dfs(grid, row, col, x + 1, y)
+        if y != 0:
+            self.dfs(grid, row, col, x, y - 1)
+        if y != col - 1:
+            self.dfs(grid, row, col, x, y + 1)
