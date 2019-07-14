163063a77f96e437e1badbd2d8d1835ec2326bc7
yennanliu <f339339@gmail.com>
2019-07-14 16:06:36 +0800

    add dfs count-univalue-subtrees.py (dev)

---
+++ leetcode_python/Depth-First-Search/count-univalue-subtrees.py
@@ -0,0 +1,54 @@
+# V0 
+
+# V1 
+# https://xingxingpark.com/Leetcode-250-Count-Univalue-Subtrees/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def countUnivalSubtrees(self, root):
+        self.count = 0
+        self.checkUni(root)
+        return self.count
+
+# If both children are "True" and root.val is equal to both children's values that exist, 
+# then root node is uniValue subtree node. 
+    def checkUni(self, root):
+        if not root:
+            return True
+        l, r = self.checkUni(root.left), self.checkUni(root.right)
+        if l and r and (not root.left or root.left.val == root.val) and \
+        (not root.right or root.right.val == root.val):
+            self.count += 1
+            return True
+        return False
+
+# V2 
+# Time:  O(n)
+# Space: O(h)
+class Solution(object):
+    # @param {TreeNode} root
+    # @return {integer}
+    def countUnivalSubtrees(self, root):
+        [is_uni, count] = self.isUnivalSubtrees(root, 0)
+        return count
+
+    def isUnivalSubtrees(self, root, count):
+        if not root:
+            return [True, count]
+
+        [left, count] = self.isUnivalSubtrees(root.left, count)
+        [right, count] = self.isUnivalSubtrees(root.right, count)
+        if self.isSame(root, root.left, left) and \
+           self.isSame(root, root.right, right):
+                count += 1
+                return [True, count]
+
+        return [False, count]
+
+    def isSame(self, root, child, is_uni):
+        return not child or (is_uni and root.val == child.val)
