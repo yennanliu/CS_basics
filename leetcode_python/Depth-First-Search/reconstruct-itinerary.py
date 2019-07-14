e199c45e537df419c772a34e6d196394fdc72024
yennanliu <f339339@gmail.com>
2019-07-14 16:50:03 +0800

    add dfs reconstruct-itinerary.py (dev)

---
+++ leetcode_python/Depth-First-Search/reconstruct-itinerary.py
@@ -0,0 +1,61 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/83551204
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        graph = collections.defaultdict(list)
+        for frm, to in tickets:
+            graph[frm].append(to)
+        for frm, tos in graph.items():
+            tos.sort(reverse=True)
+        res = []
+        self.dfs(graph, "JFK", res)
+        return res[::-1]
+
+    def dfs(self, graph, source, res):
+        while graph[source]:
+            v = graph[source].pop()
+            self.dfs(graph, v, res)
+        res.append(source)
+        
+# V2 
+# Time:  O(t! / (n1! * n2! * ... nk!)), t is the total number of tickets,
+#                                       ni is the number of the ticket which from is city i,
+#                                       k is the total number of cities.
+# Space: O(t)
+import collections
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        def route_helper(origin, ticket_cnt, graph, ans):
+            if ticket_cnt == 0:
+                return True
+
+            for i, (dest, valid)  in enumerate(graph[origin]):
+                if valid:
+                    graph[origin][i][1] = False
+                    ans.append(dest)
+                    if route_helper(dest, ticket_cnt - 1, graph, ans):
+                        return ans
+                    ans.pop()
+                    graph[origin][i][1] = True
+            return False
+
+        graph = collections.defaultdict(list)
+        for ticket in tickets:
+            graph[ticket[0]].append([ticket[1], True])
+        for k in graph.keys():
+            graph[k].sort()
+
+        origin = "JFK"
+        ans = [origin]
+        route_helper(origin, len(tickets), graph, ans)
+        return ans
