afcc490a5f4a106cee68bdc2858da6fb366705e6
yennanliue <f339339@gmail.com>
2019-07-15 09:01:16 +0800

    add dfs pacific-atlantic-water-flow.py (dev)

---
+++ leetcode_python/Depth-First-Search/pacific-atlantic-water-flow.py
@@ -0,0 +1,76 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/82917037
+class Solution(object):
+    def pacificAtlantic(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if not matrix or not matrix[0]: return []
+        m, n = len(matrix), len(matrix[0])
+        p_visited = [[False] * n for _ in range(m)]
+        a_visited = [[False] * n for _ in range(m)]
+        for i in range(m):
+            self.dfs(p_visited, matrix, m, n, i, 0)
+            self.dfs(a_visited, matrix, m, n, i, n -1)
+        for j in range(n):
+            self.dfs(p_visited, matrix, m, n, 0, j)
+            self.dfs(a_visited, matrix, m, n, m - 1, j)
+        res = []
+        for i in range(m):
+            for j in range(n):
+                if p_visited[i][j] and a_visited[i][j]:
+                    res.append([i, j])
+        return res
+        
+    def dfs(self, visited, matrix, m, n, i, j):
+        visited[i][j] = True
+        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        for dire in directions:
+            x, y = i + dire[0], j + dire[1]
+            if x < 0 or x >= m or y < 0 or y >= n or visited[x][y] or matrix[x][y] < matrix[i][j]:
+                continue
+            self.dfs(visited, matrix, m, n, x, y)
+            
+# V2 
+# Time:  O(m * n)
+# Space: O(m * n)
+class Solution(object):
+    def pacificAtlantic(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        PACIFIC, ATLANTIC = 1, 2
+
+        def pacificAtlanticHelper(matrix, x, y, prev_height, prev_val, visited, res):
+            if (not 0 <= x < len(matrix)) or \
+               (not 0 <= y < len(matrix[0])) or \
+               matrix[x][y] < prev_height or \
+               (visited[x][y] | prev_val) == visited[x][y]:
+                return
+
+            visited[x][y] |= prev_val
+            if visited[x][y] == (PACIFIC | ATLANTIC):
+                res.append((x, y))
+
+            for d in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+                pacificAtlanticHelper(matrix, x + d[0], y + d[1], matrix[x][y], visited[x][y], visited, res)
+
+        if not matrix:
+            return []
+
+        res = []
+        m, n = len(matrix),len(matrix[0])
+        visited = [[0 for _ in range(n)] for _ in range(m)]
+
+        for i in range(m):
+            pacificAtlanticHelper(matrix, i, 0, float("-inf"), PACIFIC, visited, res)
+            pacificAtlanticHelper(matrix, i, n - 1, float("-inf"), ATLANTIC, visited, res)
+        for j in range(n):
+            pacificAtlanticHelper(matrix, 0, j, float("-inf"), PACIFIC, visited, res)
+            pacificAtlanticHelper(matrix, m - 1, j, float("-inf"), ATLANTIC, visited, res)
+
+        return res
