7135bc74de53b9a3f034a5e885903bfb31b17465
yennanliu <f339339@gmail.com>
2019-07-15 18:34:56 +0800

    add dfs flood-fill.py (dev)

---
+++ leetcode_python/Depth-First-Search/flood-fill.py
@@ -0,0 +1,82 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79401716
+# IDEA : DFS 
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        SR, SC = len(image), len(image[0])
+        color = image[sr][sc]
+        if color == newColor: return image
+        def dfs(r, c):
+            if image[r][c] == color:
+                image[r][c] = newColor
+                if r >= 1: dfs(r - 1, c)
+                if r < SR - 1: dfs(r + 1, c)
+                if c >= 1: dfs(r, c - 1)
+                if c < SC - 1: dfs(r, c + 1)
+        dfs(sr, sc)
+        return image
+
+# V1' 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79401716
+# IDEA : BFS 
+class Solution:
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        que = collections.deque()
+        que.append((sr, sc))
+        start = image[sr][sc]
+        if start == newColor: return image
+        M, N = len(image), len(image[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        while que:
+            pos = que.popleft()
+            image[pos[0]][pos[1]] = newColor
+            for d in directions:
+                newx, newy = pos[0] + d[0], pos[1] + d[1]
+                if 0 <= newx < M and 0 <= newy < N and image[newx][newy] == start:
+                    que.append((newx, newy))
+        return image
+        
+# V2 
+# Time:  O(m * n)
+# Space: O(m * n)
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+        def dfs(image, r, c, newColor, color):
+            if not (0 <= r < len(image) and \
+                    0 <= c < len(image[0]) and \
+                    image[r][c] == color):
+                return
+
+            image[r][c] = newColor
+            for d in directions:
+                dfs(image, r+d[0], c+d[1], newColor, color)
+
+        color = image[sr][sc]
+        if color == newColor: return image
+        dfs(image, sr, sc, newColor, color)
+        return image
