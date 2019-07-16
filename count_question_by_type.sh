32810812b1aa5c45ce38d57cca4855881de3def4
yennanliu <f339339@gmail.com>
2019-07-16 17:55:19 +0800

    count questions via grep after and grep before method, (update code)

--- count_question_by_type.sh
+++ count_question_by_type.sh
@@ -8,12 +8,16 @@
 # dev 
 
 #question_type=$(cat README.md | grep '##')
-question_type=('## Array Easy and Medium' '## Hash Table Easy and Medium')
+question_type=('## Array Easy and Medium' 
+               '## Hash Table Easy and Medium'
+               '## Linked List Easy and Medium')
 
 for type in  "${question_type[@]}"
   do
      #cat README.md | sed '1,/^## Shell Script easy and medium$/d'
      #cat README.md | sed '1,/^## Heap Easy and Medium $/d'  | grep -i "BitMap Easy and Medium" -B  1000 | wc -l 
-     echo $type && cat README.md | sed '1,/^${type}$/d'  |  wc -l 
-     #echo $type && cat README.md | sed '1,/^$type$/d'
+     #echo $type && cat README.md | sed '1,/^"$type"$/d' |  wc -l 
+     #cat README.md | grep -i "SQL Easy and Medium" -A 1000 | grep -i "Shell Script Easy and Medium" -B  1000 | wc -l 
+     echo $type && cat README.md | grep -i "$type" -A 1000 | wc -l 
+
   done 
