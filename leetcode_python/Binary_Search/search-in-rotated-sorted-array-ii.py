b70ff94d625b9e3a70af145825bbdbb5cc0c2e9b
yennanliu <f339339@gmail.com>
2019-07-16 16:02:59 +0800

    add bi search search-in-rotated-sorted-array-ii.py (dev)

---
+++ leetcode_python/Binary_Search/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,57 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/83214278
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        N = len(nums)
+        l, r = 0, N - 1
+        while l <= r:
+            while l < r and nums[l] == nums[r]:
+                l += 1
+            mid = l + (r - l) / 2
+            if nums[mid] == target:
+                return True
+            if nums[mid] >= nums[l]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            elif nums[mid] <= nums[r]:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return False
+
+# V2 
+# Time:  O(logn)
+# Space: O(1)
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) / 2
+
+            if nums[mid] == target:
+                return True
+            elif nums[mid] == nums[left]:
+                left += 1
+            elif (nums[mid] > nums[left] and nums[left] <= target < nums[mid]) or \
+                 (nums[mid] < nums[left] and not (nums[mid] < target <= nums[right])):
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return False
