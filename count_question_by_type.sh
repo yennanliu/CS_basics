feced2fb1de816f4689d246446db360d6b98ac63
yennanliu <f339339@gmail.com>
2019-07-16 17:30:01 +0800

    backup code

--- count_question_by_type.sh
+++ count_question_by_type.sh
@@ -3,13 +3,17 @@
 #################################################################
 # SCRIPT COUNT NUMBER OF LEETCODE QUESTIONS BY TYPE  
 #################################################################
-# https://unix.stackexchange.com/questions/80399/cat-all-lines-from-file-which-are-after-keyword
+# get call for lines after word  :  https://unix.stackexchange.com/questions/80399/cat-all-lines-from-file-which-are-after-keyword
+# get lines after and before via bash : https://stackoverflow.com/questions/12444808/how-do-i-fetch-lines-before-after-the-grep-result-in-bash
 # dev 
 
-question_type=$(cat README.md | grep '##')
+#question_type=$(cat README.md | grep '##')
+question_type=('## Array Easy and Medium' '## Hash Table Easy and Medium')
 
 for type in  "${question_type[@]}"
   do
      #cat README.md | sed '1,/^## Shell Script easy and medium$/d'
-     echo $type && cat README.md | sed '1,/^$type$/d'
+     #cat README.md | sed '1,/^## Heap Easy and Medium $/d'  | grep -i "BitMap Easy and Medium" -B  1000 | wc -l 
+     echo $type && cat README.md | sed '1,/^${type}$/d'  |  wc -l 
+     #echo $type && cat README.md | sed '1,/^$type$/d'
   done 
