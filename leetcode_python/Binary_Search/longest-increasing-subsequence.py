8612a86d848d96268eb0e7380b3939a0fbb8031d
yennanliu <f339339@gmail.com>
2019-07-17 17:04:37 +0800

    add bi search longest-increasing-subsequence,py (dev)

---
+++ leetcode_python/Binary_Search/longest-increasing-subsequence.py
@@ -0,0 +1,68 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79820919
+# IDEA : DP 
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums: return 0
+        dp = [0] * len(nums)
+        dp[0] = 1
+        for i in range(1, len(nums)):
+            tmax = 1
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    tmax = max(tmax, dp[j] + 1)
+            dp[i] = tmax
+        return max(dp)
+       
+# V2 
+# Time:  O(nlogn)
+# Space: O(n)
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        LIS = []
+        def insert(target):
+            left, right = 0, len(LIS) - 1
+            # Find the first index "left" which satisfies LIS[left] >= target
+            while left <= right:
+                mid = left + (right - left) // 2
+                if LIS[mid] >= target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # If not found, append the target.
+            if left == len(LIS):
+                LIS.append(target)
+            else:
+                LIS[left] = target
+
+        for num in nums:
+            insert(num)
+
+        return len(LIS)
+
+# Time:  O(n^2)
+# Space: O(n)
+# Traditional DP solution.
+class Solution2(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = []  # dp[i]: the length of LIS ends with nums[i]
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp) if dp else 0
