1bcdb020ddab654bab14273c8e2f7a38a90be9f5
yennanliu <f339339@gmail.com>
2019-07-19 20:31:25 +0800

    add backtracking generalized-abbreviation.py (dev)

---
+++ leetcode_python/Backtracking/generalized-abbreviation.py
@@ -0,0 +1,31 @@
+# V0 
+
+# V1 
+# https://segmentfault.com/a/1190000005762481
+# DEV 
+
+# V2 
+# Time:  O(n * 2^n)
+# Space: O(n)
+class Solution(object):
+    def generateAbbreviations(self, word):
+        """
+        :type word: str
+        :rtype: List[str]
+        """
+        def generateAbbreviationsHelper(word, i, cur, res):
+            if i == len(word):
+                res.append("".join(cur))
+                return
+            cur.append(word[i])
+            generateAbbreviationsHelper(word, i + 1, cur, res)
+            cur.pop()
+            if not cur or not cur[-1][-1].isdigit():
+                for l in range(1, len(word) - i + 1):
+                    cur.append(str(l))
+                    generateAbbreviationsHelper(word, i + l, cur, res)
+                    cur.pop()
+
+        res, cur = [], []
+        generateAbbreviationsHelper(word, 0, cur, res)
+        return res
