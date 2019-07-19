54c0eff6bc26e3b4ff4ce284a7377c2829798d68
yennanliu <f339339@gmail.com>
2019-07-19 18:27:12 +0800

    add backtracking subsets-ii.py (dev)

---
+++ leetcode_python/Backtracking/subsets-ii.py
@@ -0,0 +1,89 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79785548
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        self.dfs(nums, 0, res, [])
+        return res
+        
+    def dfs(self, nums, index, res, path):
+        if path not in res:
+            res.append(path)
+        for i in range(index, len(nums)):
+            if i > index and nums[i] == nums[i - 1]:
+                continue
+            self.dfs(nums, i + 1, res, path + [nums[i]])
+
+# V2 
+# Time:  O(n * 2^n)
+# Space: O(1)
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = [[]]
+        previous_size = 0
+        for i in xrange(len(nums)):
+            size = len(result)
+            for j in xrange(size):
+                # Only union non-duplicate element or new union set.
+                if i == 0 or nums[i] != nums[i - 1] or j >= previous_size:
+                    result.append(list(result[j]))
+                    result[-1].append(nums[i])
+            previous_size = size
+        return result
+
+
+# Time:  O(n * 2^n) ~ O((n * 2^n)^2)
+# Space: O(1)
+class Solution2(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        i, count = 0, 1 << len(nums)
+        nums.sort()
+
+        while i < count:
+            cur = []
+            for j in xrange(len(nums)):
+                if i & 1 << j:
+                    cur.append(nums[j])
+            if cur not in result:
+                result.append(cur)
+            i += 1
+
+        return result
+
+
+# Time:  O(n * 2^n) ~ O((n * 2^n)^2)
+# Space: O(1)
+class Solution3(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.subsetsWithDupRecu(result, [], sorted(nums))
+        return result
+
+    def subsetsWithDupRecu(self, result, cur, nums):
+        if not nums:
+            if cur not in result:
+                result.append(cur)
+        else:
+            self.subsetsWithDupRecu(result, cur, nums[1:])
+            self.subsetsWithDupRecu(result, cur + [nums[0]], nums[1:])
