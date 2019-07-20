12cb4a1a9e3aacb3ae3ce5e7c5ff0eb04563ac0f
yennanliu <f339339@gmail.com>
2019-07-20 16:52:51 +0800

    add greedy minimum-number-of-arrows-to-burst-balloons.py (dev)

---
+++ leetcode_python/Greedy/minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,47 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79888836
+# https://blog.csdn.net/MebiuW/article/details/53096708
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        if not points: return 0
+        points.sort(key = lambda x : x[1])
+        curr_pos = points[0][1]
+        ans = 1
+        for i in range(len(points)):
+            if curr_pos >= points[i][0]:
+                continue
+            curr_pos = points[i][1]
+            ans += 1
+        return ans
+
+# V2 
+# Time:  O(nlogn)
+# Space: O(1)
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        if not points:
+            return 0
+
+        points.sort()
+
+        result = 0
+        i = 0
+        while i < len(points):
+            j = i + 1
+            right_bound = points[i][1]
+            while j < len(points) and points[j][0] <= right_bound:
+                right_bound = min(right_bound, points[j][1])
+                j += 1
+            result += 1
+            i = j
+        return result
