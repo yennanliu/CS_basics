87614ce8c45c97bf49bdff14116bec2c7f045bc0
yennanliu <f339339@gmail.com>
2019-07-20 16:14:28 +0800

    add greedy remove-k-digits.py (dev)

---
+++ leetcode_python/Greedy/remove-k-digits.py
@@ -0,0 +1,44 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/81034522
+# IDEA : STACK 
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        if len(num) == k:
+            return '0'
+        stack = []
+        for n in num:
+            while stack and k and int(stack[-1]) > int(n):
+                stack.pop()
+                k -= 1
+            stack.append(n)
+        while k:
+            stack.pop()
+            k -= 1
+        if not stack:
+            return '0'
+        return str(int("".join(stack)))
+       
+# V2 
+# Time:  O(n)
+# Space: O(n)
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        result = []
+        for d in num:
+            while k and result and result[-1] > d:
+                result.pop()
+                k -= 1
+            result.append(d)
+        return ''.join(result).lstrip('0')[:-k or None] or '0'
