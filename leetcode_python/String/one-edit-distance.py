0f7cb9688c3b16089b9159fa41b50345b14b1662
Travis CI <f339339@gmail.com>
2019-07-23 23:12:54 +0800

    add string one-edit-distance.py (dev)

---
+++ leetcode_python/String/one-edit-distance.py
@@ -0,0 +1,63 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/zhangpeterx/article/details/90577678
+# https://www.cnblogs.com/lightwindy/p/8606871.html
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
+        lenS=len(s)
+        lenT=len(t)
+        if lenS>lenT:
+            s,t=t,s
+            lenS,lenT=lenT,lenS
+        
+        if lenS==lenT:
+            count=0
+            for i in range(0,lenS):
+                if s[i]!=t[i]:
+                    count+=1
+                    if count>=2:
+                        break
+            if count==1:
+                return True
+            else:
+                return False
+        elif lenT-lenS>=2:
+            return False
+        else:
+            flag=0
+            for i in range(0,lenT):
+                if i==lenT-1 and flag==0:
+                    return True
+                elif s[i-flag]!=t[i]:
+                    if flag==0:
+                        flag=1
+                    else:
+                        return False
+            return True
+
+# V2 
+# Time:  O(m + n)
+# Space: O(1)
+class Solution(object):
+    def isOneEditDistance(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        m, n = len(s), len(t)
+        if m > n:
+            return self.isOneEditDistance(t, s)
+        if n - m > 1:
+            return False
+
+        i, shift = 0, n - m
+        while i < m and s[i] == t[i]:
+            i += 1
+        if shift == 0:
+            i += 1
+        while i < m and s[i] == t[i + shift]:
+            i += 1
+
+        return i == m
