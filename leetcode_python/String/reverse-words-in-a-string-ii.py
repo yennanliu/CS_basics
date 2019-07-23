c365370aa0563cbaaa03809a08154cc6a0f789c2
Travis CI <f339339@gmail.com>
2019-07-24 00:10:34 +0800

    add string reverse-words-in-a-string-ii.py (dev)

---
+++ leetcode_python/String/reverse-words-in-a-string-ii.py
@@ -0,0 +1,41 @@
+# V0 
+
+# V1 
+# http://www.voidcn.com/article/p-eggrnnob-zo.html
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: a list of 1 length strings (List[str])
+        :rtype: nothing
+        """
+        s.reverse()
+        i = 0
+        while i < len(s):
+            j = i
+            while j < len(s) and s[j] != " ":
+                j += 1
+            for k in range(i, i + (j - i) / 2 ):
+                t = s[k]
+                s[k] = s[i + j - 1 - k]
+                s[i + j - 1 - k] = t
+            i = j + 1
+            
+# V2 
+# Time: O(n)
+# Space:O(1)
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: a list of 1 length strings (List[str])
+        :rtype: nothing
+        """
+        def reverse(s, begin, end):
+            for i in range((end - begin) / 2):
+                s[begin + i], s[end - 1 - i] = s[end - 1 - i], s[begin + i]
+
+        reverse(s, 0, len(s))
+        i = 0
+        for j in range(len(s) + 1):
+            if j == len(s) or s[j] == ' ':
+                reverse(s, i, j)
+                i = j + 1
