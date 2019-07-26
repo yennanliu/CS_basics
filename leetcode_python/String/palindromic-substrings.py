0faed5d2bc6fa45a8cc149849f61fad00b0bab3c
Travis CI <f339339@gmail.com>
2019-07-26 23:44:37 +0800

    add string palindromic-substrings.py (dev)

---
+++ leetcode_python/String/palindromic-substrings.py
@@ -0,0 +1,91 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79433960
+# IDEA : GREEDY 
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if s[i:j + 1] == s[i:j + 1][::-1]:
+                    count += 1
+        return count
+
+# V1' 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79433960
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(s)):
+            count += 1
+            #length of palindromic substrings is odd 
+            left = i - 1
+            right = i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+            #length of palindromic substrings is even 
+            left = i
+            right = i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+        return count
+
+
+# V1'' 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79433960
+# IDEA : DP 
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        count = 0
+        start, end, maxL = 0, 0, 0
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(i):
+                dp[j][i] = (s[j] == s[i]) & ((i - j < 2) | dp[j + 1][i - 1])
+                if dp[j][i]:
+                    count += 1
+            dp[i][i] = 1
+            count += 1
+        return count
+
+# V2 
+# Time:  O(n)
+# Space: O(n)
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        def manacher(s):
+            s = '^#' + '#'.join(s) + '#$'
+            P = [0] * len(s)
+            C, R = 0, 0
+            for i in range(1, len(s) - 1):
+                i_mirror = 2*C-i
+                if R > i:
+                    P[i] = min(R-i, P[i_mirror])
+                while s[i+1+P[i]] == s[i-1-P[i]]:
+                    P[i] += 1
+                if i+P[i] > R:
+                    C, R = i, i+P[i]
+            return P
+        return sum((max_len+1)//2 for max_len in manacher(s))
