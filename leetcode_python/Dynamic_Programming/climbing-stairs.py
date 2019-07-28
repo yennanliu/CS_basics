8088b36c96aaacc7b5df74749859cb21cb1154ff
Travis CI <f339339@gmail.com>
2019-07-28 22:13:35 +0800

    update DP climbing-stairs.py

--- leetcode_python/Dynamic_Programming/climbing-stairs.py
+++ leetcode_python/Dynamic_Programming/climbing-stairs.py
@@ -6,20 +6,46 @@
 # Each time you can either climb 1 or 2 steps.
 # In how many distinct ways can you climb to the top?
 
-
-# V0 : DEV 
-
+# V0  
+class Solution:
+    # Time:  O(2^n)
+    # Space: O(n)
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
 
 # V1 
+# https://blog.csdn.net/coder_orz/article/details/51506414
 # n = 1,2,3,4,5,6,7,8,9... -> output : 1,2,3,5,8,13,21,34....
 # -> output(n) = output(n-2) + output(n-1)
-# https://blog.csdn.net/coder_orz/article/details/51506414
+# IDEA  : Fibonacci SERIES 
 class Solution:
     def climbStairs(self, n):
         prev, current = 0, 1 
         for i in range(n):
             prev, current =  current, prev+current
         return current
+
+
+# V1' 
+# https://blog.csdn.net/coder_orz/article/details/51506414
+# IDEA : DP 
+# RECURSION WOULD BE TOO SLOW, SO USE DP SPEED UP THE PROCESS (DP CAN RECORD HISTORY)
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0 or n == 1 or n == 2:
+            return n
+        steps = [1, 1]
+        for i in range(2, n+1):
+            steps.append(steps[i-1] + steps[i-2])
+        return steps[n]
 
 # V2 
 class Solution:
@@ -41,13 +67,6 @@ class Solution:
         if n == 2:
             return 2
         return self.climbStairs(n - 1) + self.climbStairs(n - 2)
-
-
-"""
-if __name__ == "__main__":
-    result = Solution().climbStairs(2)
-    print result
-"""
 
 # V3 
 # Time:  O(logn)
@@ -77,7 +96,6 @@ class Solution(object):
         T = [[1, 1],
              [1, 0]]
         return matrix_expo(T, n)[0][0]
-
 
 # Time:  O(n)
 # Space: O(1)
