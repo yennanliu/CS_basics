b4f292456ae72a9728b36110a12f91627691d8a6
Travis CI <f339339@gmail.com>
2019-07-29 11:06:53 +0800

    update comment, status DP :  minimum-path-sum.py

--- leetcode_python/Dynamic_Programming/minimum-path-sum.py
+++ leetcode_python/Dynamic_Programming/minimum-path-sum.py
@@ -2,18 +2,28 @@
 
 # V1 
 # https://blog.csdn.net/xx_123_1_rj/article/details/84571645
-# idea : 
-# dp[i][j] = dp[i][j] + min(dp[i-1][j], dp[i][j-1])
+# IDEA : 
+# DP : dp[i][j] = dp[i][j] + min(dp[i-1][j], dp[i][j-1])
+# DEMO 
+#    ...: grid = [
+#    ...:     [1, 3, 1],
+#    ...:     [1, 5, 1],
+#    ...:     [4, 2, 1]]
+#    ...: solu = Solution()
+#    ...: print(solu.minPathSum(grid))
+#    ...:     
+# grid : [[1, 4, 5], [2, 7, 6], [6, 8, 7]]
+# 7
 class Solution:
     def minPathSum(self, grid):
 
         if not grid: return None
         m, n = len(grid), len(grid[0])  # row, column 
 
-        for i in range(1, m):  # deal with boundary  -- the 1sr row 
+        for i in range(1, m):  ###  deal with boundary (→)  -- the 1st row, since the 1st row can only be visited via →. so we can fill them first at beginning 
             grid[i][0] += grid[i-1][0]
 
-        for j in range(1, n):  # deal with boundary  -- the 1sr column  
+        for j in range(1, n):  ### deal with boundary (↓) -- the 1st column, since the 1st column can only be visited via ↓. so we can fill them first at beginning  
             grid[0][j] += grid[0][j-1]
 
         for i in range(1, m):  # get dp[i][j] += min(dp[i-1][j], dp[i][j-1])
