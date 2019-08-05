530adc4c7e35a53b60fba80d3d197b3fcb8577e7
yennanliu <f339339@gmail.com>
2019-08-05 14:06:00 +0800

    update status/code /Hash_table/longest-palindrome.py

--- leetcode_python/Hash_table/longest-palindrome.py
+++ leetcode_python/Hash_table/longest-palindrome.py
@@ -40,14 +40,30 @@ One longest palindrome that can be built is "dccaccd", whose length is 7.
 # Explanation:
 # One longest palindrome that can be built is "dccaccd", whose length is 7.
 
+# V0 
 
-# V1  : dev 
 
+# V1 
+# http://bookshadow.com/weblog/2016/10/02/leetcode-longest-palindrome/
+# IDEA : collections.Counter
+import collections 
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ans = odd = 0
+        cnt = collections.Counter(s)
+        for c in cnt:
+            ans += cnt[c]
+            if cnt[c] % 2 == 1:
+                ans -= 1        # for cases : "ccc", will only take substring "cc" to form the palindrome string.  
+                odd += 1        # to cacluate how many odd elements in the string, and ans = ans +1 if there are odd elements 
+        return ans + (odd > 0)  # for cases : "cccaadd", "cc" can be palindrome, while c is odd, and can palindrome string allow 1 odd element.  if true, (odd > 0) == 1 ; else : (odd > 0) == 0  
 
 # V2 
 import collections
-
-
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -66,6 +82,3 @@ class Solution(object):
         """
         odd = sum([x & 1 for x in list(collections.Counter(s).values())])
         return len(s) - odd + int(odd > 0)
-
-
-
