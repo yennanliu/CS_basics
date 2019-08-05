c574690ae93cb4ef3775a948a6b41958893b84ea
yennanliu <f339339@gmail.com>
2019-08-05 15:24:21 +0800

    add Hash_table/4sum-ii.py (dev)

---
+++ leetcode_python/Hash_table/4sum-ii.py
@@ -0,0 +1,39 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/danspace1/article/details/87854254
+# http://bookshadow.com/weblog/2016/11/13/leetcode-4sum-ii/
+class Solution(object):
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        ans = 0
+        cnt = collections.defaultdict(int)
+        for a in A:
+            for b in B:
+                cnt[a + b] += 1
+        for c in C:
+            for d in D:
+                ans += cnt[-(c + d)]
+        return ans
+
+# V2 
+# Time:  O(n^2)
+# Space: O(n^2)
+import collections
+class Solution(object):
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        A_B_sum = collections.Counter(a+b for a in A for b in B)
+        return sum(A_B_sum[-c-d] for c in C for d in D)
