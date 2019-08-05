85649c0d81ae0dde92db6c5efaee2e5a06de4f79
yennanliu <f339339@gmail.com>
2019-08-05 14:59:15 +0800

    update status/code Hash_table/number-of-boomerangs.py

--- leetcode_python/Hash_table/number-of-boomerangs.py
+++ leetcode_python/Hash_table/number-of-boomerangs.py
@@ -1,7 +1,5 @@
-
 # Time:  O(n^2)
 # Space: O(n)
-
 # Given n points in the plane that are all pairwise distinct,
 # a "boomerang" is a tuple of points (i, j, k) such that the distance
 # between i and j equals the distance between i and k (the order of the tuple matters).
@@ -19,14 +17,48 @@
 # Explanation:
 # The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
 
-# V1 : dev 
+# Given n points in the plane that are all pairwise distinct, a "boomerang" is a tuple of points (i, j, k) 
+# such that the distance between i and j equals the distance between i and k 
+# (the order of the tuple matters).
+# e.g. : 
+# Input:
+# [[0,0],[1,0],[2,0]]
+
+# Output:
+# 2
+
+# Explanation:
+# The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
 
 
+# V0 
+
+# V1 
+# IDEA : collections.defaultdict
+# http://bookshadow.com/weblog/2016/11/06/leetcode-number-of-boomerangs/
+# https://blog.csdn.net/fuxuemingzhu/article/details/54379349
+import collections
+class Solution(object):
+    def numberOfBoomerangs(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        ans = 0
+        for x1, y1 in points:
+            dmap = collections.defaultdict(int)
+            for x2, y2 in points:
+                dmap[(x1 - x2) ** 2 + (y1 - y2) ** 2] += 1
+            for d in dmap:
+                ans += dmap[d] * (dmap[d] - 1)
+        return ans
+
+# V1'
+# IDEA : TWO POINTERS
+# DEV 
 
 # V2 
 import collections
-
-
 class Solution(object):
     def numberOfBoomerangs(self, points):
         """
