322528169c54ab79dbe53f4b5a8c5eca7af368bc
Travis CI <f339339@gmail.com>
2019-08-06 08:27:07 +0800

    update status/code Hash_table/array-partition-i.py

--- leetcode_python/Hash_table/array-partition-i.py
+++ leetcode_python/Hash_table/array-partition-i.py
@@ -1,6 +1,5 @@
 # Time:  O(r), r is the range size of the integers
 # Space: O(r)
-
 # Given an array of 2n integers, your task is to group these integers into n pairs of integer,
 # say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
 #
@@ -14,32 +13,21 @@
 # All the integers in the array will be in the range of [-10000, 10000].
 
 
-# V1 : dev 
-# sum up all sorted elements from index 0 to index even
-# https://xenstalker.tw/2017/05/07/leetcode-problems-561-array-partition-i/index.html
+# V0 
+
+# V1 
+# http://bookshadow.com/weblog/2017/04/23/leetcode-array-partition-i/
 class Solution(object):
     def arrayPairSum(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        result = 0
-        for i in range(0, len(nums), 2):
-            result += nums[i]
-        return result
-
-
-
-
-
-
-try:
-    xrange          # Python 2
-except NameError:
-    xrange = range  # Python 3
+        return sum(sorted(nums)[::2])
 
 # V2 
+# Time:  O(r), r is the range size of the integers
+# Space: O(r)
 class Solution(object):
     def arrayPairSum(self, nums):
         """
@@ -51,12 +39,11 @@ class Solution(object):
         for num in nums:
             lookup[num-LEFT] += 1
         r, result = 0, 0
-        for i in range(LEFT, RIGHT+1):
+        for i in xrange(LEFT, RIGHT+1):
             result += (lookup[i-LEFT] + 1 - r) / 2 * i
             r = (lookup[i-LEFT] + r) % 2
         return result
 
-# V3 
 # Time:  O(nlogn)
 # Space: O(1)
 class Solution2(object):
@@ -71,7 +58,6 @@ class Solution2(object):
             result += nums[i]
         return result
 
-# V4 
 # Time:  O(nlogn)
 # Space: O(n)
 class Solution3(object):
