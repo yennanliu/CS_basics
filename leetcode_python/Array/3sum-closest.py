1385e7125d3d23ee8fba86a51a0e093d51d0dc74
Travis CI <f339339@gmail.com>
2019-08-09 10:51:19 +0800

    update status/code Array/3sum-closest.py (dev)

--- leetcode_python/Array/3sum-closest.py
+++ leetcode_python/Array/3sum-closest.py
@@ -1,7 +1,6 @@
 # https://leetcode.com/problems/3sum-closest/description/
 # Time:  O(n^2)
 # Space: O(1)
-
 # Given an array S of n integers,
 # find three integers in S such that the sum is closest to a given number,
 # target.
@@ -12,14 +11,57 @@
 #
 # The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
+# V0 
 
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/83116781
+# IDEA : SORT + DOUBLE POINTER
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        N = len(nums)
+        nums.sort()
+        res = float('inf') # sum of 3 number
+        for t in range(N):
+            i, j = t + 1, N - 1
+            while i < j:
+                _sum = nums[t] + nums[i] + nums[j]
+                if abs(_sum - target) < abs(res - target):
+                    res = _sum
+                if _sum > target:
+                    j -= 1
+                elif _sum < target:
+                    i += 1
+                else:
+                    return target
+        return res
 
-# V1 : dev 
-
-
-
-
+# V1'
+# https://www.cnblogs.com/zuoyuan/p/3699449.html
+class Solution:
+    # @return an integer
+    def threeSumClosest(self, num, target):
+        num.sort()
+        mindiff=100000
+        res=0
+        for i in range(len(num)):
+            left=i+1; right=len(num)-1
+            while left<right:
+                sum=num[i]+num[left]+num[right]
+                diff=abs(sum-target)
+                if diff<mindiff: mindiff=diff; res=sum
+                if sum==target: return sum
+                elif sum<target: left+=1
+                else: right-=1
+        return res
+        
 # V2 
+# Time:  O(n^2)
+# Space: O(1)
 class Solution(object):
     def threeSumClosest(self, nums, target):
         """
@@ -44,6 +86,3 @@ class Solution(object):
                         return target
             i += 1
         return result
-
-
-
