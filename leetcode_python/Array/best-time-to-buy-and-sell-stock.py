f69f1230a93143641382bfb9cb4749ca2e0711b2
yennanliu <f339339@gmail.com>
2019-08-10 16:05:52 +0800

    update status/code Array/best-time-to-buy-and-sell-stock.py

--- leetcode_python/Array/best-time-to-buy-and-sell-stock.py
+++ leetcode_python/Array/best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,7 @@
 """
-
 Say you have an array for which the ith element is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+If you were only permitted to complete AT MOST ONE transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
 Note that you cannot sell a stock before you buy one.
 
@@ -17,52 +16,75 @@ Example 2:
 Input: [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
-
-
 """
 
+# V0 
 
-# V1  : dev (run time error)
-"""
-class Solution:
+# V1 
+# https://blog.csdn.net/coder_orz/article/details/51520971
+# TO NOTE : CAN ONLY DO ONE TRANSACTION IN THE PROBLEM  
+class Solution(object):
     def maxProfit(self, prices):
-        price_list = []
-        min_max_parice=[]
-        for index, price in enumerate(prices):
-            if index >= len(prices)-1 :
-                break 
-            else:
-                prices_ = prices[index+1:]
-                profit = max(prices_) - price
-                if profit>0:
-                    price_list.append(profit)
-                else:
-                    price_list.append(0)
-        if len(price_list) == 0:
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) == 0:
             return 0
-        else:
-            return max(price_list)
-       
+        minPrice = prices[0]
+        maxProfit = 0
+        for p in prices:
+            if p < minPrice:
+                minPrice = p
+            elif p - minPrice > maxProfit:
+                maxProfit = p - minPrice
+        return maxProfit
 
-"""
+# V1' 
+# https://blog.csdn.net/coder_orz/article/details/51520971
+# IDEA : DP 
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) == 0:
+            return 0
+        minPrice = prices[0]
+        dp = [0] * len(prices)
+        for i in range(0, len(prices)):
+            dp[i] = max(dp[i-1], prices[i] - minPrice)
+            minPrice = min(minPrice, prices[i])
+        return dp[-1]
 
+# V1''
+# https://www.jiuzhang.com/solution/best-time-to-buy-and-sell-stock/#tag-highlight-lang-python
+class Solution:
+    """
+    @param prices: Given an integer array
+    @return: Maximum profit
+    """
+    def maxProfit(self, prices):
+        # write your code here
+        total = 0
+        low, high = sys.maxint, 0
+        for x in prices:
+            if x - low > total:
+                total = x - low
+            if x < low:
+                low = x
+        return total
 
 # V2 
-# https://github.com/yennanliu/LeetCode/blob/master/Python/best-time-to-buy-and-sell-stock.py
+# Time:  O(n)
+# Space: O(1)
 class Solution(object):
     # @param prices, a list of integer
     # @return an integer
     def maxProfit(self, prices):
-        max_profit= 0 
-        min_price = float("inf")
+        max_profit, min_price = 0, float("inf")
         for price in prices:
             min_price = min(min_price, price)
             max_profit = max(max_profit, price - min_price)
         return max_profit
-
-
-
-
-
-
-
