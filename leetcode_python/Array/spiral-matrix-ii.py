0917fc87a917f92ce36a5e7f0a6db385633d877e
Travis CI <f339339@gmail.com>
2019-08-10 11:28:20 +0800

    update status/code Array/spiral-matrix-ii.py (dev)

--- leetcode_python/Array/spiral-matrix-ii.py
+++ leetcode_python/Array/spiral-matrix-ii.py
@@ -1,16 +1,72 @@
-
-# Time:  O(n^2)
-# Space: O(1)
-#
 # Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
-#
+
 # For example,
 # Given n = 3,
-#
+
 # You should return the following matrix:
 # [
 #  [ 1, 2, 3 ],
 #  [ 8, 9, 4 ],
 #  [ 7, 6, 5 ]
 # ]
-#
+
+
+# V0 
+
+# V1 
+class Solution:
+    # @return a list of lists of integer
+    def generateMatrix(self, n):
+        if n == 0: return []
+        matrix = [[0 for i in range(n)] for j in range(n)]
+        up = 0; down = len(matrix)-1
+        left = 0; right = len(matrix[0])-1
+        direct = 0; count = 0
+        while True:
+            if direct == 0:
+                for i in range(left, right+1):
+                    count += 1; matrix[up][i] = count
+                up += 1
+            if direct == 1:
+                for i in range(up, down+1):
+                    count += 1; matrix[i][right] = count
+                right -= 1
+            if direct == 2:
+                for i in range(right, left-1, -1):
+                    count += 1; matrix[down][i] = count
+                down -= 1
+            if direct == 3:
+                for i in range(down, up-1, -1):
+                    count += 1; matrix[i][left] = count
+                left += 1
+            if count == n*n: return matrix
+            direct = (direct+1) % 4
+            
+# V2 
+# Time:  O(n^2)
+# Space: O(1)
+class Solution(object):
+    # @return a list of lists of integer
+    def generateMatrix(self, n):
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+
+        left, right, top, bottom, num = 0, n - 1, 0, n - 1, 1
+
+        while left <= right and top <= bottom:
+            for j in range(left, right + 1):
+                matrix[top][j] = num
+                num += 1
+            for i in range(top + 1, bottom):
+                matrix[i][right] = num
+                num += 1
+            for j in reversed(range(left, right + 1)):
+                if top < bottom:
+                    matrix[bottom][j] = num
+                    num += 1
+            for i in reversed(range(top + 1, bottom)):
+                if left < right:
+                    matrix[i][left] = num
+                    num += 1
+            left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
+
+        return matrix
