0eca56bff72ee8f86f30693cabe6778ba2679ff7
yennanliu <f339339@gmail.com>
2019-08-12 14:40:55 +0800

    add Array/product-of-array-except-self.py (dev)

---
+++ leetcode_python/Array/product-of-array-except-self.py
@@ -0,0 +1,51 @@
+# V0 
+
+# V1 
+# http://bookshadow.com/weblog/2015/07/16/leetcode-product-array-except-self/
+# https://blog.csdn.net/fuxuemingzhu/article/details/79325534
+# IDEA : 
+# SINCE output[i] = (x0 * x1 * ... * xi-1) * (xi+1 * .... * xn-1)
+# -> SO DO A 2 LOOP
+# -> 1ST LOOP : GO THROGH THE ARRAY (->) : (x0 * x1 * ... * xi-1)
+# -> 2ND LOOP : GO THROGH THE ARRAY (<-) : (xi+1 * .... * xn-1)
+# e.g.
+# given [1,2,3,4], return [24,12,8,6].
+# -> output = [2*3*4, 1,1,1]  <-- 2*3*4    (right of 1: 2,3,4)
+# -> output = [2*3*4, 1*3*4,1,1] <-- 1*3*4 (left of 2 :1, right of 2: 3,4)
+# -> output = [2*3*4, 1*3*4,1*2*4,1] <-- 1*2*4 (left of 3: 1,2 right of 3 : 4)
+# -> output = [2*3*4, 1*3*4,1*2*4,1*2*3] <-- 1*2*3 (left of 4 : 1,2,3)
+# -> final output  = [2*3*4, 1*3*4,1*2*4,1*2*3] = [24,12,8,6]
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[]}
+    def productExceptSelf(self, nums):
+        size = len(nums)
+        output = [1] * size
+        left = 1
+        for x in range(size - 1):
+            left *= nums[x]
+            output[x + 1] *= left
+        right = 1
+        for x in range(size - 1, 0, -1):
+            right *= nums[x]
+            output[x - 1] *= right
+        return output
+
+# V2 
+# Time:  O(n)
+# Space: O(1)
+class Solution(object):
+    # @param {integer[]} nums
+    # @return {integer[]}
+    def productExceptSelf(self, nums):
+        if not nums:
+            return []
+        left_product = [1 for _ in range(len(nums))]
+        for i in range(1, len(nums)):
+            left_product[i] = left_product[i - 1] * nums[i - 1]
+
+        right_product = 1
+        for i in range(len(nums) - 2, -1, -1):
+            right_product *= nums[i + 1]
+            left_product[i] = left_product[i] * right_product
+        return left_product
