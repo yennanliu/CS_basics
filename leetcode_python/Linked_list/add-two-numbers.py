a71d7b8cb0fc88427a00f07065e43974c0e7806a
yennanliu <f339339@gmail.com>
2019-08-12 10:52:16 +0800

    update status/code /Linked_list/add-two-numbers.py

--- leetcode_python/Linked_list/add-two-numbers.py
+++ leetcode_python/Linked_list/add-two-numbers.py
@@ -1,19 +1,83 @@
-# V1 : dev 
+# You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+
+# V0 
 
 
+# V1
+# http://bookshadow.com/weblog/2015/04/05/leetcode-add-two-numbers/
+# IDEA : LINKED LIST
+# e.g.  :
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+#    2 -> 4 -> 3
+# +  5 -> 6 -> 4
+# -------------------
+#    7 -> 10 -> 7
 
+# 7 -> 10 -> 7  ----->  7 -> 10-10 -> 7+1  -----> 7 -> 0 -> 8  
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    # @return a ListNode
+    def addTwoNumbers(self, l1, l2):
+        head = ListNode(0)
+        l = head
+        carry = 0
+        while l1 or l2 or carry:
+            sum, carry = carry, 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            if sum > 9:
+                carry = 1
+                sum -= 10
+            l.next = ListNode(sum)
+            l = l.next
+        return head.next
 
+# V1'
+# https://blog.csdn.net/fuxuemingzhu/article/details/79379626
+# IDEA : STRING  
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        num1 = ''
+        num2 = ''
+        while l1:
+            num1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            num2 += str(l2.val)
+            l2 = l2.next
+        add = str(int(num1[::-1]) + int(num2[::-1]))[::-1]
+        head = ListNode(add[0])
+        answer = head
+        for i in range(1, len(add)):
+            node = ListNode(add[i])
+            head.next = node
+            head = head.next
+        return answer
 
-
-# V2
+# V2 
 # Time:  O(n)
 # Space: O(1)
-
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
-
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -29,15 +93,13 @@ class Solution(object):
             val = carry
             if l1:
                 val += l1.val
-                l1 = l1.__next__
+                l1 = l1.next
             if l2:
                 val += l2.val
-                l2 = l2.__next__
+                l2 = l2.next
             carry, val = divmod(val, 10)
             current.next = ListNode(val)
-            current = current.__next__
-
+            current = current.next
         if carry == 1:
             current.next = ListNode(1)
-
-        return dummy.__next__
+        return dummy.next
