619d84d76c7e91d894ef6a38850024822bd7af0b
yennanliu <f339339@gmail.com>
2019-08-13 16:21:01 +0800

    update status/code /Array/flip-game.py

--- leetcode_python/Array/flip-game.py
+++ leetcode_python/Array/flip-game.py
@@ -1,6 +1,4 @@
-
 """
-
 You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
 
 Write a function to compute all possible states of the string after one valid move.
@@ -13,13 +11,10 @@ For example, given s = "++++", after one move, it may become one of the followin
   "++--"
 ]
  
-
 If there is no valid move, return an empty list [].
-
 """
 
-
-# V1 
+# V0  
 class Solution(object):
     def generatePossibleNextMoves(self, s):
       states = []
@@ -29,14 +24,22 @@ class Solution(object):
         #states.append('-'*i + '++' +  s[2+i:])
       return list(set(states))
 
+# V1 
+# http://www.voidcn.com/article/p-umymlqqs-qp.html
+# IDEA : FIND OUT ALL EXISTING "++"  AND REPLACE WITH "--"  <------ flip two consecutive "++" into "--"
+# http://www.voidcn.com/article/p-umymlqqs-qp.html
+# https://blog.csdn.net/qq508618087/article/details/50855968
+class Solution(object):
+    def generatePossibleNextMoves(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        return [(s[:i] + "--" + s[i+2:]) for i in range(len(s)) if s[i:i+2] == "++"]
 
-
-
-# V2 
+# V2
 # Time:  O(c * n + n) = O(n * (c+1))
 # Space: O(n)
-
-# This solution compares only O(1) times for the two consecutive "+"
 class Solution(object):
     def generatePossibleNextMoves(self, s):
         """
@@ -64,8 +67,3 @@ class Solution2(object):
       :rtype: List[str]
       """
       return [s[:i] + "--" + s[i+2:] for i in range(len(s) - 1) if s[i:i+2] == "++"]
-
-
-
-
-
