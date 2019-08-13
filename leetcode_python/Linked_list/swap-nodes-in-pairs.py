97c633ce001857b36a57b5e4f1d8a9c1967b0209
yennanliu <f339339@gmail.com>
2019-08-13 12:05:32 +0800

    update status/code Linked_list/swap-nodes-in-pairs.py

--- leetcode_python/Linked_list/swap-nodes-in-pairs.py
+++ leetcode_python/Linked_list/swap-nodes-in-pairs.py
@@ -1,49 +1,55 @@
+# V0 
 
-
-# V1  : dev 
-
-
-
+# V1 
+# https://blog.csdn.net/coder_orz/article/details/51532184
+# IDEA : LINKED LIST 
+# Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        pre = new_head = ListNode(0)
+        while head and head.next:
+            tmp = head.next
+            head.next = tmp.next
+            tmp.next = head
+            pre.next = tmp
+            pre = head
+            head = head.next
+        return new_head.next
 
-#     def __repr__(self):
-#         if self:
-#             return "{} -> {}".format(self.val, self.next)
-
-# class Solution(object):
-# 	# @param a ListNode
-# 	# @return a ListNode
-# 	def swapPairs(self, list_):
-# 		dummy = ListNode(0)
-# 		#dummy.next = head
-# 		current = dummy
-# 		output = []
-# 		if list_.next is None:
-# 			return list_
-# 		while list_:
-# 			if list_.next and  list_.next.next:
-# 				output.append(list_.next.val)
-# 				output.append(list_.val)
-# 				list_=list_.next.next
-# 			elif list_.next:
-# 				output.append(list_.next.val)
-# 				output.append(list_.val)
-# 				list_=list_.next
-# 			elif list_ and list_.next is None:
-# 				output.append(list_.val)
-# 				#current = current.next 
-# 				return output 
-# 		return output
-
-
+# V1'
+# https://blog.csdn.net/coder_orz/article/details/51532184
+# IDEA : ITERATION  
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        new_head = head.next
+        head.next = self.swapPairs(head.next.next)
+        new_head.next = head
+        return new_head
 
 # V2 
 # Time:  O(n)
 # Space: O(1)
-
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -51,7 +57,7 @@ class ListNode(object):
 
     def __repr__(self):
         if self:
-            return "{} -> {}".format(self.val, self.__next__)
+            return "{} -> {}".format(self.val, self.next)
 
 class Solution(object):
     # @param a ListNode
@@ -60,11 +66,10 @@ class Solution(object):
         dummy = ListNode(0)
         dummy.next = head
         current = dummy
-        while current.__next__ and current.next.__next__:
-            next_one, next_two, next_three = current.__next__, current.next.__next__, current.next.next.__next__
+        while current.next and current.next.next:
+            next_one, next_two, next_three = current.next, current.next.next, current.next.next.next
             current.next = next_two
             next_two.next = next_one
             next_one.next = next_three
             current = next_one
-        return dummy.__next__
-
+        return dummy.next
