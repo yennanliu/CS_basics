8b9898a60e002e8b03732021695cb6b9c06cb78b
yennanliu <f339339@gmail.com>
2019-08-13 17:19:51 +0800

    add /Array/sparse-matrix-multiplication.py (dev)

---
+++ leetcode_python/Array/sparse-matrix-multiplication.py
@@ -0,0 +1,59 @@
+# V0 
+# TODO : OPTIMIZE THE PROCESS DUE TO THE SPARSE-MATRIX CONDITION 
+class Solution(object):
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        m, n, l = len(A), len(A[0]), len(B[0])
+        res = [[0 for _ in range(l)] for _ in range(m)]
+        for i in range(m):
+            for k in range(n):
+                if A[i][k]:
+                    for j in range(l):
+                        res[i][j] += A[i][k] * B[k][j]
+        return res
+
+# V1 
+# http://www.voidcn.com/article/p-muhtjhos-qp.html
+# https://www.cnblogs.com/grandyang/p/5282959.html
+# IDEA : MAXTRIX OP
+class Solution(object):
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        result = []
+        BT = zip(*B) # transfer B,get the column of B
+        length = len(B)
+        for row in A:
+            if not any(row):
+                result.append([0] * length)
+                continue
+            result.append([sum(map(lambda (x, y): x*y, zip(row, col))) 
+                                        if any(col) else 0 for col in BT])
+        return result
+
+# V2 
+# Time:  O(m * n * l), A is m x n matrix, B is n x l matrix
+# Space: O(m * l)
+class Solution(object):
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        m, n, l = len(A), len(A[0]), len(B[0])
+        res = [[0 for _ in range(l)] for _ in range(m)]
+        for i in range(m):
+            for k in range(n):
+                if A[i][k]:
+                    for j in range(l):
+                        res[i][j] += A[i][k] * B[k][j]
+        return res
+
