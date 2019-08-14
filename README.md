37b64d23dfefeca2da8988fbfd4074d58613e14b
yennanliu <f339339@gmail.com>
2019-08-14 14:47:25 +0800

    update status/code Array/third-maximum-number.py

--- README.md
+++ README.md
@@ -120,7 +120,7 @@
 384| [Shuffle an Array](https://leetcode.com/problems/shuffle-an-array/) | [Python](https://github.com/yennanliu/CS_basics/blob/master/leetcode_python/Array/shuffle-an-array.py)  | _O(n)_ |  _O(n)_ | Medium   | EPI, `trick` | OK*
 396| [Rotate Function](https://leetcode.com/problems/rotate-function/) | [Python](https://github.com/yennanliu/CS_basics/blob/master/leetcode_python/Array/rotate-function.py) | _O(n)_ |  _O(1)_ | Easy |`trick`| AGAIN (not start)
 412| [Fizz Buzz](https://leetcode.com/problems/fizz-buzz/) | [Python](https://github.com/yennanliu/CS_basics/blob/master/leetcode_python/Array/fizz-buzz.py)| _O(n)_ |  _O(1)_ | Easy || OK 
-414| [Third Maximum Number](https://leetcode.com/problems/third-maximum-number/) | [C++](./C++/third-maximum-number.cpp) [Python](./Python/third-maximum-number.py)  | _O(n)_ |  _O(1)_ | Easy         |||
+414| [Third Maximum Number](https://leetcode.com/problems/third-maximum-number/) | [Python](https://github.com/yennanliu/CS_basics/blob/master/leetcode_python/Array/third-maximum-number.py) | _O(n)_ |  _O(1)_ | Easy |`good basic`| AGAIN**
 419| [Battleships in a Board](https://leetcode.com/problems/battleships-in-a-board/) | [C++](./C++/battleships-in-a-board.cpp) [Python](./Python/battleships-in-a-board.py)  | _O(m * n)_ |  _O(1)_ | Medium         |||
 422| [Valid Word Square](https://leetcode.com/problems/valid-word-square/) | [C++](./C++/valid-word-square.cpp) [Python](./Python/valid-word-square.py)  | _O(m * n)_ |  _O(1)_ | Easy         |🔒||
 442| [Find All Duplicates in an Array](https://leetcode.com/problems/find-all-duplicates-in-an-array/) | [C++](./C++/find-all-duplicates-in-an-array.cpp) [Python](./Python/find-all-duplicates-in-an-array.py)  | _O(n)_ |  _O(1)_ | Medium         |||
--- leetcode_python/Array/third-maximum-number.py
+++ leetcode_python/Array/third-maximum-number.py
@@ -1,5 +1,4 @@
 """
-
 Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
 
 Example 1:
@@ -21,25 +20,69 @@ Output: 1
 
 Explanation: Note that the third maximum here means the third maximum distinct number.
 Both numbers with value 2 are both considered as second maximum.
-
 """
 
+# V0 
 
 # V1 
+# http://bookshadow.com/weblog/2016/10/09/leetcode-third-maximum-number/
+# IDEA : 3 VARIABLES 
 class Solution(object):
-	def thirdMax(self, nums):
-		nums_ = sorted(list(set(nums)),reverse=True)
-		try:
-			return nums_[2]
-		except:
-			if len(nums_) ==2:
-				return max(nums)
-			else:
-				return nums_[0]
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = b = c = None
+        for n in nums:
+            if n > a:
+                a, b, c = n, a, b
+            elif a > n > b:
+                b, c = n, b
+            elif b > n > c:
+                c = n
+        return c if c is not None else a
 
+# V1'
+# https://blog.csdn.net/fuxuemingzhu/article/details/79255652
+# IDEA : REPLACE MAX 
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def setMax(nums):
+            _max = max(nums)
+            for i, num in enumerate(nums):
+                if num == _max:
+                    nums[i] = float('-inf')
+            return _max
+        max1 = setMax(nums)
+        max2 = setMax(nums)
+        max3 = setMax(nums)
+        return max3 if max3 != float('-inf') else max(max1, max2)
 
-
+# V1'' 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79255652
+# IDEA : SET 
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_set = set(nums)
+        if len(nums_set) < 3:
+            return max(nums_set)
+        nums_set.remove(max(nums_set))
+        nums_set.remove(max(nums_set))
+        _max = max(nums_set)
+        return _max
+        
 # V2 
+# Time:  O(n)
+# Space: O(1)
 class Solution(object):
     def thirdMax(self, nums):
         """
@@ -61,7 +104,4 @@ class Solution(object):
 
         if count < 3:
             return top[0]
-
         return top[2]
-
-
