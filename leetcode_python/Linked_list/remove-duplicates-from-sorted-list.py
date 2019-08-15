df1968f9d92e3c770fac3fce8193847c95bd5da5
yennanliu <f339339@gmail.com>
2019-08-15 09:23:10 +0800

    update status/code Linked_list/remove-duplicates-from-sorted-list.py

--- leetcode_python/Linked_list/remove-duplicates-from-sorted-list.py
+++ leetcode_python/Linked_list/remove-duplicates-from-sorted-list.py
@@ -1,14 +1,59 @@
-# V1 : dev 
+# V0
 
+# V1 
+# https://blog.csdn.net/coder_orz/article/details/51506143
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        p = head
+        while p:
+            if p.next and p.next.val == p.val:
+                p.next = p.next.next
+            else:
+                p = p.next
+        return head
 
+# V1' 
+# https://blog.csdn.net/coder_orz/article/details/51506143
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        p = q = head
+        while p:
+            while q and q.val == p.val:
+                q = q.next
+            p.next = q
+            p = q
+        return head
 
-
-
+# V1'' 
+# https://blog.csdn.net/coder_orz/article/details/51506143
+# IDEA : ITERATION 
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        head.next = self.deleteDuplicates(head.next)
+        return head if head.val != head.next.val else head.next
 
 # V2 
 # Time:  O(n)
 # Space: O(1)
-
 class ListNode(object):
     def __init__(self, x):
         self.val = x
