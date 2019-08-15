5a821c991ed9b1e61b4c81bc8ac82bb187020532
yennanliu <f339339@gmail.com>
2019-08-15 09:58:10 +0800

    update status/code /Linked_list/remove-duplicates-from-sorted-list-ii.py

--- leetcode_python/Linked_list/remove-duplicates-from-sorted-list-ii.py
+++ leetcode_python/Linked_list/remove-duplicates-from-sorted-list-ii.py
@@ -1,41 +1,63 @@
+# Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
 
-# V1 : DEV 
+# Example 1:
 
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+# Input: 1->2->3->3->4->4->5
+# Output: 1->2->5
 
-#     def __repr__(self):
-#         if self is None:
-#             return "Nil"
-#         else:
-#             return "{} -> {}".format(self.val, repr(self.next))
+# Example 2:
 
-# class Solution(object):
+# Input: 1->1->1->2->3
+# Output: 2->3
+
+# V0 : TO DOUBLE CHECK 
+# class Solution:
 #     def deleteDuplicates(self, head):
 #         """
 #         :type head: ListNode
 #         :rtype: ListNode
 #         """
-# 		if not head.next:
-# 			return head 
-# 		dummy = ListNode(0)
-# 		pre, cur = dummy, head
-# 		while  cur.next and ( cur.val = cur.next.val):
-# 			cur = cur.next
-# 			pre.next = cur 
-# 		if head.next:
-# 			cur = cur.next
-# 			pre = cur 
-# 			cur = cur.next # double check 
-# 		return dummy.next
+#         root = ListNode(0)
+#         root.next = head
+#         head = root
+#         while head and head.next:
+#             if head.next.val != head.next.next.val:
+#                 head.next = head.next.next
+#             else:
+#                 head = head.next
+#         return root.next
 
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/80786545
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        root = ListNode(0)
+        root.next = head
+        val_list = []
+        while head:
+            val_list.append(head.val)
+            head = head.next
+        counter = collections.Counter(val_list)
+        head = root
+        while head and head.next:
+            if counter[head.next.val] != 1:
+                head.next = head.next.next
+            else:
+                head = head.next
+        return root.next
 
 # V2 
 # Time:  O(n)
 # Space: O(1)
-
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -66,5 +88,3 @@ class Solution(object):
                 pre = cur
                 cur = cur.__next__
         return dummy.__next__
-
-
