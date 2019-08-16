f65a5cb01263f7de49f668d89bf118fbfdaa1cc0
yennanliu <f339339@gmail.com>
2019-08-16 08:33:50 +0800

    update status/code /Linked_list/reverse-linked-list.py (dev)

--- leetcode_python/Linked_list/reverse-linked-list.py
+++ leetcode_python/Linked_list/reverse-linked-list.py
@@ -1,14 +1,64 @@
+# V0 
 
+# V1 
+# https://blog.csdn.net/coder_orz/article/details/51306170
+# IDEA : STACK 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        p = head
+        newList = []
+        while p:
+            newList.insert(0, p.val)
+            p = p.next
 
+        p = head
+        for v in newList:
+            p.val = v
+            p = p.next
+        return head
 
-# V1 : dev 
+# V1'
+# http://bookshadow.com/weblog/2015/05/05/leetcode-reverse-linked-list/
+# IDEA :  LINKED LIST 
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        dummy = ListNode(0)
+        while head:
+            next = head.next
+            head.next = dummy.next
+            dummy.next = head
+            head = next
+        return dummy.next
 
-
+# V1'
+# http://bookshadow.com/weblog/2015/05/05/leetcode-reverse-linked-list/
+# IDEA :  ITERATION  
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        return self.doReverse(head, None)
+    def doReverse(self, head, newHead):
+        if head is None:
+            return newHead
+        next = head.next
+        head.next = newHead
+        return self.doReverse(next, head)
 
 # V2 
 # Time:  O(n)
 # Space: O(1)
-
 class ListNode(object):
     def __init__(self, x):
         self.val = x
