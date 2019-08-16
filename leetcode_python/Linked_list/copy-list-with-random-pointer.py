3b0f3afcfaf0a06007b3516dae1644b157908791
yennanliu <f339339@gmail.com>
2019-08-16 09:38:37 +0800

    update status/code Linked_list/copy-list-with-random-pointer.py (dev)

--- leetcode_python/Linked_list/copy-list-with-random-pointer.py
+++ leetcode_python/Linked_list/copy-list-with-random-pointer.py
@@ -1,10 +1,60 @@
-# V1 : DEV 
+# V0
 
+# V1 
+# http://bookshadow.com/weblog/2015/07/31/leetcode-copy-list-random-pointer/
+class Solution:
+    # @param head, a RandomListNode
+    # @return a RandomListNode
+    def copyRandomList(self, head):
+        nodeDict = dict()
+        dummy = RandomListNode(0)
+        pointer, newHead = head, dummy
+        while pointer:
+            newNode = RandomListNode(pointer.label)
+            nodeDict[pointer] = newHead.next = newNode
+            newHead, pointer = newHead.next, pointer.next
+        pointer, newHead = head, dummy.next
+        while pointer:
+            if pointer.random:
+                newHead.random = nodeDict[pointer.random]
+            pointer, newHead = pointer.next, newHead.next
+        return dummy.next
 
+# V2
+# https://blog.csdn.net/fuxuemingzhu/article/details/80787528
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+"""
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        nodeDict = dict()
+        dummy = Node(0, None, None)
+        nodeDict[head] = dummy
+        newHead, pointer = dummy, head
+        while pointer:
+            node = Node(pointer.val, pointer.next, None)
+            nodeDict[pointer] = node
+            newHead.next = node
+            newHead, pointer = newHead.next, pointer.next
+        pointer = head
+        while pointer:
+            if pointer.random:
+                nodeDict[pointer].random = nodeDict[pointer.random]
+            pointer = pointer.next
+        return dummy.next
+        
 # V2 
 # Time:  O(n)
 # Space: O(1)
-
 class RandomListNode(object):
     def __init__(self, x):
         self.label = x
