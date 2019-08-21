9e64ec43273f2e085a640a353a5ad4635dad4f14
yennanliu <f339339@gmail.com>
2019-08-21 09:06:50 +0800

    update status/code Two_Pointers/two-sum-ii-input-array-is-sorted.py

--- leetcode_python/Two_Pointers/two-sum-ii-input-array-is-sorted.py
+++ leetcode_python/Two_Pointers/two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,43 @@
+# V0
 
-# V1  : need to improve ***
-
+# V1 
+# https://blog.csdn.net/coder_orz/article/details/52388066
+# IDEA : TWO POINTER
 class Solution(object):
-	def twoSum(self, nums, target):
-		for i in range(len(nums)):
-			for j in range( i+1 ,len(nums)):
-				if nums[i] + nums[j] == target:
-					return [i+1, j+1]
-			else:
-				pass 
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            else:
+                left += 1
+                
+# V1' 
+# https://blog.csdn.net/coder_orz/article/details/52388066
+# IDEA : DICT
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_dict = {}
+        for i, num in enumerate(numbers):
+            if (target - num) in num_dict:
+                return [num_dict[target - num], i + 1]
+            num_dict[num] = i + 1
 
 # V2 
 # Time:  O(n)
 # Space: O(1)
-
 class Solution(object):
     def twoSum(self, nums, target):
         start, end = 0, len(nums) - 1
