0d7a8819238bec8fda4dc8d067734e9c538c3138
yennanliu <f339339@gmail.com>
2019-08-21 09:48:58 +0800

    update status/code Two_Pointers/binary-subarrays-with-sum.py (dev)

--- leetcode_python/Two_Pointers/binary-subarrays-with-sum.py
+++ leetcode_python/Two_Pointers/binary-subarrays-with-sum.py
@@ -1,38 +1,73 @@
+# V0
 
+# V1
+# https://www.jiuzhang.com/solution/binary-subarrays-with-sum/#tag-highlight-lang-python
+import collections
+class Solution:
+    """
+    @param A: an array
+    @param S: the sum
+    @return: the number of non-empty subarrays
+    """
+    def numSubarraysWithSum(self, A, S):
+        # Write your code here.
+        c = collections.Counter({0: 1})
+        psum = res = 0
+        for i in A:
+            psum += i
+            res += c[psum - S]
+            c[psum] += 1
+        return res
 
-# V1 : DEV 
+# V1'
+# https://blog.csdn.net/fuxuemingzhu/article/details/83478995
+# IDEA : DICT 
+import collections
+class Solution(object):
+    def numSubarraysWithSum(self, A, S):
+        """
+        :type A: List[int]
+        :type S: int
+        :rtype: int
+        """
+        N = len(A)
+        res = 0
+        preS = 0
+        count = collections.Counter({0 : 1})
+        for i in range(1, N + 1):
+            preS += A[i - 1]
+            res += count[preS - S]
+            count[preS] += 1
+        return res
 
-# class Solution(object):
-#     def numSubarraysWithSum(self, A, S):
-#         """
-#         :type A: List[int]
-#         :type S: int
-#         :rtype: int
-#         """
-#         result = 0
-#         count = 0 
-#         for i in range(len(A)):
-#             print (i)
-#             for j in range(i+1,len(A)+1):
-#                 print ('j :', j ,  'A[i:j] :', A[i:j])
-#                 if sum(A[i:j]) == S:
-#                     count = count + 1 
-#                 if sum(A[i:j]) > S:
-#                     break
-#                 else:
-#                     pass
-#         return count 
-
-
-# V2 :  dev 
-# https://blog.csdn.net/u013383813/article/details/83476371
-
-
-# V3
-
+# V1'' 
+# https://blog.csdn.net/fuxuemingzhu/article/details/83478995
+# IDEA : GREEDY 
+class Solution(object):
+    def numSubarraysWithSum(self, A, S):
+        """
+        :type A: List[int]
+        :type S: int
+        :rtype: int
+        """
+        N = len(A)
+        tsum = [0] * (N + 1)
+        for i in range(1, N + 1):
+            tsum[i] = tsum[i - 1] + A[i - 1]
+        res = 0
+        for i in range(1, N + 1):
+            remain = tsum[i] - S
+            if remain < 0:
+                continue
+            left = bisect.bisect_left(tsum, remain)
+            right = bisect.bisect_right(tsum, remain)
+            right = min(i, right)
+            res += right - left
+        return res
+        
+# V2
 # Time:  O(n)
 # Space: O(1)
-
 # Two pointers solution
 class Solution(object):
     def numSubarraysWithSum(self, A, S):
