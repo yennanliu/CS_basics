a4b850cbb0696a625df4d9e225e3492f23e1fab3
yennanliu <f339339@gmail.com>
2019-08-22 10:18:36 +0800

    update status/code Two_Pointers/most-profit-assigning-work.py

--- leetcode_python/Two_Pointers/most-profit-assigning-work.py
+++ leetcode_python/Two_Pointers/most-profit-assigning-work.py
@@ -1,44 +1,69 @@
+"""
+LeetCode 826. Most Profit Assigning Work
 
+We have jobs: difficulty[i] is the difficulty of the ith job, and profit[i] is the profit of the ith job. 
 
-# V1 : dev 
+Now we have some workers. worker[i] is the ability of the ith worker, which means that this worker can only complete a job with difficulty at most worker[i]. 
 
-# class Solution(object):
-#     def maxProfitAssignment(self, difficulty, profit, worker):
-#         """
-#         :type difficulty: List[int]
-#         :type profit: List[int]
-#         :type worker: List[int]
-#         :rtype: int
-#         """
-#         jobs = []
-#         output = 0
-#         jobs.sort()
-#         worker.sort()
-#         for w_i, w_j in enumerate(worker):
-#             for d_i, d_j in enumerate(difficulty):
-#                 print (w_j, d_j)
-#                 if w_j < d_j and  w_j < difficulty[d_i+1] and  w_j < difficulty[d_i-1] :
-#                     jobs.append(0)
-#                     break 
-#                 elif w_j < d_j and  ( w_j == difficulty[d_i+1] or  w_j >= difficulty[d_i -1] ): 
-#                     jobs.append(difficulty[d_i -1])
-#                     break
-#                 else:
-#                     pass
-#         j_p = dict(zip(difficulty,profit))
-#         j_p[0] = 0
-#         for job in jobs:
-#             output += j_p[job]
-#         return output 
-        
-          
-         
+Every worker can be assigned at most one job, but one job can be completed multiple times.
+
+For example, if 3 people attempt the same job that pays $1, then the total profit will be $3.  If a worker cannot complete any job, his profit is $0.
+
+What is the most profit we can make?
+
+Example 1:
+
+Input: difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7]
+Output: 100 
+Explanation: Workers are assigned jobs of difficulty [4,4,6,6] and they get profit of [20,20,30,30] seperately.
+Notes:
+
+1 <= difficulty.length = profit.length <= 10000
+1 <= worker.length <= 10000
+difficulty[i], profit[i], worker[i]  are in range [1, 10^5]
+"""
+
+# V0
+
+# V1 
+# https://www.jiuzhang.com/solution/most-profit-assigning-work/#tag-highlight-lang-python
+# IDEA : GREEDY 
+class Solution:
+    def maxProfitAssignment(self, difficulty, profit, worker):
+        jobs = sorted([a, b] for a, b in zip(difficulty, profit))
+        res = i = maxp = 0
+        for ability in sorted(worker):
+            while i < len(jobs) and ability >= jobs[i][0]:
+                maxp = max(jobs[i][1], maxp)
+                i += 1
+            res += maxp
+        return res   
+
+# V1'
+# http://bookshadow.com/weblog/2018/04/29/leetcode-most-profit-assigning-work/
+# IDEA : GREEDY
+import collections
+class Solution(object):
+    def maxProfitAssignment(self, difficulty, profit, worker):
+        """
+        :type difficulty: List[int]
+        :type profit: List[int]
+        :type worker: List[int]
+        :rtype: int
+        """
+        diffPro = collections.defaultdict(int)
+        for diff, pro in zip(difficulty, profit):
+            diffPro[diff] = max(diffPro[diff], pro)
+        maxVal = 0
+        for x in range(min(difficulty + worker), max(difficulty + worker) + 1):
+            diffPro[x] = max(diffPro[x], maxVal)
+            maxVal = max(diffPro[x], maxVal)
+        return sum(diffPro[w] for w in worker)
 
 # V2 
 # Time:  O(mlogm + nlogn), m is the number of workers,
 #                        , n is the number of jobs
 # Space: O(n)
-
 class Solution(object):
     def maxProfitAssignment(self, difficulty, profit, worker):
         """
