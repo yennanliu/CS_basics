11c79e3d62cdf36cdc1adc20bc89adac18537487
yennanliu <f339339@gmail.com>
2019-08-23 10:54:10 +0800

    update status/code Math/smallest-range-i.py

--- leetcode_python/Math/smallest-range-i.py
+++ leetcode_python/Math/smallest-range-i.py
@@ -1,6 +1,6 @@
-# V1 : DEV 
+# V0 
 
-# V2 
+# V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/82824685
 class Solution(object):
     def smallestRangeI(self, A, K):
@@ -10,11 +10,25 @@ class Solution(object):
         :rtype: int
         """
         return max(max(A) - min(A) - 2 * K, 0)
+        
+# V1'
+# https://www.cnblogs.com/seyjs/p/9692725.html
+class Solution(object):
+    def smallestRangeI(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        minv,maxv = A[0] + K, A[0] - K
+        for i in A:
+            minv = min(minv,i+K)
+            maxv = max(maxv,i-K)
+        return max(0,maxv-minv)
 
-# V3 
+# V2 
 # Time:  O(n)
 # Space: O(1)
-
 class Solution(object):
     def smallestRangeI(self, A, K):
         """
