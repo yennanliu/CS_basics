069efae892e31188d50deff118e1a49d2562449a
yennanliu <f339339@gmail.com>
2019-08-24 14:09:49 +0800

    update status/code Math/factorial-trailing-zeroes.py

--- leetcode_python/Math/factorial-trailing-zeroes.py
+++ leetcode_python/Math/factorial-trailing-zeroes.py
@@ -1,37 +1,33 @@
+# V0
 
-# V1  : dev 
-# TODO : fix time out error 
-# class Solution(object):
-# 	# @return an integer
-# 	def trailingZeroes(self, n):
-# 		result = 1 
-# 		for i in range(1,n+1):
-# 			result = result*i 
-# 		count = 0
-# 		for i in str(result)[::-1]:
-# 			if i == '0':
-# 				count += 1 
-# 			else:
-# 				break
-# 		return  count 
+# V1 
+# https://blog.csdn.net/coder_orz/article/details/51590478
+# IDEA : Iteration
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        while n > 0:
+            n = n/5
+            res += n
+        return res
 
-# V2  : dev 
-# class Solution(object):
-# 	# @return an integer
-# 	def trailingZeroes(self, n):
-# 		count = 0 
-# 		for i in range(1,n+1):
-# 			if (i%5==0): 
-# 				while i >0:
-# 					i = int(i/5) 
-# 					count = count+1  
-# 		return 0 if count - 1  < 0 else count - 1 
+# V1' 
+# https://blog.csdn.net/coder_orz/article/details/51590478
+# IDEA : Recursion
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return 0 if n == 0 else n / 5 + self.trailingZeroes(n / 5)
 
-
-
-# V3 
+# V2 
 """
-
 * only have to consider 5, but not 2, since the amount of 2 is always enough 
 for example :
 
@@ -57,7 +53,7 @@ class Solution(object):
             res += n
         return res
 
-# V4 
+# V3 
 # Time:  O(logn) = O(1)
 # Space: O(1)
 class Solution(object):
