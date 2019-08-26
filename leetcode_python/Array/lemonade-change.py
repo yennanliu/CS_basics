587a925d5a7f3352f8c8430c518269394ad7e1df
yennanliu <f339339@gmail.com>
2019-08-26 15:27:26 +0800

    add Array/lemonade-change.py (dev)

---
+++ leetcode_python/Array/lemonade-change.py
@@ -0,0 +1,108 @@
+# V0 
+
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/80913955
+# IDEA : GREEDY 
+class Solution:
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        changes = {5:0, 10:0}
+        for bill in bills:
+            if bill == 5:
+                changes[5] += 1
+            elif bill == 10:
+                if changes[5] == 0:
+                    return False
+                else:
+                    changes[10] += 1
+                    changes[5] -= 1
+            elif bill == 20:
+                if changes[10] != 0:
+                    if changes[5] == 0:
+                        return False
+                    else:
+                        changes[5] -= 1
+                        changes[10] -= 1
+                else:
+                    if changes[5] < 3:
+                        return False
+                    else:
+                        changes[5] -= 3
+        return True
+
+# V1'
+# https://leetcode-cn.com/problems/lemonade-change/solution/lemonade-change-tan-xin-suan-fa-by-jyd/
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five, ten = 0, 0
+        for b in bills:
+            if b == 5:
+                five += 1
+            elif b == 10:
+                if not five: return False
+                five -= 1
+                ten += 1
+            else:
+                if ten and five:
+                    ten -= 1
+                    five -= 1
+                elif five > 2:
+                    five -= 3
+                else:
+                    return False
+        return True
+        
+# V2 
+# Time:  O(n)
+# Space: O(1)
+import collections
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        coins = [20, 10, 5]
+        counts = collections.defaultdict(int)
+        for bill in bills:
+            counts[bill] += 1
+            change = bill - coins[-1]
+            for coin in coins:
+                if change == 0:
+                    break
+                if change >= coin:
+                    count = min(counts[coin], change//coin)
+                    counts[coin] -= count
+                    change -= coin * count
+            if change != 0:
+                return False
+        return True
+
+
+class Solution2(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        five, ten = 0, 0
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if not five:
+                    return False
+                five -= 1
+                ten += 1
+            else:
+                if ten and five:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
