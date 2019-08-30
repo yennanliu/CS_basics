18c0011955de75e9a0e7c562759b925ea75de6ab
yennanliu <f339339@gmail.com>
2019-08-30 09:09:40 +0800

    update status/code Sort/wiggle-sort-ii.py

--- leetcode_python/Sort/wiggle-sort-ii.py
+++ leetcode_python/Sort/wiggle-sort-ii.py
@@ -1,32 +1,106 @@
+# V0 
 
+# V1 
+# https://www.hrwhisper.me/leetcode-wiggle-sort-ii/
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        temp = sorted(nums)
+        s, t = (len(nums) + 1) >> 1, len(nums)
+        for i in range(len(nums)):
+            if i & 1 == 0:
+                s -= 1
+                nums[i] = temp[s]
+            else:
+                t -= 1
+                nums[i] = temp[t]
 
+# V1'
+# http://bookshadow.com/weblog/2015/12/31/leetcode-wiggle-sort-ii/
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        size = len(nums)
+        snums = sorted(nums)
+        for x in range(1, size, 2) + range(0, size, 2):
+            nums[x] = snums.pop()
 
-# V1 : DEV 
+# V1''
+# https://www.jiuzhang.com/solution/wiggle-sort-ii/#tag-highlight-lang-python
+class Solution:
+    """
+    @param: nums: A list of integers
+    @return: nothing
+    """
+    def wiggleSort(self, nums):
+        if not nums:
+            return
+        
+        # partition nums into smaller half and bigger half
+        # all nums in smaller half <= any num in bigger half
+        median = self.find_median(nums)
+        
+        n = len(nums)
 
+        # reorder the nums from
+        # 0 => n-1(odd), (n-2)(even)
+        # 1 => n-3
+        # 2 => n-5
+        # ...
+        # (n - 1) / 2 => 0
+        # (n - 1) / 2 + 1 => n - 2(odd), n - 1(even)
+        # (n - 1) / 2 + 2 => n - 4(odd), n - 3(even)
+        # ... 
+        def get_index(i):
+            if i <= (n - 1) // 2:
+                return n - i * 2 - 1 - (n + 1) % 2
+            i -= (n - 1) // 2 + 1
+            return n - i * 2 - 1 - n % 2
+            
+        # 3-way partition
+        left, i, right = 0, 0, n - 1
+        while i <= right:
+            if nums[get_index(i)] < median:
+                nums[get_index(left)], nums[get_index(i)] = nums[get_index(i)], nums[get_index(left)]
+                i += 1
+                left += 1
+            elif nums[get_index(i)] == median:
+                i += 1
+            else:
+                nums[get_index(right)], nums[get_index(i)] = nums[get_index(i)], nums[get_index(right)]
+                right -= 1
+        
+    def find_median(self, nums):
+        return self.find_kth(nums, 0, len(nums) - 1, (len(nums) - 1) // 2)
+    
+    def find_kth(self, nums, start, end, kth):
+        # k is zero based
+        left, right = start, end
+        mid = nums[(left + right) // 2]
+        
+        while left <= right:
+            while left <= right and nums[left] < mid:
+                left += 1
+            while left <= right and nums[right] > mid:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left, right = left + 1, right - 1
+                
+        if kth <= right:
+            return self.find_kth(nums, start, right, kth)
+        elif kth >= left:
+            return self.find_kth(nums, left, end, kth)
+        else:
+            return nums[kth]
 
-
-# V2 : DEV 
-
-
-
-# V3  : dev 
-# Time:  O(nlogn)
-# Space: O(n)
-
-# class Solution(object):
-#     def wiggleSort(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: void Do not return anything, modify nums in-place instead.
-#         """
-#         import math 
-
-#         nums.sort()
-#         med = math.ceil((len(nums) - 1) / 2)
-#         nums[::2], nums[1::2] = nums[med::-1], nums[:med:-1]
-
-
-# V4 
+# V2 
 # Time:  O(n) ~ O(n^2)
 # Space: O(1)
 # Tri Partition (aka Dutch National Flag Problem) with virtual index solution. (TLE)
