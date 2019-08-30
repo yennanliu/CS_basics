23f3810864dc43d6f834ac973b5f5bc1bd9ef8f7
yennanliu <f339339@gmail.com>
2019-08-30 12:14:04 +0800

    update status/code /Sort/meeting-rooms-ii.py (dev)

--- leetcode_python/Sort/meeting-rooms-ii.py
+++ leetcode_python/Sort/meeting-rooms-ii.py
@@ -1,20 +1,81 @@
-
-
-
 # Given an array of meeting time intervals consisting 
 # of start and end times [[s1,e1],[s2,e2],...] (si < ei),
 # find the minimum number of conference rooms required.
-
 # For example,
 # Given [[0, 30],[5, 10],[15, 20]],
 # return 2.
 
+# V0 
 
 
 # V1 
+# https://www.jiuzhang.com/solution/meeting-rooms-ii/#tag-highlight-lang-python
+# IDEA  : TO HAVE A ARRAY OF ALL "ROOM OPEN" AND "ROOM CLOSE" EVENTS
+# "ROOM OPEN" EVENT : (TIME, 1)
+# "ROOM CLOSE" EVENT : (TIME, -1)
+# SO AFTER RE-ORDER ON THE TIME, WE WILL HAVE AN ORDERED EVENT ARRAY LIKE BELOW
+# [ [t1, 1], [t1,1], [t3, -1], [t4, 1], [t5, -1]]
+# THEN WE CAN GO THROUGH ALL EVENT IN THE EVENTS ARRAY AND CALCULATE # OF ROOM NEEDED
+# DEMO 1
+# In [23]: intervals= [[0, 30],[5, 10],[15, 20]]
+# In [24]: Solution().minMeetingRooms(intervals)
+# Out[24]: 2
+# DEMO 2 
+# In [40]: intervals
+# Out[40]: [[0, 30], [5, 10], [15, 20]]
 
+# In [41]: sorted([(0, 1), (30, -1), (5, 1), (10, -1), (15, 1), (20, -1)])
+# Out[41]: [(0, 1), (5, 1), (10, -1), (15, 1), (20, -1), (30, -1)]
 
+# In [42]: Solution().minMeetingRooms(intervals)
+# []
+# [(0, 1), (30, -1)]
+# [(0, 1), (30, -1), (5, 1), (10, -1)]
+# [(0, 1), (30, -1), (5, 1), (10, -1), (15, 1), (20, -1)]
+# Out[42]: 2
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def minMeetingRooms(self, intervals):
+        points = []
+        for interval in intervals:
+            print (points)
+            points.append((interval[0], 1))
+            points.append((interval[1], -1))
+        print (points)    
+        meeting_rooms = 0
+        ongoing_meetings = 0
+        for _, delta in sorted(points):
+            ongoing_meetings += delta
+            meeting_rooms = max(meeting_rooms, ongoing_meetings)          
+        return meeting_rooms
 
+# V1'
+# https://blog.csdn.net/yurenguowang/article/details/76665171
+class Solution:
+    def minMeetingRooms(self, intervals):
+        if intervals is None or len(intervals) == 0:
+            return 0
+        tmp = []
+        # label start and end point 
+        for inter in intervals:
+            tmp.append((inter.start, True))
+            tmp.append((inter.end, False))
+        # order the array with time 
+        tmp = sorted(tmp, key=lambda v: (v[0], v[1]))
+        n = 0
+        max_num = 0
+        for arr in tmp:
+            # start point : +1 
+            if arr[1]:
+                n += 1
+            # end point : -1 
+            else:
+                n -= 1
+            max_num = max(n, max_num)
+        return max_num
 
 # V2 
 # https://blog.csdn.net/yurenguowang/article/details/76665171
@@ -50,11 +111,9 @@ class Solution:
             max_num = max(n, max_num)
         return max_num
 
-
 # V3
 # Time:  O(nlogn)
 # Space: O(n)
-
 class Solution(object):
     # @param {Interval[]} intervals
     # @return {integer}
@@ -81,7 +140,6 @@ class Solution(object):
 
         return min_rooms
 
-
 # time: O(nlogn)
 # space: O(n)
 from heapq import heappush, heappop
@@ -106,4 +164,3 @@ class Solution2(object):
             heappush(free_rooms, interval.end)
         
         return len(free_rooms)
-
