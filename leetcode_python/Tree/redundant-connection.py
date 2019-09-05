442df618e2c0e1258f016dcc7774cff78a651dfd
yennanliu <f339339@gmail.com>
2019-09-05 15:22:14 +0800

    update status/code Tree/redundant-connection.py (dev)

--- leetcode_python/Tree/redundant-connection.py
+++ leetcode_python/Tree/redundant-connection.py
@@ -25,6 +25,47 @@ class Solution:
             tree[x] = root
             return root
 
+# V1'
+# https://www.jiuzhang.com/solution/redundant-connection/#tag-highlight-lang-python
+class Solution:
+
+    def findRedundantConnection(self, edges):
+        if not edges: return None
+        uf = UnionFind(len(edges))
+        
+        for first, second in edges:
+            # check if 2 trees have same "father", if not, join them 
+            if not uf.query(first,second):
+                uf.connect(first,second)
+            # keep the process till the same element shown ; or should be a loop 
+            else:
+                return (first, second)
+        return None
+
+class UnionFind(object):
+    def __init__(self,n):
+        self.father = {}
+        for i in range(1, n+1):
+            self.father[i] = i
+            
+    def find(self, node):
+        path = []
+        while self.father[node]!= node:
+            node = self.father[node]
+            path.append(node)
+        for n in path:
+            self.father[n] = node
+        return node
+    
+    def query(self, a, b):
+        return self.find(a) == self.find(b)
+    def connect(self, a, b):
+        self.father[self.find(a)] = self.find(b)
+# if __name__== "__main__":
+#     t = Solution()
+#     x= t.findRedundantConnection([[1,2], [2,3], [3,4], [1,4], [1,5]])
+#     print(x)
+
 # V2 
 # Time:  O(nlog*n) ~= O(n), n is the length of the positions
 # Space: O(n)
