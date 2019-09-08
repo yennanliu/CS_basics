70512cdfb7a64f41dfff66082a236456978e7182
yennanliu <f339339@gmail.com>
2019-09-08 21:21:56 +0800

    update status/code Tree/average-of-levels-in-binary-tree.py

--- leetcode_python/Tree/average-of-levels-in-binary-tree.py
+++ leetcode_python/Tree/average-of-levels-in-binary-tree.py
@@ -1,4 +1,22 @@
 # V0 
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[float]
+        """
+        res = []
+        self.getLevel(root, 0, res)
+        return [sum(line) / float(len(line)) for line in res]
+    
+    def getLevel(self, root, level, res):
+        if not root:
+            return
+        if level >= len(res):
+            res.append([])
+        res[level].append(root.val)
+        self.getLevel(root.left, level + 1, res)
+        self.getLevel(root.right, level + 1, res)
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79088554
@@ -66,7 +84,6 @@ class Solution(object):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution(object):
     def averageOfLevels(self, root):
         """
