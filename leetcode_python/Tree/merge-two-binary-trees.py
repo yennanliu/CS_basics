18aff468d6f7e885f4d9b7008d1ef4d47e0ce971
yennanliu <f339339@gmail.com>
2019-09-08 22:33:08 +0800

    update status/code Tree/merge-two-binary-trees.py

--- leetcode_python/Tree/merge-two-binary-trees.py
+++ leetcode_python/Tree/merge-two-binary-trees.py
@@ -15,7 +15,26 @@ class Solution:
             t1.val += t2.val
             return t1
         return t1 if t2 is None else t2
-        
+
+# V1'
+# https://www.jiuzhang.com/solution/merge-two-binary-trees/#tag-highlight-lang-python
+class Solution:
+    """
+    @param t1: the root of the first tree
+    @param t2: the root of the second tree
+    @return: the new binary tree after merge
+    """
+    def mergeTrees(self, t1, t2):
+        # Write your code here
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+        t3 = TreeNode(t1.val + t2.val)
+        t3.left = self.mergeTrees(t1.left, t2.left)
+        t3.right = self.mergeTrees(t1.right, t2.right)
+        return t3
+
 # V2 
 # Time:  O(n)
 # Space: O(h)
