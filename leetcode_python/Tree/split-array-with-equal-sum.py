720513dd2652ed07de87d18483b3560d3dc947f9
yennanliu <f339339@gmail.com>
2019-09-09 11:11:18 +0800

    update status/code Tree/split-array-with-equal-sum.py

--- leetcode_python/Tree/split-array-with-equal-sum.py
+++ leetcode_python/Tree/split-array-with-equal-sum.py
@@ -1,6 +1,38 @@
 # V0
 
-# V1 
+# V1
+# https://www.jiuzhang.com/solution/split-array-with-equal-sum/#tag-highlight-lang-python
+# IDEA : GREEDY 
+# PROCESS 
+# 1) INIT A NULL ARRAY 
+# 2) SET INDEX j to split array into left and right sub array 
+# 3) CHECK IF THERE EXIST INDEX i IN LEFT ARRAY THAT CAN SPLIT 2 SUB ARRAY WITH SAME SUM
+# 4) CHECK IF THERE EXIST INDEX k IN RIGHT ARRAY THAT CAN SPLIT 2 SUB ARRAY WITH SAME SUM
+# 5) RETURN TRUE IF ANY CASE HAPPEN ABOVE
+# 6) RETURN FALSE IF NO ABOVE CASE 
+class Solution:
+    """
+    @param nums: a list of integer
+    @return: return a boolean
+    """
+    def splitArray(self, nums):
+        n = len(nums)
+        sum = [0] * n
+        sum[0] = nums[0]
+        for i in range(1, n):
+            sum[i] = sum[i - 1] + nums[i]                # cache the current sum, use sum(n) to caculate sum(n+1)
+        for j in range(3, n - 3):                        # j start from 3 
+            S = set()
+            for i in range(1, j - 1):
+                if(sum[i - 1] == sum[j - 1] - sum[i]):   # use sum cache array to check if sum[i - 1] == sum[j - 1] - sum[i]
+                    S.add(sum[i - 1])
+            for k in range(j + 2, n - 1):                # k start from j+2
+                x, y = sum[k - 1] - sum[j], sum[n - 1] - sum[k]
+                if(x == y and x in S):
+                    return True
+        return False
+
+# V1' 
 # http://bookshadow.com/weblog/2017/04/03/leetcode-split-array-with-equal-sum/
 class Solution(object):
     def splitArray(self, nums):
