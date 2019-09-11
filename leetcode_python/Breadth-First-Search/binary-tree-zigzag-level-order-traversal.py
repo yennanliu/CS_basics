6504a7c659be5179fad73faf4f8dad6973a5ba29
yennanliu <f339339@gmail.com>
2019-09-11 16:35:45 +0800

    update status/code /Breadth-First-Search/binary-tree-zigzag-level-order-traversal.py

--- leetcode_python/Breadth-First-Search/binary-tree-zigzag-level-order-traversal.py
+++ leetcode_python/Breadth-First-Search/binary-tree-zigzag-level-order-traversal.py
@@ -1,11 +1,47 @@
-# V1 : dev 
+# V0 
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.level(root, 0, res)
+        return [j if i%2 == 0 else j[::-1] for i, j in enumerate(res)]
 
+    def level(self, root, level, res):
+        if not root: return
+        if len(res) == level: res.append([])
+        res[level].append(root.val)
+        if root.left: self.level(root.left, level + 1, res)
+        if root.right: self.level(root.right, level + 1, res)
 
+# V1 
+# https://www.cnblogs.com/zuoyuan/p/3722022.html
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def preorder(self, root, level, res):
+        if root:
+            if len(res) < level+1: res.append([])
+            if level % 2 == 0: res[level].append(root.val)
+            else: res[level].insert(0, root.val)
+            self.preorder(root.left, level+1, res)
+            self.preorder(root.right, level+1, res)
+    def zigzagLevelOrder(self, root):
+        res=[]
+        self.preorder(root, 0, res)
+        return res
 
 # V2 
 # Time:  O(n)
 # Space: O(n)
-
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
