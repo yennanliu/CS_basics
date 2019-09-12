17e901127d6dff30ade86357a2db48f8bd94e116
yennanliu <f339339@gmail.com>
2019-09-12 13:28:13 +0800

    update status/code /Breadth-First-Search/surrounded-regions.py (dev)

--- leetcode_python/Breadth-First-Search/surrounded-regions.py
+++ leetcode_python/Breadth-First-Search/surrounded-regions.py
@@ -34,6 +34,69 @@ class Solution(object):
                 elif board[r][c] == 'D':
                     board[r][c] = 'O'
 
+# V1'
+# https://www.jiuzhang.com/solution/surrounded-regions/#tag-highlight-lang-python
+class Solution:
+    # @param {list[list[str]]} board a 2D board containing 'X' and 'O'
+    # @return nothing 
+    def surroundedRegions(self, board):
+        # Write your code here
+        if not any(board):
+            return
+
+        n, m = len(board), len(board[0])
+        q = [ij for k in range(max(n,m)) for ij in ((0, k), (n-1, k), (k, 0), (k, m-1))]
+        while q:
+            i, j = q.pop()
+            if 0 <= i < n and 0 <= j < m and board[i][j] == 'O':
+                board[i][j] = 'W'
+                q += (i, j-1), (i, j+1), (i-1, j), (i+1, j)
+
+        board[:] = [['XO'[c == 'W'] for c in row] for row in board]
+
+# V1'' 
+# https://www.jiuzhang.com/solution/surrounded-regions/#tag-highlight-lang-python
+class Solution:
+    # @param {list[list[str]]} board a 2D board containing 'X' and 'O'
+    # @return nothing
+    def surroundedRegions(self, board):
+        def fill(x, y):
+            if x < 0 or x > m-1 or y < 0 or y > n-1 or board[x][y] != 'O':
+                return
+            queue.append((x, y))
+            board[x][y] = 'D'
+
+        def bfs(x, y):
+            if board[x][y] == 'O':
+                queue.append((x, y))
+                fill(x, y)
+
+            while queue:
+                curr = queue.pop(0)
+                i, j = curr[0], curr[1]
+                fill(i+1, j)
+                fill(i-1, j)
+                fill(i, j+1)
+                fill(i, j-1)
+
+        if len(board) == 0:
+            return
+        m, n, queue = len(board), len(board[0]), []
+        for i in range(n):
+            bfs(0, i)
+            bfs(m-1, i)
+
+        for j in range(1, m-1):
+            bfs(j, 0)
+            bfs(j, n-1)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'D':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
+                    
 # V2 
 # Time:  O(m * n)
 # Space: O(m + n)
