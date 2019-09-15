824901d5604b28b9e32f7d23cbc126d91ddca913
yennanliu <f339339@gmail.com>
2019-09-15 16:04:56 +0800

    update status/code Breadth-First-Search/course-schedule.py (dev)

--- leetcode_python/Breadth-First-Search/course-schedule.py
+++ leetcode_python/Breadth-First-Search/course-schedule.py
@@ -1,13 +1,98 @@
 # V0
 
 # V1 
+# https://www.jiuzhang.com/solution/course-schedule/#tag-highlight-lang-python
+from collections import deque
+class Solution:
+    # @param {int} numCourses a total of n courses
+    # @param {int[][]} prerequisites a list of prerequisite pairs
+    # @return {boolean} true if can finish all courses or false
+    def canFinish(self, numCourses, prerequisites):
+        # Write your code here
+        edges = {i: [] for i in range(numCourses)}
+        degrees = [0 for i in range(numCourses)] 
+        for i, j in prerequisites:
+            edges[j].append(i)
+            degrees[i] += 1
+            
+        queue, count = deque([]), 0
+        
+        for i in range(numCourses):
+            if degrees[i] == 0:
+                queue.append(i)
+
+        while queue:
+            node = queue.popleft()
+            count += 1
+
+            for x in edges[node]:
+                degrees[x] -= 1
+                if degrees[x] == 0:
+                    queue.append(x)
+        return count == numCourses
+
+# V1'
+# https://blog.csdn.net/fuxuemingzhu/article/details/82951771
+# IDEA : DFS + topological sort 
+class Solution(object):
+    def canFinish(self, N, prerequisites):
+        """
+        :type N,: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = collections.defaultdict(list)
+        for u, v in prerequisites:
+            graph[u].append(v)
+        # 0 = Unknown, 1 = visiting, 2 = visited
+        visited = [0] * N
+        for i in range(N):
+            if not self.dfs(graph, visited, i):
+                return False
+        return True
+        
+    # Can we add node i to visited successfully?
+    def dfs(self, graph, visited, i):
+        if visited[i] == 1: return False
+        if visited[i] == 2: return True
+        visited[i] = 1
+        for j in graph[i]:
+            if not self.dfs(graph, visited, j):
+                return False
+        visited[i] = 2
+        return True
+
+# V1''
+# https://blog.csdn.net/fuxuemingzhu/article/details/82951771
+# IDEA : BFS + topological sort
+class Solution(object):
+    def canFinish(self, N, prerequisites):
+        """
+        :type N,: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = collections.defaultdict(list)
+        indegrees = collections.defaultdict(int)
+        for u, v in prerequisites:
+            graph[v].append(u)
+            indegrees[u] += 1
+        for i in range(N):
+            zeroDegree = False
+            for j in range(N):
+                if indegrees[j] == 0:
+                    zeroDegree = True
+                    break
+            if not zeroDegree: return False
+            indegrees[j] = -1
+            for node in graph[j]:
+                indegrees[node] -= 1
+        return True           
 
 # V2 
 # Time:  O(|V| + |E|)
 # Space: O(|E|)
 from collections import defaultdict, deque
-
-
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
