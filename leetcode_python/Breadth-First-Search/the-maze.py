bf5efab92b0f6d000ee91c4026c5aa6b2a044612
yennanliu <f339339@gmail.com>
2019-09-20 16:30:38 +0800

    update status/code /Breadth-First-Search/the-maze.py

--- leetcode_python/Breadth-First-Search/the-maze.py
+++ leetcode_python/Breadth-First-Search/the-maze.py
@@ -32,9 +32,40 @@ class Solution(object):
         visited = set()
         return dfs(start[0], start[1])
 
-# V1' 
-# IDEA : BFS  
-
+# V1'
+# https://www.jiuzhang.com/solution/the-maze/#tag-highlight-lang-python
+# IDEA : BFS 
+class Solution:
+    """
+    @param maze: the maze
+    @param start: the start
+    @param destination: the destination
+    @return: whether the ball could stop at the destination
+    """
+    def hasPath(self, maze, start, destination):
+        # write your code here
+        Q = [start]
+        n = len(maze)
+        m = len(maze[0])
+        dirs = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        while Q:
+            i, j = Q.pop(0)
+            maze[i][j] = 2
+            if i == destination[0] and j == destination[1]:
+                return True
+            for x, y in dirs:
+                row = i + x
+                col = j + y
+                while 0 <= row < n and 0 <= col < m and maze[row][col] != 1:
+                    row += x
+                    col += y
+                row -= x
+                col -= y
+                if maze[row][col] == 0:
+                    Q.append([row, col])
+        
+        return False
+        
 # V2 
 # Time:  O(max(r, c) * w)
 # Space: O(w)
