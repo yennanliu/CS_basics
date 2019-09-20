957cdcf9dd2070c9d7f5d93e998ae22a79fbf314
yennanliu <f339339@gmail.com>
2019-09-20 15:46:31 +0800

    update status/code Breadth-First-Search/sequence-reconstruction.py

--- leetcode_python/Breadth-First-Search/sequence-reconstruction.py
+++ leetcode_python/Breadth-First-Search/sequence-reconstruction.py
@@ -60,7 +60,71 @@ class Solution(object):
             if (org[x - 1], org[x]) not in edges:
                 return False
         return True
+
+# V1''
+# https://www.jiuzhang.com/solution/sequence-reconstruction/#tag-highlight-lang-python
+class Solution:
+    """
+    @param org: a permutation of the integers from 1 to n
+    @param seqs: a list of sequences
+    @return: true if it can be reconstructed only one or false
+    """
+    def sequenceReconstruction(self, org, seqs):
+        graph = self.build_graph(seqs)
+        topo_order = self.topological_sort(graph)
+        return topo_order == org
+            
+    def build_graph(self, seqs):
+        # initialize graph
+        graph = {}
+        for seq in seqs:
+            for node in seq:
+                if node not in graph:
+                    graph[node] = set()
         
+        for seq in seqs:
+            for i in range(1, len(seq)):
+                graph[seq[i - 1]].add(seq[i])
+
+        return graph
+    
+    def get_indegrees(self, graph):
+        indegrees = {
+            node: 0
+            for node in graph
+        }
+        
+        for node in graph:
+            for neighbor in graph[node]:
+                indegrees[neighbor] += 1
+                
+        return indegrees
+        
+    def topological_sort(self, graph):
+        indegrees = self.get_indegrees(graph)
+        
+        queue = []
+        for node in graph:
+            if indegrees[node] == 0:
+                queue.append(node)
+        
+        topo_order = []
+        while queue:
+            if len(queue) > 1:
+                # there must exist more than one topo orders
+                return None
+                
+            node = queue.pop()
+            topo_order.append(node)
+            for neighbor in graph[node]:
+                indegrees[neighbor] -= 1
+                if indegrees[neighbor] == 0:
+                    queue.append(neighbor)
+                    
+        if len(topo_order) == len(graph):
+            return topo_order        
+        return None
+
 # V2 
 # Time:  O(n * s), n is the size of org, s is the size of seqs
 # Space: O(n)
