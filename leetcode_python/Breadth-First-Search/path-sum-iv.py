4983343590233209e4e19a09417cdc9bc91b3d67
yennanliu <f339339@gmail.com>
2019-09-20 20:51:47 +0800

    update status/code Breadth-First-Search/path-sum-iv.py (dev)

--- leetcode_python/Breadth-First-Search/path-sum-iv.py
+++ leetcode_python/Breadth-First-Search/path-sum-iv.py
@@ -18,6 +18,33 @@ class Solution(object):
             leaves.add(path)
             if parent in leaves: leaves.remove(parent)
         return sum(dmap[v] for v in leaves)
+        
+# V1'
+# https://www.jiuzhang.com/solution/path-sum-iv/#tag-highlight-lang-python
+class Solution:
+    """
+    @param nums: the list
+    @return: the sum of all paths from the root towards the leaves
+    """
+    def pathSumIV(self, nums):
+        # Write your code here.
+        if len(nums) == 1:    
+            return nums[0] % 10;        
+        a = [-1 for i in range(100)];
+        for i in nums:
+            a[i / 10] = i % 10;
+        ret = 0;
+        for i in range(2,5):
+            for j in range(1,9):
+                idx = i * 10 + j;
+                pre = (i - 1) * 10 + (j + 1)/2;
+                next1 = (i + 1) * 10 + 2 * j;
+                next2 = (i + 1) * 10 + 2 * j - 1;
+                if a[idx] != -1 and a[pre] != -1: 
+                    a[idx] = a[idx] + a[pre];               
+                if a[next1] == -1 and a[next2] == -1 and a[idx] != -1:
+                    ret = ret + a[idx];         
+        return ret;
 
 # V2 
 # Time:  O(n)
