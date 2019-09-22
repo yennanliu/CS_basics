b9534517d36fe629f0a40c68dd8b1c4dabcc0ffc
yennanliu <f339339@gmail.com>
2019-09-22 16:05:30 +0800

    update status/code Depth-First-Search/find-eventual-safe-states.py (dev)

--- leetcode_python/Depth-First-Search/find-eventual-safe-states.py
+++ leetcode_python/Depth-First-Search/find-eventual-safe-states.py
@@ -2,6 +2,7 @@
 
 # V1
 # http://bookshadow.com/weblog/2018/03/18/leetcode-find-eventual-safe-states/
+import collections
 class Solution(object):
     def eventualSafeNodes(self, graph):
         """
@@ -24,7 +25,33 @@ class Solution(object):
                     if not tgts[s]: ndegZeros.append(s)
             degZeros = ndegZeros
         return [k for k in range(len(graph)) if not tgts[k]] 
-        
+
+# V1'
+# https://www.jiuzhang.com/solution/find-eventual-safe-states/#tag-highlight-lang-python
+class Solution:
+    """
+    @param graph: a 2D integers array
+    @return: return a list of integers
+    """
+    def eventualSafeNodes(self, graph):
+        def dfs(graph, i, visited):
+            for j in graph[i]:
+                if j in visited:
+                    return False
+                if j in ans:
+                    continue
+                visited.add(j)
+                if not dfs(graph, j, visited):
+                    return False
+                visited.remove(j)
+            ans.add(i)
+            return True
+        ans = set()
+        for i in range(len(graph)):
+            visited = set([i])
+            dfs(graph, i, visited)
+        return sorted(list(ans))
+
 # V2 
 # Time:  O(|V| + |E|)
 # Space: O(|V|)
