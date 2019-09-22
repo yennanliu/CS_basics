41b7470139067837fb01772e1dec64fb71e6ec6f
yennanliu <f339339@gmail.com>
2019-09-22 18:26:10 +0800

    update status/code Depth-First-Search/is-graph-bipartite.py (dev)

--- leetcode_python/Depth-First-Search/is-graph-bipartite.py
+++ leetcode_python/Depth-First-Search/is-graph-bipartite.py
@@ -1,7 +1,34 @@
 # V0 
 
-# V1 
+# V1
+# https://www.jiuzhang.com/solution/is-graph-bipartite/#tag-highlight-lang-python
+# IDEA : GRAPH + DFS OR BFS 
+class Solution:
+    """
+    @param graph: the given undirected graph
+    @return:  return true if and only if it is bipartite
+    """
+    def isBipartite(self, graph):
+        n = len(graph)
+        self.color = [0] * n
+        for i in range(n):
+            if self.color[i] == 0 and not self.colored(i, graph, 1):
+                return False
+        return True        
+    
+    def colored(self, now, graph, c):
+        self.color[now] = c
+        for nxt in graph[now]:
+            if self.color[nxt] == 0 and not self.colored(nxt, graph, -c):
+                return False
+            elif self.color[nxt] == self.color[now]:
+                return False
+        return True
+
+# V1'
 # http://bookshadow.com/weblog/2018/02/18/leetcode-is-graph-bipartite/
+# IDEA : DFS + GRAPH 
+import collections
 class Solution(object):
     def isBipartite(self, graph):
         """
