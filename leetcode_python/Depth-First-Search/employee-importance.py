34fca0b8c9543259371874f175c9f67d57460b5a
yennanliu <f339339@gmail.com>
2019-09-23 16:11:01 +0800

    update status/code Depth-First-Search/employee-importance.py

--- leetcode_python/Depth-First-Search/employee-importance.py
+++ leetcode_python/Depth-First-Search/employee-importance.py
@@ -1,6 +1,60 @@
 # V0 
+"""
+# Employee info
+class Employee:
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+"""
+class Solution:
+    """
+    @param imput: 
+    @param id: 
+    @return: the total importance value 
+    """
+    def getImportance(self, employees, id):
+        # Write your code here.
 
-# V1 
+        def dfs(id):
+            count = G[id][0]
+            for sub in G[id][1]:
+                count += dfs(sub)
+            return count
+
+        G = {}
+        for emp in employees:
+            G[emp.id] = [emp.importance, emp.subordinates]
+        return dfs(id) if id in G else 0
+
+# V1
+# https://www.jiuzhang.com/solution/employee-importance/#tag-highlight-lang-python
+# IDEA : DFS + PROBLEM GIVE FUNC 
+class Solution:
+    """
+    @param imput: 
+    @param id: 
+    @return: the total importance value 
+    """
+    def getImportance(self, employees, id):
+        # Write your code here.
+
+        def dfs(id):
+            count = G[id][0]
+            for sub in G[id][1]:
+                count += dfs(sub)
+            return count
+
+        G = {}
+        for emp in employees:
+            G[emp.id] = [emp.importance, emp.subordinates]
+        return dfs(id) if id in G else 0
+
+# V1' 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79091343
 """
 # Employee info
@@ -26,7 +80,7 @@ class Solution(object):
             return employee_dict[id].importance + sum(dfs(id) for id in employee_dict[id].subordinates)
         return dfs(id)
 
-# V1' 
+# V1'' 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79091343
 class Solution:
     def getImportance(self, employees, id):
@@ -40,11 +94,32 @@ class Solution:
         for sub in emap[id].subordinates:
             res += self.getImportance(employees, sub)
         return res
-        
+
+# V1''' 
+# https://www.jiuzhang.com/solution/employee-importance/#tag-highlight-lang-python
+class Solution:
+    """
+    @param imput: 
+    @param id: 
+    @return: the total importance value 
+    """
+    def getImportance(self, employees, id):
+        # Write your code here.
+
+        def dfs(id):
+            count = G[id][0]
+            for sub in G[id][1]:
+                count += dfs(sub)
+            return count
+
+        G = {}
+        for emp in employees:
+            G[emp.id] = [emp.importance, emp.subordinates]
+        return dfs(id) if id in G else 0
+
 # V2 
 # Time:  O(n)
 # Space: O(h)
-import collections
 """
 # Employee info
 class Employee(object):
@@ -57,6 +132,7 @@ class Employee(object):
         # the id of direct subordinates
         self.subordinates = subordinates
 """
+import collections
 class Solution(object):
     def getImportance(self, employees, id):
         """
@@ -74,6 +150,7 @@ class Solution(object):
 
 # Time:  O(n)
 # Space: O(w), w is the max number of nodes in the levels of the tree
+import collections
 class Solution2(object):
     def getImportance(self, employees, id):
         """
