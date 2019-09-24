8e610e89faf31c48106f8e6d026b13af4f453571
yennanliu <f339339@gmail.com>
2019-09-24 17:47:31 +0800

    update status/code Depth-First-Search/pacific-atlantic-water-flow.py

--- leetcode_python/Depth-First-Search/pacific-atlantic-water-flow.py
+++ leetcode_python/Depth-First-Search/pacific-atlantic-water-flow.py
@@ -33,7 +33,39 @@ class Solution(object):
             if x < 0 or x >= m or y < 0 or y >= n or visited[x][y] or matrix[x][y] < matrix[i][j]:
                 continue
             self.dfs(visited, matrix, m, n, x, y)
-            
+
+# V1'
+# https://www.jiuzhang.com/solution/pacific-atlantic-water-flow/#tag-highlight-lang-python
+# IDEA : DFS 
+class Solution:
+    """
+    @param matrix: the given matrix
+    @return: The list of grid coordinates
+    """
+    def pacificAtlantic(self, matrix):
+        if not matrix: return []
+        pacific,atlantic = set(),set()
+        m,n = len(matrix),len(matrix[0])
+        for i in xrange(n):
+            self.dfs(0, i, matrix, pacific, 0)
+            self.dfs(m - 1, i, matrix, atlantic, 0)
+        for i in xrange(m):
+            self.dfs(i, 0, matrix, pacific, -1)
+            self.dfs(i, n - 1, matrix, atlantic, 0)
+        return list(pacific&atlantic)
+        
+    def dfs(self, x, y, matrix, visit, height):
+        m,n = len(matrix),len(matrix[0])
+        if x < 0 or x >= m or y < 0 or y >= n or (x,y) in visit:
+            return
+        if matrix[x][y] < height:
+            return
+        visit.add((x,y))
+        self.dfs(x - 1, y, matrix, visit, matrix[x][y]) 
+        self.dfs(x + 1, y, matrix, visit, matrix[x][y]) 
+        self.dfs(x, y - 1, matrix, visit, matrix[x][y]) 
+        self.dfs(x, y + 1, matrix, visit, matrix[x][y])
+
 # V2 
 # Time:  O(m * n)
 # Space: O(m * n)
