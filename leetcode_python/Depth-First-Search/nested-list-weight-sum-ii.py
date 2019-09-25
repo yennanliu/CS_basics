2377a649fe9421310b54d8eb658d4b78acc239b6
yennanliu <f339339@gmail.com>
2019-09-25 13:18:37 +0800

    update status/code Depth-First-Search/nested-list-weight-sum.py, nested-list-weight-sum-ii.py

--- leetcode_python/Depth-First-Search/nested-list-weight-sum-ii.py
+++ leetcode_python/Depth-First-Search/nested-list-weight-sum-ii.py
@@ -3,6 +3,42 @@
 # V1 
 # https://blog.csdn.net/qq508618087/article/details/51743408
 
+# V1'
+# https://www.jiuzhang.com/solution/nested-list-weight-sum-ii/#tag-highlight-lang-python
+class Solution:
+    """
+    @param nestedList: a list of NestedInteger
+    @return: the sum
+    """
+    def depthSumInverse(self, nestedList):
+        # Write your code here.
+        listq = nestedList
+        qintq = [] # queue of integer queue
+        
+        while listq:
+            intq = [] # this level integer queue
+            newlistq = [] # next level list queue
+            
+            while listq:
+                ni = listq.pop()
+                if ni.isInteger():
+                    intq.append(ni.getInteger())
+                else:
+                    newlistq.extend(ni.getList())
+            
+            qintq.append(intq)
+            listq = newlistq
+        
+        wsum = 0 # weight sum
+        w = 0 # weight
+        while qintq:
+            w += 1
+            intq = qintq.pop()
+            while intq:
+                wsum += w * intq.pop()
+        
+        return wsum
+        
 # V2 
 # Time:  O(n)
 # Space: O(h)
--- leetcode_python/Depth-First-Search/nested-list-weight-sum.py
+++ leetcode_python/Depth-First-Search/nested-list-weight-sum.py
@@ -3,6 +3,28 @@
 # V1 
 # https://www.cnblogs.com/grandyang/p/5340305.html
 
+# V1'
+# https://www.jiuzhang.com/solution/nested-list-weight-sum/#tag-highlight-lang-python
+class Solution(object):
+    # @param {NestedInteger[]} nestedList a list of NestedInteger Object
+    # @return {int} an integer
+    def depthSum(self, nestedList):
+        # Write your code here
+        if len(nestedList) == 0:
+            return 0
+        stack = []
+        sum = 0
+        for n in nestedList:
+            stack.append((n, 1))
+        while stack:
+            next, d = stack.pop(0)
+            if next.isInteger():
+               sum += d * next.getInteger()
+            else:
+                for i in next.getList():
+                    stack.append((i, d+1))
+        return sum
+
 # V2 
 # Time:  O(n)
 # Space: O(h)
