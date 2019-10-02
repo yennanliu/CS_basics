8ef5b84bb76322d21ddd8118752a107e2624c963
yennanliu <f339339@gmail.com>
2019-10-02 17:50:36 +0800

    update status/code Hash_table/accounts-merge.py

--- leetcode_python/Hash_table/accounts-merge.py
+++ leetcode_python/Hash_table/accounts-merge.py
@@ -37,6 +37,70 @@ class Solution(object):
             result[findParent(email)[0]].append(email)
         return [[owner[k]] + sorted(v) for k, v in result.iteritems()]
 
+# V1'
+# https://www.jiuzhang.com/solution/accounts-merge/#tag-highlight-lang-python
+class Solution:
+    """
+    @param accounts: List[List[str]]
+    @return: return a List[List[str]]
+    """
+    def accountsMerge(self, accounts):
+        self.initialize(len(accounts))
+        email_to_ids = self.get_email_to_ids(accounts)
+        
+        # union
+        for email, ids in email_to_ids.items():
+            root_id = ids[0]
+            for id in ids[1:]:
+                self.union(id, root_id)
+                
+        id_to_email_set = self.get_id_to_email_set(accounts)
+        
+        merged_accounts = []
+        for user_id, email_set in id_to_email_set.items():
+            merged_accounts.append([
+                accounts[user_id][0],
+                *sorted(email_set),
+            ])
+        return merged_accounts
+    
+    def get_id_to_email_set(self, accounts):
+        id_to_email_set = {}
+        for user_id, account in enumerate(accounts):
+            root_user_id = self.find(user_id)
+            email_set = id_to_email_set.get(root_user_id, set())
+            for email in account[1:]:
+                email_set.add(email)
+            id_to_email_set[root_user_id] = email_set
+        return id_to_email_set
+            
+    def get_email_to_ids(self, accounts):
+        email_to_ids = {}
+        for i, account in enumerate(accounts):
+            for email in account[1:]:
+                email_to_ids[email] = email_to_ids.get(email, [])
+                email_to_ids[email].append(i)
+        return email_to_ids
+        
+    def initialize(self, n):
+        self.father = {}
+        for i in range(n):
+            self.father[i] = i
+            
+    def union(self, id1, id2):
+        self.father[self.find(id1)] = self.find(id2)
+
+    def find(self, user_id):
+        path = []
+        while user_id != self.father[user_id]:
+            path.append(user_id)
+            user_id = self.father[user_id]
+            
+        for u in path:
+            self.father[u] = user_id
+            
+        return user_id
+
 # V2 
 # Time:  O(nlogn), n is the number of total emails,
 #                  and the max length ofemail is 320, p.s. {64}@{255}
