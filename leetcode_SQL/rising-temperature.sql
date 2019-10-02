820dbe8a64a592c7cce0ce5cb0eedd8cd5d0009a
yennanliu <f339339@gmail.com>
2019-10-02 22:06:35 +0800

    fix typo

--- leetcode_SQL/rising-temperature.sql
+++ leetcode_SQL/rising-temperature.sql
@@ -38,4 +38,4 @@ and (b.Temperature)  > (a.Temperature)
 SELECT wt1.Id 
 FROM Weather wt1, Weather wt2
 WHERE wt1.Temperature > wt2.Temperature AND 
-      TO_DAYS(wt1.DATE)-TO_DAYS(wt2.DATE)=1;
+      TO_DAYS(wt1.RecordDate)-TO_DAYS(wt2.RecordDate)=1;
