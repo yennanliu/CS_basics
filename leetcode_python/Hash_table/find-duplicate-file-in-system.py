498e9b4d1a83ed7b71876f5a3eb37e4ad4c2fca9
yennanliu <f339339@gmail.com>
2019-10-02 18:26:22 +0800

    update status/code Hash_table/find-duplicate-file-in-system.py

--- leetcode_python/Hash_table/find-duplicate-file-in-system.py
+++ leetcode_python/Hash_table/find-duplicate-file-in-system.py
@@ -1,9 +1,5 @@
 # V0 
-
-# V1 
-# https://blog.csdn.net/fuxuemingzhu/article/details/79434941
-# http://bookshadow.com/weblog/2017/06/04/leetcode-find-duplicate-file-in-system/
-# IDEA : collections.defaultdict 
+import collections
 class Solution(object):
     def findDuplicate(self, paths):
         """
@@ -19,6 +15,40 @@ class Solution(object):
                 name, content = tokens[0], tokens[1][4:-1]
                 contentDict[content].append(dir + '/' + name + '.txt')
         return [g for g in contentDict.values() if len(g) > 1]
+        
+# V1 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79434941
+# http://bookshadow.com/weblog/2017/06/04/leetcode-find-duplicate-file-in-system/
+# IDEA : collections.defaultdict 
+import collections
+class Solution(object):
+    def findDuplicate(self, paths):
+        """
+        :type paths: List[str]
+        :rtype: List[List[str]]
+        """
+        contentDict = collections.defaultdict(list)
+        for path in paths:
+            parts = path.split()
+            dir, files = parts[0], parts[1:]
+            for file in files:
+                tokens = file.split('.')
+                name, content = tokens[0], tokens[1][4:-1]
+                contentDict[content].append(dir + '/' + name + '.txt')
+        return [g for g in contentDict.values() if len(g) > 1]
+
+# V1'
+# https://www.jiuzhang.com/solution/find-duplicate-file-in-system/#tag-highlight-lang-python
+import collections
+class Solution:
+    def findDuplicate(self, paths):
+        dic = collections.defaultdict(list)
+        for path in paths:
+            root, *f = path.split(" ")
+            for file in f:
+                txt, content = file.split("(")
+                dic[content] += root + "/" + txt,
+        return [dic[key] for key in dic if len(dic[key]) > 1]
 
 # V2 
 # Time:  O(n * l), l is the average length of file content
