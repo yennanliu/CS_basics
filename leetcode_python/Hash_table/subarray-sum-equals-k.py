56064fed1fd726bffb6912a18a20aa818d3b31ca
yennanliu <f339339@gmail.com>
2019-10-03 18:36:10 +0800

    update status/code Hash_table/subarray-sum-equals-k.py

--- leetcode_python/Hash_table/subarray-sum-equals-k.py
+++ leetcode_python/Hash_table/subarray-sum-equals-k.py
@@ -1,5 +1,24 @@
 # V0 
-
+class Solution:
+    """
+    @param nums: a list of integer
+    @param k: an integer
+    @return: return an integer, denote the number of continuous subarrays whose sum equals to k
+    """
+    def subarraySumEqualsK(self, nums, k):
+        # write your code here
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        d, ans = {0 : 1}, 0
+        for i in range(len(nums)):
+            if(d.get(nums[i] - k) != None):
+                ans += d[nums[i] - k]
+            if(d.get(nums[i]) == None):
+                d[nums[i]] = 1
+            else:
+                d[nums[i]] += 1
+        return ans
+        
 # V1 
 # http://bookshadow.com/weblog/2017/04/30/leetcode-subarray-sum-equals-k/
 import collections
@@ -39,6 +58,28 @@ class Solution(object):
                 res += d[sum - k]
             d[sum] += 1
         return res
+
+# V1''
+# https://www.jiuzhang.com/solution/subarray-sum-equals-k/#tag-highlight-lang-python
+class Solution:
+    """
+    @param nums: a list of integer
+    @param k: an integer
+    @return: return an integer, denote the number of continuous subarrays whose sum equals to k
+    """
+    def subarraySumEqualsK(self, nums, k):
+        # write your code here
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        d, ans = {0 : 1}, 0
+        for i in range(len(nums)):
+            if(d.get(nums[i] - k) != None):
+                ans += d[nums[i] - k]
+            if(d.get(nums[i]) == None):
+                d[nums[i]] = 1
+            else:
+                d[nums[i]] += 1
+        return ans
 
 # V2 
 # Time:  O(n)
