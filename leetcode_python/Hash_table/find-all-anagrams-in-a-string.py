ffbd6d49a389217b787bec2f1be6b28fcfd6b007
yennanliu <f339339@gmail.com>
2019-10-05 15:22:06 +0800

    update status/code Hash_table/find-all-anagrams-in-a-string.py

--- leetcode_python/Hash_table/find-all-anagrams-in-a-string.py
+++ leetcode_python/Hash_table/find-all-anagrams-in-a-string.py
@@ -67,6 +67,50 @@ The substring with start index = 2 is "ab", which is an anagram of "ab".
 
 
 # V0 
+# DEV 
+# import collections
+# class Solution(object):
+#     def findAnagrams(self, s, p):
+#         """
+#         :type s: str
+#         :type p: str
+#         :rtype: List[int]
+#         """
+#         ls, lp = len(s), len(p)
+#         cp = collections.Counter(p)
+#         cs = collections.Counter()
+#         ans = []
+#         if lp > ls:
+#             return ans 
+#         for i in range(ls):
+#             if len(s[i:]) >= lp and   collections.Counter(s[i:i+lp]) == collections.Counter(p):
+#                 ans.append(i)
+#         return ans 
+
+# V0'
+from collections import Counter
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        answer = []
+        m, n = len(s), len(p)
+        if m < n:
+            return answer
+        pCounter = Counter(p)
+        sCounter = Counter(s[:n-1])
+        index = 0
+        for index in range(n - 1, m):
+                sCounter[s[index]] += 1
+                if sCounter == pCounter:
+                    answer.append(index - n + 1)
+                sCounter[s[index - n + 1]] -= 1
+                if sCounter[s[index - n + 1]] == 0:   # NOTE : HAVE TO REMOVE "COUNT = 0" CASE IN COUNTER 
+                    del sCounter[s[index - n + 1]]
+        return answer
 
 # V1 
 # http://bookshadow.com/weblog/2016/10/23/leetcode-find-all-anagrams-in-a-string/
@@ -93,6 +137,83 @@ class Solution(object):
                 ans.append(i - lp + 1)
         return ans
 
+ 
+# V1'
+# https://blog.csdn.net/fuxuemingzhu/article/details/79184109
+# IDEA : collections.Counter
+from collections import Counter
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        answer = []
+        m, n = len(s), len(p)
+        if m < n:
+            return answer
+        pCounter = Counter(p)
+        sCounter = Counter(s[:n-1])
+        index = 0
+        for index in range(n - 1, m):
+                sCounter[s[index]] += 1
+                if sCounter == pCounter:
+                    answer.append(index - n + 1)
+                sCounter[s[index - n + 1]] -= 1
+                if sCounter[s[index - n + 1]] == 0:   # NOTE : HAVE TO REMOVE "COUNT = 0" CASE IN COUNTER 
+                    del sCounter[s[index - n + 1]]
+        return answer
+
+# V1''
+# https://blog.csdn.net/fuxuemingzhu/article/details/79184109
+# IDEA : TWO POINTERS 
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        count = collections.Counter()
+        M, N = len(s), len(p)
+        left, right = 0, 0
+        pcount = collections.Counter(p)
+        res = []
+        while right < M:
+            count[s[right]] += 1
+            if right - left + 1 == N:
+                if count == pcount:
+                    res.append(left)
+                count[s[left]] -= 1
+                if count[s[left]] == 0:
+                    del count[s[left]]
+                left += 1
+            right += 1
+        return res
+
+# V1''
+# http://zxi.mytechroad.com/blog/hashtable/leetcode-438-find-all-anagrams-in-a-string/
+# C++
+# // Author: Huahua
+# // Running time: 35 ms
+# class Solution {
+# public:
+#   vector<int> findAnagrams(string s, string p) {
+#     int n = s.length();
+#     int l = p.length();
+#     vector<int> ans;
+#     vector<int> vp(26, 0);
+#     vector<int> vs(26, 0);
+#     for (char c : p) ++vp[c - 'a'];    
+#     for (int i = 0; i < n; ++i) {
+#       if (i >= l) --vs[s[i - l] - 'a'];        
+#       ++vs[s[i] - 'a'];
+#       if (vs == vp) ans.push_back(i + 1 - l);
+#     }
+#     return ans;
+#   }
+# };
 
 # V2 
 # Time:  O(n)
