432e8cd604f0dcd489db54ae55e7fa7671653728
yennanliu <f339339@gmail.com>
2019-10-05 16:20:55 +0800

    update code Hash_table/longest-absolute-file-path.py

--- leetcode_python/Hash_table/longest-absolute-file-path.py
+++ leetcode_python/Hash_table/longest-absolute-file-path.py
@@ -43,7 +43,30 @@ class Solution(object):
             else:
                 pathlen[depth + 1] = pathlen[depth] + len(name) + 1
         return maxlen
-        
+
+# V1'
+# https://www.jiuzhang.com/solution/longest-absolute-file-path/#tag-highlight-lang-python
+import re, collections
+class Solution:
+    # @param {string} input an abstract file system
+    # @return {int} return the length of the longest absolute path to file
+    def lengthLongestPath(self, input):
+        # Write your code here
+        dict = collections.defaultdict(lambda: "")
+        lines = input.split("\n")
+
+        n = len(lines)
+        result = 0
+        for i in xrange(n):
+            count = lines[i].count("\t")
+
+            lines[i] = dict[count - 1] + re.sub("\\t+","/", lines[i])
+            if "." in lines[i]:
+                result = max(result, len(lines[i]))
+            dict[count] = lines[i]
+
+        return result
+
 # V2
 # Time:  O(n)
 # Space: O(d), d is the max depth of the paths
