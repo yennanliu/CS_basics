348c0d6836cdb665cdf2677c59f6907b4af60e6b
yennanliu <f339339@gmail.com>
2019-10-06 23:50:05 +0800

    update status/code Hash_table/word-pattern.py

--- leetcode_python/Hash_table/word-pattern.py
+++ leetcode_python/Hash_table/word-pattern.py
@@ -16,6 +16,25 @@
 #   4. Each letter in pattern must map to a word with length that is at least 1.
 
 # V0 
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        words = str.split()
+        if len(pattern) != len(words):
+            return False
+        ptnDict, wordDict = {}, {}
+        for ptn, word in zip(pattern, words):
+            if ptn not in ptnDict:
+                ptnDict[ptn] = word
+            if word not in wordDict:
+                wordDict[word] = ptn
+            if wordDict[word] != ptn or ptnDict[ptn] != word:
+                return False
+        return True
 
 # V1 
 # http://bookshadow.com/weblog/2015/10/05/leetcode-word-pattern/
@@ -40,13 +59,44 @@ class Solution(object):
         if len(pattern) != len(words):
             return False
         ptnDict, wordDict = {}, {}
-        for ptn, word in zip(pattern, words):
+        for ptn, word in zip(pattern, words):  # use "for loop on zip(pattern, words)" trick reduce original  2 loops to 1 loop
             if ptn not in ptnDict:
                 ptnDict[ptn] = word
             if word not in wordDict:
                 wordDict[word] = ptn
             if wordDict[word] != ptn or ptnDict[ptn] != word:
                 return False
+        return True
+
+# V1'
+# https://www.jiuzhang.com/solution/word-pattern/#tag-highlight-lang-python
+class Solution:
+    """
+    @param pattern: a string,denote pattern string
+    @param str: a string,denote matching string
+    @return: return an boolean,denote whether the pattern string and the matching string match or not
+    """
+    def wordPattern(self, pattern, str):
+        # write your code here
+        mp1, mp2 = {}, {}
+        tmp = ""
+        str += ' '
+        cnt, now = 0, 0
+        for i in range(len(str)):
+            if(str[i] == ' '):
+                if((not pattern[cnt] in mp1) and (not tmp in mp2)):
+                    mp1[pattern[cnt]] = now
+                    mp2[tmp] = now
+                    now += 1
+                elif ((pattern[cnt] in mp1) and (tmp in mp2)):
+                    if(mp1[pattern[cnt]] != mp2[tmp]):
+                        return False
+                else:
+                    return False
+                tmp = ""
+                cnt += 1
+            else:
+                tmp += str[i]
         return True
 
 # V2 
