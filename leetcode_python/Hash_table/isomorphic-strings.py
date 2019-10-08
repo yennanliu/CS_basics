60d8044bf90bfdfc8a9c0e8d6f6e4ef7cf267141
yennanliu <f339339@gmail.com>
2019-10-08 13:10:33 +0800

    udpate comment/code Hash_table/isomorphic-strings.py

--- leetcode_python/Hash_table/isomorphic-strings.py
+++ leetcode_python/Hash_table/isomorphic-strings.py
@@ -61,6 +61,30 @@ class Solution1(object):
             a2[ord(t[i])]=i
         return True
 
+# V1''
+# https://www.jiuzhang.com/solution/isomorphic-strings/#tag-highlight-lang-python
+class Solution:
+    """
+    @param s: a string
+    @param t: a string
+    @return: true if the characters in s can be replaced to get t or false
+    """
+    def isIsomorphic(self, s, t):
+        # write your code here
+        map1 = {}
+        map2 = {}
+        for i in range(len(s)):
+            if s[i] not in map1:
+                map1[s[i]] = t[i]
+            elif map1[s[i]] != t[i]:
+                return False
+        for i in range(len(t)):
+            if t[i] not in map2:
+                map2[t[i]] = s[i]
+            elif map2[t[i]] != s[i]:
+                return False
+        return True
+        
 # V2 
 # Time:  O(n)
 # Space: O(1)
