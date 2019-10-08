1e8764f21fa01df752c27574c1512db26581f01a
yennanliu <f339339@gmail.com>
2019-10-08 15:32:10 +0800

    update status/code Hash_table/two-sum-iii-data-structure-design.py

--- leetcode_python/Hash_table/two-sum-iii-data-structure-design.py
+++ leetcode_python/Hash_table/two-sum-iii-data-structure-design.py
@@ -26,6 +26,25 @@ find(7) -> false
 # find(7) -> false
 
 # V0
+class TwoSum(object):
+
+    def __init__(self):
+        self.dic = {}
+
+    def add(self, number):
+        if number not in self.dic:
+            self.dic[number] = 1
+        else:
+            self.dic[number] += 1
+
+    def find(self, value):
+        dic = self.dic
+        for num in dic:
+            # Find if there exists any pair of numbers (in the dict) which sum is equal to the value.
+            if value - num in dic and (value - num != num or dic[num] > 1): # 2 sum, so the sum can only be came from 2 number in the dict
+                return True
+        return False
+
 
 # V1 
 # http://www.voidcn.com/article/p-qhmcmxvf-qp.html
@@ -43,8 +62,19 @@ class TwoSum(object):
     def find(self, value):
         dic = self.dic
         for num in dic:
-            # Find if there exists any pair of numbers (in the dict) which sum is equal to the value.
-            if value - num in dic and (value - num != num or dic[num] > 1): # 2 sum, so the sum can only be came from 2 number in the dict
+            """
+            This is actually the "2 sum" problem 
+            # 001 TWO SUM 
+            # https://github.com/yennanliu/CS_basics/blob/master/leetcode_python/Hash_table/two-sum.py
+            1.  Find if there exists any pair of numbers (in the dict) which sum is equal to the value.
+            2.  Consinder two cases in below logic:
+                GIVEN num + sth = value ->  sth = value - num
+                case 1) sth ! = num --> value - num in dic and value - num != num
+                case 2) sth == num --> value - num in dic and dic[num] > 1
+
+                --> sum up case 1) & 2) : value - num in dic and (value - num != num or dic[num] > 1)
+            """
+            if value - num in dic and (value - num != num or dic[num] > 1): 
                 return True
         return False
 
