fccd6cc5325a6f3f8cf5753bb06f4846d92b1359
yennanliu <f339339@gmail.com>
2019-10-14 18:36:34 +0800

    update status/code Linked_list/swap-nodes-in-pairs.py

--- leetcode_python/Linked_list/swap-nodes-in-pairs.py
+++ leetcode_python/Linked_list/swap-nodes-in-pairs.py
@@ -1,6 +1,56 @@
 # V0 
+class Solution:
+  def swapPairs(self, head):
+    if not head or not head.next: return head
+    dummy = ListNode(0)
+    dummy.next = head
+    head = dummy
+    while head.next and head.next.next:
+      n1, n2 = head.next, head.next.next
+      n1.next = n2.next
+      n2.next = n1
+      head.next = n2      
+      head = n1
+    return dummy.next
+    
+# V1
+# https://zxi.mytechroad.com/blog/list/leetcode-24-swap-nodes-in-pairs/
+# VIDEO DEMO 
+# https://www.youtube.com/watch?v=f45_eF1gmn8
+# IDEA 
+# *** EACH "SWAP" OPERATION WILL IMPLMENT ON 3 NODES
+# e.g.
+#  1 -> 2 -> 3 -> 4 -> 5 ----SWAP----> 2 -> 1 -> 3 -> 4 -> 5 (swap 1,2 ; but 1,2,3 are affected)
+#
+# SO FOR THIS PROBLEM, WE NEED TO DEFINE 3 NODES : 
+# dummy (pre), cur, next 
+#
+# STEP 1) 
+# 1 -> 2 -> 3 -> 4 -> 5  =>  dummy -> 1 -> 2 -> 3 -> 4 -> 5   
+#
+# STEP 2) 
+# if dummy.next and dummy.next.next exist
+#     dummy -> 1 -> 2 -> 3 -> 4 -> 5  =>  dummy -> 2 -> 1 -> 3 -> 4 -> 5
+#     # 1 as pre node, doing next SWAP 
+# STEP 3) 
+# if dummy.next and dummy.next.next exist
+#     dummy -> 2 -> 1 -> 3 -> 4 -> 5  =>  dummy -> 2 -> 1 -> 4 -> 3 -> 5
+#     # 3 as pre node, but no pre.next or pre.next.next, SO STOP THE PROCESS
+class Solution:
+  def swapPairs(self, head):
+    if not head or not head.next: return head
+    dummy = ListNode(0)
+    dummy.next = head
+    head = dummy
+    while head.next and head.next.next:
+      n1, n2 = head.next, head.next.next
+      n1.next = n2.next
+      n2.next = n1
+      head.next = n2      
+      head = n1
+    return dummy.next
 
-# V1 
+# V1' 
 # https://blog.csdn.net/coder_orz/article/details/51532184
 # IDEA : LINKED LIST 
 # Definition for singly-linked list.
@@ -26,7 +76,7 @@ class Solution(object):
             head = head.next
         return new_head.next
 
-# V1'
+# V1'' 
 # https://blog.csdn.net/coder_orz/article/details/51532184
 # IDEA : ITERATION  
 # Definition for singly-linked list.
@@ -46,6 +96,26 @@ class Solution(object):
         head.next = self.swapPairs(head.next.next)
         new_head.next = head
         return new_head
+
+# V1''' 
+# https://www.jiuzhang.com/solution/swap-nodes-in-pairs/#tag-highlight-lang-python
+class Solution:
+    # @param head, a ListNode
+    # @return a ListNode
+    def swapPairs(self, head):
+        # Write your code here
+        if head == None or head.next == None:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        p = dummy
+        while p.next and p.next.next:
+            tmp = p.next.next
+            p.next.next = tmp.next
+            tmp.next = p.next
+            p.next = tmp
+            p = p.next.next
+        return dummy.next
 
 # V2 
 # Time:  O(n)
