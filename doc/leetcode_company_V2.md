571a6d0051e86cc0c6dbee52982cbc610ce92805
yennanliu <f339339@gmail.com>
2019-10-17 17:31:44 +0800

    update status/code String/add-binary.py

--- doc/leetcode_company_V2.md
+++ doc/leetcode_company_V2.md
@@ -710,7 +710,7 @@
 | 56 | [Merge Intervals](https://leetcode.com/problems/merge-intervals/) | Medium | 33.1% | 0.623 | AGAIN******
 | 57 | [Insert Interval](https://leetcode.com/problems/insert-interval/) | Hard | 29.7% | 0.079 |
 | 65 | [Valid Number](https://leetcode.com/problems/valid-number/) | Hard | 13.1% | 0.79 |
-| 67 | [Add Binary](https://leetcode.com/problems/add-binary/) | Easy | 35.8% | 0.978 |
+| 67 | [Add Binary](https://leetcode.com/problems/add-binary/) | Easy | 35.8% | 0.978 |  OK* (3) (but again)
 | 75 | [Sort Colors](https://leetcode.com/problems/sort-colors/) | Medium | 40.0% | 0.024 |
 | 76 | [Minimum Window Substring](https://leetcode.com/problems/minimum-window-substring/) | Hard | 28.2% | 0.699 |
 | 78 | [Subsets](https://leetcode.com/problems/subsets/) | Medium | 47.8% | 0.019 |
--- leetcode_python/String/add-binary.py
+++ leetcode_python/String/add-binary.py
@@ -12,7 +12,24 @@ Input: a = "1010", b = "1011"
 Output: "10101" 
 """
 
-# V0 
+# V0
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        res = ''
+        i, j, plus = len(a)-1, len(b)-1, 0
+        while i>=0 or j>=0 or plus==1:
+            plus += int(a[i]) if i>= 0 else 0
+            plus += int(b[j]) if j>= 0 else 0
+            res = str(plus % 2) + res
+            i, j, plus = i-1, j-1, plus//2  # since max of "plus" is 3 in bimary case, so "plus//2" works here
+        return res
+
+# V0'
 class Solution(object):
     # @param a, a string
     # @param b, a string
@@ -71,7 +88,7 @@ class Solution(object):
             plus += int(a[i]) if i>= 0 else 0
             plus += int(b[j]) if j>= 0 else 0
             res = str(plus % 2) + res
-            i, j, plus = i-1, j-1, plus/2
+            i, j, plus = i-1, j-1, plus//2
         return res
 
 # V1'' 
@@ -92,6 +109,30 @@ class Solution(object):
             return self.addBinary(a[:-1], b[:-1]) + '0'
         else:
             return self.addBinary(a[:-1], b[:-1]) + '1'
+
+# V1'''
+# https://www.jiuzhang.com/solution/add-binary/#tag-highlight-lang-python
+class Solution:
+    # @param {string} a a number
+    # @param {string} b a number
+    # @return {string} the result
+    def addBinary(self, a, b):
+        indexa = len(a) - 1
+        indexb = len(b) - 1
+        carry = 0
+        sum = ""
+        while indexa >= 0 or indexb >= 0:
+            x = int(a[indexa]) if indexa >= 0 else 0
+            y = int(b[indexb]) if indexb >= 0 else 0
+            if (x + y + carry) % 2 == 0:
+                sum = '0' + sum
+            else:
+                sum = '1' + sum
+            carry = (x + y + carry) / 2
+            indexa, indexb = indexa - 1, indexb - 1
+        if carry == 1:
+            sum = '1' + sum
+        return sum
 
 # V2 
 class Solution:
