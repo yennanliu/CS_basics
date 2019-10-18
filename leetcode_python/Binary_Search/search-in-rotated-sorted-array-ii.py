f80a3ab0b15f844277e196910601e09461c9c18a
yennanliu <f339339@gmail.com>
2019-10-18 18:35:06 +0800

    update status/code search-in-rotated-sorted-array.py

--- leetcode_python/Binary_Search/search-in-rotated-sorted-array-ii.py
+++ leetcode_python/Binary_Search/search-in-rotated-sorted-array-ii.py
@@ -29,6 +29,16 @@ class Solution(object):
                     r = mid - 1
         return False
 
+# V1'
+# https://www.jiuzhang.com/solution/search-in-rotated-sorted-array-ii/#tag-highlight-lang-python
+class Solution(object):
+    def search(self, A, target):
+        # write your code here
+        for num in A:
+            if num == target:
+                return True
+        return False
+        
 # V2 
 # Time:  O(logn)
 # Space: O(1)
--- leetcode_python/Binary_Search/search-in-rotated-sorted-array.py
+++ leetcode_python/Binary_Search/search-in-rotated-sorted-array.py
@@ -1,7 +1,4 @@
 # V0
-
-# V1
-# https://blog.csdn.net/fuxuemingzhu/article/details/79534213
 class Solution(object):
     def search(self, nums, target):
         """
@@ -16,12 +13,41 @@ class Solution(object):
             if nums[mid] == target:
                 return mid
             if nums[mid] < nums[right]:
-                if target > nums[mid] and target <= nums[right]:
+                if target > nums[mid] and target <= nums[right]: # check the relationship with target, which is different from the default binary search
                     left = mid + 1
                 else:
                     right = mid - 1
             else:
-                if target < nums[mid] and target >= nums[left]:
+                if target < nums[mid] and target >= nums[left]:  # check the relationship with target, which is different from the default binary search
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1     
+
+# V1
+# https://blog.csdn.net/fuxuemingzhu/article/details/79534213
+# CHECK ALSO : BINARY SEARCH 
+# https://github.com/yennanliu/CS_basics/blob/master/algorithm/python/binary_search.py
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums: return -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < nums[right]:
+                if target > nums[mid] and target <= nums[right]: # check the relationship with target, which is different from the default binary search
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target < nums[mid] and target >= nums[left]:  # check the relationship with target, which is different from the default binary search
                     right = mid - 1
                 else:
                     left = mid + 1
