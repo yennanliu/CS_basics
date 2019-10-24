30c6ae9fe90eae082c20784b315c71a92d9f6680
yennanliu <f339339@gmail.com>
2019-10-25 00:21:57 +0800

    update status/code Recursion/minimum-depth-of-binary-tree.py

--- doc/leetcode_company_V2.md
+++ doc/leetcode_company_V2.md
@@ -728,7 +728,7 @@
 | 105 | [Construct Binary Tree from Preorder and Inorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/) | Medium | 36.7% | 0.012 | AGAIN*** (3)
 | 109 | [Convert Sorted List to Binary Search Tree](https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/) | Medium | 37.4% | 0.021 |  AGAIN*** (3)
 | 110 | [Balanced Binary Tree](https://leetcode.com/problems/balanced-binary-tree/) | Easy | 39.2% | 0.011 |  AGAIN****** (3)
-| 111 | [Minimum Depth of Binary Tree](https://leetcode.com/problems/minimum-depth-of-binary-tree/) | Easy | 34.1% | 0.024 |
+| 111 | [Minimum Depth of Binary Tree](https://leetcode.com/problems/minimum-depth-of-binary-tree/) | Easy | 34.1% | 0.024 |  AGAIN***** (3)
 | 113 | [Path Sum II](https://leetcode.com/problems/path-sum-ii/) | Medium | 37.5% | 0.015 |
 | 114 | [Flatten Binary Tree to Linked List](https://leetcode.com/problems/flatten-binary-tree-to-linked-list/) | Medium | 38.6% | 0.084 |
 | 121 | [Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/) | Easy | 44.4% | 0.911 |
--- leetcode_python/Recursion/minimum-depth-of-binary-tree.py
+++ leetcode_python/Recursion/minimum-depth-of-binary-tree.py
@@ -1,9 +1,112 @@
-# V0 : DEV 
+# V0
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
 
-# V1 
+        if not root.left:
+            return 1 + self.minDepth(root.right)
+        elif not root.right:
+            return 1 + self.minDepth(root.left)
+        else:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+# V1
+# https://blog.csdn.net/coder_orz/article/details/51337522
+# IDEA : DFS
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+
+        if not root.left:
+            return 1 + self.minDepth(root.right)
+        elif not root.right:
+            return 1 + self.minDepth(root.left)
+        else:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+# V1
+# http://bookshadow.com/weblog/2015/11/28/leetcode-minimum-depth-binary-tree/
+# IDEA : DFS
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if left and right:
+            return min(left, right) + 1
+        return max(left, right) + 1
+
+# V1'
+# http://bookshadow.com/weblog/2015/11/28/leetcode-minimum-depth-binary-tree/
+# IDEA : BFS 
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        queue = collections.deque([(root, 1)])
+        while queue:
+            node, step = queue.popleft()
+            if not node.left and not node.right:
+                return step
+            if node.left:
+                queue += (node.left, step + 1)
+            if node.right:
+                queue += (node.right, step + 1)
+                
+# V1''
+# https://www.jiuzhang.com/solution/minimum-depth-of-binary-tree/#tag-highlight-lang-python
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+"""
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """ 
+    def minDepth(self, root):
+        # write your code here
+        return self.find(root)
+
+    def find(self, node):
+        if node is None:
+            return 0
+        left, right = 0, 0
+        if node.left != None:
+            left = self.find(node.left)
+        else:
+            return self.find(node.right) + 1
+
+        if node.right != None:
+            right = self.find(node.right)
+        else:
+            return left + 1
+        return min(left,right) + 1
+
+# V2 
 # Time:  O(n)
 # Space: O(h), h is height of binary tree
-
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
