3c43555ce82712f23e42e8c2420a2741fe2eacc9
yennanliu <f339339@gmail.com>
2019-10-28 11:19:42 +0800

    update status/code /flatten-binary-tree-to-linked-list.py

--- doc/leetcode_company_V2.md
+++ doc/leetcode_company_V2.md
@@ -730,7 +730,7 @@
 | 110 | [Balanced Binary Tree](https://leetcode.com/problems/balanced-binary-tree/) | Easy | 39.2% | 0.011 |  AGAIN****** (3)
 | 111 | [Minimum Depth of Binary Tree](https://leetcode.com/problems/minimum-depth-of-binary-tree/) | Easy | 34.1% | 0.024 |  AGAIN***** (3)
 | 113 | [Path Sum II](https://leetcode.com/problems/path-sum-ii/) | Medium | 37.5% | 0.015 | AGAIN**** (4)
-| 114 | [Flatten Binary Tree to Linked List](https://leetcode.com/problems/flatten-binary-tree-to-linked-list/) | Medium | 38.6% | 0.084 |
+| 114 | [Flatten Binary Tree to Linked List](https://leetcode.com/problems/flatten-binary-tree-to-linked-list/) | Medium | 38.6% | 0.084 | AGAIN**** (4)
 | 121 | [Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/) | Easy | 44.4% | 0.911 |
 | 123 | [Best Time to Buy and Sell Stock III](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/) | Hard | 31.5% | 0.088 |
 | 124 | [Binary Tree Maximum Path Sum](https://leetcode.com/problems/binary-tree-maximum-path-sum/) | Hard | 28.0% | 0.544 |
--- leetcode_python/Recursion/flatten-binary-tree-to-linked-list.py
+++ leetcode_python/Recursion/flatten-binary-tree-to-linked-list.py
@@ -1,10 +1,118 @@
-# V0 : DEV 
+# V0  
 
+# V1
+# https://blog.csdn.net/fuxuemingzhu/article/details/70241424
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        res = []
+        self.preOrder(root, res)
+        for i in range(len(res) - 1):
+            res[i].left = None
+            res[i].right = res[i + 1]
+    
+    def preOrder(self, root, res):
+        if not root: return
+        res.append(root)
+        self.preOrder(root.left, res)
+        self.preOrder(root.right, res)
 
-# V1 
+# V1'
+# http://bookshadow.com/weblog/2016/09/02/leetcode-flatten-binary-tree-to-linked-list/
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        pointer = dummy = TreeNode(None)
+        stack = [root]
+        while stack:
+            top = stack.pop()
+            if not top: continue
+            stack.append(top.right)
+            stack.append(top.left)
+            pointer.right = top
+            pointer.left = None
+            pointer = top
+
+# V1''
+# http://bookshadow.com/weblog/2016/09/02/leetcode-flatten-binary-tree-to-linked-list/
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        self.pointer = None
+        def traverse(root):
+            if not root: return
+            traverse(root.right)
+            traverse(root.left)
+            root.right = self.pointer
+            root.left = None
+            self.pointer = root
+        traverse(root)
+
+# V1'''
+# https://www.jiuzhang.com/solution/flatten-binary-tree-to-linked-list/#tag-highlight-lang-python
+class Solution:
+    last_node = None
+    
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root):
+        if root is None:
+            return
+        
+        if self.last_node is not None:
+            self.last_node.left = None
+            self.last_node.right = root
+            
+        self.last_node = root
+        right = root.right
+        self.flatten(root.left)
+        self.flatten(right)
+
+# V1''''
+# https://www.jiuzhang.com/solution/flatten-binary-tree-to-linked-list/#tag-highlight-lang-python
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root):
+        self.helper(root)
+        
+    # restructure and return last node in preorder
+    def helper(self, root):
+        if root is None:
+            return None
+            
+        left_last = self.helper(root.left)
+        right_last = self.helper(root.right)
+        
+        # connect 
+        if left_last is not None:
+            left_last.right = root.right
+            root.right = root.left
+            root.left = None
+            
+        if right_last is not None:
+            return right_last
+            
+        if left_last is not None:
+            return left_last            
+        return root
+
+# V2 
 # Time:  O(n)
 # Space: O(h), h is height of binary tree
-
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
