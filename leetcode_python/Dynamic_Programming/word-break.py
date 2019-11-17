af23a7881fcac2b27cb52bd951a924a566aae61d
yennanliu <f339339@gmail.com>
2019-11-17 17:34:27 +0800

    update status/code word-break.py

--- leetcode_python/Dynamic_Programming/word-break.py
+++ leetcode_python/Dynamic_Programming/word-break.py
@@ -20,6 +20,29 @@ class Solution(object):
                     dp[i] = True
                     print(dp)
         return dp.pop()
+
+# V1'
+# https://www.jiuzhang.com/solution/word-break/#tag-highlight-lang-python
+class Solution:
+    # @param s: A string s
+    # @param dict: A dictionary of words dict
+    def wordBreak(self, s, dict):
+        if len(dict) == 0:
+            return len(s) == 0
+            
+        n = len(s)
+        f = [False] * (n + 1)
+        f[0] = True     
+        maxLength = max([len(w) for w in dict])
+        for i in range(1, n + 1):
+            for j in range(1, min(i, maxLength) + 1):
+                if not f[i - j]:
+                    continue
+                if s[i - j:i] in dict:
+                    f[i] = True
+                    break        
+        return f[n]
+
 # V2 
 # Time:  O(n * l^2)
 # Space: O(n)
