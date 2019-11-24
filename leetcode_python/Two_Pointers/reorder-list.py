7767a63e43bcb60febb9033bd512f2f4defb7797
yennanliu <f339339@gmail.com>
2019-11-24 12:10:40 +0800

    update status/code Two_Pointers/reorder-list.py

--- leetcode_python/Two_Pointers/reorder-list.py
+++ leetcode_python/Two_Pointers/reorder-list.py
@@ -1,84 +1,133 @@
+# V0 
 
-# V1 : DEV 
-
-
-
-# V2 
-# https://blog.csdn.net/qqxx6661/article/details/78817958
-"""
-
-PROCESS 
-
-1) SPLIT THE LINKED LIST into 2 sub-list : sub list 1, sub list 2  (from midpint)
-2) reverse the 2nd sub list (sub list 2 )
-3) merge sub list 1 and sub list 2 
-
-
-"""
-class Solution:
-    def reorderList(self, head):
-        """
-        :type head: ListNode
-        :rtype: void Do not return anything, modify head in-place instead.
-        """
-        if not head:
-            return
-        # split {1,2,3,4,5} to {1,2,3}{4,5}
-        fast = slow = head 
-        while fast and fast.__next__:
-            slow = slow.__next__
-            fast = fast.next.__next__
-        head1 = head
-        head2 = slow.__next__
-        slow.next = None
-        # reverse the second {4,5} to {5,4}
-        cur, pre = head2, None
-        while cur:
-            nex = cur.__next__
-            cur.next = pre
-            pre = cur
-            cur = nex
-        # merge
-        cur1, cur2 = head1, pre
-        while cur2:
-            nex1, nex2 = cur1.__next__, cur2.__next__
-            cur1.next = cur2
-            cur2.next = nex1
-            cur1, cur2 = nex1, nex2
-
-
-# V3 
-# class ListNode(object):
+# V1
+# http://bookshadow.com/weblog/2015/01/29/leetcode-reorder-list/
+# Definition for singly-linked list.
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+class Solution:
+    # @param head, a ListNode
+    # @return nothing
+    def reorderList(self, head):
+        if head is None:
+            return head
 
-#     def __repr__(self):
-#         if self:
-#             return "{} -> {}".format(self.val, repr(self.next))
+        #find mid
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow
 
-# class Solution(object):
-#     # @param head, a ListNode
-#     # @return nothing
-#     def reorderList(self, head):
-#         if head == None or head.next == None:
-#             return head
+        #cut in the mid
+        left = head
+        right = mid.next
+        if right is None:
+            return head
+        mid.next = None
 
-#         fast, slow, prev = head, head, None
-#         while fast != None and fast.next != None:
-#             fast, slow, prev = fast.next.next, slow.next, slow
-#         current, prev.next, prev = slow, None, None
+        #reverse right half
+        cursor = right.next
+        right.next = None
+        while cursor:
+            next = cursor.next
+            cursor.next = right
+            right = cursor
+            cursor = next
+        
+        #merge left and right
+        dummy = ListNode(0)
+        while left or right:
+            if left is not None:
+                dummy.next = left
+                left = left.next
+                dummy = dummy.next
+            if right is not None:
+                dummy.next = right
+                right = right.next
+                dummy = dummy.next
+        return head
 
-#         while current != None:
-#             current.next, prev, current = prev, current, current.next
+# V1'
+# https://www.jiuzhang.com/solution/reorder-list/#tag-highlight-lang-python
+from lintcode import ListNode
+# Definition of ListNode
+# class ListNode(object):
+#     def __init__(self, val, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    """
+    @param head: The first node of the linked list.
+    @return: nothing
+    """
+    def reorderList(self, head):
+        # write your code here
+        if None == head or None == head.next:
+            return head
 
-#         l1, l2 = head, prev
-#         dummy = ListNode(0)
-#         current = dummy
+        pfast = head
+        pslow = head
+        
+        while pfast.next and pfast.next.next:
+            pfast = pfast.next.next
+            pslow = pslow.next
+        pfast = pslow.next
+        pslow.next = None
+        
+        pnext = pfast.next
+        pfast.next = None
+        while pnext:
+            q = pnext.next
+            pnext.next = pfast
+            pfast = pnext
+            pnext = q
 
-#         while l1 != None and l2 != None:
-#             current.next, current, l1 = l1, l1, l1.next
-#             current.next, current, l2 = l2, l2, l2.next
+        tail = head
+        while pfast:
+            pnext = pfast.next
+            pfast.next = tail.next
+            tail.next = pfast
+            tail = tail.next.next
+            pfast = pnext
+        return head
 
-#         return dummy.next
+# V2
+# Time:  O(n)
+# Space: O(1)
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, repr(self.next))
+
+class Solution(object):
+    # @param head, a ListNode
+    # @return nothing
+    def reorderList(self, head):
+        if head == None or head.next == None:
+            return head
+
+        fast, slow, prev = head, head, None
+        while fast != None and fast.next != None:
+            fast, slow, prev = fast.next.next, slow.next, slow
+        current, prev.next, prev = slow, None, None
+
+        while current != None:
+            current.next, prev, current = prev, current, current.next
+
+        l1, l2 = head, prev
+        dummy = ListNode(0)
+        current = dummy
+
+        while l1 != None and l2 != None:
+            current.next, current, l1 = l1, l1, l1.next
+            current.next, current, l2 = l2, l2, l2.next
+
+        return dummy.next
