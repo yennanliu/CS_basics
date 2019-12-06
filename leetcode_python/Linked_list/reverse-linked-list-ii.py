7c66023807bb025db0e308a25969224ad18f26cf
yennanliu <f339339@gmail.com>
2019-12-06 15:01:54 +0800

    update status/code

--- leetcode_python/Linked_list/reverse-linked-list-ii.py
+++ leetcode_python/Linked_list/reverse-linked-list-ii.py
@@ -4,6 +4,16 @@
 # http://bookshadow.com/weblog/2015/01/29/leetcode-reverse-linked-list-ii/
 # IDEA : dummyNode
 # Definition for singly-linked list.
+# PATTERN OF REVERSE LINKED LIST
+# def reverse(head):
+#     p = head
+#     start = None
+#     while p
+#         next = p.next
+#         p.next = start
+#         start = p
+#         p = next
+#     return start  
 # class ListNode:
 #    def __init__(self, x):
 #        self.val = x
@@ -35,6 +45,40 @@ class Solution:
         p.next = start
         end.next = next
         return dummyNode.next
+
+# V1'
+# https://www.jiuzhang.com/solution/reverse-linked-list-ii/#tag-highlight-lang-python
+from lintcode import ListNode
+class Solution:
+
+    def reverse(self, head):
+        prev = None
+        while head != None:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
+
+    def findkth(self, head, k):
+        for i in xrange(k):
+            if head is None:
+                return None
+            head = head.next
+        return head
+
+    def reverseBetween(self, head, m, n):
+        dummy = ListNode(-1, head)
+        mth_prev = self.findkth(dummy, m - 1)
+        mth = mth_prev.next
+        nth = self.findkth(dummy, n)
+        nth_next = nth.next
+        nth.next = None
+
+        self.reverse(mth)
+        mth_prev.next = nth
+        mth.next = nth_next
+        return dummy.next
 
 # V2 
 # Time:  O(n)
