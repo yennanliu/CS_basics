a57b006fd9ec247653fb8f1dc21f0b37cbde2dcb
yennanliu <f339339@gmail.com>
2019-12-15 20:47:09 +0800

    update status/code closest-binary-search-tree-value.py

--- leetcode_python/Binary_Search_Tree/closest-binary-search-tree-value.py
+++ leetcode_python/Binary_Search_Tree/closest-binary-search-tree-value.py
@@ -1,4 +1,12 @@
 # V0 
+# KEY : BST PROPERTY : RIGHT > ROOT > LEFT
+class Solution(object):
+    def closestValue(self, root, target):
+        a = root.val
+        kid = root.left if target < a else root.right
+        if not kid: return a
+        b = self.closestValue(kid, target)
+        return min((b, a), key=lambda x: abs(target - x))
 
 # V1 
 # http://www.voidcn.com/article/p-phbluudb-qp.html
@@ -52,10 +60,78 @@ class Solution(object):
             root = root.left if target < root.val else root.right
         return min(path[::-1], key=lambda x: abs(target - x))
 
+# V1'''
+# https://www.jiuzhang.com/solution/closest-binary-search-tree-value/#tag-highlight-lang-python
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        upper = root
+        lower = root
+        while root:
+            if target > root.val:
+                lower = root
+                root = root.right
+            elif target < root.val:
+                upper = root
+                root = root.left
+            else:
+                return root.val
+        if abs(upper.val - target) <= abs(lower.val - target):
+            return upper.val
+        return lower.val
+
+# V1''''
+# https://www.jiuzhang.com/solution/closest-binary-search-tree-value/#tag-highlight-lang-python
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        if root is None:
+            return None
+            
+        lower = self.get_lower_bound(root, target)
+        upper = self.get_upper_bound(root, target)
+        if lower is None:
+            return upper.val
+        if upper is None:
+            return lower.val
+            
+        if target - lower.val < upper.val - target:
+            return lower.val
+        return upper.val
+        
+    def get_lower_bound(self, root, target):
+        # get the largest node that < target
+        if root is None:
+            return None
+        
+        if target < root.val:
+            return self.get_lower_bound(root.left, target)
+            
+        lower = self.get_lower_bound(root.right, target)
+        return root if lower is None else lower
+        
+    def get_upper_bound(self, root, target):
+        # get the smallest node that >= target
+        if root is None:
+            return None
+        
+        if target >= root.val:
+            return self.get_upper_bound(root.right, target)
+            
+        upper = self.get_upper_bound(root.left, target)
+        return root if upper is None else upper
+
 # V2 
 # Time:  O(h)
 # Space: O(1)
-
 class Solution(object):
     def closestValue(self, root, target):
         """
