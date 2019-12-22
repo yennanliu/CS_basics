6de124cc671d0e5fc7725c60bfe981c06f5828eb
yennanliu <f339339@gmail.com>
2019-12-22 21:06:29 +0800

    update status/code moving-average-from-data-stream.py

--- leetcode_python/Queue/moving-average-from-data-stream.py
+++ leetcode_python/Queue/moving-average-from-data-stream.py
@@ -1,24 +1,15 @@
-
 """
-
-
 Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
 
 For example,
-
 MovingAverage m = new MovingAverage(3);
 m.next(1) = 1
 m.next(10) = (1 + 10) / 2
 m.next(3) = (1 + 10 + 3) / 3
 m.next(5) = (10 + 3 + 5) / 3
-
-
 """
 
-
-
-# V1 : dev
- 
+# V0
 from collections import deque
 class MovingAverage(object):
 
@@ -32,14 +23,35 @@ class MovingAverage(object):
         self.__q.append(val)
         return 1.0 * sum(self.__q) / len(self.__q)
 
-
-
+# V1
+# https://www.cnblogs.com/lightwindy/p/9736836.html
+# https://nifannn.github.io/2018/10/01/Algorithm-Notes-Leetcode-346-Moving-Average-from-Data-Stream/
+from collections import deque
+ class MovingAverage(object):
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        :type size: int
+        """
+        self.__size = size
+        self.__sum = 0
+        self.__q = deque([])
+ 
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+        if len(self.__q) == self.__size:
+            self.__sum -= self.__q.popleft()
+        self.__sum += val
+        self.__q.append(val)
+        return 1.0 * self.__sum / len(self.__q)
+ 
 # V2 
 # Time:  O(1)
 # Space: O(w)
-
 from collections import deque
-
 class MovingAverage(object):
 
     def __init__(self, size):
@@ -61,12 +73,6 @@ class MovingAverage(object):
         self.__sum += val
         self.__q.append(val)
         return 1.0 * self.__sum / len(self.__q)
-
-
 # Your MovingAverage object will be instantiated and called as such:
 # obj = MovingAverage(size)
 # param_1 = obj.next(val)
-
-
-
-
