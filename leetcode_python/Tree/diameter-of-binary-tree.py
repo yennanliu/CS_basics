d44dd7ae902e0a7206e3b75db890832c69d23c2a
yennanliu <f339339@gmail.com>
2020-01-01 16:23:31 +0800

    update status/code /diameter-of-binary-tree.py , Continuous Subarray Sum.py

--- leetcode_python/Tree/diameter-of-binary-tree.py
+++ leetcode_python/Tree/diameter-of-binary-tree.py
@@ -1,4 +1,23 @@
 # V0 
+class Solution:
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        self.diameter = 0
+        self.dfs(root)
+        return self.diameter
+        
+    def dfs(self, root):
+        if not root:
+            return 0
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        self.diameter = max(self.diameter, left + right)
+        return 1 + max(left, right)
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/70338312
@@ -26,6 +45,24 @@ class Solution:
         left = self.getDepth(root.left)
         right = self.getDepth(root.right)
         self.diameter = max(self.diameter, left + right)
+        return 1 + max(left, right)
+        
+# V1'
+# https://www.jiuzhang.com/solution/diameter-of-binary-tree/#tag-highlight-lang-python
+class Solution():
+    def __init__(self):
+        self.max = 0
+ 
+    def diameterOfBinaryTree(self, root):
+        self.helper(root)
+        return self.max
+ 
+    def helper(self, root):
+        if not root:
+            return 0
+        left = self.helper(root.left)
+        right = self.helper(root.right)
+        self.max = max(self.max, left + right)
         return 1 + max(left, right)
 
 # V2 
