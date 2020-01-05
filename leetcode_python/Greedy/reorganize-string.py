24971b5dee1d0722bbfd49f806128a7e64ed76fa
yennanliu <f339339@gmail.com>
2020-01-05 16:43:00 +0800

    update status/code /reorganize-string.py

--- leetcode_python/Greedy/reorganize-string.py
+++ leetcode_python/Greedy/reorganize-string.py
@@ -1,4 +1,24 @@
 # V0 
+# IDEA : GREEDY + COUNTER
+class Solution(object):
+    def reorganizeString(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        cnt = collections.Counter(S)
+        ans = '#'
+        while cnt:
+            stop = True
+            for v, c in cnt.most_common():
+                if v != ans[-1]:
+                    stop = False
+                    ans += v
+                    cnt[v] -= 1
+                    if not cnt[v]: del cnt[v]
+                    break
+            if stop: break
+        return ans[1:] if len(ans[1:]) == len(S) else ''
 
 # V1 
 # http://bookshadow.com/weblog/2018/01/21/leetcode-reorganize-string/
@@ -22,7 +42,83 @@ class Solution(object):
                     break
             if stop: break
         return ans[1:] if len(ans) == len(S) + 1 else ''
-        
+
+# V1'
+# https://blog.csdn.net/fuxuemingzhu/article/details/80680454
+class Solution(object):
+    def reorganizeString(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        counter = collections.Counter(S)
+        ans = "#"
+        while counter:
+            stop = True
+            for item, times in counter.most_common():
+                if ans[-1] != item:
+                    ans += item
+                    counter[item] -= 1
+                    if not counter[item]:
+                        del counter[item]
+                    stop = False
+                    break
+            if stop: break
+        return ans[1:] if len(ans) == len(S) + 1 else ""
+
+# V1''
+# https://blog.csdn.net/fuxuemingzhu/article/details/80680454
+class Solution(object):
+    def reorganizeString(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        _len = len(S)
+        count = collections.Counter(S)
+        que = [(-v, c) for c, v in count.items()]
+        heapq.heapify(que)
+        res = ""
+        while _len:
+            cnt = min(_len, 2)
+            temp = list()
+            for i in range(cnt):
+                if not que:
+                    return ""
+                v, c = heapq.heappop(que)
+                res += c
+                if v + 1 != 0:
+                    temp.append((v + 1, c))
+                _len -= 1
+            for x in temp:
+                heapq.heappush(que, x)
+        return res
+
+# V1''
+# https://www.jiuzhang.com/solution/reorganize-string/#tag-highlight-lang-python
+class Solution:
+    """
+    @param S: a string
+    @return: return a string
+    """
+    def reorganizeString(self, S):
+        # write your code here
+        if (S == ""):
+            return ""
+        from collections import Counter
+        counter = Counter(S).most_common() # 1
+        _, max_freq = counter[0]
+        if max_freq > (len(S)+1)//2:
+            return ""
+        else:
+            buckets = [[] for i in range(max_freq)] #2
+            begin = 0
+            for letter, count in counter:
+                for i in range(count):
+                    buckets[(i+begin)%max_freq].append(letter) #3
+                begin += count
+        return "".join("".join(bucket) for bucket in buckets) #4 
+
 # V2 
 # Time:  O(nloga) = O(n), a is the size of alphabet
 # Space: O(a) = O(1)
