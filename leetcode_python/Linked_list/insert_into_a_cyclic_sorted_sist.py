85cb9bd6a1d480f486db540218d1e25d99680547
yennanliu <f339339@gmail.com>
2020-01-06 14:57:31 +0800

    add insert_into_a_cyclic_sorted_sist.py (dev)

---
+++ leetcode_python/Linked_list/insert_into_a_cyclic_sorted_sist.py
@@ -0,0 +1,31 @@
+# V0
+
+# V1
+# https://blog.csdn.net/weixin_41677877/article/details/81200818
+class Solution:
+    def insert(self, node, x):
+        # write your code 
+        originNode = node
+        tmp = ListNode(x)
+        if node == None:
+            node = tmp
+            node.next = node
+            return node
+        else:
+            while True:
+                if node.next.next == node:
+                    tmp.next = node.next
+                    node.next = tmp
+                    return node
+                if (node.val<=x and node.next.val>x) or (node.val<x and node.next.val>=x) or (node.val>node.next.val and node.val<x and node.next.val<x) or (node.val>node.next.val and node.val>x and node.next.val>x):
+                    tmp.next = node.next
+                    node.next = tmp
+                    return node
+                node = node.next
+                if node == originNode:
+                    tmp.next = node.next
+                    node.next = tmp
+                    return node
+
+# V2
+
