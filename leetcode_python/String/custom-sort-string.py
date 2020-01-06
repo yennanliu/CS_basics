3fbac77f9bdfd500aae6b6eaceca1f85f9c70de6
yennanliu <f339339@gmail.com>
2020-01-06 14:40:59 +0800

    update status/code custom-sort-string.py

--- leetcode_python/String/custom-sort-string.py
+++ leetcode_python/String/custom-sort-string.py
@@ -1,4 +1,36 @@
 # V0 
+# IDEA : COUNTER 
+from collections import Counter
+class Solution(object):
+    def customSortString(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: str
+        """
+        count = Counter(T)
+        answer = ''
+        for s in S:
+            answer += s * count[s]
+            count[s] = 0
+        for c in count:
+            answer += c * count[c]
+        return answer
+        
+# V0'
+# IDEA : COUNTER + SORT KEY LAMBDA 
+class Solution(object):
+    def customSortString(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: str
+        """
+        pos = collections.defaultdict(int)
+        for i in range(len(S)):
+            pos[S[i]] = i
+        res = sorted(T, key = lambda x : pos[x])
+        return "".join(res)
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79378688
@@ -35,6 +67,29 @@ class Solution(object):
             pos[S[i]] = i
         res = sorted(T, key = lambda x : pos[x])
         return "".join(res)
+
+# V1''
+# https://www.jiuzhang.com/solution/custom-sort-string/#tag-highlight-lang-python
+class Solution:
+    """
+    @param S: The given string S
+    @param T: The given string T
+    @return: any permutation of T (as a string) that satisfies this property
+    """
+    def customSortString(self, S, T):
+        # Write your code here
+        res = ""
+        cnt, t = [0] * 26, 0
+        for i in range(0, len(T)):
+            cnt[ord(T[i]) - ord('a')] += 1
+        for i in range(0, len(S)):
+            for j in range(0, cnt[ord(S[i]) - ord('a')]):
+                res += S[i]
+            cnt[ord(S[i]) - ord('a')] = 0
+        for i in range(0, 26):
+            for j in range(0, cnt[i]):
+                res += chr(ord('a') + i)
+        return res
 
 # V2 
 # Time:  O(n)
