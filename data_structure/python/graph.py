f650d61db407c8bbc34f72cd3be9e2c61a1779ab
yennanliu <f339339@gmail.com>
2020-01-08 14:18:04 +0800

    update data_structure graph.py

--- data_structure/python/graph.py
+++ data_structure/python/graph.py
@@ -1,10 +1,60 @@
-
 #################################################################
 # DATA STRUCTURE DEMO : Graph 
 #################################################################
 
-# https://github.com/OmkarPathak/Data-Structures-using-Python/blob/master/Graph/Graph.py
+# V0
+# https://github.com/yennanliu/CS_basics/blob/master/data_structure/js/graph.js
+class Graph(object):
 
+    def __init__(self):
+        self.numberOfNodes = 0
+        self.adjacentList = {}
+
+    def addVertex(self, node):
+        self.adjacentList[node] = []
+        self.numberOfNodes += 1 
+
+    def addEdge(self, node1, node2):
+        self.adjacentList[node1].append(node2)
+        self.adjacentList[node2].append(node1)
+
+    def showConnections(self):
+        allNodes = self.adjacentList.keys()
+        for node in allNodes:
+            nodeConnections = self.adjacentList[node]
+            connections = ""
+            for vertex in nodeConnections:
+                connections += vertex + " "
+            print (node + "-->" + connections)
+            
+# myGraph = Graph();
+# myGraph.addVertex('0');
+# myGraph.addVertex('1');
+# myGraph.addVertex('2');
+# myGraph.addVertex('3');
+# myGraph.addVertex('4');
+# myGraph.addVertex('5');
+# myGraph.addVertex('6');
+# myGraph.addEdge('3', '1'); 
+# myGraph.addEdge('3', '4'); 
+# myGraph.addEdge('4', '2'); 
+# myGraph.addEdge('4', '5'); 
+# myGraph.addEdge('1', '2'); 
+# myGraph.addEdge('1', '0'); 
+# myGraph.addEdge('0', '2'); 
+# myGraph.addEdge('6', '5');
+# myGraph.showConnections(); 
+# OUTPUT:
+# 0-->1 2 
+# 1-->3 2 0 
+# 2-->4 1 0 
+# 3-->1 4 
+# 4-->3 2 5 
+# 5-->4 6 
+# 6-->5 
+
+# V1
+# https://github.com/OmkarPathak/Data-Structures-using-Python/blob/master/Graph/Graph.py
 class AdjacencyList(object):
     def __init__(self):
         self.List = {}
@@ -20,24 +70,22 @@ class AdjacencyList(object):
         for i  in self.List:
             print((i,'->',' -> '.join([str(j) for j in self.List[i]])))
 
-if __name__ == '__main__':
-    al = AdjacencyList()
-    al.addEdge(0, 1)
-    al.addEdge(0, 4)
-    al.addEdge(4, 1)
-    al.addEdge(4, 3)
-    al.addEdge(1, 0)
-    al.addEdge(1, 4)
-    al.addEdge(1, 3)
-    al.addEdge(1, 2)
-    al.addEdge(2, 3)
-    al.addEdge(3, 4)
+# al = AdjacencyList()
+# al.addEdge(0, 1)
+# al.addEdge(0, 4)
+# al.addEdge(4, 1)
+# al.addEdge(4, 3)
+# al.addEdge(1, 0)
+# al.addEdge(1, 4)
+# al.addEdge(1, 3)
+# al.addEdge(1, 2)
+# al.addEdge(2, 3)
+# al.addEdge(3, 4)
+# al.printList()
 
-    al.printList()
-
-    # OUTPUT:
-    # 0 -> 1 -> 4
-    # 1 -> 0 -> 4 -> 3 -> 2
-    # 2 -> 3
-    # 3 -> 4
-    # 4 -> 1 -> 3
+# # OUTPUT:
+# # 0 -> 1 -> 4
+# # 1 -> 0 -> 4 -> 3 -> 2
+# # 2 -> 3
+# # 3 -> 4
+# # 4 -> 1 -> 3
