7b1f834785d2a51b8c2b27b8b4c81f931033bcc7
yennanliu <f339339@gmail.com>
2020-01-17 12:45:29 +0800

    update status/code count-and-say.py

--- leetcode_python/String/count-and-say.py
+++ leetcode_python/String/count-and-say.py
@@ -13,22 +13,60 @@
 #
 
 """
-
 i.e. 
 
-n = 1  ->  1   
-n = 2  ->  11       ( there is 1 "1" when n=1 )
-n = 3  ->  21       ( there is 2 "1" when n=2 )
-n = 4  ->  1211     ( there is 1 "2" and 1 "1"  when n= 3 )
-n = 5  ->  111221   ( there is 1 "1" and 1 "2" and  2 "1"  when n=4 )
-n = 6  ->  312211   ( there is 3 "1" and 2 "2" and 1 "1" when n = 5  )
-.
-.
-.
+n = 1  ->  1        ( 1, when n = 1 )
+n = 2  ->  11       ( there is 1 "1" when n = 2 )
+n = 3  ->  21       ( there is 2 "1" when n = 3 )
+n = 4  ->  1211     ( there is 1 "2" and 1 "1"  when n= 4 )
+n = 5  ->  111221   ( there is 1 "1" and 1 "2" and  2 "1"  when n = 5)
+n = 6  ->  312211   ( there is 3 "1" and 2 "2" and 1 "1" when n = 6 )
 
+.
+.
+.
 """
 
 # V0 
+# IDEA : RECURSION
+# for example : 
+# n = 4 --> s = self.countAndSay(3) 
+#           while n = 3 --> s = self.countAndSay(2)
+#                               while n = 2 --> s = self.countAndSay(1)
+#                                       while n = 1 --> 1
+
+# DEMO 
+# s :  1
+# res :  11
+# s :  11
+# res :  21
+# s :  21
+# res :  1211
+# s :  1211
+# res :  111221
+# s :  111221
+# res :  312211
+# s :  312211
+# res :  13112221
+# s :  13112221
+# res :  1113213211
+# s :  1113213211
+# res :  31131211131221
+# s :  31131211131221
+# res :  13211311123113112211
+# 13211311123113112211
+class Solution(object):
+    def countAndSay(self, n):  # recursion 
+        if n == 1: return '1'  # end loop of recursion 
+        s = self.countAndSay(n-1)
+        res, count = '', 0
+        for i in range(len(s)):
+            count += 1
+            if i == len(s) - 1 or s[i] != s[i+1]:  # end before meet the last string element 
+                res += str(count)
+                res += s[i]
+                count = 0
+        return res
 
 
 # V1 
