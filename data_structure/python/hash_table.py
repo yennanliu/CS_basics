5fe8504de13a222a8993c3197156779cabb7cbb6
yennanliu <f339339@gmail.com>
2020-01-18 14:52:40 +0800

    fix data_structure/python/hash_table.py and its  README ref

--- data_structure/python/hash_table.py
+++ data_structure/python/hash_table.py
@@ -2,50 +2,96 @@
 # # DATA STRUCTURE DEMO : Hash table 
 # #################################################################
 
-# # V0 : DEV 
+# # V0 
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        
+class HashTable:
+    def __init__(self, init_size, hash=hash):
+        self.__slot = [[] for _ in range(init_size)]
+        self.__size = init_size
+        self.hash = hash
+ 
+    def put(self, key, value):
+        node = Node(key, value)
+        # get hashmap index (address) via hash func
+        address = self.hash(node.key) % self.__size
+        self.__slot[address].append(node)
+ 
+    def get(self, key):
+    	# get key via hash func
+        _key = self.hash(key)
+        address = _key % self.__size
+        for node in self.__slot[address]:
+            if node.key == key:
+                return node.value
+        return None
+ 
+    def remove(self, key):
+        # get hashmap index (address) via hash func
+        address = self.hash(key) % self.__size
+        for idx, node in enumerate(self.__slot[address].copy()):
+            if node.key == key:
+                self.__slot[address].pop(idx)             
+# map = HashTable(5)
+# print (map)
+# print ('*** put into hashmap')
+# for i in range(5):
+#     map.put(i, i)
+# print ('*** get from hashmap')
+# for i in range(5):
+#     print (map.get(i)) 
+# print ('*** remove from hashmap')
+# for i in range(5):
+#     (map.remove(i))
+# print ('*** get from hashmap')
+# for i in range(5):
+#     print (map.get(i)) 
 
-# # V1 
-# # http://zhaochj.github.io/2016/05/16/2016-05-16-%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84-hash/
-# class Node:
-#     def __init__(self, key, value):
-#         self.key = key
-#         self.value = value
+# V1 
+# http://zhaochj.github.io/2016/05/16/2016-05-16-%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84-hash/
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+class HashTable:
+    def __init__(self, init_size, hash=hash):
+        self.__slot = [[] for _ in range(init_size)]
+        #self.__slot = []
+        #for _ in range(init_size):
+        #   self.__slot.append([])
+        self.__size = init_size
+        self.hash = hash
  
-# class Map:
-#     def __init__(self, init_size, hash=hash):
-#         self.__slot = [[] for _ in range(init_size)]
-#         #self.__slot = []
-#         #for _ in range(init_size):
-#         #   self.__slot.append([])
-#         self.__size = init_size
-#         self.hash = hash
+    def put(self, key, value):
+        node = Node(key, value)
+        address = self.hash(node.key) % self.__size
+        self.__slot[address].append(node)
  
-#     def put(self, key, value):
-#         node = Node(key, value)
-#         address = self.hash(node.key) % self.__size
-#         self.__slot[address].append(node)
+    def get(self, key, default=None):
+        _key = self.hash(key)
+        address = _key % self.__size
+        for node in self.__slot[address]:
+            if node.key == key:
+                return node.value
+        return default
  
-#     def get(self, key, default=None):
-#         _key = self.hash(key)
-#         address = _key % self.__size
-#         for node in self.__slot[address]:
-#             if node.key == key:
-#                 return node.value
-#         return default
+    def remove(self, key):
+        address = self.hash(key) % self.__size
+        for idx, node in enumerate(self.__slot[address].copy()):
+            if node.key == key:
+                self.__slot[address].pop(idx)
  
-#     def remove(self, key):
-#         address = self.hash(key) % self.__size
-#         for idx, node in enumerate(self.__slot[address].copy()):
-#             if node.key == key:
-#                 self.__slot[address].pop(idx)
- 
-# # if __name__ == '__main__':
-# #     map = Map(16)
-# #     for i in range(5):
-# #         map.put(i, i)
-# #     map.remove(3)
-# #     for i in range(5):
-# #         print(map.get(i, 'not set'))
+
+# map = HashTable(16)
+# for i in range(5):
+#     map.put(i, i)
+# map.remove(3)
+# for i in range(5):
+#     print(map.get(i, 'not set'))
 
 
 # # V2 
