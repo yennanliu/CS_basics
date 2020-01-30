275f8d25d7520dbe4556ea996c9f94ba2cdc8511
yennanliu <f339339@gmail.com>
2020-01-30 15:08:02 +0800

    fix code layout (insertion_sort.py)

--- algorithm/python/insertion_sort.py
+++ algorithm/python/insertion_sort.py
@@ -2,17 +2,16 @@
 # Function to do insertion sort 
 def insertionSort(arr): 
 
-	# Traverse through 1 to len(arr) 
-	for i in range(1, len(arr)): 
-		key = arr[i] 
-		j = i-1
-		while j >=0 and key < arr[j] : 
-				arr[j+1] = arr[j] 
-				j -= 1
-		arr[j+1] = key 
-	return arr
-	
-# Driver code to test above 
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+        key = arr[i] 
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+    return arr
+    
 arr = [12, 11, 13, 5, 6] 
 r = insertionSort(arr) 
 print ("Sorted array is:", r) 
