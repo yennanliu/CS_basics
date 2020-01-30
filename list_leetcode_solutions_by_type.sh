d18a88661e256eba1aa554fb8fc3984e4ba02332
yennanliu <f339339@gmail.com>
2020-01-30 15:08:27 +0800

    add script list leetcode solution scripts and move legacy one to archived

--- list_leetcode_solutions_by_type.sh
+++ list_leetcode_solutions_by_type.sh
@@ -1,1 +1,12 @@
-ls -lR leetcode_python
+get_brief_leetcode_solution_list(){
+
+    ls -lR leetcode_python | egrep "leetcode_python/|total "
+}
+
+get_leetcode_solution_list(){
+    
+    ls -lR leetcode_python 
+}
+
+
+get_brief_leetcode_solution_list
