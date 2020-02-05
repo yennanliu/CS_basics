dfd82369cd91f8818af021e43faf0185de897dc8
yennanliu <f339339@gmail.com>
2020-02-05 17:36:17 +0800

    update status/code course-schedule.py

--- leetcode_python/Breadth-First-Search/course-schedule.py
+++ leetcode_python/Breadth-First-Search/course-schedule.py
@@ -1,4 +1,29 @@
 # V0
+# IDEA : DFS + topological sort 
+import collections
+class Solution(object):
+    def canFinish(self, N, prerequisites):
+        graph = collections.defaultdict(list)
+        for u, v in prerequisites:
+            graph[u].append(v)
+        # 0 = Unknown, 1 = visiting, 2 = visited
+        visited = [0] * N
+        for i in range(N):
+            if not self.dfs(graph, visited, i):
+                return False
+        return True
+        
+    # Can we add node i to visited successfully?
+    def dfs(self, graph, visited, i):
+        # 0 = Unknown, 1 = visiting, 2 = visited
+        if visited[i] == 1: return False
+        if visited[i] == 2: return True
+        visited[i] = 1
+        for j in graph[i]:
+            if not self.dfs(graph, visited, j):
+                return False
+        visited[i] = 2
+        return True
 
 # V1 
 # https://www.jiuzhang.com/solution/course-schedule/#tag-highlight-lang-python
@@ -53,6 +78,7 @@ class Solution(object):
         
     # Can we add node i to visited successfully?
     def dfs(self, graph, visited, i):
+        # 0 = Unknown, 1 = visiting, 2 = visited
         if visited[i] == 1: return False
         if visited[i] == 2: return True
         visited[i] = 1
