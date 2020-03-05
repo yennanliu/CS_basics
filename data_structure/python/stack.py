36655cd632de31c0f9b58afb23a72384a3199cf6
yennanliu <f339339@gmail.com>
2020-03-05 19:42:21 +0800

    update code (data_structure/python/stack.py), progress

--- data_structure/python/stack.py
+++ data_structure/python/stack.py
@@ -1,10 +1,48 @@
-
 #################################################################
 # DATA STRUCTURE DEMO : Stack
 #################################################################
 
-# https://github.com/OmkarPathak/Data-Structures-using-Python/blob/master/Stack/Stack.py
+# V0
+class Stack(object):
+    def __init__(self, limit = 10):
+        self.stack = []
+        self.limit = limit
 
+    # for printing the stack contents
+    def __str__(self):
+        return ' '.join([str(i) for i in self.stack])
+
+    # for pushing an element on to the stack
+    def push(self, data):
+        if len(self.stack) >= self.limit:
+            print('Stack Overflow')
+        else:
+            self.stack.append(data)
+
+    # for popping the uppermost element
+    def pop(self):
+        if len(self.stack) <= 0:
+            return -1
+        else:
+            return self.stack.pop()
+
+    # for peeking the top-most element of the stack
+    def peek(self):
+        if len(self.stack) <= 0:
+            return -1
+        else:
+            return self.stack[len(self.stack) - 1]
+
+    # to check if stack is empty
+    def isEmpty(self):
+        return self.stack == []
+
+    # for checking the size of stack
+    def size(self):
+        return len(self.stack)
+        
+# V1
+# https://github.com/OmkarPathak/Data-Structures-using-Python/blob/master/Stack/Stack.py
 class Stack(object):
     def __init__(self, limit = 10):
         self.stack = []
@@ -43,15 +81,14 @@ class Stack(object):
     def size(self):
         return len(self.stack)
 
-if __name__ == '__main__':
-    myStack = Stack()
-    for i in range(10):
-        myStack.push(i)
-    print(myStack)          # show all elements in the Stack  (method 1 )
-    myStack.pop()           # popping the top element
-    print(myStack)
-    myStack.peek()          # printing the top element
-    myStack.isEmpty()
-    myStack.size() 
-    myStack.__str__()       # show all elements in the Stack  (method 2 )
-
+# if __name__ == '__main__':
+#     myStack = Stack()
+#     for i in range(10):
+#         myStack.push(i)
+#     print(myStack)          # show all elements in the Stack  (method 1 )
+#     myStack.pop()           # popping the top element
+#     print(myStack)
+#     myStack.peek()          # printing the top element
+#     myStack.isEmpty()
+#     myStack.size() 
+#     myStack.__str__()       # show all elements in the Stack  (method 2 )
