c12851dd15343933a9484ddb40ee8ccf9d25e320
yennanliu <f339339@gmail.com>
2020-03-11 11:49:15 +0800

    update status/code (sparse-matrix-multiplication.py), progress

--- leetcode_python/Array/sparse-matrix-multiplication.py
+++ leetcode_python/Array/sparse-matrix-multiplication.py
@@ -1,3 +1,27 @@
+"""
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+"""
+
 # V0 
 # TODO : OPTIMIZE THE PROCESS DUE TO THE SPARSE-MATRIX CONDITION 
 class Solution(object):
@@ -56,4 +80,3 @@ class Solution(object):
                     for j in range(l):
                         res[i][j] += A[i][k] * B[k][j]
         return res
-
