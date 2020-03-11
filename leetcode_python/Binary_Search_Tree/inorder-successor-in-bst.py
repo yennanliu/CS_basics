88bd8f2478f6046086e195efbb179452681958e5
yennanliu <f339339@gmail.com>
2020-03-11 11:22:53 +0800

    update status/code (inorder-successor-in-bst.py), progress

--- leetcode_python/Binary_Search_Tree/inorder-successor-in-bst.py
+++ leetcode_python/Binary_Search_Tree/inorder-successor-in-bst.py
@@ -1,22 +1,43 @@
+"""
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+Note: If the given node has no in-order successor in the tree, return null.
+
+"""
+
 # V0 
+# IDEA : DFS
+class Solution(object):
+    def inorderSuccessor(self, root, p):         
+        def inOrder(root):
+            if not root:
+                return
+            inOrder(root.left)
+            l.append(root)
+            inOrder(root.right)
+            
+        l = []
+        inOrder(root)
+        for i in range(len(l)):
+            if l[i] == p:
+                return l[i+1] if i+1 < len(l) else None
 
 # V1 
 # https://blog.csdn.net/danspace1/article/details/86667504
+# IDEA : DFS
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution(object):
     def inorderSuccessor(self, root, p):
         """
         :type root: TreeNode
         :type p: TreeNode
         :rtype: TreeNode
-        """       
-        
+        """           
         def inOrder(root):
             if not root:
                 return
@@ -64,7 +85,6 @@ class Solution(object):
             while p.left:
                 p = p.left
             return p
-
         # Search from root.
         successor = None
         while root and root != p:
@@ -73,5 +93,4 @@ class Solution(object):
                 root = root.left
             else:
                 root = root.right
-
         return successor
