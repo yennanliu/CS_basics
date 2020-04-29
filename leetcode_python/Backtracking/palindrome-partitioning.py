d1fb1c5f29c1e6ce7bf8437af6ab54f8ed73c088
yennanliu <f339339@gmail.com>
2020-04-29 11:14:32 +0800

    update status/code (palindrome-partitioning.py), progress, README

--- leetcode_python/Backtracking/palindrome-partitioning.py
+++ leetcode_python/Backtracking/palindrome-partitioning.py
@@ -1,7 +1,33 @@
 # V0 
+class Solution(object):
+    def partition(self, s):
+        res = []
+        self.helper(s, res, [])
+        return res
+        
+    def helper(self, s, res, path):
+        if not s:
+            res.append(path)
+            return
+        # beware of the start and the end index
+        for i in range(1, len(s) + 1): 
+            if self.isPalindrome(s[:i]):
+                self.helper(s[i:], res, path + [s[:i]])
+
+    def isPalindrome(self, x):
+        return x == x[::-1]
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79574462
+# IDEA : BACKTRACKING 
+# NOTE :
+# In [12]: x=[1,2,3]
+# In [13]: for i in range(1,len(x)+1):
+#     ...:     print (i)
+#     ...:     
+# 1
+# 2
+# 3
 class Solution(object):
     def partition(self, s):
         """
@@ -17,9 +43,75 @@ class Solution(object):
         if not s:
             res.append(path)
             return
-        for i in range(1, len(s) + 1): # beware of the start and the end index
+        # beware of the start and the end index
+        for i in range(1, len(s) + 1): 
             if self.isPalindrome(s[:i]):
                 self.helper(s[i:], res, path + [s[:i]])
+
+### Test case:
+s=Solution()
+assert s.partition("aab")==[['a', 'a', 'b'], ['aa', 'b']]
+assert s.partition("")==[[]]
+assert s.partition("a")==[['a']]
+assert s.partition("ab")==[['a', 'b']]
+assert s.partition(" b")==[[' ', 'b']]
+assert s.partition("abc")==[['a', 'b', 'c']]
+assert s.partition("abcbc")==[['a', 'b', 'c', 'b', 'c'], ['a', 'b', 'cbc'], ['a', 'bcb', 'c']]
+assert s.partition("aaa")==[['a', 'a', 'a'], ['a', 'aa'], ['aa', 'a'], ['aaa']]
+
+# V1'
+# https://leetcode.com/problems/palindrome-partitioning/discuss/42100/Python-easy-to-understand-backtracking-solution.
+# IDEA : DFS + backtracking
+class Solution(object):
+    def partition(self, s):
+        res = []
+        self.dfs(s, [], res)
+        return res
+        
+    def dfs(self, s, path, res):
+        if not s: # backtracking
+            res.append(path)
+        for i in range(1, len(s)+1):
+            if self.isPar(s[:i]):
+                self.dfs(s[i:], path+[s[:i]], res)
+                
+    def isPar(self, s):
+        return s == s[::-1]
+
+# V1''
+# https://leetcode.com/problems/palindrome-partitioning/discuss/441529/backtracking-python
+# IDEA : BACKTRACKING + DFS + DP
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        dp = [[False for _ in range(len(s))] for _ in range(len(s))]
+        for j in range(len(s)):
+            dp[j][j] = True
+            for i in range(j):
+                if s[i]==s[j]:
+                    dp[i][j] = (j-i==1) or (dp[i+1][j-1])
+    
+        res = []
+        def dfs(start, curpath):
+            if start>=len(s):
+                res.append(curpath)
+            for i in range(start, len(s)):
+                if dp[start][i] is True:
+                    dfs(i+1, curpath+[s[start:i+1]])
+        dfs(0, [])
+        return res
+
+# V1'''
+# https://leetcode.com/problems/palindrome-partitioning/discuss/42025/1-liner-Python-Ruby
+class Solution(object):
+    def partition(self, s):
+        return [[s[:i]] + rest
+            for i in range(1, len(s)+1)
+            if s[:i] == s[i-1::-1]
+            for rest in self.partition(s[i:])] or [[]]
 
 # V2 
 # Time:  O(n^2 ~ 2^n)
