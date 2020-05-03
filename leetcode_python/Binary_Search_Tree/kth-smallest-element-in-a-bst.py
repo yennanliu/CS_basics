e0facc60f27f2d4aa1ad02e19a9e8c4fa43f641a
yennanliu <f339339@gmail.com>
2020-05-04 00:30:36 +0800

    update status/code (/kth-smallest-element-in-a-bst.py), progress, README

--- leetcode_python/Binary_Search_Tree/kth-smallest-element-in-a-bst.py
+++ leetcode_python/Binary_Search_Tree/kth-smallest-element-in-a-bst.py
@@ -1,4 +1,38 @@
+# Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+#
+# Note: 
+#
+# You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+#
+# Follow up:
+#
+# What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+#
+# Hint:
+#
+# Try to utilize the property of a BST.
+#
+# What if you could modify the BST node's structure?
+#
+# The optimal runtime complexity is O(height of BST).
+
 # V0 
+class Solution(object):
+    def kthSmallest(self, root, k):
+        self.k = k
+        self.res = None
+        self.dfs(root)
+        return self.res
+
+    def dfs(self, node):
+        if not node:
+            return
+        self.dfs(node.left)
+        self.k -= 1
+        if self.k == 0:
+            self.res = node.val
+            return
+        self.dfs(node.right)
 
 # V1 
 # http://bookshadow.com/weblog/2015/07/02/leetcode-kth-smallest-element-bst/
@@ -9,7 +43,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution:
     # @param {TreeNode} root
     # @param {integer} k
@@ -30,10 +63,97 @@ class Solution:
                 right = right.left
         return node.val
 
+### Test case
+# dev
+
+# V1'
+# IDEA : Recursive 
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/discuss/63829/Python-Easy-Iterative-and-Recursive-Solution
+class Solution(object):
+    def kthSmallest(self, root, k):
+        self.k = k
+        self.res = None
+        self.dfs(root)
+        return self.res
+
+    def dfs(self, node):
+        if not node:
+            return
+        self.dfs(node.left)
+        self.k -= 1
+        if self.k == 0:
+            self.res = node.val
+            return
+        self.dfs(node.right)
+
+# V1'''
+# IDEA : Iterative 
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/discuss/63829/Python-Easy-Iterative-and-Recursive-Solution
+class Solution(object):
+    def kthSmallest(self,root, k):
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+
+# V1'''''
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/solution/
+# IDEA : RECURSION 
+#Time complexity : O(N), to build a traversal.
+# Space complexity : O(N) to keep an inorder traversal.
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+    
+        return inorder(root)[k - 1]
+
+# V1''''''
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/solution/
+# IDEA : ITERATION
+# Time complexity : O(H+k), where HH is a tree height. This complexity is defined by the stack,
+# Space complexity : O(H+k), the same as for time complexity, O(N+k) in the worst case, and O(logN+k) in the average case.
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
+
+# V1''''''''
+# https://blog.csdn.net/zhangpeterx/article/details/102879948
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []    
+        return inorder(root)[k - 1]
+
 # V2 
 # Time:  O(max(h, k))
 # Space: O(h)
-
 class Solution(object):
     # @param {TreeNode} root
     # @param {integer} k
@@ -54,13 +174,9 @@ class Solution(object):
 
         return float("-inf")
 
-
 # time: O(max(h, k))
 # space: O(h)
-
 from itertools import islice
-
-
 class Solution2(object):
     def kthSmallest(self, root, k):
         """
@@ -77,5 +193,4 @@ class Solution2(object):
 
                 for n in gen_inorder(root.right):
                     yield n
-
         return next(islice(gen_inorder(root), k-1, k))
