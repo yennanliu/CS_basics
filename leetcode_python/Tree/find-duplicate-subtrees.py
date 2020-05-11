0f8d0394330322dbdf484424fdb492bf8cb99da0
yennanliu <f339339@gmail.com>
2020-05-11 11:13:10 +0800

    update status/code  (find-duplicate-subtrees.py), progress, REAMDE, add v

--- leetcode_python/Tree/find-duplicate-subtrees.py
+++ leetcode_python/Tree/find-duplicate-subtrees.py
@@ -1,14 +1,80 @@
 # V0
+# IDEA : DFS
+# DEMO : defaultdict
+# In [26]: import collections 
+# In [27]: m = collections.defaultdict(int)
+#
+# In [28]: m
+# Out[28]: defaultdict(int, {})
+#
+# In [29]: m[0]
+# Out[29]: 0
+#
+# In [30]: m
+# Out[30]: defaultdict(int, {0: 0})
+#
+# In [31]: m[1]
+# Out[31]: 0
+#
+# In [32]: m
+# Out[32]: defaultdict(int, {0: 0, 1: 0})
+#
+# In [33]: m[1]=1
+#
+# In [34]: m
+# Out[34]: defaultdict(int, {0: 0, 1: 1})
+#
+# In [35]: m[2] +=1
+#
+# In [36]: m
+# Out[36]: defaultdict(int, {0: 0, 1: 1, 2: 1})
+import collections
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        res = []
+        m = collections.defaultdict(int)
+        self.dfs(root, m, res)
+        return res
+
+    def dfs(self, root, m, res):
+        if not root:
+            return '#'
+        path = str(root.val) + '-' + self.dfs(root.left, m, res) + '-' + self.dfs(root.right, m, res)
+        ### Notice here
+        # if m[path] == 1 means if there is already one element in the m 
+        #   -> so res append the path  (if m[path] == 1)
+        # becasue it means "deplicate"
+        if m[path] == 1:
+            res.append(root) 
+        m[path] += 1
+        return path
+
+# V0'
+# IDEA : DFS 
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        count = collections.Counter()
+        ans = []
+        def dfs(node):
+            if not node: return "#"
+            serial = "{}-{}-{}".format(node.val, dfs(node.left), dfs(node.right))
+            count[serial] += 1
+            if count[serial] == 2:
+                ans.append(node)
+            return serial
+
+        dfs(root)
+        return ans
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/81053453
+# IDEA : DFS
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution(object):
     def findDuplicateSubtrees(self, root):
         """
@@ -24,11 +90,54 @@ class Solution(object):
         if not root:
             return '#'
         path = str(root.val) + ',' + self.helper(root.left, m, res) + ',' + self.helper(root.right, m, res)
+        ### Notice here
+        # if m[path] == 1 means if there is already one element in the m 
+        #   -> so res append the path  (if m[path] == 1)
+        # becasue it means "deplicate"
         if m[path] == 1:
             res.append(root) 
         m[path] += 1
         return path
-        
+
+### Test case : dev 
+
+# V1'
+# IDEA : DFS 
+# https://leetcode.com/problems/find-duplicate-subtrees/solution/
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        count = collections.Counter()
+        ans = []
+        def collect(node):
+            if not node: return "#"
+            serial = "{},{},{}".format(node.val, collect(node.left), collect(node.right))
+            count[serial] += 1
+            if count[serial] == 2:
+                ans.append(node)
+            return serial
+
+        collect(root)
+        return ans
+     
+# V1''
+# IDEA :  Unique Identifier
+# https://leetcode.com/problems/find-duplicate-subtrees/solution/
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        trees = collections.defaultdict()
+        trees.default_factory = trees.__len__
+        count = collections.Counter()
+        ans = []
+        def lookup(node):
+            if node:
+                uid = trees[node.val, lookup(node.left), lookup(node.right)]
+                count[uid] += 1
+                if count[uid] == 2:
+                    ans.append(node)
+                return uid
+        lookup(root)
+        return ans
+
 # V2 
 # Time:  O(n)
 # Space: O(n)
@@ -52,7 +161,6 @@ class Solution(object):
         getid(root, lookup, trees)
         return [roots[0] for roots in trees.values() if len(roots) > 1]
 
-
 # Time:  O(n * h)
 # Space: O(n * h)
 class Solution2(object):
@@ -72,7 +180,6 @@ class Solution2(object):
                 result.append(node)
             lookup[s] += 1
             return s
-
         lookup = collections.defaultdict(int)
         result = []
         postOrderTraversal(root, lookup, result)
