e3b2888ce8e6242dba3b006af552bffa1e90e307
yennanliu <f339339@gmail.com>
2020-05-11 18:19:41 +0800

    add get_leetcode_fib_sequence, get_reivew_list func for process leetocode-id - date dict and output to-review list

--- script/get_review_list.py
+++ script/get_review_list.py
@@ -4,12 +4,12 @@ by mnemonic learning with fibonacci sequence
 """
 
 from datetime import datetime, timedelta, date
+import collections
 
 class GetReviewList:
 
     def __init__(self):
-        self.my_list=[]
-        self.my_dict=[]
+        self.review_list=collections.defaultdict(list)
         self.current_date=datetime.now()
 
     def process_datetime(self, datestring):
@@ -45,19 +45,36 @@ class GetReviewList:
             result.append(self.fib(i))
         return list(result)
 
-    def get_review_list(self):
+    def get_leetcode_fib_sequence(self):
+        """
+        get "leetcode id - date dict based on fibonacci sequence
+        """
+        progress="data/progress.txt"
+        tmp="data/tmp.txt"
+        fib_seq=self.get_fib_10()
+        with open(progress, 'r') as f,\
+             open(tmp, 'w') as f_tmp:
+            for line in f:
+                if line:
+                    date, lc_ids= line.split(":")[0], str(line.split(":")[1].strip())
+                    date_fix=self.process_datetime(date)
+                    sequence_date= [(date_fix + timedelta(days=day)).strftime('%Y-%m-%d') for day in fib_seq]
+                    data="{} : {}\n".format(lc_ids, sequence_date)
+                    print (data)
+                    f_tmp.writelines(data)
+                    self.review_list[lc_ids]=sequence_date
+
+    def get_reivew_list(self):
         """
         generate the to-review list based on fibonacci sequence and progress.txt
         """
-        filename="data/progress.txt"
-        fib_seq=self.get_fib_10()
-        with open(filename, 'r') as f:
-            for line in f:
-                if line:
-                    date, lc_ids= line.split(":")[0], line.split(":")[1]
-                    date_fix=self.process_datetime(date)
-                    print (date_fix.strftime('%Y-%m-%d'), [ (date_fix + timedelta(days=day)).strftime('%Y-%m-%d') for day in fib_seq])
+        self.get_leetcode_fib_sequence()
+        print ("*"*30)
+        print (self.review_list)
+        print (self.review_list.keys())
+        print ("*"*30)
+
 
 if __name__ == '__main__':
     g=GetReviewList()
-    g.get_review_list()
+    g.get_reivew_list()
