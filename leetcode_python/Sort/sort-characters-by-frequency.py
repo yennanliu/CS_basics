50c25accf9cf77384041522ff7dc1e79ce2b2d9d
yennanliu <f339339@gmail.com>
2020-05-24 13:07:49 +0800

    update status/code Sort/sort-characters-by-frequency.py, update progress, README,  add cheatsheet/Collection.md

--- leetcode_python/Sort/sort-characters-by-frequency.py
+++ leetcode_python/Sort/sort-characters-by-frequency.py
@@ -1,11 +1,46 @@
-
-
-
+# V0
+# IDEA : collections.Counter(s).most_common
+class Solution(object):
+    def frequencySort(self, s):
+        return ''.join(c * t for c, t in collections.Counter(s).most_common())
 
 # V1 
-
+# IDEA : SORT 
+# https://blog.csdn.net/fuxuemingzhu/article/details/79437548
 import collections
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        count = collections.Counter(s).most_common()
+        res = ''
+        for c, v in count:
+            res += c * v
+        return res
 
+### Test case:
+s=Solution()
+assert s.frequencySort(['a','b','c','c']) == 'ccab'
+assert s.frequencySort(['a']) == 'a'
+assert s.frequencySort(['a','A','c','c']) == 'ccaA'
+assert s.frequencySort(['c','c','c']) == 'ccc'
+assert s.frequencySort([]) == ''
+assert s.frequencySort(['','','']) == ''
+
+# V1'
+# http://bookshadow.com/weblog/2016/11/02/leetcode-sort-characters-by-frequency/
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ''.join(c * t for c, t in collections.Counter(s).most_common())
+
+# V2
+import collections
 class Solution(object):
     def frequencySort(self, s):
     	# sort Counter by value 
@@ -16,14 +51,10 @@ class Solution(object):
             output = output + (s_freq_dict[i][0]*s_freq_dict[i][1])
         return output
 
-
-# V2 
+# V2' 
 # Time:  O(n)
 # Space: O(n)
-
 import collections
-
-
 class Solution(object):
     def frequencySort(self, s):
         """
@@ -42,5 +73,4 @@ class Solution(object):
         for count in reversed(range(len(counts)-1)):
             for c in counts[count]:
                 result += c * count
-
         return result
