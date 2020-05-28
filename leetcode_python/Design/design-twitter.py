d0ad76efa3e2056844f3ee539f9efa18ccc017d3
yennanliu <f339339@gmail.com>
2020-05-28 11:59:51 +0800

    update status/code (/Design/design-twitter.py), progress, README

--- leetcode_python/Design/design-twitter.py
+++ leetcode_python/Design/design-twitter.py
@@ -1,6 +1,211 @@
 # V0
+from collections import defaultdict
+from heapq import merge
+class Twitter(object):
+    
+    def __init__(self):
+        self.follower_followees_map = defaultdict(set)
+        self.user_tweets_map = defaultdict(list)
+        self.time_stamp = 0
+
+    def postTweet(self, userId, tweetId):
+        self.user_tweets_map[userId].append((self.time_stamp, tweetId))
+        self.time_stamp -= 1
+
+    def getNewsFeed(self, userId):
+        followees = self.follower_followees_map[userId]
+        followees.add(userId)
+        
+        # reversed(.) returns a listreverseiterator, so the complexity is O(1) not O(n)
+        candidate_tweets = [reversed(self.user_tweets_map[u]) for u in followees]
+
+        tweets = []
+        # complexity is 10lg(n), n is twitter's user number in worst case
+        for t in merge(*candidate_tweets):
+            tweets.append(t[1])
+            if len(tweets) == 10:
+                break
+        return tweets
+
+    def follow(self, followerId, followeeId):
+        self.follower_followees_map[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        self.follower_followees_map[followerId].discard(followeeId)
+
+# V0'
+import collections, itertools, heapq
+class Twitter(object):
+
+    def __init__(self):
+        self.timer = itertools.count(step=-1)
+        self.tweets = collections.defaultdict(collections.deque)
+        self.followees = collections.defaultdict(set)
+
+    def postTweet(self, userId, tweetId):
+        self.tweets[userId].appendleft((next(self.timer), tweetId))
+
+    def getNewsFeed(self, userId):
+        tweets = heapq.merge(*(self.tweets[u] for u in self.followees[userId] | {userId}))
+        return [t for _, t in itertools.islice(tweets, 10)]
+
+    def follow(self, followerId, followeeId):
+        self.followees[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        self.followees[followerId].discard(followeeId)
 
 # V1
+# https://leetcode.com/problems/design-twitter/discuss/82943/Use-python-heapq.merge
+# IDEA : heapq.merge
+from collections import defaultdict
+from heapq import merge
+class Twitter(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.follower_followees_map = defaultdict(set)
+        self.user_tweets_map = defaultdict(list)
+        self.time_stamp = 0
+
+    def postTweet(self, userId, tweetId):
+        """
+        Compose a new tweet.
+        :type userId: int
+        :type tweetId: int
+        :rtype: void
+        """
+        self.user_tweets_map[userId].append((self.time_stamp, tweetId))
+        self.time_stamp -= 1
+
+    def getNewsFeed(self, userId):
+        """
+        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+        :type userId: int
+        :rtype: List[int]
+        """
+        followees = self.follower_followees_map[userId]
+        followees.add(userId)
+        
+        # reversed(.) returns a listreverseiterator, so the complexity is O(1) not O(n)
+        candidate_tweets = [reversed(self.user_tweets_map[u]) for u in followees]
+
+        tweets = []
+        # complexity is 10lg(n), n is twitter's user number in worst case
+        for t in merge(*candidate_tweets):
+            tweets.append(t[1])
+            if len(tweets) == 10:
+                break
+        return tweets
+
+    def follow(self, followerId, followeeId):
+        """
+        Follower follows a followee. If the operation is invalid, it should be a no-op.
+        :type followerId: int
+        :type followeeId: int
+        :rtype: void
+        """
+        self.follower_followees_map[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        """
+        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+        :type followerId: int
+        :type followeeId: int
+        :rtype: void
+        """
+        self.follower_followees_map[followerId].discard(followeeId)
+
+# V1'
+# https://leetcode.com/problems/design-twitter/discuss/82863/Python-solution
+# IDEA : collections.defaultdict
+# * DEMO : itertools.count
+# EXAMPLE 1
+# In [23]: import itertools
+#     ...: mytimer  = itertools.count(step=-1)
+#     ...: print (mytimer)
+#     ...: for _ in range(5):
+#     ...:     print (next(mytimer))
+#     ...:     
+# count(0, -1)
+# 0
+# -1
+# -2
+# -3
+# -4
+# EXAMPLE 2 
+# In [24]: import itertools
+#     ...: mytimer2  = itertools.count(step=1)
+#     ...: print (mytimer2)
+#     ...: for _ in range(5):
+#     ...:     print (next(mytimer2))
+#     ...:     
+# count(0)
+# 0
+# 1
+# 2
+# 3
+# 4
+# 
+# * DEMO collections.defaultdict(collections.deque) :
+# In [31]: mytweets = collections.defaultdict(collections.deque)
+#     ...: mytweets['001'].appendleft(('1', 'twitterid_1'))
+#     ...: mytweets['002'].appendleft(('2', 'twitterid_2'))
+#     ...: mytweets['003'].appendleft(('3', 'twitterid_3'))
+#     ...: mytweets['001'].appendleft(('10', 'twitterid_10'))
+#     ...: print (mytweets)
+#     ...: 
+# defaultdict(<class 'collections.deque'>, {'001': deque([('10', 'twitterid_10'), ('1', 'twitterid_1')]), '002': deque([('2', 'twitterid_2')]), '003': deque([('3', 'twitterid_3')])})
+# * DEMO heapq.merge(*iterables) : 
+# https://www.geeksforgeeks.org/merge-two-sorted-arrays-python-using-heapq/
+# heapq.merge(*iterables) -> Merge multiple sorted inputs into a single sorted output (for example, merge timestamped entries from multiple log files). Returns an iterator over the sorted values.
+# In [46]: mytweets = collections.defaultdict(collections.deque)
+#     ...: mytweets['1'].appendleft(('1', 'twitterid_1'))
+#     ...: mytweets['2'].appendleft(('2', 'twitterid_2'))
+#     ...: mytweets['3'].appendleft(('3', 'twitterid_3'))
+#     ...: mytweets['1'].appendleft(('10', 'twitterid_10'))
+#     ...: #print (mytweets)
+#     ...: print (list(heapq.merge(*(mytweets[u] for u in ['1','2','3']))))
+#     ...: print ('='*30)
+#     ...: print (list(heapq.merge((mytweets[u] for u in ['1','2','3']))))
+#     ...: 
+# [('10', 'twitterid_10'), ('1', 'twitterid_1'), ('2', 'twitterid_2'), ('3', 'twitterid_3')]
+# ==============================
+# [deque([('10', 'twitterid_10'), ('1', 'twitterid_1')]), deque([('2', 'twitterid_2')]), deque([('3', 'twitterid_3')])]
+# * DEMO : bitwise OR (a | b)
+# ->  a | b  : bitwise OR  Each bit position in the result is the logical OR of the bits in the corresponding position of the operands. (1 if either is 1, otherwise 0.)
+# https://realpython.com/python-operators-expressions/
+# In [67]: 1 | 1
+# Out[67]: 1
+
+# In [68]: 1 | 2
+# Out[68]: 3
+
+# In [69]: 1 | 0
+# Out[69]: 1
+import collections, itertools, heapq
+class Twitter(object):
+
+    def __init__(self):
+        self.timer = itertools.count(step=-1)
+        self.tweets = collections.defaultdict(collections.deque)
+        self.followees = collections.defaultdict(set)
+
+    def postTweet(self, userId, tweetId):
+        self.tweets[userId].appendleft((next(self.timer), tweetId))
+
+    def getNewsFeed(self, userId):
+        tweets = heapq.merge(*(self.tweets[u] for u in self.followees[userId] | {userId}))
+        return [t for _, t in itertools.islice(tweets, 10)]
+
+    def follow(self, followerId, followeeId):
+        self.followees[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        self.followees[followerId].discard(followeeId)
+
+# V1''
 # https://blog.csdn.net/fuxuemingzhu/article/details/82155420
 # IDEA : heapq
 class User(object):
@@ -99,30 +304,7 @@ class Twitter(object):
         if followee in self.userMap.get(followerId).following:
             self.userMap.get(followerId).following.remove(followee)
 
-# V1'
-# https://leetcode.com/problems/design-twitter/discuss/82863/Python-solution
-# IDEA : collections.defaultdict
-class Twitter(object):
-
-    def __init__(self):
-        self.timer = itertools.count(step=-1)
-        self.tweets = collections.defaultdict(collections.deque)
-        self.followees = collections.defaultdict(set)
-
-    def postTweet(self, userId, tweetId):
-        self.tweets[userId].appendleft((next(self.timer), tweetId))
-
-    def getNewsFeed(self, userId):
-        tweets = heapq.merge(*(self.tweets[u] for u in self.followees[userId] | {userId}))
-        return [t for _, t in itertools.islice(tweets, 10)]
-
-    def follow(self, followerId, followeeId):
-        self.followees[followerId].add(followeeId)
-
-    def unfollow(self, followerId, followeeId):
-        self.followees[followerId].discard(followeeId)
-
-# V1''
+# V1'''
 # https://www.hrwhisper.me/leetcode-design-twitter/
 class Twitter(object):
     def __init__(self):
@@ -153,7 +335,7 @@ class Twitter(object):
         user_list = list(self.follower_ship[userId]) + [userId]
         userId_tweet_index = [[userId, len(self.tweets[userId]) - 1] for userId in user_list if userId in self.tweets]
 
-        for _ in xrange(10):
+        for _ in range(10):
             max_index = max_tweet_id = max_user_id = -1
             for i, (user_id, tweet_index) in enumerate(userId_tweet_index):
                 if tweet_index >= 0:
@@ -187,69 +369,7 @@ class Twitter(object):
         if followerId in self.follower_ship and followeeId in self.follower_ship[followerId]:
             self.follower_ship[followerId].remove(followeeId)
 
-# V1'''
-# https://leetcode.com/problems/design-twitter/discuss/82943/Use-python-heapq.merge
-# IDEA : heapq.merge
-from collections import defaultdict
-from heapq import merge
-class Twitter(object):
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.follower_followees_map = defaultdict(set)
-        self.user_tweets_map = defaultdict(list)
-        self.time_stamp = 0
-
-    def postTweet(self, userId, tweetId):
-        """
-        Compose a new tweet.
-        :type userId: int
-        :type tweetId: int
-        :rtype: void
-        """
-        self.user_tweets_map[userId].append((self.time_stamp, tweetId))
-        self.time_stamp -= 1
-
-    def getNewsFeed(self, userId):
-        """
-        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
-        :type userId: int
-        :rtype: List[int]
-        """
-        followees = self.follower_followees_map[userId]
-        followees.add(userId)
-        
-        # reversed(.) returns a listreverseiterator, so the complexity is O(1) not O(n)
-        candidate_tweets = [reversed(self.user_tweets_map[u]) for u in followees]
-
-        tweets = []
-        # complexity is 10lg(n), n is twitter's user number in worst case
-        for t in merge(*candidate_tweets):
-            tweets.append(t[1])
-            if len(tweets) == 10:
-                break
-        return tweets
-
-    def follow(self, followerId, followeeId):
-        """
-        Follower follows a followee. If the operation is invalid, it should be a no-op.
-        :type followerId: int
-        :type followeeId: int
-        :rtype: void
-        """
-        self.follower_followees_map[followerId].add(followeeId)
-
-    def unfollow(self, followerId, followeeId):
-        """
-        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
-        :type followerId: int
-        :type followeeId: int
-        :rtype: void
-        """
-        self.follower_followees_map[followerId].discard(followeeId)
-
-# V1'''
+# V1''''
 # http://bookshadow.com/weblog/2016/06/11/leetcode-design-twitter/
 # IDEA : JAVA
 
