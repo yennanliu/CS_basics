4bdd184bdbf699ef2f075c72d86a3893338775e4
yennanliu <f339339@gmail.com>
2020-05-30 13:42:01 +0800

    update status/code (String/repeated-substring-pattern.py), README

--- leetcode_python/String/repeated-substring-pattern.py
+++ leetcode_python/String/repeated-substring-pattern.py
@@ -1,6 +1,6 @@
 # Time:  O(n)
 # Space: O(n)
-
+#
 # Given a non-empty string check if it can be constructed by taking a substring of it
 # and appending multiple copies of the substring together.
 # You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
@@ -21,12 +21,51 @@
 # Output: True
 #
 # Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
-
+#
 # KMP solution.
 
 # V0 
+class Solution(object):
+    def repeatedSubstringPattern(self, str):
+        size = len(str)
+        # only have to go through till half of s's length, since it's not possbile to find the SubstringPattern if len(s[:x]) > size//2
+        for x in range(1, size // 2 + 1):
+            # if len(s) is not len(s[:x]) 's  multiple
+            if size % x == 1:
+                continue
+            # if len(s) is  len(s[:x]) 's  multiple, check if SubstringPattern 
+            if str[:x] * (size // x) == str:
+                return True
+        return False
 
-# V1 
+# V1
+# http://bookshadow.com/weblog/2016/11/13/leetcode-repeated-substring-pattern/
+class Solution(object):
+    def repeatedSubstringPattern(self, str):
+        """
+        :type str: str
+        :rtype: bool
+        """
+        size = len(str)
+        for x in range(1, size // 2 + 1):
+            if size % x:
+                continue
+            if str[:x] * (size // x) == str:
+                return True
+        return False
+
+### Test case:
+s=Solution()
+assert s.repeatedSubstringPattern("abab") == True
+assert s.repeatedSubstringPattern("aba") == False
+assert s.repeatedSubstringPattern("ababab") == True
+assert s.repeatedSubstringPattern("") == False
+assert s.repeatedSubstringPattern("a") == False
+assert s.repeatedSubstringPattern("aaaaa") == True
+assert s.repeatedSubstringPattern("aaabbb") == False
+assert s.repeatedSubstringPattern("".join([ 'a' for _ in range(100)])) == True
+
+# V1'
 # https://blog.csdn.net/fuxuemingzhu/article/details/54564801
 # IDEA : 
 # go through the sting, if can find any sub-string 
