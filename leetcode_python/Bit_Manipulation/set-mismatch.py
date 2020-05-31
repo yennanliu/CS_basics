ba4affd4ab309652408f62db9d76f361f81d6d1f
yennanliu <f339339@gmail.com>
2020-05-31 15:31:12 +0800

    update status/code (Bit_Manipulation/set-mismatch.py)

--- leetcode_python/Bit_Manipulation/set-mismatch.py
+++ leetcode_python/Bit_Manipulation/set-mismatch.py
@@ -1,6 +1,31 @@
-# V0 
+# 645. Set Mismatch
+# Easy
+#
+#
+# Add to List
+#
+# The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, one of the numbers in the set got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+#
+# Given an array nums representing the data status of this set after the error. Your task is to firstly find the number occurs twice and then find the number that is missing. Return them in the form of an array.
+#
+# Example 1:
+# Input: nums = [1,2,2,4]
+# Output: [2,3]
+# Note:
+# The given array size will in the range [2, 10000].
+# The given array's numbers won't have any order.
 
-# V1' 
+
+# V0 
+class Solution(object):
+    def findErrorNums(self, nums):
+        N = len(nums)
+        nset = set(nums)
+        missing = N * (N + 1) // 2 - sum(nset)
+        duplicated = sum(nums) - sum(nset)
+        return [duplicated, missing]
+
+# V1
 # https://blog.csdn.net/fuxuemingzhu/article/details/79247916
 # http://bookshadow.com/weblog/2017/07/24/leetcode-set-mismatch/
 # IDEA : SUM
@@ -12,10 +37,17 @@ class Solution(object):
         """
         N = len(nums)
         nset = set(nums)
-        missing = N * (N + 1) / 2 - sum(nset)
+        missing = N * (N + 1) // 2 - sum(nset)
         duplicated = sum(nums) - sum(nset)
         return [duplicated, missing]
-               
+
+### Test case :
+s=Solution()
+assert s.findErrorNums([1,2,2,4]) == [2,3]       
+assert s.findErrorNums([1,1,3,4]) == [1,2]       
+assert s.findErrorNums([]) == [0,0]       
+assert s.findErrorNums([1,2,3]) == [0,0]       
+
 # V1' 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79247916
 # http://bookshadow.com/weblog/2017/07/24/leetcode-set-mismatch/
@@ -31,6 +63,36 @@ class Solution(object):
         for i in range(len(nums)):
             hashs[nums[i] - 1] += 1
         return [hashs.index(2) + 1, hashs.index(0) + 1]
+
+# V1''
+# https://leetcode.com/problems/set-mismatch/discuss/105558/Oneliner-Python
+class Solution(object):
+    def findErrorNums(self, nums):
+        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]
+
+# V1'''
+# https://leetcode.com/problems/set-mismatch/discuss/345631/Multiple-Python-Solution
+class Solution:
+    def findErrorNums(self, nums):
+        x = sum(nums) - sum(set(nums))
+        y = sum(range(len(nums)+1))-(sum(nums)-x)
+        return [x,y]
+
+# V1''''
+# https://leetcode.com/problems/set-mismatch/discuss/345631/Multiple-Python-Solution
+class Solution:
+    def findErrorNums(self, nums):
+       count = [0] * (len(nums)+1)
+        for x in nums:
+            count[x] += 1
+            
+        twice = z = 0
+        for x in range(1, len(nums)+1):
+            if count[x] == 2:
+                twice = x
+            if count[x] == 0:
+                z = x
+        return [twice, z]
 
 # V2 
 # Time:  O(n)
