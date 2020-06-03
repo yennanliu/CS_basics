17adca837692176cb7d8b2f568b51da120954070
yennanliu <f339339@gmail.com>
2020-06-03 12:58:36 +0800

    fix code (maximum-width-of-binary-tree.py), update progress, to_review

--- leetcode_python/Tree/maximum-width-of-binary-tree.py
+++ leetcode_python/Tree/maximum-width-of-binary-tree.py
@@ -124,18 +124,19 @@ class Solution(object):
 # IDEA : BFS 
 # Time Complexity: O(N)
 # Space Complexity: O(N)
-def widthOfBinaryTree(self, root):
-    queue = [(root, 0, 0)]
-    cur_depth = left = ans = 0
-    for node, depth, pos in queue:
-        if node:
-            queue.append((node.left, depth+1, pos*2))
-            queue.append((node.right, depth+1, pos*2 + 1))
-            if cur_depth != depth:
-                cur_depth = depth
-                left = pos
-            ans = max(pos - left + 1, ans)
-    return ans
+class Solution(object):
+    def widthOfBinaryTree(self, root):
+        queue = [(root, 0, 0)]
+        cur_depth = left = ans = 0
+        for node, depth, pos in queue:
+            if node:
+                queue.append((node.left, depth+1, pos*2))
+                queue.append((node.right, depth+1, pos*2 + 1))
+                if cur_depth != depth:
+                    cur_depth = depth
+                    left = pos
+                ans = max(pos - left + 1, ans)
+        return ans
 
 # V1'''
 # https://leetcode.com/problems/maximum-width-of-binary-tree/solution/
