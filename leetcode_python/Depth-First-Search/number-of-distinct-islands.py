d18ae59b3a4f2315309d36cb5c6b33c517c00822
yennanliu <f339339@gmail.com>
2020-06-05 15:04:54 +0800

    update status/code (Depth-First-Search/number-of-distinct-islands.py), progress, README

--- leetcode_python/Depth-First-Search/number-of-distinct-islands.py
+++ leetcode_python/Depth-First-Search/number-of-distinct-islands.py
@@ -1,4 +1,57 @@
+# 694 Number of Distinct Islands
+#
+# Given a non-empty 2D array grid of 0’s and 1’s, an island is a group of 1’s (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+#
+# Count the number of distinct islands. An island is considered to be the same as another if and only if one island can be translated (and not rotated or reflected) to equal the other.
+#
+# Example 1:
+# 11000
+# 11000
+# 00011
+# 00011
+# Given the above grid map, return 1.
+# Example 2:
+# 11011
+# 10000
+# 00001
+# 11011
+# Given the above grid map, return 3.
+#
+# Notice that:
+# 11
+# 1
+# and
+# 1
+# 11
+# are considered different island shapes, because we do not consider reflection / rotation.
+# Note: The length of each dimension in the given grid does not exceed 50.
+
+
 # V0 
+# IDEA : DFS
+class Solution:
+    def numDistinctIslands(self, grid):
+        def dfs(x, y, pos, rel_pos):
+            if grid[x][y] != 1:
+                return
+            grid[x][y] = -1
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            for dx, dy in directions:
+                if 0 <= x+dx < row and 0 <= y+dy < col and grid[x+dx][y+dy] == 1:
+                    new_rel_pos = (rel_pos[0] + dx, rel_pos[1] + dy)
+                    pos.append(new_rel_pos)
+                    dfs(x+dx, y+dy, pos, new_rel_pos)
+            
+        shapes = set()
+        row, col = len(grid), len(grid[0])
+        for x in range(row):
+            for y in range(col):
+                if grid[x][y] == 1:
+                    # get the shape of island
+                    pos = []
+                    dfs(x, y, pos, (0, 0))
+                    shapes.add(tuple(pos))            
+        return len(shapes)
 
 # V1 
 # https://blog.csdn.net/danspace1/article/details/86610850
@@ -29,6 +82,8 @@ class Solution:
                     dfs(x, y, pos, (0, 0))
                     shapes.add(tuple(pos))            
         return len(shapes)
+
+### Test case : dev 
 
 # V1'
 # https://www.jiuzhang.com/solution/number-of-distinct-islands/#tag-highlight-lang-python
