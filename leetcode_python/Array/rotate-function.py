b8667695353c85bcb3504531c46419c5639c3324
yennanliu <f339339@gmail.com>
2020-06-08 10:07:39 +0800

    [update-code] /Array/rotate-function.py, update progress, README

--- leetcode_python/Array/rotate-function.py
+++ leetcode_python/Array/rotate-function.py
@@ -23,6 +23,43 @@ So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.
 """
 
 # V0
+# IDEA : MATH
+# first, we represent the F(1) op as below:
+
+# F(0) = 0A + 1B + 2C +3D
+
+# F(1) = 0D + 1A + 2B +3C
+
+# F(2) = 0C + 1D + 2A +3B
+
+# F(3) = 0B + 1C + 2D +3A
+
+# then, by some math manipulation, we have below relation:
+
+# set sum = 1A + 1B + 1C + 1D
+
+# -> F(1) = F(0) + sum - 4D
+
+# -> F(2) = F(1) + sum - 4C
+
+# -> F(3) = F(2) + sum - 4B
+
+# so we find the rules!
+
+# => F(i) = F(i-1) + sum - n*A[n-i]
+
+# https://www.cnblogs.com/grandyang/p/5869791.html
+# http://bookshadow.com/weblog/2016/09/11/leetcode-rotate-function/
+class Solution(object):
+    def maxRotateFunction(self, A):
+        size = len(A)
+        sums = sum(A)
+        sumn = sum(x * n for x, n in enumerate(A))
+        ans = sumn
+        for x in range(size - 1, 0, -1):
+            sumn += sums - size * A[x]
+            ans = max(ans, sumn)
+        return ans
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/83002609
@@ -61,6 +98,18 @@ class Solution:
             f = f + _sum - N * A[i]
             res = max(res, f)       # since we want to calculate the MAX value of F(0), F(1), ..., F(n-1).
         return res
+
+### Test case
+s=Solution()
+assert s.maxRotateFunction([]) == 0
+assert s.maxRotateFunction([7]) == 0
+assert s.maxRotateFunction([7,2,1]) == 15
+assert s.maxRotateFunction([4, 3, 2, 6]) == 26
+assert s.maxRotateFunction([0,0,0,0]) == 0
+assert s.maxRotateFunction([3,7,0,1]) == 28
+assert s.maxRotateFunction([1,1,1,1]) == 6
+assert s.maxRotateFunction([-1,-1,-1,-1]) == -6
+assert s.maxRotateFunction([-1,10,-5,1]) == 29
 
 # V1'
 # http://bookshadow.com/weblog/2016/09/11/leetcode-rotate-function/
