b43469ae177549847d44805fd01ab0131f99d3ea
yennanliu <f339339@gmail.com>
2020-06-14 18:23:46 +0800

    [update progress/code] third-maximum-number.py, progress.md, README

--- leetcode_python/Array/third-maximum-number.py
+++ leetcode_python/Array/third-maximum-number.py
@@ -48,6 +48,20 @@ class Solution(object):
         _max = max(nums_set)
         return _max
 
+# V0''
+class Solution(object):
+    def thirdMax(self, nums):
+        # s1 > s2 > s3
+        s1, s2, s3 = float('-inf'), float('-inf'), float('-inf')
+        for num in nums:
+            if num > s1:
+                s1, s2, s3 = num, s1, s2
+            elif num < s1 and num > s2:
+                s2, s3 = num, s2
+            elif num < s2 and num > s3:
+                s3 = num
+        return s3 if s3 != float('-inf') else s1
+
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79255652
 # http://bookshadow.com/weblog/2016/10/09/leetcode-third-maximum-number/
