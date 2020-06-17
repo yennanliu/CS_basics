725b720e3bd08ed58810d97d9cfaa8f19f480a5a
yennanliu <f339339@gmail.com>
2020-06-17 11:29:30 +0800

    [update code/progress] Greedy/maximum-length-of-pair-chain.py, progress, add cheatsheet/greedy.md

---
+++ doc/cheatsheet/greedy.md
@@ -0,0 +1,29 @@
+# Greedy cheatsheet 
+
+## 0) Concept  
+
+### 0-1) Framework
+
+### 0-2) Pattern
+
+## 1) General form
+
+### 1-1) Basic OP
+
+## 2) LC Example
+
+### 2-1) Maximum Length of Pair Chain
+```python
+# 646 Maximum Length of Pair Chain
+class Solution(object):
+    def findLongestChain(self, pairs):
+        pairs = sorted(pairs, key=lambda x : x[1])
+        ### NOTICE HERE
+        currTime, ans = float('-inf'), 0
+        for x, y in pairs:
+            ### NOTICE HERE
+            if currTime < x:
+                currTime = y
+                ans += 1
+        return ans
+```
--- leetcode_python/Greedy/maximum-length-of-pair-chain.py
+++ leetcode_python/Greedy/maximum-length-of-pair-chain.py
@@ -1,7 +1,20 @@
 # V0 
+# IDEA : GREEDY
+class Solution(object):
+    def findLongestChain(self, pairs):
+        pairs = sorted(pairs, key=lambda x : x[1])
+        ### NOTICE HERE
+        currTime, ans = float('-inf'), 0
+        for x, y in pairs:
+            ### NOTICE HERE
+            if currTime < x:
+                currTime = y
+                ans += 1
+        return ans
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79826524
+# http://bookshadow.com/weblog/2017/07/24/leetcode-maximum-length-of-pair-chain/
 # IDEA : GREEDY
 class Solution(object):
     def findLongestChain(self, pairs):
@@ -16,7 +29,87 @@ class Solution(object):
                 currTime = y
                 ans += 1
         return ans
+
+### Test case :
+s=Solution()
+assert s.findLongestChain([[1,2], [2,3], [3,4]]) == 2
+assert s.findLongestChain([]) == 0
+assert s.findLongestChain([[1,1]]) == 1
+assert s.findLongestChain([[1,2]]) == 1
+assert s.findLongestChain([[1,2], [2,3], [3,4], [9,10]]) == 3
+assert s.findLongestChain([[1,2], [2,3], [2,3], [3,4]]) == 2
+assert s.findLongestChain([[1,2], [1,2], [1,2]]) == 1
+assert s.findLongestChain([[i, i+1] for i in range(10)]) == 5
+
+# V1'
+# https://leetcode.com/problems/maximum-length-of-pair-chain/solution/
+# IDEA : DP 
+# time complexity : O(N^2)
+# space complexity : O(N)
+class Solution(object): #Time Limit Exceeded
+    def findLongestChain(self, pairs):
+        pairs.sort()
+        dp = [1] * len(pairs)
+
+        for j in range(len(pairs)):
+            for i in range(j):
+                if pairs[i][1] < pairs[j][0]:
+                    dp[j] = max(dp[j], dp[i] + 1)
+
+        return max(dp)
+
+# V1''
+# https://leetcode.com/problems/maximum-length-of-pair-chain/solution/
+# IDEA : GREEDY
+class Solution(object):
+    def findLongestChain(self, pairs):
+        cur, ans = float('-inf'), 0
+        for x, y in sorted(pairs, key = operator.itemgetter(1)):
+            if cur < x:
+                cur = y
+                ans += 1
+        return ans
+
+# V1'''
+# https://leetcode.com/problems/maximum-length-of-pair-chain/discuss/105608/Python-DP-solution
+# IDEA : DP
+class Solution(object):
+    def findLongestChain(self, pairs):
+        """
+        :type pairs: List[List[int]]
+        :rtype: int
+        """
+        pairs.sort()
+        n = len(pairs)
+        dp = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if pairs[i][0] > pairs[j][1] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
+
+# V1''''
+# https://leetcode.com/problems/maximum-length-of-pair-chain/discuss/105640/O(nlogn)-Python-solution-binary-search-easy-to-understand
+# IDEA : BINARY SEARCH
+class Solution(object):
+    def findLongestChain(self, pairs):
+        """
+        :type pairs: List[List[int]]
+        :rtype: int
+        """
+        # sort by x for pairs (x1, y1), (x2, y2), (x3, y3)...
+        pairs.sort()
+            
+        # min_end_y[i] is the ending tuple minimum y of length=i chain
+        min_end_y = [float('inf')] * len(pairs)
+        for x, y in pairs:
+            # since (a, b) can chain (c, d) iff b < c, use bisect_left
+            i = bisect.bisect_left(min_end_y, x)
+            # greedy method, for the same length chain, the smaller y the better
+            min_end_y[i] = min(min_end_y[i], y)  
         
+        return bisect.bisect_left(min_end_y, float('inf'))
+
 # V2 
 # Time:  O(nlogn)
 # Space: O(1)
