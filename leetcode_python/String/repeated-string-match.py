d67c5b1957e5e7b7bdff63975f341a79440176c4
yennanliu <f339339@gmail.com>
2020-06-22 11:21:16 +0800

    [update code/progress/status] String/repeated-string-match.py, progress, README

--- leetcode_python/String/repeated-string-match.py
+++ leetcode_python/String/repeated-string-match.py
@@ -10,7 +10,24 @@
 # The length of A and B will be between 1 and 10000.
 
 # V0
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        sa, sb = len(A), len(B)
+        x = 1
+        while (x - 1) * sa <= 2 * max(sa, sb):
+            if B in A * x: 
+                return x
+            x += 1
+        return -1
 
+# V0'
+class Solution:
+    def repeatedStringMatch(self, A, B):
+        r = math.ceil(len(B) / len(A))
+        for a in [r, r + 1]:
+            if B in A * a:
+                return a
+        return -1
 
 # V1 
 # http://bookshadow.com/weblog/2017/10/01/leetcode-repeated-string-match/
@@ -28,7 +45,70 @@ class Solution(object):
                 return x
             x += 1
         return -1
-    
+
+### Test case : dev 
+
+# V1'
+# https://leetcode.com/problems/repeated-string-match/discuss/224182/Explanation-on-the-Intuitive-Python-2-liner-solution
+class Solution:
+    def repeatedStringMatch(self, A, B):
+        r = math.ceil(len(B) / len(A))
+        for a in [r, r + 1]:
+            if B in A * a:
+                return a
+        return -1
+
+# V1'
+# https://leetcode.com/problems/repeated-string-match/solution/
+# IDEA : BRUTE FORCE
+# time complexity : O(N*(M+N))
+# space complexity : O(M+N)
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        q = (len(B) - 1) // len(A) + 1
+        for i in range(2):
+            if B in A * (q+i): return q+i
+        return -1
+
+# V1''
+# https://leetcode.com/problems/repeated-string-match/solution/
+# IDEA : Rabin-Karp (Rolling Hash)
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        def check(index):
+            return all(A[(i + index) % len(A)] == x
+                       for i, x in enumerate(B))
+
+        q = (len(B) - 1) // len(A) + 1
+
+        p, MOD = 113, 10**9 + 7
+        p_inv = pow(p, MOD-2, MOD)
+        power = 1
+
+        b_hash = 0
+        for x in map(ord, B):
+            b_hash += power * x
+            b_hash %= MOD
+            power = (power * p) % MOD
+
+        a_hash = 0
+        power = 1
+        for i in xrange(len(B)):
+            a_hash += power * ord(A[i % len(A)])
+            a_hash %= MOD
+            power = (power * p) % MOD
+
+        if a_hash == b_hash and check(0): return q
+
+        power = (power * p_inv) % MOD
+        for i in xrange(len(B), (q+1) * len(A)):
+            a_hash = (a_hash - ord(A[(i - len(B)) % len(A)])) * p_inv
+            a_hash += power * ord(A[i % len(A)])
+            a_hash %= MOD
+            if a_hash == b_hash and check(i - len(B) + 1):
+                return q if i < q * len(A) else q+1
+        return -1
+
 # V2 
 # Rabin-Karp Algorithm (rolling hash)
 class Solution(object):
@@ -67,5 +147,4 @@ class Solution(object):
             a_hash %= M
             if a_hash == b_hash and check(i-len(B)+1):
                 return q if i < q*len(A) else q+1
-
         return -1
