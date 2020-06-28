988cc0a11064cb76ab507b45097d5f0b15829d45
yennanliu <f339339@gmail.com>
2020-06-28 14:03:56 +0800

    [update code/status/progress] Bit_Manipulation/utf-8-validation.py. to_review.txt, progress.md, README

--- leetcode_python/Bit_Manipulation/utf-8-validation.py
+++ leetcode_python/Bit_Manipulation/utf-8-validation.py
@@ -1,4 +1,5 @@
 # V0 
+# https://leetcode.com/problems/utf-8-validation/discuss/400575/case
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/82963310
@@ -25,7 +26,63 @@ class Solution(object):
                 cnt -= 1
         return cnt == 0
 
-# V1' 
+### Test case : dev
+
+# V1'
+# https://leetcode.com/problems/utf-8-validation/discuss/87478/Simplest-Python-Solution
+class Solution(object):
+    def validUtf8(self, data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
+        count = 0
+        
+        for byte in data:
+            if byte >= 128 and byte <= 191:
+                if not count:
+                    return False
+                count -= 1
+            else:
+                if count:
+                    return False
+                if byte < 128:
+                    continue
+                elif byte < 224:
+                    count = 1
+                elif byte < 240:
+                    count = 2
+                elif byte < 248:
+                    count = 3
+                else:
+                    return False                  
+        return count == 0
+
+# V1''
+# https://leetcode.com/problems/utf-8-validation/discuss/156588/My-easy-to-understand-Python-solution
+class Solution:
+    def validUtf8(self, data):
+        
+        def rest(i):
+            if len(data) < i: return False
+            for _ in range(i):
+                if not data.pop().startswith("10"): return False
+            return True
+        
+        data = [str(bin(seq)[2:].zfill(8)) for seq in data[::-1]]
+        while data:
+            seq = data.pop()
+            if seq.startswith("0"): continue
+            if seq.startswith("110"):
+                if not rest(1): return False
+            elif seq.startswith("1110"):
+                if not rest(2): return False
+            elif seq.startswith("11110"):
+                if not rest(3): return False
+            else: return False
+        return True
+
+# V1'''
 # http://bookshadow.com/weblog/2016/09/04/leetcode-utf-8-validation/
 class Solution(object):
     def validUtf8(self, data):
@@ -46,7 +103,104 @@ class Solution(object):
                     return False
             data = data[x:]
         return True
-               
+
+# V1'''''
+# https://leetcode.com/problems/utf-8-validation/solution/
+# IDEA : String Manipulation
+# time complexity : O(N)
+# space complexity : O(N)
+class Solution:
+    def validUtf8(self, data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
+
+        # Number of bytes in the current UTF-8 character
+        n_bytes = 0
+
+        # For each integer in the data array.
+        for num in data:
+
+            # Get the binary representation. We only need the least significant 8 bits
+            # for any given number.
+            bin_rep = format(num, '#010b')[-8:]
+
+            # If this is the case then we are to start processing a new UTF-8 character.
+            if n_bytes == 0:
+
+                # Get the number of 1s in the beginning of the string.
+                for bit in bin_rep:
+                    if bit == '0': break
+                    n_bytes += 1
+
+                # 1 byte characters
+                if n_bytes == 0:
+                    continue
+
+                # Invalid scenarios according to the rules of the problem.
+                if n_bytes == 1 or n_bytes > 4:
+                    return False
+            else:
+                # Else, we are processing integers which represent bytes which are a part of
+                # a UTF-8 character. So, they must adhere to the pattern `10xxxxxx`.
+                if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                    return False
+
+            # We reduce the number of bytes to process by 1 after each integer.
+            n_bytes -= 1
+
+        # This is for the case where we might not have the complete data for
+        # a particular UTF-8 character.
+        return n_bytes == 0     
+
+# V1'''''''
+# https://leetcode.com/problems/utf-8-validation/solution/
+# Bit Manipulation
+# time complexity : O(N)
+# space complexity : O(1)
+class Solution:
+    def validUtf8(self, data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
+
+        # Number of bytes in the current UTF-8 character
+        n_bytes = 0
+
+        # Mask to check if the most significant bit (8th bit from the left) is set or not
+        mask1 = 1 << 7
+
+        # Mask to check if the second most significant bit is set or not
+        mask2 = 1 << 6
+        for num in data:
+
+            # Get the number of set most significant bits in the byte if
+            # this is the starting byte of an UTF-8 character.
+            mask = 1 << 7
+            if n_bytes == 0:
+                while mask & num:
+                    n_bytes += 1
+                    mask = mask >> 1
+
+                # 1 byte characters
+                if n_bytes == 0:
+                    continue
+
+                # Invalid scenarios according to the rules of the problem.
+                if n_bytes == 1 or n_bytes > 4:
+                    return False
+            else:
+
+                # If this byte is a part of an existing UTF-8 character, then we
+                # simply have to look at the two most significant bits and we make
+                # use of the masks we defined before.
+                if not (num & mask1 and not (num & mask2)):
+                    return False
+            n_bytes -= 1
+        return n_bytes == 0     
+
 # V2 
 # Time:  O(n)
 # Space: O(1)
