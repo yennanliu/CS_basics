abfcf8425d5ccd8ee9c50344e0d02aaaa0f9d8f3
yennanliu <f339339@gmail.com>
2020-07-13 11:46:26 +0800

    [add/update algorithm] add algorithm/java/Graph.java, update GraphClient.java

--- algorithm/java/Graph.java
+++ algorithm/java/Graph.java
@@ -1,34 +1,25 @@
-// Graph basic API (OP)
+// Graph API (OP)
 // https://www.coursera.org/learn/algorithms-part2/lecture/4ZE6G/graph-api
 
 
-public class Graph {
+public class Graph{
+    private final int V;
+    private Bag<Integer>[] adj;
 
-    public static int degree(Graph G, int v){
-        int degree = 0;
-        for (int w : G.adj(v)) degree++;
-        return degree; 
+    public Graph(int V){
+        this.V = V;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<Integer>();
     }
 
-    public static int maxDegree(Graph G){
-        int max = 0;
-        for (int v = 0; v < G.V(); v++)
-            if (degree(G, v) > max)
-                max = degree(G, v);
-        return max; 
+    public void addEdge(int v, int w){
+        adj[v].add(w);
+        adj[w].add(v);
     }
 
-    public static double averageDegree(Graph G){
-        return 2.0 * G.E() / G.V();
+    public Iterable<Integer> adj(int v){
+        // iterator for vertices adjacent to v
+        return adj[v];
     }
-
-
-    public static int numberOfSelfLoops(Graph G){
-        int count = 0;
-        for (int v = 0; v < G.V(); v++)
-            for (int w : G.adj(v))
-                if (v == w) count ++; 
-        return count / 2 ;
-    }
-
 }
---
+++ algorithm/java/GraphClient.java
@@ -0,0 +1,33 @@
+// Graph client basic API (OP)
+// https://www.coursera.org/learn/algorithms-part2/lecture/4ZE6G/graph-api
+
+
+public class GraphClient{
+
+    public static int degree(Graph G, int v){
+        int degree = 0;
+        for (int w : G.adj(v)) degree++;
+        return degree; 
+    }
+
+    public static int maxDegree(Graph G){
+        int max = 0;
+        for (int v = 0; v < G.V(); v++)
+            if (degree(G, v) > max)
+                max = degree(G, v);
+        return max; 
+    }
+
+    public static double averageDegree(Graph G){
+        return 2.0 * G.E() / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G){
+        int count = 0;
+        for (int v = 0; v < G.V(); v++)
+            for (int w : G.adj(v))
+                if (v == w) count ++; 
+        return count / 2 ;
+    }
+
+}
