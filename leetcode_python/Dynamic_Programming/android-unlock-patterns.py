ecfbde63590216302b4881ff091f938138127511
yennanliu <f339339@gmail.com>
2020-07-13 18:34:00 +0800

    [update code/status] Dynamic_Programming/android-unlock-patterns.py (dev), README

--- leetcode_python/Dynamic_Programming/android-unlock-patterns.py
+++ leetcode_python/Dynamic_Programming/android-unlock-patterns.py
@@ -1,34 +1,33 @@
 # Given an Android 3x3 key lock screen and two integers m and n, where 1 ≤ m ≤ n ≤ 9, count the total number of unlock patterns of the Android lock screen, which consist of minimum of m keys and maximum n keys.
-
+#
 # Rules for a valid pattern:
-
+#
 # Each pattern must connect at least m keys and at most n keys.
 # All the keys must be distinct.
 # If the line connecting two consecutive keys in the pattern passes through any other keys, the other keys must have previously selected in the pattern. No jumps through non selected key is allowed.
 # The order of keys used matters.
- 
-
+#
+#
 # \
-
+#
 # Explanation:
-
+#
 # | 1 | 2 | 3 |
 # | 4 | 5 | 6 |
 # | 7 | 8 | 9 |
- 
-
+#
 # Invalid move: 4 - 1 - 3 - 6 
 # Line 1 - 3 passes through key 2 which had not been selected in the pattern.
-
+#
 # Invalid move: 4 - 1 - 9 - 2
 # Line 1 - 9 passes through key 5 which had not been selected in the pattern.
-
+#
 # Valid move: 2 - 4 - 1 - 3 - 6
 # Line 1 - 3 is valid because it passes through key 2, which had been selected in the pattern
-
+#
 # Valid move: 6 - 5 - 4 - 1 - 9 - 2
 # Line 1 - 9 is valid because it passes through key 5, which had been selected in the pattern.
-
+#
 # Example:
 # Given m = 1, n = 1, return 9.
 
@@ -36,6 +35,41 @@
 # V0 
 
 # V1 
+# https://blog.csdn.net/qq508618087/article/details/51758481
+# IDEA : DP
+# C++
+# class Solution {
+# public:
+#     int DFS(int m, int n, int len, int num)
+#     {
+#         int cnt = 0;
+#         if(len >= m) cnt++;
+#         if(++len > n) return cnt;
+#         visited[num] = true;
+#         for(int i = 1; i<= 9; i++)
+#             if(!visited[i] && visited[hash[num][i]])
+#                 cnt += DFS(m, n, len, i);
+#         visited[num] = false;
+#         return cnt;
+#     }
+#   
+#     int numberOfPatterns(int m, int n) {
+#         if(m < 1 || n < 1) return 0;
+#         visited.resize(10, false);
+#         visited[0] = true;
+#         hash.resize(10, vector<int>(10, 0));
+#         hash[1][3] = hash[3][1] = 2;
+#         hash[1][7] = hash[7][1] = 4;
+#         hash[3][9] = hash[9][3] = 6;
+#         hash[7][9] = hash[9][7] = 8;
+#         hash[2][8] = hash[8][2] = hash[4][6] = hash[6][4] = 5;
+#         hash[1][9] = hash[9][1] = hash[3][7] = hash[7][3] = 5;
+#         return DFS(m, n, 1, 1)*4 + DFS(m, n, 1, 2)*4 + DFS(m, n, 1, 5);
+#     }
+# private:
+#     vector<bool> visited;
+#     vector<vector<int>> hash;
+# };
 
 # V2 
 # Time:  O(9^2 * 2^9)
