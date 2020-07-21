77530fcaaa7198f7433121526cccdb38d91e0949
yennanliu <f339339@gmail.com>
2020-07-21 11:56:05 +0800

    update algorithm/java/DijkstraSP.java, README

--- algorithm/java/DijkstraSP.java
+++ algorithm/java/DijkstraSP.java
@@ -2,8 +2,39 @@
 
 public class DijkstraSP
 {
-	private DirectedEdge[] edgeTo;
-	private double[] distTo;
-	private IndexMinPQ<double> pq;
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<double> pq; // pq : priority queue
+
+    public DijkstraSP(EdgeWeightedDigraph G, int s)
+    {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+        pq = new IndexMinPQ<Double>(G.V());
+
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        pq.insert(s, 0.0);
+        while (!pq.isEmpty())
+        {
+            int v = pq.delMin();
+            for (DirectedEdge e : G.adj(v))
+                relax(e);
+        }
+    }
+
+    private void relax(DirectedEdge e)
+    {
+        int v = e.from(), w = e.to();
+        if (distTo[w] > distTo[v] + e.weight())
+        {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+            if (pq.contains(w))   pq.decreaseKey(w, distTo[w]);
+            else                  pq.insert (w, distTo[w]);
+        }  
+    }
 
 }
