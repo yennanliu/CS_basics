9996a8c9837b0cbbb53d7fbc5fc5ec767d9f8f83
yennanliu <f339339@gmail.com>
2020-07-21 11:44:45 +0800

    add algorithm/java/DijkstraSP.java

---
+++ algorithm/java/DijkstraSP.java
@@ -0,0 +1,9 @@
+// https://www.coursera.org/learn/algorithms-part2/lecture/2e9Ic/dijkstras-algorithm
+
+public class DijkstraSP
+{
+	private DirectedEdge[] edgeTo;
+	private double[] distTo;
+	private IndexMinPQ<double> pq;
+
+}
