18a59258dbedae387001a64a5576ebffc0350f58
yennanliu <f339339@gmail.com>
2020-07-24 18:40:24 +0800

    [update code/status] /Greedy/non-overlapping-intervals.py, README

--- leetcode_python/Greedy/non-overlapping-intervals.py
+++ leetcode_python/Greedy/non-overlapping-intervals.py
@@ -1,4 +1,18 @@
 # V0 
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        if not intervals: return 0
+        intervals.sort(key = lambda x : x[0])
+        last = 0
+        res = 0
+        for i in range(1, len(intervals)):
+            if intervals[last][1] > intervals[i][0]:
+                if intervals[i][1] < intervals[last][1]:
+                    last = i
+                res += 1
+            else:
+                last = i
+        return res
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/82728387
@@ -25,6 +39,53 @@ class Solution(object):
             else:
                 last = i
         return res
+
+### Test case : dev
+
+# V1'
+# https://leetcode.com/problems/non-overlapping-intervals/discuss/91721/Short-Ruby-and-Python
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        end = float('-inf')
+        erased = 0
+        for i in sorted(intervals, key=lambda i: i.end):
+            if i.start >= end:
+                end = i.end
+            else:
+                erased += 1
+        return erased
+
+# V1''
+# https://leetcode.com/problems/non-overlapping-intervals/discuss/91768/Python-greedy-solution-with-explanation
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        if not intervals: return 0
+        intervals.sort(key=lambda x: x.start)  # sort on start time
+        currEnd, cnt = intervals[0].end, 0
+        for x in intervals[1:]:
+            if x.start < currEnd:  # find overlapping interval
+                cnt += 1
+                currEnd = min(currEnd, x.end)  # erase the one with larger end time
+            else:
+                currEnd = x.end   # update end time
+        return cnt
+
+# V1'''
+# https://leetcode.com/problems/non-overlapping-intervals/discuss/276056/Python-Greedy-Interval-Scheduling
+# IDEA : GREEDY
+class Solution(object):
+    def eraseOverlapIntervals(intervals):
+        end, cnt = float('-inf'), 0
+        for i in sorted(intervals, key=lambda x: x[1]):
+            if i[0] >= end: 
+                end = i[1]
+            else: 
+                cnt += 1
+        return cnt
 
 # V2 
 # Time:  O(nlogn)
