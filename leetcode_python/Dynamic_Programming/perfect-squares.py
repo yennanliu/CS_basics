7f278cbb2f64295ec868a54b9a18d6a3e91b57df
yennanliu <f339339@gmail.com>
2020-07-25 14:50:26 +0800

    [update code/status] /Dynamic_Programming/perfect-squares.py, README

--- leetcode_python/Dynamic_Programming/perfect-squares.py
+++ leetcode_python/Dynamic_Programming/perfect-squares.py
@@ -1,5 +1,28 @@
-# V0 
-import collections
+# V0
+# IDEA : BFS 
+class Solution(object):
+    def numSquares(self, n):
+        if n < 2:
+            return n
+        lst = []
+        i = 1
+        while i * i <= n:
+            lst.append( i * i )
+            i += 1
+        cnt = 0
+        toCheck = {n}
+        while toCheck:
+            cnt += 1
+            temp = set()
+            for x in toCheck:
+                for y in lst:
+                    if x == y:
+                        return cnt
+                    if x < y:
+                        break
+                    temp.add(x-y)
+            toCheck = temp
+        return cnt
 
 # V1 
 # http://bookshadow.com/weblog/2015/09/09/leetcode-perfect-squares/
@@ -23,8 +46,12 @@ class Solution(object):
                 y += 1
         return dp[n]
 
+### Test case : dev
+
 # V1'
+# IDEA : DP
 # http://bookshadow.com/weblog/2015/09/09/leetcode-perfect-squares/
+# https://leetcode.com/problems/perfect-squares/discuss/71512/Static-DP-C%2B%2B-12-ms-Python-172-ms-Ruby-384-ms
 class Solution(object):
     _dp = [0]
     def numSquares(self, n):
@@ -32,6 +59,34 @@ class Solution(object):
         while len(dp) <= n:
             dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1
         return dp[n]
+
+# V1''
+# IDEA : BFS
+# https://leetcode.com/problems/perfect-squares/discuss/71475/Short-Python-solution-using-BFS
+# IDEA : -> CHECK the LC discussion pic (BFS)
+class Solution(object):
+    def numSquares(self, n):
+        if n < 2:
+            return n
+        lst = []
+        i = 1
+        while i * i <= n:
+            lst.append( i * i )
+            i += 1
+        cnt = 0
+        toCheck = {n}
+        while toCheck:
+            cnt += 1
+            temp = set()
+            for x in toCheck:
+                for y in lst:
+                    if x == y:
+                        return cnt
+                    if x < y:
+                        break
+                    temp.add(x-y)
+            toCheck = temp
+        return cnt
 
 # V2 
 # Time:  O(n * sqrt(n))
