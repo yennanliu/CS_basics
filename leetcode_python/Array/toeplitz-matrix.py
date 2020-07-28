bc7d01b4ee78204ecdc7c8b50ee8d7285a5638e0
yennanliu <f339339@gmail.com>
2020-07-28 17:47:01 +0800

    [update code/status] Array/toeplitz-matrix.py, README

--- leetcode_python/Array/toeplitz-matrix.py
+++ leetcode_python/Array/toeplitz-matrix.py
@@ -30,6 +30,14 @@ The diagonal "[1, 2]" has different elements.
 """
 
 # V0 
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        m, n = len(matrix), len(matrix[0])
+        for i in range(m-1):
+            for j in range(n-1):
+                if matrix[i][j] != matrix[i+1][j+1]:
+                    return False
+        return True
 
 # V1 
 # https://blog.csdn.net/mario_mmh/article/details/79940837
@@ -44,6 +52,8 @@ class Solution(object):
                 if matrix[i][j] != matrix[i+1][j+1]:
                     return False
         return True
+
+### Test case : dev
 
 # V1' 
 # http://bookshadow.com/weblog/2018/01/21/leetcode-toeplitz-matrix/
@@ -60,7 +70,34 @@ class Solution(object):
                 vmap[y - x].add(matrix[x][y])
                 if len(vmap[y - x]) > 1: return False
         return True
-        
+
+# V1''
+# https://leetcode.com/problems/toeplitz-matrix/solution/
+# IDEA : GROUP BY CATEGORY
+# Time Complexity: O(M*N)
+# Space Complexity: O(M+N)
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        groups = {}
+        for r, row in enumerate(matrix):
+            for c, val in enumerate(row):
+                if r-c not in groups:
+                    groups[r-c] = val
+                elif groups[r-c] != val:
+                    return False
+        return True
+
+# V1'''
+# https://leetcode.com/problems/toeplitz-matrix/solution/
+# IDEA :  Compare With Top-Left Neighbor
+# Time Complexity: O(M*N)
+# Space Complexity: O(1)
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        return all(r == 0 or c == 0 or matrix[r-1][c-1] == val
+                   for r, row in enumerate(matrix)
+                   for c, val in enumerate(row))
+
 # V2 
 # Time:  O(m * n)
 # Space: O(1)
