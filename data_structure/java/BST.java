fd51a244682f08a2922d732e4e5e19135995a7d1
yennanliu <f339339@gmail.com>
2020-08-02 15:19:44 +0800

    [update BST.java] data_structure/java/BST.java, add 	doc/pic/BST.png

--- data_structure/java/BST.java
+++ data_structure/java/BST.java
@@ -2,7 +2,6 @@
 
 import java.util.LinkedList;
 
- 
 public class BST<Key extends Comparable<Key>, Value>
 {
     private Node root;
@@ -20,11 +19,32 @@ public class BST<Key extends Comparable<Key>, Value>
 
     }
         
+    // put method via recursive way
     public void put(Key key, Value val)
-    {}
+    { root = put(root, key, val); }
+
+    private Node put(Node x, Key key, Value val)
+    {
+      if (x == null) return new Node(key, val);
+      int cmp = key.copmpareTo(x.key);
+      if (cmp < 0) x.left = put(x.left, key, val);
+      else if (cmp > 0) x.right = put(x.right, key, val);
+      else if (cmp == 0) x.val = val;
+      return x;
+    }
 
     public void get(Key key)
-    {}
+    { 
+      Node x = root;
+      while (x != null)
+      {
+        int cmp = key.copmpareTo(x.key);
+        if (cmp < 0) x = x.left;
+        else if (cmp > 0) x = x.right;
+        else if (cmp == 0) return x.val; 
+      } 
+      return null;
+    } 
 
     public void delete(Key key)
      {}
@@ -33,4 +53,3 @@ public class BST<Key extends Comparable<Key>, Value>
     {}
 
 }
-
