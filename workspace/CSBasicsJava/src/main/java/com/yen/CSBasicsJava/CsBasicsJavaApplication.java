6c01afe5fe302448c9baa52fc9dce0a794cd867c
yennanliu <f339339@gmail.com>
2020-12-12 14:41:10 +0800

    add workspace/ (java

---
+++ workspace/CSBasicsJava/src/main/java/com/yen/CSBasicsJava/CsBasicsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.yen.CSBasicsJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsBasicsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsBasicsJavaApplication.class, args);
+	}
+
+}
