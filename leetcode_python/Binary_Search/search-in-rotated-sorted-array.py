5921eb7080c48fe0dfa9496982c95882815437c7
yennj12 <f339339@gmail.com>
2021-05-25 14:15:10 +0800

    Update search-in-rotated-sorted-array.py

--- leetcode_python/Binary_Search/search-in-rotated-sorted-array.py
+++ leetcode_python/Binary_Search/search-in-rotated-sorted-array.py
@@ -1,3 +1,38 @@
+"""
+
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is rotated 
+at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+You must write an algorithm with O(log n) runtime complexity.
+
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
+
+"""
 # V0
 class Solution(object):
     def search(self, nums, target):
@@ -12,7 +47,8 @@ class Solution(object):
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            # case 1 :  descending  mid  ascending
+            # case 1 :  nums[mid] ~ nums[right] is ordering
+            # all we need to do is : 1) check if target is within mid - right, and move the left or right pointer
             if nums[mid] < nums[right]:
                 # mind NOT use (" nums[mid] < target <= nums[right]")
                 # mind the "<="
@@ -20,7 +56,8 @@ class Solution(object):
                     left = mid + 1
                 else:
                     right = mid - 1
-            # case 2 :  ascending  mid  descending  
+            # case 2 :  nums[left] ~ nums[mid] is ordering
+            # all we need to do is : 1) check if target is within left - mid, and move the left or right pointer
             else:
                 # # mind NOT use (" nums[left] <= target < nums[mid]")
                 # mind the "<="
