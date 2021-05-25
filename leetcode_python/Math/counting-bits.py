155a413b275fc7bd64933420f53fe0d86b2fe204
yennj12 <f339339@gmail.com>
2021-05-25 15:26:14 +0800

    Update counting-bits.py

--- leetcode_python/Math/counting-bits.py
+++ leetcode_python/Math/counting-bits.py
@@ -1,3 +1,55 @@
+"""
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
+ans[i] is the number of 1's in the binary representation of i.
+
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+
+Constraints:
+
+0 <= n <= 105
+ 
+
+Follow up:
+
+It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+Could you solve it in O(n) space complexity?
+Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+
+"""
+
+# V0
+class Solution:
+    def countBits(self,string_num):
+        result = []
+        for i in range(string_num + 1):
+            result.append(self.get_one_from_binar(i))
+        return result
+
+    def get_one_from_binar(self, x):
+        tmp = bin(x)
+        return tmp.count('1') 
+
 # V1 
 class Solution(object):
 	# python dec to bin 
@@ -30,7 +82,6 @@ class Solution(object):
 # V2 
 # Time:  O(n)
 # Space: O(n)
-
 class Solution(object):
     def countBits(self, num):
         """
