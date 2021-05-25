963b6fb16f6c6ad653edeeba7e658821855ac41b
yennj12 <f339339@gmail.com>
2021-05-25 14:39:04 +0800

    Update container-with-most-water.py

--- leetcode_python/Greedy/container-with-most-water.py
+++ leetcode_python/Greedy/container-with-most-water.py
@@ -1,4 +1,55 @@
+"""
+Given n non-negative integers a1, a2, ..., an , 
+where each represents a point at coordinate (i, ai). 
+n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, 
+which, together with the x-axis forms a container, such that the container contains the most water.
+
+Notice that you may not slant the container.
+
+Example 1
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+
+Example 3:
+
+Input: height = [4,3,2,1,4]
+Output: 16
+
+Example 4:
+
+Input: height = [1,2,1]
+Output: 2
+ 
+
+Constraints:
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
+
+"""
+
 # V0 
+# IDEA : TWO POINTERS 
+class Solution(object):
+    def maxArea(self, height):
+        ans = 0
+        l = 0
+        r = len(height) - 1
+        while l < r:
+            ans = max(ans, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return ans
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/82822939
