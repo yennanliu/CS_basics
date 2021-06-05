428e21003f6151ca3ecd27c608f07b1fec465e7a
yennanliu <f339339@gmail.com>
2021-06-05 08:24:44 +0800

    add 057 insert-interval.py, update progress

---
+++ leetcode_python/Array/insert-interval.py
@@ -0,0 +1,77 @@
+"""
+
+Given a set of non-overlapping intervals, 
+insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+Example 3:
+
+Input: intervals = [], newInterval = [5,7]
+Output: [[5,7]]
+
+Example 4:
+
+Input: intervals = [[1,5]], newInterval = [2,3]
+Output: [[1,5]]
+
+Example 5:
+
+Input: intervals = [[1,5]], newInterval = [2,7]
+Output: [[1,7]]
+ 
+
+Constraints:
+
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= intervals[i][0] <= intervals[i][1] <= 105
+intervals is sorted by intervals[i][0] in ascending order.
+newInterval.length == 2
+0 <= newInterval[0] <= newInterval[1] <= 105
+
+"""
+
+# V0
+# IDEA : compare merged[-1][1]. interval[0]
+# https://leetcode.com/problems/insert-interval/discuss/1236101/Python3-Easy-to-Understand-Solution
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x:x[0])
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1]<interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1]= max(merged[-1][1],interval[1])
+        return merged
+
+# V1
+class Solution:
+    def insert(self, intervals, newInterval):
+        res= []
+        intervals.append(newInterval)
+        intervals= sorted(intervals)
+        cur_interval = intervals[0]
+        for i in range(1, len(intervals)):
+            if cur_interval[1]>=intervals[i][0]:
+                cur_interval[1]= max(cur_interval[1], intervals[i][1])
+            else:
+                res.append(cur_interval)
+                cur_interval=intervals[i]
+        res.append(cur_interval)
+        return res
