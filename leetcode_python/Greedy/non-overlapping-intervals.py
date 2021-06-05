9c66a64ee7e64875e4bbfd51cd661c86a58cba72
yennj12 <f339339@gmail.com>
2021-06-05 08:55:59 +0800

    Update non-overlapping-intervals.py

--- leetcode_python/Greedy/non-overlapping-intervals.py
+++ leetcode_python/Greedy/non-overlapping-intervals.py
@@ -1,4 +1,40 @@
-# V0 
+"""
+
+Given an array of intervals intervals where intervals[i] = [starti, endi], 
+return the minimum number of intervals you need to remove 
+to make the rest of the intervals non-overlapping.
+
+
+
+Example 1:
+
+Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+
+Example 2:
+
+Input: intervals = [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+
+Example 3:
+
+Input: intervals = [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 2 * 104
+intervals[i].length == 2
+-2 * 104 <= starti < endi <= 2 * 104
+
+"""
+
+# V0
+# IDEA : 2 POINTERS
 class Solution(object):
     def eraseOverlapIntervals(self, intervals):
         if not intervals: return 0
