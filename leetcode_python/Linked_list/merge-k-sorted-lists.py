ffe3d3a2805ffa5febce1a5c21b83648861c7b05
yennj12 <f339339@gmail.com>
2021-06-05 12:36:14 +0800

    Update merge-k-sorted-lists.py

--- leetcode_python/Linked_list/merge-k-sorted-lists.py
+++ leetcode_python/Linked_list/merge-k-sorted-lists.py
@@ -1,18 +1,125 @@
-# Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
-#
-# Example:
-#
-# Input:
-# [
-#   1->4->5,
-#   1->3->4,
-#   2->6
-# ]
-# Output: 1->1->2->3->4->4->5->6
+"""
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length won't exceed 10^4.
+
+"""
 # V0 
 
 # V1
+# IDEA : BRUTE FORCE
+# https://leetcode.com/problems/merge-k-sorted-lists/solution/
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        self.nodes = []
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for x in sorted(self.nodes):
+            point.next = ListNode(x)
+            point = point.next
+        return head.next
+
+# V2
+# IDEA : Optimize Approach 2 by Priority Queue
+# https://leetcode.com/problems/merge-k-sorted-lists/solution/
+from Queue import PriorityQueue
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put((l.val, l))
+        while not q.empty():
+            val, node = q.get()
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                q.put((node.val, node))
+        return head.next
+
+# V3
+# IDEA : Merge with Divide And Conquer
+# https://leetcode.com/problems/merge-k-sorted-lists/solution/
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else None
+
+    def merge2Lists(self, l1, l2):
+        head = point = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l1
+                l1 = point.next.next
+            point = point.next
+        if not l1:
+            point.next=l2
+        else:
+            point.next=l1
+        return head.next
+
+# V4
 # https://blog.csdn.net/fuxuemingzhu/article/details/83068632
 # IDEA : HEAP SAVE KEY & VALUE
 import heapq
@@ -42,7 +149,7 @@ class Solution:
 
 ### Test case : dev 
 
-# V1'
+# V5
 # https://blog.csdn.net/fuxuemingzhu/article/details/83068632
 # IDEA : HEAP SAVE VALUE AND NODE 
 class Solution(object):
@@ -67,7 +174,7 @@ class Solution(object):
                 heapq.heappush(heap, (curHead.val, curHead))
         return head.next
 
-# V1''
+# V6
 # https://blog.csdn.net/fuxuemingzhu/article/details/83068632
 # IDEA : BRUTE FORCE -> TIME OUT ERROR
 # Definition for singly-linked list.
@@ -100,7 +207,7 @@ class Solution:
             lists[curIndex] = curHead
         return head.next
 
-# V1'''
+# V7
 # https://blog.csdn.net/qian2729/article/details/50528385
 class Solution(object):
     def mergeKLists(self, lists):
@@ -122,5 +229,3 @@ class Solution(object):
             if h.next:
                 heapq.heappush(heap,(h.next.val,h.next))
         return dummy.next
-
-# V2
