52b290721c38eace35630ebe83fc577da2b67234
yennj12 <f339339@gmail.com>
2021-06-05 12:20:02 +0800

    Update merge-two-sorted-lists.py

--- leetcode_python/Linked_list/merge-two-sorted-lists.py
+++ leetcode_python/Linked_list/merge-two-sorted-lists.py
@@ -1,4 +1,34 @@
-# V0 
+"""
+
+Merge two sorted linked lists and return it as a sorted list. 
+The list should be made by splicing together the nodes of the first two lists.
+
+
+Example 1:
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: l1 = [], l2 = []
+Output: []
+
+Example 3:
+
+Input: l1 = [], l2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both l1 and l2 are sorted in non-decreasing order.
+
+"""
+# V0
+# IDEA : LOOP 2 LINKED LISTS
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
         """
