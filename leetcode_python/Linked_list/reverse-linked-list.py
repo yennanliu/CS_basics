335d327708c073ec77d33b1ed8ad2ec8e89db178
yennj12 <f339339@gmail.com>
2021-06-05 11:41:21 +0800

    Update reverse-linked-list.py

--- leetcode_python/Linked_list/reverse-linked-list.py
+++ leetcode_python/Linked_list/reverse-linked-list.py
@@ -1,6 +1,35 @@
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+"""
+
 # V0 
 # https://github.com/yennanliu/CS_basics/blob/master/data_structure/python/linkedList.py
-# IDEA : ITERATION
+# IDEA : Linkedlist basics
 class Solution:
     def reverseList(self, head: ListNode):
         prev = None
