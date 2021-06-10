65c6167537e71194b2aacb01d90e78d36886896f
yennj12 <f339339@gmail.com>
2021-06-11 00:45:07 +0800

    Update remove-nth-node-from-end-of-list.py

--- leetcode_python/Two_Pointers/remove-nth-node-from-end-of-list.py
+++ leetcode_python/Two_Pointers/remove-nth-node-from-end-of-list.py
@@ -15,25 +15,30 @@ Follow up:
 Could you do this in one pass?
 
 """
-# Time:  O(n)
-# Space: O(1)
-#
-# Given a linked list, remove the nth node from the end of list and return its head.
-#
-# For example,
-#
-#    Given linked list: 1->2->3->4->5, and n = 2.
-#
-#    After removing the second node from the end, the linked list becomes 1->2->3->5.
-# Note:
-# Given n will always be valid.
-# Try to do this in one pass.
-#
 
 # V0
+# IDEA : FAST-SLOW POINTERS
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        new_head = ListNode(0)
+        new_head.next = head
+        fast = slow = new_head
+        for i in range(n+1):
+            fast = fast.next
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return new_head.next
 
 
-# V1 
+# V1
+# IDEA : FAST-SLOW POINTERS
 # https://blog.csdn.net/coder_orz/article/details/51691267
 class Solution(object):
     def removeNthFromEnd(self, head, n):
@@ -68,7 +73,8 @@ class Solution(object):
         tmp.next = tmp.next.next
         return res.next
 
-# V1'' 
+# V1''
+# IDEA : MOVE last ~ n node
 # https://blog.csdn.net/coder_orz/article/details/51691267
 class Solution(object):
     def removeNthFromEnd(self, head, n):
