c631a7b770bd7f223324c00c4c8e188a3b293ff8
yennj12 <f339339@gmail.com>
2021-06-11 17:01:42 +0800

    Update word-search.py

--- leetcode_python/Backtracking/word-search.py
+++ leetcode_python/Backtracking/word-search.py
@@ -40,6 +40,36 @@ board and word consists of only lowercase and uppercase English letters.
 # V0
 # IDEA : DFS
 class Solution(object):
+ 
+    def exist(self, board, word):
+        visited = [[False for j in range(len(board[0]))] for i in range(len(board))]
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.existRecu(board, word, 0, i, j, visited):
+                    return True
+
+        return False
+
+    def existRecu(self, board, word, cur, i, j, visited):
+        if cur == len(word):
+            return True
+
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or visited[i][j] or board[i][j] != word[cur]:
+            return False
+
+        visited[i][j] = True
+        result = self.existRecu(board, word, cur + 1, i + 1, j, visited) or\
+                 self.existRecu(board, word, cur + 1, i - 1, j, visited) or\
+                 self.existRecu(board, word, cur + 1, i, j + 1, visited) or\
+                 self.existRecu(board, word, cur + 1, i, j - 1, visited)
+        visited[i][j] = False
+
+        return result
+       
+# V0'
+# IDEA : DFS
+class Solution(object):
     def exist(self, board, word):
         """
         :type board: List[List[str]]
