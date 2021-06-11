e202ab0f61aa4807127d0fbcaf6389cc823a1966
yennj12 <f339339@gmail.com>
2021-06-11 16:52:51 +0800

    Update word-search.py

--- leetcode_python/Backtracking/word-search.py
+++ leetcode_python/Backtracking/word-search.py
@@ -1,4 +1,44 @@
-# V0 
+"""
+
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells,
+where adjacent cells are horizontally or vertically neighboring. 
+The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+ 
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+
+"""
+
+# V0
+# IDEA : DFS
 class Solution(object):
     def exist(self, board, word):
         """
@@ -19,10 +59,10 @@ class Solution(object):
             return False
         if board[y][x] != word[i]:
             return False
-        board[y][x] = "#" # to mark if the route already been passed (.swapcase(), e.g. A->a)
-        isexit =  self.dfs(board, word, x + 1, y, i + 1) or self.exit(board, word, x, y + 1, i + 1) or self.exit(board, word, x - 1, y, i + 1) or self.exit(board, word, x, y - 1, i + 1)
-        board[y][x] = "#" # if already visited all possible route within the route collection, then roll back the maked route (.swapcase(), e.g. a->A), and run the other visit again 
-        return isexit
+        board[y][x] = board[y][x].swapcase() # to mark if the route already been passed (.swapcase(), e.g. A->a)
+        isExit =  self.dfs(board, word, x + 1, y, i + 1) or self.dfs(board, word, x, y + 1, i + 1) or self.dfs(board, word, x - 1, y, i + 1) or self.dfs(board, word, x, y - 1, i + 1)
+        board[y][x] = board[y][x].swapcase() # if already visited all possible route within the route collection, then roll back the maked route (.swapcase(), e.g. a->A), and run the other visit again 
+        return isExit
 
 # V1
 # https://blog.csdn.net/fuxuemingzhu/article/details/79386066
