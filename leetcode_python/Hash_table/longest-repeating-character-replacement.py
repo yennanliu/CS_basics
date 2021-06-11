de6d0e517b7a8c3c3b074060b9a0702af117ade5
yennj12 <f339339@gmail.com>
2021-06-11 18:28:46 +0800

    Update longest-repeating-character-replacement.py

--- leetcode_python/Hash_table/longest-repeating-character-replacement.py
+++ leetcode_python/Hash_table/longest-repeating-character-replacement.py
@@ -1,4 +1,59 @@
-# V0 
+"""
+
+You are given a string s and an integer k. 
+You can choose any character of the string and change it to any other uppercase English character. 
+You can perform this operation at most k times.
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only uppercase English letters.
+0 <= k <= s.length
+
+"""
+
+# V0
+# IDEA : SLIDING WINDOW + DICT
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        table = collections.Counter()
+        res = 0
+        p1 = p2 = 0
+        while p2 < len(s):
+            table[s[p2]] += 1
+            p2 += 1
+            # NOTE : if remain elements > k, means there is no possibility to make this substring as "longest substring containing the same letter"
+            #       ->  remain elements = p1 - p2 - max(table.values())
+            #       ->  so we need to clean the "potentail candidate" for next iteration
+            while p2 - p1 - max(table.values()) > k:
+                table[s[p1]] -= 1
+                p1 += 1
+            res = max(res, p2 - p1)
+        return res
+    
+# V0'
 from collections import defaultdict
 class Solution:
     """
