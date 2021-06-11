53db67cecbc0b860cadab84904355ea2e9459875
yennanliu <f339339@gmail.com>
2021-06-11 19:07:58 +0800

    add minimum_window_substring.py (again

---
+++ leetcode_python/String/minimum_window_substring.py
@@ -0,0 +1,162 @@
+"""
+
+Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+Example 2:
+
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+
+Example 3:
+
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+ 
+
+Constraints:
+
+m == s.length
+n == t.length
+1 <= m, n <= 105
+s and t consist of uppercase and lowercase English letters.
+ 
+
+Follow up: Could you find an algorithm that runs in O(m + n) time?
+
+"""
+
+# V0
+
+# V1
+# IDEA : SLIDING WINDOW
+# https://leetcode.com/problems/minimum-window-substring/solution/
+def minWindow(self, s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: str
+    """
+
+    if not t or not s:
+        return ""
+
+    # Dictionary which keeps a count of all the unique characters in t.
+    dict_t = Counter(t)
+
+    # Number of unique characters in t, which need to be present in the desired window.
+    required = len(dict_t)
+
+    # left and right pointer
+    l, r = 0, 0
+
+    # formed is used to keep track of how many unique characters in t are present in the current window in its desired frequency.
+    # e.g. if t is "AABC" then the window must have two A's, one B and one C. Thus formed would be = 3 when all these conditions are met.
+    formed = 0
+
+    # Dictionary which keeps a count of all the unique characters in the current window.
+    window_counts = {}
+
+    # ans tuple of the form (window length, left, right)
+    ans = float("inf"), None, None
+
+    while r < len(s):
+
+        # Add one character from the right to the window
+        character = s[r]
+        window_counts[character] = window_counts.get(character, 0) + 1
+
+        # If the frequency of the current character added equals to the desired count in t then increment the formed count by 1.
+        if character in dict_t and window_counts[character] == dict_t[character]:
+            formed += 1
+
+        # Try and contract the window till the point where it ceases to be 'desirable'.
+        while l <= r and formed == required:
+            character = s[l]
+
+            # Save the smallest window until now.
+            if r - l + 1 < ans[0]:
+                ans = (r - l + 1, l, r)
+
+            # The character at the position pointed by the `left` pointer is no longer a part of the window.
+            window_counts[character] -= 1
+            if character in dict_t and window_counts[character] < dict_t[character]:
+                formed -= 1
+
+            # Move the left pointer ahead, this would help to look for a new window.
+            l += 1    
+
+        # Keep expanding the window once we are done contracting.
+        r += 1    
+    return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+
+# V1'
+# IDEA : OPTIMIZED SLIDING WINDOW
+# https://leetcode.com/problems/minimum-window-substring/solution/
+def minWindow(self, s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: str
+    """
+    if not t or not s:
+        return ""
+
+    dict_t = Counter(t)
+
+    required = len(dict_t)
+
+    # Filter all the characters from s into a new list along with their index.
+    # The filtering criteria is that the character should be present in t.
+    filtered_s = []
+    for i, char in enumerate(s):
+        if char in dict_t:
+            filtered_s.append((i, char))
+
+    l, r = 0, 0
+    formed = 0
+    window_counts = {}
+
+    ans = float("inf"), None, None
+
+    # Look for the characters only in the filtered list instead of entire s. This helps to reduce our search.
+    # Hence, we follow the sliding window approach on as small list.
+    while r < len(filtered_s):
+        character = filtered_s[r][1]
+        window_counts[character] = window_counts.get(character, 0) + 1
+
+        if window_counts[character] == dict_t[character]:
+            formed += 1
+
+        # If the current window has all the characters in desired frequencies i.e. t is present in the window
+        while l <= r and formed == required:
+            character = filtered_s[l][1]
+
+            # Save the smallest window until now.
+            end = filtered_s[r][0]
+            start = filtered_s[l][0]
+            if end - start + 1 < ans[0]:
+                ans = (end - start + 1, start, end)
+
+            window_counts[character] -= 1
+            if window_counts[character] < dict_t[character]:
+                formed -= 1
+            l += 1    
+
+        r += 1    
+    return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+
+# V2
