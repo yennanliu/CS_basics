27f821fc0edf32c8fb78ccbc25ea48299fc38899
yennj12 <f339339@gmail.com>
2021-06-11 08:03:24 +0800

    Update spiral-matrix.py

--- leetcode_python/Array/spiral-matrix.py
+++ leetcode_python/Array/spiral-matrix.py
@@ -1,18 +1,30 @@
-# Time:  O(m * n)
-# Space: O(1)
-#
-# Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
-#
-# For example,
-# Given the following matrix:
-#
-# [
-#  [ 1, 2, 3 ],
-#  [ 4, 5, 6 ],
-#  [ 7, 8, 9 ]
-# ]
-# You should return [1,2,3,6,9,8,7,4,5].
-#
+"""
+
+Given an m x n matrix, 
+return all elements of the matrix in spiral order.
+
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+
+"""
 
 # V0
 class Solution:
@@ -120,6 +132,62 @@ assert s.spiralOrder([[1,2,3]]) == [1,2,3]
 assert s.spiralOrder([[1,2,3],[4,5,6]]) == [1,2,3,6,5,4]
 
 # V1'
+# IDEA : SIMULATION
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+# https://leetcode.com/problems/spiral-matrix/solution/
+class Solution(object):
+    def spiralOrder(self, matrix):
+        if not matrix: return []
+        R, C = len(matrix), len(matrix[0])
+        seen = [[False] * C for _ in matrix]
+        ans = []
+        dr = [0, 1, 0, -1]
+        dc = [1, 0, -1, 0]
+        r = c = di = 0
+        for _ in range(R * C):
+            ans.append(matrix[r][c])
+            seen[r][c] = True
+            cr, cc = r + dr[di], c + dc[di]
+            if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
+                r, c = cr, cc
+            else:
+                di = (di + 1) % 4
+                r, c = r + dr[di], c + dc[di]
+        return ans
+
+# V1''
+# IDEA : Layer-by-Layer
+# Time Complexity: O(N)
+# Space Complexity:
+#   -> O(1)O(1) without considering the output array, since we don't use any additional data structures for our computations.
+#   -> O(N)O(N) if the output array is taken into account.
+# https://leetcode.com/problems/spiral-matrix/solution/
+class Solution(object):
+    def spiralOrder(self, matrix):
+        def spiral_coords(r1, c1, r2, c2):
+            for c in range(c1, c2 + 1):
+                yield r1, c
+            for r in range(r1 + 1, r2 + 1):
+                yield r, c2
+            if r1 < r2 and c1 < c2:
+                for c in range(c2 - 1, c1, -1):
+                    yield r2, c
+                for r in range(r2, r1, -1):
+                    yield r, c1
+
+        if not matrix: return []
+        ans = []
+        r1, r2 = 0, len(matrix) - 1
+        c1, c2 = 0, len(matrix[0]) - 1
+        while r1 <= r2 and c1 <= c2:
+            for r, c in spiral_coords(r1, c1, r2, c2):
+                ans.append(matrix[r][c])
+            r1 += 1; r2 -= 1
+            c1 += 1; c2 -= 1
+        return ans
+
+# V1'''
 # https://www.cnblogs.com/zuoyuan/p/3769829.html
 # https://blog.csdn.net/qian2729/article/details/50539281
 # IDEA : STATUS + POINTER 
