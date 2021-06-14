3a2d99880fc514600cebda3fe71040414cb796e9
yennj12 <f339339@gmail.com>
2021-06-14 12:58:07 +0800

    Update same-tree.py

--- leetcode_python/Recursion/same-tree.py
+++ leetcode_python/Recursion/same-tree.py
@@ -1,42 +1,40 @@
-# 100. Same Tree
-# Easy
-#
-# 56
-#
-# Add to List
-#
-# Share
-# Given two binary trees, write a function to check if they are the same or not.
-#
-# Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
-#
-# Example 1:
-#
-# Input:     1         1
-#           / \       / \
-#          2   3     2   3
-#
-#         [1,2,3],   [1,2,3]
-#
-# Output: true
-# Example 2:
-#
-# Input:     1         1
-#           /           \
-#          2             2
-#
-#         [1,2],     [1,null,2]
-#
-# Output: false
-# Example 3:
-#
-# Input:     1         1
-#           / \       / \
-#          2   1     1   2
-#
-#         [1,2,1],   [1,1,2]
-#
-# Output: false
+"""
+
+Given two binary trees, write a function to check if they are the same or not.
+Two binary trees are considered the same if 
+they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+
+"""
 
 # V0 
 # IDEA : Recursion
@@ -49,9 +47,12 @@ class Solution(object):
                        self.isSameTree(p.right, q.right)
         # if p == None or q == None  or p == q == None
         return p == q == None
+    
+
 
 # V0'
 # IDEA : Iteration (DFS)
+# https://leetcode.com/problems/same-tree/solution/
 class Solution(object):
     def isSameTree(self, p, q):
         def dfs(p, q):
