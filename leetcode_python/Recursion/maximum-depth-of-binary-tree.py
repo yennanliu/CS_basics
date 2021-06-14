b4459c9c636b9d007c169bdcad1a64977e413a35
yennj12 <f339339@gmail.com>
2021-06-14 12:22:38 +0800

    Update maximum-depth-of-binary-tree.py

--- leetcode_python/Recursion/maximum-depth-of-binary-tree.py
+++ leetcode_python/Recursion/maximum-depth-of-binary-tree.py
@@ -1,3 +1,39 @@
+"""
+
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the 
+root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+
+Example 3:
+
+Input: root = []
+Output: 0
+
+Example 4:
+
+Input: root = [0]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+
+"""
+
 # V0
 # IDEA : DFS
 class Solution(object):
@@ -7,6 +43,21 @@ class Solution(object):
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
+# V0'
+# IDEA : DFS
+class Solution:
+    def maxDepth(self, root):
+        self.layer_final = 0
+        self.dfs(root, 0, self.layer_final)
+        return self.layer_final
+        
+    def dfs(self, root, layer, layer_final):
+        if root:
+            if layer + 1 > layer_final:
+                self.layer_final += 1
+            self.dfs(root.left,  layer + 1, self.layer_final)
+            self.dfs(root.right, layer + 1, self.layer_final)
+            
 # V1
 # https://blog.csdn.net/coder_orz/article/details/51337420
 # IDEA : DFS
