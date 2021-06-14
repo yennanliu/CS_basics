bbdff4a6189063f47a8c17894fb407892a072959
yennj12 <f339339@gmail.com>
2021-06-14 15:03:13 +0800

    Update binary-tree-level-order-traversal.py

--- leetcode_python/Breadth-First-Search/binary-tree-level-order-traversal.py
+++ leetcode_python/Breadth-First-Search/binary-tree-level-order-traversal.py
@@ -1,5 +1,30 @@
 """
-Given a binary tree, return the level order traversal of its nodes’ values. (ie, from left to right, level by level).
+Given a binary tree, return the level order traversal of its nodes’ values. 
+(ie, from left to right, level by level).
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+
+
 
 For example:
 
