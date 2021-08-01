7798534ffb8c80137f2d05528d45dfd2e578468d
yennanliu <f339339@gmail.com>
2021-08-01 16:21:20 +0800

    update 572

--- leetcode_python/Tree/subtree-of-another-tree.py
+++ leetcode_python/Tree/subtree-of-another-tree.py
@@ -1,4 +1,56 @@
-# V0 
+"""
+
+Given the root of a binary tree, construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way, and return it.
+
+Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4]
+Output: "1(2(4))(3)"
+Explanation: Originallay it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+Example 2:
+
+
+Input: root = [1,2,3,null,4]
+Output: "1(2()(4))(3)"
+Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-1000 <= Node.val <= 1000
+Accepted
+109,899
+Submissions
+194,930
+
+"""
+
+# V0 (### TO FIX)
+# IDEA : tree -> string, and check if sub string
+# # https://blog.csdn.net/fuxuemingzhu/article/details/71440802
+# class Solution(object):
+#     def isSubtree(self, s, t):
+#         r_s = self.dfs(s, ",")
+#         r_t = self.dfs(t, ",")
+#         if r_t == None and r_s == None:
+#             return True
+#         return r_t in r_s
+#
+#     def dfs(self, root, tmp):
+#         if root == None:
+#             tmp += "#,"
+#             return
+#         self.dfs(root.left, tmp + str(root.val) + ",")
+#         self.dfs(root.right,tmp + str(root.val) + ",")
+
+
+# V0' 
 # DFS + DFS 
 class Solution(object):
     def isSubtree(self, s, t):
