a844f943ebadfa8d0bc5f7c0965c12d8ae1fa6bc
yennanliu <f339339@gmail.com>
2021-08-07 13:16:42 +0800

    update 210

--- leetcode_python/Breadth-First-Search/course-schedule-ii.py
+++ leetcode_python/Breadth-First-Search/course-schedule-ii.py
@@ -1,5 +1,44 @@
+"""
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+
+Example 2:
+
+Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
+So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+
+Example 3:
+
+Input: numCourses = 1, prerequisites = []
+Output: [0]
+ 
+
+Constraints:
+
+1 <= numCourses <= 2000
+0 <= prerequisites.length <= numCourses * (numCourses - 1)
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+ai != bi
+All the pairs [ai, bi] are distinct.
+
+"""
+
 # V0 
-# IDEA : DFS + topological sort 
+# IDEA : DFS + topological sort
+# SAME dfs logic as LC 207 (Course Schedule)
 class Solution(object):
     def findOrder(self, numCourses, prerequisites):
         """
@@ -14,6 +53,7 @@ class Solution(object):
         visited = [0] * numCourses
         path = []
         for i in range(numCourses):
+            ### NOTE : if not a valid "prerequisites", then will return NULL list
             if not self.dfs(graph, visited, i, path):
                 return []
         return path
@@ -25,12 +65,14 @@ class Solution(object):
         visited[i] = 1
         for j in graph[i]:
             if not self.dfs(graph, visited, j, path):
+                ### NOTE : the quit condition
                 return False
         visited[i] = 2
         path.append(i)
         return True
 
-# V1 
+# V1
+# IDEA : BFS 
 # http://bookshadow.com/weblog/2015/05/14/leetcode-course-schedule-ii/
 class Solution:
     # @param {integer} numCourses
