2b568dad9d7f47ea80a96ede8dc4cd675e628884
yennanliu <f339339@gmail.com>
2021-08-07 12:02:21 +0800

    update 207, progress

--- leetcode_python/Breadth-First-Search/course-schedule.py
+++ leetcode_python/Breadth-First-Search/course-schedule.py
@@ -34,6 +34,33 @@ All the pairs prerequisites[i] are unique.
 """
 
 # V0
+import collections
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]):
+        _graph = collections.defaultdict(list)
+        for i in range(len(prerequisites)):
+            _graph[prerequisites[i][0]].append(prerequisites[i][1])
+
+        visited = [0] * numCourses
+        for i in range(numCourses):
+            if not self.dfs(_graph, visited, i):
+                return False
+        return True
+
+    # 0 : unknown, 1 :visiting, 2 : visited    
+    def dfs(self, _graph, visited, i):
+        if visited[i] == 1:
+            return False
+        if visited[i] == 2:
+            return True
+        visited[i] = 1
+        for item in _graph[i]:
+            if not self.dfs(_graph, visited, item):
+                return False
+        visited[i] = 2
+        return True
+
+# V0'
 # IDEA : DFS + topological sort 
 import collections
 class Solution(object):
@@ -60,39 +87,35 @@ class Solution(object):
         visited[i] = 2
         return True
 
-# V1 
-# https://www.jiuzhang.com/solution/course-schedule/#tag-highlight-lang-python
-from collections import deque
-class Solution:
-    # @param {int} numCourses a total of n courses
-    # @param {int[][]} prerequisites a list of prerequisite pairs
-    # @return {boolean} true if can finish all courses or false
-    def canFinish(self, numCourses, prerequisites):
-        # Write your code here
-        edges = {i: [] for i in range(numCourses)}
-        degrees = [0 for i in range(numCourses)] 
-        for i, j in prerequisites:
-            edges[j].append(i)
-            degrees[i] += 1
-            
-        queue, count = deque([]), 0
-        
-        for i in range(numCourses):
-            if degrees[i] == 0:
-                queue.append(i)
-
-        while queue:
-            node = queue.popleft()
-            count += 1
-
-            for x in edges[node]:
-                degrees[x] -= 1
-                if degrees[x] == 0:
-                    queue.append(x)
-        return count == numCourses
-
-# V1'
+# V0'' (AGAIN!)
+# IDEA : BFS + topological sort
+class Solution(object):
+    def canFinish(self, N, prerequisites):
+        """
+        :type N,: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        graph = collections.defaultdict(list)
+        indegrees = collections.defaultdict(int)
+        for u, v in prerequisites:
+            graph[v].append(u)
+            indegrees[u] += 1
+        for i in range(N):
+            zeroDegree = False
+            for j in range(N):
+                if indegrees[j] == 0:
+                    zeroDegree = True
+                    break
+            if not zeroDegree: return False
+            indegrees[j] = -1
+            for node in graph[j]:
+                indegrees[node] -= 1
+        return True       
+# V1
 # https://blog.csdn.net/fuxuemingzhu/article/details/82951771
+# diagram explaination:
+# https://leetcode.com/problems/course-schedule/discuss/658379/Python-by-DFS-and-cycle-detection-w-Graph
 # IDEA : DFS + topological sort 
 class Solution(object):
     def canFinish(self, N, prerequisites):
@@ -122,6 +145,37 @@ class Solution(object):
                 return False
         visited[i] = 2
         return True
+
+# V1'
+# https://www.jiuzhang.com/solution/course-schedule/#tag-highlight-lang-python
+from collections import deque
+class Solution:
+    # @param {int} numCourses a total of n courses
+    # @param {int[][]} prerequisites a list of prerequisite pairs
+    # @return {boolean} true if can finish all courses or false
+    def canFinish(self, numCourses, prerequisites):
+        # Write your code here
+        edges = {i: [] for i in range(numCourses)}
+        degrees = [0 for i in range(numCourses)] 
+        for i, j in prerequisites:
+            edges[j].append(i)
+            degrees[i] += 1
+            
+        queue, count = deque([]), 0
+        
+        for i in range(numCourses):
+            if degrees[i] == 0:
+                queue.append(i)
+
+        while queue:
+            node = queue.popleft()
+            count += 1
+
+            for x in edges[node]:
+                degrees[x] -= 1
+                if degrees[x] == 0:
+                    queue.append(x)
+        return count == numCourses
 
 # V1''
 # https://blog.csdn.net/fuxuemingzhu/article/details/82951771
