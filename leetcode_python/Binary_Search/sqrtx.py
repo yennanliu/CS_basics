e304f3dca98cf632eb0fa8162bc75682c007c2cd
yennanliu <f339339@gmail.com>
2021-08-07 19:36:13 +0800

    update 69

--- leetcode_python/Binary_Search/sqrtx.py
+++ leetcode_python/Binary_Search/sqrtx.py
@@ -1,11 +1,52 @@
+"""
+
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+
+"""
+
+# V0
+# IDEA : binary search
+class Solution(object):
+    def mySqrt(self, x):
+        l, r = 0, x
+        mid = (l + r) // 2
+        while r >= l:
+            if mid * mid == x: # optional
+                return mid
+            elif mid * mid > x:
+                ### NOTE THE CONDITION
+                r = mid - 1
+            else:
+                ### NOTE THE CONDITION
+                l = mid + 1
+            mid = (l + r) // 2
+        return mid
+
 # V0 
 # IDEA : binary search 
 class Solution(object):
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
         low, high, mid = 0, x, (x+0)//2
         while low <= high:
             if mid * mid > x:
