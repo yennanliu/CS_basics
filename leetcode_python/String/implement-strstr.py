0b5f06f7f1a3767f87bbcffec8b69067bfaf87b1
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-08-10 17:44:12 +0800

    update 028

--- leetcode_python/String/implement-strstr.py
+++ leetcode_python/String/implement-strstr.py
@@ -1,11 +1,38 @@
-# Time:  O(n + k)
-# Space: O(k)
-#
-# Implement strStr().
-#
-# Returns a pointer to the first occurrence of needle in haystack,
-#  or null if needle is not part of haystack.
-#
+"""
+
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+ 
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+Example 3:
+
+Input: haystack = "", needle = ""
+Output: 0
+ 
+
+Constraints:
+
+0 <= haystack.length, needle.length <= 5 * 104
+haystack and needle consist of only lower-case English characters.
+
+"""
+
 # V0 
 class Solution(object):
     def strStr(self, haystack, needle):
