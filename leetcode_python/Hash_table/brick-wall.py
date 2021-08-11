0c80c110e85fe2d7c906cc64419d2a70cd15510e
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-08-11 16:17:17 +0800

    update 554

--- leetcode_python/Hash_table/brick-wall.py
+++ leetcode_python/Hash_table/brick-wall.py
@@ -1,11 +1,77 @@
-# V0 
-# IDEA : HASH TABLE + COUNTER UPDATE
+"""
+
+There is a rectangular brick wall in front of you with n rows of bricks. The ith row has some number of bricks each of the same height (i.e., one unit) but they can be of different widths. The total width of each row is the same.
+
+Draw a vertical line from the top to the bottom and cross the least bricks. If your line goes through the edge of a brick, then the brick is not considered as crossed. You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
+
+Given the 2D array wall that contains the information about the wall, return the minimum number of crossed bricks after drawing such a vertical line.
+
+
+Example 1:
+
+
+Input: wall = [[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]]
+Output: 2
+Example 2:
+
+Input: wall = [[1],[1],[1]]
+Output: 3
+ 
+
+Constraints:
+
+n == wall.length
+1 <= n <= 104
+1 <= wall[i].length <= 104
+1 <= sum(wall[i].length) <= 2 * 104
+sum(wall[i]) is the same for each row i.
+1 <= wall[i][j] <= 231 - 1
+
+
+"""
+
+# V0
+# IDEA : HASH TABLE + COUNTER UPDATE (looping every element in the list and cumsum and get the max count)
+### DEMO : counter.update()
+# In [87]: _counter = collections.Counter()
+
+# In [88]: _counter
+# Out[88]: Counter()
+
+# In [89]: _counter.update([1])
+
+# In [90]: _counter
+# Out[90]: Counter({1: 1})
+
+# In [91]: _counter.update([1])
+
+# In [92]: _counter
+# Out[92]: Counter({1: 2})
+
+# In [93]: _counter.update([2])
+
+# In [94]: _counter
+# Out[94]: Counter({1: 2, 2: 1})
+import collections
 class Solution(object):
     def leastBricks(self, wall):
-        """
-        :type wall: List[List[int]]
-        :rtype: int
-        """
+        _counter = collections.Counter()
+        count = 0
+        # go through every sub-wall in wall
+        for w in wall:
+            cum_sum = 0
+            # go through every element in sub-wall
+            for i in range(len(w) - 1):
+                cum_sum += w[i]
+                ### NOTE we can update collections.Counter() via below
+                _counter.update([cum_sum])
+                count = max(count, _counter[cum_sum])
+        return len(wall) - count
+
+# V0'
+# IDEA : HASH TABLE + COUNTER UPDATE (looping every element in the list and cumsum and get the max count)
+class Solution(object):
+    def leastBricks(self, wall):
         left_counter = collections.Counter()
         count = 0
         for row in wall:
