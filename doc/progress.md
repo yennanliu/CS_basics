62333300af7c4ade8f5f560a041a6e8446710120
yennanliu <f339339@gmail.com>
2021-08-12 10:01:32 +0800

    update 1097, 1098

--- doc/progress.md
+++ doc/progress.md
@@ -29,6 +29,8 @@
 			- 1082
 			- 1083
 			- 1084
+			- 1097
+			- 1098
 		- AGAIN:
 			- union find
 	- https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-100-LeetCode-Questions-to-Save-Your-Time-OaM1orEU
--- leetcode_SQL/unpopular-books.sql
+++ leetcode_SQL/unpopular-books.sql
@@ -57,7 +57,17 @@
 -- | 5         | "The Hunger Games" |
 -- +-----------+--------------------+
 
-# V0 
+# V0
+### NOTE : the datetime condition (in where)
+select book_id, name
+from Books
+where book_id not in (
+    select book_id
+    from Orders
+    where dispatch_date >= '2018-06-23' and dispatch_date <= '2019-06-22'
+    group by book_id
+    having sum(quantity) >= 10)
+and available_from < '2019-05-23'
 
 # V1 
 # https://code.dennyzhang.com/unpopular-books
