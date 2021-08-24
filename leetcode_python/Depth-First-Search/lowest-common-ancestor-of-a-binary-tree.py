0dd24d4759ceab9095c4b19dc419fcf5e8eae029
yennanliu <f339339@gmail.com>
2021-08-25 00:22:06 +0800

    update 236

--- leetcode_python/Depth-First-Search/lowest-common-ancestor-of-a-binary-tree.py
+++ leetcode_python/Depth-First-Search/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,12 +1,56 @@
-# V0 
+"""
+
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ 
+
+Example 1:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the tree.
+
+"""
+
+# V0
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
+        # if not root or find p in the tree or find q in the tree
+        if not root or p == root or q == root:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        # find q and p on the same time -> LCA is the current node (root)
+        if left != None and right != None:
+            return root
+        # if only find p or only find q -> LCA is the we found at the moment
+        return left if left else right
+
+# V0'
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
         if not root or p == root or q == root:
             return root
         left = self.lowestCommonAncestor(root.left, p, q)
@@ -15,7 +59,38 @@ class Solution(object):
             return root
         return left if left else right
 
-# V0'
+# V0''
+class Solution:
+    def lowestCommonAncestor(self, root, A, B):
+        # A & : if there is B below => A 
+        # B & : if there is A below => B 
+        # A & : if there is nothing below => A 
+        # B & : if there is everything below => B 
+        if root is None:
+            return None
+        
+        if root == A or root == B:
+            return root
+        
+        left_result = self.lowestCommonAncestor(root.left, A, B)
+        right_result = self.lowestCommonAncestor(root.right, A, B)
+        
+        # split to A and B 
+        if left_result and right_result: 
+            return root
+        
+        # if there is one point or LCA at sub left tree 
+        if left_result:
+            return left_result
+        
+        # if there is one point or LCA at sub right tree 
+        if right_result:
+            return right_result
+        
+        # if there is nothing at left, right sub tree 
+        return None
+
+# V0''
 # DFS : TO FIX 
 # class Solution(object):
 #     def lowestCommonAncestor(self, root, p, q):
@@ -145,6 +220,179 @@ class Solution:
         while q not in ancestors:
             q = parent[q]
         return q
+
+# V1'''
+# IDEA : Recursive Approach
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/
+class Solution:
+
+    def __init__(self):
+        # Variable to store LCA node.
+        self.ans = None
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        def recurse_tree(current_node):
+
+            # If reached the end of a branch, return False.
+            if not current_node:
+                return False
+
+            # Left Recursion
+            left = recurse_tree(current_node.left)
+
+            # Right Recursion
+            right = recurse_tree(current_node.right)
+
+            # If the current node is one of p or q
+            mid = current_node == p or current_node == q
+
+            # If any two of the three flags left, right or mid become True.
+            if mid + left + right >= 2:
+                self.ans = current_node
+
+            # Return True if either of the three bool values is True.
+            return mid or left or right
+
+        # Traverse the tree
+        recurse_tree(root)
+        return self.ans
+
+# V1''''
+# IDEA :  Iterative using parent pointers
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/
+class Solution:
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # Stack for tree traversal
+        stack = [root]
+
+        # Dictionary for parent pointers
+        parent = {root: None}
+
+        # Iterate until we find both the nodes p and q
+        while p not in parent or q not in parent:
+
+            node = stack.pop()
+
+            # While traversing the tree, keep saving the parent pointers.
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+
+        # Ancestors set() for node p.
+        ancestors = set()
+
+        # Process all ancestors for node p using parent pointers.
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+
+        # The first ancestor of q which appears in
+        # p's ancestor set() is their lowest common ancestor.
+        while q not in ancestors:
+            q = parent[q]
+        return q
+
+# V1''''
+# IDEA :  Iterative without parent pointers
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/
+class Solution:
+
+    # Three static flags to keep track of post-order traversal.
+
+    # Both left and right traversal pending for a node.
+    # Indicates the nodes children are yet to be traversed.
+    BOTH_PENDING = 2
+    # Left traversal done.
+    LEFT_DONE = 1
+    # Both left and right traversal done for a node.
+    # Indicates the node can be popped off the stack.
+    BOTH_DONE = 0
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # Initialize the stack with the root node.
+        stack = [(root, Solution.BOTH_PENDING)]
+
+        # This flag is set when either one of p or q is found.
+        one_node_found = False
+
+        # This is used to keep track of LCA index.
+        LCA_index = -1
+
+        # We do a post order traversal of the binary tree using stack
+        while stack:
+
+            parent_node, parent_state = stack[-1]
+
+            # If the parent_state is not equal to BOTH_DONE,
+            # this means the parent_node can't be popped of yet.
+            if parent_state != Solution.BOTH_DONE:
+
+                # If both child traversals are pending
+                if parent_state == Solution.BOTH_PENDING:
+
+                    # Check if the current parent_node is either p or q.
+                    if parent_node == p or parent_node == q:
+
+                        # If one_node_found is set already, this means we have found both the nodes.
+                        if one_node_found:
+                            return stack[LCA_index][0]
+                        else:
+                            # Otherwise, set one_node_found to True,
+                            # to mark one of p and q is found.
+                            one_node_found = True
+
+                            # Save the current top index of stack as the LCA_index.
+                            LCA_index = len(stack) - 1
+
+                    # If both pending, traverse the left child first
+                    child_node = parent_node.left
+                else:
+                    # traverse right child
+                    child_node = parent_node.right
+
+                # Update the node state at the top of the stack
+                # Since we have visited one more child.
+                stack.pop()
+                stack.append((parent_node, parent_state - 1))
+
+                # Add the child node to the stack for traversal.
+                if child_node:
+                    stack.append((child_node, Solution.BOTH_PENDING))
+            else:
+
+                # If the parent_state of the node is both done,
+                # the top node could be popped off the stack.
+
+                # i.e. If LCA_index is equal to length of stack. Then we decrease LCA_index by 1.
+                if one_node_found and LCA_index == len(stack) - 1:
+                    LCA_index -= 1
+                stack.pop()
+
+        return None
 
 # V2 
 # Time:  O(n)
