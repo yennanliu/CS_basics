ad2e044e97f330f9bfb1757a37a0038403145a18
yennanliu <f339339@gmail.com>
2021-08-28 00:59:55 +0800

    update

---
+++ workspace/Pipeline/gameApp/etl/extract.py
@@ -0,0 +1,24 @@
+import json
+
+class Extract:
+
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    # TODO : try use yield
+    # TODO : consider scalability
+    def get_txt_data(self, txt_path):
+        r = []
+        with open(txt_path, 'r') as f:
+            for line in f:
+                r.append(line)
+        return r
+
+    # TODO : try use yield
+    def get_json_data(self, json_path):
+        r = []
+        with open(json_path, 'r') as f:
+            for line in f:
+                data = json.loads(line)
+                r.append(r)
+        return r
