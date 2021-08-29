c8c70047cda741ce775dfdd1dde013969f3efd8a
yennanliu <f339339@gmail.com>
2021-08-29 16:15:28 +0800

    update 088

--- leetcode_python/Sort/merge-sorted-array.py
+++ leetcode_python/Sort/merge-sorted-array.py
@@ -1,17 +1,93 @@
-# Time:  O(n)
-# Space: O(1)
-#
-# Given two sorted integer arrays A and B, merge B into A as one sorted array.
-#
-# Note:
-# You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B.
-# The number of elements initialized in A and B are m and n respectively.
-#
+"""
 
-# V0 
-# IDEA 
+88. Merge Sorted Array
+Easy
+
+983
+
+118
+
+Add to List
+
+Share
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ 
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+
+"""
+
+# V0
+# IDEA : 2 pointers
+# TRICK : START FROM BIGGEST -> SMALLEST
+### NOTE : we need to merge the sorted arrat to nums1 with IN PLACE (CAN'T USE EXTRA CACHE)
+# -> SO WE START FROM RIGHT HAND SIDE (biggeest element) to LEFT HAND SIDE (smallest element)
+# -> Then paste the remain elements
+class Solution:
+    def merge(self, A, m, B, n):
+        pos = m + n - 1 
+        i = m - 1  
+        j = n - 1
+        while  i >= 0 and j >= 0 :
+            if A[i]>B[j] :
+                A[pos]=A[i]
+                pos-=1
+                i-=1
+            else :
+                A[pos]=B[j]
+                pos-=1
+                j-=1
+                
+        while i >= 0 :
+            A[pos] = A[i]
+            pos-=1
+            i-=1
+        while j >= 0:
+            A[pos] = B[j]
+            pos-=1
+            j-=1
+
+# V0' 
+# IDEA : 2 pointers
+### NOTE : we need to merge the sorted arrat to nums1 with IN PLACE (CAN'T USE EXTRA CACHE)
+# -> SO WE START FROM RIGHT HAND SIDE (biggeest element) to LEFT HAND SIDE (smallest element)
+# -> Then paste the remain elements
 class Solution(object):
-    def merge(self, nums1, m, nums2, n):
         """
         :type nums1: List[int]
         :type m: int
@@ -19,6 +95,7 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
+    def merge(self, nums1, m, nums2, n):
         p, q = m-1, n-1
         while p >= 0 and q >= 0:
             if nums1[p] > nums2[q]:
@@ -88,7 +165,7 @@ class Solution:
     @param: n: An integer
     @return: nothing
     """
-    def mergeSortedArray(self, A, m, B, n):
+    def merge(self, A, m, B, n):
         # write your code here
         pos = m + n - 1 
         i = m - 1  
