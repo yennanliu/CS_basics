9c380d0fd5d37d6057836f38cc1349cc7fc2d088
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-08-31 15:11:06 +0800

    udpate 078

--- leetcode_python/Backtracking/subsets.py
+++ leetcode_python/Backtracking/subsets.py
@@ -1,3 +1,40 @@
+"""
+
+78. Subsets
+Medium
+
+6854
+
+121
+
+Add to List
+
+Share
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+
+"""
+
 # V0
 # IDEA : DFS 
 # SAME AS # 90 subset-ii 
@@ -15,6 +52,7 @@ class Solution(object):
             for i in range(start, len(nums)):
                 dfs(depth + 1, i + 1, valueList+[nums[i]])
         
+        # this sort is optional
         nums.sort()
         res = []
         dfs(0, 0, [])
