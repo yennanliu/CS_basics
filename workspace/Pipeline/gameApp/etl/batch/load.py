55c0af2d866ec05b3f3e504e2c45b4ecdb49d65d
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-08-31 17:41:03 +0800

    update

--- workspace/Pipeline/gameApp/etl/batch/load.py
+++ workspace/Pipeline/gameApp/etl/batch/load.py
@@ -1,28 +1,35 @@
 """
 class for load (save) transformed data into DB
 """
+
+import db_client
+
 class Load:
 
     def __init__(self, cfg):
         self.cfg = cfg
-        self.db_conn = set_db_conn()
         self.BATCH_SIZE = 100
+
+        self.db_client = db_client(cfg)
 
     def set_db_conn(self):
         db_conn = {'db': 'some_db', 'password': 'some_pwd', 'db_url': 'db_url'}
-        return db_conn
+        return self.db_client.connect(db_conn)
 
     def insert_to_db(self, data):
-        db_client = DB(self.db_conn)
+        insert_status = False
         try:
             for row in data:
-                db_client.insert(row)
+                self.db_client.insert(row)
         except Exception as e:
             print ("isert failed! " + str(e))
-        print ("isert OK")
+        if insert_status:
+            self.db_client.commit()
+            print ("isert OK")
+        self.db_client.close()
  
      def insert_to_db_batch(self, data):
-        db_client = DB(self.db_conn)
+        insert_status = False
         batch_counts = (len(data) // self.BATCH_SIZE)  + 1
         start = end = 0
         try:
@@ -30,7 +37,9 @@ class Load:
                 start = batch
                 end += (start + batch)
                 batch_data = data[start:end]
-                db_client.insert(row)
+                self.db_client.commit()
         except Exception as e:
             print ("batch isert failed! " + str(e))
-        print ("batch isert OK")       
+        if insert_status:
+            print ("batch isert OK")   
+        self.db_client.close()   
