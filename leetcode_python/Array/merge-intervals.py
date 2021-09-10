5b2fc9d74a6f2220bfa9118c2958cb2c160202b0
yennj12 <f339339@gmail.com>
2021-09-10 15:57:40 +0800

    Update merge-intervals.py

--- leetcode_python/Array/merge-intervals.py
+++ leetcode_python/Array/merge-intervals.py
@@ -27,6 +27,7 @@ intervals[i].length == 2
 
 """
 # V0
+# https://github.com/labuladong/fucking-algorithm/blob/master/%E7%AE%97%E6%B3%95%E6%80%9D%E7%BB%B4%E7%B3%BB%E5%88%97/%E5%8C%BA%E9%97%B4%E8%B0%83%E5%BA%A6%E9%97%AE%E9%A2%98%E4%B9%8B%E5%8C%BA%E9%97%B4%E5%90%88%E5%B9%B6.md
 class Solution:
     # @param intervals, a list of Interval
     # @return a list of Interval
