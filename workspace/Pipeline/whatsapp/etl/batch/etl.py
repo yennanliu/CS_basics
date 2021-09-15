744da44d0c43c8927b9de32b36be226e405b187d
yennanliu <f339339@gmail.com>
2021-09-15 23:23:21 +0800

    update

--- workspace/Pipeline/whatsapp/etl/batch/etl.py
+++ workspace/Pipeline/whatsapp/etl/batch/etl.py
@@ -6,51 +6,48 @@ import logging
 
 class ETL:
 
-    def __init__(self, dest_table, look_table, where_condition):
+    def __init__(self):
 
         self.cursor = cursor
         self.conn = conn
 
+        self.insert_sql = """INSERT INTO {table_name}"""
         self.join_base_sql = """SELECT a.*, b.prod_id FROM base_table a INNER JOIN {table_b} b ON a.prod_id = b.prod_id"""
         self.transform_base_sql = """SELECT *, upper(customer_code) AS customer_code_ FROM {stg_table}"""
         self.union_base_sql = """SELECT table_a.* FROM {table_a} UNION ALL SELECT table_b.* FROM {table_b}"""
         self.lookup_base_sql = None
         self.insert_base_sql = """INSERT INTO {dest_table} FROM SELECT * FROM {lookup_table} WHERE {where_condition}"""
 
-        self.dest_table = dest_table 
-        self.look_table = look_table
-        self.where_condition = where_condition
+    def join(self, to_join_table, dest_table):
 
-    def join(self, to_join_table):
-
-        join_base_sql = self.join_base_sql.format(to_join_table)
+        join_base_sql = self.insert_sql.format(dest_table) + self.join_base_sql.format(to_join_table)
 
         try:
             self.cursor.execute(join_base_sql)
         except Exception as e:
             logging.info("join failed " + str(e))
 
-    def transform(self, stg_table):
+    def transform(self, stg_table, dest_table):
 
-        transform_base_sql = self.transform_base_sql.format(stg_table)
+        transform_base_sql = self.insert_sql.format(dest_table) + self.transform_base_sql.format(stg_table)
 
         try:
             self.cursor.execute(transform_base_sql)
         except Exception as e:
             logging.info("transform failed " + str(e))
 
-    def union(self, table_a, table_b):
+    def union(self, table_a, table_b, dest_table):
 
-        union_base_sql = self.union_base_sql.format(table_a, table_b)
+        union_base_sql = self.insert_sql.format(dest_table) + self.union_base_sql.format(table_a, table_b)
 
         try:
             self.cursor.execute(union_base_sql)
         except Exception as e:
             logging.info("union failed " + str(e))
 
-    def lookup(self):
+    def lookup(self, dest_table, dest_table):
 
-        lookup_base_sql = self.lookup_base_sql
+        lookup_base_sql =  self.insert_sql.format(dest_table) + self.lookup_base_sql
 
         try:
             self.cursor.execute(lookup_base_sql)
@@ -66,6 +63,11 @@ class ETL:
         except Exception as e:
             logging.info("insert failed " + str(e))
 
-    def run(self):
+    def run(self, to_join_table, dest_table_1, stg_table, dest_table_2, table_a, table_b, dest_table_3, dest_table_4, dest_table_5, look_table, where_condition):
 
-        insert(dest_table, look_table, where_condition)
+        # join -> transform -> union -> lookup -> insert
+        join(to_join_table, dest_table_1)
+        transform(stg_table, dest_table_2)
+        union(table_a, table_b, dest_table_3)
+        lookup(dest_table_4)
+        insert(dest_table_5, look_table, where_condition)
