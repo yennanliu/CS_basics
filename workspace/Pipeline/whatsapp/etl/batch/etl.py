e6eff0895ad7b8ae7cbc2ca050a3ddad7e37bbdd
yennanliu <f339339@gmail.com>
2021-09-15 17:35:49 +0800

    update

--- workspace/Pipeline/whatsapp/etl/batch/etl.py
+++ workspace/Pipeline/whatsapp/etl/batch/etl.py
@@ -6,7 +6,7 @@ import logging
 
 class ETL:
 
-    def __init__(self):
+    def __init__(self, dest_table, look_table, where_condition):
 
         self.cursor = cursor
         self.conn = conn
@@ -14,6 +14,12 @@ class ETL:
         self.join_base_sql = """SELECT a.*, b.prod_id FROM base_table a INNER JOIN {table_b} b ON a.prod_id = b.prod_id"""
         self.transform_base_sql = """SELECT *, upper(customer_code) AS customer_code_ FROM {stg_table}"""
         self.union_base_sql = """SELECT table_a.* FROM {table_a} UNION ALL SELECT table_b.* FROM {table_b}"""
+        self.lookup_base_sql = None
+        self.insert_base_sql = """INSERT INTO {dest_table} FROM SELECT * FROM {lookup_table} WHERE {where_condition}"""
+
+        self.dest_table = dest_table 
+        self.look_table = look_table
+        self.where_condition = where_condition
 
     def join(self, to_join_table):
 
@@ -44,12 +50,22 @@ class ETL:
 
     def lookup(self):
 
-        pass
+        lookup_base_sql = self.lookup_base_sql
 
-    def insert(self):
+        try:
+            self.cursor.execute(lookup_base_sql)
+        except Exception as e:
+            logging.info("lookup failed " + str(e))
 
-        pass
+    def insert(self, dest_table, look_table, where_condition):
+
+        insert_base_sql =  self.insert_base_sql.format(dest_table, look_table, where_condition)
+
+        try:
+            self.cursor.execute(insert_base_sql)
+        except Exception as e:
+            logging.info("insert failed " + str(e))
 
     def run(self):
 
-        pass
+        insert(dest_table, look_table, where_condition)
