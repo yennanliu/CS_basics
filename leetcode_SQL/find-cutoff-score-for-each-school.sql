5ba7c34669606a5065ce1f33b13c06f4c5218f39
yennanliu <f339339@gmail.com>
2021-09-18 11:06:55 +0800

    add 1972, 1978, 1988, update readme ref

---
+++ leetcode_SQL/find-cutoff-score-for-each-school.sql
@@ -0,0 +1,11 @@
+# V0
+
+# V1
+
+# V2
+# Time:  O(n * m)
+# Space: O(n * m)
+SELECT school_id, IFNULL(min(score), -1) as score
+FROM Schools LEFT JOIN Exam ON capacity >= student_count
+GROUP BY school_id
+ORDER BY NULL;
