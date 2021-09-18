c232d50065b8e2ecacf4e39f859a281134feecac
yennanliu <f339339@gmail.com>
2021-09-18 15:46:04 +0800

    fix

--- workspace/Pipeline/songify/etl/helper.py
+++ workspace/Pipeline/songify/etl/helper.py
@@ -1,5 +1,5 @@
 
-import glob
+import glob, json
 
 def list_json_files(json_path):
     result = []
@@ -9,22 +9,45 @@ def list_json_files(json_path):
     return result
 
 def load_data(json_file):
-    counter = 0
-    with open(json_file, 'r') as f:
+    result = []
+    with open(json_file, "r") as f:
         for line in f.readlines():
-            if counter > 3:
-                break
-            print (str(counter))
-            print (line)
-            counter += 1
+            data = json.loads(line)
+            result.append(data)
+    return result
 
 def load_all_data(json_path):
+    result = []
     json_files = list_json_files(json_path)
     for json_file in json_files:
         print ("*** json_file = " + str(json_file))
-        load_data(json_file)
+        tmp = load_data(json_file)
+        result.append(tmp)
+    return result
 
+
+def save_data(input_data, dest_file):
+    # with open(dest_file, 'w') as file:
+    #     for line in input_data:
+    #         #file.write(str(line))
+    #         json.dump(line, file)
+    #     file.close()
+    #     print ('save OK!')
+
+    with open(dest_file, "wb") as f:
+        json.dump(input_data, f)
 
 if __name__ == '__main__':
     json_path = "data/log_data/2018/11/*"
-    load_all_data(json_path)
+    dest_file = "output/output.txt"
+    json_file = "data/log_data/2018/11/2018-11-09-events.json"
+    #merged_data = load_all_data(json_path)
+    #save_data(merged_data, dest_file)
+
+    x = load_data(json_file)
+    print (x[:3])
+    print (len(x))
+    print (x[0])
+
+    #print ("merged data sile = " + str(len(merged_data)))
+    #print (merged_data)
