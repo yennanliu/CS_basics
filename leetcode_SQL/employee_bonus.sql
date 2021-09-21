37dc83adb9453ca1e09b912a7a20653b528260e7
yennanliu <f339339@gmail.com>
2021-09-21 15:19:35 +0800

    udpate progress

---
+++ leetcode_SQL/employee_bonus.sql
@@ -0,0 +1,50 @@
+/*
+
+Description
+LeetCode Problem 577.
+
+Select all employee’s name and bonus whose bonus is < 1000.
+
+Table:Employee
+
++-------+--------+-----------+--------+
+| empId |  name  | supervisor| salary |
++-------+--------+-----------+--------+
+|   1   | John   |  3        | 1000   |
+|   2   | Dan    |  3        | 2000   |
+|   3   | Brad   |  null     | 4000   |
+|   4   | Thomas |  3        | 4000   |
++-------+--------+-----------+--------+
+empId is the primary key column for this table.
+Table: Bonus
+
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+empId is the primary key column for this table.
+Example ouput:
+
++-------+-------+
+| name  | bonus |
++-------+-------+
+| John  | null  |
+| Dan   | 500   |
+| Brad  | null  |
++-------+-------+
+
+*/
+
+# V0
+
+# V1
+# https://circlecoder.com/employee-bonus/
+select name, bonus
+from Employee e
+left join Bonus b
+on e.empId = b.empId 
+where b.bonus < 1000 or b.bonus is null
+
+# V2
