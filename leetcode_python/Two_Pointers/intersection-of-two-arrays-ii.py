4a69372d5962fead0c59442ea6b84409c1309187
yennj12 <f339339@gmail.com>
2021-09-22 15:55:50 +0800

    Update intersection-of-two-arrays-ii.py

--- leetcode_python/Two_Pointers/intersection-of-two-arrays-ii.py
+++ leetcode_python/Two_Pointers/intersection-of-two-arrays-ii.py
@@ -1,34 +1,56 @@
-# If the given array is not sorted and the memory is unlimited:
-#   - Time:  O(m + n)
-#   - Space: O(min(m, n))
-# elif the given array is already sorted:
-#   if m << n or m >> n:
-#     - Time:  O(min(m, n) * log(max(m, n)))
-#     - Space: O(1)
-#   else:
-#     - Time:  O(m + n)
-#     - Soace: O(1)
-# else: (the given array is not sorted and the memory is limited)
-#     - Time:  O(max(m, n) * log(max(m, n)))
-#     - Space: O(1)
+"""
 
-# Given two arrays, write a function to compute their intersection.
-#
-# Example:
-# Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
-#
-# Note:
-# Each element in the result should appear as many times as it shows in both arrays.
-# The result can be in any order.
-#
-# Follow up:
-# - What if the given array is already sorted? How would you optimize your algorithm?
-# - What if nums1's size is small compared to num2's size? Which algorithm is better?
-# - What if elements of nums2 are stored on disk, and the memory is limited such that
-#   you cannot load all elements into the memory at once?
-# If the given array is not sorted and the memory is unlimited.
+350. Intersection of Two Arrays II
+Easy
 
-# V0 
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+ 
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+ 
+
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+"""
+
+# V0
+# IDEA : 2 POINTERS
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        res = []
+        nums1.sort()
+        nums2.sort()
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+        return res
+  
+# V0' 
 class Solution(object):
     def intersect(self, nums1, nums2):
         """
