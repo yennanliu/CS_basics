512996aa4f74c1fbf95195c51f8860292b8f2a9b
yennj12 <f339339@gmail.com>
2021-09-22 15:58:42 +0800

    Update permutation-in-string.py

--- leetcode_python/Two_Pointers/permutation-in-string.py
+++ leetcode_python/Two_Pointers/permutation-in-string.py
@@ -1,3 +1,32 @@
+"""
+
+567. Permutation in String
+Medium
+
+Share
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+ 
+
+Constraints:
+
+1 <= s1.length, s2.length <= 104
+s1 and s2 consist of lowercase English letters.
+
+"""
+
 # V0 
 import collections
 class Solution(object):
