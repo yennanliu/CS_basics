c930a3c92983fbdf453dedb98331072c4344cac6
yennj12 <f339339@gmail.com>
2021-09-22 16:38:04 +0800

    Update top-k-frequent-elements.py

--- leetcode_python/Sort/top-k-frequent-elements.py
+++ leetcode_python/Sort/top-k-frequent-elements.py
@@ -1,3 +1,29 @@
+"""
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+"""
+
 # V0
 import collections
 class Solution(object):
