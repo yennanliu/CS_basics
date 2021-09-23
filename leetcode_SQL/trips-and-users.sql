959b0ca046537a7f40ac91f919c1491283f3a518
yennanliu <f339339@gmail.com>
2021-09-23 17:53:04 +0800

    update progress

--- leetcode_SQL/trips-and-users.sql
+++ leetcode_SQL/trips-and-users.sql
@@ -41,6 +41,14 @@
 -- +------------+-------------------+
 
 # V0 
+select 
+t.Request_at Day, 
+round(sum(case when t.Status like 'cancelled_%' then 1 else 0 end)/count(*),2) Rate
+from Trips t 
+inner join Users u 
+on t.Client_Id = u.Users_Id and u.Banned='No'
+where t.Request_at between '2013-10-01' and '2013-10-03'
+group by t.Request_at
 
 # V1
 # https://leetcode.com/problems/trips-and-users/discuss/69151/Sharing-my-solution
