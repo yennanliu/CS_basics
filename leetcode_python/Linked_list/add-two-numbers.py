f75a4d0fa07f0247f311d6ce7b15f00a5a551774
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-10-06 19:10:26 +0800

    udpate 002, progress, readme

--- leetcode_python/Linked_list/add-two-numbers.py
+++ leetcode_python/Linked_list/add-two-numbers.py
@@ -1,11 +1,61 @@
-# You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+"""
 
-# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-# Output: 7 -> 0 -> 8
+2. Add Two Numbers
+Medium
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
+"""
 
 # V0 
-
-
+class Solution:
+    # @return a ListNode
+    def addTwoNumbers(self, l1, l2):
+        # note below init
+        head = ListNode(0)
+        l = head
+        carry = 0
+        while l1 or l2 or carry:
+            ### NOTE below (sum = carry)
+            sum, carry = carry, 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            if sum > 9:
+                carry = 1
+                sum -= 10
+            l.next = ListNode(sum)
+            l = l.next
+        return head.next
+    
 # V1
 # http://bookshadow.com/weblog/2015/04/05/leetcode-add-two-numbers/
 # IDEA : LINKED LIST
@@ -26,10 +76,12 @@
 class Solution:
     # @return a ListNode
     def addTwoNumbers(self, l1, l2):
+        # note below init
         head = ListNode(0)
         l = head
         carry = 0
         while l1 or l2 or carry:
+            ### NOTE below (sum = carry)
             sum, carry = carry, 0
             if l1:
                 sum += l1.val
