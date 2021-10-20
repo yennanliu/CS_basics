3c50c819897a572ff38179bfb0083a19b2325fde
yennanliu <f339339@gmail.com>
2021-10-20 08:33:28 +0800

    update 167

--- leetcode_python/Two_Pointers/two-sum-ii-input-array-is-sorted.py
+++ leetcode_python/Two_Pointers/two-sum-ii-input-array-is-sorted.py
@@ -1,4 +1,65 @@
+"""
+
+167. Two Sum II - Input array is sorted
+Easy
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= first < second <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+ 
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3.
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2.
+ 
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.
+
+"""
+
 # V0
+# IDEA : TWO POINTERS
+#       -> l = 0, r = len(numbers) - 1
+class Solution(object):
+    def twoSum(self, numbers, target):
+        l = 0
+        ### NOTE this
+        r = len(numbers) - 1
+        res = []
+        while r > l:
+            #print ("l, r = {}, {}".format(l, r))
+            tmp = numbers[l] + numbers[r]
+            if tmp == target:
+                return [l+1, r+1]
+            ### NOTE this
+            elif tmp > target:
+                r -= 1
+            ### NOTE this
+            else:
+                l += 1
+        return [-1, -1]
+
+# V0'
 # IDEA : TWO POINTERS 
 class Solution(object):
     def twoSum(self, numbers, target):
