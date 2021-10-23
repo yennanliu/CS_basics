96a8dc3f2b196876edaef4aaad9026b519ec936b
yennanliu <f339339@gmail.com>
2021-10-23 20:15:50 +0800

    update 725

--- leetcode_python/Linked_list/split-linked-list-in-parts.py
+++ leetcode_python/Linked_list/split-linked-list-in-parts.py
@@ -1,4 +1,71 @@
+"""
+
+725. Split Linked List in Parts
+Medium
+
+0Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+
+The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+
+The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+
+Return an array of the k parts.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3], k = 5
+Output: [[1],[2],[3],[],[]]
+Explanation:
+The first element output[0] has output[0].val = 1, output[0].next = null.
+The last element output[4] is null, but its string representation as a ListNode is [].
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6,7,8,9,10], k = 3
+Output: [[1,2,3,4],[5,6,7],[8,9,10]]
+Explanation:
+The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 1000].
+0 <= Node.val <= 1000
+1 <= k <= 50
+
+"""
+
 # V0
+# IDEA : LINKED LIST OP
+class Solution:
+    def splitListToParts(self, root, k):
+        def get_length(root):
+            ans = 0
+            while root is not None:
+                root = root.next
+                ans += 1
+            return ans
+            
+        ans = [None]*k
+        cur = root
+        length = get_length(root)
+        
+        for i in range(k):
+            no_elems = (length // k) + (1 if i < (length % k) else 0)
+            for j in range(no_elems):
+                if j == 0:
+                    ans[i] = cur
+
+                if j == no_elems - 1:
+                    temp = cur.next
+                    cur.next = None
+                    cur = temp
+                else:
+                    cur = cur.next   
+        return ans
 
 # V1
 # https://leetcode.com/problems/split-linked-list-in-parts/discuss/109284/Elegant-Python-with-Explanation-45ms
