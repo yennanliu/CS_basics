4ddccf647d9e97baae6766dd2aabf668d71f6186
yennanliu <f339339@gmail.com>
2021-10-31 16:42:26 +0800

    update 355

--- leetcode_python/Design/design-twitter.py
+++ leetcode_python/Design/design-twitter.py
@@ -1,3 +1,48 @@
+"""
+
+355. Design Twitter
+Medium
+
+Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
+
+Implement the Twitter class:
+
+Twitter() Initializes your twitter object.
+void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. Each call to this function will be made with a unique tweetId.
+List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user themself. Tweets must be ordered from most recent to least recent.
+void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+ 
+
+Example 1:
+
+Input
+["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+[[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+Output
+[null, null, [5], null, null, [6, 5], null, [5]]
+
+Explanation
+Twitter twitter = new Twitter();
+twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+twitter.follow(1, 2);    // User 1 follows user 2.
+twitter.postTweet(2, 6); // User 2 posts a new tweet (id = 6).
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+twitter.unfollow(1, 2);  // User 1 unfollows user 2.
+twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.
+ 
+
+Constraints:
+
+1 <= userId, followerId, followeeId <= 500
+0 <= tweetId <= 104
+All the tweets have unique IDs.
+At most 3 * 104 calls will be made to postTweet, getNewsFeed, follow, and unfollow.
+
+"""
+
+
 # V0
 # https://github.com/labuladong/fucking-algorithm/blob/master/%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84%E7%B3%BB%E5%88%97/%E8%AE%BE%E8%AE%A1Twitter.md
 from collections import defaultdict
@@ -34,7 +79,47 @@ class Twitter(object):
     def unfollow(self, followerId, followeeId):
         self.follower_followees_map[followerId].discard(followeeId)
 
-# V0'
+# V0 : TODO : fix getNewsFeed returned format
+from collections import defaultdict
+
+class Twitter(object):
+
+    def __init__(self):
+
+         self.users = []
+         # {'id_1' : [foller_1, 2...], 'id_2' : [foller_3, 4...]..}
+         self.follower_followees = defaultdict(set)
+         # {'id_1' : [twitter_1, 2...], 'id_2' : [twitter_3, 4...]..}
+         self.user_tweets = defaultdict(list)
+
+    def postTweet(self, userId, tweetId):
+
+        self.user_tweets[userId].append(tweetId)
+        
+    #TODO : fix this
+    def getNewsFeed(self, userId):
+
+        res = []
+        for user in list(self.follower_followees[userId]) + [userId]:
+            if user in self.user_tweets:
+                res.append(self.user_tweets[userId])
+                if len(res) >= 10:
+                    break
+            else:
+                break
+        return res[::-1]
+        
+    def follow(self, followerId, followeeId):
+
+        self.follower_followees[followeeId].add(followeeId)
+        
+    def unfollow(self, followerId, followeeId):
+
+        if followeeId in self.follower_followees[followerId]:
+            idx = self.follower_followees[followeeId]
+            self.follower_followees[followeeId].pop(idx)
+
+# V0''
 import collections, itertools, heapq
 class Twitter(object):
 
--- system_design/design_twitter/README.md
+++ system_design/design_twitter/README.md
@@ -1,7 +1,8 @@
 # Twitter System design
 
 ### Ref
--  https://www.youtube.com/watch?v=wYk0xPP_P_8&list=RDCMUCn1XnDWhsLS5URXTi5wtFTA&index=5
+- https://www.youtube.com/watch?v=wYk0xPP_P_8&list=RDCMUCn1XnDWhsLS5URXTi5wtFTA&index=5
+- https://github.com/yennanliu/CS_basics/blob/master/leetcode_python/Design/design-twitter.py
 
 ### Key points
 
