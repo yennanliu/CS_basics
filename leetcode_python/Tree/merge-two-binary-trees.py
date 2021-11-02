3d488450fd1571b541d6a0b6ac69c7726221acbe
yennanliu <f339339@gmail.com>
2021-11-02 08:39:22 +0800

    update 617, cheatsheet

--- leetcode_python/Tree/merge-two-binary-trees.py
+++ leetcode_python/Tree/merge-two-binary-trees.py
@@ -1,5 +1,59 @@
+"""
+
+617. Merge Two Binary Trees
+Easy
+
+You are given two binary trees root1 and root2.
+
+Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+Return the merged tree.
+
+Note: The merging process must start from the root nodes of both trees.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+Example 2:
+
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
+ 
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 2000].
+-104 <= Node.val <= 104
+
+"""
+
 # V0
-# IDEA : DFS 
+# IDEA : DFS + BACKTRACK
+class Solution:
+    def mergeTrees(self, t1, t2):
+        return self.dfs(t1,t2)
+
+    def dfs(self, t1, t2):
+        if not t1 and not t2:
+            return
+        
+        if t1 and t2:
+            ### NOTE here
+            newT = TreeNode(t1.val +  t2.val)
+            newT.right = self.mergeTrees(t1.right, t2.right)
+            newT.left = self.mergeTrees(t1.left, t2.left)   
+            return newT
+        
+        ### NOTE here
+        else:
+            return t1 or t2
+
+# V0'
+# IDEA : DFS + BACKTRACK
 class Solution:
     def mergeTrees(self, t1, t2):
         return self.dfs(t1,t2)
