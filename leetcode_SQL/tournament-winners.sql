3ae109edb021a62ab2b894069733e6c48cbbec50
yennanliu <f339339@gmail.com>
2021-11-11 07:45:08 +0800

    update 1194, progress

--- leetcode_SQL/tournament-winners.sql
+++ leetcode_SQL/tournament-winners.sql
@@ -72,6 +72,28 @@ Result table:
 */
 
 # V0
+# IDEA : UNION ALL
+SELECT group_id, 
+       player_id 
+FROM   (SELECT p.group_id, 
+               ps.player_id, 
+               Sum(ps.score) AS score 
+        FROM   players p INNER JOIN
+               (SELECT first_player AS player_id, 
+                       first_score  AS score 
+                FROM   matches 
+                UNION ALL 
+                SELECT second_player AS player_id,
+                       second_score  AS score
+                FROM   matches) ps 
+        ON  p.player_id = ps.player_id 
+        GROUP  BY ps.player_id 
+        ORDER  BY group_id, 
+                  score DESC, 
+                  player_id) top_scores 
+GROUP  BY group_id
+
+# V0'
 -- WITH 
 -- _1st_score AS ( 
 -- SELECT
