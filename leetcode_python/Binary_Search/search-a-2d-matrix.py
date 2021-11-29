267970d2990f752a5b326f51b743bdf3f434bc67
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-11-29 17:44:31 +0800

    update comment in 74

--- leetcode_python/Binary_Search/search-a-2d-matrix.py
+++ leetcode_python/Binary_Search/search-a-2d-matrix.py
@@ -1,3 +1,36 @@
+"""
+
+74. Search a 2D Matrix
+Medium
+
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+
+"""
+
 # V0 
 # IDEA : MATRIX IN ORDER + BRUTE FORCE
 # Space: O(1)
