b186f561935bedf3b039c714633511b766b4852d
yennanliu <f339339@gmail.com>
2021-12-14 22:51:08 +0800

    update 040, progress

--- leetcode_python/Backtracking/combination-sum-ii.py
+++ leetcode_python/Backtracking/combination-sum-ii.py
@@ -42,24 +42,30 @@ Constraints:
 
 # V0 
 class Solution(object):
+
     def combinationSum2(self, candidates, target):
 
-    def dfs(self, nums, target, index, res, path):
-        if target < 0:
-            return
-        elif target == 0:
-            res.append(path)
-            return
-        for i in range(index, len(nums)):
-            if i > index and nums[i] == nums[i-1]:
-                continue
-            self.dfs(nums, target - nums[i], i + 1, res, path + [nums[i]])
+        """
+        NOTE : we have index here for tracking
+               -> avoid reuse same element from candidates
+        """
+        def dfs(nums, target, index, res, path):
+            if target < 0:
+                return
+            elif target == 0:
+                res.append(path)
+                return
+            for i in range(index, len(nums)):
+                ### NOTE : below condition
+                if i > index and nums[i] == nums[i-1]:
+                    continue
+                dfs(nums, target - nums[i], i + 1, res, path + [nums[i]])
 
         ### NOTE : once we sort the input array, the occurrance of each unique number would be adjacent to each other.
         candidates.sort()
         print(candidates)
         res = []
-        self.dfs(candidates, target, 0, res, [])
+        dfs(candidates, target, 0, res, [])
         return res
 
 # V1 
