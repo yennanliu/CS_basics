d7246f9b1cc7ae8086b5995f447bb56bff4b3f4f
yennanliu <f339339@gmail.com>
2021-12-30 15:53:00 +0800

    add 1170, update code comment, progress, readme

---
+++ leetcode_python/Greedy/maximum-units-on-a-truck.py
@@ -0,0 +1,90 @@
+"""
+
+1710. Maximum Units on a Truck
+Easy
+
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+
+ 
+
+Example 1:
+
+Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+Output: 8
+Explanation: There are:
+- 1 box of the first type that contains 3 units.
+- 2 boxes of the second type that contain 2 units each.
+- 3 boxes of the third type that contain 1 unit each.
+You can take all the boxes of the first and second types, and one box of the third type.
+The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+Example 2:
+
+Input: boxTypes = [[5,10],[2,5],[4,7],[3,9]], truckSize = 10
+Output: 91
+ 
+
+Constraints:
+
+1 <= boxTypes.length <= 1000
+1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000
+1 <= truckSize <= 106
+
+"""
+
+# V0
+
+# V1
+# IDEA : GREEDY
+class Solution(object):
+    def maximumUnits(self, boxTypes, truckSize):
+        res = 0
+        for number, unit in sorted(boxTypes, key=lambda d:d[1], reverse=True):
+            if truckSize >= number:
+                res += number * unit
+                truckSize -= number
+            else:
+                res += truckSize * unit
+                break        
+        return res
+
+# V1
+# IDEA : GREEDY
+# https://leetcode.com/problems/maximum-units-on-a-truck/discuss/1045318/Python-solution
+class Solution(object):
+    def maximumUnits(self, boxTypes, truckSize):
+        boxTypes.sort(key = lambda x: -x[1])
+        n = len(boxTypes)
+        result = 0
+        i = 0
+        while truckSize >= boxTypes[i][0]:
+            result += boxTypes[i][1] * boxTypes[i][0]
+            truckSize -= boxTypes[i][0]
+            i += 1
+            if i == n:
+                return result
+        result += truckSize * boxTypes[i][1]
+        return result
+
+# V1'
+# https://leetcode.com/problems/maximum-units-on-a-truck/discuss/1000278/Python-sort.-Short-and-fast-(100)
+class Solution:
+    def maximumUnits(self, boxTypes, truckSize):
+        boxTypes.sort(reverse=True, key=itemgetter(1))
+        
+        result = 0
+        for boxes, units in boxTypes:
+            if boxes < truckSize:
+                result += boxes * units
+                truckSize -= boxes
+            else:
+                result += truckSize * units
+                break        
+        return result
+
+# V2
