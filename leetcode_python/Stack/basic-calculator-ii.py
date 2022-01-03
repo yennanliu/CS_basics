06b51a9efcb17fcab308e6ce5db408a48fb07d84
yennanliu <f339339@gmail.com>
2022-01-03 08:36:20 +0800

    update 227, cheatsheet, progress

--- leetcode_python/Stack/basic-calculator-ii.py
+++ leetcode_python/Stack/basic-calculator-ii.py
@@ -1,3 +1,42 @@
+"""
+
+227. Basic Calculator II
+Medium
+
+Given a string s which represents an expression, evaluate this expression and return its value. 
+
+The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+ 
+
+Example 1:
+
+Input: s = "3+2*2"
+Output: 7
+Example 2:
+
+Input: s = " 3/2 "
+Output: 1
+Example 3:
+
+Input: s = " 3+5 / 2 "
+Output: 5
+ 
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
+s represents a valid expression.
+All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+The answer is guaranteed to fit in a 32-bit integer
+
+"""
+
 # V0 
 
 # V1 
@@ -13,6 +52,7 @@
 # 1
 # 10
 # 100
+# python 3
 class Solution:
     def calculate(self, s):
         """
@@ -45,10 +85,6 @@ class Solution:
 # V1'
 # https://www.jiuzhang.com/solution/basic-calculator-ii/#tag-highlight-lang-python
 class Solution:
-    """
-    @param s: the given expression
-    @return: the result of expression
-    """
     def calculate(self, s):
         # Write your code here
         if not s:
