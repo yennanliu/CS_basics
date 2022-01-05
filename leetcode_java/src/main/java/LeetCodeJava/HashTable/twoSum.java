74a2292fc9c71ffd7199a5524e9f19d49c120135
yennanliu <f339339@gmail.com>
2022-01-06 00:26:13 +0800

    fix leetcode_java can run with intellj, remove previous lc-java file

---
+++ leetcode_java/src/main/java/LeetCodeJava/HashTable/twoSum.java
@@ -0,0 +1,35 @@
+package LeetCodeJava.HashTable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class twoSum {
+
+    // V1
+    public int[] twoSum_1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    // V2
+    public int[] twoSum_2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
---
+++ leetcode_java/src/main/java/dev/hello.java
@@ -0,0 +1,7 @@
+package dev;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
