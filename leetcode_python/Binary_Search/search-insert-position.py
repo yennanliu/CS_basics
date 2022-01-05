3145a2fb784998b11a534e52379e25cd8e58631a
yennanliu <f339339@gmail.com>
2022-01-05 08:26:46 +0800

    update cheatsheet, add code comment, progress

--- leetcode_python/Binary_Search/search-insert-position.py
+++ leetcode_python/Binary_Search/search-insert-position.py
@@ -40,6 +40,19 @@ nums contains distinct values sorted in ascending order.
 """
 
 # V0
+# IDEA : BINARY SEARCH
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
+# V0'
 # IDEA : BINARY SEARCH + OTHER CASES HANDLING
 class Solution(object):
     def searchInsert(self, nums, target):
