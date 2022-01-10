677faaa3abb341ea58b923d8f72b63ed32e6aed6
yennanliu <f339339@gmail.com>
2022-01-10 16:34:44 +0800

    update 969, cheatsheet, progress, algorithm/python/pancake_sort.py

--- algorithm/python/pancake_sort.py
+++ algorithm/python/pancake_sort.py
@@ -2,8 +2,42 @@
 # ALGORITHM DEMO : PANCAKE SORT
 #################################################################
 
-# V0 : DEV 
-
+# V0
+# LC 969. Pancake Sorting
+# IDEA : pankcake sort + while loop
+# IDEA : 3 STEPS
+#   -> step 1) Find the maximum number in arr
+#   -> step 2) Reverse from 0 to max_idx
+#   -> step 3) Reverse whole list
+# https://github.com/yennanliu/CS_basics/blob/master/algorithm/python/pancake_sort.py
+class Solution(object):
+    def pancakeSort(self, arr):
+        """Sort Array with Pancake Sort.
+        :param arr: Collection containing comparable items
+        :return: Collection ordered in ascending order of items
+        Examples:
+        >>> pancake_sort([0, 5, 3, 2, 2])
+        [0, 2, 2, 3, 5]
+        >>> pancake_sort([])
+        []
+        >>> pancake_sort([-2, -5, -45])
+        [-45, -5, -2]
+        """
+        cur = len(arr)
+        res = []
+        while cur > 1:
+            # step 1) Find the maximum number in arr
+            max_idx = arr.index(max(arr[0:cur]))
+            res = res + [max_idx+1, cur] # idx is 1 based
+            # step 2) Reverse from 0 to max_idx
+            #arr = arr[max_idx::-1] + arr[max_idx + 1 : len(arr)] # this is OK as well
+            arr = arr[:max_idx][::-1] + arr[max_idx + 1 : len(arr)]
+            # step 3) Reverse whole list
+            #arr = arr[cur - 1 :: -1] + arr[cur : len(arr)] # this is OK as well
+            arr = arr[:cur - 1][::-1] + arr[cur : len(arr)]
+            cur -= 1
+        print ("arr = " + str(arr))
+        return res
 
 # V1 
 # Pancake sort algorithm 
@@ -20,5 +54,3 @@ def pancakesort(arr):
         arr = arr[cur-1::-1] + arr[cur:len(arr)]
         cur -= 1
     return arr
-
-#print(pancakesort([0,10,15,3,2,9,14,13]))
--- doc/cheatsheet/sort.md
+++ doc/cheatsheet/sort.md
@@ -22,7 +22,43 @@
 ### 2-1) Pancake Sorting
 ```python
 # python
-# LC Pancake Sorting
+# LC 969 Pancake Sorting
+# V0
+# IDEA : pankcake sort + while loop
+# IDEA : 3 STEPS
+#   -> step 1) Find the maximum number in arr
+#   -> step 2) Reverse from 0 to max_idx
+#   -> step 3) Reverse whole list
+# https://github.com/yennanliu/CS_basics/blob/master/algorithm/python/pancake_sort.py
+class Solution(object):
+    def pancakeSort(self, arr):
+        """Sort Array with Pancake Sort.
+        :param arr: Collection containing comparable items
+        :return: Collection ordered in ascending order of items
+        Examples:
+        >>> pancake_sort([0, 5, 3, 2, 2])
+        [0, 2, 2, 3, 5]
+        >>> pancake_sort([])
+        []
+        >>> pancake_sort([-2, -5, -45])
+        [-45, -5, -2]
+        """
+        cur = len(arr)
+        res = []
+        while cur > 1:
+            # step 1) Find the maximum number in arr
+            max_idx = arr.index(max(arr[0:cur]))
+            res = res + [max_idx+1, cur] # idx is 1 based
+            # step 2) Reverse from 0 to max_idx
+            #arr = arr[max_idx::-1] + arr[max_idx + 1 : len(arr)] # this is OK as well
+            arr = arr[:max_idx][::-1] + arr[max_idx + 1 : len(arr)]
+            # step 3) Reverse whole list
+            #arr = arr[cur - 1 :: -1] + arr[cur : len(arr)] # this is OK as well
+            arr = arr[:cur - 1][::-1] + arr[cur : len(arr)]
+            cur -= 1
+        print ("arr = " + str(arr))
+        return res
+
 # V1
 # https://leetcode.com/problems/pancake-sorting/discuss/817978/Python-O(n2)-by-simulation-w-Comment
 # https://leetcode.com/problems/pancake-sorting/discuss/330990/Python
