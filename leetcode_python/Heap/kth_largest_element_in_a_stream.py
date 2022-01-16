43cb21798b75f379d192222062bd12ddd75e87f5
yennanliu <f339339@gmail.com>
2022-01-16 21:31:53 +0800

    update 703, cheatsheet, progress

--- leetcode_python/Heap/kth_largest_element_in_a_stream.py
+++ leetcode_python/Heap/kth_largest_element_in_a_stream.py
@@ -1,22 +1,43 @@
-# 703. Kth Largest Element in a Stream
-# Easy
-#
-# Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
-#
-# Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, which contains initial elements from the stream. For each call to the method KthLargest.add, return the element representing the kth largest element in the stream.
-#
-# Example:
-#
-# int k = 3;
-# int[] arr = [4,5,8,2];
-# KthLargest kthLargest = new KthLargest(3, arr);
-# kthLargest.add(3);   // returns 4
-# kthLargest.add(5);   // returns 5
-# kthLargest.add(10);  // returns 5
-# kthLargest.add(9);   // returns 8
-# kthLargest.add(4);   // returns 8
-# Note:
-# You may assume that nums' length ≥ k-1 and k ≥ 1.
+"""
+
+703. Kth Largest Element in a Stream
+Easy
+
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+ 
+
+Constraints:
+
+1 <= k <= 104
+0 <= nums.length <= 104
+-104 <= nums[i] <= 104
+-104 <= val <= 104
+At most 104 calls will be made to add.
+It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+
+"""
 
 # V0
 class KthLargest(object):
