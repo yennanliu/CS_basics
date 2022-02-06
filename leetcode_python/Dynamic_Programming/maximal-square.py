ffee21e5b1b791311ebe8918c5fea2112c8d2d87
yennanliu <f339339@gmail.com>
2022-02-06 09:20:00 +0800

    update 207, cheatsheet, progress

--- leetcode_python/Dynamic_Programming/maximal-square.py
+++ leetcode_python/Dynamic_Programming/maximal-square.py
@@ -1,3 +1,37 @@
+"""
+
+221. Maximal Square
+Medium
+
+Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 4
+Example 2:
+
+
+Input: matrix = [["0","1"],["1","0"]]
+Output: 1
+Example 3:
+
+Input: matrix = [["0"]]
+Output: 0
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'.
+
+"""
+
 # V0  
 
 # V1 
