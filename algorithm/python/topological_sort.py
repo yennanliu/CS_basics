7364678f5d811a6215a73571d1f2719ce9419c5f
yennanliu <f339339@gmail.com>
2022-02-06 10:56:06 +0800

    update  algorithm/python/topological_sort.py

--- algorithm/python/topological_sort.py
+++ algorithm/python/topological_sort.py
@@ -1,10 +1,148 @@
-#################################################################
+#---------------------------------------------------------------
 # ALGORITHM DEMO : TOPLOGICAL SORT
-#################################################################
+#---------------------------------------------------------------
+
+# Topological Sort is a algorithm can find "ordering" on an "order dependency" graph
+# Concept
+# https://blog.techbridge.cc/2020/05/10/leetcode-topological-sort/
+# https://alrightchiu.github.io/SecondRound/graph-li-yong-dfsxun-zhao-dagde-topological-sorttuo-pu-pai-xu.html
+
 
 # V0
+# IDEA : implement topologicalSortUtil, topologicalSort, and addEdge methods
+# step 1) maintain a stack, save "ordering" nodes in it (and return in final step)
+# step 2) init visited as [False]*self.V  (all nodes are NOT visited yet)
+# step 3) iterate over all vertices in graph, if not visited, then run topologicalSortUtil
+# step 4) return result (stack)
+from collections import defaultdic t 
+class Graph: 
+    def __init__(self,vertices): 
+        self.graph = defaultdict(list) 
+        self.V = vertices
+  
+    # for testing (build graph)
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
+  
+    def topologicalSortUtil(self,v,visited,stack): 
+  
+        visited[v] = True
+  
+        for i in self.graph[v]: 
+            if visited[i] == False: 
+                self.topologicalSortUtil(i,visited,stack) 
+  
+        stack.insert(0,v) 
+  
+    def topologicalSort(self): 
+        visited = [False]*self.V 
+        stack =[] 
+  
+        for i in range(self.V): 
+            if visited[i] == False: 
+                self.topologicalSortUtil(i,visited,stack) 
+  
+        print (stack) 
 
 # V1
+# https://www.geeksforgeeks.org/topological-sorting/
+# Python program to print topological sorting of a DAG
+from collections import defaultdict
+class Graph:
+    def __init__(self, vertices):
+        self.graph = defaultdict(list) # dictionary containing adjacency List
+        self.V = vertices # No. of vertices
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    # A recursive function used by topologicalSort
+    def topologicalSortUtil(self, v, visited, stack):
+
+        # Mark the current node as visited.
+        visited[v] = True
+
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.topologicalSortUtil(i, visited, stack)
+
+        # Push current vertex to stack which stores result
+        stack.append(v)
+
+    # The function to do Topological Sort. It uses recursive
+    # topologicalSortUtil()
+    def topologicalSort(self):
+        # Mark all the vertices as not visited
+        visited = [False]*self.V
+        stack = []
+
+        # Call the recursive helper function to store Topological
+        # Sort starting from all vertices one by one
+        for i in range(self.V):
+            if visited[i] == False:
+                self.topologicalSortUtil(i, visited, stack)
+
+        # Print contents of the stack
+        print(stack[::-1]) # return list in reverse order
+
+
+# TEST
+# Driver Code
+# g = Graph(6)
+# g.addEdge(5, 2)
+# g.addEdge(5, 0)
+# g.addEdge(4, 0)
+# g.addEdge(4, 1)
+# g.addEdge(2, 3)
+# g.addEdge(3, 1)
+#
+# print ("Following is a Topological Sort of the given graph")
+#
+# # Function Call
+# g.topologicalSort()
+
+
+# V1
+# https://github.com/TheAlgorithms/Python/blob/master/sorts/topological_sort.py
+"""Topological Sort."""
+
+#     a
+#    / \
+#   b  c
+#  / \
+# d  e
+# edges = {"a": ["c", "b"], "b": ["d", "e"], "c": [], "d": [], "e": []}
+# vertices = ["a", "b", "c", "d", "e"]
+class Graph:
+    def topological_sort(self, start, visited, sort):
+        """Perform topological sort on a directed acyclic graph."""
+        current = start
+        # add current to visited
+        visited.append(current)
+        neighbors = edges[current]
+        for neighbor in neighbors:
+            # if neighbor not in visited, visit
+            if neighbor not in visited:
+                sort = topological_sort(neighbor, visited, sort)
+        # if all neighbors visited add current to sort
+        sort.append(current)
+        # if all vertices haven't been visited select a new one to visit
+        if len(visited) != len(vertices):
+            for vertice in vertices:
+                if vertice not in visited:
+                    sort = topological_sort(vertice, visited, sort)
+        # return sort
+        return sort
+
+# TEST
+# edges = {"a": ["c", "b"], "b": ["d", "e"], "c": [], "d": [], "e": []}
+# vertices = ["a", "b", "c", "d", "e"]
+# sort = topological_sort("a", [], [])
+# print(sort)
+
+# V1'
 # http://www.runoob.com/python3/python-topological-sorting.html
 class Graph: 
     from collections import defaultdict 
@@ -33,7 +171,8 @@ class Graph:
             if visited[i] == False: 
                 self.topologicalSortUtil(i,visited,stack) 
         print (stack) 
-  
+
+# TEST  
 # g= Graph(6) 
 # g.addEdge(5, 2); 
 # g.addEdge(5, 0); 
@@ -67,6 +206,7 @@ def topoSort(graph):
     else:         
         return None
 
+# TEST
 # G = {
 #     'a':'bf',
 #     'b':'cdf',
@@ -118,12 +258,12 @@ def topological_sort(vertices, edges):
 
     return sortedOrder
 
-def main():
-  print("Topological sort: " +
-        str(topological_sort(4, [[3, 2], [3, 0], [2, 0], [2, 1]])))
-  print("Topological sort: " +
-        str(topological_sort(5, [[4, 2], [4, 3], [2, 0], [2, 1], [3, 1]])))
-  print("Topological sort: " +
-        str(topological_sort(7, [[6, 4], [6, 2], [5, 3], [5, 4], [3, 0], [3, 1], [3, 2], [4, 1]])))
-
+# TEST
+# def main():
+#   print("Topological sort: " +
+#         str(topological_sort(4, [[3, 2], [3, 0], [2, 0], [2, 1]])))
+#   print("Topological sort: " +
+#         str(topological_sort(5, [[4, 2], [4, 3], [2, 0], [2, 1], [3, 1]])))
+#   print("Topological sort: " +
+#         str(topological_sort(7, [[6, 4], [6, 2], [5, 3], [5, 4], [3, 0], [3, 1], [3, 2], [4, 1]])))
 #main()
