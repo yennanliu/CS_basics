f84e77d3ac22d2a015f3c13fbe7c359e560a7161
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2022-02-08 15:36:42 +0800

    update 445, cheatsheet, progress

--- leetcode_python/Linked_list/add-two-numbers-ii.py
+++ leetcode_python/Linked_list/add-two-numbers-ii.py
@@ -1,4 +1,49 @@
+"""
+
+445. Add Two Numbers II
+Medium
+
+You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [7,2,4,3], l2 = [5,6,4]
+Output: [7,8,0,7]
+Example 2:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [8,0,7]
+Example 3:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+ 
+
+Follow up: Could you solve it without reversing the input lists?
+
+"""
+
 # V0
+# IDEA : string + linked list
+# DEMO
+# input :
+# [7,2,4,3]
+# [5,6,4]
+# intermedia output : 
+# l1_num = 7243
+# l2_num = 564
 class Solution:
     def addTwoNumbers(self, l1, l2):
         if not l1 and not l2:
@@ -14,13 +59,22 @@ class Solution:
             l2_num = l2_num * 10 + l2.val
             l2 = l2.next
 
+        print ("l1_num = " + str(l1_num))
+        print ("l2_num = " + str(l2_num))
+
+
+        ### NOTE : trick here :
+        #    -> get int format of 2 linked list first (l1, l2)
+        #    -> then sum them (l1_num + l2_num)
         lsum = l1_num + l2_num
 
         head = ListNode(None)
         cur = head
+        ### NOTE : go thrpigh the linked list int sum, append each digit to ListNode and return it
         for istr in str(lsum):
             cur.next = ListNode(int(istr))
             cur = cur.next
+        # NOTE : need to return head (but not cur, since cur already meet the end of ListNode)
         return head.next
 
 # V1 
