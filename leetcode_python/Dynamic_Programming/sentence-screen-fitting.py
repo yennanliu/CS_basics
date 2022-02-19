fb53af4ba7807e5e47970076ed30a9087270fec3
yennanliu <f339339@gmail.com>
2022-02-19 09:21:30 +0800

    fix 734, 747 paths. code

--- leetcode_python/Dynamic_Programming/sentence-screen-fitting.py
+++ leetcode_python/Dynamic_Programming/sentence-screen-fitting.py
@@ -1,3 +1,53 @@
+"""
+
+418. Sentence Screen Fitting
+
+Medium
+
+Given a rows x cols screen and a sentence represented as a list of strings, return the number of times the given sentence can be fitted on the screen.
+
+The order of words in the sentence must remain unchanged, and a word cannot be split into two lines. A single space must separate two consecutive words in a line.
+
+ 
+
+Example 1:
+
+Input: sentence = ["hello","world"], rows = 2, cols = 8
+Output: 1
+Explanation:
+hello---
+world---
+The character '-' signifies an empty space on the screen.
+Example 2:
+
+Input: sentence = ["a", "bcd", "e"], rows = 3, cols = 6
+Output: 2
+Explanation:
+a-bcd- 
+e-a---
+bcd-e-
+The character '-' signifies an empty space on the screen.
+Example 3:
+
+Input: sentence = ["i","had","apple","pie"], rows = 4, cols = 5
+Output: 1
+Explanation:
+i-had
+apple
+pie-i
+had--
+The character '-' signifies an empty space on the screen.
+ 
+
+Constraints:
+
+1 <= sentence.length <= 100
+1 <= sentence[i].length <= 10
+sentence[i] consists of lowercase English letters.
+1 <= rows, cols <= 2 * 104
+
+"""
+
 # V0
 
 # V1
