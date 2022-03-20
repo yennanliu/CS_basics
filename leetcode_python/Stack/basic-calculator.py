95d0d65b34dafc41a5e4605132fccc056e38416c
yennanliu <f339339@gmail.com>
2022-03-20 19:49:03 +0800

    update 227, 224, progress, readme

--- leetcode_python/Stack/basic-calculator.py
+++ leetcode_python/Stack/basic-calculator.py
@@ -36,6 +36,99 @@ Every number and running calculation will fit in a signed 32-bit integer.
 """
 
 # V0
+# IDEA : STACK
+class Solution(object):
+    def calculate(self, s):
+        res = 0
+        num = 0
+        sign = 1
+        stack = [1]
+        s += "+"
+        for i in s:
+            if i.isdigit():
+                num = 10*num + int(i)
+            elif i in "+-":
+                res += num * sign * stack[-1]
+                #sign = 1 if i=="+" else -1
+                if i == "+":
+                    sign = 1
+                else:
+                    sign = -1
+                num = 0
+            elif i == "(":
+                stack.append(sign * stack[-1])
+                sign = 1
+            elif i == ")":
+                res += num * sign * stack[-1]
+                num = 0
+                stack.pop(-1)
+        return res
+
+# V0'
+# IDEA : STACK
+class Solution:
+    def calculate(self, s):
+
+        stack = []
+        operand = 0
+        res = 0 # For the on-going result
+        sign = 1 # 1 means positive, -1 means negative  
+
+        for ch in s:
+            if ch.isdigit():
+
+                # Forming operand, since it could be more than one digit
+                operand = (operand * 10) + int(ch)
+
+            elif ch == '+':
+
+                # Evaluate the expression to the left,
+                # with result, sign, operand
+                res += sign * operand
+
+                # Save the recently encountered '+' sign
+                sign = 1
+
+                # Reset operand
+                operand = 0
+
+            elif ch == '-':
+
+                res += sign * operand
+                sign = -1
+                operand = 0
+
+            elif ch == '(':
+
+                # Push the result and sign on to the stack, for later
+                # We push the result first, then sign
+                stack.append(res)
+                stack.append(sign)
+
+                # Reset operand and result, as if new evaluation begins for the new sub-expression
+                sign = 1
+                res = 0
+
+            elif ch == ')':
+
+                # Evaluate the expression to the left
+                # with result, sign and operand
+                res += sign * operand
+
+                # ')' marks end of expression within a set of parenthesis
+                # Its result is multiplied with sign on top of stack
+                # as stack.pop() is the sign before the parenthesis
+                res *= stack.pop() # stack pop 1, sign
+
+                # Then add to the next operand on the top.
+                # as stack.pop() is the result calculated before this parenthesis
+                # (operand on stack) + (sign on stack * (result from parenthesis))
+                res += stack.pop() # stack pop 2, operand
+
+                # Reset the operand
+                operand = 0
+
+        return res + sign * operand
 
 # V1
 # https://leetcode.com/problems/basic-calculator/discuss/62418/Python-with-stack
@@ -58,6 +151,137 @@ class Solution(object):
                 num = 0
                 stack.pop()
         return res
+
+# V1
+# IDEA : Stack and String Reversal
+# https://leetcode.com/problems/basic-calculator/solution/
+class Solution:
+
+    def evaluate_expr(self, stack):
+        
+        # If stack is empty or the expression starts with
+        # a symbol, then append 0 to the stack.
+        # i.e. [1, '-', 2, '-'] becomes [1, '-', 2, '-', 0]
+        if not stack or type(stack[-1]) == str:
+            stack.append(0)
+            
+        res = stack.pop()
+
+        # Evaluate the expression till we get corresponding ')'
+        while stack and stack[-1] != ')':
+            sign = stack.pop()
+            if sign == '+':
+                res += stack.pop()
+            else:
+                res -= stack.pop()
+        return res       
+
+    def calculate(self, s: str) -> int:
+
+        stack = []
+        n, operand = 0, 0
+
+        for i in range(len(s) - 1, -1, -1):
+            ch = s[i]
+
+            if ch.isdigit():
+
+                # Forming the operand - in reverse order.
+                operand = (10**n * int(ch)) + operand
+                n += 1
+
+            elif ch != " ":
+                if n:
+                    # Save the operand on the stack
+                    # As we encounter some non-digit.
+                    stack.append(operand)
+                    n, operand = 0, 0
+
+                if ch == '(':         
+                    res = self.evaluate_expr(stack)
+                    stack.pop()        
+
+                    # Append the evaluated result to the stack.
+                    # This result could be of a sub-expression within the parenthesis.
+                    stack.append(res)
+
+                # For other non-digits just push onto the stack.
+                else:
+                    stack.append(ch)
+
+        # Push the last operand to stack, if any.
+        if n:
+            stack.append(operand)
+
+        # Evaluate any left overs in the stack.
+        return self.evaluate_expr(stack)
+
+# V1
+# IDEA : Stack and No String Reversal
+# https://leetcode.com/problems/basic-calculator/solution/
+class Solution:
+    def calculate(self, s: str) -> int:
+
+        stack = []
+        operand = 0
+        res = 0 # For the on-going result
+        sign = 1 # 1 means positive, -1 means negative  
+
+        for ch in s:
+            if ch.isdigit():
+
+                # Forming operand, since it could be more than one digit
+                operand = (operand * 10) + int(ch)
+
+            elif ch == '+':
+
+                # Evaluate the expression to the left,
+                # with result, sign, operand
+                res += sign * operand
+
+                # Save the recently encountered '+' sign
+                sign = 1
+
+                # Reset operand
+                operand = 0
+
+            elif ch == '-':
+
+                res += sign * operand
+                sign = -1
+                operand = 0
+
+            elif ch == '(':
+
+                # Push the result and sign on to the stack, for later
+                # We push the result first, then sign
+                stack.append(res)
+                stack.append(sign)
+
+                # Reset operand and result, as if new evaluation begins for the new sub-expression
+                sign = 1
+                res = 0
+
+            elif ch == ')':
+
+                # Evaluate the expression to the left
+                # with result, sign and operand
+                res += sign * operand
+
+                # ')' marks end of expression within a set of parenthesis
+                # Its result is multiplied with sign on top of stack
+                # as stack.pop() is the sign before the parenthesis
+                res *= stack.pop() # stack pop 1, sign
+
+                # Then add to the next operand on the top.
+                # as stack.pop() is the result calculated before this parenthesis
+                # (operand on stack) + (sign on stack * (result from parenthesis))
+                res += stack.pop() # stack pop 2, operand
+
+                # Reset the operand
+                operand = 0
+
+        return res + sign * operand
 
 # V1'
 # https://leetcode.com/problems/basic-calculator/discuss/196363/Python-solution
