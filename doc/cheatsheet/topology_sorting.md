197780cb2b81b421a125e026868f8a94570ff29d
yennanliu <f339339@gmail.com>
2022-03-27 18:53:31 +0800

    update 207, cheatsheet, progress

--- doc/cheatsheet/topology_sorting.md
+++ doc/cheatsheet/topology_sorting.md
@@ -103,6 +103,56 @@ print (r)
 ```python
 # LC 207 Course Schedule
 # NOTE : there are also bracktrack, dfs approachs for this problem
+# V0
+# IDEA : LC Course Schedule II 
+from collections import defaultdict
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        # edge case
+        if not prerequisites:
+            return [x for x in range(numCourses)]
+        
+        # help func : dfs
+        # 3 cases :  0 : unknown, 1 :visiting, 2 : visited   
+        def dfs(idx, visited, g, res):
+            if visited[idx] == 1:
+                return False
+            # NOTE !!! if visited[idx] == 2, means already visited, return True directly (and check next idx in range(numCourses))
+            if visited[idx] == 2:
+                return True
+            visited[idx] = 1
+            """
+            NOTE this !!!
+                1) for j in g[idx] (but not for i in range(numCourses))
+                2) go through idx in g[idx]
+            """
+            for j in g[idx]:
+                if not dfs(j, visited, g, res):
+                    return False
+            """
+            don't forget to make idx as visited (visited[idx] = 2)
+            """
+            visited[idx] = 2
+            """
+            NOTE : the main difference between LC 207, 210
+            -> we append idx to res (our ans)
+            """
+            res.append(idx)
+            return True
+        # init
+        visited = [0] * numCourses
+        # build grath
+        g = defaultdict(list)
+        for p in prerequisites:
+            g[p[0]].append(p[1])
+        res = []
+        """
+        NOTE :  go through idx in numCourses (for idx in range(numCourses))
+        """
+        for idx in range(numCourses):
+            if not dfs(idx, visited, g, res):
+                return False #[]
+        return len(res) > 0
 
 # V1
 # IDEA : Topological Sort
--- leetcode_python/Breadth-First-Search/course-schedule.py
+++ leetcode_python/Breadth-First-Search/course-schedule.py
@@ -31,14 +31,62 @@ Constraints:
 prerequisites[i].length == 2
 0 <= ai, bi < numCourses
 All the pairs prerequisites[i] are unique.
-Accepted
-809,202
-Submissions
-1,801,276
+
 
 """
 
 # V0
+# IDEA : LC Course Schedule II 
+from collections import defaultdict
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        # edge case
+        if not prerequisites:
+            return [x for x in range(numCourses)]
+        
+        # help func : dfs
+        # 3 cases :  0 : unknown, 1 :visiting, 2 : visited   
+        def dfs(idx, visited, g, res):
+            if visited[idx] == 1:
+                return False
+            # NOTE !!! if visited[idx] == 2, means already visited, return True directly (and check next idx in range(numCourses))
+            if visited[idx] == 2:
+                return True
+            visited[idx] = 1
+            """
+            NOTE this !!!
+                1) for j in g[idx] (but not for i in range(numCourses))
+                2) go through idx in g[idx]
+            """
+            for j in g[idx]:
+                if not dfs(j, visited, g, res):
+                    return False
+            """
+            don't forget to make idx as visited (visited[idx] = 2)
+            """
+            visited[idx] = 2
+            """
+            NOTE : the main difference between LC 207, 210
+            -> we append idx to res (our ans)
+            """
+            res.append(idx)
+            return True
+        # init
+        visited = [0] * numCourses
+        # build grath
+        g = defaultdict(list)
+        for p in prerequisites:
+            g[p[0]].append(p[1])
+        res = []
+        """
+        NOTE :  go through idx in numCourses (for idx in range(numCourses))
+        """
+        for idx in range(numCourses):
+            if not dfs(idx, visited, g, res):
+                return False #[]
+        return len(res) > 0
+
+# V0'
 # IDEA : DFS
 from collections import defaultdict
 class Solution(object):
