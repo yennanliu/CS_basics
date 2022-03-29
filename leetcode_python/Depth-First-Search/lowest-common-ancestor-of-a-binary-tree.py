3eeb68ffe5e56ba6a2e12b03bdcbbfb713ef23c1
yennanliu <f339339@gmail.com>
2022-03-29 08:35:26 +0800

    update 236, progress, add new problems

--- leetcode_python/Depth-First-Search/lowest-common-ancestor-of-a-binary-tree.py
+++ leetcode_python/Depth-First-Search/lowest-common-ancestor-of-a-binary-tree.py
@@ -1,5 +1,8 @@
 """
 
+236. Lowest Common Ancestor of a Binary Tree
+Medium
+
 Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
 According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
@@ -47,7 +50,7 @@ class Solution(object):
         if not root or p == root or q == root:
             return root
         ### NOTE here
-        #  -> not root.left, root.right, BUT left, right
+        # -> WE DON'T need to have if root.left, if root.right logic, but get left, right directly (search to left, right)
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
 
