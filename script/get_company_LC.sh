3a97306b8a144682f2409ca3a90479372eb5b387
yennanliu <f339339@gmail.com>
2022-04-01 08:24:50 +0800

    fix script header

--- script/get_company_LC.sh
+++ script/get_company_LC.sh
@@ -11,4 +11,10 @@ if [[ ! $COMPANY || ! $OUTPUT_FILE ]]; then
     exit 1 ; 
 fi
 
+header1='|  #  | Title           |  Solution       |  Time           | Space           | Difficulty    | Note           | Status|'
+header2='|-----|---------------- | --------------- | --------------- | --------------- | ------------- |--------------|-----|'
+
+echo $header1 >> $OUTPUT_FILE
+echo $header2 >> $OUTPUT_FILE
+
 cat README.md | grep $COMPANY >> $OUTPUT_FILE
