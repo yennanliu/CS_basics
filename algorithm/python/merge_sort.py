a6aac9d3d2c127a26b3f85eed09f6b5bc93949aa
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2022-04-14 13:40:29 +0800

    update merge_sort.py, readme

--- algorithm/python/merge_sort.py
+++ algorithm/python/merge_sort.py
@@ -2,6 +2,14 @@
 # MERGE SORT
 #---------------------------------------------------------------
 
+# https://www.journaldev.com/31541/merge-sort-algorithm-java-c-python#:~:text=The%20time%20complexity%20of%20MergeSort,time%20to%20merge%20two%20halves.
+# Merge sort is one of the most efficient sorting algorithms. 
+# It works on the principle of Divide and Conquer based on the 
+# idea of breaking down a list into several sub-lists until each 
+# sublist consists of a single element and merging those sublists in a manner 
+# that results into a sorted list.
+
+
 # V0 
 # https://www.geeksforgeeks.org/merge-sort/
 # IDEA : MERGE SORT 
@@ -42,6 +50,57 @@ def mergeSort(arr):
             arr[k] = R[j] 
             j+=1
             k+=1
+
+# V1
+# https://www.journaldev.com/31541/merge-sort-algorithm-java-c-python#:~:text=The%20time%20complexity%20of%20MergeSort,time%20to%20merge%20two%20halves.
+def merge_sort(unsorted_array):
+    if len(unsorted_array) > 1:
+        mid = len(unsorted_array) // 2  # Finding the mid of the array
+        left = unsorted_array[:mid]  # Dividing the array elements
+        right = unsorted_array[mid:]  # into 2 halves
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        #  data to temp arrays L[] and R[]
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                unsorted_array[k] = left[i]
+                i += 1
+            else:
+                unsorted_array[k] = right[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(left):
+            unsorted_array[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            unsorted_array[k] = right[j]
+            j += 1
+            k += 1
+
+
+# Code to print the list
+def print_list(array1):
+    for i in range(len(array1)):
+        print(array1[i], end=" ")
+    print()
+
+
+# driver code to test the above code
+# if __name__ == '__main__':
+#     array = [12, 11, 13, 5, 6, 7]
+#     print("Given array is", end="\n")
+#     print_list(array)
+#     merge_sort(array)
+#     print("Sorted array is: ", end="\n")
+#     print_list(array)
 
 # V1 
 # https://github.com/TheAlgorithms/Python/blob/master/sorts/merge_sort.py
