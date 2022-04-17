54482d5f2c6f1da979b22159ec3a75166c77e879
yennanliu <f339339@gmail.com>
2022-04-17 17:40:03 +0800

    update 463

--- leetcode_python/Hash_table/island_perimeter.py
+++ leetcode_python/Hash_table/island_perimeter.py
@@ -59,6 +59,9 @@ class Solution:
 
 # V0'
 # IDEA : SIMPLE COUNTING
+# IDEA :
+# -> Go through every cell on the grid and whenever you are at cell 1 (land cell), look for surrounding (UP, RIGHT, DOWN, LEFT) cells. A land cell without any surrounding land cell will have a perimeter of 4. Subtract 1 for each surrounding land cell.
+# -> When you are at cell 0 (water cell), you don't need to do anything. Just proceed to another cell.
 class Solution:
     def islandPerimeter(self, grid):
         
@@ -93,6 +96,8 @@ class Solution:
                 
 # V1
 # IDEA : SIMPLE COUNTING
+# -> Go through every cell on the grid and whenever you are at cell 1 (land cell), look for surrounding (UP, RIGHT, DOWN, LEFT) cells. A land cell without any surrounding land cell will have a perimeter of 4. Subtract 1 for each surrounding land cell.
+# -> When you are at cell 0 (water cell), you don't need to do anything. Just proceed to another cell.
 # https://leetcode.com/problems/island-perimeter/solution/
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
