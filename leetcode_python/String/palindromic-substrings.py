c8ae4acf44101d885f49decdebaa1830e4728245
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2022-04-19 18:04:09 +0800

    update 647, cheatsheet, progress

--- leetcode_python/String/palindromic-substrings.py
+++ leetcode_python/String/palindromic-substrings.py
@@ -1,34 +1,32 @@
 """
 
-Given a string s, return the longest palindromic substring in s.
+647. Palindromic Substrings
+Medium
 
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
 
 Example 1:
 
-Input: s = "babad"
-Output: "bab"
-Note: "aba" is also a valid answer.
-
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
 Example 2:
 
-Input: s = "cbbd"
-Output: "bb"
-
-Example 3:
-
-Input: s = "a"
-Output: "a"
-
-Example 4:
-
-Input: s = "ac"
-Output: "a"
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
  
 
 Constraints:
 
 1 <= s.length <= 1000
-s consist of only digits and English letters (lower-case and/or upper-case),
+s consists of lowercase English letters.
 
 """
 
@@ -146,6 +144,106 @@ class Solution(object):
             dp[i][i] = 1
             count += 1
         return count
+
+# V1'''
+# IDEA : Check All Substrings
+# https://leetcode.com/problems/palindromic-substrings/solution/
+# JAVA 
+# class Solution {
+#     private boolean isPalindrome(String s, int start, int end) {
+#         while (start < end) {
+#             if (s.charAt(start) != s.charAt(end)) 
+#                 return false;
+#
+#             ++start;
+#             --end;
+#         }
+#
+#         return true;
+#     }
+#
+#     public int countSubstrings(String s) {
+#         int ans = 0;
+#
+#         for (int start = 0; start < s.length(); ++start)
+#             for (int end = start; end < s.length(); ++end) 
+#                 ans += isPalindrome(s, start, end) ? 1 : 0;
+#
+#         return ans;
+#     }
+# }
+
+# V1'''''
+# IDEA : DP
+# https://leetcode.com/problems/palindromic-substrings/solution/
+# JAVA
+# class Solution {
+#     public int countSubstrings(String s) {
+#         int n = s.length(), ans = 0;
+#
+#         if (n <= 0) 
+#             return 0;
+#
+#         boolean[][] dp = new boolean[n][n];
+#
+#         // Base case: single letter substrings
+#         for (int i = 0; i < n; ++i, ++ans) 
+#             dp[i][i] = true;
+#
+#         // Base case: double letter substrings
+#         for (int i = 0; i < n - 1; ++i) {
+#             dp[i][i + 1] = (s.charAt(i) == s.charAt(i + 1));
+#             ans += (dp[i][i + 1] ? 1 : 0);
+#         }
+#
+#         // All other cases: substrings of length 3 to n
+#         for (int len = 3; len <= n; ++len)
+#             for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
+#                 dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
+#                 ans += (dp[i][j] ? 1 : 0);
+#             }
+#
+#         return ans;
+#     }
+# }
+
+
+# V1''''''
+# IDEA : Expand Around Possible Centers
+# https://leetcode.com/problems/palindromic-substrings/solution/
+# JAVA 
+# class Solution {
+#     public int countSubstrings(String s) {
+#         int ans = 0;
+#
+#         for (int i = 0; i < s.length(); ++i) {
+#             // odd-length palindromes, single character center
+#             ans += countPalindromesAroundCenter(s, i, i);
+#
+#             // even-length palindromes, consecutive characters center
+#             ans += countPalindromesAroundCenter(s, i, i + 1);
+#         }
+#
+#         return ans;
+#     }
+#
+#     private int countPalindromesAroundCenter(String ss, int lo, int hi) {
+#         int ans = 0;
+#
+#         while (lo >= 0 && hi < ss.length()) {
+#             if (ss.charAt(lo) != ss.charAt(hi))
+#                 break;      // the first and last characters don't match!
+#
+#             // expand around the center
+#             lo--;
+#             hi++;
+#
+#             ans++;
+#         }
+#
+#         return ans;
+#     }
+# }
 
 # V2 
 # Time:  O(n)
