8d196531aab7e11c88ea048d6d28dad51eb85c70
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2022-04-25 18:53:19 +0800

    update 346, progress

--- leetcode_python/Queue/moving-average-from-data-stream.py
+++ leetcode_python/Queue/moving-average-from-data-stream.py
@@ -3,20 +3,58 @@
 # https://baihuqian.github.io/2018-08-09-moving-average-from-data-stream/
 # https://www.cnblogs.com/grandyang/p/5450001.html
 
-[LeetCode] 346. Moving Average from Data Stream
- 
+346. Moving Average from Data Stream
+Easy
 
 Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
 
-Example:
+Implement the MovingAverage class:
 
-MovingAverage m = new MovingAverage(3);
-m.next(1) = 1
-m.next(10) = (1 + 10) / 2
-m.next(3) = (1 + 10 + 3) / 3
-m.next(5) = (10 + 3 + 5) / 3
+MovingAverage(int size) Initializes the object with the size of the window size.
+double next(int val) Returns the moving average of the last size values of the stream.
+ 
+
+Example 1:
+
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+Output
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+Explanation
+MovingAverage movingAverage = new MovingAverage(3);
+movingAverage.next(1); // return 1.0 = 1 / 1
+movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+ 
+
+Constraints:
+
+1 <= size <= 1000
+-105 <= val <= 105
+At most 104 calls will be made to next.
 
 """
+
+# V0
+# IDEA : pop, append op
+# python 3
+class MovingAverage(object):
+
+    def __init__(self, size):
+        self.size = size
+        self.stack = []
+        self.cur = 0
+        
+    def next(self, val):
+        if self.cur == self.size:
+            self.stack.pop(0)
+            self.cur -= 1
+        self.stack.append(val)
+        self.cur += 1
+        return sum(self.stack) / self.cur
 
 # V0
 # IDEA : deque
