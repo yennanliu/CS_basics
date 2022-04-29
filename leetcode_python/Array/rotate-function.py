6c75b23dc9b6d3de0fe1a97dd9248356b97aae3a
yennanliu <f339339@gmail.com>
2022-04-29 19:23:14 +0800

    update 396, progress

--- leetcode_python/Array/rotate-function.py
+++ leetcode_python/Array/rotate-function.py
@@ -1,53 +1,68 @@
 """
-Given an array of integers A and let n to be its length.
 
-Assume Bk to be an array obtained by rotating the array A k positions clock-wise, we define a "rotation function" F on A as follow:
+396. Rotate Function
+Medium
 
-F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1].
+You are given an integer array nums of length n.
 
-Calculate the MAX value of F(0), F(1), ..., F(n-1).
+Assume arrk to be an array obtained by rotating nums by k positions clock-wise. We define the rotation function F on nums as follow:
 
-Note:
-n is guaranteed to be less than 105.
+F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
+Return the maximum value of F(0), F(1), ..., F(n-1).
 
-Example:
+The test cases are generated so that the answer fits in a 32-bit integer.
 
-A = [4, 3, 2, 6]
+ 
+Example 1:
 
+Input: nums = [4,3,2,6]
+Output: 26
+Explanation:
 F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
 F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
 F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
 F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
-
 So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.
+Example 2:
+
+Input: nums = [100]
+Output: 0
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+-100 <= nums[i] <= 100
+
 """
 
 # V0
 # IDEA : MATH
 # first, we represent the F(1) op as below:
-
+#
 # F(0) = 0A + 1B + 2C +3D
-
+#
 # F(1) = 0D + 1A + 2B +3C
-
+#
 # F(2) = 0C + 1D + 2A +3B
-
+#
 # F(3) = 0B + 1C + 2D +3A
-
+#
 # then, by some math manipulation, we have below relation:
-
+#
 # set sum = 1A + 1B + 1C + 1D
-
+#
 # -> F(1) = F(0) + sum - 4D
-
+#
 # -> F(2) = F(1) + sum - 4C
-
+#
 # -> F(3) = F(2) + sum - 4B
-
+#
 # so we find the rules!
-
+#
 # => F(i) = F(i-1) + sum - n*A[n-i]
-
+#
 # https://www.cnblogs.com/grandyang/p/5869791.html
 # http://bookshadow.com/weblog/2016/09/11/leetcode-rotate-function/
 class Solution(object):
@@ -59,6 +74,30 @@ class Solution(object):
         for x in range(size - 1, 0, -1):
             sumn += sums - size * A[x]
             ans = max(ans, sumn)
+        return ans
+
+# V0'
+# IDEA : BRUTE FORCE (TLE)
+class Solution(object):
+    def maxRotateFunction(self, nums):
+        # help func
+        def help(arr):
+            ans = 0
+            for i in range(len(arr)):
+                tmp = i * arr[i]
+                ans += tmp
+            return ans
+        # edge case
+        if not nums:
+            return 0
+        # rotate
+        ans = -float('inf')
+        for i in range(len(nums)):
+            tmp = nums.pop(-1)
+            nums.insert(0, tmp)
+            cur = help(nums)
+            ans = max(ans, cur)
+            #print("nums = " + str(nums) + " cur = " + str(cur))
         return ans
 
 # V1 
@@ -141,7 +180,6 @@ class Solution(object):
         fi = 0
         for i in range(len(A)):
             fi += i * A[i]
-
         result = fi
         for i in range(1, len(A)+1):
             fi += s - len(A) * A[-i]
