f400a84a1abb3cc6ae3a61f0ea4d1e7f03b37302
yennanliu <f339339@gmail.com>
2022-05-01 19:42:41 +0800

    fix 101

--- leetcode_python/Stack/symmetric-tree.py
+++ leetcode_python/Stack/symmetric-tree.py
@@ -1,6 +1,6 @@
 """
 
-                                       . Symmetric Tree
+101. Symmetric Tree
 Easy
 
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
