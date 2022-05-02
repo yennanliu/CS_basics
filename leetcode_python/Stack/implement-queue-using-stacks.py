7e2ef6447da674189e7b9cd4a03b9b0c583978c2
yennanliu <f339339@gmail.com>
2022-05-02 15:37:28 +0800

    update 232, cheatsheet, progress

--- leetcode_python/Stack/implement-queue-using-stacks.py
+++ leetcode_python/Stack/implement-queue-using-stacks.py
@@ -1,43 +1,49 @@
-# Implement the following operations of a queue using stacks.
-#
-# push(x) -- Push element x to the back of queue.
-# pop() -- Removes the element from in front of queue.
-# peek() -- Get the front element.
-# empty() -- Return whether the queue is empty.
-# Example:
-#
-# MyQueue queue = new MyQueue();
-#
-# queue.push(1);
-# queue.push(2);  
-# queue.peek();  // returns 1
-# queue.pop();   // returns 1
-# queue.empty(); // returns false
-# Notes:
-#
-# You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
-# Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
-# You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
-#
-# Time:  O(1), amortized
-# Space: O(n)
-#
-# Implement the following operations of a queue using stacks.
-#
-# push(x) -- Push element x to the back of queue.
-# pop() -- Removes the element from in front of queue.
-# peek() -- Get the front element.
-# empty() -- Return whether the queue is empty.
-#
-# Notes:
-# You must use only standard operations of a stack
-# -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
-# Depending on your language, stack may not be supported natively.
-# You may simulate a stack by using a list or deque (double-ended queue),
-# as long as you use only standard operations of a stack.
-# You may assume that all operations are valid
-# (for example, no pop or peek operations will be called on an empty queue).
-#
+"""
+
+232. Implement Queue using Stacks
+Easy
+
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, peek, and empty.
+All the calls to pop and peek are valid.
+ 
+
+Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.
+
+"""
 
 # V0 
 # IDEA : STACK 
@@ -226,6 +232,10 @@ class MyQueue:
         :rtype: bool
         """
         return self.stack == []
+
+# V1'''''
+# https://leetcode.com/problems/implement-queue-using-stacks/solution/
+# JAVA
 
 # V2
 class Queue:
