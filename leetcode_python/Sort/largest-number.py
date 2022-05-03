920deae859e54fec6c4e8730998905c151ab3d4f
yennanliu <f339339@gmail.com>
2022-05-03 14:26:54 +0800

    update 179, cheatsheet, progress

--- leetcode_python/Sort/largest-number.py
+++ leetcode_python/Sort/largest-number.py
@@ -1,4 +1,45 @@
+"""
+
+179. Largest Number
+Medium
+
+Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+Since the result may be very large, so you need to return a string instead of an integer.
+
+ 
+
+Example 1:
+
+Input: nums = [10,2]
+Output: "210"
+Example 2:
+
+Input: nums = [3,30,34,5,9]
+Output: "9534330"
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 109
+
+"""
+
 # V0
+# IDEA : Sorting via Custom Comparator
+class compare(str):
+    # __lt__ defines ">" operator in python
+    def __lt__(x, y):
+        return x+y > y+x
+
+class Solution:
+    def largestNumber(self, nums):
+        largest = sorted([str(v) for v in nums], key=compare) 
+        largest = ''.join(largest) 
+        return '0' if largest[0] == '0' else largest 
+
+# V0'
 from functools import cmp_to_key
 class Solution:
     # @param num, a list of integers
@@ -22,6 +63,18 @@ class Solution:
         largest = sorted([str(v) for v in nums], key=compare) 
         largest = ''.join(largest) 
         return '0' if largest[0] == '0' else largest 
+
+# V1
+# IDEA : Sorting via Custom Comparator
+# https://leetcode.com/problems/largest-number/solution/
+class LargerNumKey(str):
+    def __lt__(x, y):
+        return x+y > y+x
+        
+class Solution:
+    def largestNumber(self, nums):
+        largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
+        return '0' if largest_num[0] == '0' else largest_num
 
 # V1
 # http://bookshadow.com/weblog/2015/01/13/leetcode-largest-number/
--- leetcode_python/Sort/longest-consecutive-sequence.py
+++ leetcode_python/Sort/longest-consecutive-sequence.py
@@ -1,16 +1,19 @@
 """
 
-Given an unsorted array of integers nums, 
-return the length of the longest consecutive elements sequence.
+128. Longest Consecutive Sequence
+Medium
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
 You must write an algorithm that runs in O(n) time.
 
+ 
 
 Example 1:
 
 Input: nums = [100,4,200,1,3,2]
 Output: 4
 Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
-
 Example 2:
 
 Input: nums = [0,3,7,2,5,8,4,6,0,1]
@@ -177,7 +180,6 @@ class Solution:
 
         return max(longest_streak, current_streak)
 
-
 # V3
 # IDEA : HashSet and Intelligent Sequence Building
 # https://leetcode.com/problems/longest-consecutive-sequence/solution/
@@ -198,7 +200,6 @@ class Solution:
                 longest_streak = max(longest_streak, current_streak)
 
         return longest_streak
-
 
 # V4
 # https://leetcode.com/problems/longest-consecutive-sequence/discuss/1231015/Concise-Python-Solution-8-lines
