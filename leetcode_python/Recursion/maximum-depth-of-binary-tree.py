0d11a81d808f40f99f64a41938a71f50a7f13433
yennanliu <f339339@gmail.com>
2022-05-04 15:42:53 +0800

    update 100, 104, cheatsheet, progress

--- leetcode_python/Recursion/maximum-depth-of-binary-tree.py
+++ leetcode_python/Recursion/maximum-depth-of-binary-tree.py
@@ -1,36 +1,30 @@
 """
 
+104. Maximum Depth of Binary Tree
+Easy
+
 Given the root of a binary tree, return its maximum depth.
-A binary tree's maximum depth is the number of nodes along the longest path from the 
-root node down to the farthest leaf node.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
  
 
 Example 1:
 
+
 Input: root = [3,9,20,null,null,15,7]
 Output: 3
-
 Example 2:
 
 Input: root = [1,null,2]
 Output: 2
-
-Example 3:
-
-Input: root = []
-Output: 0
-
-Example 4:
-
-Input: root = [0]
-Output: 1
  
 
 Constraints:
 
 The number of nodes in the tree is in the range [0, 104].
 -100 <= Node.val <= 100
+
 
 """
 
