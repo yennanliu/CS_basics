180e89475924e6eb6bd835fdb545858873ebe94a
yennanliu <f339339@gmail.com>
2022-05-04 16:27:17 +0800

    update 116, 117, progress

--- leetcode_python/Recursion/populating-next-right-pointers-in-each-node-ii.py
+++ leetcode_python/Recursion/populating-next-right-pointers-in-each-node-ii.py
@@ -43,8 +43,68 @@ The recursive approach is fine. You may assume implicit stack space does not cou
 """
 
 # V0
-# BFS 
-# DFS (?)
+# IDEA : BFS
+class Solution:
+    def connect(self, root):
+        if not root: return
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            _len = len(queue)
+            for i in range(_len):
+                node = queue.popleft()
+                ### IF NOT LAST NODE, POINT NEXT TO FIRST NODE IN THE QUEUE
+                if i < _len - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
+
+# V0'
+# IDEA : BFS 
+class Solution:
+    def connect(self, root: Optional['Node']) -> Optional['Node']:
+        
+        if not root:
+            return root
+        
+        # Initialize a queue data structure which contains
+        # just the root of the tree
+        Q = collections.deque([root])
+        
+        # Outer while loop which iterates over 
+        # each level
+        while Q:
+            
+            # Note the size of the queue
+            size = len(Q)
+            
+            # Iterate over all the nodes on the current level
+            for i in range(size):
+                
+                # Pop a node from the front of the queue
+                node = Q.popleft()
+                
+                # This check is important. We don't want to
+                # establish any wrong connections. The queue will
+                # contain nodes from 2 levels at most at any
+                # point in time. This check ensures we only 
+                # don't establish next pointers beyond the end
+                # of a level
+                if i < size - 1:
+                    node.next = Q[0]
+                
+                # Add the children, if any, to the back of
+                # the queue
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
+        
+        # Since the tree has now been modified, return the root node
+        return root
 
 # V1
 # IDEA : Level Order Traversal
