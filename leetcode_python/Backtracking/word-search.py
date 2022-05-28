b8819d6aab685216b65179eb667cc1e23791d8c4
yennanliu <f339339@gmail.com>
2022-05-28 20:40:53 +0800

    update 079, progress

--- leetcode_python/Backtracking/word-search.py
+++ leetcode_python/Backtracking/word-search.py
@@ -1,10 +1,11 @@
 """
 
+79. Word Search
+Medium
+
 Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 
-The word can be constructed from letters of sequentially adjacent cells,
-where adjacent cells are horizontally or vertically neighboring. 
-The same letter cell may not be used more than once.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
 
  
 
@@ -13,13 +14,11 @@ Example 1:
 
 Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
 Output: true
-
 Example 2:
 
 
 Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
 Output: true
-
 Example 3:
 
 
@@ -34,6 +33,10 @@ n = board[i].length
 1 <= m, n <= 6
 1 <= word.length <= 15
 board and word consists of only lowercase and uppercase English letters.
+ 
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
+
 
 """
 
@@ -105,6 +108,86 @@ class Solution(object):
         isExit =  self.dfs(board, word, x + 1, y, i + 1) or self.dfs(board, word, x, y + 1, i + 1) or self.dfs(board, word, x - 1, y, i + 1) or self.dfs(board, word, x, y - 1, i + 1)
         board[y][x] = board[y][x].swapcase() # if already visited all possible route within the route collection, then roll back the maked route (.swapcase(), e.g. a->A), and run the other visit again 
         return isExit
+
+# V1
+# IDEA : BACKTEACKING
+# https://leetcode.com/problems/word-search/solution/
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+        self.board = board
+
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.backtrack(row, col, word):
+                    return True
+
+        # no match found after all exploration
+        return False
+
+
+    def backtrack(self, row, col, suffix):
+        # bottom case: we find match for each letter in the word
+        if len(suffix) == 0:
+            return True
+
+        # Check the current status, before jumping into backtracking
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS \
+                or self.board[row][col] != suffix[0]:
+            return False
+
+        ret = False
+        # mark the choice before exploring further.
+        self.board[row][col] = '#'
+        # explore the 4 neighbor directions
+        for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            ret = self.backtrack(row + rowOffset, col + colOffset, suffix[1:])
+            # break instead of return directly to do some cleanup afterwards
+            if ret: break
+
+        # revert the change, a clean slate and no side-effect
+        self.board[row][col] = suffix[0]
+
+        # Tried all directions, and did not find any match
+        return ret
+
+# V1
+# IDEA : BACKTEACKING
+# https://leetcode.com/problems/word-search/solution/
+
+    def backtrack(self, row, col, suffix):
+        """
+            backtracking with side-effect,
+               the matched letter in the board would be marked with "#".
+        """
+        # bottom case: we find match for each letter in the word
+        if len(suffix) == 0:
+            return True
+
+        # Check the current status, before jumping into backtracking
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS \
+                or self.board[row][col] != suffix[0]:
+            return False
+
+        # mark the choice before exploring further.
+        self.board[row][col] = '#'
+        # explore the 4 neighbor directions
+        for rowOffset, colOffset in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
+            # sudden-death return, no cleanup.
+            if self.backtrack(row + rowOffset, col + colOffset, suffix[1:]):
+                return True
+
+        # revert the marking
+        self.board[row][col] = suffix[0]
+
+        # Tried all directions, and did not find any match
+        return False
 
 # V1
 # https://blog.csdn.net/fuxuemingzhu/article/details/79386066
