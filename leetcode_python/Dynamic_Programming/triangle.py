83ff8c064d6b770b6b48fc5ba466469523b7200c
yennanliu <f339339@gmail.com>
2022-06-18 19:33:26 +0800

    update 120, progress

--- leetcode_python/Dynamic_Programming/triangle.py
+++ leetcode_python/Dynamic_Programming/triangle.py
@@ -1,26 +1,121 @@
-# 120. Triangle
-# Medium
-#
-# Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
-#
-# For example, given the following triangle
-#
-# [
-#      [2],
-#     [3,4],
-#    [6,5,7],
-#   [4,1,8,3]
-# ]
-# The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
-#
-# Note:
-#
-# Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+"""
 
+120. Triangle
+Medium
+
+Given a triangle array, return the minimum path sum from top to bottom.
+
+For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+ 
+
+Example 1:
+
+Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+Output: 11
+Explanation: The triangle looks like:
+   2
+  3 4
+ 6 5 7
+4 1 8 3
+The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+Example 2:
+
+Input: triangle = [[-10]]
+Output: -10
+ 
+
+Constraints:
+
+1 <= triangle.length <= 200
+triangle[0].length == 1
+triangle[i].length == triangle[i - 1].length + 1
+-104 <= triangle[i][j] <= 104
+ 
+
+Follow up: Could you do this using only O(n) extra space, where n is the total number of rows in the triangle?
+
+"""
 
 # V0
 
-# V1 
+# V1
+# https://leetcode.com/problems/triangle/solution/
+# IDEA :  Dynamic Programming (Bottom-up: In-place)
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for row in range(1, len(triangle)):
+            for col in range(row + 1):
+                smallest_above = math.inf
+                if col > 0:
+                    smallest_above = triangle[row - 1][col - 1]
+                if col < row:
+                    smallest_above = min(smallest_above, triangle[row - 1][col])
+                triangle[row][col] += smallest_above
+        return min(triangle[-1])
+
+# V1'
+# https://leetcode.com/problems/triangle/solution/
+# IDEA : Dynamic Programming (Bottom-up: Auxiliary Space)
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        prev_row = triangle[0]
+        for row in range(1, len(triangle)):
+            curr_row = []
+            for col in range(row + 1):
+                smallest_above = math.inf
+                if col > 0:
+                    smallest_above = prev_row[col - 1]
+                if col < row:
+                    smallest_above = min(smallest_above, prev_row[col])
+                curr_row.append(triangle[row][col] + smallest_above)
+            prev_row = curr_row
+        return min(prev_row)
+
+# V1''
+# https://leetcode.com/problems/triangle/solution/
+# IDEA : Dynamic Programming (Bottom-up: Flip Triangle Upside Down)
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        below_row = triangle[-1] 
+        n = len(triangle)
+        for row in reversed(range(n - 1)):     
+            curr_row = []
+            for col in range(row + 1):
+                smallest_below = min(below_row[col], below_row[col + 1])
+                curr_row.append(triangle[row][col] + smallest_below)
+            below_row = curr_row
+        return below_row[0]
+
+# V1'''
+# https://leetcode.com/problems/triangle/solution/
+# IDEA : Dynamic Programming (Bottom-up: Flip Triangle Upside Down)
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        below_row = triangle[-1] 
+        n = len(triangle)
+        for row in reversed(range(n - 1)):       
+            curr_row = []
+            for col in range(row + 1):
+                smallest_below = min(below_row[col], below_row[col + 1])
+                curr_row.append(triangle[row][col] + smallest_below)
+            below_row = curr_row
+        return below_row[0]
+
+# V1''''
+# https://leetcode.com/problems/triangle/solution/
+# IDEA : Memoization (Top-Down)
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        @lru_cache(maxsize=None)
+        def min_path(row, col):
+            path = triangle[row][col]
+            if row < len(triangle) - 1:
+                path += min(min_path(row + 1, col), min_path(row + 1, col + 1))
+            return path
+        return min_path(0, 0)
+
+# V1'''''
 # https://blog.csdn.net/qqxx6661/article/details/78474268
 # IDEA : DP 
 # dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
@@ -38,7 +133,7 @@ class Solution(object):
         print(dp)
         return dp[0]
 
-# V1'
+# V1''''''
 # https://blog.csdn.net/fuxuemingzhu/article/details/82883187
 # IDEA : DP
 # minpath[k][i] = min( minpath[k+1][i], minpath[k+1][i+1]) + triangle[k][i]
@@ -57,7 +152,7 @@ class Solution(object):
                 dp[i] = min(dp[i], dp[i + 1]) + triangle[layer][i]
         return dp[0]
 
-# V2 
+# V2
 # Time:  O(m * n)
 # Space: O(n)
 from functools import reduce
