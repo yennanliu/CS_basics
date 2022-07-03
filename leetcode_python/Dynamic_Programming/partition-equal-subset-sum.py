8b912e93ebbfc4a44ab6ebf4719e9de6c6d3033e
yennanliu <f339339@gmail.com>
2022-07-03 20:45:41 +0800

    update 416, progress

--- leetcode_python/Dynamic_Programming/partition-equal-subset-sum.py
+++ leetcode_python/Dynamic_Programming/partition-equal-subset-sum.py
@@ -1,6 +1,113 @@
+"""
+
+416. Partition Equal Subset Sum
+Medium
+
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+
+"""
+
 # VO 
 
-# V1 
+# V1
+# IDEA : brute force (TLE)
+# https://leetcode.com/problems/partition-equal-subset-sum/solution/
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        def dfs(nums: List[int], n: int, subset_sum: int) -> bool:
+            # Base cases
+            if subset_sum == 0:
+                return True
+            if n == 0 or subset_sum < 0:
+                return False
+            result = (dfs(nums, n - 1, subset_sum - nums[n - 1])
+                    or dfs(nums, n - 1, subset_sum))
+            return result
+
+        # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
+            return False
+
+        subset_sum = total_sum // 2
+        n = len(nums)
+        return dfs(nums, n - 1, subset_sum)
+
+# V1'
+# IDEA : Top Down Dynamic Programming - Memoization
+# https://leetcode.com/problems/partition-equal-subset-sum/solution/
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        @lru_cache(maxsize=None)
+        def dfs(nums: Tuple[int], n: int, subset_sum: int) -> bool:
+            # Base cases
+            if subset_sum == 0:
+                return True
+            if n == 0 or subset_sum < 0:
+                return False
+            result = (dfs(nums, n - 1, subset_sum - nums[n - 1])
+                    or dfs(nums, n - 1, subset_sum))
+            return result
+
+        # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
+            return False
+
+        subset_sum = total_sum // 2
+        n = len(nums)
+        return dfs(tuple(nums), n - 1, subset_sum)
+
+# V1''
+# IDEA : Bottom Up Dynamic Programming
+# https://leetcode.com/problems/partition-equal-subset-sum/solution/
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
+            return False
+        subset_sum = total_sum // 2
+        n = len(nums)
+
+        # construct a dp table of size (n+1) x (subset_sum + 1)
+        dp = [[False] * (subset_sum + 1) for _ in range(n + 1)]
+        dp[0][0] = True
+        for i in range(1, n + 1):
+            curr = nums[i - 1]
+            for j in range(subset_sum + 1):
+                if j < curr:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - curr]
+        return dp[n][subset_sum]
+
+# V1'''
 # https://blog.csdn.net/fuxuemingzhu/article/details/79787425
 # DFS 
 class Solution:
@@ -24,7 +131,7 @@ class Solution:
                 target[i] += nums[index]
         return False
 
-# V1'
+# V1'''''
 # https://blog.csdn.net/fuxuemingzhu/article/details/79787425
 # dp[j] = dp[j] || dp[j - nums[i]]
 class Solution(object):
@@ -44,7 +151,6 @@ class Solution(object):
 # V2 
 # Time:  O(n * s), s is the sum of nums
 # Space: O(s)
-
 class Solution(object):
     def canPartition(self, nums):
         """
