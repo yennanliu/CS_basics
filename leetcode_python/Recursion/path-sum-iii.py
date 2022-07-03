3254955a839eef478e4015dac489527dadd438f6
yennanliu <f339339@gmail.com>
2022-07-03 22:09:03 +0800

    update

--- leetcode_python/Recursion/path-sum-iii.py
+++ leetcode_python/Recursion/path-sum-iii.py
@@ -1,3 +1,34 @@
+"""
+
+437. Path Sum III
+Medium
+
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+ 
+
+Example 1:
+
+
+Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+Output: 3
+Explanation: The paths that sum to 8 are shown.
+Example 2:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 1000].
+-109 <= Node.val <= 109
+-1000 <= targetSum <= 1000
+
+"""
+
 # V0 
 # IDEA : BFS + DFS 
 # => USE BFS FIND EVERY NODE IN THE TREE, AND USE DFS GET THR PATH SUM ON EVERY NODE (FOUND BY BFS)
