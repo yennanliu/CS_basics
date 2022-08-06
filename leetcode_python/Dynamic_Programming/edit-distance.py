7f08939ad2e09d57e0edcc21e289d401ede07592
yennanliu <f339339@gmail.com>
2022-08-06 15:59:17 +0800

    add 072, update progress

---
+++ leetcode_python/Dynamic_Programming/edit-distance.py
@@ -0,0 +1,81 @@
+"""
+
+72. Edit Distance
+Hard
+
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+ 
+
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+Example 2:
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+ 
+
+Constraints:
+
+0 <= word1.length, word2.length <= 500
+
+"""
+
+# V0
+
+# V1
+# IDEA : DP
+# https://leetcode.com/problems/edit-distance/solution/
+class Solution:
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        n = len(word1)
+        m = len(word2)
+        
+        # if one of the strings is empty
+        if n * m == 0:
+            return n + m
+        
+        # array to store the convertion history
+        d = [ [0] * (m + 1) for _ in range(n + 1)]
+        
+        # init boundaries
+        for i in range(n + 1):
+            d[i][0] = i
+        for j in range(m + 1):
+            d[0][j] = j
+        
+        # DP compute 
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                left = d[i - 1][j] + 1
+                down = d[i][j - 1] + 1
+                left_down = d[i - 1][j - 1] 
+                if word1[i - 1] != word2[j - 1]:
+                    left_down += 1
+                d[i][j] = min(left, down, left_down)
+        
+        return d[n][m]
+
+# V2
