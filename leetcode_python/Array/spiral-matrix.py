e82dbdc3e1111f29f177bed0aa50a53dd1f6755b
yennanliu <f339339@gmail.com>
2022-10-23 18:24:09 +0800

    update 073, progress, cheatsheet

--- leetcode_python/Array/spiral-matrix.py
+++ leetcode_python/Array/spiral-matrix.py
@@ -1,15 +1,17 @@
 """
 
-Given an m x n matrix, 
-return all elements of the matrix in spiral order.
+54. Spiral Matrix
+Medium
 
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+ 
 
 Example 1:
 
 
 Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Output: [1,2,3,6,9,8,7,4,5]
-
 Example 2:
 
 
@@ -23,6 +25,7 @@ m == matrix.length
 n == matrix[i].length
 1 <= m, n <= 10
 -100 <= matrix[i][j] <= 100
+
 
 """
 
