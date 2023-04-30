c1dc3553388547ace541e7dc79ba0f39cced5c06
yennanliu <f339339@gmail.com>
2023-04-30 17:05:08 +0100

    add 246 java, update readme

---
+++ leetcode_java/src/main/java/LeetCodeJava/HashTable/GroupShiftedStrings.java
@@ -0,0 +1,14 @@
+package LeetCodeJava.HashTable;
+
+// https://leetcode.com/problems/group-shifted-strings/
+
+import java.util.List;
+
+public class GroupShiftedStrings {
+
+    public List<List<String>> groupStrings(String[] strings) {
+
+        return null;
+    }
+
+}
