aa3e2b22c4e2b371bb978f81a0312e0fcd870615
yennanliu <f339339@gmail.com>
2023-05-02 23:05:00 +0100

    add 66 java, update progress

---
+++ leetcode_java/src/main/java/LeetCodeJava/Array/PlusOne.java
@@ -0,0 +1,50 @@
+package LeetCodeJava.Array;
+
+// https://leetcode.com/problems/plus-one/
+
+public class PlusOne {
+
+    // V0
+
+    // V1
+    // https://leetcode.com/problems/plus-one/editorial/
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        // move along the input array starting from the end
+        for (int idx = n - 1; idx >= 0; --idx) {
+            // set all the nines at the end of array to zeros
+            if (digits[idx] == 9) {
+                digits[idx] = 0;
+            }
+            // here we have the rightmost not-nine
+            else {
+                // increase this rightmost not-nine by 1
+                digits[idx]++;
+                // and the job is done
+                return digits;
+            }
+        }
+        // we're here because all the digits are nines
+        digits = new int[n + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    // V2
+    // https://leetcode.com/problems/plus-one/solutions/2706861/java-fastest-0ms-runtime-easy-and-elegant-solution/
+    public int[] plusOne_2(int[] digits){
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+}
