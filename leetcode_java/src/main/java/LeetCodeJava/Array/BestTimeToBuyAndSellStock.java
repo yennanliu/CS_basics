0887c3afe583648131e0af7b409e54e4ff802b46
yennanliu <f339339@gmail.com>
2023-05-03 08:21:24 +0100

    rename file, update

---
+++ leetcode_java/src/main/java/LeetCodeJava/Array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,11 @@
+package LeetCodeJava.Array;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+
+        return 0;
+    }
+}
