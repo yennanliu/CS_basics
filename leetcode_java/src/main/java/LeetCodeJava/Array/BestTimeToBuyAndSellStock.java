0ce505a9fcfce4d99bee7d6638300d869e991cde
yennanliu <f339339@gmail.com>
2023-05-03 22:57:18 +0100

    add 121 java, update progress

--- leetcode_java/src/main/java/LeetCodeJava/Array/BestTimeToBuyAndSellStock.java
+++ leetcode_java/src/main/java/LeetCodeJava/Array/BestTimeToBuyAndSellStock.java
@@ -4,8 +4,43 @@ package LeetCodeJava.Array;
 
 public class BestTimeToBuyAndSellStock {
 
+    // V0
     public int maxProfit(int[] prices) {
 
-        return 0;
+        int minVal = (int) Math.pow(10, 4);
+        int maxVal = 0;
+        int maxProfit = 0;
+        //HashSet<Integer> seen = new HashSet<Integer>();
+        for (int i = 0; i < prices.length; i++){
+            int cur = prices[i];
+            //seen.add(cur);
+            if (cur < minVal){
+                minVal = cur;
+            }else{
+                maxVal = cur;
+                maxProfit = Math.max( maxVal - minVal, maxProfit);
+            }
+        }
+        return maxProfit > 0 ? maxProfit : 0;
     }
+
+    // V1
+    // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/1735493/java-c-best-ever-explanation-could-possible/
+    public int maxProfit_2(int[] prices) {
+        int lsf = Integer.MAX_VALUE;
+        int op = 0;
+        int pist = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+            pist = prices[i] - lsf;
+            if(op < pist){
+                op = pist;
+            }
+        }
+        return op;
+    }
+
 }
