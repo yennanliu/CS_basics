4da3a6864e8f5671d00fa82a52c401e5f6776992
yennanliu <f339339@gmail.com>
2023-05-08 22:09:03 +0100

    add 028 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/String/ZigzagConversion.java
@@ -0,0 +1,11 @@
+package LeetCodeJava.String;
+
+// https://leetcode.com/problems/zigzag-conversion/
+
+public class ZigzagConversion {
+
+    public String convert(String s, int numRows) {
+        return s;
+    }
+
+}
