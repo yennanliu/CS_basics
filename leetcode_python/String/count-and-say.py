cca0a5b29b595e31846ca5a274978005984315ce
yennanliu <f339339@gmail.com>
2023-05-08 23:03:46 +0100

    update 038 java, python, progress

--- leetcode_python/String/count-and-say.py
+++ leetcode_python/String/count-and-say.py
@@ -1,17 +1,43 @@
-# Time:  O(n * 2^n)
-# Space: O(2^n)
-#
-# The count-and-say sequence is the sequence of integers beginning as follows:
-# 1, 11, 21, 1211, 111221, ...
-#
-# 1 is read off as "one 1" or 11.
-# 11 is read off as "two 1s" or 21.
-# 21 is read off as "one 2, then one 1" or 1211.
-# Given an integer n, generate the nth sequence.
-#
-# Note: The sequence of integers will be represented as a string.
-#
+"""
 
+38. Count and Say
+
+Medium
+
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
+
+For example, the saying and conversion for digit string "3322251":
+
+
+Given a positive integer n, return the nth term of the count-and-say sequence.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: "1"
+Explanation: This is the base case.
+Example 2:
+
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = say "1" = one 1 = "11"
+countAndSay(3) = say "11" = two 1's = "21"
+countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ 
+
+Constraints:
+
+1 <= n <= 30
+
+"""
 """
 i.e. 
 
