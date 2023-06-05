ef581c6def7ed8c2ae49f29233593dd734f800ea
yennanliu <f339339@gmail.com>
2023-06-05 16:46:36 +0100

    add 203 java, update 203 python, update progress

---
+++ leetcode_java/src/main/java/LeetCodeJava/LinkedList/RemoveLinkedListElements.java
@@ -0,0 +1,48 @@
+package LeetCodeJava.LinkedList;
+
+// https://leetcode.com/problems/remove-linked-list-elements/
+
+public class RemoveLinkedListElements {
+
+    // V0
+    public ListNode removeElements(ListNode head, int val) {
+
+        if (head == null){
+            return null;
+        }
+
+        if (String.valueOf(val).equals(null)){
+            return head;
+        }
+
+        ListNode ans = new ListNode();
+        ListNode res = ans;
+
+        while(head != null){
+            if (head.val != val){
+                ans.next = new ListNode(head.val);
+                ans = ans.next;
+            }
+
+            head = head.next;
+        }
+
+        return res.next;
+    }
+
+    // V1
+    // https://leetcode.com/problems/remove-linked-list-elements/editorial/
+    public ListNode removeElements_2(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+
+        ListNode prev = sentinel, curr = head;
+        while (curr != null) {
+            if (curr.val == val) prev.next = curr.next;
+            else prev = curr;
+            curr = curr.next;
+        }
+        return sentinel.next;
+    }
+
+}
--- leetcode_python/Linked_list/remove-linked-list-elements.py
+++ leetcode_python/Linked_list/remove-linked-list-elements.py
@@ -1,3 +1,35 @@
+"""
+
+203. Remove Linked List Elements
+Easy
+
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+
+"""
+
 # V0
 class Solution:
     def removeElements(self, head, val):
