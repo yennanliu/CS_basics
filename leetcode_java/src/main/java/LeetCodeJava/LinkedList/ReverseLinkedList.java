5a0dd898afe5f577b9ca4ebf94a13c59b023b444
yennanliu <f339339@gmail.com>
2023-06-06 22:35:27 +0100

    update java 206

--- leetcode_java/src/main/java/LeetCodeJava/LinkedList/ReverseLinkedList.java
+++ leetcode_java/src/main/java/LeetCodeJava/LinkedList/ReverseLinkedList.java
@@ -27,5 +27,33 @@ public class ReverseLinkedList {
 
         }
 
+        // V1
+        // IDEA : iteration
+        // https://leetcode.com/problems/reverse-linked-list/editorial/
+        public ListNode reverseList_2(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode nextTemp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextTemp;
+            }
+            return prev;
+        }
+
+        // V2
+        // IDEA : Recursive
+        // https://leetcode.com/problems/reverse-linked-list/editorial/
+        public ListNode reverseList_3(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode p = reverseList_3(head.next);
+            head.next.next = head;
+            head.next = null;
+            return p;
+        }
+
     }
 }
