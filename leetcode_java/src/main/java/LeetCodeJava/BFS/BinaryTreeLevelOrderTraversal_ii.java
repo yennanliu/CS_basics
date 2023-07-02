999ab7f6e7b8adb63d5cc67d9bd3403ee73c7526
yennanliu <f339339@gmail.com>
2023-07-02 12:21:28 +0100

    add 107 java, update cheatsheet

--- leetcode_java/src/main/java/LeetCodeJava/BFS/BinaryTreeLevelOrderTraversal_ii.java
+++ leetcode_java/src/main/java/LeetCodeJava/BFS/BinaryTreeLevelOrderTraversal_ii.java
@@ -4,16 +4,16 @@ package LeetCodeJava.BFS;
 
 import LeetCodeJava.DataStructure.TreeNode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class BinaryTreeLevelOrderTraversal_ii {
 
+    // V0
+    // IDEA : BFS
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
 
         List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> resFinal = new ArrayList<>();
 
         if (root == null){
             return res;
@@ -25,14 +25,11 @@ public class BinaryTreeLevelOrderTraversal_ii {
 
         while (!queue.isEmpty()){
 
+            /** NOTE !!! we add new null array before for loop */
+            res.add(new ArrayList<>());
+            /** NOTE !!! we get queue size before for loop */
             int q_size = queue.size();
             for (int i = 0; i < q_size; i++){
-
-//                if (res.size() <= layer){
-//                    res.add(new ArrayList<>());
-//                }
-
-                res.add(new ArrayList<>());
 
                 TreeNode cur = queue.remove();
                 res.get(layer).add(cur.val);
@@ -50,7 +47,71 @@ public class BinaryTreeLevelOrderTraversal_ii {
 
         }
 
-        return res;
+        for (int j = res.size()-1; j >= 0; j--){
+            List<Integer> cur = res.get(j);
+            //resFinal.add(new ArrayList<>());
+            resFinal.add(cur);
+        }
+
+        return resFinal;
+    }
+
+    // V1
+    // IDEA : BFS
+    public List<List<Integer>> levelOrderBottom_2(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if (root == null) return levels;
+
+        ArrayDeque<TreeNode> nextLevel = new ArrayDeque() {{ offer(root); }};
+        ArrayDeque<TreeNode> currLevel = new ArrayDeque();
+
+        while (!nextLevel.isEmpty()) {
+            currLevel = nextLevel.clone();
+            nextLevel.clear();
+            levels.add(new ArrayList<Integer>());
+
+            for (TreeNode node : currLevel) {
+                // append the current node value
+                levels.get(levels.size() - 1).add(node.val);
+
+                // process child nodes for the next level
+                if (node.left != null)
+                    nextLevel.offer(node.left);
+                if (node.right != null)
+                    nextLevel.offer(node.right);
+            }
+        }
+
+        /** NOTE !!! we can reverse list via Collections.reverse */
+        Collections.reverse(levels);
+        return levels;
+    }
+
+    // V2
+    // IDEA : RECURSION
+    // https://leetcode.com/problems/binary-tree-level-order-traversal-ii/editorial/
+    List<List<Integer>> levels = new ArrayList<List<Integer>>();
+
+    public void helper(TreeNode node, int level) {
+        // start the current level
+        if (levels.size() == level)
+            levels.add(new ArrayList<Integer>());
+
+        // append the current node value
+        levels.get(level).add(node.val);
+
+        // process child nodes for the next level
+        if (node.left != null)
+            helper(node.left, level + 1);
+        if (node.right != null)
+            helper(node.right, level + 1);
+    }
+
+    public List<List<Integer>> levelOrderBottom_3(TreeNode root) {
+        if (root == null) return levels;
+        helper(root, 0);
+        Collections.reverse(levels);
+        return levels;
     }
 
 }
