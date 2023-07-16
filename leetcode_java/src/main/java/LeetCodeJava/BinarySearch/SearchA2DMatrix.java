12358ecb3dce68f4fdd94c0b0484f4c9164b376b
yennanliu <f339339@gmail.com>
2023-07-16 22:27:02 +0100

    update 074 java

--- leetcode_java/src/main/java/LeetCodeJava/BinarySearch/SearchA2DMatrix.java
+++ leetcode_java/src/main/java/LeetCodeJava/BinarySearch/SearchA2DMatrix.java
@@ -2,77 +2,40 @@ package LeetCodeJava.BinarySearch;
 
 // https://leetcode.com/problems/search-a-2d-matrix/
 
-import java.util.Arrays;
-
 public class SearchA2DMatrix {
 
     // V0
-    // IDEA : BINARY SEARCH
-//    public boolean searchMatrix(int[][] matrix, int target) {
-//
-//        if (matrix.length == 0 && matrix[0].length == 0){
-//            return false;
-//        }
-//
-//        if (matrix.length == 1 && matrix[0].length == 1){
-//            return matrix[0][0] == target;
-//        }
-//
-//        if (matrix.length == 1){
-//            //return Arrays.asList(matrix[0]).contains(target);
-//            for (int x : matrix[0]){
-//                if (x == target){
-//                    return true;
-//                }
-//            }
-//            return false;
-//        }
-//
-//        int length = matrix.length;
-//        int width = matrix[0].length;;
-//
-//        int l = 0;
-//        int r = matrix[0].length - 1;
-//        for (int i = 0; i < length - 1; i++){
-//
-//            int mid = (l + r) / 2;
-//            int cur = matrix[i][mid];
-//            // case 1 : match target
-//            if (cur == target){
-//                return true;
-//                // case 2 : target > biggest num in current row, move to next row
-//            }else if (target > matrix[i][width - 1]){
-//                // to next row
-//                continue;
-//            }else{
-//                if (binarySearch(matrix[i], target)){
-//                    return true;
-//                }
-//            }
-//        }
-//
-//        return false;
-//    }
-//
-//    private Boolean binarySearch(int[] input, int target){
-//
-//        int l = 0;
-//        int r = input.length -1;
-//
-//        while (r >= l){
-//            int mid = (l + r) / 2;
-//            int cur = input[mid];
-//            if (cur == target){
-//                return true;
-//            }
-//            else if (cur > target){
-//                r = mid - 1;
-//            }else{
-//                l = mid + 1;
-//            }
-//        }
-//        return false;
-//    }
+    // IDEA : BINARY SEARCH + FLATTEN MATRIX
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int length = matrix[0].length;
+        int width = matrix.length;
+
+        // flatten matrix + binary search
+        /** NOTE !!! we FLATTEN matrix as 1D array, and do binary search */
+        int l = 0;
+        int r = length * width - 1;
+
+        while (r >= l){
+            int mid = (l + r) / 2;
+            /** NOTE !!!
+             *   -> whether x or y axis
+             *   -> we do with length
+             *   -> e.g. y = mid / length
+             *   ->      x = mid % length
+             */
+            int cur = matrix[mid / length][mid % length];
+            if (cur == target){
+                return true;
+            }else if (cur < target){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+
+        return false;
+    }
 
 
     // V1
