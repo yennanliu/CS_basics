0e4a3581420e8a2c20cd5ca65761ae799be88490
yennanliu <f339339@gmail.com>
2023-07-16 22:27:02 +0100

    update 074 java

--- leetcode_java/src/main/java/dev/workSpace1.java
+++ leetcode_java/src/main/java/dev/workSpace1.java
@@ -3,10 +3,8 @@ package dev;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Stack;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
-public class workspace1 {
+public class workSpace1 {
 
     public int carFleet(int target, int[] position, int[] speed) {
 
@@ -60,4 +58,28 @@ public class workspace1 {
         Arrays.stream(x).forEach(System.out::println);
 
     }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int length = matrix[0].length;
+        int width = matrix.length;
+
+        // flatten matrix + binary search
+        int l = 0;
+        int r = length * width - 1;
+        while (r >= l){
+            int mid = (l + r) / 2;
+            int cur = matrix[mid / length][mid % length];
+            if (cur == target){
+                return true;
+            }else if (cur < target){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
 }
