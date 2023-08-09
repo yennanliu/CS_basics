f028c9fd268c0794b99c0359c3afe24405509174
yennanliu <f339339@gmail.com>
2023-08-09 09:21:01 +0100

    add 973 java

--- leetcode_java/src/main/java/dev/Test1.java
+++ leetcode_java/src/main/java/dev/Test1.java
@@ -14,4 +14,13 @@ public class Test1 {
         }
         return null;
     }
+
+    public static void main(String[] args) {
+        String[] my_array = new String[10];
+        my_array[1] = "wef";
+        my_array[2] = "xxxf";
+
+        System.out.println(String.valueOf(my_array[1]));
+    }
+
 }
