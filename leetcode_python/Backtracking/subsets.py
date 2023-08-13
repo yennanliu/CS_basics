23d0c424b68bb3a93e1127a41f971e5c3f35c4e4
yennanliu <f339339@gmail.com>
2023-08-13 11:20:31 +0100

    update 78 py, java, cheatsheet

--- leetcode_python/Backtracking/subsets.py
+++ leetcode_python/Backtracking/subsets.py
@@ -182,6 +182,30 @@ class Solution:
             self.dfs(nums, i + 1, combination, combinations)
             combination.pop()
 
+# V1
+# IDEA : BACKTRACK
+# https://www.youtube.com/watch?v=REOH22Xwdkk&t=4s
+# https://github.com/neetcode-gh/leetcode/blob/main/python/0078-subsets.py
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
+
 # V1'
 # https://leetcode.com/problems/subsets/solution/
 # IDEA : Cascading
