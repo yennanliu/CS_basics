64b172f239163bf84fff31338e0b4cc2460b7f04
yennanliu <f339339@gmail.com>
2023-08-20 18:07:09 +0100

    update

--- leetcode_java/src/main/java/LeetCodeJava/BackTrack/CombinationSum.java
+++ leetcode_java/src/main/java/LeetCodeJava/BackTrack/CombinationSum.java
@@ -97,6 +97,28 @@ public class CombinationSum {
 
     // V1
     // IDEA : BACKTRACK
+    // https://leetcode.com/problems/subsets/solutions/27281/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partitioning/
+    public List<List<Integer>> combinationSum_1(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        _backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void _backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for(int i = start; i < nums.length; i++){
+                tempList.add(nums[i]);
+                _backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    // V1
+    // IDEA : BACKTRACK
     // https://leetcode.com/problems/combination-sum/editorial/
     protected void backtrack(
             int remain,
