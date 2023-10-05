e851eebc83c0847dfa867124b9debf678ef9bf02
yennanliu <f339339@gmail.com>
2023-10-05 14:45:00 +0800

    update 130 java, add DataStructure/MyPair.java

---
+++ leetcode_java/src/main/java/LeetCodeJava/DataStructure/MyPair.java
@@ -0,0 +1,13 @@
+package LeetCodeJava.DataStructure;
+
+public class MyPair<U, V> {
+
+    public U first;
+    public V second;
+
+    MyPair(U first, V second){
+        this.first = first;
+        this.second = second;
+    }
+
+}
