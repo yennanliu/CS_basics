81d28657c758af11978dcec562ae73418ebcba2b
yennanliu <f339339@gmail.com>
2023-11-13 18:21:59 +0800

    update/add faq, add pic

---
+++ doc/faq/java_multi_thread.md
@@ -0,0 +1,7 @@
+# Java Multi-thread FAQ
+
+- https://javaguide.cn/java/concurrent/java-concurrent-questions-01.html
+- https://javaguide.cn/java/concurrent/java-concurrent-questions-02.html
+- https://javaguide.cn/java/concurrent/java-concurrent-questions-03.html
+
+### Java multi thread ?
--- leetcode_java/src/main/java/dev/workSpace2.java
+++ leetcode_java/src/main/java/dev/workSpace2.java
@@ -12,12 +12,22 @@ public class workSpace2 {
 //
 //        }
 
-        String x = "abcd";
-        System.out.println(x.substring(1,3));
-        StringBuilder sb = new StringBuilder(x.substring(1,3));
-        System.out.println(sb.reverse());
+//        String x = "abcd";
+//        System.out.println(x.substring(1,3));
+//        StringBuilder sb = new StringBuilder(x.substring(1,3));
+//        System.out.println(sb.reverse());
+//
+//        System.out.println(x.substring(x.length()-1, x.length()));
 
-        System.out.println(x.substring(x.length()-1, x.length()));
+
+        String a = new String("123");
+        String b = new String("123");
+
+        System.out.println(a==b); // false ( "new" will use new address in internal storage)
+
+        String c = b;
+
+        System.out.println(c==b); // true ( c will fetch from constant pool (常量池))
 
     }
 
