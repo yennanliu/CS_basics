677c2c14092bdcf20c76ab15d27c0b7b55a3c2e9
yennanliu <f339339@gmail.com>
2023-12-06 17:39:48 +0800

    update 003 java, progress

--- leetcode_java/src/main/java/dev/Test1.java
+++ leetcode_java/src/main/java/dev/Test1.java
@@ -3,10 +3,7 @@ package dev;
 import LeetCodeJava.DataStructure.ListNode;
 
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Test1 {
 
@@ -71,6 +68,172 @@ public class Test1 {
 
         int[] _array = {0,1,2,3};
         System.out.println(_array);
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+    // https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+    // idea : slow - fast pointers
+    // NOTE !!! Given the head of a linked list, remove the nth node from the end of the list and return its head.
+    // FROM THE END
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        if (head == null || head.next == null){
+            return null;
+        }
+
+        if (n < 0){
+            return null;
+        }
+
+//        ListNode head_ = head;
+//        int len = 0;
+//        while (head_ != null){
+//            len += 1;
+//            head_ = head_.next;
+//        }
+
+        ListNode dummy = new ListNode(0);
+
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        // move fast n+1 steps
+        //int j = len - n;
+        for (int i = 0; i < n; i++){
+            fast = fast.next;
+        }
+
+        // move fast, and slow on the same time, till fast reach the end of listNode
+        while (fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // return slow as result
+        slow = slow.next.next;
+        return dummy.next;
+
+        //return null;
+    }
+
+    /**
+     *  "abcabcbb"
+     *   lr
+     *
+     *   "abcabcbb"
+     *    l r
+     *
+     *    "abcabcbb"
+     *     l  r
+     *
+     *     "abcabcbb"
+     *       l r
+     *
+     *
+     *     "pwwkew"
+     *      lr
+     *
+     *      "pwwkew"
+     *       l r
+     *
+     *     "pwwkew"
+     *       lr
+     *
+     *     "pwwkew"
+     *        l
+     *        r
+     *
+     *    "pwwkew"
+     *        l
+     *        r
+     *
+     *
+     *  5:25
+     *
+     */
+
+    // https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    // IDEA : sliding window + hashset
+    public int lengthOfLongestSubstring(String s) {
+
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+
+        HashSet<String> set = new HashSet<>();
+        int l = 0;
+        int ans = 1;
+        int r = 0;
+        while (r < s.length() && l < s.length()){
+            System.out.println("r = " + r + " l = " + l + " set = " + set + " ans =  " + ans);
+            String cur = String.valueOf(s.charAt(r));
+            if (!set.contains(cur)){
+                set.add(cur);
+                //ans += 1;
+                ans = Math.max(ans, r - l + 1);
+                r += 1;
+                //continue;
+            }else{
+                while (set.contains(cur) && l < r){
+                    // keep moving left pointer, if sub array element still in set
+                    l += 1;
+                }
+                // finally remove cur from set
+                set.remove(cur);
+            }
+        }
+
+        return ans;
+    }
+
+    // V2
+    // IDEA : SLIDING WINDOW
+    // https://leetcode.com/problems/longest-substring-without-repeating-characters/editorial/
+    public int lengthOfLongestSubstring_3(String s) {
+
+        Map<String, Integer> map = new HashMap();
+
+        // left pointer
+        int left = 0;
+        // right pointer
+        int right = 0;
+        // final result
+        int res = 0;
+
+        while (right < s.length()) {
+
+            String cur = String.valueOf(s.charAt(right));
+
+            // NOTE !!! we add element to map first
+            if (map.containsKey(cur)){
+                map.put(cur, map.get(cur)+1);
+            }else{
+                map.put(cur, 1);
+            }
+
+            // NOTE !!! if map has element -> duplicated elelment
+            //          -> keep remove count from element and move left pointer to right
+            while (map.get(cur) > 1) {
+                String l = String.valueOf(s.charAt(left));
+                map.put(l, map.get(l) - 1);
+                left += 1;
+            }
+
+            res = Math.max(res, right - left + 1);
+
+            right += 1;
+        }
+        return res;
     }
 
 }
