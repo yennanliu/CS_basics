d6c91cfd1c4fb8cd17c560913839cdbb48111513
yennanliu <f339339@gmail.com>
2024-03-08 09:03:47 +0800

    update 153 java, progress

--- leetcode_java/src/main/java/LeetCodeJava/BinarySearch/FindMinimumInRotatedSortedArray.java
+++ leetcode_java/src/main/java/LeetCodeJava/BinarySearch/FindMinimumInRotatedSortedArray.java
@@ -9,15 +9,22 @@ public class FindMinimumInRotatedSortedArray {
 
     // V0
     // IDEA : BINARY SEARCH (CLOSED BOUNDARY)
-    // NOTE !!! the turing point (rotation point)
-    //          is ALWAYS the area that min element located (may at at i, i+1, i-1 place)
+    /**
+     *
+     * NOTE !!! the turing point (rotation point)
+     *          -> is ALWAYS the area that min element located (may at at i, i+1, i-1 place)
+     *
+     *
+     *    case 1) check turing point
+     *    case 2) check if left / right sub array is in Ascending order
+     */
     public int findMin(int[] nums) {
 
         if (nums.length == 0 || nums.equals(null)){
             return 0;
         }
 
-        // already in ascending order
+        // check if already in ascending order
         if (nums[0] < nums[nums.length - 1]){
             return nums[0];
         }
@@ -29,26 +36,32 @@ public class FindMinimumInRotatedSortedArray {
         // binary search
         int l = 0;
         int r = nums.length - 1;
+
         /** NOTE !!! here : r >= l (closed boundary) */
         while (r >= l){
             int mid = (l + r) / 2;
+
             /** NOTE !!! we found turing point, and it's the minimum element  (idx = mid + 1) */
             if (nums[mid] > nums[mid+1]){
                 return nums[mid+1];
+
            /** NOTE !!! we found turing point, and it's the minimum element (idx = mid -1) */
             }else if (nums[mid-1] > nums[mid]){
                 return nums[mid];
+
             /** NOTE !!! compare mid with left, and this means left sub array is ascending order,
              *           so minimum element MUST in right sub array
              */
             }else if (nums[mid] > nums[l]){
                 l = mid + 1;
+
             /** NOTE !!! compare mid with left, and this means right sub array is ascending order,
             *            so minimum element MUST in left sub array
             */
             }else{
                 r = mid - 1;
             }
+
         }
 
         return nums[l];
