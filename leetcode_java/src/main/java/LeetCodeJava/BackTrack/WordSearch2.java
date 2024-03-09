7ff21c05725fc74e8013900715ce2a91c0926414
yennanliu <f339339@gmail.com>
2024-03-09 16:55:22 +0800

    update 79 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/BackTrack/WordSearch2.java
@@ -0,0 +1,14 @@
+package LeetCodeJava.BackTrack;
+
+// https://leetcode.com/problems/word-search-ii/description/?envType=list&envId=xoqag3yj
+
+import java.util.List;
+
+public class WordSearch2 {
+
+    // V0
+    public List<String> findWords(char[][] board, String[] words) {
+        return null;
+    }
+
+}
