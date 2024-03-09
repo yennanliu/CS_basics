83a51115c250f0e6d74bbf6ce146321c3835d3ed
yennanliu <f339339@gmail.com>
2024-03-09 16:14:53 +0800

    update 211 java, progress

--- leetcode_java/src/main/java/LeetCodeJava/Trie/DesignAddAndSearchWordsDataStructure.java
+++ leetcode_java/src/main/java/LeetCodeJava/Trie/DesignAddAndSearchWordsDataStructure.java
@@ -13,93 +13,107 @@ public class DesignAddAndSearchWordsDataStructure {
 
     // V0
     // IDEA : TRIE
-    // TODO: fix search method (with "." case, use recursion)
-//    class myNode{
-//
-//        // attr
-//        HashMap<String, myNode> child;
-//        Boolean isEnd;
-//
-//        // constructor
-//        myNode(){
-//            HashMap<String, myNode> child = new HashMap<>();
-//            Boolean isEnd = false;
-//        }
-//
-//    }
-//
-//    class WordDictionary {
-//
-//        myNode node;
-//
-//        public WordDictionary() {
-//
-//            this.node = new myNode();
-//        }
-//
-//        public void addWord(String word) {
-//
-//            char[] wordArray = word.toCharArray();
-//            for (char x: wordArray){
-//                String cur = String.valueOf(x);
-//                this.node.child = new HashMap<>();
-//                this.node.child.put(cur, new myNode());
-//                this.node = this.node.child.get(cur);
-//            }
-//
-//            this.node.isEnd = true;
-//        }
-//
-//        public boolean search(String word) {
-//
-//            char[] wordArray = word.toCharArray();
-//            for (char x : wordArray){
-//
-//                String cur = String.valueOf(x);
-//                HashMap<String, myNode> child = this.node.child;
-//
-//                if (child == null){
-//                    return false;
-//                }
-//                else if (cur.equals(".")){
-//                    continue;
-//                }else{
-//                    if (this.node.child.containsKey(x)){
-//                        return false;
-//                    }
-//                    this.node = this.node.child.get(cur);
-//                }
-//            }
-//
-//            return true ? this.node.isEnd: false;
-//        }
-//
-//    }
-
-    // V1
-    // IDEA : TRIE
-    // https://leetcode.com/problems/design-add-and-search-words-data-structure/editorial/
     class TrieNode {
-        Map<Character, TrieNode> children = new HashMap();
-        boolean word = false;
+        Map<String, TrieNode> children = new HashMap<>();
+        boolean isEnd = false;
         public TrieNode() {}
     }
 
-    class WordDictionary2 {
+    class WordDictionary {
         TrieNode trie;
 
         /** Initialize your data structure here. */
-        public WordDictionary2() {
+        public WordDictionary() {
             trie = new TrieNode();
         }
 
         /** Adds a word into the data structure. */
         public void addWord(String word) {
+
             TrieNode node = trie;
 
             for (char ch : word.toCharArray()) {
+                String key = String.valueOf(ch);
+                if (!node.children.containsKey(key)) {
+                    node.children.put(key, new TrieNode());
+                }
+                node = node.children.get(key);
+            }
+            node.isEnd = true;
+        }
+
+        /** Returns if the word is in the node. */
+        public boolean searchInNode(String word, TrieNode node) {
+
+            for (int i = 0; i < word.length(); ++i) {
+                char ch = word.charAt(i);
+                String key = String.valueOf(ch);
+                /** CASE 1 : node has NO such child */
+                if (!node.children.containsKey(key)) {
+                    // if the current character is '.'
+                    // check all possible nodes at this level
+                    /** CASE 1-1 : element is "." */
+                    if (ch == '.') {
+                        for (String x : node.children.keySet()) {
+                            TrieNode child = node.children.get(x);
+                            /** NOTE !!!
+                             *  -> if ".", we HAVE to go through all nodes in next levels
+                             *  -> and check if any of them is valid
+                             *  -> so we need to RECURSIVELY call searchInNode method with "i+1" sub string
+                             */
+
+                            /** if can find remain elements in word */
+                            if (searchInNode(word.substring(i + 1), child)) {
+                                return true;
+                            }
+                        }
+                    }
+                    /** if can NOT find remain elements in word */
+                    // if no nodes lead to answer
+                    // or the current character != '.'
+                    return false;
+                /** CASE 2 : node has such child */
+                } else {
+                    // if the character is found
+                    // go down to the next level in trie
+                    node = node.children.get(key);
+                }
+            }
+            // check if is "end", so to-search word is 100% match word Trie
+            return node.isEnd;
+        }
+
+        /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+        public boolean search(String word) {
+            return searchInNode(word, trie);
+        }
+
+    }
+
+    // V1
+    // IDEA : TRIE
+    // https://leetcode.com/problems/design-add-and-search-words-data-structure/editorial/
+    class TrieNode2 {
+        Map<Character, TrieNode2> children = new HashMap();
+        boolean word = false;
+        public TrieNode2() {}
+    }
+
+    class WordDictionary2 {
+        TrieNode2 trie;
+
+        /** Initialize your data structure here. */
+        public WordDictionary2() {
+            trie = new TrieNode2();
+        }
+
+        /** Adds a word into the data structure. */
+        public void addWord(String word) {
+            TrieNode2 node = trie;
+
+            for (char ch : word.toCharArray()) {
                 if (!node.children.containsKey(ch)) {
-                    node.children.put(ch, new TrieNode());
+                    node.children.put(ch, new TrieNode2());
                 }
                 node = node.children.get(ch);
             }
@@ -107,7 +121,7 @@ public class DesignAddAndSearchWordsDataStructure {
         }
 
         /** Returns if the word is in the node. */
-        public boolean searchInNode(String word, TrieNode node) {
+        public boolean searchInNode(String word, TrieNode2 node) {
             for (int i = 0; i < word.length(); ++i) {
                 char ch = word.charAt(i);
                 if (!node.children.containsKey(ch)) {
@@ -115,7 +129,7 @@ public class DesignAddAndSearchWordsDataStructure {
                     // check all possible nodes at this level
                     if (ch == '.') {
                         for (char x : node.children.keySet()) {
-                            TrieNode child = node.children.get(x);
+                            TrieNode2 child = node.children.get(x);
                             /** NOTE !!!
                              *  -> if ".", we HAVE to go through all nodes in next levels
                              *  -> and check if any of them is valid
