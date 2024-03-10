e9f1ad7e7d497beae539de562930c76618d923df
yennanliu <f339339@gmail.com>
2024-03-10 19:26:37 +0800

    update 207 java

--- leetcode_java/src/main/java/LeetCodeJava/BFS/CourseSchedule.java
+++ leetcode_java/src/main/java/LeetCodeJava/BFS/CourseSchedule.java
@@ -70,18 +70,32 @@ public class CourseSchedule {
 
     // V0'
     // IDEA : BFS
+    // NOTE !!! we have 3 loop : numCourses, prerequisites, numCourses
     public boolean canFinish_(int numCourses, int[][] prerequisites) {
+
+        // save course - prerequisites info
         Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        // save "degree" for every course
         int[] indegree = new int[numCourses];
+        // for BFS
         Queue<Integer> queue = new LinkedList<Integer>();
+        // check how many courses are NOT yet visited
         int count = numCourses;
 
-        /** NOTE !!! numCourses */
+        /** NOTE !!! numCourses
+         *
+         *   -> construct map (course - prerequisites info)
+         */
         for (int i = 0; i < numCourses; i++) {
             map.put(i, new ArrayList<Integer>());
         }
 
-        /** NOTE !!! prerequisites */
+        /** NOTE !!! prerequisites
+         *
+         *   -> 1) inset info to map (course - prerequisites info)
+         *   -> 2) insert degree for every course
+         *
+         */
         for (int i = 0; i < prerequisites.length; i++) {
             map.get(prerequisites[i][0]).add(prerequisites[i][1]);
 
@@ -92,7 +106,11 @@ public class CourseSchedule {
             indegree[prerequisites[i][1]] += 1;
         }
 
-        /** NOTE !!! numCourses */
+        /** NOTE !!! numCourses
+         *
+         *  -> via this loop, we insert elements for "first" visiting
+         *     (degree == 0)
+         */
         for (int i = 0; i < numCourses; i++) {
             // only insert to queue if "indegree == 0" (course has NO prerequisite)
             if (indegree[i] == 0) {
@@ -111,9 +129,13 @@ public class CourseSchedule {
 //                }
 
                 // V2
+                // NOTE !!! if "degree" == 1, then we insert it to queue,
+                //          for next visiting
                 if (indegree[i] == 1) {
                     queue.offer(i);
                 } else {
+                    // NOTE !!! if if "degree" != 1,
+                    // minus its degree (indegree[i] -= 1), since current degree (other element) is visited
                     indegree[i] -= 1;
                 }
 
