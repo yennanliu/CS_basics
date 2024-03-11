8b125b4974f69534ee94a4dc13c81b6a26e6a254
yennanliu <f339339@gmail.com>
2024-03-11 23:03:05 +0800

    update 271 java, py, progress

--- leetcode_python/String/encode-and-decode-strings.py
+++ leetcode_python/String/encode-and-decode-strings.py
@@ -1,7 +1,6 @@
 """
 
-Design an algorithm to encode a list of strings to a string.
-The encoded string is then sent over the network and is decoded back to the original list of strings.
+Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
 
 Machine 1 (sender) has the function:
 
@@ -9,23 +8,53 @@ string encode(vector<string> strs) {
   // ... your code
   return encoded_string;
 }
-
 Machine 2 (receiver) has the function:
+
 vector<string> decode(string s) {
   //... your code
   return strs;
 }
-
 So Machine 1 does:
 
 string encoded_string = encode(strs);
-
 and Machine 2 does:
 
 vector<string> strs2 = decode(encoded_string);
 strs2 in Machine 2 should be the same as strs in Machine 1.
 
 Implement the encode and decode methods.
+
+You are not allowed to solve the problem using any serialize methods (such as eval).
+
+ 
+
+Example 1:
+
+Input: dummy_input = ["Hello","World"]
+Output: ["Hello","World"]
+Explanation:
+Machine 1:
+Codec encoder = new Codec();
+String msg = encoder.encode(strs);
+Machine 1 ---msg---> Machine 2
+
+Machine 2:
+Codec decoder = new Codec();
+String[] strs = decoder.decode(msg);
+Example 2:
+
+Input: dummy_input = [""]
+Output: [""]
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] contains any possible characters out of 256 valid ASCII characters.
+ 
+
+Follow up: Could you write a generalized algorithm to work on any possible set of characters?
 
 Note:
 The string may contain any possible characters out of 256 valid ascii characters. 
@@ -34,6 +63,9 @@ Do not use class member/global/static variables to store states.
 Your encode and decode algorithms should be stateless.
 Do not rely on any library method such as eval or serialize methods. 
 You should implement your own encode/decode algorithm.
+
+
+https://leetcode.ca/2016-08-27-271-Encode-and-Decode-Strings/
 
 """
 
