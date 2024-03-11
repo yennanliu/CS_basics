4eacb0935156ef6a1b3eac9fc1ee0ff2748cc201
yennanliu <f339339@gmail.com>
2024-03-11 23:59:56 +0800

    add 295 java, update progress

---
+++ leetcode_java/src/main/java/LeetCodeJava/Heap/FindMedianFromDataStream.java
@@ -0,0 +1,25 @@
+package LeetCodeJava.Heap;
+
+// https://leetcode.com/problems/find-median-from-data-stream/?envType=list&envId=xoqag3yj
+
+public class FindMedianFromDataStream {
+
+    // V0
+    // TODO : implement
+    class MedianFinder {
+
+        public MedianFinder() {
+
+        }
+
+        public void addNum(int num) {
+
+        }
+
+        public double findMedian() {
+            return 0.0;
+        }
+    }
+
+
+}
