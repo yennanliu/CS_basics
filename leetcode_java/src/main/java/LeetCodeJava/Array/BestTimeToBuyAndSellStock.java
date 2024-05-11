883ef1ca5ec36cf0511c9e6c731d4c2cb392e3fd
yennanliu <f339339@gmail.com>
2024-05-11 17:08:48 +0800

    update 121 java, progress

--- leetcode_java/src/main/java/LeetCodeJava/Array/BestTimeToBuyAndSellStock.java
+++ leetcode_java/src/main/java/LeetCodeJava/Array/BestTimeToBuyAndSellStock.java
@@ -7,6 +7,44 @@ public class BestTimeToBuyAndSellStock {
     // V0
     public int maxProfit(int[] prices) {
 
+        if (prices.length == 0){
+            return 0;
+        }
+
+        int res = 0;
+        int min = -1;
+        int max = -1;
+
+        for (int i : prices){
+            int cur = i;
+            //System.out.println("cur = " + cur);
+            if (min == -1){
+                min = cur;
+                continue;
+            }
+            if (min > cur){
+                min = cur;
+                continue;
+            }
+            if (max == -1){
+                max = cur;
+            }
+            if (cur > max){
+                max = cur;
+            }
+            int tmp = max - min;
+            //System.out.println("max = " + max + " min = " + min + " tmp = " + tmp);
+            /** NOTE : need to reset max val after get "revenue", so we don't reuse previous max val */
+            max = -1;
+            res = Math.max(tmp, res);
+        }
+
+        return res;
+    }
+
+    // V0'
+    public int maxProfit_0(int[] prices) {
+
         int minVal = (int) Math.pow(10, 4);
         int maxVal = 0;
         int maxProfit = 0;
