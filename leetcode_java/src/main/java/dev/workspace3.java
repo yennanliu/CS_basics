0cae20e230c612020724ea12236ddf88f870b5fe
yennanliu <f339339@gmail.com>
2024-05-19 17:50:38 +0800

    update 198 java, progress

--- leetcode_java/src/main/java/dev/workspace3.java
+++ leetcode_java/src/main/java/dev/workspace3.java
@@ -973,4 +973,53 @@ public class workspace3 {
         return prev;
     }
 
+    // LC 198
+    // dp
+    public int rob_(int[] nums) {
+
+        if (nums.length <= 3){
+
+            if (nums.length == 0){
+                return 0;
+            }
+
+            if (nums.length == 1){
+                return nums[0];
+            }
+
+            if (nums.length == 2){
+                return Math.max(nums[0], nums[1]);
+            }
+
+            return Math.max(nums[0]+nums[2], nums[1]);
+        }
+
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        // 1, 2, 3, ... k-2, k-1, k
+        // dp[k] = max(dp(k-2) + k, dp(k-1))
+        /**
+         *  2,1,1,2
+         *
+         *  -> dp[0] = 2
+         *  -> dp[1] = 2
+         *
+         *  -> dp[2] = max(dp[0]+nums[2], dp[1]) = 3
+         *  -> dp[3] = max(dp[1]+nums[3], dp[2]) = 4
+         *
+         */
+        for (int i = 2; i < nums.length; i++){
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+        }
+
+//        System.out.println("dp = " + dp.toString());
+//        for (int x : dp){
+//            System.out.println(x);
+//        }
+        return dp[nums.length-1];
+    }
+
 }
