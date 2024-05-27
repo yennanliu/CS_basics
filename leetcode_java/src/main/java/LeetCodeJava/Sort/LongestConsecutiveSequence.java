ed91573fe440fb2b9359bb9d8df1fdd323f8f478
yennanliu <f339339@gmail.com>
2024-05-28 07:59:19 +0800

    update 128 java

--- leetcode_java/src/main/java/LeetCodeJava/Sort/LongestConsecutiveSequence.java
+++ leetcode_java/src/main/java/LeetCodeJava/Sort/LongestConsecutiveSequence.java
@@ -7,11 +7,81 @@ import java.util.*;
 public class LongestConsecutiveSequence {
 
     // V0
+    // IDEA : SORT + SLIDING WINDOW (gpt)
+    // https://github.com/yennanliu/CS_basics/blob/master/leetcode_python/Sort/longest-consecutive-sequence.py#L32
+    public int longestConsecutive(int[] nums) {
+        // Edge case
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        // Remove duplicates and sort the array
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        nums = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            nums[index++] = num;
+        }
+        Arrays.sort(nums);
+
+        int res = 0;
+        int l = 0;
+        int r = 1;
+
+        /*
+         * Sliding window here :
+         * condition : l, r are still in the array (r < nums.length and l < nums.length)
+         *
+         * 2 cases
+         *
+         * case 1) nums[r] != nums[r - 1] + 1
+         *
+         *      -> means not continuous,
+         *      -> so we need to move r to right (1 index)
+         *      -> and MOVE l to r - 1, since it's NOT possible to have any continuous
+         *      subarray within [l, r] anymore
+         *
+         * case 2) nums[r] == nums[r - 1] + 1
+         *
+         *      -> means there is a continuous subarray currently, so we keep moving r to
+         *      right (r += 1) and get the current max
+         *      subarray length (res = max(res, r - l + 1))
+         */
+        /**
+         * NOTE !!!
+         *
+         *  keep comparing nums[r], nums[r-1]
+         *
+         *  "l idx" is just as left boundary that for sub array length calculation
+         *
+         */
+        while (r < nums.length && l < nums.length) {
+            // case 1)  nums[r] != nums[r - 1] + 1
+            if (nums[r] != nums[r - 1] + 1) {
+                r += 1;
+                l = (r - 1);
+            }
+            // case 2) nums[r] == nums[r - 1] + 1
+            else {
+                res = Math.max(res, r - l + 1);
+                r += 1;
+            }
+        }
+
+        // Edge case: if res == 0, means no continuous array (with length > 1), so we return 1
+        // (a single element can be recognized as a "continuous array", and its length = 1)
+        return res > 1 ? res : 1;
+    }
+
+    // V0'
     // IDEA : SET + SORT + for loop
     // step 1) set : only collect unique element
     // step 2) sort, so element is in ascending ordering
     // step 3) loop over final list, and maintain a max len of sequence
-    public int longestConsecutive(int[] nums) {
+    public int longestConsecutive_0(int[] nums) {
 
         if (nums.length == 0 || nums.equals(null)){
             return 0;
@@ -50,14 +120,14 @@ public class LongestConsecutiveSequence {
         return ans+1;
     }
 
-    // V0'
+    // V0''
     // IDEA : SET + SORT + SHIFT ARRAY
     // Step 1) we get unique array as new one, since we get possible longest array from it
     // Step 2) we sort unique array from above
     // Step 3) we shift to with 1 index from above unique array as shift array
     // Step 4) we loop over unique array, shift array and calculate max length when diff = 1
     // Step 5) return amx length from above
-    public int longestConsecutive_1(int[] nums) {
+    public int longestConsecutive_0_1(int[] nums) {
 
         if (nums.length == 0 || nums.equals(null)){
             return 0;
