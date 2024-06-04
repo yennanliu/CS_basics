dd939c4ff67cf0665e8ae4c46032e3c5e232c977
yennanliu <f339339@gmail.com>
2024-06-04 18:26:25 +0800

    update 567 java, progress

--- leetcode_java/src/main/java/LeetCodeJava/TwoPointer/PermutationInString.java
+++ leetcode_java/src/main/java/LeetCodeJava/TwoPointer/PermutationInString.java
@@ -222,4 +222,26 @@ public class PermutationInString {
         return count == 26;
     }
 
+
+    // V5
+    // https://github.com/neetcode-gh/leetcode/blob/main/java/0567-permutation-in-string.java
+    public boolean checkInclusion_8(String s1, String s2) {
+        int n = s1.length();
+        int[] freq = new int[26];
+        int m = s2.length();
+        for (int i = 0; i < n; i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        int[] freq2 = new int[26];
+        for (int i = 0; i < m; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            if (i >= n) {
+                freq2[s2.charAt(i - n) - 'a']--;
+            }
+            if (Arrays.equals(freq, freq2))
+                return true;
+        }
+        return false;
+    }
+
 }
