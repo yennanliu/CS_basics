2e95755a69fb053c161d0dbf00cea58b4b1b5396
yennanliu <f339339@gmail.com>
2024-09-08 17:07:01 +0800

    update 1066 java

--- leetcode_java/src/main/java/LeetCodeJava/Sort/CampusBike2.java
+++ leetcode_java/src/main/java/LeetCodeJava/Sort/CampusBike2.java
@@ -4,6 +4,7 @@ package LeetCodeJava.Sort;
 // https://leetcode.ca/2018-10-31-1066-Campus-Bikes-II/#google_vignette
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  *  1066. Campus Bikes II
@@ -45,8 +46,68 @@ import java.util.Arrays;
 public class CampusBike2 {
 
     // V0
-    // TODO : implement below
-    //public int assignBikes(int[][] workers, int[][] bikes) {}
+    // IDEA : custom sorting (Comparator), already-visited check
+    // TODO : validate below
+//    public int assignBikes(int[][] workers, int[][] bikes){
+//
+//        int n = workers.length;
+//        int m = bikes.length;
+//        int k = 0;
+//        // get all possible Manhattan distances
+//        int[][] cache = new int[n * m][3];
+//        for (int i = 0; i < n; i++){
+//            for (int j = 0; j < m; j++){
+//                int dist
+//                        = Math.abs(workers[i][0] - bikes[j][0]) + Math.abs(workers[i][1] - bikes[j][1]);
+//                // NOTE !!! here we append 3 var
+//                //cache.add(dist, i, j);
+//                cache[k] = new int[]{dist, i, j};
+//                k += 1;
+//            }
+//        }
+//
+//        // sorting
+//        Arrays.stream(cache).sorted(new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                // dist
+//                if (o1[0] != o2[0]){
+//                    return o1[0] - o2[0];
+//                }
+//                // compare i (worker index)
+//                if (o1[1] != o2[1]){
+//                    return o1[1] - o2[1];
+//                }
+//                // compare j (bike index)
+////                if (o1[2] != o2[2]){
+////                    return o1[2] - o2[2];
+////                }
+//                // TODO : check
+//                // return 0;
+//                return o1[2] - o2[2];
+//            }
+//        });
+//
+//        int res = 0;
+//
+//        // prepare final result
+//        Boolean[] visited_1 = new Boolean[n];
+//        Boolean[] visited_2 = new Boolean[m];
+//        for (int[] c : cache){
+//            //Object y = x;
+//            int dist = c[0];
+//            int i = c[1];
+//            int j = c[2];
+//            if (!visited_1[i] && !visited_2[j]){
+//                res += dist;
+//                visited_1[i] = true;
+//                visited_2[j] = true;
+//            }
+//        }
+//
+//        return res;
+//    }
+
 
     // V1_1
     // https://leetcode.ca/2018-10-31-1066-Campus-Bikes-II/#google_vignette
@@ -73,5 +134,32 @@ public class CampusBike2 {
     }
 
     // V2
+    // https://github.com/doocs/leetcode/blob/main/solution/1000-1099/1066.Campus%20Bikes%20II/README.md
+    // IDEA : ROUTE COMPRESSION + DP
+    public int assignBikes_2(int[][] workers, int[][] bikes) {
+        int n = workers.length;
+        int m = bikes.length;
+        int[][] f = new int[n + 1][1 << m];
+        for (int[] g : f) {
+            Arrays.fill(g, 1 << 30);
+        }
+        f[0][0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < 1 << m; ++j) {
+                for (int k = 0; k < m; ++k) {
+                    if ((j >> k & 1) == 1) {
+                        int d = Math.abs(workers[i - 1][0] - bikes[k][0])
+                                + Math.abs(workers[i - 1][1] - bikes[k][1]);
+                        f[i][j] = Math.min(f[i][j], f[i - 1][j ^ (1 << k)] + d);
+                    }
+                }
+            }
+        }
+        return Arrays.stream(f[n]).min().getAsInt();
+    }
+
+
+    // V3
+    // https://blog.csdn.net/qq_46105170/article/details/119177348
 
 }
