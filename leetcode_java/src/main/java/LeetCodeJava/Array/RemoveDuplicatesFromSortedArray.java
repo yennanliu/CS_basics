396a4aefc240b66e7f1235c17a810fe34faaa99e
yennanliu <f339339@gmail.com>
2024-11-02 19:37:23 +0800

    update 26 java

--- leetcode_java/src/main/java/LeetCodeJava/Array/RemoveDuplicatesFromSortedArray.java
+++ leetcode_java/src/main/java/LeetCodeJava/Array/RemoveDuplicatesFromSortedArray.java
@@ -1,16 +1,78 @@
 package LeetCodeJava.Array;
 
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
-
+/**
+ * 26. Remove Duplicates from Sorted Array
+ * Solved
+ * Easy
+ * Topics
+ * Companies
+ * Hint
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+ *
+ * Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+ *
+ * Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+ * Return k.
+ * Custom Judge:
+ *
+ * The judge will test your solution with the following code:
+ *
+ * int[] nums = [...]; // Input array
+ * int[] expectedNums = [...]; // The expected answer with correct length
+ *
+ * int k = removeDuplicates(nums); // Calls your implementation
+ *
+ * assert k == expectedNums.length;
+ * for (int i = 0; i < k; i++) {
+ *     assert nums[i] == expectedNums[i];
+ * }
+ * If all assertions pass, then your solution will be accepted.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2,_]
+ * Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ * Example 2:
+ *
+ * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+ * Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -100 <= nums[i] <= 100
+ * nums is sorted in non-decreasing order.
+ *
+ *
+ */
 import java.util.LinkedHashSet;
 
 public class RemoveDuplicatesFromSortedArray {
 
     // V0
+    // IDEA : 2 POINTERS
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
 
     // V1
     // https://leetcode.com/problems/remove-duplicates-from-sorted-array/solutions/3416595/c-java-python-javascript-fully-explained-two-pointer/
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicates_1(int[] nums) {
         int i = 0;
         for (int j = 1; j < nums.length; j++) {
             if (nums[i] != nums[j]) {
@@ -58,6 +120,5 @@ public class RemoveDuplicatesFromSortedArray {
         }
         return set.size();
     }
-
 
 }
