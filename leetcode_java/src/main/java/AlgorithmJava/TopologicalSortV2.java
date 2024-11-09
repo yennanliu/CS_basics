b3f32208b3f166e1eb49caffd060a7a50e21a57e
yennanliu <f339339@gmail.com>
2024-11-09 19:11:19 +0800

    update TopologicalSortV2.java, backup code

--- leetcode_java/src/main/java/AlgorithmJava/TopologicalSortV2.java
+++ leetcode_java/src/main/java/AlgorithmJava/TopologicalSortV2.java
@@ -25,6 +25,11 @@ public class TopologicalSortV2 {
         // Step 2: Initialize a queue with nodes that have in-degree 0
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numNodes; i++) {
+            /**
+             * NOTE !!!
+             *
+             *  we add ALL nodes with degree = 0 to queue at init step
+             */
             if (inDegree[i] == 0) {
                 queue.offer(i);
             }
@@ -34,11 +39,25 @@ public class TopologicalSortV2 {
 
         // Step 3: Process the nodes in topological order
         while (!queue.isEmpty()) {
+            /**
+             * NOTE !!!
+             *
+             *  ONLY "degree = 0"  nodes CAN be added to queue
+             *
+             *  -> so we can add whatever node from queue to final result (topologicalOrder)
+             */
             int current = queue.poll();
             topologicalOrder.add(current);
 
             for (int neighbor : graph.get(current)) {
-                inDegree[neighbor]--;
+                inDegree[neighbor] -= 1;
+                /**
+                 * NOTE !!!
+                 *
+                 *  if a node "degree = 0"  means this node can be ACCESSED now,
+                 *
+                 *  -> so we need to add it to the queue (for adding to topologicalOrder in the following while loop iteration)
+                 */
                 if (inDegree[neighbor] == 0) {
                     queue.offer(neighbor);
                 }
