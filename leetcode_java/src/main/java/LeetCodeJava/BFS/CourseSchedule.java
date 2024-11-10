1fc2f1ba74e92978fb270707965cd232efce808b
yennanliu <f339339@gmail.com>
2024-11-10 22:10:57 +0800

    update 207 java, progress

--- leetcode_java/src/main/java/LeetCodeJava/BFS/CourseSchedule.java
+++ leetcode_java/src/main/java/LeetCodeJava/BFS/CourseSchedule.java
@@ -6,13 +6,96 @@ import java.util.*;
 
 public class CourseSchedule {
 
-    // V0
+    // IDEA : TOPOLOGICAL SORT
+    // LC 210
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (prerequisites.length==0){
+            return true;
+        }
+
+        if (TopologicalSort(numCourses, prerequisites) == null){
+            return false;
+        }
+
+        return true;
+    }
+
+    public List<Integer> TopologicalSort(int numNodes, int[][] edges) {
+        // Step 1: Build the graph and calculate in-degrees
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        int[] inDegree = new int[numNodes];
+
+        for (int i = 0; i < numNodes; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int from = edge[0];
+            int to = edge[1];
+            graph.get(from).add(to);
+            inDegree[to]++;
+        }
+
+        // Step 2: Initialize a queue with nodes that have in-degree 0
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numNodes; i++) {
+            /**
+             * NOTE !!!
+             *
+             *  we add ALL nodes with degree = 0 to queue at init step
+             */
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        List<Integer> topologicalOrder = new ArrayList<>();
+
+        // Step 3: Process the nodes in topological order
+        while (!queue.isEmpty()) {
+            /**
+             * NOTE !!!
+             *
+             *  ONLY "degree = 0"  nodes CAN be added to queue
+             *
+             *  -> so we can add whatever node from queue to final result (topologicalOrder)
+             */
+            int current = queue.poll();
+            topologicalOrder.add(current);
+
+            for (int neighbor : graph.get(current)) {
+                inDegree[neighbor] -= 1;
+                /**
+                 * NOTE !!!
+                 *
+                 *  if a node "degree = 0"  means this node can be ACCESSED now,
+                 *
+                 *  -> so we need to add it to the queue (for adding to topologicalOrder in the following while loop iteration)
+                 */
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        // If topologicalOrder does not contain all nodes, there was a cycle in the graph
+        if (topologicalOrder.size() != numNodes) {
+            //throw new IllegalArgumentException("The graph has a cycle, so topological sort is not possible.");
+            return null;
+        }
+
+        /** NOTE !!! reverse ordering */
+        Collections.reverse(topologicalOrder);
+        return topologicalOrder;
+    }
+
+    // V0'
     // IDEA : DFS (fix by gpt)
     // NOTE !!! instead of maintain status (0,1,2), below video offers a simpler approach
     //      -> e.g. use a set, recording the current visiting course, if ANY duplicated (already in set) course being met,
     //      -> means "cyclic", so return false directly
     // https://www.youtube.com/watch?v=EgI5nU9etnU
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean canFinish_0_0(int numCourses, int[][] prerequisites) {
         // Initialize adjacency list for storing prerequisites
         /**
          *  NOTE !!!
