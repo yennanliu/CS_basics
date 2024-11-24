874d556ad6ede81c8dcd297c9bac6124486a23f9
yennanliu <f339339@gmail.com>
2024-11-24 18:05:28 +0800

    update 153 java, progress

--- leetcode_java/src/main/java/LeetCodeJava/BinarySearch/FindMinimumInRotatedSortedArray.java
+++ leetcode_java/src/main/java/LeetCodeJava/BinarySearch/FindMinimumInRotatedSortedArray.java
@@ -1,13 +1,60 @@
 package LeetCodeJava.BinarySearch;
 
+import java.util.Arrays;
+
+
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
-import java.util.Arrays;
+/**
+ * 153. Find Minimum in Rotated Sorted Array
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+ *
+ * [4,5,6,7,0,1,2] if it was rotated 4 times.
+ * [0,1,2,4,5,6,7] if it was rotated 7 times.
+ * Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+ *
+ * Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+ *
+ * You must write an algorithm that runs in O(log n) time.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [3,4,5,1,2]
+ * Output: 1
+ * Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+ * Example 2:
+ *
+ * Input: nums = [4,5,6,7,0,1,2]
+ * Output: 0
+ * Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+ * Example 3:
+ *
+ * Input: nums = [11,13,15,17]
+ * Output: 11
+ * Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+ *
+ *
+ * Constraints:
+ *
+ * n == nums.length
+ * 1 <= n <= 5000
+ * -5000 <= nums[i] <= 5000
+ * All the integers of nums are unique.
+ * nums is sorted and rotated between 1 and n times.
+ *
+ *
+ */
 
 public class FindMinimumInRotatedSortedArray {
 
-
-    // V0
+    // V0'
     // IDEA : BINARY SEARCH (CLOSED BOUNDARY)
     /**
      *
@@ -17,7 +64,7 @@ public class FindMinimumInRotatedSortedArray {
      *    case 1) check turing point
      *    case 2) check if left / right sub array is in Ascending order
      */
-    public int findMin(int[] nums) {
+    public int findMin_0_1(int[] nums) {
 
         if (nums.length == 0 || nums.equals(null)){
             return 0;
@@ -75,7 +122,7 @@ public class FindMinimumInRotatedSortedArray {
         return nums[l];
     }
 
-    // V0'
+    // V0''
     // IDEA : BINARY SEARCH (OPEN BOUNDARY)
     public int findMin_2(int[] nums) {
 
