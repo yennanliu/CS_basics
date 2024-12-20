b9799db60e0eb6e813d51f4a3b2833741c429781
yennanliu <f339339@gmail.com>
2024-12-20 15:51:38 +0800

    add 1109 java

--- leetcode_java/src/main/java/LeetCodeJava/Array/CorporateFlightBookings.java
+++ leetcode_java/src/main/java/LeetCodeJava/Array/CorporateFlightBookings.java
@@ -1,6 +1,10 @@
 package LeetCodeJava.Array;
 
 // https://leetcode.com/problems/corporate-flight-bookings/description/
+// https://leetcode.cn/problems/corporate-flight-bookings/description/
+
+import java.util.Arrays;
+
 /**
  * 1109. Corporate Flight Bookings
  * Solved
@@ -46,15 +50,126 @@ package LeetCodeJava.Array;
  * bookings[i].length == 3
  * 1 <= firsti <= lasti <= n
  * 1 <= seatsi <= 104
+ *
+ *
+ * Example :
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：bookings = [[1,2,10],[2,3,20],[2,5,25]], n = 5
+ * 输出：[10,55,45,25,25]
+ * 解释：
+ * 航班编号        1   2   3   4   5
+ * 预订记录 1 ：   10  10
+ * 预订记录 2 ：       20  20
+ * 预订记录 3 ：       25  25  25  25
+ * 总座位数：      10  55  45  25  25
+ * 因此，answer = [10,55,45,25,25]
+ * 示例 2：
+ *
+ * 输入：bookings = [[1,2,10],[2,2,15]], n = 2
+ * 输出：[10,25]
+ * 解释：
+ * 航班编号        1   2
+ * 预订记录 1 ：   10  10
+ * 预订记录 2 ：       15
+ * 总座位数：      10  25
+ * 因此，answer = [10,25]
+ *
  */
 public class CorporateFlightBookings {
 
-    // V0
-//    public int[] corpFlightBookings(int[][] bookings, int n) {
-//
-//    }
+  // V0
+  // TODO : implement
+  //    public int[] corpFlightBookings(int[][] bookings, int n) {
+  //
+  //    }
 
-    // V1
+  // V1
+  // IDEA : difference ARRAY (gpt)
+  public int[] corpFlightBookings_1(int[][] bookings, int n) {
+      int[] res = new int[n + 1]; // Use n+1 to handle range updates easily with a difference array.
 
-    // V2
+      // Create a difference array
+      for (int[] booking : bookings) {
+          int start = booking[0]; // Starting index (1-based)
+          int end = booking[1]; // Ending index (1-based)
+          int seats = booking[2]; // Seats to add
+
+          res[start] += seats; // Add seats at the start
+          if (end + 1 <= n) { // Subtract seats after the end
+              res[end + 1] -= seats;
+          }
+      }
+
+      // Compute the prefix sum to get the final result
+      for (int i = 1; i <= n; i++) {
+          res[i] += res[i - 1];
+      }
+
+      // Exclude the extra index used for the difference array
+      return Arrays.copyOfRange(res, 1, n + 1);
+  }
+
+  // V2
+  // https://leetcode.com/problems/corporate-flight-bookings/submissions/1483568206/
+  public int[] corpFlightBookings_2(int[][] bookings, int n) {
+      int[] ans = new int[n];
+      for (int[] booking : bookings) {
+          int i = booking[0] - 1;
+          int j = booking[1];
+          int seats = booking[2];
+          ans[i] += seats;
+          if (j != n)
+              ans[j] -= seats;
+      }
+
+      int count = 0;
+      for (int i = 0; i < ans.length; i++) {
+          ans[i] += count;
+          count = ans[i];
+      }
+      return ans;
+  }
+
+  // V3-1
+  // https://leetcode.com/problems/corporate-flight-bookings/solutions/1338804/java-2-approaches-brute-force-optimal-ap-e12t/
+  // IDEA : BRUTE FORCE
+  public int[] corpFlightBookings_3_1(int[][] bookings, int n) {
+
+      int[] res = new int[n];
+      for (int[] curr : bookings) {
+          int start = curr[0] - 1;
+          int end = curr[1];
+          int val = curr[2];
+
+          for (int i = start; i < end; i++) {
+              res[i] += val;
+          }
+      }
+      return res;
+  }
+
+  // V3-2
+  // https://leetcode.com/problems/corporate-flight-bookings/solutions/1338804/java-2-approaches-brute-force-optimal-ap-e12t/
+  // Optimized  BRUTE FORCE
+  public int[] corpFlightBookings_3_2(int[][] bookings, int n) {
+
+      int[] res = new int[n];
+      for (int[] curr : bookings) {
+          int start = curr[0] - 1;
+          int end = curr[1];
+          int val = curr[2];
+          res[start] += val;
+          if (end < n) {
+              res[end] -= val;
+          }
+      }
+      for (int i = 1; i < n; i++) {
+          res[i] += res[i - 1];
+      }
+      return res;
+  }
+
 }
