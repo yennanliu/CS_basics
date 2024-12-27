ccd07668f654e7ded1c94f4edafcd0bb48296672
yennanliu <f339339@gmail.com>
2024-12-27 14:30:33 +0800

    add 079, 212 problem description, progress

--- leetcode_java/src/main/java/LeetCodeJava/BackTrack/WordSearch.java
+++ leetcode_java/src/main/java/LeetCodeJava/BackTrack/WordSearch.java
@@ -1,8 +1,43 @@
 package LeetCodeJava.BackTrack;
 
 // https://leetcode.com/problems/word-search/
-
-
+/**
+ * 79. Word Search
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+ *
+ * The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * Output: true
+ * Example 2:
+ *
+ *
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+ * Output: true
+ * Example 3:
+ *
+ *
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * m == board.length
+ * n = board[i].length
+ * 1 <= m, n <= 6
+ * 1 <= word.length <= 15
+ * board and word consists of only lowercase and uppercase English letters.
+ */
 public class WordSearch {
 
     // V0
--- leetcode_java/src/main/java/LeetCodeJava/BackTrack/WordSearch2.java
+++ leetcode_java/src/main/java/LeetCodeJava/BackTrack/WordSearch2.java
@@ -5,6 +5,43 @@ package LeetCodeJava.BackTrack;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 212. Word Search II
+ * Solved
+ * Hard
+ * Topics
+ * Companies
+ * Hint
+ * Given an m x n board of characters and a list of strings words, return all words on the board.
+ *
+ * Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]
+ * Output: ["eat","oath"]
+ * Example 2:
+ *
+ *
+ * Input: board = [["a","b"],["c","d"]], words = ["abcb"]
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * m == board.length
+ * n == board[i].length
+ * 1 <= m, n <= 12
+ * board[i][j] is a lowercase English letter.
+ * 1 <= words.length <= 3 * 104
+ * 1 <= words[i].length <= 10
+ * words[i] consists of lowercase English letters.
+ * All the strings of words are unique.
+ *
+ */
 public class WordSearch2 {
 
     // V0
