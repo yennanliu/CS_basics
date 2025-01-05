f6eac385539cfa9d7223847100f174ac63cf840b
yennanliu <f339339@gmail.com>
2025-01-05 19:20:53 +0800

    add 399 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/DFS/EvaluateDivision.java
@@ -0,0 +1,265 @@
+package LeetCodeJava.DFS;
+
+// https://leetcode.com/problems/evaluate-division/description/
+
+import java.util.*;
+
+/**
+ * 399. Evaluate Division
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+ *
+ * You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+ *
+ * Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+ *
+ * Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+ *
+ * Note: The variables that do not occur in the list of equations are undefined, so the answer cannot be determined for them.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+ * Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+ * Explanation:
+ * Given: a / b = 2.0, b / c = 3.0
+ * queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+ * return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+ * note: x is undefined => -1.0
+ * Example 2:
+ *
+ * Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+ * Output: [3.75000,0.40000,5.00000,0.20000]
+ * Example 3:
+ *
+ * Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+ * Output: [0.50000,2.00000,-1.00000,-1.00000]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= equations.length <= 20
+ * equations[i].length == 2
+ * 1 <= Ai.length, Bi.length <= 5
+ * values.length == equations.length
+ * 0.0 < values[i] <= 20.0
+ * 1 <= queries.length <= 20
+ * queries[i].length == 2
+ * 1 <= Cj.length, Dj.length <= 5
+ * Ai, Bi, Cj, Dj consist of lower case English letters and digits.
+ *
+ */
+public class EvaluateDivision {
+    // V0
+    // TODO: implement
+//    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+//
+//    }
+
+
+    // V1
+
+    // V2
+    // IDEA: DFS
+    // https://leetcode.com/problems/evaluate-division/solutions/3543256/image-explanation-easiest-concise-comple-okpu/
+    public void dfs(String node, String dest, HashMap<String, HashMap<String, Double>> gr, HashSet<String> vis,
+                    double[] ans, double temp) {
+        if (vis.contains(node))
+            return;
+
+        vis.add(node);
+        if (node.equals(dest)) {
+            ans[0] = temp;
+            return;
+        }
+
+        for (Map.Entry<String, Double> entry : gr.get(node).entrySet()) {
+            String ne = entry.getKey();
+            double val = entry.getValue();
+            dfs(ne, dest, gr, vis, ans, temp * val);
+        }
+    }
+
+    public HashMap<String, HashMap<String, Double>> buildGraph(List<List<String>> equations, double[] values) {
+        HashMap<String, HashMap<String, Double>> gr = new HashMap<>();
+
+        for (int i = 0; i < equations.size(); i++) {
+            String dividend = equations.get(i).get(0);
+            String divisor = equations.get(i).get(1);
+            double value = values[i];
+
+            gr.putIfAbsent(dividend, new HashMap<>());
+            gr.putIfAbsent(divisor, new HashMap<>());
+
+            gr.get(dividend).put(divisor, value);
+            gr.get(divisor).put(dividend, 1.0 / value);
+        }
+
+        return gr;
+    }
+
+    public double[] calcEquation_2(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        HashMap<String, HashMap<String, Double>> gr = buildGraph(equations, values);
+        double[] finalAns = new double[queries.size()];
+
+        for (int i = 0; i < queries.size(); i++) {
+            String dividend = queries.get(i).get(0);
+            String divisor = queries.get(i).get(1);
+
+            if (!gr.containsKey(dividend) || !gr.containsKey(divisor)) {
+                finalAns[i] = -1.0;
+            } else {
+                HashSet<String> vis = new HashSet<>();
+                double[] ans = { -1.0 };
+                double temp = 1.0;
+                dfs(dividend, divisor, gr, vis, ans, temp);
+                finalAns[i] = ans[0];
+            }
+        }
+
+        return finalAns;
+    }
+
+    // V3
+    // IDEA: DFS
+    // https://leetcode.com/problems/evaluate-division/solutions/1992891/java-dfs-solution-with-comments-evaluate-6gmn/
+    private Map<String, Map<String, Double>> makeGraph(List<List<String>> e, double[] values) {
+        // build a graph
+        // like a -> b = values[i]
+        // and b -> a = 1.0 / values[i];
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+        String u, v;
+
+        for (int i = 0; i < e.size(); i++) {
+            u = e.get(i).get(0);
+            v = e.get(i).get(1);
+
+            graph.putIfAbsent(u, new HashMap<>());
+            graph.get(u).put(v, values[i]);
+
+            graph.putIfAbsent(v, new HashMap<>());
+            graph.get(v).put(u, 1 / values[i]);
+
+        }
+        return graph;
+    }
+
+    public double[] calcEquation_3(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, Map<String, Double>> graph = makeGraph(equations, values);
+
+        double[] ans = new double[queries.size()];
+
+        // check for every Querie
+        // store it in ans array;
+        for (int i = 0; i < queries.size(); i++) {
+            ans[i] = dfs(queries.get(i).get(0), queries.get(i).get(1), new HashSet<>(), graph);
+        }
+        return ans;
+    }
+
+    public double dfs(String src, String dest, Set<String> visited, Map<String, Map<String, Double>> graph) {
+        // check the terminated Case
+        // if string is not present in graph return -1.0;
+        // like [a, e] or [x, x] :)
+        if (graph.containsKey(src) == false)
+            return -1.0;
+
+        // simply say check src and dest are equal :) then return dest
+        // store it in weight varaible;
+        // case like [a,a] also handle
+        if (graph.get(src).containsKey(dest)) {
+            return graph.get(src).get(dest);
+        }
+
+        visited.add(src);
+
+        for (Map.Entry<String, Double> nbr : graph.get(src).entrySet()) {
+            if (visited.contains(nbr.getKey()) == false) {
+                double weight = dfs(nbr.getKey(), dest, visited, graph);
+
+                // if weight is not -1.0(terminate case)
+                // then mutliply it
+                // like in querie a -> c => 2 * 3 = 6
+                if (weight != -1.0) {
+                    return nbr.getValue() * weight;
+                }
+            }
+        }
+        return -1.0;
+    }
+
+    // V4
+    // IDEA: BFS
+    // https://leetcode.com/problems/evaluate-division/solutions/3543150/pythonjavacsimple-solutioneasy-to-unders-7uwo/
+//    public double[] calcEquation_4(List<List<String>> equations, double[] values, List<List<String>> queries) {
+//        Map<String, Map<String, Double>> graph = buildGraph_4(equations, values);
+//        double[] results = new double[queries.size()];
+//
+//        for (int i = 0; i < queries.size(); i++) {
+//            List<String> query = queries.get(i);
+//            String dividend = query.get(0);
+//            String divisor = query.get(1);
+//
+//            if (!graph.containsKey(dividend) || !graph.containsKey(divisor)) {
+//                results[i] = -1.0;
+//            } else {
+//                results[i] = bfs(dividend, divisor, graph);
+//            }
+//        }
+//
+//        return results;
+//    }
+//
+//    private Map<String, Map<String, Double>> buildGraph_4(List<List<String>> equations, double[] values) {
+//        Map<String, Map<String, Double>> graph = new HashMap<>();
+//
+//        for (int i = 0; i < equations.size(); i++) {
+//            List<String> equation = equations.get(i);
+//            String dividend = equation.get(0);
+//            String divisor = equation.get(1);
+//            double value = values[i];
+//
+//            graph.putIfAbsent(dividend, new HashMap<>());
+//            graph.putIfAbsent(divisor, new HashMap<>());
+//            graph.get(dividend).put(divisor, value);
+//            graph.get(divisor).put(dividend, 1.0 / value);
+//        }
+//
+//        return graph;
+//    }
+//
+//    private double bfs(String start, String end, Map<String, Map<String, Double>> graph) {
+//        Queue<Pair<String, Double>> queue = new LinkedList<>();
+//        Set<String> visited = new HashSet<>();
+//        queue.offer(new Pair<>(start, 1.0));
+//
+//        while (!queue.isEmpty()) {
+//            Pair<String, Double> pair = queue.poll();
+//            String node = pair.getKey();
+//            double value = pair.getValue();
+//
+//            if (node.equals(end)) {
+//                return value;
+//            }
+//
+//            visited.add(node);
+//
+//            for (Map.Entry<String, Double> neighbor : graph.get(node).entrySet()) {
+//                String neighborNode = neighbor.getKey();
+//                double neighborValue = neighbor.getValue();
+//
+//                if (!visited.contains(neighborNode)) {
+//                    queue.offer(new Pair<>(neighborNode, value * neighborValue));
+//                }
+//            }
+//        }
+//
+//        return -1.0;
+//    }
+}
