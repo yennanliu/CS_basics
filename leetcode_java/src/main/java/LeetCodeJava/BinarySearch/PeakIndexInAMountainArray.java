26669097a00601e54b78435f463be3144e265386
yennanliu <f339339@gmail.com>
2025-01-08 07:48:55 +0800

    add 852 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/BinarySearch/PeakIndexInAMountainArray.java
@@ -0,0 +1,69 @@
+package LeetCodeJava.BinarySearch;
+
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+// https://leetcode.cn/problems/peak-index-in-a-mountain-array/description/
+/**
+ * 852. Peak Index in a Mountain Array
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+ *
+ * Return the index of the peak element.
+ *
+ * Your task is to solve it in O(log(n)) time complexity.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: arr = [0,1,0]
+ *
+ * Output: 1
+ *
+ * Example 2:
+ *
+ * Input: arr = [0,2,1,0]
+ *
+ * Output: 1
+ *
+ * Example 3:
+ *
+ * Input: arr = [0,10,5,2]
+ *
+ * Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 3 <= arr.length <= 105
+ * 0 <= arr[i] <= 106
+ * arr is guaranteed to be a mountain array.
+ * Seen this question in a real interview before?
+ * 1/5
+ * Yes
+ * No
+ * Accepted
+ * 928.3K
+ * Submissions
+ * 1.4M
+ * Acceptance Rate
+ * 67.9%
+ * Topics
+ * Companies
+ * Similar Questions
+ * Discussion (183)
+ *
+ *
+ */
+public class PeakIndexInAMountainArray {
+
+    // V0
+//    public int peakIndexInMountainArray(int[] arr) {
+//
+//    }
+    // V1
+    // V2
+}
