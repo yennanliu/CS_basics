d9a414e3c7522952932d25d90c814f6f17a0a463
yennj12 <f339339@uber.com>
2025-01-09 12:49:22 +0800

    update

--- leetcode_java/src/main/java/dev/Sorting/ListSortTest.java
+++ leetcode_java/src/main/java/dev/Sorting/ListSortTest.java
@@ -16,25 +16,33 @@ public class ListSortTest {
         list.add(-3);
 
         System.out.println(">>> (before) list = " + list);
-        
+
         // V1: lambda
-        Collections.sort(list, (x,y) -> {
-            int diff = y - x;
-            return diff;
-        });
-//
-//        // V2: Comparator.compare
-//        Collections.sort(list, new Comparator<Integer>(){
-//            @Override
-//            public int compare(Integer o1, Integer o2) {
-//                //int diff = o1 - o2;  // smaller first (increasing order)
-//                int diff = o2 - o1; // bigger first, (decreasing order)
-////                if(diff == 0){
-////                    return
-////                }
-//                return diff;
-//            }
+//        Collections.sort(list, (x,y) -> {
+//            int diff = y - x;
+//            return diff;
 //        });
+
+        // V2: Comparator.compare
+        /**
+         * NOTE !!!
+         *
+         *
+         *  need to offer TYPE for comparator
+         *
+         *  e.g. `new Comparator<Integer>()`
+         */
+        Collections.sort(list, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                //int diff = o1 - o2;  // smaller first (increasing order)
+                int diff = o2 - o1; // bigger first, (decreasing order)
+//                if(diff == 0){
+//                    return
+//                }
+                return diff;
+            }
+        });
 
         System.out.println(">>> (after) list = " + list);
 
