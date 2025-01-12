ad213da59f3e497e5a7b9656a775491e5ffeb47d
yennanliu <f339339@gmail.com>
2025-01-12 19:41:38 +0800

    update 986 java

--- leetcode_java/src/main/java/LeetCodeJava/TwoPointer/IntervalListIntersections.java
+++ leetcode_java/src/main/java/LeetCodeJava/TwoPointer/IntervalListIntersections.java
@@ -1,6 +1,10 @@
 package LeetCodeJava.TwoPointer;
 
 // https://leetcode.com/problems/interval-list-intersections/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 986. Interval List Intersections
  * Medium
@@ -44,6 +48,86 @@ public class IntervalListIntersections {
 //    }
 
     // V1
+    // https://leetcode.com/problems/interval-list-intersections/solutions/1593579/java-two-pointers-most-intutive-by-chait-8cfr/
+    // IDEA: 2 POINTERS
+    public int[][] intervalIntersection_1(int[][] firstList, int[][] secondList) {
+        if (firstList.length == 0 || secondList.length == 0)
+            return new int[0][0];
+        int i = 0;
+        int j = 0;
+        int startMax = 0, endMin = 0;
+        List<int[]> ans = new ArrayList<>();
+
+        while (i < firstList.length && j < secondList.length) {
+            startMax = Math.max(firstList[i][0], secondList[j][0]);
+            endMin = Math.min(firstList[i][1], secondList[j][1]);
+
+            // you have end greater than start and you already know that this interval is
+            // sorrounded with startMin and endMax so this must be the intersection
+            if (endMin >= startMax) {
+                ans.add(new int[] { startMax, endMin });
+            }
+
+            // the interval with min end has been covered completely and have no chance to
+            // intersect with any other interval so move that list's pointer
+            if (endMin == firstList[i][1])
+                i++;
+            if (endMin == secondList[j][1])
+                j++;
+        }
+
+        return ans.toArray(new int[ans.size()][2]);
+    }
 
     // V2
+    // https://leetcode.com/problems/interval-list-intersections/solutions/592550/2-ms-faster-than-9963-of-java-and-memory-0pqj/
+    public int[][] intervalIntersection_2(int[][] A, int[][] B) {
+        int i = 0;
+        int j = 0;
+        List<int[]> list = new ArrayList<>();
+        while (i < A.length && j < B.length) {
+            int lower = Math.max(A[i][0], B[j][0]);
+            int upper = Math.min(A[i][1], B[j][1]);
+            if (lower <= upper) {
+                list.add(new int[] { lower, upper });
+            }
+            if (A[i][1] < B[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+
+        }
+        return list.toArray(new int[list.size()][]);
+    }
+
+    // V3
+    // https://leetcode.com/problems/interval-list-intersections/solutions/6048050/simple-solution-with-diagrams-in-short-v-u77n/
+    public int[][] intervalIntersection_3(int[][] firstList, int[][] secondList) {
+        List<int[]> intersections = new ArrayList<>(); // to store all
+        // intersecting intervals
+        // index "i" to iterate over the length of list a and index "j"
+        // to iterate over the length of list b
+        int i = 0, j = 0;
+        // while loop will break whenever either of the lists ends
+        while (i < firstList.length && j < secondList.length) {
+            // Let's check if firstList[i] intersects secondList[j]
+            // 1. start - the potential startpoint of the intersection
+            // 2. end - the potential endpoint of the intersection
+            int start = Math.max(firstList[i][0], secondList[j][0]);
+            int end = Math.min(firstList[i][1], secondList[j][1]);
+
+            if (start <= end) // if this is an actual intersection
+                intersections.add(new int[] { start, end }); // add it to the list
+            // Move forward in the list whose interval ends earlier
+            if (firstList[i][1] < secondList[j][1])
+                i += 1;
+            else
+                j += 1;
+        }
+
+        return intersections.toArray(new int[0][]);
+    }
+    
+
 }
