3d63fa62fbcf681c4d6b81af313c5078d4312ab7
yennanliu <f339339@gmail.com>
2025-02-04 15:18:09 +0800

    update 102 java

--- leetcode_java/src/main/java/LeetCodeJava/BFS/BinaryTreeLevelOrderTraversal_ii.java
+++ leetcode_java/src/main/java/LeetCodeJava/BFS/BinaryTreeLevelOrderTraversal_ii.java
@@ -1,7 +1,47 @@
 package LeetCodeJava.BFS;
 
 // https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
-
+/**
+ *  107. Binary Tree Level Order Traversal II
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[15,7],[9,20],[3]]
+ * Example 2:
+ *
+ * Input: root = [1]
+ * Output: [[1]]
+ * Example 3:
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -1000 <= Node.val <= 1000
+ * Seen this question in a real interview before?
+ * 1/5
+ * Yes
+ * No
+ * Accepted
+ * 706.5K
+ * Submissions
+ * 1.1M
+ * Acceptance Rate
+ * 65.2%
+ *
+ */
 import LeetCodeJava.DataStructure.TreeNode;
 
 import java.util.*;
