da4c2db44364faeb86a18c64fe1d2784ab564454
yennanliu <f339339@gmail.com>
2025-02-04 15:07:31 +0800

    update 102 java

--- leetcode_java/src/main/java/LeetCodeJava/BFS/BinaryTreeLevelOrderTraversal.java
+++ leetcode_java/src/main/java/LeetCodeJava/BFS/BinaryTreeLevelOrderTraversal.java
@@ -1,7 +1,47 @@
 package LeetCodeJava.BFS;
 
 // https://leetcode.com/problems/binary-tree-level-order-traversal/
-
+/**
+ *  102. Binary Tree Level Order Traversal
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[9,20],[15,7]]
+ * Example 2:
+ *
+ * Input: root = [1]
+ * Output: [[1]]
+ * Example 3:
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -1000 <= Node.val <= 1000
+ * Seen this question in a real interview before?
+ * 1/5
+ * Yes
+ * No
+ * Accepted
+ * 2.6M
+ * Submissions
+ * 3.8M
+ * Acceptance Rate
+ * 69.5%
+ */
 import LeetCodeJava.DataStructure.TreeNode;
 
 import java.util.*;
@@ -49,9 +89,9 @@ public class BinaryTreeLevelOrderTraversal {
         return res;
     }
 
-    // V0'
+    // V0-1
     // IDEA : BFS + custom class (save layer info, and TreeNode)
-    public List<List<Integer>> levelOrder_0(TreeNode root) {
+    public List<List<Integer>> levelOrder_0_1(TreeNode root) {
 
         if (root == null){
             return new ArrayList();
@@ -95,6 +135,98 @@ public class BinaryTreeLevelOrderTraversal {
             this.layer = layer;
             this.root = root;
         }
+    }
+
+    // V0-2
+    // IDEA: BFS + CUSTOM CLASS
+    public class MyTreeLayer {
+        // attr
+        int layer;
+        TreeNode root;
+
+        // constructor
+        MyTreeLayer(int layer, TreeNode root) {
+            this.layer = layer;
+            this.root = root;
+        }
+
+        // getter, setter
+        public int getLayer() {
+            return layer;
+        }
+
+        public void setLayer(int layer) {
+            this.layer = layer;
+        }
+
+        public TreeNode getRoot() {
+            return root;
+        }
+
+        public void setRoot(TreeNode root) {
+            this.root = root;
+        }
+    }
+
+    public List<List<Integer>> levelOrder_0_2(TreeNode root) {
+        // edge
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        if (root.left == null && root.right == null) {
+            List<Integer> tmp = new ArrayList<>();
+            tmp.add(root.val);
+            List<List<Integer>> res = new ArrayList<>();
+            res.add(tmp);
+            return res;
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        // bfs
+        Queue<MyTreeLayer> q = new LinkedList<>();
+        q.add(new MyTreeLayer(0, root));
+        while (!q.isEmpty()) {
+
+            MyTreeLayer myTreeLayer = q.poll();
+            int depth = myTreeLayer.getLayer();
+            TreeNode curRoot = myTreeLayer.getRoot();
+
+            if (res.size() < depth + 1) {
+                res.add(new ArrayList<>());
+            }
+
+      // NOTE !!! below is wrong !!!!
+      // List<Integer> tmp = res.get(depth);
+      // tmp.add(curRoot.val);
+      // res.add(depth, tmp); // /??
+
+      /**
+       *  NOTE !!!
+       *
+       *   via below, we can retrieve List val by idx,
+       *   append new val to the val (with the same idx)
+       *
+       *
+       *   code breakdown:
+       *
+       *   	•	res is a List<List<Integer>>, where each inner list represents a level of the tree.
+       * 	•	res.get(depth) retrieves the list at the given depth.
+       * 	•	.add(curRoot.val) adds the current node’s value to the corresponding depth level.
+       *
+       */
+      res.get(depth).add(curRoot.val);
+
+            if (curRoot.left != null) {
+                q.add(new MyTreeLayer(depth + 1, curRoot.left));
+            }
+            if (curRoot.right != null) {
+                q.add(new MyTreeLayer(depth + 1, curRoot.right));
+            }
+
+        }
+
+        //System.out.println(">>> res = " + res);
+        return res;
     }
 
 
