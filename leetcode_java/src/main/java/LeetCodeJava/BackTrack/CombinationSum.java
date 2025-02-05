a79df954f6720bdc9d97590d712ace70206eb9f9
yennj12 <f339339@uber.com>
2025-02-05 10:25:40 +0800

    update 39 java

--- leetcode_java/src/main/java/LeetCodeJava/BackTrack/CombinationSum.java
+++ leetcode_java/src/main/java/LeetCodeJava/BackTrack/CombinationSum.java
@@ -1,7 +1,48 @@
 package LeetCodeJava.BackTrack;
 
 // https://leetcode.com/problems/combination-sum/
-
+/**
+ *  39. Combination Sum
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+ *
+ * The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the
+ * frequency
+ *  of at least one of the chosen numbers is different.
+ *
+ * The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: candidates = [2,3,6,7], target = 7
+ * Output: [[2,2,3],[7]]
+ * Explanation:
+ * 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+ * 7 is a candidate, and 7 = 7.
+ * These are the only two combinations.
+ * Example 2:
+ *
+ * Input: candidates = [2,3,5], target = 8
+ * Output: [[2,2,2,2],[2,3,3],[3,5]]
+ * Example 3:
+ *
+ * Input: candidates = [2], target = 1
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= candidates.length <= 30
+ * 2 <= candidates[i] <= 40
+ * All elements of candidates are distinct.
+ * 1 <= target <= 40
+ *
+ */
 import java.util.*;
 
 public class CombinationSum {
@@ -80,11 +121,11 @@ public class CombinationSum {
         return res;
     }
 
-    // V0'
+    // V0-1
     // IDEA : BACKTRACK (GPT)
     List<List<Integer>> res_0 = new ArrayList<>();
 
-    public List<List<Integer>> combinationSum_0(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum_0_1(int[] candidates, int target) {
         List<Integer> cur = new ArrayList<>();
         //Arrays.sort(candidates); // Optional: sort to improve efficiency
         backtrack_0(candidates, target, cur, res_0, 0);
@@ -124,7 +165,7 @@ public class CombinationSum {
         return res;
     }
 
-    // V0'
+    // V0-2
     // IDEA : BACKTRACK
     /**
      *  Example output :
@@ -156,7 +197,7 @@ public class CombinationSum {
      * i = 2 cur = [5]
      *
      */
-    public List<List<Integer>> combinationSum_(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum_0_2(int[] candidates, int target) {
 
         // sort
         Arrays.sort(candidates);
