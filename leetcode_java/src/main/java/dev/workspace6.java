79c10305fcbf1e6c98862db8ebb04b63899eef93
yennanliu <f339339@gmail.com>
2025-02-06 20:51:17 +0800

    update 211 java

--- leetcode_java/src/main/java/dev/workspace6.java
+++ leetcode_java/src/main/java/dev/workspace6.java
@@ -6587,6 +6587,29 @@ public class workspace6 {
         }
     }
 
+    /**
+     * Your WordDictionary object will be instantiated and called as such:
+     * WordDictionary obj = new WordDictionary();
+     * obj.addWord(word);
+     * boolean param_2 = obj.search(word);
+     */
+    // LC 211
+    class WordDictionary {
+
+        public WordDictionary() {
+
+        }
+
+        public void addWord(String word) {
+
+        }
+
+        public boolean search(String word) {
+
+            return false;
+        }
+    }
+
 
 }
 
