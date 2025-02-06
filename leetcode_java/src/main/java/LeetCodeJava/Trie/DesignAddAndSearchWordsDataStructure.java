cfca6dd6871bb23ac5d6b059563a228cb7a8b691
yennanliu <f339339@gmail.com>
2025-02-06 20:51:17 +0800

    update 211 java

--- leetcode_java/src/main/java/LeetCodeJava/Trie/DesignAddAndSearchWordsDataStructure.java
+++ leetcode_java/src/main/java/LeetCodeJava/Trie/DesignAddAndSearchWordsDataStructure.java
@@ -1,7 +1,50 @@
 package LeetCodeJava.Trie;
 
 // https://leetcode.com/problems/design-add-and-search-words-data-structure/
-
+/**
+ *  211. Design Add and Search Words Data Structure
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * Design a data structure that supports adding new words and finding if a string matches any previously added string.
+ *
+ * Implement the WordDictionary class:
+ *
+ * WordDictionary() Initializes the object.
+ * void addWord(word) Adds word to the data structure, it can be matched later.
+ * bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+ *
+ *
+ * Example:
+ *
+ * Input
+ * ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+ * [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+ * Output
+ * [null,null,null,null,false,true,true,true]
+ *
+ * Explanation
+ * WordDictionary wordDictionary = new WordDictionary();
+ * wordDictionary.addWord("bad");
+ * wordDictionary.addWord("dad");
+ * wordDictionary.addWord("mad");
+ * wordDictionary.search("pad"); // return False
+ * wordDictionary.search("bad"); // return True
+ * wordDictionary.search(".ad"); // return True
+ * wordDictionary.search("b.."); // return True
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= word.length <= 25
+ * word in addWord consists of lowercase English letters.
+ * word in search consist of '.' or lowercase English letters.
+ * There will be at most 2 dots in word for search queries.
+ * At most 104 calls will be made to addWord and search.
+ *
+ */
 import LeetCodeJava.DataStructure.TreeNode;
 
 import java.util.HashMap;
