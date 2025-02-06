f165e990818b8e21ab25dc07af472da5600856fb
yennanliu <f339339@gmail.com>
2025-02-06 20:31:58 +0800

    update 208 java

--- leetcode_java/src/main/java/LeetCodeJava/Trie/ImplementTrie.java
+++ leetcode_java/src/main/java/LeetCodeJava/Trie/ImplementTrie.java
@@ -1,19 +1,60 @@
 package LeetCodeJava.Trie;
 
 // https://leetcode.com/problems/implement-trie-prefix-tree/
-
 /**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
+ * 208. Implement Trie (Prefix Tree)
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+ *
+ * Implement the Trie class:
+ *
+ * Trie() Initializes the trie object.
+ * void insert(String word) Inserts the string word into the trie.
+ * boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+ * boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ *
+ *
+ * Example 1:
+ *
+ * Input
+ * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+ * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+ * Output
+ * [null, null, true, false, true, null, true]
+ *
+ * Explanation
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // return True
+ * trie.search("app");     // return False
+ * trie.startsWith("app"); // return True
+ * trie.insert("app");
+ * trie.search("app");     // return True
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= word.length, prefix.length <= 2000
+ * word and prefix consist only of lowercase English letters.
+ * At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+ *
+ *
  */
 import java.util.HashMap;
 import java.util.Map;
 
 public class ImplementTrie {
 
+    /**
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.insert(word);
+     * boolean param_2 = obj.search(word);
+     * boolean param_3 = obj.startsWith(prefix);
+     */
     // V0
     // IDEA : https://github.com/yennanliu/CS_basics/blob/master/leetcode_python/Tree/implement-trie-prefix-tree.py#L49
     // modified by GPT
