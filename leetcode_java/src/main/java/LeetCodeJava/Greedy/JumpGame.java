0fe029828ba6b84c429d5eee3976307023064630
yennj12 <f339339@uber.com>
2025-02-11 10:12:00 +0800

    update 55 java

--- leetcode_java/src/main/java/LeetCodeJava/Greedy/JumpGame.java
+++ leetcode_java/src/main/java/LeetCodeJava/Greedy/JumpGame.java
@@ -1,5 +1,36 @@
 package LeetCodeJava.Greedy;
 
+// https://leetcode.com/problems/jump-game/description/
+/**
+ * 55. Jump Game
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+ *
+ * Return true if you can reach the last index, or false otherwise.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ * Example 2:
+ *
+ * Input: nums = [3,2,1,0,4]
+ * Output: false
+ * Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 104
+ * 0 <= nums[i] <= 105
+ *
+ */
 public class JumpGame {
 
     // V0
@@ -60,23 +91,40 @@ public class JumpGame {
         int cur = 0;
         for (int i = 0; i < nums.length; i++){
             System.out.println("cur = " + cur + " i = " + i);
-            // NOTE !!! : if cur < i, means it's NOT possible to visit idx i, so should return false directly
+            /**
+             * NOTE !!! :
+             *
+             *   if cur < i, means it's NOT possible to visit idx i,
+             *   -> so should return false directly
+             */
             if (cur < i){
                 return false;
             }
-            // NOTE !!! : we keep get bigger one from cur, i + nums[i], so we have "MUST" steps can move forward
-            //           -> no need to do "cur -= 1", since "i++" when loop goes next iteration, cur will be compared with updated i
+            /**
+             * NOTE !!! :
+             *
+             *     we keep get bigger one from cur, i + nums[i],
+             *     so we have "MUST" steps can move forward
+             *
+             *  -> no need to do "cur -= 1",
+             *     since "i++" when loop goes next iteration,
+             *     cur will be compared with updated i
+             */
             cur = Math.max(cur, i + nums[i]);
         }
 
         return true;
     }
 
-    // V0'
+    // V0-1
     // IDEA : GREEDY
-    public boolean canJump_(int[] nums) {
+    public boolean canJump_0_1(int[] nums) {
         int lastPos = nums.length - 1;
-        // NOTE : loop from right to left (<--)
+        /**
+         * NOTE :
+         *
+         *   loop from right to left (<-----)
+         */
         for (int i = nums.length - 1; i >= 0; i--) {
             if (i + nums[i] >= lastPos) {
                 lastPos = i;
@@ -85,9 +133,9 @@ public class JumpGame {
         return lastPos == 0;
     }
 
-    // V0''
+    // V0-2
     // IDEA : GREEDY (modified by GPT)
-    public boolean canJump_0(int[] nums) {
+    public boolean canJump_0_2(int[] nums) {
         // If there is only one element, we are already at the last index
         if (nums.length == 1) {
             return true;
