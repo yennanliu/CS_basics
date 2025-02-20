ece345bdcec696c42779826fe68d046186676906
yennj12 <f339339@uber.com>
2025-02-20 11:26:44 +0800

    add 287 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/Array/FindTheDuplicateNumber.java
@@ -0,0 +1,69 @@
+package LeetCodeJava.Array;
+
+// https://leetcode.com/problems/find-the-duplicate-number/description/
+
+import java.util.HashSet;
+
+/**
+ * 287. Find the Duplicate Number
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+ *
+ * There is only one repeated number in nums, return this repeated number.
+ *
+ * You must solve the problem without modifying the array nums and using only constant extra space.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,3,4,2,2]
+ * Output: 2
+ * Example 2:
+ *
+ * Input: nums = [3,1,3,4,2]
+ * Output: 3
+ * Example 3:
+ *
+ * Input: nums = [3,3,3,3,3]
+ * Output: 3
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 105
+ * nums.length == n + 1
+ * 1 <= nums[i] <= n
+ * All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ *
+ *
+ * Follow up:
+ *
+ * How can we prove that at least one duplicate number must exist in nums?
+ * Can you solve the problem in linear runtime complexity?
+ *
+ *
+ */
+public class FindTheDuplicateNumber {
+
+    // V0
+    // IDEA: HASHSET
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int x : nums) {
+            if (set.contains(x)) {
+                return x;
+            }
+            set.add(x);
+        }
+
+        return -1;
+    }
+
+    // V1
+
+    // V2
+}
--- leetcode_java/src/main/java/dev/workspace8.java
+++ leetcode_java/src/main/java/dev/workspace8.java
@@ -1981,5 +1981,48 @@ public class workspace8 {
     }
 
 
+    // LC 287
+    // 11.23 - 11.33
+    /**
+     * 287. Find the Duplicate Number
+     *
+     * Given an array nums containing n + 1 integers
+     * where each integer is between 1 and n (inclusive),
+     * prove that at least one duplicate number must exist.
+     * Assume that there is only one duplicate number, find the duplicate one.
+     *
+     * Example 1:
+     *
+     * Input: [1,3,4,2,2]
+     * Output: 2
+     *
+     *
+     * Example 2:
+     *
+     * Input: [3,1,3,4,2]
+     * Output: 3
+     * Note:
+     *
+     * You must not modify the array (assume the array is read only).
+     * You must use only constant, O(1) extra space.
+     * Your runtime complexity should be less than O(n2).
+     * There is only one duplicate number in the array, but it could be repeated more than once.
+     *
+     */
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int x: nums){
+            if(set.contains(x)){
+                return x;
+            }
+            set.add(x);
+        }
+
+        return -1;
+    }
+
+
+
+
 
 }
