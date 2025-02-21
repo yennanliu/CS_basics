a140723fb847084db70d1da9dd20994609a2f071
yennanliu <f339339@gmail.com>
2025-02-21 18:17:11 +0800

    update 199 java, progress

--- leetcode_java/src/main/java/LeetCodeJava/DFS/BinaryTreeRightSideView.java
+++ leetcode_java/src/main/java/LeetCodeJava/DFS/BinaryTreeRightSideView.java
@@ -1,29 +1,78 @@
 package LeetCodeJava.DFS;
 
 // https://leetcode.com/problems/binary-tree-right-side-view/
-
+/**
+ * 199. Binary Tree Right Side View
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: root = [1,2,3,null,5,null,4]
+ *
+ * Output: [1,3,4]
+ *
+ * Explanation:
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input: root = [1,2,3,4,null,null,null,5]
+ *
+ * Output: [1,3,4,5]
+ *
+ * Explanation:
+ *
+ *
+ *
+ * Example 3:
+ *
+ * Input: root = [1,null,3]
+ *
+ * Output: [1,3]
+ *
+ * Example 4:
+ *
+ * Input: root = []
+ *
+ * Output: []
+ *
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ */
 import LeetCodeJava.DataStructure.TreeNode;
 
 import java.util.*;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
 public class BinaryTreeRightSideView {
 
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
     // VO
     // IDEA : BFS
 //    public List<Integer> rightSideView(TreeNode root) {
@@ -81,9 +130,9 @@ public class BinaryTreeRightSideView {
 //    }
 
 
-    // VO'
+    // VO-1
     // IDEA : BFS + LC 102
-    public List<Integer> rightSideView(TreeNode root) {
+    public List<Integer> rightSideView_0_1(TreeNode root) {
 
         List<Integer> res = new ArrayList<>();
 
@@ -139,7 +188,7 @@ public class BinaryTreeRightSideView {
     // V1
     // IDEA: BFS: Two Queues
     // https://leetcode.com/problems/binary-tree-right-side-view/editorial/
-    public List<Integer> rightSideView_2(TreeNode root) {
+    public List<Integer> rightSideView_1(TreeNode root) {
         if (root == null) return new ArrayList<Integer>();
 
         ArrayDeque<TreeNode> nextLevel = new ArrayDeque() {{ offer(root); }};
@@ -174,7 +223,7 @@ public class BinaryTreeRightSideView {
     // V2
     // IDEA:  BFS: One Queue + Sentinel
     // https://leetcode.com/problems/binary-tree-right-side-view/editorial/
-    public List<Integer> rightSideView_3(TreeNode root) {
+    public List<Integer> rightSideView_2(TreeNode root) {
         if (root == null) return new ArrayList<Integer>();
 
         Queue<TreeNode> queue = new LinkedList(){{ offer(root); offer(null); }};
@@ -212,7 +261,7 @@ public class BinaryTreeRightSideView {
     // V3
     // IDEA: BFS: One Queue + Level Size Measurements
     // https://leetcode.com/problems/binary-tree-right-side-view/editorial/
-    public List<Integer> rightSideView_4(TreeNode root) {
+    public List<Integer> rightSideView_3(TreeNode root) {
         if (root == null) return new ArrayList<Integer>();
 
         ArrayDeque<TreeNode> queue = new ArrayDeque(){{ offer(root); }};
@@ -254,7 +303,7 @@ public class BinaryTreeRightSideView {
             helper(node.left, level + 1);
     }
 
-    public List<Integer> rightSideView_5(TreeNode root) {
+    public List<Integer> rightSideView_4(TreeNode root) {
         if (root == null) return rightside;
 
         helper(root, 0);
