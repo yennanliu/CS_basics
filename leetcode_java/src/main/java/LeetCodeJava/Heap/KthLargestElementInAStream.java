50d466cfa3032f4df7f4481bf919fc7ea3a1472c
yennanliu <f339339@gmail.com>
2025-02-23 15:52:34 +0800

    update 703 java, cheatsheet

--- leetcode_java/src/main/java/LeetCodeJava/Heap/KthLargestElementInAStream.java
+++ leetcode_java/src/main/java/LeetCodeJava/Heap/KthLargestElementInAStream.java
@@ -1,7 +1,65 @@
 package LeetCodeJava.Heap;
 
 // https://leetcode.com/problems/kth-largest-element-in-a-stream/
-
+/**
+ * 703. Kth Largest Element in a Stream
+ * Solved
+ * Easy
+ * Topics
+ * Companies
+ * You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+ *
+ * You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+ *
+ * Implement the KthLargest class:
+ *
+ * KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+ * int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+ *
+ *
+ * Example 1:
+ *
+ * Input:
+ * ["KthLargest", "add", "add", "add", "add", "add"]
+ * [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+ *
+ * Output: [null, 4, 5, 5, 8, 8]
+ *
+ * Explanation:
+ *
+ * KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+ * kthLargest.add(3); // return 4
+ * kthLargest.add(5); // return 5
+ * kthLargest.add(10); // return 5
+ * kthLargest.add(9); // return 8
+ * kthLargest.add(4); // return 8
+ *
+ * Example 2:
+ *
+ * Input:
+ * ["KthLargest", "add", "add", "add", "add"]
+ * [[4, [7, 7, 7, 7, 8, 3]], [2], [10], [9], [9]]
+ *
+ * Output: [null, 7, 7, 7, 8]
+ *
+ * Explanation:
+ *
+ * KthLargest kthLargest = new KthLargest(4, [7, 7, 7, 7, 8, 3]);
+ * kthLargest.add(2); // return 7
+ * kthLargest.add(10); // return 7
+ * kthLargest.add(9); // return 7
+ * kthLargest.add(9); // return 8
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= nums.length <= 104
+ * 1 <= k <= nums.length + 1
+ * -104 <= nums[i] <= 104
+ * -104 <= val <= 104
+ * At most 104 calls will be made to add.
+ *
+ */
 import java.util.PriorityQueue;
 
 public class KthLargestElementInAStream {
@@ -26,6 +84,14 @@ public class KthLargestElementInAStream {
         public KthLargest(int k, int[] nums) {
 
             this.k = k;
+            /**
+             * // NOTE !!! we use small PQ
+             * e.g. : small -> big (increasing order)
+             *
+             * -> so, we pop elements when PQ size  > k
+             * -> and we're sure that the top element is the `k th biggest element
+             * -> e.g. (k-big, k-1 big, k-2 big, ...., 2 big, 1 big)
+             */
             this.heap = new PriorityQueue<>();
             for (int x : nums){
                 //this.heap.add(x); // this one is OK as well
@@ -48,6 +114,44 @@ public class KthLargestElementInAStream {
             }
 
             return this.heap.peek();
+        }
+    }
+
+    // V0-1
+    // IDEA: SMALL PQ (fixed by gpt)
+    class KthLargest_0_1 {
+        int k;
+        int[] nums;
+        PriorityQueue<Integer> pq;
+
+        public KthLargest_0_1(int k, int[] nums) {
+            this.k = k;
+            this.nums = nums; // ???
+            /**
+             * // NOTE !!! we use small PQ
+             * e.g. : small -> big (increasing order)
+             *
+             * -> so, we pop elements when PQ size  > k
+             * -> and we're sure that the top element is the `k th biggest element
+             * -> e.g. (k-big, k-1 big, k-2 big, ...., 2 big, 1 big)
+             */
+            this.pq = new PriorityQueue<>();
+            // NOTE !!! we also need to add eleemnts to PQ
+            for (int x : nums) {
+                pq.add(x);
+            }
+        }
+
+        public int add(int val) {
+            this.pq.add(val);
+            // NOTE !!! need to remove elements when PQ size > k
+            while (this.pq.size() > k) {
+                this.pq.poll();
+            }
+            if (!this.pq.isEmpty()) {
+                return this.pq.peek();
+            }
+            return -1;
         }
     }
 
@@ -102,4 +206,5 @@ public class KthLargestElementInAStream {
 //        if (heap.size() > k) heap.poll();
 //        return heap.peek();
 //    }
+
 }
