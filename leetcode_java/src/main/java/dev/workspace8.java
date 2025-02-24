0280390330f953c8b082310e4dad3af709b43c5e
yennj12 <f339339@uber.com>
2025-02-24 11:11:42 +0800

    update 355, 627 java

--- leetcode_java/src/main/java/dev/workspace8.java
+++ leetcode_java/src/main/java/dev/workspace8.java
@@ -3239,4 +3239,42 @@ public class workspace8 {
         return  Math.sqrt(x + y);
     }
 
+    // LC 621
+    /**
+     * 621. Task Scheduler
+     *
+     * Given a char array representing tasks  CPU need to do.
+     *
+     * It contains capital letters A to Z where different letters
+     * represent different tasks. Tasks could be done without original order.
+     * Each task could be done in one interval. For each interval,
+     * CPU could finish one task or just be idle.
+     *
+     * However, there is a non-negative cooling interval n that
+     * means between two same tasks, there must be at least n intervals
+     * that CPU are doing different tasks or just be idle.
+     *
+     * You need to return the least number of intervals the CPU will take
+     * to finish all the given tasks.
+     *
+     *
+     *
+     * Example:
+     *
+     * Input: tasks = ["A","A","A","B","B","B"], n = 2
+     * Output: 8
+     * Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
+     *
+     *
+     * Note:
+     *
+     * The number of tasks is in the range [1, 10000].
+     * The integer n is in the range [0, 100].
+     *
+     *
+     */
+    public int leastInterval(char[] tasks, int n) {
+        return 0;
+    }
+
 }
