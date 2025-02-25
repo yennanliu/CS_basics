7b9d46ae43f1f29c67754620c1cbf3df02374c63
yennj12 <f339339@uber.com>
2025-02-25 11:28:07 +0800

    update 46 java

--- leetcode_java/src/main/java/LeetCodeJava/BackTrack/Permutations.java
+++ leetcode_java/src/main/java/LeetCodeJava/BackTrack/Permutations.java
@@ -1,7 +1,37 @@
 package LeetCodeJava.BackTrack;
 
 // https://leetcode.com/problems/permutations/
-
+/**
+ * 46. Permutations
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * Example 2:
+ *
+ * Input: nums = [0,1]
+ * Output: [[0,1],[1,0]]
+ * Example 3:
+ *
+ * Input: nums = [1]
+ * Output: [[1]]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * All the integers of nums are unique.
+ *
+ */
 import java.util.*;
 
 public class Permutations {
