de72ecaa3ec654bc1e59bdaf58fb5240bbb8419f
yennj12 <f339339@uber.com>
2025-02-25 10:44:25 +0800

    update 78 java

--- leetcode_java/src/main/java/LeetCodeJava/BackTrack/Subsets.java
+++ leetcode_java/src/main/java/LeetCodeJava/BackTrack/Subsets.java
@@ -1,7 +1,35 @@
 package LeetCodeJava.BackTrack;
 
 // https://leetcode.com/problems/subsets/
-
+/**
+ * 78. Subsets
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given an integer array nums of unique elements, return all possible subsets (the power set).
+ *
+ * The solution set must not contain duplicate subsets. Return the solution in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3]
+ * Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ * Example 2:
+ *
+ * Input: nums = [0]
+ * Output: [[],[0]]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ * All the numbers of nums are unique.
+ *
+ */
 import java.util.*;
 
 public class Subsets {
