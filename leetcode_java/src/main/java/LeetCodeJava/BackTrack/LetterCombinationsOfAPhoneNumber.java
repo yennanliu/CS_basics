68e4041cd1add20dfaae7ec5fbd3779ae344271e
yennj12 <f339339@uber.com>
2025-02-26 12:38:13 +0800

    update 17 java

--- leetcode_java/src/main/java/LeetCodeJava/BackTrack/LetterCombinationsOfAPhoneNumber.java
+++ leetcode_java/src/main/java/LeetCodeJava/BackTrack/LetterCombinationsOfAPhoneNumber.java
@@ -1,7 +1,39 @@
 package LeetCodeJava.BackTrack;
 
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-
+/**
+ * 17. Letter Combinations of a Phone Number
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+ *
+ * A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: digits = "23"
+ * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * Example 2:
+ *
+ * Input: digits = ""
+ * Output: []
+ * Example 3:
+ *
+ * Input: digits = "2"
+ * Output: ["a","b","c"]
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= digits.length <= 4
+ * digits[i] is a digit in the range ['2', '9'].
+ *
+ */
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -51,88 +83,57 @@ public class LetterCombinationsOfAPhoneNumber {
         }
     }
 
+    // V0-1
+    // IDEA: BACKTRACK (fixed by gpt)
+    public List<String> letterCombinations_0_1(String digits) {
+        // Mapping of digit to letters
+        HashMap<Character, String> letters = new HashMap<>();
+        letters.put('2', "abc");
+        letters.put('3', "def");
+        letters.put('4', "ghi");
+        letters.put('5', "jkl");
+        letters.put('6', "mno");
+        letters.put('7', "pqrs");
+        letters.put('8', "tuv");
+        letters.put('9', "wxyz");
 
-    // V0
-    // IDEA : BACKTRACK
-//    public List<String> letterCombinations(String digits) {
-//
-//        List<String> res = new ArrayList<>();
-//
-//        HashMap<String,String> map = new HashMap<>();
-//        // d =  {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-//        map.put("2", "abc");
-//        map.put("3", "def");
-//        map.put("4", "ghi");
-//        map.put("5", "jkl");
-//        map.put("6", "mno");
-//        map.put("7", "pqrs");
-//        map.put("8", "tuv");
-//        map.put("9", "wxyz");
-//
-//        if (digits.length()==0){
-//            return res;
-//        }
-//
-//        if (digits.length()==1){
-//            String _str = map.get(digits);
-//            char[] _strArray = _str.toCharArray();
-//            for (int x = 0; x < _strArray.length; x ++){
-//                res.add(String.valueOf(_strArray[x]));
-//            }
-//        }
-//
-//        String cur = "";
-//        _helper(digits, 0, map, cur, res);
-//        return res;
-//    }
-//
-//    private void _helper(String digits, int idx, HashMap<String, String> map, String cur, List<String> res){
-//
-//        if (cur.length() > digits.length()){
-//            return;
-//        }
-//
-//        if (cur.length() == digits.length()){
-//            if (!res.contains(cur)){
-//                StringBuilder sb = new StringBuilder();
-//                sb.append(cur);
-//                res.add(sb.toString());
-//            }
-//        }
-//
-//        map.get()
-//        String _curVal = map.get(digits[idx]).toCharArray();
-//        for (char x : map.get(digits[idx]).toCharArray()){
-//            char c = digitsArray[i];
-//            cur += c;
-//            _helper(digits, i+1, map, cur, res);
-//            // backtrack
-//            cur.substring(0, cur.length()-2);
-//        }
-//
-//    }
+        // Edge case: if digits is empty, return an empty list
+        if (digits == null || digits.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        // List to store the final result
+        List<String> letterRes = new ArrayList<>();
+
+        // Backtracking to find combinations
+        backtrack(digits, 0, new StringBuilder(), letterRes, letters);
+
+        return letterRes;
+    }
+
+    private void backtrack(String digits, int idx, StringBuilder current, List<String> letterRes,
+                           HashMap<Character, String> letters) {
+        // Base case: if the current combination length matches the digits length, add
+        // to result
+        if (current.length() == digits.length()) {
+            letterRes.add(current.toString());
+            return;
+        }
+
+        // Get the letters corresponding to the current digit
+        char digit = digits.charAt(idx);
+        String possibleLetters = letters.get(digit);
+
+        // Explore each possible letter for the current digit
+        for (char letter : possibleLetters.toCharArray()) {
+            current.append(letter); // Choose the letter
+            backtrack(digits, idx + 1, current, letterRes, letters); // Recurse for the next digit
+            current.deleteCharAt(current.length() - 1); // Backtrack, undo the choice
+        }
+    }
+
 
     // V1
-
-//    private Map<Character, String> digitToChar = Map.of(
-//            '2',
-//            "abc",
-//            '3',
-//            "def",
-//            '4',
-//            "ghi",
-//            '5',
-//            "jkl",
-//            '6',
-//            "mno",
-//            '7',
-//            "pqrs",
-//            '8',
-//            "tuv",
-//            '9',
-//            "wxyz"
-//    );
-
     private Map<Character, String> digitToChar = new HashMap<>();
 
     public List<String> letterCombinations_1(String digits) {
