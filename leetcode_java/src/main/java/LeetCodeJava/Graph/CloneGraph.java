72d0f5e5290dd0347b96d238cdf2970c3cd8a7b3
yennanliu <f339339@gmail.com>
2025-02-28 17:45:28 +0800

    update 133 java

--- leetcode_java/src/main/java/LeetCodeJava/Graph/CloneGraph.java
+++ leetcode_java/src/main/java/LeetCodeJava/Graph/CloneGraph.java
@@ -7,6 +7,66 @@ import java.util.*;
 // https://leetcode.com/problems/clone-graph/
 // NOTE !!! :  Node.val is unique for each node.
 /**
+ * 133. Clone Graph
+ * Solved
+ * Medium
+ * Topics
+ * Companies
+ * Given a reference of a node in a connected undirected graph.
+ *
+ * Return a deep copy (clone) of the graph.
+ *
+ * Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+ *
+ * class Node {
+ *     public int val;
+ *     public List<Node> neighbors;
+ * }
+ *
+ *
+ * Test case format:
+ *
+ * For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+ *
+ * An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+ *
+ * The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+ * Output: [[2,4],[1,3],[2,4],[1,3]]
+ * Explanation: There are 4 nodes in the graph.
+ * 1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+ * 2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+ * 3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+ * 4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+ * Example 2:
+ *
+ *
+ * Input: adjList = [[]]
+ * Output: [[]]
+ * Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
+ * Example 3:
+ *
+ * Input: adjList = []
+ * Output: []
+ * Explanation: This an empty graph, it does not have any nodes.
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the graph is in the range [0, 100].
+ * 1 <= Node.val <= 100
+ * Node.val is unique for each node.
+ * There are no repeated edges and no self-loops in the graph.
+ * The Graph is connected and all nodes can be visited starting from the given node.
+ *
+ */
+/**
  *  Constraints:
  *
  * - The number of nodes in the graph is in the range [0, 100].
@@ -17,27 +77,27 @@ import java.util.*;
  */
 
 
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 public class CloneGraph {
+
+    /*
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+    */
 
     // V0
     // IDEA : DFS
@@ -133,11 +193,62 @@ public class CloneGraph {
 //
 //    }
 
-    // V1
+    // V1-1
+    // https://neetcode.io/problems/clone-graph
+    // IDEA: DFS
+    public Node cloneGraph_1_1(Node node) {
+        Map<Node, Node> oldToNew = new HashMap<>();
+
+        return dfs(node, oldToNew);
+    }
+
+    private Node dfs(Node node, Map<Node, Node> oldToNew) {
+        if (node == null) {
+            return null;
+        }
+
+        if (oldToNew.containsKey(node)) {
+            return oldToNew.get(node);
+        }
+
+        Node copy = new Node(node.val);
+        oldToNew.put(node, copy);
+
+        for (Node nei : node.neighbors) {
+            copy.neighbors.add(dfs(nei, oldToNew));
+        }
+
+        return copy;
+    }
+
+    // V1-2
+    // https://neetcode.io/problems/clone-graph
+    // IDEA: BFS
+    public Node cloneGraph_1_2(Node node) {
+        if (node == null) return null;
+        Map<Node, Node> oldToNew = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        oldToNew.put(node, new Node(node.val));
+        q.add(node);
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+            for (Node nei : cur.neighbors) {
+                if (!oldToNew.containsKey(nei)) {
+                    oldToNew.put(nei, new Node(nei.val));
+                    q.add(nei);
+                }
+                oldToNew.get(cur).neighbors.add(oldToNew.get(nei));
+            }
+        }
+        return oldToNew.get(node);
+    }
+
+    // V2
     // IDEA : DFS
     // https://leetcode.com/problems/clone-graph/editorial/
     private HashMap <Node, Node> visited = new HashMap <> ();
-    public Node cloneGraph_1(Node node) {
+    public Node cloneGraph_2(Node node) {
         if (node == null) {
             return node;
         }
@@ -157,17 +268,17 @@ public class CloneGraph {
         // Iterate through the neighbors to generate their clones
         // and prepare a list of cloned neighbors to be added to the cloned node.
         for (Node neighbor: node.neighbors) {
-            cloneNode.neighbors.add(cloneGraph_1(neighbor));
+            cloneNode.neighbors.add(cloneGraph_2(neighbor));
         }
 
         // NOTE !!! after dfs, we return final result
         return cloneNode;
     }
 
-    // V2
+    // V3
     // IDEA : BFS
     // https://leetcode.com/problems/clone-graph/editorial/
-    public Node cloneGraph_2(Node node) {
+    public Node cloneGraph_3(Node node) {
         if (node == null) {
             return node;
         }
