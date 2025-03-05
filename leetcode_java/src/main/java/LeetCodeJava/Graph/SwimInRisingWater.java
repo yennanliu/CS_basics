ccbe6df7edb3706c7351943ef949a5de95cd2840
yennj12 <f339339@uber.com>
2025-03-05 10:00:13 +0800

    add 778 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/Graph/SwimInRisingWater.java
@@ -0,0 +1,214 @@
+package LeetCodeJava.Graph;
+
+// https://leetcode.com/problems/swim-in-rising-water/description/
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 778. Swim in Rising Water
+ * Hard
+ * Topics
+ * Companies
+ * Hint
+ * You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
+ *
+ * The rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+ *
+ * Return the least time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: grid = [[0,2],[1,3]]
+ * Output: 3
+ * Explanation:
+ * At time 0, you are in grid location (0, 0).
+ * You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+ * You cannot reach point (1, 1) until time 3.
+ * When the depth of water is 3, we can swim anywhere inside the grid.
+ * Example 2:
+ *
+ *
+ * Input: grid = [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+ * Output: 16
+ * Explanation: The final route is shown.
+ * We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+ *
+ *
+ * Constraints:
+ *
+ * n == grid.length
+ * n == grid[i].length
+ * 1 <= n <= 50
+ * 0 <= grid[i][j] < n2
+ * Each value grid[i][j] is unique.
+ *
+ *
+ */
+public class SwimInRisingWater {
+
+    // V0
+//    public int swimInWater(int[][] grid) {
+//
+//    }
+
+    // V1-1
+    // https://neetcode.io/problems/swim-in-rising-water
+    // IDEA: BRUTE FORCE
+    public int swimInWater_1_1(int[][] grid) {
+        int n = grid.length;
+        boolean[][] visit = new boolean[n][n];
+
+        return dfs(grid, visit, 0, 0, 0);
+    }
+
+    private int dfs(int[][] grid, boolean[][] visit,
+                    int r, int c, int t) {
+        int n = grid.length;
+        if (r < 0 || c < 0 || r >= n || c >= n || visit[r][c]) {
+            return 1000000;
+        }
+        if (r == n - 1 && c == n - 1) {
+            return Math.max(t, grid[r][c]);
+        }
+        visit[r][c] = true;
+        t = Math.max(t, grid[r][c]);
+        int res = Math.min(Math.min(dfs(grid, visit, r + 1, c, t),
+                        dfs(grid, visit, r - 1, c, t)),
+                Math.min(dfs(grid, visit, r, c + 1, t),
+                        dfs(grid, visit, r, c - 1, t)));
+        visit[r][c] = false;
+        return res;
+    }
+
+    // V1-2
+    // https://neetcode.io/problems/swim-in-rising-water
+    // IDEA: DFS
+    public int swimInWater_1_2(int[][] grid) {
+        int n = grid.length;
+        boolean[][] visit = new boolean[n][n];
+        int minH = grid[0][0], maxH = grid[0][0];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                maxH = Math.max(maxH, grid[row][col]);
+                minH = Math.min(minH, grid[row][col]);
+            }
+        }
+
+        for (int t = minH; t < maxH; t++) {
+            if (dfs_1_1(grid, visit, 0, 0, t)) {
+                return t;
+            }
+            for (int r = 0; r < n; r++) {
+                Arrays.fill(visit[r], false);
+            }
+        }
+        return maxH;
+    }
+
+    private boolean dfs_1_1(int[][] grid, boolean[][] visit, int r, int c, int t) {
+        if (r < 0 || c < 0 || r >= grid.length ||
+                c >= grid.length || visit[r][c] || grid[r][c] > t) {
+            return false;
+        }
+        if (r == grid.length - 1 && c == grid.length - 1) {
+            return true;
+        }
+        visit[r][c] = true;
+        return dfs_1_1(grid, visit, r + 1, c, t) ||
+                dfs_1_1(grid, visit, r - 1, c, t) ||
+                dfs_1_1(grid, visit, r, c + 1, t) ||
+                dfs_1_1(grid, visit, r, c - 1, t);
+    }
+
+
+    // V1-3
+    // https://neetcode.io/problems/swim-in-rising-water
+    // IDEA: Binary Search + DFS
+    public int swimInWater_1_3(int[][] grid) {
+        int n = grid.length;
+        boolean[][] visit = new boolean[n][n];
+        int minH = grid[0][0], maxH = grid[0][0];
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                maxH = Math.max(maxH, grid[row][col]);
+                minH = Math.min(minH, grid[row][col]);
+            }
+        }
+
+        int l = minH, r = maxH;
+        while (l < r) {
+            int m = (l + r) >> 1;
+            if (dfs_1_3(grid, visit, 0, 0, m)) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+            for (int row = 0; row < n; row++) {
+                Arrays.fill(visit[row], false);
+            }
+        }
+        return r;
+    }
+
+    private boolean dfs_1_3(int[][] grid, boolean[][] visit, int r, int c, int t) {
+        if (r < 0 || c < 0 || r >= grid.length ||
+                c >= grid.length || visit[r][c] || grid[r][c] > t) {
+            return false;
+        }
+        if (r == grid.length - 1 && c == grid.length - 1) {
+            return true;
+        }
+        visit[r][c] = true;
+        return dfs_1_3(grid, visit, r + 1, c, t) ||
+                dfs_1_3(grid, visit, r - 1, c, t) ||
+                dfs_1_3(grid, visit, r, c + 1, t) ||
+                dfs_1_3(grid, visit, r, c - 1, t);
+    }
+
+
+    // V1-4
+    // https://neetcode.io/problems/swim-in-rising-water
+    // IDEA:  Dijkstra's Algorithm
+    public int swimInWater_1_4(int[][] grid) {
+        int N = grid.length;
+        boolean[][] visit = new boolean[N][N];
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+                Comparator.comparingInt(a -> a[0])
+        );
+        int[][] directions = {
+                {0, 1}, {0, -1}, {1, 0}, {-1, 0}
+        };
+
+        minHeap.offer(new int[]{grid[0][0], 0, 0});
+        visit[0][0] = true;
+
+        while (!minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int t = curr[0], r = curr[1], c = curr[2];
+            if (r == N - 1 && c == N - 1) {
+                return t;
+            }
+            for (int[] dir : directions) {
+                int neiR = r + dir[0], neiC = c + dir[1];
+                if (neiR >= 0 && neiC >= 0 && neiR < N &&
+                        neiC < N && !visit[neiR][neiC]) {
+                    visit[neiR][neiC] = true;
+                    minHeap.offer(new int[]{
+                            Math.max(t, grid[neiR][neiC]),
+                            neiR, neiC
+                    });
+                }
+            }
+        }
+        return N * N;
+    }
+
+
+    // V2
+
+}
