5791390eb920194d46f12fb053af5e9eab779124
yennj12 <f339339@uber.com>
2025-03-11 10:05:58 +0800

    add 329 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/DynamicProgramming/LongestIncreasingPathInAMatrix.java
@@ -0,0 +1,175 @@
+package LeetCodeJava.DynamicProgramming;
+
+// https://leetcode.com/problems/longest-increasing-path-in-a-matrix/description/
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 329. Longest Increasing Path in a Matrix
+ * Hard
+ * Topics
+ * Companies
+ * Given an m x n integers matrix, return the length of the longest increasing path in matrix.
+ *
+ * From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+ * Output: 4
+ * Explanation: The longest increasing path is [1, 2, 6, 9].
+ * Example 2:
+ *
+ *
+ * Input: matrix = [[3,4,5],[3,2,6],[2,2,1]]
+ * Output: 4
+ * Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+ * Example 3:
+ *
+ * Input: matrix = [[1]]
+ * Output: 1
+ *
+ *
+ * Constraints:
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 200
+ * 0 <= matrix[i][j] <= 231 - 1
+ *
+ *
+ */
+public class LongestIncreasingPathInAMatrix {
+
+    // V0
+//    public int longestIncreasingPath(int[][] matrix) {
+//
+//    }
+
+    // V1-1
+    // https://neetcode.io/problems/longest-increasing-path-in-matrix
+    // IDEA: Recursion
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    private int dfs(int[][] matrix, int r, int c, int prevVal) {
+        int ROWS = matrix.length, COLS = matrix[0].length;
+        if (r < 0 || r >= ROWS || c < 0 ||
+                c >= COLS || matrix[r][c] <= prevVal) {
+            return 0;
+        }
+
+        int res = 1;
+        for (int[] d : directions) {
+            res = Math.max(res, 1 + dfs(matrix, r + d[0],
+                    c + d[1], matrix[r][c]));
+        }
+        return res;
+    }
+
+    public int longestIncreasingPath_1_1(int[][] matrix) {
+        int ROWS = matrix.length, COLS = matrix[0].length;
+        int LIP = 0;
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                LIP = Math.max(LIP, dfs(matrix, r, c, Integer.MIN_VALUE));
+            }
+        }
+        return LIP;
+    }
+
+    // V1-2
+    // https://neetcode.io/problems/longest-increasing-path-in-matrix
+    // IDEA: Dynamic Programming (Top-Down)
+    //int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int[][] dp;
+
+    private int dfs_1_2(int[][] matrix, int r, int c, int prevVal) {
+        int ROWS = matrix.length, COLS = matrix[0].length;
+        if (r < 0 || r >= ROWS || c < 0 ||
+                c >= COLS || matrix[r][c] <= prevVal) {
+            return 0;
+        }
+        if (dp[r][c] != -1) return dp[r][c];
+
+        int res = 1;
+        for (int[] d : directions) {
+            res = Math.max(res, 1 + dfs_1_2(matrix, r + d[0],
+                    c + d[1], matrix[r][c]));
+        }
+        return dp[r][c] = res;
+    }
+
+    public int longestIncreasingPath_1_2(int[][] matrix) {
+        int ROWS = matrix.length, COLS = matrix[0].length;
+        int LIP = 0;
+        dp = new int[ROWS][COLS];
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                LIP = Math.max(LIP, dfs_1_2(matrix, r, c, Integer.MIN_VALUE));
+            }
+        }
+        return LIP;
+    }
+
+    // V1-3
+    // https://neetcode.io/problems/longest-increasing-path-in-matrix
+    // IDEA:  Topological Sort (Kahn's Algorithm)
+    public int longestIncreasingPath_1_3(int[][] matrix) {
+        int ROWS = matrix.length, COLS = matrix[0].length;
+        int[][] indegree = new int[ROWS][COLS];
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for (int r = 0; r < ROWS; ++r) {
+            for (int c = 0; c < COLS; ++c) {
+                for (int[] d : directions) {
+                    int nr = r + d[0], nc = c + d[1];
+                    if (nr >= 0 && nr < ROWS && nc >= 0 &&
+                            nc < COLS && matrix[nr][nc] < matrix[r][c]) {
+                        indegree[r][c]++;
+                    }
+                }
+            }
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        for (int r = 0; r < ROWS; ++r) {
+            for (int c = 0; c < COLS; ++c) {
+                if (indegree[r][c] == 0) {
+                    q.offer(new int[]{r, c});
+                }
+            }
+        }
+
+        int LIS = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                int[] node = q.poll();
+                int r = node[0], c = node[1];
+                for (int[] d : directions) {
+                    int nr = r + d[0], nc = c + d[1];
+                    if (nr >= 0 && nr < ROWS && nc >= 0 &&
+                            nc < COLS && matrix[nr][nc] > matrix[r][c]) {
+                        if (--indegree[nr][nc] == 0) {
+                            q.offer(new int[]{nr, nc});
+                        }
+                    }
+                }
+            }
+            LIS++;
+        }
+        return LIS;
+    }
+
+    // V2
+
+}
