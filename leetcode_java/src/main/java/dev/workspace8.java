de1e50e23af2991ee57543a793928ea53615dc7c
yennj12 <f339339@uber.com>
2025-03-11 10:46:26 +0800

    add 312 java

--- leetcode_java/src/main/java/dev/workspace8.java
+++ leetcode_java/src/main/java/dev/workspace8.java
@@ -6980,4 +6980,41 @@ class Node {
       return 0;
   }
 
+  /**
+   *  Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+   *
+   * You have the following 3 operations permitted on a word:
+   *
+   * Insert a character
+   * Delete a character
+   * Replace a character
+   * Example 1:
+   *
+   * Input: word1 = "horse", word2 = "ros"
+   * Output: 3
+   * Explanation:
+   * horse -> rorse (replace 'h' with 'r')
+   * rorse -> rose (remove 'r')
+   * rose -> ros (remove 'e')
+   * Example 2:
+   *
+   * Input: word1 = "intention", word2 = "execution"
+   * Output: 5
+   * Explanation:
+   * intention -> inention (remove 't')
+   * inention -> enention (replace 'i' with 'e')
+   * enention -> exention (replace 'n' with 'x')
+   * exention -> exection (replace 'n' with 'c')
+   * exection -> execution (insert 'u')
+   * Difficulty:
+   *
+   *
+   *
+   */
+  // LC 72
+  // 10.33 - 10.43 AM
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+
 }
