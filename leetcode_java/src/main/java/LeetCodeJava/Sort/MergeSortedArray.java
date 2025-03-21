c9a9e65fdcfda5b48b77cc97b2b64ecbdf981a3e
yennj12 <f339339@uber.com>
2025-03-21 10:55:47 +0800

    update 88 java

--- leetcode_java/src/main/java/LeetCodeJava/Sort/MergeSortedArray.java
+++ leetcode_java/src/main/java/LeetCodeJava/Sort/MergeSortedArray.java
@@ -1,16 +1,87 @@
 package LeetCodeJava.Sort;
 
-// https://leetcode.com/problems/merge-sorted-array/
-
 import java.util.Arrays;
 
+// https://leetcode.com/problems/merge-sorted-array/
+/**
+ * 88. Merge Sorted Array
+ * Solved
+ * Easy
+ * Topics
+ * Companies
+ * Hint
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ *
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ *
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+ * The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+ * Example 2:
+ *
+ * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ * Output: [1]
+ * Explanation: The arrays we are merging are [1] and [].
+ * The result of the merge is [1].
+ * Example 3:
+ *
+ * Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+ * Output: [1]
+ * Explanation: The arrays we are merging are [] and [1].
+ * The result of the merge is [1].
+ * Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ *
+ *
+ * Constraints:
+ *
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -109 <= nums1[i], nums2[j] <= 109
+ *
+ *
+ * Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+ *
+ *
+ */
 public class MergeSortedArray {
 
     // V0
+    // IDEA: ARRAY OP + SORT
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        /**
+         *   NOTE !!! we loop from 0  to `n`
+         */
+        for (int i = 0; i < n; i++) {
+            /**
+             *   NOTE !!! we assign nums2[i] val to `i+m` idx at nums1 (nums1[i + m] )
+             */
+            nums1[i + m] = nums2[i];
+        }
+        // NOTE: we sort merged array
+        Arrays.sort(nums1);
+    }
 
     // V1
     // IDEA : MERGE AND SORT
     // https://leetcode.com/problems/merge-sorted-array/editorial/
+    /**
+     *   NOTE !!!
+     *
+     *  two integers m and n,
+     *  representing the number of elements in nums1 and nums2 respectively.
+     *
+     *  -> m : nums1 element cnt
+     *  -> n : nums2 element cnt
+     */
     public void merge_2(int[] nums1, int m, int[] nums2, int n) {
         for (int i = 0; i < n; i++) {
             nums1[i + m] = nums2[i];
