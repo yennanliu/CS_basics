bc4d2ca5b1106d07bdef12e049afa7849c672baf
yennanliu <f339339@gmail.com>
2025-03-23 13:32:09 +0800

    update 424 java

--- leetcode_java/src/main/java/LeetCodeJava/HashTable/LongestRepeatingCharacterReplacement.java
+++ leetcode_java/src/main/java/LeetCodeJava/HashTable/LongestRepeatingCharacterReplacement.java
@@ -211,48 +211,80 @@ public class LongestRepeatingCharacterReplacement {
         return max;
     }
 
+    // V1-1
+    // https://neetcode.io/problems/longest-repeating-substring-with-replacement
+    // IDEA: BRUTE FORCE
+    public int characterReplacement_1_1(String s, int k) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            HashMap<Character, Integer> count = new HashMap<>();
+            int maxf = 0;
+            for (int j = i; j < s.length(); j++) {
+                count.put(s.charAt(j), count.getOrDefault(s.charAt(j), 0) + 1);
+                maxf = Math.max(maxf, count.get(s.charAt(j)));
+                if ((j - i + 1) - maxf <= k) {
+                    res = Math.max(res, j - i + 1);
+                }
+            }
+        }
+        return res;
+    }
 
-//    // V0
-//    // IDEA : SLIDING WINDOW
-//    public int characterReplacement(String s, int k) {
-//
-//        if (s.length() == 0 || s.equals(null)){
-//            return 0;
-//        }
-//
-//        int ans = 0;
-//        char[] s_char = s.toCharArray();
-//
-//        for (int i = 0; i < s_char.length-1; i++){
-//
-//            Set set = new HashSet<>();
-//            int j = i;
-//            int tmp_k = k;
-//
-//            while (tmp_k >= 0 && j < s_char.length){
-//                String cur = String.valueOf(s_char[j]);
-//                if (set.isEmpty()){
-//                    set.add(cur);
-//                }else if(set.contains(cur)){
-//                    j += 1;
-//                    ans = Math.max(ans, j - i + 1);
-//                }else {
-//                    if (k > 0){
-//                        j += 1;
-//                        k -= 1;
-//                        ans = Math.max(ans, j - i + 1);
-//                    }else{
-//                        ans = Math.max(ans, j - i + 1);
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//
-//        return ans;
-//    }
 
-    // V1
+    // V1-2
+    // https://neetcode.io/problems/longest-repeating-substring-with-replacement
+    // IDEA: SLIDING WINDOW
+    public int characterReplacement_1_2(String s, int k) {
+        int res = 0;
+        HashSet<Character> charSet = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            charSet.add(c);
+        }
+
+        for (char c : charSet) {
+            int count = 0, l = 0;
+            for (int r = 0; r < s.length(); r++) {
+                if (s.charAt(r) == c) {
+                    count++;
+                }
+
+                while ((r - l + 1) - count > k) {
+                    if (s.charAt(l) == c) {
+                        count--;
+                    }
+                    l++;
+                }
+
+                res = Math.max(res, r - l + 1);
+            }
+        }
+        return res;
+    }
+
+
+    // V1-3
+    // https://neetcode.io/problems/longest-repeating-substring-with-replacement
+    // IDEA: Sliding Window (Optimal)
+    public int characterReplacement_1_3(String s, int k) {
+        HashMap<Character, Integer> count = new HashMap<>();
+        int res = 0;
+
+        int l = 0, maxf = 0;
+        for (int r = 0; r < s.length(); r++) {
+            count.put(s.charAt(r), count.getOrDefault(s.charAt(r), 0) + 1);
+            maxf = Math.max(maxf, count.get(s.charAt(r)));
+
+            while ((r - l + 1) - maxf > k) {
+                count.put(s.charAt(l), count.get(s.charAt(l)) - 1);
+                l++;
+            }
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+
+    // V2
     // IDEA : Sliding Window + Binary Search
     // https://leetcode.com/problems/longest-repeating-character-replacement/editorial/
     public int characterReplacement_2(String s, int k) {
@@ -316,10 +348,10 @@ public class LongestRepeatingCharacterReplacement {
     }
 
 
-    // V2
+    // V3
     // IDEA : Sliding Window (Slow)
     // https://leetcode.com/problems/longest-repeating-character-replacement/editorial/
-    public int characterReplacement_4(String s, int k) {
+    public int characterReplacement_3(String s, int k) {
         HashSet<Character> allLetters = new HashSet();
 
         // collect all unique letters
@@ -358,10 +390,10 @@ public class LongestRepeatingCharacterReplacement {
     }
 
 
-    // V3
+    // V4
     // IDEA : Sliding Window (Fast)
     // https://leetcode.com/problems/longest-repeating-character-replacement/editorial/
-    public int characterReplacement_5(String s, int k) {
+    public int characterReplacement_4(String s, int k) {
         int start = 0;
         int[] frequencyMap = new int[26];
         int maxFrequency = 0;
