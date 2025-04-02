75b2bab5abe8278b928afc42e9df0ed38b99d517
yennj12 <f339339@uber.com>
2025-04-02 13:14:11 +0800

    update 895 java

--- leetcode_java/src/main/java/LeetCodeJava/Stack/MaximumFrequencyStack.java
+++ leetcode_java/src/main/java/LeetCodeJava/Stack/MaximumFrequencyStack.java
@@ -59,169 +59,212 @@ public class MaximumFrequencyStack {
      * int param_2 = obj.pop();
      */
     // V0
-//    class FreqStack {
-//
-//        public FreqStack() {
-//
-//        }
-//
-//        public void push(int val) {
-//
-//        }
-//
-//        public int pop() {
-//
-//        }
-//    }
-
-    // V0-1
-    class FreqStack_0_1 {
-
-        // attr
-        // map : {val : cnt} -> frequency map
-        private Map<Integer, Integer> map;
-        // stack of values
-        private Map<Integer, Stack<Integer>> freqMap;
-        private int maxFreq;
-
-        public FreqStack_0_1() {
-            this.map = new HashMap<>();
-            this.freqMap = new HashMap<>();
-            this.maxFreq = 0;
-        }
-
-        public void push(int val) {
-            // Update the frequency count of the value
-            int freq = map.getOrDefault(val, 0) + 1;
-            map.put(val, freq);
-
-            // Update maxFreq
-            maxFreq = Math.max(maxFreq, freq);
-
-            // Push the value onto the appropriate stack for its frequency
-            freqMap.putIfAbsent(freq, new Stack<>());
-            freqMap.get(freq).push(val);
-        }
-
-        public int pop() {
-            if (maxFreq == 0) {
-                throw new RuntimeException("Empty stack");
-            }
-
-            // Get the most frequent element
-            Stack<Integer> stack = freqMap.get(maxFreq);
-            int val = stack.pop();
-
-            // Decrease frequency of that value
-            map.put(val, map.get(val) - 1);
-
-            // If no more elements at the current maxFreq, decrease maxFreq
-            if (stack.isEmpty()) {
-                maxFreq--;
-            }
-
-            return val;
-        }
-    }
-
-    // V1
+    // IDEA: cnt map + freq map (map<cnt: Stack[val_1, val_2, .....] )) + `group val cnt` (fixed by gpt)
     // https://www.youtube.com/watch?v=Z6idIicFDOE
     // https://github.com/yennanliu/CS_basics/blob/master/doc/pic/lc_895.png
+    class FreqStack {
+        // To keep track of the current max frequency
+        int maxCnt;
 
-    // V2
-    // https://leetcode.com/problems/maximum-frequency-stack/solutions/1086543/js-python-java-c-frequency-map-stack-sol-o8kz/
-    class FreqStack_2 {
-        HashMap<Integer, Integer> fmap;
-        List<Stack<Integer>> stack;
+        // Map to track the frequency of each element
+        Map<Integer, Integer> cntMap;
 
-        public FreqStack_2() {
-            fmap = new HashMap();
-            stack = new ArrayList();
-            stack.add(new Stack());
+        // Map to track the elements for each frequency count
+        Map<Integer, Stack<Integer>> freqMap;
+
+        public FreqStack() {
+            this.maxCnt = 0; // Initially no elements, so max frequency is 0
+            this.cntMap = new HashMap<>();
+            this.freqMap = new HashMap<>();
         }
 
-        public void push(int x) {
-            int freq = fmap.getOrDefault(x, 0) + 1;
-            fmap.put(x, freq);
-            if (freq == stack.size())
-                stack.add(new Stack());
-            stack.get(freq).add(x);
-        }
-
-        public int pop() {
-            Stack<Integer> top = stack.get(stack.size() - 1);
-            int x = top.pop();
-            if (top.size() == 0)
-                stack.remove(stack.size() - 1);
-            fmap.put(x, fmap.get(x) - 1);
-            return x;
-        }
-    }
-
-    // V3
-    // https://leetcode.com/problems/maximum-frequency-stack/solutions/1861901/java-stacks-explained-by-prashant404-cnz7/
-    class FreqStack_3 {
-        private final Map<Integer, Deque<Integer>> freqToStack = new HashMap<>();
-        private final Map<Integer, Integer> numToMaxFreq = new HashMap<>();
-        private int maxFreq;
-
-        /**
-         * Constructs an empty frequency stack
-         */
-        public FreqStack_3() {
-        }
-
-        /**
-         * Pushes an integer val onto the top of the stack.
-         *
-         * @param val integer to be pushed
-         */
         public void push(int val) {
-            Integer freq = numToMaxFreq.compute(val, (k, v) -> v == null ? 1 : ++v);
-            freqToStack.computeIfAbsent(freq, k -> new ArrayDeque<>())
-                    .push(val);
-            maxFreq = Math.max(maxFreq, freq);
-        }
+            // Update the count of the value in cntMap
+            int count = cntMap.getOrDefault(val, 0) + 1;
+            cntMap.put(val, count);
 
-        /**
-         * Removes and returns the most frequent element in the stack. If there is a tie for the most frequent element,
-         * the element closest to the stack's top is removed and returned.
-         *
-         * @return The most frequent element in the stack
-         */
-        public int pop() {
-            Deque<Integer> stack = freqToStack.get(maxFreq);
-            Integer top = stack.pop();
-            numToMaxFreq.compute(top, (k, v) -> --v);
-            if (stack.isEmpty())
-                maxFreq--;
-            return top;
-        }
-    }
+            // Update the freqMap to push the value onto the stack for the given frequency
+            freqMap.putIfAbsent(count, new Stack<>());
+            freqMap.get(count).push(val);
 
-    // V4
-    // https://leetcode.com/problems/maximum-frequency-stack/solutions/5172171/stack-implementation-problem-by-dixon_n-dq2i/
-    class FreqStack_4 {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        HashMap<Integer, Stack<Integer>> m = new HashMap<>();
-        int maxfreq = 0;
-
-        public void push(int x) {
-            int f = freq.getOrDefault(x, 0) + 1;
-            freq.put(x, f);
-            maxfreq = Math.max(maxfreq, f);
-            if (!m.containsKey(f))
-                m.put(f, new Stack<Integer>());
-            m.get(f).push(x);// for the frequency f the elements are pushed into the stack. ie. For every Frequency Its own Key Exist
+            // Update maxCnt to keep track of the maximum frequency
+            maxCnt = Math.max(maxCnt, count);
         }
 
         public int pop() {
-            int x = m.get(maxfreq).pop();
-            freq.put(x, maxfreq - 1);
-            if (m.get(maxfreq).size() == 0)
-                maxfreq--;
-            return x;
+            if (maxCnt == 0) {
+                throw new RuntimeException("no element to pop");
+            }
+
+            // Get the most frequent stack
+            Stack<Integer> st = freqMap.get(maxCnt);
+
+            // Pop the element
+            int val = st.pop();
+
+            // Update the count of the element in cntMap
+            cntMap.put(val, cntMap.get(val) - 1);
+
+            // If the stack for maxCnt is empty, reduce maxCnt
+            if (st.isEmpty()) {
+                freqMap.remove(maxCnt);
+                maxCnt--;
+            }
+
+            // Return the popped element
+            return val;
+        }
+
+        // V0-1
+        // IDEA: cnt map + freq map (map<cnt: Stack[val_1, val_2, .....] )) + `group val cnt`
+        class FreqStack_0_1 {
+
+            // attr
+            // map : {val : cnt} -> frequency map
+            private Map<Integer, Integer> map;
+            // stack of values
+            private Map<Integer, Stack<Integer>> freqMap;
+            private int maxFreq;
+
+            public FreqStack_0_1() {
+                this.map = new HashMap<>();
+                this.freqMap = new HashMap<>();
+                this.maxFreq = 0;
+            }
+
+            public void push(int val) {
+                // Update the frequency count of the value
+                int freq = map.getOrDefault(val, 0) + 1;
+                map.put(val, freq);
+
+                // Update maxFreq
+                maxFreq = Math.max(maxFreq, freq);
+
+                // Push the value onto the appropriate stack for its frequency
+                freqMap.putIfAbsent(freq, new Stack<>());
+                freqMap.get(freq).push(val);
+            }
+
+            public int pop() {
+                if (maxFreq == 0) {
+                    throw new RuntimeException("Empty stack");
+                }
+
+                // Get the most frequent element
+                Stack<Integer> stack = freqMap.get(maxFreq);
+                int val = stack.pop();
+
+                // Decrease frequency of that value
+                map.put(val, map.get(val) - 1);
+
+                // If no more elements at the current maxFreq, decrease maxFreq
+                if (stack.isEmpty()) {
+                    maxFreq--;
+                }
+
+                return val;
+            }
+        }
+
+        // V1
+        // https://www.youtube.com/watch?v=Z6idIicFDOE
+        // https://github.com/yennanliu/CS_basics/blob/master/doc/pic/lc_895.png
+
+        // V2
+        // https://leetcode.com/problems/maximum-frequency-stack/solutions/1086543/js-python-java-c-frequency-map-stack-sol-o8kz/
+        class FreqStack_2 {
+            HashMap<Integer, Integer> fmap;
+            List<Stack<Integer>> stack;
+
+            public FreqStack_2() {
+                fmap = new HashMap();
+                stack = new ArrayList();
+                stack.add(new Stack());
+            }
+
+            public void push(int x) {
+                int freq = fmap.getOrDefault(x, 0) + 1;
+                fmap.put(x, freq);
+                if (freq == stack.size())
+                    stack.add(new Stack());
+                stack.get(freq).add(x);
+            }
+
+            public int pop() {
+                Stack<Integer> top = stack.get(stack.size() - 1);
+                int x = top.pop();
+                if (top.size() == 0)
+                    stack.remove(stack.size() - 1);
+                fmap.put(x, fmap.get(x) - 1);
+                return x;
+            }
+        }
+
+        // V3
+        // https://leetcode.com/problems/maximum-frequency-stack/solutions/1861901/java-stacks-explained-by-prashant404-cnz7/
+        class FreqStack_3 {
+            private final Map<Integer, Deque<Integer>> freqToStack = new HashMap<>();
+            private final Map<Integer, Integer> numToMaxFreq = new HashMap<>();
+            private int maxFreq;
+
+            /**
+             * Constructs an empty frequency stack
+             */
+            public FreqStack_3() {
+            }
+
+            /**
+             * Pushes an integer val onto the top of the stack.
+             *
+             * @param val integer to be pushed
+             */
+            public void push(int val) {
+                Integer freq = numToMaxFreq.compute(val, (k, v) -> v == null ? 1 : ++v);
+                freqToStack.computeIfAbsent(freq, k -> new ArrayDeque<>())
+                        .push(val);
+                maxFreq = Math.max(maxFreq, freq);
+            }
+
+            /**
+             * Removes and returns the most frequent element in the stack. If there is a tie for the most frequent element,
+             * the element closest to the stack's top is removed and returned.
+             *
+             * @return The most frequent element in the stack
+             */
+            public int pop() {
+                Deque<Integer> stack = freqToStack.get(maxFreq);
+                Integer top = stack.pop();
+                numToMaxFreq.compute(top, (k, v) -> --v);
+                if (stack.isEmpty())
+                    maxFreq--;
+                return top;
+            }
+        }
+
+        // V4
+        // https://leetcode.com/problems/maximum-frequency-stack/solutions/5172171/stack-implementation-problem-by-dixon_n-dq2i/
+        class FreqStack_4 {
+            HashMap<Integer, Integer> freq = new HashMap<>();
+            HashMap<Integer, Stack<Integer>> m = new HashMap<>();
+            int maxfreq = 0;
+
+            public void push(int x) {
+                int f = freq.getOrDefault(x, 0) + 1;
+                freq.put(x, f);
+                maxfreq = Math.max(maxfreq, f);
+                if (!m.containsKey(f))
+                    m.put(f, new Stack<Integer>());
+                m.get(f).push(x);// for the frequency f the elements are pushed into the stack. ie. For every Frequency Its own Key Exist
+            }
+
+            public int pop() {
+                int x = m.get(maxfreq).pop();
+                freq.put(x, maxfreq - 1);
+                if (m.get(maxfreq).size() == 0)
+                    maxfreq--;
+                return x;
+            }
         }
     }
-
+    
 }
