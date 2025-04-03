758789be1f2a94290d6771cb6e656cac467a46cc
yennanliu <f339339@gmail.com>
2025-04-03 18:04:41 +0800

    update 004 java

--- leetcode_java/src/main/java/LeetCodeJava/Array/MedianOfTwoSortedArrays.java
+++ leetcode_java/src/main/java/LeetCodeJava/Array/MedianOfTwoSortedArrays.java
@@ -1,6 +1,9 @@
 package LeetCodeJava.Array;
 
 // https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+
+import java.util.Arrays;
+
 /**
  * 4. Median of Two Sorted Arrays
  * Solved
@@ -42,16 +45,140 @@ public class MedianOfTwoSortedArrays {
 //
 //    }
 
-    // V1
-    // https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/4070500/99journey-from-brute-force-to-most-optim-z3k8/
+    // V1-1
+    // https://neetcode.io/problems/median-of-two-sorted-arrays
+    // IDEA: BRUTE FORCE
+    public double findMedianSortedArrays_1_1(int[] nums1, int[] nums2) {
+        int len1 = nums1.length, len2 = nums2.length;
+        int[] merged = new int[len1 + len2];
+        System.arraycopy(nums1, 0, merged, 0, len1);
+        System.arraycopy(nums2, 0, merged, len1, len2);
+        Arrays.sort(merged);
+
+        int totalLen = merged.length;
+        if (totalLen % 2 == 0) {
+            return (merged[totalLen / 2 - 1] + merged[totalLen / 2]) / 2.0;
+        } else {
+            return merged[totalLen / 2];
+        }
+    }
+
+    // V1-2
+    // https://neetcode.io/problems/median-of-two-sorted-arrays
+    // IDEA: 2 POINTERS
+    public double findMedianSortedArrays_1_2(int[] nums1, int[] nums2) {
+        int len1 = nums1.length, len2 = nums2.length;
+        int i = 0, j = 0;
+        int median1 = 0, median2 = 0;
+
+        for (int count = 0; count < (len1 + len2) / 2 + 1; count++) {
+            median2 = median1;
+            if (i < len1 && j < len2) {
+                if (nums1[i] > nums2[j]) {
+                    median1 = nums2[j];
+                    j++;
+                } else {
+                    median1 = nums1[i];
+                    i++;
+                }
+            } else if (i < len1) {
+                median1 = nums1[i];
+                i++;
+            } else {
+                median1 = nums2[j];
+                j++;
+            }
+        }
+
+        if ((len1 + len2) % 2 == 1) {
+            return (double) median1;
+        } else {
+            return (median1 + median2) / 2.0;
+        }
+    }
+
+    // V1-3
+    // https://neetcode.io/problems/median-of-two-sorted-arrays
+    // IDEA: Binary Search
+    public double findMedianSortedArrays_1_3(int[] nums1, int[] nums2) {
+        int left = (nums1.length + nums2.length + 1) / 2;
+        int right = (nums1.length + nums2.length + 2) / 2;
+        return (getKth(nums1, nums1.length, nums2, nums2.length, left, 0, 0) +
+                getKth(nums1, nums1.length, nums2, nums2.length, right, 0, 0)) / 2.0;
+    }
+
+    public int getKth(int[] a, int m, int[] b, int n, int k, int aStart, int bStart) {
+        if (m > n) {
+            return getKth(b, n, a, m, k, bStart, aStart);
+        }
+        if (m == 0) {
+            return b[bStart + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(a[aStart], b[bStart]);
+        }
+
+        int i = Math.min(m, k / 2);
+        int j = Math.min(n, k / 2);
+
+        if (a[aStart + i - 1] > b[bStart + j - 1]) {
+            return getKth(a, m, b, n - j, k - j, aStart, bStart + j);
+        } else {
+            return getKth(a, m - i, b, n, k - i, aStart + i, bStart);
+        }
+    }
+
+    // V1-4
+    // https://neetcode.io/problems/median-of-two-sorted-arrays
+    // IDEA: Binary Search (Optimal)
+    public double findMedianSortedArrays_1_4(int[] nums1, int[] nums2) {
+        int[] A = nums1;
+        int[] B = nums2;
+        int total = A.length + B.length;
+        int half = (total + 1) / 2;
+
+        if (B.length < A.length) {
+            int[] temp = A;
+            A = B;
+            B = temp;
+        }
+
+        int l = 0;
+        int r = A.length;
+        while (l <= r) {
+            int i = (l + r) / 2;
+            int j = half - i;
+
+            int Aleft = i > 0 ? A[i - 1] : Integer.MIN_VALUE;
+            int Aright = i < A.length ? A[i] : Integer.MAX_VALUE;
+            int Bleft = j > 0 ? B[j - 1] : Integer.MIN_VALUE;
+            int Bright = j < B.length ? B[j] : Integer.MAX_VALUE;
+
+            if (Aleft <= Bright && Bleft <= Aright) {
+                if (total % 2 != 0) {
+                    return Math.max(Aleft, Bleft);
+                }
+                return (Math.max(Aleft, Bleft) + Math.min(Aright, Bright)) / 2.0;
+            } else if (Aleft > Bright) {
+                r = i - 1;
+            } else {
+                l = i + 1;
+            }
+        }
+        return -1;
+    }
+
 
     // V2
-    // https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/6401742/1ms-100-beats-single-loop-beginner-frien-6bqd/
+    // https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/4070500/99journey-from-brute-force-to-most-optim-z3k8/
 
     // V3
-    public double findMedianSortedArrays_3(int[] nums1, int[] nums2) {
+    // https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/6401742/1ms-100-beats-single-loop-beginner-frien-6bqd/
+
+    // V4
+    public double findMedianSortedArrays_4(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length) {
-            return findMedianSortedArrays_3(nums2, nums1); // Ensure nums1 is the smaller array
+            return findMedianSortedArrays_4(nums2, nums1); // Ensure nums1 is the smaller array
         }
 
         int len1 = nums1.length;
@@ -87,5 +214,6 @@ public class MedianOfTwoSortedArrays {
         throw new IllegalArgumentException("Input arrays are not sorted.");
     }
 
-    // V4
+    // V5
+
 }
