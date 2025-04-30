9d7e13fefa0c1e77d03d21fd922363d56d3a2da6
yennj12 <f339339@uber.com>
2025-04-30 12:17:42 +0800

    update 399 java

--- leetcode_java/src/main/java/dev/workspace10.java
+++ leetcode_java/src/main/java/dev/workspace10.java
@@ -1,5 +1,90 @@
 package dev;
 
+import java.util.*;
+
 public class workspace10 {
-    // 
+
+
+    // LC 399
+    // 11.31 - 11. 47 am
+    /**
+     *  IDEA 1) DFS
+     *
+     *  IDEA 2) BFS ???
+     *
+     *  IDEA 3) UNION FIND ???
+     *
+     *
+     *  ----------------------
+     *
+     *  IDEA: DFS
+     *
+     *
+     * Example 1):
+     *  Given a / b = 2.0, b / c = 3.0.
+     *  queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
+     *  return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+     *
+     *  ->
+     *
+     *
+     */
+    // 12.04 - 12.14 pm
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+        double[] res = new double[queries.size()];
+
+        // edge
+        if(queries.isEmpty() || queries.size() == 0){
+            return null;
+        }
+        if(equations == null || equations.size() == 0){
+            Arrays.fill(res, -1.0);
+            return res;
+        }
+
+        // build map
+        Map<String, Double> valMap = new HashMap<>();
+        for(int i = 0; i < equations.size(); i++){
+
+            List<String> eq = equations.get(i);
+            String var_1 = eq.get(0);
+            String var_2 = eq.get(1);
+            double val = values[i];
+
+            valMap.put(var_1 + " / " + var_2, val);
+            valMap.put(var_2 + " / " + var_1, 1 / val);
+        }
+
+        List<Double> cache = new ArrayList<>();
+
+        // dfs
+        for(List<String> q: queries){
+            double tmp = 1.0;
+            calHelper(q, valMap, tmp);
+            cache.add(tmp);
+        }
+
+        return res;
+    }
+
+    public Double calHelper(List<String> query, Map<String, Double> valMap, double tmp){
+
+        String var_1 = query.get(0);
+        String var_2 = query.get(1);
+        if(!valMap.containsKey(var_1) || !valMap.containsKey(var_2)){
+            return -1.0;
+        }
+        if (var_1.equals(var_2)) {
+            return 1.0;
+        }
+
+        // dfs call
+//        if(valMap.containsKey(var_1)){
+//            for(Str)
+//        }
+
+        return tmp;
+    }
+
 }
