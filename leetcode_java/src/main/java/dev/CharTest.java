e032dd181ba4484dacbd54fd9d180c72b652fb9a
yennanliu <f339339@gmail.com>
2025-05-05 20:18:22 +0800

    update java_trick.md, fix LC 126, add dev/CharTest.java

---
+++ leetcode_java/src/main/java/dev/CharTest.java
@@ -0,0 +1,17 @@
+package dev;
+
+public class CharTest {
+  public static void main(String[] args) {
+
+    // TEST 1
+    // LC 127. Word Ladder
+    // for (char c = 'a'; c <= 'z'; c++) {
+    for (char c = 'a'; c <= 'z'; c++) {
+      System.out.println(c);
+      // output as below:
+      /** a b c d e f g h i j k l m n o p q r s t u v w x y z */
+    }
+
+
+  }
+}
