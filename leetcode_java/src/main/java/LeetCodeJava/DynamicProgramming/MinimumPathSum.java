ca6724531dd7b58c1ba3defc86e52fb7abb71063
yennanliu <f339339@gmail.com>
2025-05-11 15:06:20 +0800

    add 64 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/DynamicProgramming/MinimumPathSum.java
@@ -0,0 +1,135 @@
+package LeetCodeJava.DynamicProgramming;
+
+// https://leetcode.com/problems/minimum-path-sum/description/
+
+import java.util.Arrays;
+
+/**
+ * 64. Minimum Path Sum
+ * Medium
+ * Topics
+ * Companies
+ * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+ *
+ * Note: You can only move either down or right at any point in time.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * Output: 7
+ * Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+ * Example 2:
+ *
+ * Input: grid = [[1,2,3],[4,5,6]]
+ * Output: 12
+ *
+ *
+ * Constraints:
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 200
+ *
+ */
+public class MinimumPathSum {
+
+    // V0
+//    public int minPathSum(int[][] grid) {
+//
+//    }
+
+    // V1
+    // https://www.youtube.com/watch?v=pGMsrvt0fpk
+    // https://github.com/neetcode-gh/leetcode/blob/main/java%2F0064-minimum-path-sum.java
+    public int minPathSum_1(int[][] grid) {
+        int m = grid.length - 1;
+        int n = grid[0].length - 1;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        return helper(grid, m, n, dp);
+    }
+
+    public int helper(int[][] grid, int m, int n, int[][] dp) {
+        if (m == 0 && n == 0) return grid[0][0];
+        if (m == 0) {
+            dp[m][n] = grid[m][n] + helper(grid, m, n - 1, dp);
+            return dp[m][n];
+        }
+        if (n == 0) {
+            dp[m][n] = grid[m][n] + helper(grid, m - 1, n, dp);
+            return dp[m][n];
+        }
+        if (dp[m][n] != -1) return dp[m][n];
+        dp[m][n] =
+                grid[m][n] +
+                        Math.min(helper(grid, m, n - 1, dp), helper(grid, m - 1, n, dp));
+        return dp[m][n];
+    }
+
+    // V2
+    // https://leetcode.com/problems/minimum-path-sum/solutions/3345656/pythonjava-csimple-solutioneasy-to-under-occy/
+    public int minPathSum_2(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+
+    // V3-1
+    // https://leetcode.com/problems/minimum-path-sum/solutions/3345863/image-explanation-recursion-dp-4-methods-pyw1/
+    public int minPathSum_3_1(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        for (int i = 1; i < m; i++)
+            grid[i][0] += grid[i - 1][0];
+
+        for (int j = 1; j < n; j++)
+            grid[0][j] += grid[0][j - 1];
+
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+
+        return grid[m - 1][n - 1];
+    }
+
+    // V3-2
+    // https://leetcode.com/problems/minimum-path-sum/solutions/3345863/image-explanation-recursion-dp-4-methods-pyw1/
+    public int minPathSum_3_2(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] cur = new int[m];
+        cur[0] = grid[0][0];
+
+        for (int i = 1; i < m; i++)
+            cur[i] = cur[i - 1] + grid[i][0];
+
+        for (int j = 1; j < n; j++) {
+            cur[0] += grid[0][j];
+            for (int i = 1; i < m; i++)
+                cur[i] = Math.min(cur[i - 1], cur[i]) + grid[i][j];
+        }
+        return cur[m - 1];
+    }
+
+
+}
