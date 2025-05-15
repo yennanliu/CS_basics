6e324f0697df73d0c664d5a6c3cc1f3b066034a2
yenj12 <yenj12@uber.com>
2025-05-15 12:59:11 +0800

    update 83 java

--- leetcode_java/src/main/java/LeetCodeJava/LinkedList/RemoveDuplicatesFromSortedList.java
+++ leetcode_java/src/main/java/LeetCodeJava/LinkedList/RemoveDuplicatesFromSortedList.java
@@ -1,7 +1,35 @@
 package LeetCodeJava.LinkedList;
 
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
-
+/**
+ * 83. Remove Duplicates from Sorted List
+ * Solved
+ * Easy
+ * Topics
+ * Companies
+ * Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,1,2]
+ * Output: [1,2]
+ * Example 2:
+ *
+ *
+ * Input: head = [1,1,2,3,3]
+ * Output: [1,2,3]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is in the range [0, 300].
+ * -100 <= Node.val <= 100
+ * The list is guaranteed to be sorted in ascending order.
+ *
+ */
 import LeetCodeJava.DataStructure.ListNode;
 
 import java.util.HashSet;
@@ -10,6 +38,7 @@ import java.util.Set;
 public class RemoveDuplicatesFromSortedList {
 
     // V0
+    // IDEA: HASHSET + LINKED LIST OP
     public ListNode deleteDuplicates(ListNode head) {
 
         ListNode ans = new ListNode();
@@ -28,6 +57,36 @@ public class RemoveDuplicatesFromSortedList {
 
         return ans.next;
     }
+
+    // V0-1
+    // IDEA: FAST, SLOW POINTER, LC 26
+    public ListNode deleteDuplicates_0_1(ListNode head) {
+        // edge
+        if(head == null){
+            return head;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null){
+
+            // NOTE !!! below
+            if(fast.val != slow.val){
+
+                slow.next = fast;
+                slow = slow.next;
+            }
+
+            fast = fast.next;
+        }
+
+        // NOTE !!! below
+        slow.next = null;
+
+        return head; // ???
+    }
+
 
     //  V1
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list/solutions/3257316/c-python-c-java-easiest-solution-o-n-time/
