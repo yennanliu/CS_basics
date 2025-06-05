1fad69d122ce56c3046c04a7cdae6fc3a2a6eb64
yenj12 <yenj12@uber.com>
2025-06-05 11:05:14 +0800

    update 13 java, progress, comment syntax error jdk 8

--- leetcode_java/src/main/java/LeetCodeJava/Heap/FindKPairsWithSmallestSums.java
+++ leetcode_java/src/main/java/LeetCodeJava/Heap/FindKPairsWithSmallestSums.java
@@ -2,7 +2,7 @@ package LeetCodeJava.Heap;
 
 // https://leetcode.com/problems/find-k-pairs-with-smallest-sums/description/
 
-import jdk.internal.net.http.common.Pair;
+//import jdk.internal.net.http.common.Pair;
 
 import java.util.*;
 
@@ -143,37 +143,37 @@ public class FindKPairsWithSmallestSums {
     // V1
     // https://leetcode.com/problems/find-k-pairs-with-smallest-sums/editorial/
     // IDEA: PQ
-    public List<List<Integer>> kSmallestPairs_1(int[] nums1, int[] nums2, int k) {
-        int m = nums1.length;
-        int n = nums2.length;
-
-        List<List<Integer>> ans = new ArrayList<>();
-        Set<Pair<Integer, Integer>> visited = new HashSet<>();
-
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
-        minHeap.offer(new int[] { nums1[0] + nums2[0], 0, 0 });
-        visited.add(new Pair<Integer, Integer>(0, 0));
-
-        while (k-- > 0 && !minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int i = top[1];
-            int j = top[2];
-
-            ans.add(List.of(nums1[i], nums2[j]));
-
-            if (i + 1 < m && !visited.contains(new Pair<Integer, Integer>(i + 1, j))) {
-                minHeap.offer(new int[] { nums1[i + 1] + nums2[j], i + 1, j });
-                visited.add(new Pair<Integer, Integer>(i + 1, j));
-            }
-
-            if (j + 1 < n && !visited.contains(new Pair<Integer, Integer>(i, j + 1))) {
-                minHeap.offer(new int[] { nums1[i] + nums2[j + 1], i, j + 1 });
-                visited.add(new Pair<Integer, Integer>(i, j + 1));
-            }
-        }
-
-        return ans;
-    }
+//    public List<List<Integer>> kSmallestPairs_1(int[] nums1, int[] nums2, int k) {
+//        int m = nums1.length;
+//        int n = nums2.length;
+//
+//        List<List<Integer>> ans = new ArrayList<>();
+//        Set<Pair<Integer, Integer>> visited = new HashSet<>();
+//
+//        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+//        minHeap.offer(new int[] { nums1[0] + nums2[0], 0, 0 });
+//        visited.add(new Pair<Integer, Integer>(0, 0));
+//
+//        while (k-- > 0 && !minHeap.isEmpty()) {
+//            int[] top = minHeap.poll();
+//            int i = top[1];
+//            int j = top[2];
+//
+//            ans.add(List.of(nums1[i], nums2[j]));
+//
+//            if (i + 1 < m && !visited.contains(new Pair<Integer, Integer>(i + 1, j))) {
+//                minHeap.offer(new int[] { nums1[i + 1] + nums2[j], i + 1, j });
+//                visited.add(new Pair<Integer, Integer>(i + 1, j));
+//            }
+//
+//            if (j + 1 < n && !visited.contains(new Pair<Integer, Integer>(i, j + 1))) {
+//                minHeap.offer(new int[] { nums1[i] + nums2[j + 1], i, j + 1 });
+//                visited.add(new Pair<Integer, Integer>(i, j + 1));
+//            }
+//        }
+//
+//        return ans;
+//    }
 
     // V2
     // https://leetcode.com/problems/find-k-pairs-with-smallest-sums/solutions/3686980/from-dumb-to-pro-with-just-one-visit-my-u1ig6/
@@ -213,30 +213,30 @@ public class FindKPairsWithSmallestSums {
     // V3
     // https://leetcode.com/problems/find-k-pairs-with-smallest-sums/solutions/6616196/simple-min-heap-priority-queue-by-kdhaka-8np5/
     // IDEA: PQ
-    public List<List<Integer>> kSmallestPairs_3(int[] nums1, int[] nums2, int k) {
-        List<List<Integer>> list = new ArrayList<>();
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-
-        for (int x : nums1) {
-            pq.offer(new int[] { x + nums2[0], 0 });
-        }
-
-        while (k > 0 && !pq.isEmpty()) {
-            int[] pair = pq.poll();
-            int sum = pair[0];
-            int pos = pair[1];
-
-            list.add(List.of(sum - nums2[pos], nums2[pos]));
-
-            if (pos + 1 < nums2.length) {
-                pq.offer(new int[] { sum - nums2[pos] + nums2[pos + 1], pos + 1 });
-            }
-            k--;
-        }
-
-        return list;
-    }
+//    public List<List<Integer>> kSmallestPairs_3(int[] nums1, int[] nums2, int k) {
+//        List<List<Integer>> list = new ArrayList<>();
+//
+//        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+//
+//        for (int x : nums1) {
+//            pq.offer(new int[] { x + nums2[0], 0 });
+//        }
+//
+//        while (k > 0 && !pq.isEmpty()) {
+//            int[] pair = pq.poll();
+//            int sum = pair[0];
+//            int pos = pair[1];
+//
+//            list.add(List.of(sum - nums2[pos], nums2[pos]));
+//
+//            if (pos + 1 < nums2.length) {
+//                pq.offer(new int[] { sum - nums2[pos] + nums2[pos + 1], pos + 1 });
+//            }
+//            k--;
+//        }
+//
+//        return list;
+//    }
 
 
 }
--- leetcode_java/src/main/java/dev/StringTest.java
+++ leetcode_java/src/main/java/dev/StringTest.java
@@ -8,12 +8,16 @@ public class StringTest {
 //        System.out.println(x.substring(1,3));
 //
 
-        String x_1 = "9871230";
-        StringBuilder sb = new StringBuilder();
-        sb.append(x_1);
-        sb.append("0");
-        int val = Integer.parseInt(sb.toString());
-        System.out.println(val);
+//        String x_1 = "9871230";
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(x_1);
+//        sb.append("0");
+//        int val = Integer.parseInt(sb.toString());
+//        System.out.println(val);
+
+        String val = "123";
+        val = "0" + val;
+        System.out.println(val); // 0123
 
 
     }
