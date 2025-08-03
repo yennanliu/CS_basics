22aab3e6aca1add620fe8590dc7db0fcfcd8d5ba
yennanliu <f339339@gmail.com>
2025-08-03 18:41:38 +0800

    update ws

---
+++ leetcode_java/src/main/java/dev/workspace13.java
@@ -0,0 +1,55 @@
+package dev;
+
+public class workspace13 {
+
+    // LC 427
+    // 6.19 - 6.29 PM
+    class Node {
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+
+
+        public Node() {
+            this.val = false;
+            this.isLeaf = false;
+            this.topLeft = null;
+            this.topRight = null;
+            this.bottomLeft = null;
+            this.bottomRight = null;
+        }
+
+        public Node(boolean val, boolean isLeaf) {
+            this.val = val;
+            this.isLeaf = isLeaf;
+            this.topLeft = null;
+            this.topRight = null;
+            this.bottomLeft = null;
+            this.bottomRight = null;
+        }
+
+        public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+            this.val = val;
+            this.isLeaf = isLeaf;
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+        }
+    }
+    /**
+     * Return the root of the Quad-Tree representing grid.
+     *
+     *  IDEA 1) DFS
+     *
+     *
+     */
+    public Node construct(int[][] grid) {
+
+        return null;
+    }
+
+}
