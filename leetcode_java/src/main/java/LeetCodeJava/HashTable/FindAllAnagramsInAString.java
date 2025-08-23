42fbf5605d4eae18169b1e556ef8a7896f3e9893
yennanliu <f339339@gmail.com>
2025-08-23 15:47:24 +0800

    update 438 java

--- leetcode_java/src/main/java/LeetCodeJava/HashTable/FindAllAnagramsInAString.java
+++ leetcode_java/src/main/java/LeetCodeJava/HashTable/FindAllAnagramsInAString.java
@@ -67,9 +67,21 @@ public class FindAllAnagramsInAString {
             String key = s_arr[r];
             s_map.put(key, s_map.getOrDefault(key, 0) + 1);
 
+            /**
+             *  NOTE !!!
+             *
+             *   need `while loop` below
+             *   so we can `shrink` left pointer (window)
+             *   to make the sub string size equals to `p`
+             */
             // shrink window if size > p.length()
             while (r - l + 1 > p.length()) {
                 String leftKey = s_arr[l];
+                /**
+                 *  NOTE !!!
+                 *
+                 *   need to update s_map
+                 */
                 s_map.put(leftKey, s_map.get(leftKey) - 1);
                 if (s_map.get(leftKey) == 0) {
                     s_map.remove(leftKey);
@@ -77,6 +89,11 @@ public class FindAllAnagramsInAString {
                 l++;
             }
 
+            /**
+             *  NOTE !!!
+             *
+             *  if same size, compare s_map, and p_map
+             */
             // if same size, compare
             if (r - l + 1 == p.length() && isEqaual(p_map, s_map)) {
                 res.add(l);
@@ -136,6 +153,59 @@ public class FindAllAnagramsInAString {
     }
 
     // V1
+    // https://leetcode.com/problems/find-all-anagrams-in-a-string/solutions/1500039/java-tc-osp-sc-o1-sliding-window-solutio-icz6/
+    // IDEA: sliding window
+    public List<Integer> findAnagrams_1(String s, String p) {
+        if (s == null || p == null) {
+            throw new IllegalArgumentException("Input string is null");
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int sLen = s.length();
+        int pLen = p.length();
+        if (sLen * pLen == 0 || sLen < pLen) {
+            return result;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < p.length(); i++) {
+            map.put(p.charAt(i), map.getOrDefault(p.charAt(i), 0) + 1);
+        }
+
+        int toBeMatched = map.size();
+        int start = 0;
+        int end = 0;
+
+        while (end < sLen) {
+            char eChar = s.charAt(end);
+            if (map.containsKey(eChar)) {
+                int count = map.get(eChar);
+                if (count == 1) {
+                    toBeMatched--;
+                }
+                map.put(eChar, count - 1);
+            }
+            end++;
+
+            if (end - start > pLen) {
+                char sChar = s.charAt(start);
+                if (map.containsKey(sChar)) {
+                    int count = map.get(sChar);
+                    if (count == 0) {
+                        toBeMatched++;
+                    }
+                    map.put(sChar, count + 1);
+                }
+                start++;
+            }
+
+            if (toBeMatched == 0) {
+                result.add(start);
+            }
+        }
+
+        return result;
+    }
 
     // V2
 
