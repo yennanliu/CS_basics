b061e36a41cb49510a4dd947db73c158d4df0de2
yennanliu <f339339@gmail.com>
2025-08-31 16:29:28 +0800

    update progress

---
+++ leetcode_java/src/main/java/dev/workspace15.java
@@ -0,0 +1,28 @@
+package dev;
+
+import LeetCodeJava.LinkedList.FlattenMultilevelDoublyLinkedList;
+
+public class workspace15 {
+
+    // LC 430
+    // 4.28 - 4.38 pm
+    /**
+     *  IDEA 1) LINKED LIST OP
+     *
+     *
+     *
+     */
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    };
+
+    public Node flatten(Node head) {
+
+        return null;
+    }
+
+
+}
