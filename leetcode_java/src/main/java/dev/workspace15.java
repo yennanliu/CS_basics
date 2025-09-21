c97675948dbcbc465f9c8604059abd65d645ddd7
yennanliu <f339339@gmail.com>
2025-09-21 11:48:07 +0800

    update

--- leetcode_java/src/main/java/dev/workspace15.java
+++ leetcode_java/src/main/java/dev/workspace15.java
@@ -2,9 +2,7 @@ package dev;
 
 import LeetCodeJava.LinkedList.FlattenMultilevelDoublyLinkedList;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class workspace15 {
 
@@ -335,7 +333,7 @@ public class workspace15 {
    *  NOTE: A subarray is a contiguous non-empty sequence of elements within an array
    *
    */
-  public long maxTotalValue(int[] nums, int k) {
+  public long maxTotalValue_1(int[] nums, int k) {
       // edge
       if(nums.length <= 1){
           return 0;
@@ -376,5 +374,135 @@ public class workspace15 {
 
       return 0;
     }
+
+
+    // LC Q4
+    //https://leetcode.com/contest/weekly-contest-468/problems/maximum-total-subarray-value-ii/description/
+    /**
+     *  -> Return the `MAX` possible total value you can achieve.©leetcode
+     *
+     *
+     *  1. You need to choose exactly `k ` `DISTINCT`
+     *    non-empty subarrays nums[l..r] of nums©leetcode
+     *
+     *
+     *  2. Subarrays may
+     *     1) overlap, and
+     *     2) the exact same subarray (same l and r) can be chosen` more than once.`
+     *
+     *  3. !!!!
+     *
+     *   The value of a subarray nums[l..r] is defined as:
+     *      max(nums[l..r]) - min(nums[l..r]).
+     *
+     *   -> val of sub arr is
+     *      max (sub) - min(sub)
+     *
+     *  NOTE: A subarray is a contiguous non-empty sequence of elements within an array
+     *
+     *
+     *  IDEA 1) GREEDY (BRUTE FORCE)
+     *
+     *  IDEA 2) SORTING (global sort), then get max1, max2, ... min1, min2...
+     *        -> and we can the TOP K `big diff` between max, and max
+     *
+     *  IDEA 3) MAX, MIN PQ
+     *
+     *  IDEA 3) SLIDE WINDOW + HASHMAP RECORD {"start_end_idx" : biggest_diff }
+     *
+     *
+     *
+     */
+    public long maxTotalValue(int[] nums, int k) {
+        // edge
+        if(nums.length <= 1){
+            return 0;
+        }
+//        if(nums.length == 2){
+//            return (long) Math.abs(nums[0] - nums[1]) * k;
+//        }
+
+        Long res = 0L;
+        // brute force
+        // ???
+        Long tmpSum = 0L;
+        List<Integer> cache = new ArrayList<>();
+        for(int i = 0; i < nums.length - 1; i++){
+            int local_min = nums[i];
+            int local_max = nums[i];
+            for(int j = i+1; j < nums.length; j++){
+                local_min = Math.min(local_min, nums[j]);
+                local_max = Math.max(local_max, nums[j]);
+
+                cache.add(local_max - local_min);
+                tmpSum += (local_max - local_min);
+            }
+        }
+
+        // ordering
+        Collections.sort(cache, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int diff = o2 - o1;
+                return diff;
+            }
+        });
+
+        //edge k > cache size
+        if(k >= cache.size()){
+            return tmpSum;
+        }
+
+        for(int i = 0; i < k; i++){
+            res += cache.get(i);
+        }
+
+        return res;
+    }
+
+
+
+
+//    public long maxTotalValue(int[] nums, int k) {
+//        // edge
+//        if(nums.length <= 1){
+//            return 0;
+//        }
+//        if(nums.length == 2){
+//            return (long) Math.abs(nums[0] - nums[1]) * k;
+//        }
+//
+//        // ???
+//        PriorityQueue<Integer> pq_max = new PriorityQueue<>(Comparator.reverseOrder());
+//        PriorityQueue<Integer> pq_min = new PriorityQueue<>();
+//
+//        for(int x: nums){
+//            while(!pq_max.isEmpty() && pq_max.size() < k){
+//                pq_max.poll();
+//            }
+//            while(!pq_min.isEmpty() && pq_min.size() < k){
+//                pq_min.poll();
+//            }
+//            pq_max.add(x);
+//            pq_min.add(x);
+//        }
+//
+//
+//        Long res = 0L;
+//        int cnt = 0;
+//
+//        while(cnt < k){
+//            int pq_min_idx = 0;
+//            while(pq_min_idx < pq_min.size()){
+//                int max_val = pq_max.poll();
+//                res += (max_val - pq_min.peek());
+//                cnt += 1;
+//            }
+//        }
+//
+//        return res;
+//    }
+
+
 
 }
