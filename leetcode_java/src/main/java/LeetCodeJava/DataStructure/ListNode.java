212de39139ec43a93fd656d6610252277201ba26
jim <jim@uber.com>
2025-09-23 09:39:49 +0800

    add 82 jaav

--- leetcode_java/src/main/java/LeetCodeJava/DataStructure/ListNode.java
+++ leetcode_java/src/main/java/LeetCodeJava/DataStructure/ListNode.java
@@ -30,6 +30,11 @@ public class ListNode {
     public int val;
     public ListNode next;
     public ListNode() {}
-    public ListNode(int val) { this.val = val; }
-    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public ListNode(int val) {
+        this.val = val;
+    }
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
