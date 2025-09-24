f040741e449cd4e8fadd59691f5949ac9c09cf22
jim <jim@uber.com>
2025-09-24 10:19:27 +0800

    update 617 java

--- leetcode_java/src/main/java/LeetCodeJava/Tree/MergeTwoBinaryTrees.java
+++ leetcode_java/src/main/java/LeetCodeJava/Tree/MergeTwoBinaryTrees.java
@@ -1,7 +1,44 @@
 package LeetCodeJava.Tree;
 
 // https://leetcode.com/problems/merge-two-binary-trees/
-
+/**
+ * 617. Merge Two Binary Trees
+ * Solved
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * You are given two binary trees root1 and root2.
+ *
+ * Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+ *
+ * Return the merged tree.
+ *
+ * Note: The merging process must start from the root nodes of both trees.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+ * Output: [3,4,5,5,4,null,7]
+ * Example 2:
+ *
+ * Input: root1 = [1], root2 = [1,2]
+ * Output: [2,2]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in both trees is in the range [0, 2000].
+ * -104 <= Node.val <= 104
+ *
+ * Seen this question in a real interview before?
+ * 1/5
+ * Yes
+ * No
+ */
 import LeetCodeJava.DataStructure.TreeNode;
 
 import java.util.Stack;
@@ -77,5 +114,22 @@ public class MergeTwoBinaryTrees {
         }
         return t1;
     }
+
+    // V3
+    // IDEA : RECURSIVE
+    // https://leetcode.ca/2017-08-08-617-Merge-Two-Binary-Trees/
+    public TreeNode mergeTrees_3(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        TreeNode node = new TreeNode(root1.val + root2.val);
+        node.left = mergeTrees_3(root1.left, root2.left);
+        node.right = mergeTrees_3(root1.right, root2.right);
+        return node;
+    }
+
 
 }
