7d68e6132ddfa8e4396ac2357cde1ad0500b7c64
jim <jim@uber.com>
2025-10-05 18:15:49 +0800

    add Weekly324.java

---
+++ leetcode_java/src/main/java/dev/LCWeekly/Weekly324.java
@@ -0,0 +1,19 @@
+package dev.LCWeekly;
+
+// https://leetcode.com/contest/weekly-contest-324/
+
+public class Weekly324 {
+
+    // Q1
+    // https://leetcode.com/problems/count-pairs-of-similar-strings/description/
+
+    // Q2
+    // https://leetcode.com/problems/smallest-value-after-replacing-with-sum-of-prime-factors/description/
+
+    // Q3
+    // https://leetcode.com/problems/add-edges-to-make-degrees-of-all-nodes-even/description/
+
+    // Q4
+    // https://leetcode.com/problems/cycle-length-queries-in-a-tree/description/
+    
+}
