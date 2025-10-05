c0e7165e52b43a457792d2d96a0f90c02de51947
jim <jim@uber.com>
2025-10-05 18:22:07 +0800

    update

--- leetcode_java/src/main/java/dev/LCWeekly/Weekly324.java
+++ leetcode_java/src/main/java/dev/LCWeekly/Weekly324.java
@@ -6,6 +6,28 @@ public class Weekly324 {
 
     // Q1
     // https://leetcode.com/problems/count-pairs-of-similar-strings/description/
+    // LC 2506
+    /**
+     *  Return the `number` of `pairs (i, j)`
+     *  such that 0 <= i < j <= word.length - 1
+     *   and the two strings words[i] and words[j] are `similar.`
+     *
+     *   -> get the cnt of `similar word pair` for any
+     *      different word in words
+     *
+     *  - `similar` : 2 word that has the SAME SET of alphabet
+     *
+     *
+     *
+     *  IDEA 1) HASHMAP
+     *
+     *
+     */
+    public int similarPairs(String[] words) {
+
+        return 0;
+    }
+
 
     // Q2
     // https://leetcode.com/problems/smallest-value-after-replacing-with-sum-of-prime-factors/description/
@@ -15,5 +37,5 @@ public class Weekly324 {
 
     // Q4
     // https://leetcode.com/problems/cycle-length-queries-in-a-tree/description/
-    
+
 }
