d4e313db2eba16bbd26daecf34abdae9a8e67b7f
jim <jim@uber.com>
2025-10-06 17:03:04 +0800

    add 2506 java

--- leetcode_java/src/main/java/LeetCodeJava/HashTable/CountPairsOfSimilarStrings.java
+++ leetcode_java/src/main/java/LeetCodeJava/HashTable/CountPairsOfSimilarStrings.java
@@ -1,32 +1,67 @@
-package dev.LCWeekly;
+package LeetCodeJava.HashTable;
 
-// https://leetcode.com/contest/weekly-contest-324/
+// https://leetcode.com/problems/count-pairs-of-similar-strings/description/
 
 import java.util.*;
 
-public class Weekly324 {
+/**
+ * 2506. Count Pairs Of Similar Strings
+ * Solved
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * Hint
+ * You are given a 0-indexed string array words.
+ *
+ * Two strings are similar if they consist of the same characters.
+ *
+ * For example, "abca" and "cba" are similar since both consist of characters 'a', 'b', and 'c'.
+ * However, "abacba" and "bcfd" are not similar since they do not consist of the same characters.
+ * Return the number of pairs (i, j) such that 0 <= i < j <= word.length - 1 and the two strings words[i] and words[j] are similar.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: words = ["aba","aabb","abcd","bac","aabc"]
+ * Output: 2
+ * Explanation: There are 2 pairs that satisfy the conditions:
+ * - i = 0 and j = 1 : both words[0] and words[1] only consist of characters 'a' and 'b'.
+ * - i = 3 and j = 4 : both words[3] and words[4] only consist of characters 'a', 'b', and 'c'.
+ * Example 2:
+ *
+ * Input: words = ["aabb","ab","ba"]
+ * Output: 3
+ * Explanation: There are 3 pairs that satisfy the conditions:
+ * - i = 0 and j = 1 : both words[0] and words[1] only consist of characters 'a' and 'b'.
+ * - i = 0 and j = 2 : both words[0] and words[2] only consist of characters 'a' and 'b'.
+ * - i = 1 and j = 2 : both words[1] and words[2] only consist of characters 'a' and 'b'.
+ * Example 3:
+ *
+ * Input: words = ["nba","cba","dba"]
+ * Output: 0
+ * Explanation: Since there does not exist any pair that satisfies the conditions, we return 0.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 100
+ * words[i] consist of only lowercase English letters.
+ *
+ *
+ */
+public class CountPairsOfSimilarStrings {
 
-    // Q1
-    // https://leetcode.com/problems/count-pairs-of-similar-strings/description/
-    // LC 2506
-    // 16.32 - 16.42 pm
-    /**
-     *  Return the `number` of `pairs (i, j)`
-     *  such that 0 <= i < j <= word.length - 1
-     *   and the two strings words[i] and words[j] are `similar.`
-     *
-     *   -> get the cnt of `similar word pair` for any
-     *      different word in words
-     *
-     *  - `similar` : 2 word that has the SAME SET of alphabet
-     *
-     *
-     *
-     *  IDEA 1) HASHMAP
-     *
-     *
-     */
-    public int similarPairs(String[] words) {
+    // V0
+//    public int similarPairs(String[] words) {
+//
+//    }
+
+    // V0-1
+    // IDEA: SET + HASHMAP + MATH (fixed by gpt)
+    public int similarPairs_0_1(String[] words) {
         if (words == null || words.length < 2) {
             return 0;
         }
@@ -63,6 +98,7 @@ public class Weekly324 {
         return sb.toString(); // this string uniquely represents the set
     }
 
+    // TODO: fix below
 //    public int similarPairs(String[] words) {
 //        // edge
 //        if(words == null || words.length == 0){
@@ -129,13 +165,6 @@ public class Weekly324 {
 //    }
 
 
-    // Q2
-    // https://leetcode.com/problems/smallest-value-after-replacing-with-sum-of-prime-factors/description/
 
-    // Q3
-    // https://leetcode.com/problems/add-edges-to-make-degrees-of-all-nodes-even/description/
-
-    // Q4
-    // https://leetcode.com/problems/cycle-length-queries-in-a-tree/description/
 
 }
