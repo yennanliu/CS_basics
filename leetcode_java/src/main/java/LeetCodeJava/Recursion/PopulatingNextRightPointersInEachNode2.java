c7c0374633a45e452a542a70b5a6dd8dcc0af208
jim <jim@uber.com>
2025-10-07 10:57:54 +0800

    update 117 java

--- leetcode_java/src/main/java/LeetCodeJava/Recursion/PopulatingNextRightPointersInEachNode2.java
+++ leetcode_java/src/main/java/LeetCodeJava/Recursion/PopulatingNextRightPointersInEachNode2.java
@@ -1,61 +1,54 @@
 package LeetCodeJava.Recursion;
 
 // https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+/**
+ * 117. Populating Next Right Pointers in Each Node II
+ * Solved
+ * Medium
+ * Topics
+ * premium lock icon
+ * Companies
+ * Given a binary tree
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+ *
+ * Initially, all next pointers are set to NULL.
+ *
+ *  Example 1:
+ *
+ *
+ * Input: root = [1,2,3,4,5,null,7]
+ * Output: [1,#,2,3,#,4,5,7,#]
+ * Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ *
+ * Example 2:
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 6000].
+ * -100 <= Node.val <= 100
+ *
+ *
+ * Follow-up:
+ *
+ * You may only use constant extra space.
+ * The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+ *
+ */
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- *
- Code
- Testcase
- Test Result
- Test Result
- 117. Populating Next Right Pointers in Each Node II
- Solved
- Medium
- Topics
- Companies
- Given a binary tree
-
- struct Node {
- int val;
- Node *left;
- Node *right;
- Node *next;
- }
- Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
-
- Initially, all next pointers are set to NULL.
-
-
-
- Example 1:
-
-
- Input: root = [1,2,3,4,5,null,7]
- Output: [1,#,2,3,#,4,5,7,#]
- Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
- Example 2:
-
- Input: root = []
- Output: []
-
-
- Constraints:
-
- The number of nodes in the tree is in the range [0, 6000].
- -100 <= Node.val <= 100
-
-
- Follow-up:
-
- You may only use constant extra space.
- The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
-
- *
- *
- */
 public class PopulatingNextRightPointersInEachNode2 {
 
     /*
@@ -152,4 +145,6 @@ public class PopulatingNextRightPointersInEachNode2 {
     // V2-3
     // IDEA: RECURSION
     // https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/solutions/2033511/3-approaches-bfs-bfs-linkedlist-recursio-0q68/
+
+    
 }
