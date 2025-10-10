3c382fcebaf5c9068150db22dd797d06da115eef
jim <jim@uber.com>
2025-10-10 16:01:22 +0800

    update cheatsheet

--- leetcode_java/src/main/java/dev/CharTest.java
+++ leetcode_java/src/main/java/dev/CharTest.java
@@ -6,10 +6,25 @@ public class CharTest {
     // TEST 1
     // LC 127. Word Ladder
     // for (char c = 'a'; c <= 'z'; c++) {
-    for (char c = 'a'; c <= 'z'; c++) {
-      System.out.println(c);
-      // output as below:
-      /** a b c d e f g h i j k l m n o p q r s t u v w x y z */
+//    for (char c = 'a'; c <= 'z'; c++) {
+//      System.out.println(c);
+//      // output as below:
+//      /** a b c d e f g h i j k l m n o p q r s t u v w x y z */
+//    }
+
+    String x = "abcxyz";
+    for ( Character c: x.toCharArray()){
+      /**
+       *  c = a, a - ? = 0
+       *  c = b, a - ? = -1
+       *  c = c, a - ? = -2
+       *  c = x, a - ? = -23
+       *  c = y, a - ? = -24
+       *  c = z, a - ? = -25
+       */
+      System.out.println(" c = " + c +
+              ", a - ? = " + ('a' - c)
+      );
     }
 
 
