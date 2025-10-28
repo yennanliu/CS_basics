0f07ed78b00446bcb2fc2e7345350f3116cd0fea
jim <jim@uber.com>
2025-10-29 07:19:25 +0800

    update

---
+++ leetcode_java/src/main/java/dev/Sorting/BitWiseTest1.java
@@ -0,0 +1,17 @@
+package dev.Sorting;
+
+public class BitWiseTest1 {
+
+    public static void main(String[] args) {
+
+        //  // https://leetcode.com/problems/split-array-into-maximum-number-of-subarrays/description/
+        //int val1 = (4 & 7);
+        System.out.println(5 & 7);
+
+        System.out.println(5 & 7 & 1);
+
+        System.out.println(5 & 7 & 1 & 3);
+    }
+
+
+}
