07d125334656163e32c8ca6f4b1536e4637f747b
jim <jim@uber.com>
2025-10-28 17:29:08 +0800

    add weekly114, update progress

---
+++ leetcode_java/src/main/java/dev/LCWeekly/Weekly114.java
@@ -0,0 +1,27 @@
+package dev.LCWeekly;
+
+/**
+ * Leetcode weekly contest 114
+ * https://leetcode.com/contest/biweekly-contest-114/
+ * 力扣 第 114 场双周赛(中文版)
+ * https://leetcode.cn/contest/biweekly-contest-114/
+ */
+public class Weekly114 {
+
+    // Q1
+    // LC 2869
+    // https://leetcode.com/problems/minimum-operations-to-collect-elements/description/
+
+    // Q2
+    // LC 2870
+    // https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/
+
+    // Q3
+    // LC 2871
+    // https://leetcode.com/problems/split-array-into-maximum-number-of-subarrays/
+
+    // Q4
+    // LC 2872
+    // https://leetcode.com/problems/maximum-number-of-k-divisible-components/description/
+
+}
