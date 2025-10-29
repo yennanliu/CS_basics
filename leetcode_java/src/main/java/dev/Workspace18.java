7b377664fde5b68e5e1c9801a9df6ddede46a0d3
yennanliu <f339339@gmail.com>
2025-10-30 06:30:25 +0800

    update 2871 java

--- leetcode_java/src/main/java/dev/Workspace18.java
+++ leetcode_java/src/main/java/dev/Workspace18.java
@@ -2,6 +2,43 @@ package dev;
 
 public class Workspace18 {
 
-    WorkSpace17.MyTrie trie = new WorkSpace17.MyTrie();
+    ///WorkSpace17.MyTrie trie = new WorkSpace17.MyTrie();
     //WorkSpace17.MyTrie.MyTrieNode.
+
+
+    // LC 2871
+    // 6.21 - 31 am
+    /**
+     *   IDEA: BRUTE FORCE + AND bit op
+     *
+     *
+     */
+    public int maxSubarrays(int[] nums) {
+        // edge
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int cnt = 0;
+        int cur = -1; // ???? // nums[0];
+
+        for(int i = 0; i < nums.length; i++){
+            if(cur == -1){
+                cur = nums[i];
+            }else{
+                cur = cur & nums[i];
+            }
+            if(cur == 0){
+                cnt += 1;
+                // reset cur
+                cur = -1;
+            }
+        }
+
+        return cnt == 0 ? 1: cnt;
+    }
+
+
+
+
 }
