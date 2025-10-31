3d5f96a75a11868079fe440177fbf860b661f4d1
jim <jim@uber.com>
2025-10-31 09:46:21 +0800

    fix py script, update 653 java

--- leetcode_java/src/main/java/LeetCodeJava/Tree/TwoSumIV.java
+++ leetcode_java/src/main/java/LeetCodeJava/Tree/TwoSumIV.java
@@ -1,7 +1,38 @@
 package LeetCodeJava.Tree;
 
 // https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
-
+/**
+ * 653. Two Sum IV - Input is a BST
+ * Solved
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [5,3,6,2,4,null,7], k = 9
+ * Output: true
+ * Example 2:
+ *
+ *
+ * Input: root = [5,3,6,2,4,null,7], k = 28
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [1, 104].
+ * -104 <= Node.val <= 104
+ * root is guaranteed to be a valid binary search tree.
+ * -105 <= k <= 105
+ *
+ *
+ */
 import LeetCodeJava.DataStructure.TreeNode;
 
 import java.util.*;
@@ -116,5 +147,7 @@ public class TwoSumIV {
         list.add(root.val);
         inorder(root.right, list);
     }
+
+
 
 }
