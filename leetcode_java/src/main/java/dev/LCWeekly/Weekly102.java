dc53ea36148d2584981d8832b28a1a4e647b77f3
jim <jim@uber.com>
2025-11-04 10:07:12 +0800

    add weekly 102

---
+++ leetcode_java/src/main/java/dev/LCWeekly/Weekly102.java
@@ -0,0 +1,28 @@
+package dev.LCWeekly;
+
+/**
+ * 本周題目
+ * LeetCode biweekly contest 102
+ * https://leetcode.com/contest/biweekly-contest-102/
+ * 中文題目
+ * https://leetcode.cn/contest/biweekly-contest-102/
+ */
+public class Weekly102 {
+
+    // Q1
+    // LC 2639
+    // https://leetcode.com/problems/find-the-width-of-columns-of-a-grid/
+
+    // Q2
+    // LC 2640
+    // https://leetcode.com/problems/find-the-score-of-all-prefixes-of-an-array/description/
+
+    // Q3
+    // LC 2641
+    // https://leetcode.com/problems/cousins-in-binary-tree-ii/
+
+    // Q4
+    // LC 2642
+    // https://leetcode.com/problems/design-graph-with-shortest-path-calculator/
+
+}
