65baf964f029721416bd571796830912fdb6fd53
jim <jim@uber.com>
2025-11-11 06:54:12 +0800

    update 502 java

--- leetcode_java/src/main/java/LeetCodeJava/Heap/IPO.java
+++ leetcode_java/src/main/java/LeetCodeJava/Heap/IPO.java
@@ -390,5 +390,27 @@ public class IPO {
         return w;
     }
 
+    // V5
+    // https://leetcode.ca/2017-04-15-502-IPO/
+    public int findMaximizedCapital_5(int k, int w, int[] profits, int[] capital) {
+        int n = capital.length;
+        PriorityQueue<int[]> q1 = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int i = 0; i < n; ++i) {
+            q1.offer(new int[] {capital[i], profits[i]});
+        }
+        PriorityQueue<Integer> q2 = new PriorityQueue<>((a, b) -> b - a);
+        while (k-- > 0) {
+            while (!q1.isEmpty() && q1.peek()[0] <= w) {
+                q2.offer(q1.poll()[1]);
+            }
+            if (q2.isEmpty()) {
+                break;
+            }
+            w += q2.poll();
+        }
+        return w;
+    }
+
+    
 
 }
