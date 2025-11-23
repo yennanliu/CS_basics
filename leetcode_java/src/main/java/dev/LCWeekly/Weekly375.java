6b1d50af9b5347d97080e296cb09b6330646d511
jim <jim@uber.com>
2025-11-23 15:08:15 +0800

    update weekly375

--- leetcode_java/src/main/java/dev/LCWeekly/Weekly375.java
+++ leetcode_java/src/main/java/dev/LCWeekly/Weekly375.java
@@ -7,4 +7,25 @@ package dev.LCWeekly;
  * https://leetcode.cn/contest/weekly-contest-375/
  */
 public class Weekly375 {
+
+    // Q1
+    // LC 2960
+    // https://leetcode.com/problems/count-tested-devices-after-test-operations/
+
+
+    // Q2
+    // LC 2961
+    // https://leetcode.com/problems/double-modular-exponentiation/description/
+
+
+    // Q3
+    // LC 2962
+    // https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/
+
+    // Q4
+    // LC 2963
+    // https://leetcode.com/problems/count-the-number-of-good-partitions/description/
+
+
+    
 }
