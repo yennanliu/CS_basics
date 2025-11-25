0aa8a39c8ccd378d27386182eee2f22b24de155f
jim <jim@uber.com>
2025-11-25 10:50:08 +0800

    add 2962 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/SlideWindow/CountSubarraysWhereMaxElementAppearsAtLeastKTimes.java
@@ -0,0 +1,109 @@
+package LeetCodeJava.SlideWindow;
+
+// https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 2962. Count Subarrays Where Max Element Appears at Least K Times
+ * Medium
+ * Topics
+ * premium lock icon
+ * Companies
+ * You are given an integer array nums and a positive integer k.
+ *
+ * Return the number of subarrays where the maximum element of nums appears at least k times in that subarray.
+ *
+ * A subarray is a contiguous sequence of elements within an array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,3,2,3,3], k = 2
+ * Output: 6
+ * Explanation: The subarrays that contain the element 3 at least 2 times are: [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+ * Example 2:
+ *
+ * Input: nums = [1,4,2,1], k = 3
+ * Output: 0
+ * Explanation: No subarray contains the element 4 at least 3 times.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 105
+ * 1 <= nums[i] <= 106
+ * 1 <= k <= 105
+ *
+ */
+public class CountSubarraysWhereMaxElementAppearsAtLeastKTimes {
+
+    // V0
+//    public long countSubarrays(int[] nums, int k) {
+//
+//    }
+
+    // V1-1
+    // IDEA: SLIDE WINDOW
+    // https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/editorial/
+    public long countSubarrays_1_1(int[] nums, int k) {
+        int maxElement = Arrays.stream(nums).max().getAsInt();
+        long ans = 0, start = 0;
+        int maxElementsInWindow = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            if (nums[end] == maxElement) {
+                maxElementsInWindow++;
+            }
+            while (maxElementsInWindow == k) {
+                if (nums[(int) start] == maxElement) {
+                    maxElementsInWindow--;
+                }
+                start++;
+            }
+            ans += start;
+        }
+        return ans;
+    }
+
+
+    // V1-2
+    // IDEA: Track Indexes of Max Element
+    // https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/editorial/
+    public long countSubarrays_1_2(int[] nums, int k) {
+        // Finding the maximum element in the array
+
+        int maxElement = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxElement) {
+                maxElement = nums[i];
+            }
+        }
+
+        ArrayList<Integer> indexesOfMaxElements = new ArrayList<>();
+        long ans = 0;
+
+        // Iterating through the array
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] == maxElement) {
+                indexesOfMaxElements.add(index);
+            }
+
+            int freq = indexesOfMaxElements.size();
+            if (freq >= k) {
+                ans += indexesOfMaxElements.get(freq - k) + 1;
+            }
+        }
+        return ans;
+    }
+
+
+
+    // V2
+
+
+
+}
