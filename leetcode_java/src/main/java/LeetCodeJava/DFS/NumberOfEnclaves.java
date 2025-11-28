d436d5e2781cd68a8255fc4bc65737e418d2a257
jim <jim@uber.com>
2025-11-28 10:12:15 +0800

    add 1020 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/DFS/NumberOfEnclaves.java
@@ -0,0 +1,167 @@
+package LeetCodeJava.DFS;
+
+// https://leetcode.com/problems/number-of-enclaves/description/
+
+import java.util.Arrays;
+
+/**
+ *  1020. Number of Enclaves
+ * Medium
+ * Topics
+ * premium lock icon
+ * Companies
+ * Hint
+ * You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+ *
+ * A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+ *
+ * Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
+ * Output: 3
+ * Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.
+ * Example 2:
+ *
+ *
+ * Input: grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
+ * Output: 0
+ * Explanation: All 1s are either on the boundary or can reach the boundary.
+ *
+ *
+ * Constraints:
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 500
+ * grid[i][j] is either 0 or 1.
+ *
+ *
+ */
+public class NumberOfEnclaves {
+
+    // V0
+//    public int numEnclaves(int[][] grid) {
+//
+//    }
+
+    // V1
+    // IDEA: DFS
+    // https://leetcode.ca/2018-09-15-1020-Number-of-Enclaves/
+    private int m;
+    private int n;
+    private int[][] grid;
+
+    public int numEnclaves_1(int[][] grid) {
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && (i == 0 || i == m - 1 || j == 0 || j == n - 1)) {
+                    dfs_1(i, j);
+                }
+            }
+        }
+        int ans = 0;
+        for (int[] row : grid) {
+            for (int v : row) {
+                ans += v;
+            }
+        }
+        return ans;
+    }
+
+    private void dfs_1(int i, int j) {
+        grid[i][j] = 0;
+        int[] dirs = {-1, 0, 1, 0, -1};
+        for (int k = 0; k < 4; ++k) {
+            int x = i + dirs[k], y = j + dirs[k + 1];
+            if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
+                dfs_1(x, y);
+            }
+        }
+    }
+
+
+    // V2
+    // IDEA: DFS
+    // https://leetcode.com/problems/number-of-enclaves/solutions/7377896/leetcode-1020-number-of-enclaves-reverse-t7pv/
+    public int numEnclaves_2(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        // eliminate land cells connected to the first & last row
+        for (int j = 0; j < cols; j++) {
+            if (grid[0][j] == 1)
+                dfs_2(grid, 0, j);
+            if (grid[rows - 1][j] == 1)
+                dfs_2(grid, rows - 1, j);
+        }
+
+        // eliminate land cells connected to the first & last column
+        for (int i = 0; i < rows; i++) {
+            if (grid[i][0] == 1)
+                dfs_2(grid, i, 0);
+            if (grid[i][cols - 1] == 1)
+                dfs_2(grid, i, cols - 1);
+        }
+
+        // count remaining land cells (enclaves)
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                if (grid[i][j] == 1)
+                    count++;
+        }
+        return count;
+    }
+
+    private void dfs_2(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0)
+            return;
+
+        grid[i][j] = 0; // mark border-connected land as removed/safe
+
+        dfs_2(grid, i, j - 1);
+        dfs_2(grid, i - 1, j);
+        dfs_2(grid, i, j + 1);
+        dfs_2(grid, i + 1, j);
+    }
+
+    // V3
+    // IDEA: DFS
+    // https://leetcode.com/problems/number-of-enclaves/solutions/3388131/pythonjavacsimple-solutioneasy-to-unders-y0m8/
+    public int numEnclaves_3(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && grid[i][j] == 1) {
+                    dfs_3(grid, i, j);
+                }
+            }
+        }
+        return Arrays.stream(grid).mapToInt(row -> Arrays.stream(row).sum()).sum();
+    }
+
+    private void dfs_3(int[][] grid, int i, int j) {
+        grid[i][j] = 0;
+        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+        for (int[] direction : directions) {
+            int x = i + direction[0];
+            int y = j + direction[1];
+            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1) {
+                dfs_3(grid, x, y);
+            }
+        }
+    }
+
+
+
+
+}
