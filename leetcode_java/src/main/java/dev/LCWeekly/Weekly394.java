e0bc7d823067c03229c199990ea14073b44d7079
jim <jim@uber.com>
2025-11-30 14:48:54 +0800

    update progress

---
+++ leetcode_java/src/main/java/dev/LCWeekly/Weekly394.java
@@ -0,0 +1,32 @@
+package dev.LCWeekly;
+
+/**
+ * LeetCode weekly contest 394
+ * https://leetcode.com/contest/weekly-contest-394/
+ * 中文題目
+ * https://leetcode.cn/contest/weekly-contest-394/
+ *
+ */
+public class Weekly394 {
+
+    // Q1
+    // LC 3120
+    // https://leetcode.com/problems/count-the-number-of-special-characters-i/
+
+
+    // Q2
+    // LC 3121
+    // https://leetcode.com/problems/count-the-number-of-special-characters-ii/
+
+
+    // Q3
+    // LC 3122
+    // https://leetcode.com/problems/minimum-number-of-operations-to-satisfy-conditions/
+
+
+    // Q4
+    // LC 3123
+    // https://leetcode.com/problems/find-edges-in-shortest-paths/description/
+
+
+}
