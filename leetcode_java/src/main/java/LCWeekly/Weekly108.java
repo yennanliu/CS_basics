597ad0051ed34414dbca53a4f4259b6af3b8697c
jim <jim@uber.com>
2025-12-07 13:27:27 +0800

    update Weekly108.java

--- leetcode_java/src/main/java/LCWeekly/Weekly108.java
+++ leetcode_java/src/main/java/LCWeekly/Weekly108.java
@@ -1,5 +1,7 @@
 package LCWeekly;
 
+import java.util.*;
+
 /**
  * 本周題目
  * LeetCode biweekly contest 108
@@ -129,6 +131,122 @@ public class Weekly108 {
     // Q2
     // LC 2766
     // https://leetcode.com/problems/relocate-marbles/description/
+    // 12.56 - 1.06 pm
+    /**
+     *
+     * -> After completing all the steps,
+     *    return the sorted list of occupied positions.
+     *
+     *
+     *  - nums: 0 idx array
+     *          - init positions of marbles (彈珠)
+     *
+     *  -  two 0-indexed integer (same len)
+     *        - moveFrom
+     *        - moveTo
+     *
+     *  - op:
+     *      - change the position of marbles
+     *      - on i th step
+     *         - move ALL marbles at moveFrom[i] to moveTo[i]
+     *
+     *      - ....
+     *
+     *   - NOTE:
+     *     - occupied: AT LEAST one marble in that position
+     *     - could be MULTIPLE marbles at the same position
+     *
+     * ------------------------
+     *
+     *  IDEA 1) BRUTE FORCE ???
+     *
+     *  IDEA 2) DIFF ARRAY ???
+     *
+     *  IDEA 3) HAHSMAP ??
+     *    map : { position: [idx_1, idx_2 ,...] }
+     *
+     *
+     * ------------------------
+     *
+     *  ex 1)
+     *
+     *  Input: nums = [1,6,7,8], moveFrom = [1,7,2], moveTo = [2,9,5]
+     *
+     *  -> map: { 1: [0], 6: [1], 7: [2], 8: [3] }
+     *
+     *  -> idx = 0, move : 1 -> 2
+     *     map: { 2: [0], 6: [1], 7: [2], 8: [3] }
+     *
+     *     idx = 1, move: 7 -> 9
+     *     map: { 2: [0], 6: [1], 9: [2], 8: [3] }
+     *
+     *     idx =2, move: 2 -> 5
+     *     map: { 5: [0], 6: [1], 9: [2], 8: [3] }
+     *
+     *     -> res = [5, 6, 9, 8]  ???
+     *
+     *
+     *  ex 2)
+     *   nums = [1,1,3,3], moveFrom = [1,3], moveTo = [2,2]
+     *
+     *   ->
+     *    [1,1,3,3]  , idx = 0, move : 1->2
+     *    map : { position: [idx_1, idx_2 ,...] }
+     *    map: {1: [0,1], 3: [2, 3] }
+     *
+     *    -> {2: [0,1], 3: [2, 3]}
+     *
+     *    move: 3 -> 2
+     *
+     *    map: {2: [0,1], 3: [2, 3]}
+     *
+     *    map: {2: [0,1,2,3] }
+     *
+     *    -> [2,2,2,2]
+     *
+     *
+     */
+    //      *  IDEA 3) HAHSMAP ??
+    //     *    map : { position: [idx_1, idx_2 ,...] }
+    public List<Integer> relocateMarbles(int[] nums, int[] moveFrom, int[] moveTo) {
+        // edge
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        // map : { position: [idx_1, idx_2 ,...] }
+        for(int i = 0; i < nums.length; i++){
+            int position = nums[i];
+            if(!map.containsKey(position)){
+                map.put(position, new ArrayList<>());
+            }
+            map.get(position).add(i);
+        }
+
+        System.out.println(">>> (before OP) map = " + map);
+
+        // do op
+        for(int i = 0; i < moveFrom.length; i++){
+            int start = moveFrom[i];
+            int end = moveTo[i];
+            if(map.containsKey(start)){
+                List<Integer> integerList = map.get(start);
+                map.remove(start);
+                map.put(end, integerList); // ???
+            }
+        }
+
+        //Integer[]
+        List<Integer> res = new ArrayList<>(nums.length); // /??
+ 
+        System.out.println(">>> (after OP) map = " + map);
+        // ???
+        for(int key: map.keySet()){
+            for(int idx: map.get(key)){
+                res.set(idx, key);
+            }
+        }
+
+        return res;
+    }
 
     // Q3
     // LC 2767
