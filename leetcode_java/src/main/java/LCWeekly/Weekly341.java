6a5857a83734b344738b00c5b5681cae30899863
jim <jim@uber.com>
2025-12-14 14:42:27 +0800

    update progress, ad weekly341

---
+++ leetcode_java/src/main/java/LCWeekly/Weekly341.java
@@ -0,0 +1,28 @@
+package LCWeekly;
+
+/**
+ * LeetCode weekly contest 341
+ * https://leetcode.com/contest/weekly-contest-341/
+ * 中文題目
+ * https://leetcode.cn/contest/weekly-contest-341/
+ *
+ */
+public class Weekly341 {
+
+    // Q1
+    // LC 2543
+    // https://leetcode.com/problems/row-with-maximum-ones/description/
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        return null;
+    }
+
+    // Q2
+    // LC 2544
+
+    // Q3
+    // LC 2545
+
+    // Q4
+    // LC 2546
+    
+}
