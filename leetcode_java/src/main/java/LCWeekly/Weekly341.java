52bc09f7779d45084049bd521d61680190cde0ff
jim <jim@uber.com>
2025-12-14 14:55:22 +0800

    update /Weekly341.java

--- leetcode_java/src/main/java/LCWeekly/Weekly341.java
+++ leetcode_java/src/main/java/LCWeekly/Weekly341.java
@@ -1,5 +1,7 @@
 package LCWeekly;
 
+import java.util.HashMap;
+
 /**
  * LeetCode weekly contest 341
  * https://leetcode.com/contest/weekly-contest-341/
@@ -10,19 +12,73 @@ package LCWeekly;
 public class Weekly341 {
 
     // Q1
-    // LC 2543
+    // LC 2643
     // https://leetcode.com/problems/row-with-maximum-ones/description/
+    // 14.44 - 54 pm
+    /**
+     *  -> Return an array containing the
+     *     index of the row, and the number of ones in it.
+     *
+     *   - m x n matrix
+     *   - find the 0 indexed position
+     *     contains the maximum count of ones,
+     *     and the number of ones in that row.
+     *
+     *     -> [index, max_cnt_of_one]
+     *
+     *
+     * ------------------------
+     *
+     *  IDEA 1) HASHMAP + ARRAY OP
+     *
+     *
+     * ------------------------
+     *
+     *
+     */
     public int[] rowAndMaximumOnes(int[][] mat) {
-        return null;
+        // edge
+        if(mat == null || mat.length == 0 || mat[0].length == 0){
+            return new int[]{}; // ???
+        }
+      //  HashMap<Integer, Integer> map = new HashMap<>();
+        //int max
+        int maxCnt = 0;
+        int[] res = new int[2];
+
+        // ???
+        for(int i = mat.length - 1; i > 0; i--){
+            int cnt = 0;
+            for(int x: mat[i]){
+                if(x == 1){
+                    cnt += 1;
+                }
+            }
+            if(cnt > maxCnt){
+                res = new int[]{i, cnt};
+            }
+//            maxCnt = Math.max(maxCnt, cnt);
+//            map.put(i, cnt);
+        }
+
+        //int[] res = new int[2];
+        //for(int k: ma)
+
+        return res;
     }
 
     // Q2
-    // LC 2544
+    // https://leetcode.com/problems/find-the-maximum-divisibility-score/
+    // LC 2644
 
     // Q3
-    // LC 2545
+    // LC 2645
+    // https://leetcode.com/problems/minimum-additions-to-make-valid-string/
 
     // Q4
-    // LC 2546
-    
+    // LC 2646
+    // https://leetcode.com/problems/minimize-the-total-price-of-the-trips/
+
+
+
 }
