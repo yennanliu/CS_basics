732cafdc84d6b85a941bffab8a13366dd9b279a0
yennanliu <f339339@gmail.com>
2025-12-16 07:35:19 +0800

    update 841 java

--- leetcode_java/src/main/java/LeetCodeJava/DFS/KeysAndRooms.java
+++ leetcode_java/src/main/java/LeetCodeJava/DFS/KeysAndRooms.java
@@ -96,6 +96,44 @@ public class KeysAndRooms {
         return visited.size() == rooms.size(); // ???
     }
 
+    // V0-2
+    // IDEA: DFS
+    public boolean canVisitAllRooms_0_2(List<List<Integer>> rooms) {
+        // edge
+        if (rooms.isEmpty()) {
+            return true;
+        }
+        if (rooms.size() == 1) {
+            return true;
+        }
+        Set<Integer> visited = new HashSet();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < rooms.size(); i++) {
+            map.putIfAbsent(i, new ArrayList<>());
+            map.get(i).addAll(rooms.get(i)); // ??
+        }
+
+        //System.out.println(">>> map = " + map);
+        visited.add(0); // ?? can add before dfs ?
+        dfsHelper(0, map, visited);
+
+        //System.out.println(">>> visited = " + visited);
+        return visited.size() == rooms.size(); // ???
+    }
+
+    private void dfsHelper(int room, Map<Integer, List<Integer>> map, Set<Integer> visited) {
+        // add to visited
+        //visited.add(room);
+        // visit next
+        for (int next : map.get(room)) {
+            if (!visited.contains(next)) {
+                visited.add(next);
+                dfsHelper(next, map, visited);
+            }
+        }
+        // return true;
+    }
+
 
     // V1
     // IDEA: DFS
