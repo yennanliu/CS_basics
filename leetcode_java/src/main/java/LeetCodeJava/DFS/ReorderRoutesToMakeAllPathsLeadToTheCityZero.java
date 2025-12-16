67aa32bcca9ee8d55c7e81a7490872d9db1de7ac
jim <jim@uber.com>
2025-12-16 09:50:57 +0800

    add 1466 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/DFS/ReorderRoutesToMakeAllPathsLeadToTheCityZero.java
@@ -0,0 +1,164 @@
+package LeetCodeJava.DFS;
+
+// https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/
+
+import java.util.*;
+
+/**
+ * 1466. Reorder Routes to Make All Paths Lead to the City Zero
+ * Medium
+ * Topics
+ * premium lock icon
+ * Companies
+ * Hint
+ * There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between two different cities (this network form a tree). Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+ *
+ * Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
+ *
+ * This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+ *
+ * Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
+ *
+ * It's guaranteed that each city can reach city 0 after reorder.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+ * Output: 3
+ * Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+ * Example 2:
+ *
+ *
+ * Input: n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
+ * Output: 2
+ * Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+ * Example 3:
+ *
+ * Input: n = 3, connections = [[1,0],[2,0]]
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= n <= 5 * 104
+ * connections.length == n - 1
+ * connections[i].length == 2
+ * 0 <= ai, bi <= n - 1
+ * ai != bi
+ *
+ */
+public class ReorderRoutesToMakeAllPathsLeadToTheCityZero {
+
+    // V0
+//    public int minReorder(int n, int[][] connections) {
+//
+//    }
+
+    // V1-1
+    // IDEA: DFS
+    // https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/editorial/
+    int count = 0;
+
+    public void dfs(int node, int parent, Map<Integer, List<List<Integer>>> adj) {
+        if (!adj.containsKey(node)) {
+            return;
+        }
+        for (List<Integer> nei : adj.get(node)) {
+            int neighbor = nei.get(0);
+            int sign = nei.get(1);
+            if (neighbor != parent) {
+                count += sign;
+                dfs(neighbor, node, adj);
+            }
+        }
+    }
+
+    public int minReorder_1_1(int n, int[][] connections) {
+        Map<Integer, List<List<Integer>>> adj = new HashMap<>();
+        for (int[] connection : connections) {
+            adj.computeIfAbsent(connection[0], k -> new ArrayList<List<Integer>>()).add(
+                    Arrays.asList(connection[1], 1));
+            adj.computeIfAbsent(connection[1], k -> new ArrayList<List<Integer>>()).add(
+                    Arrays.asList(connection[0], 0));
+        }
+        dfs(0, -1, adj);
+        return count;
+    }
+
+
+    // V1-2
+    // IDEA: BFS
+    // https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/editorial/
+    int count_1_2 = 0;
+
+    public void bfs(int node, int n, Map<Integer, List<List<Integer>>> adj) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visit = new boolean[n];
+        q.offer(node);
+        visit[node] = true;
+
+        while (!q.isEmpty()) {
+            node = q.poll();
+            if (!adj.containsKey(node)) {
+                continue;
+            }
+            for (List<Integer> nei : adj.get(node)) {
+                int neighbor = nei.get(0);
+                int sign = nei.get(1);
+                if (!visit[neighbor]) {
+                    count_1_2 += sign;
+                    visit[neighbor] = true;
+                    q.offer(neighbor);
+                }
+            }
+        }
+    }
+
+    public int minReorder_1_2(int n, int[][] connections) {
+        Map<Integer, List<List<Integer>>> adj = new HashMap<>();
+        for (int[] connection : connections) {
+            adj.computeIfAbsent(connection[0], k -> new ArrayList<List<Integer>>()).add(
+                    Arrays.asList(connection[1], 1));
+            adj.computeIfAbsent(connection[1], k -> new ArrayList<List<Integer>>()).add(
+                    Arrays.asList(connection[0], 0));
+        }
+        bfs(0, n, adj);
+        return count_1_2;
+    }
+
+
+    // V2
+    // IDEA: DFS
+    // https://leetcode.ca/2019-12-05-1466-Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero/
+    private List<int[]>[] g;
+
+    public int minReorder_2(int n, int[][] connections) {
+        g = new List[n];
+        Arrays.setAll(g, k -> new ArrayList<>());
+        for (int[] e : connections) {
+            int a = e[0], b = e[1];
+            g[a].add(new int[] {b, 1});
+            g[b].add(new int[] {a, 0});
+        }
+        return dfs(0, -1);
+    }
+
+    private int dfs(int a, int fa) {
+        int ans = 0;
+        for (int[] e : g[a]) {
+            int b = e[0], c = e[1];
+            if (b != fa) {
+                ans += c + dfs(b, a);
+            }
+        }
+        return ans;
+    }
+
+
+    // V3
+
+
+}
