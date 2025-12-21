59d24f7f28df1f0372a43d2923ba154d0a458c12
jim <jim@uber.com>
2025-12-21 13:50:34 +0800

    update progress

---
+++ leetcode_java/src/main/java/LCWeekly/Weekly390.java
@@ -0,0 +1,10 @@
+package LCWeekly;
+
+/**
+ * LeetCode weekly contest 390
+ * https://leetcode.com/contest/weekly-contest-390/
+ * 中文題目
+ * https://leetcode.cn/contest/weekly-contest-390/
+ */
+public class Weekly390 {
+}
