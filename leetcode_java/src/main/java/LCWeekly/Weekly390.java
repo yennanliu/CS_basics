7ed63f7d0336f5089c0f0403dd45ec5798467ef6
jim <jim@uber.com>
2025-12-21 14:37:34 +0800

    update ws

--- leetcode_java/src/main/java/LCWeekly/Weekly390.java
+++ leetcode_java/src/main/java/LCWeekly/Weekly390.java
@@ -86,6 +86,101 @@ public class Weekly390 {
     // Q2
     // LC 3091
     // https://leetcode.com/problems/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/
+    // 14.09 - 19 pm
+    /**
+     *  -> Return the minimum number of operations required to make the sum
+     *    of elements of the final array  (>= ) greater than or equal to k
+     *
+     *    -  positive integer k
+     *    -  Initially, nums = [1]
+     *
+     *    - can perform any of the OP on the array any
+     *      number of times (possibly zero)
+     *
+     *      - Choose any element in the array and
+     *         increase its value by 1.
+     *
+     *      - Duplicate any element in the array and
+     *        add it to the end of the array.
+     *
+     *  ------------------
+     *
+     *   IDEA 1) GREEDY
+     *
+     *     - `plus val` first
+     *     - then `duplicate the updated val
+     *
+     *
+     *   IDEA 2) BRUTE FORCE
+     *
+     *   IDEA 3) DP ???
+     *
+     *   IDEA 4) BACKTRACK ???
+     *
+     *  ------------------
+     *
+     *  ex 1)
+     *
+     *  Input: k = 11
+     *  Output: 5
+     *
+     *   1 1 1 1 ....  , cnt = 11
+     *   1,   2,2,2,2,2,2   cnt = 7
+     *   1,2,3  3,3,3,3    cnt = 5
+     *   1,2,3,4  4,4,4    cnt = 5
+     *   1,2,3,4,5   5,5,5  cnt = 6
+     *   1,2,3,4,5,6  6,6  cnt = 6
+     *   1,2,3,4,5,6,7  7,7 cnt = 7
+     *
+     */
+    //  IDEA 2) BRUTE FORCE ???
+    public int minOperations(int k) {
+        // edge
+//        if(k == 1){
+//            return 0;
+//        }
+//        if(k == 2){
+//            return 1;
+//        }
+//        if(k == 3){
+//            return 2;
+//        }
+        if(k <= 3){
+            return k - 1; // ???
+        }
+
+        int minOp = Integer.MAX_VALUE; // ???
+
+        // addition OP: 1 -> 2 -> 3....
+        // Choose any element in the array and increase its value by 1.
+        int op1 = 0;
+
+        // duplicate OP. [1] -> [1,1] -> [1,1,1] -> ...
+        // Duplicate any element in the array and add it to the end of the array.
+        int op2 = 0;
+
+        int curVal = 1;
+
+        // ??? double loop
+        for(int x = 1; x < k; x++){
+            op1 += x;  // ???
+            // ??
+            //x += op1;
+            curVal += 1;
+            op2 = (k / curVal);
+            if(k % curVal != 0){
+                op2 += 1;
+            }
+            minOp = Math.min(minOp, op1 + op2);
+            // ??? early quit ???
+        }
+
+
+        return minOp;
+    }
+
+
+
 
 
     // Q3
