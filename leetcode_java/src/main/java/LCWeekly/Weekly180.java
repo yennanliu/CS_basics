10cc6f473da0d734e26021d4920e3b8e7508a3f1
jim <jim@uber.com>
2025-12-28 15:00:00 +0800

    update progress

---
+++ leetcode_java/src/main/java/LCWeekly/Weekly180.java
@@ -0,0 +1,28 @@
+package LCWeekly;
+
+/**
+ * LeetCode biweekly contest 180
+ * https://leetcode.com/contest/weekly-contest-180/
+ * 中文題目
+ * https://leetcode.cn/contest/weekly-contest-180/
+ *
+ */
+public class Weekly180 {
+
+    // LC 1380
+    // https://leetcode.com/problems/lucky-numbers-in-a-matrix/
+
+
+    // LC 1381
+    // https://leetcode.com/problems/design-a-stack-with-increment-operation/
+
+
+    // LC 1382
+    // https://leetcode.com/problems/balance-a-binary-search-tree/
+
+
+    // LC 1383
+    // https://leetcode.com/problems/maximum-performance-of-a-team/
+
+
+}
