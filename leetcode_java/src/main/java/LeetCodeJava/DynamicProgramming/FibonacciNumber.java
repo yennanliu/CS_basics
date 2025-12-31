1bd820667fa703c4eaefeda5efa08ba257843941
jim <jim@uber.com>
2025-12-31 10:59:09 +0800

    update 509 java

--- leetcode_java/src/main/java/LeetCodeJava/DynamicProgramming/FibonacciNumber.java
+++ leetcode_java/src/main/java/LeetCodeJava/DynamicProgramming/FibonacciNumber.java
@@ -41,9 +41,21 @@ package LeetCodeJava.DynamicProgramming;
 public class FibonacciNumber {
 
     // V0
-//    public int fib(int n) {
-//
-//    }
+    // IDEA: DP
+    public int fib(int n) {
+        // edge
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 
     // V1
     // IDEA: BRUTE FORCE
