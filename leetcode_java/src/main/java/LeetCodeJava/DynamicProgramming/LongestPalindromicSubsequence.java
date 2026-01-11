67d084c2def2bd5ef22f7b299a65545ec59f0f85
jim <jim@uber.com>
2026-01-11 15:45:18 +0800

    update 516 java

--- leetcode_java/src/main/java/LeetCodeJava/DynamicProgramming/LongestPalindromicSubsequence.java
+++ leetcode_java/src/main/java/LeetCodeJava/DynamicProgramming/LongestPalindromicSubsequence.java
@@ -146,9 +146,26 @@ public class LongestPalindromicSubsequence {
     // https://leetcode.com/problems/longest-palindromic-subsequence/
 
     // V4-4
-    // IDEA:
+    // IDEA: Bottom Up Tabulation with space optimization
     // https://leetcode.com/problems/longest-palindromic-subsequence/
+    public int longestPalindromeSubseq_4_4(String s) {
+        int[] dp = new int[s.length()];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            dp[i] = 1;
+            int pre = 0;
+            for (int j = i + 1; j < s.length(); j++) {
+                int tmp = dp[j];
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[j] = pre + 2;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+                pre = tmp;
+            }
+        }
 
+        return dp[s.length() - 1];
+    }
 
 
 
