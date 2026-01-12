1e8c92690b70e7683457b513180d3af6915c5f43
jim <jim@uber.com>
2026-01-13 07:59:22 +0800

    update ws

--- leetcode_java/src/main/java/dev/Workspace19.java
+++ leetcode_java/src/main/java/dev/Workspace19.java
@@ -116,9 +116,76 @@ public class Workspace19 {
 
 
     // LC 376
+    // 7.24 - 34 am
+    /**
+     * -> Given an integer array nums,
+     *   return the length of the `longest wiggle subsequence` of nums.
+     *
+     *
+     *     - NOTE:
+     *       - A subsequence
+     *           - by deleting some elements (possibly zero)
+     *           from the original sequence, leaving the
+     *           remaining elements in their original order.
+     *
+     *   -------------------
+     *
+     *    IDEA 1) BRUTE FORCE
+     *
+     *    IDEA 2) DP
+     *
+     *       1. get diff array
+     *         [ n1-n0, n2-n1, ...]
+     *
+     *     - dp def:
+     *
+     *        boolean dp[i][j]: [i,j] subsequence is wiggle or not
+     *
+     *     - dp eq:
+     *
+     *          dp[i][j] =
+     *             if  diff[j-2] * diff[j-1] < 1:
+     *                dp[i + 1][j - 1] + 2   // ???
+     *             else:
+     *                 max(dp[i+1][j], dp[i][j-1]) // /??
+     *
+     *   -------------------
+     *
+     *
+     *   ex 1)
+     *
+     *
+     *   ex 2)
+     *
+     *    Input: nums = [1,17,5,10,13,15,10,5,16,8]
+     *    Output: 7
+     *
+     *    ->
+     *      diff = (16, -7, 3, -3, 6, -8)
+     *
+     *      
+     *
+     *
+     */
     public int wiggleMaxLength(int[] nums) {
+        // edge
 
-        return 0;
+        int n = nums.length;
+
+        int[] diff = new int[n];
+        for(int i = 1; i < nums.length; i++){
+            diff[i] = nums[i] - nums[i-1];
+        }
+
+        // edge: if all diff > 0 or < 0
+        // -> not wiggle -> return 0
+
+        int maxWiggleLen = 0;
+        int[][] dp = new int[n+1][n+1]; // ????
+
+
+
+        return maxWiggleLen;
     }
 
 
