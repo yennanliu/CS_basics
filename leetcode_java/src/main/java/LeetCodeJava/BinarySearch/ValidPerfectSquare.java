54414e78f120966ade88cb0a9ae3db262beae8e0
jim <jim@uber.com>
2026-01-24 17:34:22 +0800

    update 367 java

--- leetcode_java/src/main/java/LeetCodeJava/BinarySearch/ValidPerfectSquare.java
+++ leetcode_java/src/main/java/LeetCodeJava/BinarySearch/ValidPerfectSquare.java
@@ -1,12 +1,75 @@
 package LeetCodeJava.BinarySearch;
 
 // https://leetcode.com/problems/valid-perfect-square/
-
+/**
+ * 367. Valid Perfect Square
+ * Solved
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * Given a positive integer num, return true if num is a perfect square or false otherwise.
+ *
+ * A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+ *
+ * You must not use any built-in library function, such as sqrt.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: num = 16
+ * Output: true
+ * Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+ * Example 2:
+ *
+ * Input: num = 14
+ * Output: false
+ * Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= num <= 231 - 1
+ */
 public class ValidPerfectSquare {
 
     // V0
     // IDEA : BINARY SEARCH
     public boolean isPerfectSquare(int num) {
+        //edge
+        if (num < 0) {
+            return false;
+        }
+        if (num <= 1) {
+            return true;
+        }
+
+        int l = 0;
+        int r = (int) Math.sqrt(num) + 1; // ??
+//        System.out.println(">>> l = " + l +
+//                ", r = " + r);
+
+        // find
+        while (r >= l) {
+            int mid = (l + r) / 2;
+            int square = mid * mid;
+            if (square == num) {
+                return true;
+            }
+            if (square < num) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    // V0-1
+    // IDEA : BINARY SEARCH
+    public boolean isPerfectSquare_0_1(int num) {
 
         if (num < 2) {
             return true;
@@ -55,6 +118,7 @@ public class ValidPerfectSquare {
         }
         return false;
     }
+
 
 
 }
