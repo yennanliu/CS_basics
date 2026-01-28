762fdac57afa130321c575c64284af5683bb5d0d
jim <jim@uber.com>
2026-01-28 10:09:10 +0800

    update 228 java

--- leetcode_java/src/main/java/LeetCodeJava/Array/SummaryRanges.java
+++ leetcode_java/src/main/java/LeetCodeJava/Array/SummaryRanges.java
@@ -2,54 +2,48 @@ package LeetCodeJava.Array;
 
 // https://leetcode.com/problems/summary-ranges/
 /**
+ *  228. Summary Ranges
+ * Solved
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * You are given a sorted unique integer array nums.
  *
- Code
- Testcase
- Testcase
- Test Result
- 228. Summary Ranges
- Solved
- Easy
- Topics
- premium lock icon
- Companies
- You are given a sorted unique integer array nums.
-
- A range [a,b] is the set of all integers from a to b (inclusive).
-
- Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
-
- Each range [a,b] in the list should be output as:
-
- "a->b" if a != b
- "a" if a == b
-
-
- Example 1:
-
- Input: nums = [0,1,2,4,5,7]
- Output: ["0->2","4->5","7"]
- Explanation: The ranges are:
- [0,2] --> "0->2"
- [4,5] --> "4->5"
- [7,7] --> "7"
- Example 2:
-
- Input: nums = [0,2,3,4,6,8,9]
- Output: ["0","2->4","6","8->9"]
- Explanation: The ranges are:
- [0,0] --> "0"
- [2,4] --> "2->4"
- [6,6] --> "6"
- [8,9] --> "8->9"
-
-
- Constraints:
-
- 0 <= nums.length <= 20
- -231 <= nums[i] <= 231 - 1
- All the values of nums are unique.
- nums is sorted in ascending order.
+ * A range [a,b] is the set of all integers from a to b (inclusive).
+ *
+ * Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+ *
+ * Each range [a,b] in the list should be output as:
+ *
+ * "a->b" if a != b
+ * "a" if a == b
+ * Example 1:
+ *
+ * Input: nums = [0,1,2,4,5,7]
+ * Output: ["0->2","4->5","7"]
+ * Explanation: The ranges are:
+ * [0,2] --> "0->2"
+ * [4,5] --> "4->5"
+ * [7,7] --> "7"
+ * Example 2:
+ *
+ * Input: nums = [0,2,3,4,6,8,9]
+ * Output: ["0","2->4","6","8->9"]
+ * Explanation: The ranges are:
+ * [0,0] --> "0"
+ * [2,4] --> "2->4"
+ * [6,6] --> "6"
+ * [8,9] --> "8->9"
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= nums.length <= 20
+ * -231 <= nums[i] <= 231 - 1
+ * All the values of nums are unique.
+ * nums is sorted in ascending order.
+ *
  *
  */
 import java.util.ArrayList;
@@ -68,6 +62,26 @@ public class SummaryRanges {
 
         // We loop through the array
         for (int r = 0; r < nums.length; r++) {
+            /**  NOTE !!!
+             *
+             *  we use `r + 1 == nums.length`
+             *  to handle `last element` op as well,
+             *  so via this, we don't need the extra
+             *  to paste `last interval` to our answer.
+             *  (e.g. as what V0-1 does)
+             *
+             *
+             *  ```
+             * // final range
+             *    int last = nums.length - 1;
+             *    if (l == last) {
+             *    res.add(String.valueOf(nums[l]));
+             *    } else {
+             *    res.add(nums[l] + "->" + nums[last]);
+             *   }
+             * ```
+             *
+             */
             // We only trigger a "range end" if:
             // 1. We are at the last element of the array
             // 2. OR the next element is not consecutive (gap detected)
@@ -99,6 +113,18 @@ public class SummaryRanges {
         int l = 0;
 
         for (int r = 1; r < nums.length; r++) {
+            /** NOTE !!!
+             *
+             *   we use below to avoid `overflow` error.
+             *
+             *   e.g. below is correct but cause `overflow` error.
+             *
+             *     //...
+             *     if((r-1) - l + 1 == 1){
+             *         // ...
+             *     }
+             *     //...
+             */
             if (nums[r] != nums[r - 1] + 1) {
 
                 if (l == r - 1) {
@@ -122,7 +148,7 @@ public class SummaryRanges {
         return res;
     }
 
-    
+
     // V1
     // https://leetcode.com/problems/summary-ranges/solutions/1805391/concise-solution-in-0-n-with-approach-explained-in-detail/
     // time: O(N), space: O(1)
