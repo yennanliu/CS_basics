49eead3fc8082a6fc12e437c3d35b09d634f0591
jim <jim@uber.com>
2026-02-16 15:19:43 +0800

    update 41 java

--- leetcode_java/src/main/java/LeetCodeJava/Array/FirstMissingPositive.java
+++ leetcode_java/src/main/java/LeetCodeJava/Array/FirstMissingPositive.java
@@ -79,7 +79,130 @@ public class FirstMissingPositive {
         return start;
     }
 
-    // V1
+    // V1-1
+    // IDEA: Boolean Array
+    // https://leetcode.com/problems/first-missing-positive/editorial/
+    public int firstMissingPositive_1_1(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1]; // Array for lookup
+
+        // Mark the elements from nums in the lookup array
+        for (int num : nums) {
+            if (num > 0 && num <= n) {
+                seen[num] = true;
+            }
+        }
+
+        // Iterate through integers 1 to n
+        // return smallest missing positive integer
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                return i;
+            }
+        }
+
+        // If seen contains all elements 1 to n
+        // the smallest missing positive number is n + 1
+        return n + 1;
+    }
+
+
+    // V1-2
+    // IDEA:  Index as a Hash Key
+    // https://leetcode.com/problems/first-missing-positive/editorial/
+    public int firstMissingPositive_1_2(int[] nums) {
+        int n = nums.length;
+        boolean contains1 = false;
+
+        // Replace negative numbers, zeros,
+        // and numbers larger than n with 1s.
+        // After this nums contains only positive numbers.
+        for (int i = 0; i < n; i++) {
+            // Check whether 1 is in the original array
+            if (nums[i] == 1) {
+                contains1 = true;
+            }
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = 1;
+            }
+        }
+
+        if (!contains1)
+            return 1;
+
+        // Mark whether integers 1 to n are in nums
+        // Use index as a hash key and negative sign as a presence detector.
+        for (int i = 0; i < n; i++) {
+            int value = Math.abs(nums[i]);
+            if (value == n) {
+                nums[0] = -Math.abs(nums[0]);
+            } else {
+                nums[value] = -Math.abs(nums[value]);
+            }
+        }
+
+        // First positive in nums is smallest missing positive integer
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0)
+                return i;
+        }
+
+        // nums[0] stores whether n is in nums
+        if (nums[0] > 0) {
+            return n;
+        }
+
+        // If nums contains all elements 1 to n
+        // the smallest missing positive number is n + 1
+        return n + 1;
+    }
+
+
+
+    // V1-3
+    // IDEA: CYCLE SORT
+    // https://leetcode.com/problems/first-missing-positive/editorial/
+    public int firstMissingPositive_1_3(int[] nums) {
+        int n = nums.length;
+
+        // Use cycle sort to place positive elements smaller than n
+        // at the correct index
+        int i = 0;
+        while (i < n) {
+            int correctIdx = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[correctIdx]) {
+                swap(nums, i, correctIdx);
+            } else {
+                i++;
+            }
+        }
+
+        // Iterate through nums
+        // return smallest missing positive integer
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all elements are at the correct index
+        // the smallest missing positive number is n + 1
+        return n + 1;
+    }
+
+    // Swaps two elements in nums
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+
 
     // V2
+
+
+    // V3
+    
+
 }
