304a8cea811f7e3416811587517626dfd6875c0f
yennanliu <f339339@gmail.com>
2026-03-02 10:53:21 +0800

    update 541 java

--- leetcode_java/src/main/java/LeetCodeJava/String/ReverseString2.java
+++ leetcode_java/src/main/java/LeetCodeJava/String/ReverseString2.java
@@ -1,6 +1,7 @@
 package LeetCodeJava.String;
 
 // https://leetcode.com/problems/reverse-string-ii/description/
+// https://leetcode.cn/problems/reverse-string-ii/
 /**
  *  541. Reverse String II
  * Solved
@@ -55,6 +56,61 @@ public class ReverseString2 {
 
 
     // V2
+    // https://leetcode.com/problems/reverse-string-ii/solutions/2609325/java1ms100-faster99-less-memory-usage-by-cxtc/
+    public String reverseStr_2(String s, int k) {
+        char[] str = s.toCharArray();
+        int n = str.length;
+        for (int i = 0; i <= n - 1; i += 2 * k) {
+            if (i + k - 1 <= n - 1) {
+                reverseK(i, i + k - 1, str);
+            } else {
+                //for fewer than k characters left (edge case)
+                reverseK(i, n - 1, str);
+            }
+        }
+        String ans = new String(str);
+        return ans;
+    }
+
+    public void reverseK(int i, int j, char[] str) {
+        while (i < j) {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    // V3
+    // https://leetcode.com/problems/reverse-string-ii/solutions/6998948/reverse-string-ii-on-time-o1-space-most-ppcxs/
+    public void reverse(char[] arr, int i, int j) {
+        while (i <= j) {
+            char t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+
+            i++;
+            j--;
+        }
+    }
+
+    public String reverseStr_3(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = s.length();
+
+        int i = 0;
+        while (i < n) {
+            int j = Math.min(i + k - 1, n - 1);
+            reverse(arr, i, j);
+
+            i += 2 * k;
+        }
+
+        return new String(arr);
+    }
+
+
 
 
 }
