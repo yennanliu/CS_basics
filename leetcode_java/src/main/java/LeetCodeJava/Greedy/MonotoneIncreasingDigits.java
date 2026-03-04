ce4fdf1c4eef2df796a3dcd0afd0f7ec7ba527d1
jim <jim@uber.com>
2026-03-04 16:57:20 +0800

    add 738 java

---
+++ leetcode_java/src/main/java/LeetCodeJava/Greedy/MonotoneIncreasingDigits.java
@@ -0,0 +1,142 @@
+package LeetCodeJava.Greedy;
+
+// https://leetcode.com/problems/monotone-increasing-digits/description/
+/**
+ *  738. Monotone Increasing Digits
+ * Solved
+ * Medium
+ * Topics
+ * premium lock icon
+ * Companies
+ * Hint
+ * An integer has monotone increasing digits if and only if each pair of adjacent digits x and y satisfy x <= y.
+ *
+ * Given an integer n, return the largest number that is less than or equal to n with monotone increasing digits.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 10
+ * Output: 9
+ * Example 2:
+ *
+ * Input: n = 1234
+ * Output: 1234
+ * Example 3:
+ *
+ * Input: n = 332
+ * Output: 299
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= n <= 109
+ *
+ *
+ */
+public class MonotoneIncreasingDigits {
+
+    // V0
+//    public int monotoneIncreasingDigits(int n) {
+//
+//    }
+
+
+    // V1-1
+    // IDEA: GREEDY
+    // https://leetcode.com/problems/monotone-increasing-digits/editorial/
+    public int monotoneIncreasingDigits_1_1(int N) {
+        String S = String.valueOf(N);
+        String ans = "";
+        search: for (int i = 0; i < S.length(); ++i) {
+            for (char d = '1'; d <= '9'; ++d) {
+                if (S.compareTo(ans + repeat(d, S.length() - i)) < 0) {
+                    ans += (char) (d - 1);
+                    continue search;
+                }
+            }
+            ans += '9';
+        }
+        return Integer.parseInt(ans);
+    }
+
+    public String repeat(char c, int count) {
+        StringBuilder sb = new StringBuilder(count);
+        for (int i = 0; i < count; ++i)
+            sb.append(c);
+        return sb.toString();
+    }
+
+
+
+    // V1-2
+    // IDEA: Truncate After Cliff
+    // https://leetcode.com/problems/monotone-increasing-digits/editorial/
+    public int monotoneIncreasingDigits_1_2(int N) {
+        char[] S = String.valueOf(N).toCharArray();
+        int i = 1;
+        while (i < S.length && S[i - 1] <= S[i])
+            i++;
+        while (0 < i && i < S.length && S[i - 1] > S[i])
+            S[--i]--;
+        for (int j = i + 1; j < S.length; ++j)
+            S[j] = '9';
+
+        return Integer.parseInt(String.valueOf(S));
+    }
+
+
+    // V2
+    // https://leetcode.com/problems/monotone-increasing-digits/solutions/3056255/javac-0ms-awesome-approach-100-faster-by-yak8/
+    public int monotoneIncreasingDigits_2(int n) {
+        int position;
+        int digitInTheNextPosition;
+        while ((position = getThePositionNotSatisfied(n)) != -1) {
+            digitInTheNextPosition = ((int) (n / Math.pow(10, position - 1))) % 10;
+            n -= Math.pow(10, position - 1) * (digitInTheNextPosition + 1);
+            n -= n % Math.pow(10, position);
+            n += Math.pow(10, position) - 1;
+        }
+        return n;
+    }
+
+    public int getThePositionNotSatisfied(int n) {
+        int k = 10;
+        int position = 0;
+        while (n > 0) {
+            if (k < n % 10) {
+                return position;
+            } else {
+                k = n % 10;
+                n /= 10;
+                position++;
+            }
+        }
+        return -1;
+    }
+
+
+    // V3
+    public int monotoneIncreasingDigits_3(int n) {
+        char[] arr = String.valueOf(n).toCharArray();
+        int start = arr.length;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > arr[i + 1]) {
+                start = i + 1;
+                arr[i]--;
+            }
+        }
+
+        for (int i = start; i < arr.length; i++) {
+            arr[i] = '9';
+        }
+
+        return Integer.parseInt(String.valueOf(arr));
+    }
+
+
+
+
+
+}
