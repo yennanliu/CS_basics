7bd3155d71e0793f1e97eac0bc91eb324367980a
jim <jim@uber.com>
2026-03-11 19:41:10 +0800

    update 213 java

--- leetcode_java/src/main/java/ws/Workspace22.java
+++ leetcode_java/src/main/java/ws/Workspace22.java
@@ -101,7 +101,73 @@ public class Workspace22 {
      *
      *
      */
+    // 18.25 - 19.01 pm
+    /**
+     *  IDEA 1) 1D DP
+     *
+     *
+     *   - DP def
+     *      - dp[i] = max money can rob at idx = i
+     *
+     *   - DP eq
+     *      *   - 2 cases
+     *      *     - rob idx = 0
+     *      *     - rob idx = nums.len - 1
+     */
     public int rob(int[] nums) {
+        // edge
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+
+        int n = nums.length;
+
+        // ??
+        // dp: rob idx = 0, but NOT rob idx = n-1
+        int[] dp1 = new int[n]; // ???
+        // init !!!
+        dp1[0] = nums[0];
+        //dp1[1] = nums[0]; // ???
+        dp1[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < n - 1; i++){
+           // dp1[i] = Math.max(dp1[i-2] + nums[i], dp1[i-1]);
+            dp1[i] = Math.max(dp1[i - 2] + nums[i], dp1[i - 1]);
+        }
+
+        // dp2: NOT rob idx = 0, but rob idx = n-1
+        int[] dp2 = new int[n]; // ???
+        dp2[0] = 0;
+        dp2[1] = nums[1]; /// ???
+
+        for(int i = 2; i < n; i++){
+            dp2[i] = Math.max(dp2[i-2] + nums[i], dp2[i-1]);
+        }
+
+        // ???
+        return Math.max(dp1[n-2], dp2[n-1]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public int rob_99(int[] nums) {
         // edge
         if(nums == null || nums.length == 0){
             return 0;
