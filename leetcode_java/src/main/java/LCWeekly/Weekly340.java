04a6e9ab527e67b00cf1e3f8696e8e248b966a6f
jim <jim@uber.com>
2026-03-14 14:08:07 +0800

    add weekly 340

---
+++ leetcode_java/src/main/java/LCWeekly/Weekly340.java
@@ -0,0 +1,48 @@
+package LCWeekly;
+
+/**
+ * LeetCode biweekly contest 340
+ * https://leetcode.com/contest/weekly-contest-340/
+ * 中文題目
+ * https://leetcode.cn/contest/weekly-contest-340/
+ *
+ */
+
+// 14.05 - 15.25
+public class Weekly340 {
+
+    // Q1
+    // LC 2614
+    // https://leetcode.com/problems/prime-in-diagonal/description/
+//    public int diagonalPrime(int[][] nums) {
+//
+//    }
+
+    // Q2
+    // LC 2615
+    // https://leetcode.com/problems/sum-of-distances/description/
+//    public long[] distance(int[] nums) {
+//
+//    }
+
+    // Q3
+    // LC 2616
+    // https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/description/
+//    public int minimizeMax(int[] nums, int p) {
+//
+//    }
+
+
+
+    // Q4
+    // LC 2617
+    // https://leetcode.com/problems/minimum-number-of-visited-cells-in-a-grid/
+//    public int minimumVisitedCells(int[][] grid) {
+//
+//    }
+
+
+    
+
+
+}
