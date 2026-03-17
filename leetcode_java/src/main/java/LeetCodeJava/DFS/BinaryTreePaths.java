7f46b3247ed09fcdb8c7cde45625530a1fe14a94
yennanliu <f339339@gmail.com>
2026-03-18 07:15:32 +0800

    fix github page CD: color, pic link, update 257 java, progress

--- leetcode_java/src/main/java/LeetCodeJava/DFS/BinaryTreePaths.java
+++ leetcode_java/src/main/java/LeetCodeJava/DFS/BinaryTreePaths.java
@@ -1,6 +1,37 @@
 package LeetCodeJava.DFS;
 
 // https://leetcode.com/problems/binary-tree-paths/
+/**
+ * 257. Binary Tree Paths
+ * Solved
+ * Easy
+ * Topics
+ * premium lock icon
+ * Companies
+ * Given the root of a binary tree, return all root-to-leaf paths in any order.
+ *
+ * A leaf is a node with no children.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,2,3,null,5]
+ * Output: ["1->2->5","1->3"]
+ * Example 2:
+ *
+ * Input: root = [1]
+ * Output: ["1"]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [1, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
 
 import LeetCodeJava.DataStructure.TreeNode;
 
@@ -131,5 +162,8 @@ public class BinaryTreePaths {
         }
         return paths;
     }
+
+
+
 
 }
