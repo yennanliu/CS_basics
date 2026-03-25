d3b3e08fc3f9e33088f8d999834d0a8b1b27ef27
yennanliu <f339339@gmail.com>
2026-03-26 07:32:54 +0800

    update 542 java

--- leetcode_java/src/main/java/LeetCodeJava/BFS/ZeroOneMatrix.java
+++ leetcode_java/src/main/java/LeetCodeJava/BFS/ZeroOneMatrix.java
@@ -5,41 +5,39 @@ package LeetCodeJava.BFS;
 import java.util.*;
 
 /**
+ *  542. 01 Matrix
+ * Solved
+ * Medium
+ * Topics
+ * premium lock icon
+ * Companies
+ * Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
  *
- 542. 01 Matrix
- Medium
- Topics
- premium lock icon
- Companies
- Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
-
- The distance between two cells sharing a common edge is 1.
-
-
-
- Example 1:
-
-
- Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
- Output: [[0,0,0],[0,1,0],[0,0,0]]
- Example 2:
-
-
- Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
- Output: [[0,0,0],[0,1,0],[1,2,1]]
-
-
- Constraints:
-
- m == mat.length
- n == mat[i].length
- 1 <= m, n <= 104
- 1 <= m * n <= 104
- mat[i][j] is either 0 or 1.
- There is at least one 0 in mat.
-
-
- Note: This question is the same as 1765: https://leetcode.com/problems/map-of-highest-peak/
+ * The distance between two cells sharing a common edge is 1.
+ *
+ * Example 1:
+ *
+ *
+ * Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+ * Output: [[0,0,0],[0,1,0],[0,0,0]]
+ * Example 2:
+ *
+ *
+ * Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+ * Output: [[0,0,0],[0,1,0],[1,2,1]]
+ *
+ *
+ * Constraints:
+ *
+ * m == mat.length
+ * n == mat[i].length
+ * 1 <= m, n <= 104
+ * 1 <= m * n <= 104
+ * mat[i][j] is either 0 or 1.
+ * There is at least one 0 in mat.
+ *
+ *
+ * Note: This question is the same as 1765: https://leetcode.com/problems/map-of-highest-peak/
  *
  *
  */
@@ -528,6 +526,9 @@ public class ZeroOneMatrix {
 
         return result;
     }
+
+
+
 
 
 
