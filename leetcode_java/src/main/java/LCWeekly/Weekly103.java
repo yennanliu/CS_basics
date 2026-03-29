f751577012fea8578fd0d48082326c7ba1801eac
jim <jim@uber.com>
2026-03-29 15:27:15 +0800

    update weekly 103

--- leetcode_java/src/main/java/LCWeekly/Weekly103.java
+++ leetcode_java/src/main/java/LCWeekly/Weekly103.java
@@ -83,10 +83,105 @@ public class Weekly103 {
     }
 
 
-    
 
     // LC 2657
     // https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/
+    /**
+     *
+     *  -> Return the prefix common array of A and B.
+     *
+     *
+     *  - 0 index permutation: A, B with len = n
+     *  - prefix common array of A, B
+     *     - C[i] is equal to the count of numbers
+     *       that are present at or before
+     *       the index i in both A and B
+     *
+     *
+     *
+     *  permutations - 排列
+     *
+     *  NOTE:
+     *
+     *   - A sequence of n integers is
+     *     called a `permutation`
+     *      -> if it contains all integers
+     *       from 1 to n `exactly once.`
+     *
+     *
+     *
+     *
+     *  -----------
+     *
+     *   IDEA 1) BRUTE FORCE
+     *
+     *   IDEA 2) SET ??
+     *
+     *
+     *  -----------
+     *
+     *  ex 1)
+     *
+     *  Input: A = [1,3,2,4], B = [3,1,2,4]
+     *   Output: [0,2,3,4]
+     *
+     *   array with 0=not existed, 1 = existd ???
+     *
+     *
+     *   A = [1,3,2,4],  arr=[1,0,0,0]
+     *        i
+     *
+     *   B = [3,1,2,4]    arr = [0,0,1,0]
+     *        i
+     *
+     *
+     *
+     *
+     *   A = [1,3,2,4],   [1,3]   arr=[1,0,1,0]
+     *          i
+     *
+     *  B = [3,1,2,4]     [3,1], arr = [1,0,1,0]
+     *         j
+     *
+     *
+     */
+    //   IDEA 1) BRUTE FORCE + set ???
+    // a better way ???
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        // edge
+        if(A == null || B == null){
+            return null; // ???
+        }
+
+        int size = A.length;
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+
+        int[] res = new int[size];
+
+        for(int i = 0; i < size; i++){
+            setA.add(A[i]);
+            setB.add(B[i]);
+            res[i] = countHelper(setA, setB);
+        }
+
+        return res;
+    }
+
+    // /??
+    private int countHelper(Set<Integer> setA, Set<Integer> setB){
+        int cnt = 0;
+        for(int x: setA){
+            if(setB.contains(x)){
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+
+
+
+
 
 
     // LC 2658
