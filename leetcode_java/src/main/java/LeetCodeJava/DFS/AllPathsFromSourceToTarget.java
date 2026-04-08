d161c64cc869c56481e0575aee71cb00669f727b
jim <jim@uber.com>
2026-04-08 18:51:20 +0800

    update 797 java

--- leetcode_java/src/main/java/LeetCodeJava/DFS/AllPathsFromSourceToTarget.java
+++ leetcode_java/src/main/java/LeetCodeJava/DFS/AllPathsFromSourceToTarget.java
@@ -45,9 +45,68 @@ import java.util.List;
 public class AllPathsFromSourceToTarget {
 
     // V0
-//    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-//
-//    }
+    // IDEA: DFS (fixed by gpt)
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<Integer> path = new ArrayList<>();
+        path.add(0); // start from source
+        dfs(graph, 0, path);
+        return res;
+    }
+
+    private void dfs(int[][] graph, int node, List<Integer> path) {
+        // if reached target
+        if (node == graph.length - 1) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        // explore neighbors
+        for (int next : graph[node]) {
+            path.add(next);
+            dfs(graph, next, path);
+            path.remove(path.size() - 1); // backtrack
+        }
+    }
+
+
+    // V0-1
+    // IDEA: DFS (fixed by gemini)
+    public List<List<Integer>> allPathsSourceTarget_0_1(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        // Every path starts at node 0
+        path.add(0);
+
+        // Start DFS from node 0, target is the last node (graph.length - 1)
+        dfs(graph, 0, graph.length - 1, path, res);
+
+        return res;
+    }
+
+    private void dfs(int[][] graph, int node, int target, List<Integer> path, List<List<Integer>> res) {
+        // Base case: we reached the target node
+        if (node == target) {
+            // We must create a new copy of the path, otherwise it will be modified later
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        // Standard DFS for DAG: no visited set needed
+        for (int neighbor : graph[node]) {
+            // 1. Choose: Add neighbor to current path
+            path.add(neighbor);
+
+            // 2. Explore: Recurse to next node
+            dfs(graph, neighbor, target, path, res);
+
+            // 3. Un-choose (Backtrack): Remove neighbor to try other branches
+            path.remove(path.size() - 1);
+        }
+    }
+
 
     // V1
     // https://leetcode.com/problems/all-paths-from-source-to-target/solutions/7469999/90-beats-easy-simple-java-solution-by-pr-r3h7/
@@ -96,6 +155,7 @@ public class AllPathsFromSourceToTarget {
         }
     }
 
+    
 
 
 }
