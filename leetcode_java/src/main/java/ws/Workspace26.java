056b824a42f59a915da2f1150839d321f681bd67
jim <jim@uber.com>
2026-05-31 15:28:29 +0800

    update ws

--- leetcode_java/src/main/java/ws/Workspace26.java
+++ leetcode_java/src/main/java/ws/Workspace26.java
@@ -1,4 +1,43 @@
 package ws;
 
+//import LeetCodeJava.DataStructure.TreeNode;
+//import com.sun.org.apache.bcel.internal.generic.IINC;
+//import com.sun.org.apache.bcel.internal.generic.SIPUSH;
+
+
+import java.math.BigInteger;
+import java.util.*;
+
+
 public class Workspace26 {
+
+    // LC 362
+    class HitCounter {
+
+
+
+//        /** Initialize your data structure here. */
+//        public HitCounter() {
+//            counter = new HashMap<>(); // ?? check
+//        }
+
+        /**
+         Record a hit.
+         @param timestamp - The current timestamp (in seconds granularity).
+         */
+        public void hit(int timestamp) {
+        }
+
+        /**
+         Return the number of hits in the past 5 minutes.
+         @param timestamp - The current timestamp (in seconds granularity).
+         */
+        public int getHits(int timestamp) {
+
+            return 0;
+        }
+    }
+
+    
+
 }
