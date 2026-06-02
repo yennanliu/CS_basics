1e51f3c90fa82154a783215d26bc49d0fd05a6da
jim <jim@uber.com>
2026-06-02 18:13:17 +0800

    update 238 py

--- leetcode_python/Array/product-of-array-except-self.py
+++ leetcode_python/Array/product-of-array-except-self.py
@@ -28,6 +28,33 @@ Follow up: Can you solve the problem in O(1) extra space complexity? (The output
 
 """
 
+
+# V0
+# IDEA: prefix,  suffix
+class Solution(object):
+    def productExceptSelf(self, nums):
+        if nums is None:
+            return []
+
+        n = len(nums)
+
+        prefix = [1] * n
+        suffix = [1] * n
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+
+        res = [1] * n
+
+        for i in range(n):
+            res[i] = prefix[i] * suffix[i]
+
+        return res
+
+
 # V0
 # IDEA : array product + deal with 0 case
 #  get all products, 
