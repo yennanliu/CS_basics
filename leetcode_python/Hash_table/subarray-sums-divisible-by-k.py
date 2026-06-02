c3aa2fdd494239c77372c749dc5f147b45ec94d4
jim <jim@uber.com>
2026-06-02 20:13:41 +0800

    update 974 py

--- leetcode_python/Hash_table/subarray-sums-divisible-by-k.py
+++ leetcode_python/Hash_table/subarray-sums-divisible-by-k.py
@@ -1,4 +1,89 @@
+"""
+
+Code
+Testcase
+Testcase
+Test Result
+974. Subarray Sums Divisible by K
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+Example 2:
+
+Input: nums = [5], k = 9
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+2 <= k <= 104
+"""
+
 # V0
+class Solution(object):
+    def subarraysDivByK(self, nums, k):
+        count_map = {0: 1}
+        prefix = 0
+        cnt = 0
+
+        for x in nums:
+            prefix += x
+
+            # normalize modulo (important for negatives)
+            prefix %= k
+
+            cnt += count_map.get(prefix, 0)
+
+            count_map[prefix] = count_map.get(prefix, 0) + 1
+
+        return cnt
+
+# V0-1
+class Solution(object):
+    def subarraysDivByK(self, nums, k):
+        if nums is None:
+            return 0
+            
+        # Map to store: { prefix_remainder : frequency }
+        # Base Case: A remainder of 0 has happened 1 time before we even start
+        remainder_map = {0: 1}
+        
+        cnt = 0
+        prefix = 0
+        
+        for x in nums:
+            # Step 1: Accumulate the running prefix sum
+            prefix += x
+            
+            # Step 2: Keep only the remainder when divided by k
+            prefix = prefix % k
+            
+            # Step 3: If this remainder has been seen before, it means the 
+            # elements in between sum up to a perfect multiple of k!
+            if prefix in remainder_map:
+                cnt += remainder_map[prefix]
+                
+            # Step 4: Record this remainder frequency into the map
+            remainder_map[prefix] = remainder_map.get(prefix, 0) + 1
+            
+        return cnt
+
 
 # V1 
 # https://blog.csdn.net/xx_123_1_rj/article/details/86549560
