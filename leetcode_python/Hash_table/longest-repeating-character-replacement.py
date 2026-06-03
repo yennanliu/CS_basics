7ba2606a8653a5288d79421e13b6fe4389a786c0
jim <jim@uber.com>
2026-06-03 09:22:08 +0800

    update 424 py

--- leetcode_python/Hash_table/longest-repeating-character-replacement.py
+++ leetcode_python/Hash_table/longest-repeating-character-replacement.py
@@ -1,8 +1,13 @@
 """
 
-You are given a string s and an integer k. 
-You can choose any character of the string and change it to any other uppercase English character. 
-You can perform this operation at most k times.
+424. Longest Repeating Character Replacement
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
 Return the length of the longest substring containing the same letter you can get after performing the above operations.
 
  
@@ -12,13 +17,13 @@ Example 1:
 Input: s = "ABAB", k = 2
 Output: 4
 Explanation: Replace the two 'A's with two 'B's or vice versa.
-
 Example 2:
 
 Input: s = "AABABBA", k = 1
 Output: 4
 Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
 The substring "BBBB" has the longest repeating letters, which is 4.
+There may exists other ways to achieve this answer too.
  
 
 Constraints:
@@ -26,8 +31,52 @@ Constraints:
 1 <= s.length <= 105
 s consists of only uppercase English letters.
 0 <= k <= s.length
+ 
 
 """
+
+# V0
+# IDEA : SLIDING WINDOW + MAP + MAX_FREQ_CNT
+class Solution(object):
+    def characterReplacement(self, s, k):
+        cnt = {}
+        l = 0
+        max_freq = 0
+        max_len = 0
+
+        for r in range(len(s)):
+            cnt[s[r]] = cnt.get(s[r], 0) + 1
+
+            """
+            NOTE !!!
+
+             update max_freq
+            """
+            max_freq = max(max_freq, cnt[s[r]])
+
+            """
+            NOTE !!!
+
+             
+             `while (r - l + 1) - max_freq`:
+                -> keep tracking if the total `need to replace` cnt
+                   is till > k 
+                    -> if so, keep `shrink left boundary
+                    -> and update cnt map as well
+            """
+            while (r - l + 1) - max_freq > k:
+                # NOTE !!!
+                # we DON'T need to update `max_freq` here,
+                # since the `s[r]` is the element that
+                # make `max_freq`,
+                # update s[l] DOES NOT affect `max_freq` at the moment
+                cnt[s[l]] -= 1
+                l += 1
+
+            max_len = max(max_len, r - l + 1)
+
+        return max_len
+
 
 # V0
 # IDEA : SLIDING WINDOW + DICT + 2 POINTERS
@@ -86,6 +135,7 @@ class Solution:
             maxLen = max(maxLen, r - l + 1)     
 
         return maxLen
+
 
 # V1
 # https://www.jiuzhang.com/solution/longest-repeating-character-replacement/#tag-highlight-lang-python
