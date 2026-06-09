c93bb83a7622ced7572ab9b6a81d9f47a37aca53
jim <jim@uber.com>
2026-06-09 08:02:40 +0800

    update 5 py

--- leetcode_python/String/longest-palindromic-substring.py
+++ leetcode_python/String/longest-palindromic-substring.py
@@ -97,6 +97,57 @@ class Solution(object):
         return res
 
 
+# V0-2
+# IDEA: 2D DP
+"""
+
+- DP def
+    dp[i][j] = True  if s[i:j+1] is a palindrome
+
+- DP eq
+    ```
+    s[i] == s[j]
+    and
+    (j - i <= 2 or dp[i+1][j-1])
+    ```
+
+"""
+class Solution(object):
+    def longestPalindrome(self, s):
+        if not s:
+            return ""
+
+        n = len(s)
+
+        dp = [[False] * n for _ in range(n)]
+
+        start = 0
+        max_len = 1
+
+        # length 1
+        for i in range(n):
+            dp[i][i] = True
+
+        # length 2+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+
+                if s[i] != s[j]:
+                    dp[i][j] = False
+                else:
+                    if length <= 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+
+                if dp[i][j] and length > max_len:
+                    start = i
+                    max_len = length
+
+        return s[start:start + max_len]
+
+
 # V0
 # IDEA : TWO POINTERS + IDX EXPANSION
 # -> DEAL WITH odd, even len cases
