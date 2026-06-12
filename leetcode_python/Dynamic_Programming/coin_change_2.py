483d8d7791816582c8178c5a3a9219f2d2e72d2d
jim <jim@uber.com>
2026-06-12 09:54:43 +0800

    update 518 py

--- leetcode_python/Dynamic_Programming/coin_change_2.py
+++ leetcode_python/Dynamic_Programming/coin_change_2.py
@@ -42,6 +42,129 @@ All the values of coins are unique.
 
 """
 
+# V0
+# IDEA: 1D DP
+"""
+NOTE !!!
+
+
+DP def:
+
+    - dp[i] = number of ways 
+              to make amount `i`
+
+
+DP eq:
+
+    - for each c,
+        dp[i] += dp[i - c]
+
+
+  
+   -> e.g.:
+        
+        ways(amount=i)
+        =
+        existing ways
+        +
+        ways(amount=i-c) with coin c appended
+
+
+"""
+class Solution(object):
+    def change(self, amount, coins):
+
+        """
+        # NOTE !!!
+        # size = amount + 1
+
+
+        -> why ?
+
+        -> Because the index of dp represents the amount itself.
+
+
+        -> example:
+
+        If amount = 5, you need entries for:
+
+        amount: 0 1 2 3 4 5
+        index : 0 1 2 3 4 5
+
+
+        -> so the size is `6`, not `5`
+
+
+        """
+        dp = [0] * (amount + 1)
+
+        dp[0] = 1
+
+        for c in coins:
+            for i in range(c, amount + 1):
+                dp[i] += dp[i - c]
+
+        """
+        # NOTE !!!
+
+        # use `amount` idx
+        # since 
+
+
+            - dp[i] = number of ways 
+              to make amount `i`
+
+
+
+
+        -> example:
+
+        If amount = 5, you need entries for:
+
+        amount: 0 1 2 3 4 5
+        index : 0 1 2 3 4 5
+
+
+        -> the idx of `amount=5` element
+           is 5 !!! but NOT 6 
+
+        """
+        return dp[amount]
+
+
+
+# V0-1
+# IDEA: 1D DP
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        # dp[i] stores the total number 
+        # of combinations to make change for amount 'i'
+        dp = [0] * (amount + 1)
+        
+        # CRITICAL FIX: Base Case. 
+        # There is exactly 1 way to make an amount of 0 (using no coins).
+        dp[0] = 1
+        
+        # CRITICAL FIX: Loop over coins on the OUTSIDE.
+        # This forces the algorithm to process one coin entirely before moving to the next,
+        # which completely prevents duplicate permutations (like counting [1,2] and [2,1] twice).
+        for coin in coins:
+            # We can only make change for amounts greater than or equal to the current coin value
+            for i in range(coin, amount + 1):
+                # The ways to form amount 'i' is its current ways plus the ways 
+                # to form the remaining amount after subtracting this coin.
+                dp[i] += dp[i - coin]
+                
+        # CRITICAL FIX: Return the final element at index 'amount'
+        return dp[amount]
+
+
+
 # V0 
 # IDEA : DP
 # DP EQUATION : dp[i] += dp[i - coin]
