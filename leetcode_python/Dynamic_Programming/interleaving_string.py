e8b9ef708fc8af8baf4ae6132560ceb6fc2184ff
jim <jim@uber.com>
2026-06-12 10:53:06 +0800

    update 97 py

--- leetcode_python/Dynamic_Programming/interleaving_string.py
+++ leetcode_python/Dynamic_Programming/interleaving_string.py
@@ -134,12 +134,14 @@ class Solution(object):
 
         for i in range(1, m + 1):
             dp[i][0] = (
+            	# need to have matched `prev` indices
                 dp[i - 1][0]
                 and s1[i - 1] == s3[i - 1]
             )
 
         for j in range(1, n + 1):
             dp[0][j] = (
+            	# need to have matched `prev` indices
                 dp[0][j - 1]
                 and s2[j - 1] == s3[j - 1]
             )
