c54ac01c6a33b85ecf21b1adcbd1b4e28545b31b
jim <jim@uber.com>
2026-06-23 11:31:24 +0800

    update 984 py

--- leetcode_python/Greedy/string-without-aaa-or-bbb.py
+++ leetcode_python/Greedy/string-without-aaa-or-bbb.py
@@ -43,3 +43,24 @@ class Solution(object):
 # V1
 
 # V2
+# IDEA: GREEDY
+# https://leetcode.com/problems/string-without-aaa-or-bbb/editorial/
+class Solution(object):
+    def strWithout3a3b(self, A, B):
+        ans = []
+
+        while A or B:
+            if len(ans) >= 2 and ans[-1] == ans[-2]:
+                writeA = ans[-1] == 'b'
+            else:
+                writeA = A >= B
+
+            if writeA:
+                A -= 1
+                ans.append('a')
+            else:
+                B -= 1
+                ans.append('b')
+
+        return "".join(ans)
+
