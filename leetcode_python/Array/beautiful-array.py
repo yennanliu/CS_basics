64146b1f1cc29d0924995af2dcfc06412f7b715f
jim <jim@uber.com>
2026-07-03 10:53:07 +0800

    update 932 py

--- leetcode_python/Array/beautiful-array.py
+++ leetcode_python/Array/beautiful-array.py
@@ -33,7 +33,99 @@ Constraints:
 
 
 
-# V0 
+# V0-1
+# IDEA: Divide-and-Conquer (gpt)
+class Solution(object):
+    def beautifulArray(self, n):
+        # Start with the smallest beautiful array.
+        # A single element is always beautiful.
+        res = [1]
+
+        # Keep expanding the beautiful array until it contains n numbers.
+        while len(res) < n:
+
+            # Store the next generation of the beautiful array.
+            tmp = []
+
+            # -----------------------------
+            # Generate all odd numbers first.
+            # -----------------------------
+            #
+            # If x is in a beautiful array,
+            # then (2*x - 1) is also beautiful.
+            #
+            # We only keep values that are <= n.
+            #
+            # Example:
+            # res = [1, 2]
+            #
+            # x = 1 -> 1
+            # x = 2 -> 3
+            #
+            # Result:
+            # [1, 3]
+            for x in res:
+                if 2 * x - 1 <= n:
+                    tmp.append(2 * x - 1)
+
+            # ------------------------------
+            # Generate all even numbers next.
+            # ------------------------------
+            #
+            # Likewise,
+            # if x is beautiful,
+            # then (2*x) is also beautiful.
+            #
+            # Example:
+            #
+            # res = [1, 2]
+            #
+            # x = 1 -> 2
+            # x = 2 -> 4
+            #
+            # Result:
+            # [2, 4]
+            for x in res:
+                if 2 * x <= n:
+                    tmp.append(2 * x)
+
+            # The newly generated array becomes the current beautiful array.
+            res = tmp
+
+        # Return the final beautiful array.
+        return res
+
+
+
+# V0-2
+# IDEA: Divide-and-Conquer (GEMINI)
+class Solution(object):
+    def __init__(self):
+        # Cache to store previously computed beautiful arrays
+        self.memo = {1: [1]}
+
+    def beautifulArray(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        # If we've already solved for this size, return it from cache
+        if n in self.memo:
+            return self.memo[n]
+            
+        # 1. Map left side to the odd numbers needed for size n
+        # (An odd sequence mapped from a smaller beautiful array remains beautiful)
+        odds = self.beautifulArray((n + 1) // 2)
+        left = [2 * x - 1 for x in odds]
+        
+        # 2. Map right side to the even numbers needed for size n
+        evens = self.beautifulArray(n // 2)
+        right = [2 * x for x in evens]
+        
+        # 3. Combine them: Odds + Evens
+        self.memo[n] = left + right
+        return self.memo[n]
+
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/83539773
