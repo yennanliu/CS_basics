01369a3acbeb849963d4ef72e383e734114b9053
jim <jim@uber.com>
2026-07-04 14:15:57 +0800

    add 1546 py

---
+++ leetcode_python/Greedy/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.py
@@ -0,0 +1,62 @@
+# https://leetcode.com/problems/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/description/
+
+"""
+
+1546. Maximum Number of Non-Overlapping Subarrays With Sum Equals Target
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an array nums and an integer target, return the maximum number of non-empty non-overlapping subarrays such that the sum of values in each subarray is equal to target.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,1,1], target = 2
+Output: 2
+Explanation: There are 2 non-overlapping subarrays [1,1,1,1,1] with sum equals to target(2).
+Example 2:
+
+Input: nums = [-1,3,5,1,4,2,-9], target = 6
+Output: 2
+Explanation: There are 3 subarrays with sum equal to 6.
+([5,1], [4,2], [3,5,1,4,2,-9]) but only the first 2 are non-overlapping.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+0 <= target <= 106
+
+"""
+
+
+# V0
+class Solution(object):
+    def maxNonOverlapping(self, nums, target):
+    	pass
+
+
+# V1
+
+
+# V2
+# IDEA: HASHMAP
+# https://leetcode.com/problems/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/solutions/780954/python-simple-solution-2-sum-variant-by-ihtfh/
+class Solution:
+    def maxNonOverlapping(self, nums: List[int], target: int) -> int:
+        ## RC ##
+        ## APPROACH : HASHMAP - 2 SUM VARIANT ##
+        lookup = {0 : -1}
+        running_sum  = 0
+        count = 0
+        for i in range(len(nums)):
+            running_sum += nums[i]
+            if running_sum - target in lookup:
+                count += 1
+                lookup = {} #reset the map
+            lookup[running_sum] = i
+        return count
