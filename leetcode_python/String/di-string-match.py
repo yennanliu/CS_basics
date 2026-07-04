c8c40e238b02724596b77e40c18ef27e7683eead
jim <jim@uber.com>
2026-07-04 13:05:23 +0800

    update 942 py

--- leetcode_python/String/di-string-match.py
+++ leetcode_python/String/di-string-match.py
@@ -1,4 +1,49 @@
-# V0 
+"""
+
+942. DI String Match
+Easy
+Topics
+premium lock icon
+Companies
+A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
+
+s[i] == 'I' if perm[i] < perm[i + 1], and
+s[i] == 'D' if perm[i] > perm[i + 1].
+Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.
+
+ 
+
+Example 1:
+
+Input: s = "IDID"
+Output: [0,4,1,3,2]
+Example 2:
+
+Input: s = "III"
+Output: [0,1,2,3]
+Example 3:
+
+Input: s = "DDI"
+Output: [3,2,0,1]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either 'I' or 'D'.
+
+
+"""
+
+
+# V0
+class Solution(object):
+    def diStringMatch(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        pass
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/84206493
