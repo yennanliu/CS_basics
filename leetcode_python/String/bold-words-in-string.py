e565f6c19de4e40386d8d25f19fe72319005fb51
jim <jim@uber.com>
2026-07-07 12:01:09 +0800

    update 758 py

--- leetcode_python/String/bold-words-in-string.py
+++ leetcode_python/String/bold-words-in-string.py
@@ -1,5 +1,8 @@
+# https://leetcode.ca/2017-12-27-758-Bold-Words-in-String/
+
 """
 
+758. Bold Words in String
 Given a set of keywords words and a string S, make all appearances of all keywords in S bold. Any letters between <b> and </b> tags become bold.
 
 The returned string should use the least number of tags possible, and of course the tags should form a valid combination.
@@ -12,17 +15,90 @@ words has length in range [0, 50].
 words[i] has length in range [1, 10].
 S has length in range [0, 500].
 All characters in words[i] and S are lowercase letters.
-
-
+Difficulty:
+Easy
+Lock:
+Prime
+Company:
+Google
+Problem Solution
+758-Bold-Words-in-String
+All Problems:
+Link to All Problems
 """
 
 # Time:  O(n * l), n is the length of S, l is the average length of words
 # Space: O(t)    , t is the size of trie
 
-# V0 
+# V0
+class Solution(object):
+    def boldWords(self, words, S):
+        pass 
 
-# V1 
-# DEV 
+
+# V1
+# IDEA: TRIE
+# https://leetcode.ca/2017-12-27-758-Bold-Words-in-String/ 
+class Trie:
+    def __init__(self):
+        self.children = [None] * 128
+        self.is_end = False
+
+    def insert(self, word):
+        node = self
+        for c in word:
+            idx = ord(c)
+            if node.children[idx] is None:
+                node.children[idx] = Trie()
+            node = node.children[idx]
+        node.is_end = True
+
+
+class Solution:
+    def boldWords(self, words: List[str], s: str) -> str:
+        trie = Trie()
+        for w in words:
+            trie.insert(w)
+        n = len(s)
+        pairs = []
+        for i in range(n):
+            node = trie
+            for j in range(i, n):
+                idx = ord(s[j])
+                if node.children[idx] is None:
+                    break
+                node = node.children[idx]
+                if node.is_end:
+                    pairs.append([i, j])
+        if not pairs:
+            return s
+        st, ed = pairs[0]
+        t = []
+        for a, b in pairs[1:]:
+            if ed + 1 < a:
+                t.append([st, ed])
+                st, ed = a, b
+            else:
+                ed = max(ed, b)
+        t.append([st, ed])
+
+        ans = []
+        i = j = 0
+        while i < n:
+            if j == len(t):
+                ans.append(s[i:])
+                break
+            st, ed = t[j]
+            if i < st:
+                ans.append(s[i:st])
+            ans.append('<b>')
+            ans.append(s[st : ed + 1])
+            ans.append('</b>')
+            j += 1
+            i = ed + 1
+
+        return ''.join(ans)
+
 
 # V2 
 # Time:  O(n * l), n is the length of S, l is the average length of words
