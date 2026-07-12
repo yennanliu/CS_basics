90638f59476bdec8634c4121a654ac6289d7dc57
jim <jim@uber.com>
2026-07-12 15:15:05 +0800

    update 593 py

--- leetcode_python/Math/valid-square.py
+++ leetcode_python/Math/valid-square.py
@@ -1,17 +1,59 @@
-# V1  : dev 
-# class Solution(object):
-# 	def validSquare(self, p1, p2, p3, p4):
-# 		def get_length(p1,p2):
-# 			return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2   
-# 		length_sq = 	min( get_length(p1,p2), get_length(p1,p3))	
-# 		print (length_sq)
-# 		count = 0 
-# 		for pair in [[p1,p3], [p1,p4], [p2,p3], [p3,p4]]:
-# 			if ( length_sq != get_length(pair[0], pair[1]) or  length_sq != 2*get_length(pair[0], pair[1])):
-# 				count = count + 1 
-# 				if count > 1:
-# 					return False
-# 		return True 
+"""
+
+593. Valid Square
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given the coordinates of four points in 2D space p1, p2, p3 and p4, return true if the four points construct a square.
+
+The coordinate of a point pi is represented as [xi, yi]. The input is not given in any order.
+
+A valid square has four equal sides with positive length and four equal angles (90-degree angles).
+
+ 
+
+Example 1:
+
+Input: p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,1]
+Output: true
+Example 2:
+
+Input: p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,12]
+Output: false
+Example 3:
+
+Input: p1 = [1,0], p2 = [-1,0], p3 = [0,1], p4 = [0,-1]
+Output: true
+ 
+
+Constraints:
+
+p1.length == p2.length == p3.length == p4.length == 2
+-104 <= xi, yi <= 104
+ 
+
+
+"""
+
+# V0
+class Solution(object):
+    def validSquare(self, p1, p2, p3, p4):
+        """
+        :type p1: List[int]
+        :type p2: List[int]
+        :type p3: List[int]
+        :type p4: List[int]
+        :rtype: bool
+        """
+        pass
+
+
+# V1
+
+
+# V2
 
 
 # V3
@@ -33,4 +75,3 @@ class Solution(object):
                       dist(p1, p4), dist(p2, p3),\
                       dist(p2, p4), dist(p3, p4)])
         return 0 not in lookup and len(lookup) == 2
-
