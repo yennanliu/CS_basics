26a1aa50f071def98a47f40f0637c78eabb32c71
jim <jim@uber.com>
2026-07-12 14:44:55 +0800

    add 1292 py

---
+++ leetcode_python/Binary_Search/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -0,0 +1,126 @@
+# https://leetcode.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/description/
+
+"""
+
+1292. Maximum Side Length of a Square with Sum Less than or Equal to Threshold
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a m x n matrix mat and an integer threshold, return the maximum side-length of a square with a sum less than or equal to threshold or return 0 if there is no such square.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[1,1,3,2,4,3,2],[1,1,3,2,4,3,2],[1,1,3,2,4,3,2]], threshold = 4
+Output: 2
+Explanation: The maximum side length of square with sum less than or equal to 4 is 2 as shown.
+Example 2:
+
+Input: mat = [[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2]], threshold = 1
+Output: 0
+ 
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 300
+0 <= mat[i][j] <= 104
+0 <= threshold <= 105
+ 
+
+
+"""
+
+
+# V0
+class Solution(object):
+    def maxSideLength(self, mat, threshold):
+        """
+        :type mat: List[List[int]]
+        :type threshold: int
+        :rtype: int
+        """
+        pass
+
+
+# V0
+
+# V1
+
+
+# V2-1
+# IDEA: BINARY SEARCH
+# https://leetcode.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/editorial/
+class Solution:
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        m, n = len(mat), len(mat[0])
+        P = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                P[i][j] = (
+                    P[i - 1][j]
+                    + P[i][j - 1]
+                    - P[i - 1][j - 1]
+                    + mat[i - 1][j - 1]
+                )
+
+        def getRect(x1, y1, x2, y2):
+            return P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1]
+
+        l, r, ans = 1, min(m, n), 0
+        while l <= r:
+            mid = (l + r) // 2
+            find = any(
+                getRect(i, j, i + mid - 1, j + mid - 1) <= threshold
+                for i in range(1, m - mid + 2)
+                for j in range(1, n - mid + 2)
+            )
+            if find:
+                ans = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return ans
+
+
+
+
+# V2-2
+# IDEA: Enumeration + Optimization
+# https://leetcode.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/editorial/
+class Solution:
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        m, n = len(mat), len(mat[0])
+        P = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                P[i][j] = (
+                    P[i - 1][j]
+                    + P[i][j - 1]
+                    - P[i - 1][j - 1]
+                    + mat[i - 1][j - 1]
+                )
+
+        def getRect(x1, y1, x2, y2):
+            return P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1]
+
+        r, ans = min(m, n), 0
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                for c in range(ans + 1, r + 1):
+                    if (
+                        i + c - 1 <= m
+                        and j + c - 1 <= n
+                        and getRect(i, j, i + c - 1, j + c - 1) <= threshold
+                    ):
+                        ans += 1
+                    else:
+                        break
+        return ans
+        
