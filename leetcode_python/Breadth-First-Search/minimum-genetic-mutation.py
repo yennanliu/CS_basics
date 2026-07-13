23767659bbce347262ba7813ae821e832c96777a
jim <jim@uber.com>
2026-07-13 08:24:01 +0800

    update 433 py

--- leetcode_python/Breadth-First-Search/minimum-genetic-mutation.py
+++ leetcode_python/Breadth-First-Search/minimum-genetic-mutation.py
@@ -1,4 +1,54 @@
+"""
+
+433. Minimum Genetic Mutation
+Medium
+Topics
+premium lock icon
+Companies
+A gene string can be represented by an 8-character long string, with choices from 'A', 'C', 'G', and 'T'.
+
+Suppose we need to investigate a mutation from a gene string startGene to a gene string endGene where one mutation is defined as one single character changed in the gene string.
+
+For example, "AACCGGTT" --> "AACCGGTA" is one mutation.
+There is also a gene bank bank that records all the valid gene mutations. A gene must be in bank to make it a valid gene string.
+
+Given the two gene strings startGene and endGene and the gene bank bank, return the minimum number of mutations needed to mutate from startGene to endGene. If there is no such a mutation, return -1.
+
+Note that the starting point is assumed to be valid, so it might not be included in the bank.
+
+ 
+
+Example 1:
+
+Input: startGene = "AACCGGTT", endGene = "AACCGGTA", bank = ["AACCGGTA"]
+Output: 1
+Example 2:
+
+Input: startGene = "AACCGGTT", endGene = "AAACGGTA", bank = ["AACCGGTA","AACCGCTA","AAACGGTA"]
+Output: 2
+ 
+
+Constraints:
+
+0 <= bank.length <= 10
+startGene.length == endGene.length == bank[i].length == 8
+startGene, endGene, and bank[i] consist of only the characters ['A', 'C', 'G', 'T'].
+ 
+
+
+"""
+
 # V0 
+class Solution(object):
+    def minMutation(self, startGene, endGene, bank):
+        """
+        :type startGene: str
+        :type endGene: str
+        :type bank: List[str]
+        :rtype: int
+        """
+        pass
+
 
 # V1
 # https://blog.csdn.net/fuxuemingzhu/article/details/82903720
