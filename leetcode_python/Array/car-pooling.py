837bd3d664001b469380afcd9bad4727f6a48abb
jim <jim@uber.com>
2026-07-16 09:49:19 +0800

    add 1094 py

---
+++ leetcode_python/Array/car-pooling.py
@@ -0,0 +1,55 @@
+# https://leetcode.com/problems/car-pooling/description/
+"""
+
+1094. Car Pooling
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
+
+You are given the integer capacity and an array trips where trips[i] = [numPassengersi, fromi, toi] indicates that the ith trip has numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively. The locations are given as the number of kilometers due east from the car's initial location.
+
+Return true if it is possible to pick up and drop off all passengers for all the given trips, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+Output: false
+Example 2:
+
+Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+Output: true
+ 
+
+Constraints:
+
+1 <= trips.length <= 1000
+trips[i].length == 3
+1 <= numPassengersi <= 100
+0 <= fromi < toi <= 1000
+1 <= capacity <= 105
+ 
+
+"""
+
+
+# V0
+class Solution(object):
+    def carPooling(self, trips, capacity):
+        """
+        :type trips: List[List[int]]
+        :type capacity: int
+        :rtype: bool
+        """
+        pass
+
+
+# V1
+
+
+# V2
