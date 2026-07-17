4d1d026e2cb8e520abee9abb8d7bb89e1bfa43d5
jim <jim@uber.com>
2026-07-17 09:57:16 +0800

    update 922 py

--- leetcode_python/Array/sort-array-by-parity-ii.py
+++ leetcode_python/Array/sort-array-by-parity-ii.py
@@ -1,3 +1,44 @@
+# https://leetcode.com/problems/sort-array-by-parity-ii/
+
+"""
+
+922. Sort Array By Parity II
+Easy
+Topics
+premium lock icon
+Companies
+Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+Return any answer array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+Example 2:
+
+Input: nums = [2,3]
+Output: [2,3]
+ 
+
+Constraints:
+
+2 <= nums.length <= 2 * 104
+nums.length is even.
+Half of the integers in nums are even.
+0 <= nums[i] <= 1000
+ 
+
+Follow Up: Could you solve it in-place?
+
+"""
+
+
 # V0
 # time = O(n log n)
 # space = O(n)
