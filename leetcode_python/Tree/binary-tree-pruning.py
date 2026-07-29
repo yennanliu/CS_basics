053218fcc4b176a1e8afe38dbd818840b2c026d2
jim <jim@uber.com>
2026-07-30 07:54:42 +0800

    update 814 py

--- leetcode_python/Tree/binary-tree-pruning.py
+++ leetcode_python/Tree/binary-tree-pruning.py
@@ -1,4 +1,58 @@
+"""
+
+814. Binary Tree Pruning
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+A subtree of a node node is node plus every node that is a descendant of node.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,0,0,1]
+Output: [1,null,0,null,1]
+Explanation: 
+Only the red nodes satisfy the property "every subtree not containing a 1".
+The diagram on the right represents the answer.
+Example 2:
+
+
+Input: root = [1,0,1,0,0,0,1]
+Output: [1,null,1,null,1]
+Example 3:
+
+
+Input: root = [1,1,0,1,1,0,1,0]
+Output: [1,1,0,1,1,null,1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 200].
+Node.val is either 0 or 1.
+
+"""
+
 # V0
+class Solution(object):
+    def pruneTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        pass
+
+
+# V0-1
+
+
+# V0-2
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79858752
