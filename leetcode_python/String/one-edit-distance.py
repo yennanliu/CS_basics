fbeacbb486653cad53fb2c904d33eec9a80cdf84
jim <jim@uber.com>
2026-08-07 12:10:39 +0800

    update 161 py

--- leetcode_python/String/one-edit-distance.py
+++ leetcode_python/String/one-edit-distance.py
@@ -40,6 +40,37 @@ class Solution:
         pass
 
 
+# V0-0-1
+# IDEA: DP (gpt)
+# https://github.com/yennanliu/CS_basics/blob/master/leetcode_java/src/main/java/LeetCodeJava/String/OneEditDistance.java#L415
+class Solution:
+    def isOneEditDistance(self, s, t):
+        m, n = len(s), len(t)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        # base cases
+        for i in range(m + 1):
+            dp[i][0] = i
+
+        for j in range(n + 1):
+            dp[0][j] = j
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(
+                        dp[i-1][j],     # delete
+                        dp[i][j-1],     # insert
+                        dp[i-1][j-1]    # replace
+                    )
+
+        return dp[m][n] == 1
+
+
 # V0-1
 # IDEA : RECURSION (gpt)
 class Solution:
