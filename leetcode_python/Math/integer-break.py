5b8312692588595e7915153d23f56291de4c5f6b
jim <jim@uber.com>
2026-08-09 08:08:46 +0800

    update 343 py

--- leetcode_python/Math/integer-break.py
+++ leetcode_python/Math/integer-break.py
@@ -1,3 +1,53 @@
+"""
+
+343. Integer Break
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+Return the maximum product you can get.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: 2 = 1 + 1, 1 × 1 = 1.
+Example 2:
+
+Input: n = 10
+Output: 36
+Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+ 
+
+Constraints:
+
+2 <= n <= 58
+ 
+
+"""
+
+# V0
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        pass
+
+
+# V0-1
+
+
+# V0-2
+
+
 # V0
 # time = O(n)
 # space = O(n)
