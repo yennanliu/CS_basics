a937d78e1fdaa6134a7a6df53dd36adccbfe2193
jim <jim@uber.com>
2026-08-10 15:07:07 +0800

    update 108 py

--- leetcode_python/Recursion/convert-sorted-array-to-binary-search-tree.py
+++ leetcode_python/Recursion/convert-sorted-array-to-binary-search-tree.py
@@ -1,3 +1,50 @@
+"""
+
+108. Convert Sorted Array to Binary Search Tree
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+ 
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in a strictly increasing order.
+ 
+
+
+"""
+
+
+# V0
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        pass
+
 # V0
 # time = O(n log n), slicing costs O(n) total per level, O(log n) levels
 # space = O(n), slices along recursion path sum to O(n)
