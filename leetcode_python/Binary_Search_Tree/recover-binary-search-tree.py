f0a26c0644ee7efc31530f1da1b857fd2e1aaed3
jim <jim@uber.com>
2026-08-13 15:51:23 +0800

    add 99 py

---
+++ leetcode_python/Binary_Search_Tree/recover-binary-search-tree.py
@@ -0,0 +1,63 @@
+# https://leetcode.com/problems/recover-binary-search-tree/description/
+
+"""
+
+99. Recover Binary Search Tree
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,3,null,null,2]
+Output: [3,1,null,null,2]
+Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+Example 2:
+
+
+Input: root = [3,1,4,null,null,2]
+Output: [2,1,4,null,null,3]
+Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 1000].
+-231 <= Node.val <= 231 - 1
+ 
+
+Follow up: A solution using O(n) space is pretty straight-forward. Could you devise a constant O(1) space solution?
+
+"""
+
+
+# V0
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def recoverTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        pass
+
+
+# V0-1
+
+# V0-2
+
+# V1
+
+
+# V2
