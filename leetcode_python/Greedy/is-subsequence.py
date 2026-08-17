344a0f07dd98fb09d3cee220f80f1ecb9f1d2780
jim <jim@uber.com>
2026-08-18 07:40:48 +0800

    update 392 py

--- leetcode_python/Greedy/is-subsequence.py
+++ leetcode_python/Greedy/is-subsequence.py
@@ -1,4 +1,50 @@
-# V0 
+"""
+
+392. Is Subsequence
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+ 
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+ 
+
+
+
+"""
+
+# V0
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        pass
 
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/79568772
