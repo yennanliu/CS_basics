f4386c4fe8fb44d9245f3b63d4ef431e5c3d7412
jim <jim@uber.com>
2026-08-28 08:47:28 +0800

    update 1143 py

--- leetcode_python/Dynamic_Programming/longest-common-subsequence.py
+++ leetcode_python/Dynamic_Programming/longest-common-subsequence.py
@@ -36,6 +36,96 @@ text1 and text2 consist of only lowercase English characters.
 
 """
 
+
+# V0
+# IDEA: 2D DP
+"""
+
+DP def:
+
+        dp[i][j] = LCS length of:
+
+                   text1[:i] and text2[:j]
+
+        where:
+            0 <= i <= len(text1)
+            0 <= j <= len(text2)
+
+        So dp size = (n1 + 1) × (n2 + 1)
+
+
+
+DP eq:
+
+
+        if text1[i-1] == text2[j-1]:
+
+            dp[i][j] = dp[i-1][j-1] + 1
+
+        else:
+
+            dp[i][j] = max(
+                dp[i-1][j],      # skip text1[i-1]
+                dp[i][j-1]       # skip text2[j-1]
+            )
+
+"""
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+
+        n1 = len(text1)
+        n2 = len(text2)
+
+        # NOTE !!!
+        # size = `n + 1`
+
+        # dp[i][j] =
+        # LCS length of text1[:i] and text2[:j]
+        dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
+
+
+        """
+        NOTE !!!
+
+        no need to setup `DP init`
+
+        e.g. below is NO needed
+
+        ```
+        for i in range(n1):
+            dp[i][0] = 1
+
+        for j in range(n2):
+            dp[0][j] = 1
+
+        ```
+
+
+        -> since if either str is "", the result should be 0, NOT 1
+
+        """
+
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+
+                if text1[i - 1] == text2[j - 1]:
+                    # Take this character
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+
+                else:
+                    # Skip text1[i - 1]
+                    # OR
+                    # Skip text2[j - 1]
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    )
+
+        # NOTE !!!
+        # final used idx is n1, n2
+        return dp[n1][n2]
+
+
 # V0
 # IDEA: 2D DP
 """
