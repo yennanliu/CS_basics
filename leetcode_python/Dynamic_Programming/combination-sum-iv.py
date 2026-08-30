1e4f224a1d66b7394bb60d6131e92d7356758b3c
jim <jim@uber.com>
2026-08-30 09:11:04 +0800

    update 377 py

--- leetcode_python/Dynamic_Programming/combination-sum-iv.py
+++ leetcode_python/Dynamic_Programming/combination-sum-iv.py
@@ -1,3 +1,74 @@
+"""
+
+377. Combination Sum IV
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The test cases are generated so that the answer can fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+Example 2:
+
+Input: nums = [9], target = 3
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 1000
+All the elements of nums are unique.
+1 <= target <= 1000
+ 
+
+Follow up: What if negative numbers are allowed in the given array? How does it change the problem? What limitation we need to add to the question to allow negative numbers?
+
+
+"""
+
+# V0
+# IDEA: 1D DP (`combination` pattern)
+class Solution(object):
+    def combinationSum4(self, nums, target):
+
+        # dp[i] = number of combinations to build sum i
+        # where ORDER MATTERS
+        dp = [0] * (target + 1)
+
+        # One way to build 0:
+        # choose nothing
+        dp[0] = 1
+
+        # Build every target from small -> large
+        for i in range(1, target + 1):
+
+            for num in nums:
+
+                if i >= num:
+                    dp[i] += dp[i - num]
+
+        return dp[target]
+
+
 # VO 
 # IDEA : DP 
 # dp[x+y] = dp[x+y] + dp[x] (for all x in range(target + 1), for all y in nums)
