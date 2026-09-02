ec8145f8d89fa82dbe9c9dab5ba45079bde9d07b
jim <jim@uber.com>
2026-09-02 11:18:13 +0800

    update 279 py

--- leetcode_python/Dynamic_Programming/perfect-squares.py
+++ leetcode_python/Dynamic_Programming/perfect-squares.py
@@ -1,3 +1,36 @@
+"""
+
+279. Perfect Squares
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer n, return the least number of perfect square numbers that sum to n.
+
+A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+
+ 
+
+Example 1:
+
+Input: n = 12
+Output: 3
+Explanation: 12 = 4 + 4 + 4.
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+ 
+
+Constraints:
+
+1 <= n <= 104
+"""
+
+
+
 # V0
 # IDEA : BFS
 """
