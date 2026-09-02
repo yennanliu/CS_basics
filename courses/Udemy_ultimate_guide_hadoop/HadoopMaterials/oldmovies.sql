f1679ce9d25bf019883140e2763d856dc9d3ff36
jim <jim@uber.com>
2026-09-03 07:36:11 +0800

    remove no needed courses

--- courses/Udemy_ultimate_guide_hadoop/HadoopMaterials/oldmovies.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE movies;
-CREATE EXTERNAL TABLE movies (movie_id INT, title STRING, release DATE) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '/user/maria_dev/movies/';
-INSERT OVERWRITE DIRECTORY '${OUTPUT}' SELECT * FROM movies WHERE release < '1940-01-01' ORDER BY release;
