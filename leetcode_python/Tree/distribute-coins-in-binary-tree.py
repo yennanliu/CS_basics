e33e3ecd7cb21e72c2c92606bd9548e3c00dd4ae
jim <jim@uber.com>
2026-09-06 11:38:31 +0800

    update 979 py

--- leetcode_python/Tree/distribute-coins-in-binary-tree.py
+++ leetcode_python/Tree/distribute-coins-in-binary-tree.py
@@ -54,6 +54,85 @@ class Solution(object):
         pass
 
 
+# V0-1
+# IDEA: Postorder DFS + Balance (GPT)
+class Solution(object):
+
+    def distributeCoins(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+
+        self.moves = 0
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            # 先處理 left / right subtree
+            left_balance = dfs(node.left)
+            right_balance = dfs(node.right)
+
+            # 每個 subtree 都需要透過 edge
+            # 傳送 |balance| 顆 coin
+            self.moves += abs(left_balance)
+            self.moves += abs(right_balance)
+
+            # node 自己留下 1 顆
+            # 剩下的 coin 可以傳給 parent
+            balance = (
+                node.val
+                + left_balance
+                + right_balance
+                - 1
+            )
+
+            return balance
+
+        dfs(root)
+
+        return self.moves
+
+
+# V0-2
+# IDEA: Postorder DFS + Balance (GEMINI)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def distributeCoins(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        self.moves = 0
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            # 1. 後序遍歷：先拿到左右子樹的硬幣淨餘缺量
+            left_balance = dfs(node.left)
+            right_balance = dfs(node.right)
+
+            # 2. 計算當前子樹總共多出或缺少多少硬幣 (當前節點自留 1 個)
+            current_balance = left_balance + right_balance + (node.val - 1)
+
+            # 3. 累積移動次數：無論是輸出還是輸入，通過這條邊的流量就是 |current_balance|
+            self.moves += abs(current_balance)
+
+            # 4. 回傳當前子樹的 balance 給父節點
+            return current_balance
+
+        dfs(root)
+        return self.moves
+
+
 # V1 
 # https://blog.csdn.net/fuxuemingzhu/article/details/86563872
 # https://blog.csdn.net/danspace1/article/details/88737508
