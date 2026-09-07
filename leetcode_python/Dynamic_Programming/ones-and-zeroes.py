530ae641c7ce812638a106236f6b80ea7f21dd4b
jim <jim@uber.com>
2026-09-07 17:46:11 +0800

    update 474 py

--- leetcode_python/Dynamic_Programming/ones-and-zeroes.py
+++ leetcode_python/Dynamic_Programming/ones-and-zeroes.py
@@ -1,4 +1,53 @@
+"""
+
+474. Ones and Zeroes
+Medium
+Topics
+premium lock icon
+Companies
+You are given an array of binary strings strs and two integers m and n.
+
+Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
+
+A set x is a subset of a set y if all elements of x are also elements of y.
+
+ 
+
+Example 1:
+
+Input: strs = ["10","0001","111001","1","0"], m = 5, n = 3
+Output: 4
+Explanation: The largest subset with at most 5 0's and 3 1's is {"10", "0001", "1", "0"}, so the answer is 4.
+Other valid but smaller subsets include {"0001", "1"} and {"10", "1", "0"}.
+{"111001"} is an invalid subset because it contains 4 1's, greater than the maximum of 3.
+Example 2:
+
+Input: strs = ["10","0","1"], m = 1, n = 1
+Output: 2
+Explanation: The largest subset is {"0", "1"}, so the answer is 2.
+ 
+
+Constraints:
+
+1 <= strs.length <= 600
+1 <= strs[i].length <= 100
+strs[i] consists only of digits '0' and '1'.
+1 <= m, n <= 100
+ 
+
+
+"""
+
 # V0
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        pass
 
 # V1 
 # http://bookshadow.com/weblog/2016/12/11/leetcode-ones-and-zeroes/
@@ -105,6 +154,7 @@ class Solution(object):
                     dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
         return dp[m][n]
 
+
 # V1''
 # https://www.jiuzhang.com/solution/ones-and-zeroes/#tag-highlight-lang-python
 # IDEA : DP
@@ -159,6 +209,7 @@ class Solution:
                     if dp[i - one][j - zero] + 1 > dp[i][j]:
                         dp[i][j] = dp[i - one][j - zero] + 1
         return dp[-1][-1]
+
 
 # V2
 """
