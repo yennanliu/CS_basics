78275470055b8e3ae7e23a0d194a5ed03856e74e
jim <jim@uber.com>
2026-09-11 18:00:43 +0800

    update 2289 py

--- leetcode_python/Stack/steps-to-make-array-non-decreasing.py
+++ leetcode_python/Stack/steps-to-make-array-non-decreasing.py
@@ -34,6 +34,16 @@ Constraints:
 """
 
 # V0
+class Solution(object):
+    def totalSteps(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        pass
+
+
+# V0
 # IDEA : MONOTONIC STACK + DP ("WHEN DOES nums[i] GET EATEN?")
 #
 #   Simulating the rounds is O(n^2). Instead ask, per element, the only
@@ -87,6 +97,42 @@ class Solution(object):
 
 
 # V0-1
+# IDEA : Monotonic Stack + DP -> LEFT (gemini)
+class Solution(object):
+
+    def totalSteps(self, nums):
+        """
+        :type nums: List[int]
+
+        :rtype: int
+        """
+        # 棧中儲存二元組：(數值 num, 該數字被消除所需的步數 step)
+        stack = []
+        max_steps = 0
+
+        for num in nums:
+            cur_steps = 0
+
+            # 單調遞減棧：當前 num >= 棧頂數字時，說明 num 會存活並「吞併」這些較小數字
+            # num 必須等待這些較小數字被消除完後，才有機會被更左邊的更大數字消除
+            while stack and stack[-1][0] <= num:
+                cur_steps = max(cur_steps, stack.pop()[1])
+
+            # 如果棧不為空，說明左邊有一個嚴格大於 num 的數字可以消除它
+            if stack:
+                cur_steps += 1
+            else:
+                # 棧為空，說明 num 是當前最大值，永遠不會被消除
+                cur_steps = 0
+
+            # 更新全局最大步數
+            max_steps = max(max_steps, cur_steps)
+            stack.append((num, cur_steps))
+
+        return max_steps
+
+
+# V0-2
 # IDEA : SAME DP, SCANNED RIGHT -> LEFT
 #
 #   Justified as a variant (not a respelling): scanning from the right lets
